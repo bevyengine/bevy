48cf0190ad250210d7d4ce4369d18c88b5280f03
Carter Anderson <mcanders1@gmail.com>
2020-01-18 15:38:57 -0800

    add comments about future entity archetype plans

--- src/ecs/archetypes.rs
+++ src/ecs/archetypes.rs
@@ -5,8 +5,13 @@ use legion::prelude::*;
 // builder macro that makes defaults easy? Object3dBuilder { Option<Material> } impl Builder for Object3dBuilder { }
 pub trait EntityArchetype {
     fn insert(self, world: &mut World) -> Entity;
+
+    // this would make composing entities from multiple archetypes possible
     // add_components appears to be missing from World. it will be less efficient without that
     // fn add_components(self, world: &mut World);
+
+    // generate by macro. maybe a separate macro?
+    // fn query() -> Query
 }
 
 pub struct Object3dEntity {
