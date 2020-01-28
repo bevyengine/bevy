6ba659049dffca30bb2e98dd3952aba588be74ea
Carter Anderson <mcanders1@gmail.com>
2020-01-27 19:36:06 -0800

    cleanup

--- src/render/render_graph_2/draw_target.rs
+++ src/render/render_graph_2/draw_target.rs
@@ -2,7 +2,7 @@ use crate::{
     asset::{AssetStorage, Handle, Mesh},
     legion::prelude::*,
     render::{
-        render_graph_2::{ShaderUniforms, RenderPass},
+        render_graph_2::{RenderPass, ShaderUniforms},
         Instanced,
     },
 };
@@ -10,45 +10,51 @@ use crate::{
 use zerocopy::AsBytes;
 
 // A set of draw calls. ex: get + draw meshes, get + draw instanced meshes, draw ui meshes, etc
+
+// TODO: consider swapping out dyn RenderPass for explicit WgpuRenderPass type to avoid dynamic dispatch
 pub type DrawTarget = fn(world: &World, render_pass: &mut dyn RenderPass);
 
 const MESH_VERTEX_BUFFER_NAME: &str = "TempMeshVertexBuffer";
 const MESH_INDEX_BUFFER_NAME: &str = "TempMeshIndexBuffer";
 pub fn mesh_draw_target(world: &World, render_pass: &mut dyn RenderPass) {
     let mut mesh_storage = world.resources.get_mut::<AssetStorage<Mesh>>().unwrap();
-    let mut last_mesh_id = None;
+    let mut current_mesh_id = None;
+    let mut current_mesh_index_length = 0;
     let mesh_query =
         <(Read<ShaderUniforms>, Read<Handle<Mesh>>)>::query().filter(!component::<Instanced>());
     for (shader_uniforms, mesh) in mesh_query.iter(world) {
-        let current_mesh_id = mesh.id;
-
-        let mut should_load_mesh = last_mesh_id == None;
-        if let Some(last) = last_mesh_id {
-            should_load_mesh = last != current_mesh_id;
+        let mut should_load_mesh = current_mesh_id == None;
+        if let Some(current) = current_mesh_id {
+            should_load_mesh = current != mesh.id;
         }
 
         if should_load_mesh {
             if let Some(mesh_asset) = mesh_storage.get(mesh.id) {
                 let renderer = render_pass.get_renderer();
-                renderer.create_buffer_with_data(MESH_VERTEX_BUFFER_NAME, mesh_asset.vertices.as_bytes(), wgpu::BufferUsage::VERTEX);
-                renderer.create_buffer_with_data(MESH_INDEX_BUFFER_NAME, mesh_asset.indices.as_bytes(), wgpu::BufferUsage::INDEX);
-                
+                renderer.create_buffer_with_data(
+                    MESH_VERTEX_BUFFER_NAME,
+                    mesh_asset.vertices.as_bytes(),
+                    wgpu::BufferUsage::VERTEX,
+                );
+                renderer.create_buffer_with_data(
+                    MESH_INDEX_BUFFER_NAME,
+                    mesh_asset.indices.as_bytes(),
+                    wgpu::BufferUsage::INDEX,
+                );
+
                 // TODO: Verify buffer format matches render pass
                 render_pass.set_index_buffer(MESH_INDEX_BUFFER_NAME, 0);
                 render_pass.set_vertex_buffer(0, MESH_VERTEX_BUFFER_NAME, 0);
+                current_mesh_id = Some(mesh.id);
+                current_mesh_index_length = mesh_asset.indices.len() as u32;
             };
         }
 
-        // TODO: re-getting the mesh isn't necessary. just store the index count
-        if let Some(mesh_asset) = mesh_storage.get(mesh.id) {
-            // TODO: validate bind group properties against shader uniform properties at least once
-            render_pass.setup_bind_groups(&&*shader_uniforms);
-            render_pass.draw_indexed(0..mesh_asset.indices.len() as u32, 0, 0..1);
-        };
-
-        last_mesh_id = Some(current_mesh_id);
+        // TODO: validate bind group properties against shader uniform properties at least once
+        render_pass.setup_bind_groups(&&*shader_uniforms);
+        render_pass.draw_indexed(0..current_mesh_index_length, 0, 0..1);
     }
-    
+
     // cleanup buffers
     let renderer = render_pass.get_renderer();
     renderer.remove_buffer(MESH_VERTEX_BUFFER_NAME);
