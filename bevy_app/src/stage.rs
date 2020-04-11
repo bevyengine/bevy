78d19580905e4de41c013d9c0a2b1555933aa360
Carter Anderson <mcanders1@gmail.com>
2020-04-10 18:25:10 -0700

    add some stage docs (and POST_UPDATE stage)

--- bevy_app/src/stage.rs
+++ bevy_app/src/stage.rs
@@ -1,6 +1,17 @@
+/// Name of app stage that runs once when an app starts up
 pub const STARTUP: &str = "startup";
 
+/// Name of app stage that runs before all other app stages
 pub const FIRST: &str = "first";
+
+/// Name of app stage that updates events. Generally this should run before UPDATE
 pub const EVENT_UPDATE: &str = "event_update";
+
+/// Name of app stage responsible for doing most app logic. Systems should be registered here by default.
 pub const UPDATE: &str = "update";
+
+/// Name of app stage responsible for processing the results of UPDATE. Runs immediately after UPDATE.
+pub const POST_UPDATE: &str = "post_update";
+
+/// Name of app stage that runs after all other app stages
 pub const LAST: &str = "last";
