736faa3f46ff475feaed7fd948c2b03dd173c265
Carter Anderson <mcanders1@gmail.com>
2020-05-03 17:05:45 -0700

    import FromBytes for glam types

--- crates/bevy_glam/src/f32/glam_zerocopy.rs
+++ crates/bevy_glam/src/f32/glam_zerocopy.rs
@@ -1,7 +1,14 @@
-use zerocopy::AsBytes;
+use zerocopy::{FromBytes, AsBytes};
 use crate::{Vec2, Vec3, Vec4, Mat2, Mat3, Mat4, Quat};
 
 unsafe impl AsBytes for Vec2 {
+    fn only_derive_is_allowed_to_implement_this_trait()
+    where
+        Self: Sized {}
+    
+}
+
+unsafe impl FromBytes for Vec2 {
     fn only_derive_is_allowed_to_implement_this_trait()
     where
         Self: Sized {}
@@ -15,7 +22,21 @@ unsafe impl AsBytes for Vec3 {
     
 }
 
+unsafe impl FromBytes for Vec3 {
+    fn only_derive_is_allowed_to_implement_this_trait()
+    where
+        Self: Sized {}
+    
+}
+
 unsafe impl AsBytes for Vec4 {
+    fn only_derive_is_allowed_to_implement_this_trait()
+    where
+        Self: Sized {}
+    
+}
+
+unsafe impl FromBytes for Vec4 {
     fn only_derive_is_allowed_to_implement_this_trait()
     where
         Self: Sized {}
@@ -29,7 +50,21 @@ unsafe impl AsBytes for Mat2 {
     
 }
 
+unsafe impl FromBytes for Mat2 {
+    fn only_derive_is_allowed_to_implement_this_trait()
+    where
+        Self: Sized {}
+    
+}
+
 unsafe impl AsBytes for Mat3 {
+    fn only_derive_is_allowed_to_implement_this_trait()
+    where
+        Self: Sized {}
+    
+}
+
+unsafe impl FromBytes for Mat3 {
     fn only_derive_is_allowed_to_implement_this_trait()
     where
         Self: Sized {}
@@ -43,7 +78,21 @@ unsafe impl AsBytes for Mat4 {
     
 }
 
+unsafe impl FromBytes for Mat4 {
+    fn only_derive_is_allowed_to_implement_this_trait()
+    where
+        Self: Sized {}
+    
+}
+
 unsafe impl AsBytes for Quat {
+    fn only_derive_is_allowed_to_implement_this_trait()
+    where
+        Self: Sized {}
+    
+}
+
+unsafe impl FromBytes for Quat {
     fn only_derive_is_allowed_to_implement_this_trait()
     where
         Self: Sized {}
