d3e0196cbbb38510fd7d08cd6982e13a16af11f9
Carter Anderson <mcanders1@gmail.com>
2020-05-24 11:01:48 -0700

    move derive(Properties) into bevy_properties

--- crates/bevy_property/bevy_property_derive/src/modules.rs
+++ crates/bevy_property/bevy_property_derive/src/modules.rs
@@ -5,16 +5,7 @@ use syn::{DeriveInput, Path};
 #[derive(FromMeta, Debug)]
 pub struct ModuleAttributeArgs {
     #[darling(default)]
-    pub bevy_render: Option<String>,
-    #[darling(default)]
-    pub bevy_asset: Option<String>,
-    #[darling(default)]
-    pub bevy_core: Option<String>,
-    #[darling(default)]
-    pub bevy_app: Option<String>,
-    #[darling(default)]
-    pub legion: Option<String>,
-
+    pub bevy_property: Option<String>,
     /// If true, it will use the meta "bevy" crate for dependencies by default (ex: bevy:app). If this is set to false, the individual bevy crates
     /// will be used (ex: "bevy_app"). Defaults to "true"
     #[darling(default)]
@@ -22,47 +13,43 @@ pub struct ModuleAttributeArgs {
 }
 
 pub struct Modules {
-    pub bevy_render: String,
-    pub bevy_asset: String,
-    pub bevy_core: String,
-    pub bevy_app: String,
-    pub legion: String,
+    pub bevy_property: String,
 }
 
 impl Modules {
     pub fn meta() -> Modules {
         Modules {
-            bevy_asset: "bevy::asset".to_string(),
-            bevy_render: "bevy::render".to_string(),
-            bevy_core: "bevy::core".to_string(),
-            bevy_app: "bevy::app".to_string(),
-            legion: "bevy".to_string(),
+            bevy_property: "bevy::property".to_string(),
         }
     }
 
     pub fn external() -> Modules {
         Modules {
-            bevy_asset: "bevy_asset".to_string(),
-            bevy_render: "bevy_render".to_string(),
-            bevy_core: "bevy_core".to_string(),
-            bevy_app: "bevy_app".to_string(),
-            legion: "legion".to_string(),
+            bevy_property: "bevy_property".to_string(),
         }
     }
 }
 
+#[cfg(feature = "default_bevy_meta")]
 impl Default for ModuleAttributeArgs {
     fn default() -> Self {
         ModuleAttributeArgs {
-            bevy_asset: None,
-            bevy_render: None,
-            bevy_core: None,
-            bevy_app: None,
-            legion: None,
+            bevy_property: None,
             meta: true,
         }
     }
 }
+
+#[cfg(not(feature = "default_bevy_meta"))]
+impl Default for ModuleAttributeArgs {
+    fn default() -> Self {
+        ModuleAttributeArgs {
+            bevy_property: None,
+            meta: false,
+        }
+    }
+}
+
 
 pub static MODULE_ATTRIBUTE_NAME: &'static str = "module";
 
@@ -82,20 +69,8 @@ pub fn get_modules(ast: &DeriveInput) -> Modules {
             Modules::external()
         };
 
-        if let Some(path) = module_attribute_args.bevy_asset {
-            modules.bevy_asset = path;
-        }
-
-        if let Some(path) = module_attribute_args.bevy_render {
-            modules.bevy_render = path;
-        }
-
-        if let Some(path) = module_attribute_args.bevy_core {
-            modules.bevy_core = path;
-        }
-
-        if let Some(path) = module_attribute_args.bevy_app {
-            modules.bevy_app = path;
+        if let Some(path) = module_attribute_args.bevy_property {
+            modules.bevy_property = path;
         }
 
         modules
--- crates/bevy_property/src/lib.rs
+++ crates/bevy_property/src/lib.rs
@@ -8,4 +8,5 @@ pub use property::*;
 pub use properties::*;
 pub use dynamic_properties::*;
 
+pub use bevy_property_derive::*; 
 pub use serde;
