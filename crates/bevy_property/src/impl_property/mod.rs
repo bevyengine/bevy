91af49ede342e6aed4edf6047984bb239042feb1
Carter Anderson <mcanders1@gmail.com>
2020-05-27 16:06:44 -0700

    props: move property impls to their own mod

---
+++ crates/bevy_property/src/impl_property/mod.rs
@@ -0,0 +1,4 @@
+mod impl_property_glam;
+mod impl_property_legion;
+mod impl_property_smallvec;
+mod impl_property_std;
--- crates/bevy_property/src/lib.rs
+++ crates/bevy_property/src/lib.rs
@@ -2,21 +2,14 @@ mod property;
 mod properties;
 mod dynamic_properties;
 mod type_registry;
-mod impl_property_std;
-mod impl_property_smallvec;
-mod impl_property_glam;
-mod impl_property_legion;
 pub mod ron;
 pub mod property_serde;
+pub mod impl_property;
 
 pub use property::*;
 pub use properties::*;
 pub use dynamic_properties::*;
 pub use type_registry::*;
-pub use impl_property_std::*;
-pub use impl_property_glam::*;
-pub use impl_property_smallvec::*;
-pub use impl_property_legion::*;
 
 pub use bevy_property_derive::*; 
 pub use serde;
