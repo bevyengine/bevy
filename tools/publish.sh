773b7bffa982f5ce2d932a7596923426dfd4ccb3
Carter Anderson <mcanders1@gmail.com>
2020-08-09 18:32:03 -0700

    update publish script

--- tools/publish.sh
+++ tools/publish.sh
@@ -1,33 +1,39 @@
+# if crate A depends on crate B, B must come before A in this list
 crates=(
+    bevy_derive
+    bevy_math
+    bevy_ecs/hecs/macros
+    bevy_ecs/hecs
+    bevy_ecs
     bevy_app
+    bevy_property/bevy_property_derive
+    bevy_property
+    bevy_type_registry
     bevy_asset
     bevy_audio
     bevy_core
-    bevy_derive
     bevy_diagnostic
-    bevy_ecs
-    bevy_ecs/hecs
-    bevy_ecs/hecs/macros
+    bevy_transform
+    bevy_window
+    bevy_render
     bevy_gltf
     bevy_input
-    bevy_math
     bevy_pbr
-    bevy_property
-    bevy_render
     bevy_scene
     bevy_sprite
     bevy_text
-    bevy_transform
-    bevy_type_registry
     bevy_ui
-    bevy_wgpu
-    bevy_window
     bevy_winit
+    bevy_wgpu
 )
 
 cd crates
 for crate in "${crates[@]}"
 do
   echo "Publishing ${crate}"
-  (cd $crate; cargo build)
+  (cd $crate; cargo publish)
+  sleep 30
 done
+
+cd ..
+cargo publish
