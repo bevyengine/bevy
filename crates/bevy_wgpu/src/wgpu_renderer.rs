f49d07c0ccfbe09bd2438c4a3f58ef40f78f0cba
Carter Anderson <mcanders1@gmail.com>
2020-08-12 18:53:56 -0700

    re-enable high performance gpu preference

--- crates/bevy_wgpu/src/wgpu_renderer.rs
+++ crates/bevy_wgpu/src/wgpu_renderer.rs
@@ -21,7 +21,7 @@ impl WgpuRenderer {
         let instance = wgpu::Instance::new(wgpu::BackendBit::PRIMARY);
         let adapter = instance
             .request_adapter(&wgpu::RequestAdapterOptions {
-                power_preference: wgpu::PowerPreference::Default,
+                power_preference: wgpu::PowerPreference::HighPerformance,
                 compatible_surface: None,
             })
             .await
