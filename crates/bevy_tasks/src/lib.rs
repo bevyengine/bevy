17e764261189be7487f624c0680a69bc3aaad05d
Lachlan Sneff <lachlan.sneff@gmail.com>
2020-08-29 15:35:41 -0400

    Task System for Bevy (#384)
    Add bevy_tasks crate to replace rayon

---
+++ crates/bevy_tasks/src/lib.rs
@@ -0,0 +1,26 @@
+mod slice;
+pub use slice::{ParallelSlice, ParallelSliceMut};
+
+mod task;
+pub use task::Task;
+
+mod task_pool;
+pub use task_pool::{Scope, TaskPool, TaskPoolBuilder};
+
+mod usages;
+pub use usages::{AsyncComputeTaskPool, ComputeTaskPool, IOTaskPool};
+
+pub mod prelude {
+    pub use crate::{
+        slice::{ParallelSlice, ParallelSliceMut},
+        usages::{AsyncComputeTaskPool, ComputeTaskPool, IOTaskPool},
+    };
+}
+
+pub fn logical_core_count() -> usize {
+    num_cpus::get()
+}
+
+pub fn physical_core_count() -> usize {
+    num_cpus::get_physical()
+}
