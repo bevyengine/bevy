e58d186caaa2cdea523a3fc11e811d96fe839389
Daniel McNab <36049421+DJMcNab@users.noreply.github.com>
2020-08-31 11:30:00 +0100

    Support skipping fields in derived `Bundle`s
    Annote fields with `#[bundle(skip)]` to skip them

--- crates/bevy_ecs/hecs/macros/src/lib.rs
+++ crates/bevy_ecs/hecs/macros/src/lib.rs
@@ -19,15 +19,18 @@ extern crate proc_macro;
 use proc_macro::TokenStream;
 use proc_macro2::Span;
 use proc_macro_crate::crate_name;
-use quote::quote;
-use syn::{parse_macro_input, DeriveInput, Path};
+use quote::{quote, quote_spanned};
+use syn::{parse_macro_input, spanned::Spanned, DeriveInput, Path};
 
 /// Implement `Bundle` for a monomorphic struct
 ///
 /// Using derived `Bundle` impls improves spawn performance and can be convenient when combined with
 /// other derives like `serde::Deserialize`.
+///
+/// Attributes: `#[bundle(skip)]` on fields, skips that field. Requires that the field type
+/// implements `Default`
 #[allow(clippy::cognitive_complexity)]
-#[proc_macro_derive(Bundle)]
+#[proc_macro_derive(Bundle, attributes(bundle))]
 pub fn derive_bundle(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
     if !input.generics.params.is_empty() {
@@ -44,7 +47,12 @@ pub fn derive_bundle(input: TokenStream) -> TokenStream {
         }
     };
     let ident = input.ident;
-    let (tys, fields) = struct_fields(&data.fields);
+    let (fields, skipped) = match struct_fields(&data.fields) {
+        Ok(fields) => fields,
+        Err(stream) => return stream,
+    };
+    let tys = fields.iter().map(|f| f.0).collect::<Vec<_>>();
+    let field_names = fields.iter().map(|f| f.1.clone()).collect::<Vec<_>>();
     let path_str = if crate_name("bevy").is_ok() {
         "bevy::ecs"
     } else if crate_name("bevy_ecs").is_ok() {
@@ -55,7 +63,7 @@ pub fn derive_bundle(input: TokenStream) -> TokenStream {
 
     let path: Path = syn::parse(path_str.parse::<TokenStream>().unwrap()).unwrap();
 
-    let n = tys.len();
+    let n = fields.len();
     let code = quote! {
         impl #path::DynamicBundle for #ident {
             fn with_ids<T>(&self, f: impl FnOnce(&[std::any::TypeId]) -> T) -> T {
@@ -68,9 +76,9 @@ pub fn derive_bundle(input: TokenStream) -> TokenStream {
 
             unsafe fn put(mut self, mut f: impl FnMut(*mut u8, std::any::TypeId, usize) -> bool) {
                 #(
-                    if f((&mut self.#fields as *mut #tys).cast::<u8>(), std::any::TypeId::of::<#tys>(), std::mem::size_of::<#tys>()) {
+                    if f((&mut self.#field_names as *mut #tys).cast::<u8>(), std::any::TypeId::of::<#tys>(), std::mem::size_of::<#tys>()) {
                         #[allow(clippy::forget_copy)]
-                        std::mem::forget(self.#fields);
+                        std::mem::forget(self.#field_names);
                     }
                 )*
             }
@@ -113,31 +121,62 @@ pub fn derive_bundle(input: TokenStream) -> TokenStream {
                 mut f: impl FnMut(std::any::TypeId, usize) -> Option<std::ptr::NonNull<u8>>,
             ) -> Result<Self, #path::MissingComponent> {
                 #(
-                    let #fields = f(std::any::TypeId::of::<#tys>(), std::mem::size_of::<#tys>())
+                    let #field_names = f(std::any::TypeId::of::<#tys>(), std::mem::size_of::<#tys>())
                             .ok_or_else(#path::MissingComponent::new::<#tys>)?
                             .cast::<#tys>()
                         .as_ptr();
                 )*
-                Ok(Self { #( #fields: #fields.read(), )* })
+                Ok(Self { #( #field_names: #field_names.read(), )* #(#skipped: Default::default(),)* })
             }
         }
     };
+    dbg!(code.to_string());
     TokenStream::from(code)
 }
 
-fn struct_fields(fields: &syn::Fields) -> (Vec<&syn::Type>, Vec<syn::Ident>) {
+fn struct_fields(
+    fields: &syn::Fields,
+) -> Result<(Vec<(&syn::Type, syn::Ident)>, Vec<syn::Ident>), TokenStream> {
+    let mut final_fields = Vec::new();
+    let mut skipped = Vec::new();
     match fields {
-        syn::Fields::Named(ref fields) => fields
-            .named
-            .iter()
-            .map(|f| (&f.ty, f.ident.clone().unwrap()))
-            .unzip(),
-        syn::Fields::Unnamed(ref fields) => fields
-            .unnamed
-            .iter()
-            .enumerate()
-            .map(|(i, f)| (&f.ty, syn::Ident::new(&i.to_string(), Span::call_site())))
-            .unzip(),
-        syn::Fields::Unit => (Vec::new(), Vec::new()),
+        syn::Fields::Named(ref fields) => {
+            for field in &fields.named {
+                if should_include_in_bundle(field)? {
+                    final_fields.push((&field.ty, field.ident.clone().unwrap()));
+                } else {
+                    skipped.push(field.ident.clone().unwrap());
+                }
+            }
+        }
+        syn::Fields::Unnamed(ref fields) => {
+            for (i, field) in fields.unnamed.iter().enumerate() {
+                if should_include_in_bundle(field)? {
+                    final_fields.push((
+                        &field.ty,
+                        syn::Ident::new(&i.to_string(), Span::call_site()),
+                    ));
+                } else {
+                    skipped.push(syn::Ident::new(&i.to_string(), Span::call_site()));
+                }
+            }
+        }
+        syn::Fields::Unit => {}
+    };
+    return Ok((final_fields, skipped));
+}
+
+fn should_include_in_bundle(f: &syn::Field) -> Result<bool, TokenStream> {
+    for attr in &f.attrs {
+        if attr.path.is_ident("bundle") {
+            let string = attr.tokens.to_string();
+            if attr.tokens.to_string() == "(skip)" {
+                return Ok(false);
+            } else {
+                let error = format!("Invalid bundle attribute #[bundle{}]", string);
+                return Err(quote_spanned! {attr.span().into() => compile_error!(#error)}.into());
+            }
+        }
     }
+    return Ok(true);
 }
