8feaccd0f7cf2d88fa4f3fae540fa88b115b19c1
Pradeep <raj.pradeep.kumar@gmail.com>
2020-09-04 22:39:23 +0530

    - added* apis to bind/unbind keyboard actions

--- crates/bevy_prototype_input_map/src/inputmap.rs
+++ crates/bevy_prototype_input_map/src/inputmap.rs
@@ -16,3 +16,49 @@ pub struct InputMap
     mouse_just_released_map: HashMap<MouseButton, String>,
 }
 
+impl InputMap
+{
+    // keyboard
+    pub fn BindKeyboardPressed(&mut self, code: KeyCode, action: String)
+    {
+        self.keyboard_pressed_map.insert(
+            code,
+            action
+            );
+    }
+    pub fn BindKeyboardJustPressed(&mut self, code: KeyCode, action: String)
+    {
+        self.keyboard_just_pressed_map.insert(
+            code,
+            action
+            );
+    }
+    pub fn BindKeyboardJustReleased(&mut self, code: KeyCode, action: String)
+    {
+        self.keyboard_just_released_map.insert(
+            code,
+            action
+            );
+    }
+    pub fn UnBindKeyboardPressed(&mut self, code: KeyCode, action: String)
+    {
+        self.keyboard_pressed_map.remove(
+            code,
+            action
+            );
+    }
+    pub fn UnBindKeyboardJustPressed(&mut self, code: KeyCode, action: String)
+    {
+        self.keyboard_just_pressed_map.remove(
+            code,
+            action
+            );
+    }
+    pub fn UnBindKeyboardJustReleased(&mut self, code: KeyCode, action: String)
+    {
+        self.keyboard_just_released_map.remove(
+            code,
+            action
+            );
+    }
+}
