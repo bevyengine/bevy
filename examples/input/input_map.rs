ccdbdddb77b21ffe24ed570d8b4dc33b3da0624b
PradeepKumar <raj.pradeep.kumar@gmail.com>
2020-09-06 14:54:40 +0530

    - added* shoot - added* exit - minor* cleanup

--- examples/input/input_map.rs
+++ examples/input/input_map.rs
@@ -1,13 +1,17 @@
 use bevy::prelude::*;
 use bevy_prototype_input_map::keyboard::KeyboardMap;
 use bevy_prototype_input_map::inputmap::InputMap;
+use bevy_app::AppExit;
 
 fn main() {
     App::build()
         .add_default_plugins()
+
+        // setup
         .add_plugin(bevy_prototype_input_map::InputMapPlugin::default())
         .add_startup_system(setup.system())
         .add_system(action_system.system())
+        
         .run();
 }
 
@@ -15,19 +19,34 @@ fn setup(
     mut key_map: ResMut<KeyboardMap>
 )
 {
+    // keyboard
+    key_map.BindKeyboardPressed(KeyCode::Up, "JUMP".to_string(), 0.25);
     key_map.BindKeyboardPressed(KeyCode::Space, "JUMP".to_string(), 0.25);
+
+    key_map.BindKeyboardPressed(KeyCode::Return, "SHOOT".to_string(), 0.25);
+
+    key_map.BindKeyboardPressed(KeyCode::Escape, "QUIT_APP".to_string(), 0.25);
+    
 }
 
 /// This system prints 'A' key state
 fn action_system(
-    input_map: Res<InputMap>
+    input_map: Res<InputMap>,
+    mut app_exit_events: ResMut<Events<AppExit>>
     ) {
         if input_map.IsActionPressed("JUMP".to_string())
         {
             println!("Jumping...");
         }
-        else
+
+        if input_map.IsActionPressed("SHOOT".to_string())
         {
-            println!("Not Jumping...");
+            println!("Bang");
+        }
+
+        if input_map.IsActionPressed("QUIT_APP".to_string())
+        {
+            println!("Quiting...");
+            app_exit_events.send(AppExit);
         }
 }
