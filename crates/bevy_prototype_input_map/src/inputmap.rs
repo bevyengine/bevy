4cf1d4dd1e211060a07180f3f661ac748af66ba3
PradeepKumar <raj.pradeep.kumar@gmail.com>
2020-09-06 13:06:00 +0530

    - new* struct keyboard - moved* keyboard code

--- crates/bevy_prototype_input_map/src/inputmap.rs
+++ crates/bevy_prototype_input_map/src/inputmap.rs
@@ -1,29 +1,29 @@
 use bevy_input::{mouse::MouseButton, prelude::KeyCode};
 use std::collections::HashMap;
 
-// TODO need to compute deadzone for GetActionStrength()
-pub(crate) struct InputMap {
+// TODO deadzone
+#[derive(Default)]
+pub struct InputMap {
+    // crate
     // action data
-    pub(crate) action_raw_strength: HashMap<String, f32>,
-    pub(crate) action_deadzone: HashMap<String, f32>,
-
-    // input map
-    pub(crate) keyboard_pressed_map: HashMap<KeyCode, String>,
-    pub(crate) mouse_pressed_map: HashMap<MouseButton, String>,
+    action_raw_strength: HashMap<String, f32>,
+    action_deadzone: HashMap<String, f32>,
 }
 
 impl InputMap {
-    // actions
+    // publics
     pub fn GetActionStrength(&self, action: String) -> f32 {
         self.action_raw_strength[&action]
-    }
-    pub fn SetActionStrength(&mut self, action: String, strength: f32) {
-        self.action_raw_strength.insert(action, strength);
     }
     pub fn IsActionPressed(&self, action: String) -> bool {
         self.GetActionStrength(action) > 0.0
     }
-    pub fn ResetActionStrength(&mut self, action: String) {
-        self.SetActionStrength(action, 0.0)
+
+    // crates
+    pub(crate) fn SetRawActionStrength(&mut self, action: String, strength: f32) {
+        self.action_raw_strength.insert(action, strength);
+    }
+    pub(crate) fn ResetActionStrength(&mut self, action: String) {
+        self.SetRawActionStrength(action, 0.0)
     }
 }
--- crates/bevy_prototype_input_map/src/keyboard.rs
+++ crates/bevy_prototype_input_map/src/keyboard.rs
@@ -3,30 +3,39 @@ use bevy_ecs::{Res, ResMut};
 use bevy_input::{prelude::KeyCode, Input};
 use std::collections::HashMap;
 
-// keyboard
-impl InputMap {
-    pub fn GetKeyPressedBindingMap(&self) -> &HashMap<KeyCode, String> {
-        &self.keyboard_pressed_map
-    }
+#[derive(Default)]
+pub struct KeyboardMap {
+    action_binding: HashMap<KeyCode, String>,
+}
 
+impl KeyboardMap {
+    // publics
     pub fn BindKeyboardPressed(&mut self, code: KeyCode, action: String, deadzone: f32) {
-        self.keyboard_pressed_map.insert(code, action);
+        self.action_binding.insert(code, action);
     }
     pub fn UnBindKeyboardPressed(&mut self, code: KeyCode, action: String) {
-        self.keyboard_pressed_map.remove(&code);
+        self.action_binding.remove(&code);
     }
 
+    // crates
+    // pub(crate) fn GetBindings(&mut self) -> &HashMap<KeyCode, String> {
+    //     &self.action_binding
+    // }
+
     // system
-    pub fn keyboard_input_map_system(
+    pub(crate) fn action_system(
         mut input_map: ResMut<InputMap>,
+        key_map: Res<KeyboardMap>,
         key_input: Res<Input<KeyCode>>,
     ) {
-        let mut bindings = &mut input_map.GetKeyPressedBindingMap();
-        for (keycode, action) in bindings.iter() {
+        let mut map = &mut input_map;
+        let bindings_iter = key_map.action_binding.iter();
+
+        for (keycode, action) in bindings_iter {
             if key_input.pressed(*keycode) {
-                input_map.action_raw_strength.insert(action.clone(), 1.0);
+                map.SetRawActionStrength(action.clone(), 1.0);
             } else {
-                input_map.action_raw_strength.insert(action.clone(), 0.0);
+                map.ResetActionStrength(action.clone());
             }
         }
     }
