49b084d8e2a020a31ed40bd8d5d3f6e0ecc7bb10
PradeepKumar <raj.pradeep.kumar@gmail.com>
2020-09-06 16:07:05 +0530

    - missed* commit

--- crates/bevy_prototype_input_map/src/lib.rs
+++ crates/bevy_prototype_input_map/src/lib.rs
@@ -1,22 +1,31 @@
 pub mod inputmap;
 pub mod keyboard;
+pub mod mouse;
+pub mod axis;
 
 use crate::inputmap::InputMap;
 use bevy_app::prelude::*;
 use bevy_ecs::IntoQuerySystem;
 use keyboard::KeyboardMap;
+use mouse::MouseMap;
 
 #[derive(Default)]
 pub struct InputMapPlugin;
 
 impl Plugin for InputMapPlugin {
     fn build(&self, app: &mut AppBuilder) {
-        app.init_resource::<InputMap>()
+        app
+            // input map
+            .init_resource::<InputMap>()
+            .add_system_to_stage(stage::EVENT_UPDATE, InputMap::action_reset_system.system())
 
             // keyboard
             .init_resource::<KeyboardMap>()
-            .add_system_to_stage(stage::EVENT_UPDATE, KeyboardMap::action_reset_system.system())
             .add_system_to_stage(stage::UPDATE, KeyboardMap::action_update_system.system())
+
+            // mouse
+            .init_resource::<MouseMap>()
+            .add_system_to_stage(stage::UPDATE, MouseMap::action_update_system.system())
             ;
     }
 }
