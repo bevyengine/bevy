feb5e1c23b03e1f4899230c0c3ed73c9c0de4cec
PradeepKumar <raj.pradeep.kumar@gmail.com>
2020-09-06 11:32:39 +0530

    - implemented* build

---
+++ crates/bevy_prototype_input_map/src/lib.rs
@@ -0,0 +1,22 @@
+use bevy_app::prelude::*;
+use bevy_input::{prelude::KeyCode, Input};
+use bevy_ecs::IntoQuerySystem;
+
+mod inputmap;
+mod keyboard;
+use crate::inputmap::InputMap;
+
+#[derive(Default)]
+pub struct InputMapPlugin;
+
+impl Plugin for InputMapPlugin {
+    fn build(&self, app: &mut AppBuilder) {
+        app.init_resource::<Input<KeyCode>>()
+            .init_resource::<InputMap>()
+            .add_system_to_stage(
+                bevy_app::stage::EVENT_UPDATE,
+                InputMap::keyboard_input_map_system.system()
+            )
+            ;
+    }
+}
