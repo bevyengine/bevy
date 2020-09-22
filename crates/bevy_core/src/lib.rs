028a22b12936797d3a7ca1ad5d715a98bdf27907
Carter Anderson <mcanders1@gmail.com>
2020-09-21 20:23:09 -0700

    asset: use bevy_tasks in AssetServer (#550)

--- crates/bevy_core/src/lib.rs
+++ crates/bevy_core/src/lib.rs
@@ -1,15 +1,17 @@
 mod bytes;
 mod float_ord;
 mod label;
+mod task_pool_options;
 mod time;
 
 pub use bytes::*;
 pub use float_ord::*;
 pub use label::*;
+pub use task_pool_options::DefaultTaskPoolOptions;
 pub use time::*;
 
 pub mod prelude {
-    pub use crate::{EntityLabels, Labels, Time, Timer};
+    pub use crate::{DefaultTaskPoolOptions, EntityLabels, Labels, Time, Timer};
 }
 
 use bevy_app::prelude::*;
@@ -23,6 +25,12 @@ pub struct CorePlugin;
 
 impl Plugin for CorePlugin {
     fn build(&self, app: &mut AppBuilder) {
+        // Setup the default bevy task pools
+        app.resources_mut()
+            .get_cloned::<DefaultTaskPoolOptions>()
+            .unwrap_or_else(DefaultTaskPoolOptions::default)
+            .create_default_pools(app.resources_mut());
+
         app.init_resource::<Time>()
             .init_resource::<EntityLabels>()
             .register_component::<Timer>()
