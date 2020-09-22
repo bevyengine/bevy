c792955e5cbbf4ed03299f05650b5c687a5de43c
PradeepKumar <raj.pradeep.kumar@gmail.com>
2020-09-22 13:37:13 +0530

    - implemented* serialise/deserialise for GamepadButton - implemented* serialise/deserialise for GamepadAxis

--- crates/bevy_input/src/gamepad.rs
+++ crates/bevy_input/src/gamepad.rs
@@ -1,5 +1,6 @@
+use serde::{Deserialize, Deserializer, Serialize, Serializer};
+
 #[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
-#[cfg_attr(feature = "serialize", derive(serde::Serialize, serde::Deserialize))]
 pub struct Gamepad(pub usize);
 
 #[derive(Debug, Copy, Clone, PartialEq)]
@@ -35,9 +36,40 @@ pub enum GamepadButtonType {
     DPadRight,
 }
 
+impl Default for GamepadButtonType {
+    fn default() -> Self {
+        GamepadButtonType::South
+    }
+}
+
 #[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
-#[cfg_attr(feature = "serialize", derive(serde::Serialize, serde::Deserialize))]
 pub struct GamepadButton(pub Gamepad, pub GamepadButtonType);
+
+#[derive(Serialize, Deserialize)]
+struct GamepadButtonHelper {
+    Pad: usize,
+    Button: GamepadButtonType,
+}
+
+impl Serialize for GamepadButton {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        GamepadButtonHelper { Pad: self.0.0, Button: self.1 }.serialize(serializer)
+    }
+}
+
+impl<'de> Deserialize<'de> for GamepadButton {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        Deserialize::deserialize(deserializer)
+            .map(|GamepadButtonHelper { Pad, Button }
+            | GamepadButton(Gamepad(Pad), Button))
+    }
+}
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
 #[cfg_attr(feature = "serialize", derive(serde::Serialize, serde::Deserialize))]
@@ -52,6 +84,37 @@ pub enum GamepadAxisType {
     DPadY,
 }
 
+impl Default for GamepadAxisType {
+    fn default() -> Self {
+        GamepadAxisType::LeftStickX
+    }
+}
+
 #[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
-#[cfg_attr(feature = "serialize", derive(serde::Serialize, serde::Deserialize))]
 pub struct GamepadAxis(pub Gamepad, pub GamepadAxisType);
+
+#[derive(Serialize, Deserialize)]
+struct GamepadAxisHelper {
+    Pad: usize,
+    Axis: GamepadAxisType,
+}
+
+impl Serialize for GamepadAxis {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        GamepadAxisHelper { Pad: self.0.0, Axis: self.1 }.serialize(serializer)
+    }
+}
+
+impl<'de> Deserialize<'de> for GamepadAxis {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        Deserialize::deserialize(deserializer)
+            .map(|GamepadAxisHelper { Pad, Axis }
+            | GamepadAxis(Gamepad(Pad), Axis))
+    }
+}
