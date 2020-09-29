4381f57c0e4e05a6ce2cba2ae02f760689245392
Alex Rozgo <alex.rozgo@gmail.com>
2020-09-29 11:09:28 -0600

    handle edge cases

--- crates/bevy_math/src/map_range.rs
+++ crates/bevy_math/src/map_range.rs
@@ -14,7 +14,7 @@ pub fn map_range<T: MapRange>(
     output_min: T,
     output_max: T,
 ) -> T {
-    let alpha = alpha_along_line(input, input_min, input_max);
+    let alpha = input.alpha(input_min, input_max);
     lerp(output_min, output_max, alpha)
 }
 
@@ -30,8 +30,8 @@ pub fn map_range_clamped<T: MapRange>(
     output_min: T,
     output_max: T,
 ) -> T {
-    let alpha = alpha_along_line(input, input_min, input_max);
-    let alpha = alpha.clamp(&T::zero(), &T::one());
+    let alpha = input.alpha(input_min, input_max);
+    let alpha = alpha.clamp(T::zero(), T::one());
     lerp(output_min, output_max, alpha)
 }
 
@@ -47,12 +47,11 @@ where
 {
     fn zero() -> Self;
     fn one() -> Self;
-    fn clamp(&self, min: &Self, max: &Self) -> Self;
-    fn near_zero(&self) -> bool;
+    fn clamp(self, min: Self, max: Self) -> Self;
+    fn alpha(self, min: Self, max: Self) -> Self;
 }
 
 const EPSILON: f32 = 1.0e-8;
-const EPSILON_SQUARED: f32 = EPSILON * EPSILON;
 
 impl MapRange for f32 {
     fn zero() -> f32 {
@@ -63,12 +62,14 @@ impl MapRange for f32 {
         1f32
     }
 
-    fn clamp(&self, min: &f32, max: &f32) -> f32 {
-        clamp::clamp(*self, *min, *max)
+    // // Component-wise clamp
+    fn clamp(self, min: f32, max: f32) -> f32 {
+        clamp::clamp(self, min, max)
     }
 
-    fn near_zero(&self) -> bool {
-        self.abs() < EPSILON
+    // Component-wise alpha of input along line from input_min to input_max
+    fn alpha(self, min: f32, max: f32) -> f32 {
+        alpha(self, min, max)
     }
 }
 
@@ -81,15 +82,18 @@ impl MapRange for Vec2 {
         Vec2::one()
     }
 
-    fn clamp(&self, min: &Vec2, max: &Vec2) -> Vec2 {
+    fn clamp(self, min: Vec2, max: Vec2) -> Vec2 {
         Vec2::new(
             clamp::clamp(self.x(), min.x(), max.x()),
             clamp::clamp(self.y(), min.y(), max.y()),
         )
     }
 
-    fn near_zero(&self) -> bool {
-        self.length_squared() < EPSILON_SQUARED
+    fn alpha(self, min: Vec2, max: Vec2) -> Vec2 {
+        Vec2::new(
+            alpha(self.x(), min.x(), max.x()),
+            alpha(self.y(), min.y(), max.y()),
+        )
     }
 }
 
@@ -102,7 +106,7 @@ impl MapRange for Vec3 {
         Vec3::one()
     }
 
-    fn clamp(&self, min: &Vec3, max: &Vec3) -> Vec3 {
+    fn clamp(self, min: Vec3, max: Vec3) -> Vec3 {
         Vec3::new(
             clamp::clamp(self.x(), min.x(), max.x()),
             clamp::clamp(self.y(), min.y(), max.y()),
@@ -110,8 +114,12 @@ impl MapRange for Vec3 {
         )
     }
 
-    fn near_zero(&self) -> bool {
-        self.length_squared() < EPSILON_SQUARED
+    fn alpha(self, min: Vec3, max: Vec3) -> Vec3 {
+        Vec3::new(
+            alpha(self.x(), min.x(), max.x()),
+            alpha(self.y(), min.y(), max.y()),
+            alpha(self.z(), min.z(), max.z()),
+        )
     }
 }
 
@@ -124,7 +132,7 @@ impl MapRange for Vec4 {
         Vec4::one()
     }
 
-    fn clamp(&self, min: &Vec4, max: &Vec4) -> Vec4 {
+    fn clamp(self, min: Vec4, max: Vec4) -> Vec4 {
         Vec4::new(
             clamp::clamp(self.x(), min.x(), max.x()),
             clamp::clamp(self.y(), min.y(), max.y()),
@@ -133,19 +141,24 @@ impl MapRange for Vec4 {
         )
     }
 
-    fn near_zero(&self) -> bool {
-        self.length_squared() < EPSILON_SQUARED
+    fn alpha(self, min: Vec4, max: Vec4) -> Vec4 {
+        Vec4::new(
+            alpha(self.x(), min.x(), max.x()),
+            alpha(self.y(), min.y(), max.y()),
+            alpha(self.z(), min.z(), max.z()),
+            alpha(self.w(), min.w(), max.w()),
+        )
     }
 }
 
-// Alpha of input along line from input_min to input_max
-fn alpha_along_line<T: MapRange>(input: T, input_min: T, input_max: T) -> T {
+// Component-wise alpha of input along line from input_min to input_max
+fn alpha(input: f32, input_min: f32, input_max: f32) -> f32 {
     let divisor = input_max - input_min;
-    if divisor.near_zero() {
+    if divisor.abs() < EPSILON {
         if input >= input_max {
-            T::one()
+            1f32
         } else {
-            T::zero()
+            0f32
         }
     } else {
         (input - input_min) / divisor
@@ -192,14 +205,6 @@ mod tests {
         for i in 0..7 {
             assert_eq!(lerp(a, b, t[i]), r[i], "lerp when a < b && a < 0");
         }
-
-        let a = Vec3::new(-25f32, 20f32, 17.5f32);
-        let b = Vec3::new(2f32, 20f32, -17.5f32);
-        assert_eq!(
-            lerp(a, b, Vec3::new(0.5f32, 0.5f32, 0.5f32)),
-            a.lerp(b, 0.5f32),
-            "lerp eq glam lerp"
-        );
     }
 
     #[test]
@@ -237,15 +242,15 @@ mod tests {
         let output = map_range_clamped(input, input_min, input_max, output_min, output_max);
         assert_eq!(output, 10f32, "map_range_clamped eq");
 
-        let input_min = Vec3::new(-100f32, -100f32, 100f32);
+        let input_min = Vec3::new(-100f32, -100f32, -100f32);
         let input_max = Vec3::new(100f32, 100f32, -100f32);
         let output_min = Vec3::new(0f32, 0f32, 0f32);
         let output_max = Vec3::new(10f32, 10f32, 10f32);
 
-        let input = Vec3::new(200f32, -50f32, 50f32);
+        let input = Vec3::new(200f32, -50f32, 500f32);
         let output = map_range_clamped(input, input_min, input_max, output_min, output_max);
         assert!(
-            output.abs_diff_eq(Vec3::new(10f32, 2.5f32, 2.5f32), EPSILON),
+            output.abs_diff_eq(Vec3::new(10f32, 2.5f32, 10f32), EPSILON),
             "map_range_clamped glam abs_diff_eq"
         );
     }
