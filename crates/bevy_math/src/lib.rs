151779e47d227b76a92ed73e1b93063fa0bdafc4
rozgo <alex.rozgo@gmail.com>
2020-09-29 01:57:38 -0600

    cleanup format

--- crates/bevy_math/src/lib.rs
+++ crates/bevy_math/src/lib.rs
@@ -6,8 +6,8 @@ mod map_range;
 pub use clamp::*;
 pub use face_toward::*;
 pub use geometry::*;
-pub use map_range::*;
 pub use glam::*;
+pub use map_range::*;
 
 pub mod prelude {
     pub use crate::{FaceToward, Mat3, Mat4, Quat, Rect, Size, Vec2, Vec3, Vec4};
