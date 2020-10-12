37c55489fd507b083054a9dcc4d2420d6041cede
Nicholas Rishel <rishel.nick@gmail.com>
2020-10-12 15:02:03 -0700

    Simplified Android example, with added logging.

--- examples/android/bevy_android.rs
+++ examples/android/bevy_android.rs
@@ -1,16 +1,6 @@
-#[macro_use] extern crate log;
-extern crate android_logger;
-
-use log::Level;
-use android_logger::Config;
-
 include!("../3d/3d_scene.rs");
 
-#[cfg(target_os = "android")]
-ndk_glue::ndk_glue!(android_main);
-
-#[cfg(target_os = "android")]
+#[cfg_attr(target_os = "android", ndk_glue::main(logger(level = "trace", tag = "bevy_android"), backtrace = "full"))]
 fn android_main() {
-    android_logger::init_once(Config::default().with_min_level(Level::Trace));
     main();
 }
