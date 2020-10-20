f226802b7478946d4b55dd3e9734c73086b29382
Dashiell Elliott <dashiell.elliott@yahoo.com>
2020-10-20 08:37:16 -0400

    Migrate to rodio 0.12 using thread local resources

--- crates/bevy_audio/src/lib.rs
+++ crates/bevy_audio/src/lib.rs
@@ -1,16 +1,18 @@
+mod audio;
 mod audio_output;
 mod audio_source;
 
+pub use audio::*;
 pub use audio_output::*;
 pub use audio_source::*;
 
 pub mod prelude {
-    pub use crate::{AudioOutput, AudioSource, Decodable};
+    pub use crate::{Audio, AudioOutput, AudioSource, Decodable};
 }
 
 use bevy_app::prelude::*;
 use bevy_asset::AddAsset;
-use bevy_ecs::IntoQuerySystem;
+use bevy_ecs::IntoThreadLocalSystem;
 
 /// Adds support for audio playback to an App
 #[derive(Default)]
@@ -18,12 +20,13 @@ pub struct AudioPlugin;
 
 impl Plugin for AudioPlugin {
     fn build(&self, app: &mut AppBuilder) {
-        app.init_resource::<AudioOutput<AudioSource>>()
+        app.init_thread_local_resource::<AudioOutput<AudioSource>>()
             .add_asset::<AudioSource>()
             .init_asset_loader::<Mp3Loader>()
+            .init_resource::<Audio<AudioSource>>()
             .add_system_to_stage(
                 stage::POST_UPDATE,
-                play_queued_audio_system::<AudioSource>.system(),
+                play_queued_audio_system::<AudioSource>.thread_local_system(),
             );
     }
 }
