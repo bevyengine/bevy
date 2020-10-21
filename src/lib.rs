350e911e893adb5ab4fab598e0007480d59e0eb4
CGMossa <cgmossa@gmail.com>
2020-10-21 16:55:54 +0200

    Minor formatting stuff

--- src/lib.rs
+++ src/lib.rs
@@ -26,9 +26,9 @@
 //! and it gets more powerful every day!
 //!
 //! ### This Crate
-//! The "bevy" crate is just a container crate that makes it easier to consume Bevy components.
+//! The `bevy` crate is just a container crate that makes it easier to consume Bevy components.
 //! The defaults provide a "full" engine experience, but you can easily enable / disable features
-//! in your project's Cargo.toml to meet your specific needs. See Bevy's Cargo.toml for a full list of features available.
+//! in your project's `Cargo.toml` to meet your specific needs. See Bevy's `Cargo.toml` for a full list of features available.
 //!
 //! If you prefer it, you can also consume the individual bevy crates directly.
 
