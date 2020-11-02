c8105962c5275bdee29f4d3bb7e8701fa3d37367
Carter Anderson <mcanders1@gmail.com>
2020-11-02 15:20:07 -0800

    make android main public

--- examples/android/bevy_android.rs
+++ examples/android/bevy_android.rs
@@ -5,6 +5,6 @@ include!("../3d/3d_scene.rs");
     target_os = "android",
     ndk_glue::main(logger(level = "trace", tag = "bevy_android"), backtrace = "full")
 )]
-fn android_main() {
+pub fn android_main() {
     main();
 }
