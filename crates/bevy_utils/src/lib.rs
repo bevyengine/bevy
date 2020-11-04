b5cfa7ec63baff04b7bad4b16e63816265759424
Joshua J. Bouw <dev@joshuajbouw.com>
2020-11-04 18:57:02 +0700

    Add missing API features for Ahash variant HashMap and HashSet

--- crates/bevy_utils/src/lib.rs
+++ crates/bevy_utils/src/lib.rs
@@ -9,15 +9,89 @@ pub type BoxedFuture<'a, T> = Pin<Box<dyn Future<Output = T> + Send + 'a>>;
 #[cfg(target_arch = "wasm32")]
 pub type BoxedFuture<'a, T> = Pin<Box<dyn Future<Output = T> + 'a>>;
 
+/// A std hash map implementing Ahash, a high speed keyed hashing algorithm
+/// intended for use in in-memory hashmaps.
+///
+/// Ahash is designed for performance and is NOT cryptographically secure.
 pub type AhashMap<K, V> = std::collections::HashMap<K, V, RandomState>;
-pub type AhashSet<K> = std::collections::HashSet<K, RandomState>;
 
 pub trait AhashExt {
-    fn with_capacity(cap: usize) -> Self;
+    fn new() -> Self;
+
+    fn with_capacity(capacity: usize) -> Self;
 }
 
 impl<K, V> AhashExt for AhashMap<K, V> {
-    fn with_capacity(cap: usize) -> Self {
-        AhashMap::with_capacity_and_hasher(cap, RandomState::default())
+    /// Creates an empty `HashMap` with Ahash.
+    ///
+    /// The hash map is initially created with a capacity of 0, so it will not
+    /// allocate until it is first inserted into.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use bevy_utils::{AhashMap, AhashExt};
+    /// let mut map: AhashMap<&str, i32> = AhashMap::new();
+    /// ```
+    #[inline]
+    fn new() -> Self {
+        Default::default()
+    }
+
+    /// Creates an empty `HashMap` with the specified capacity with Ahash.
+    ///
+    /// The hash map will be able to hold at least `capacity` elements without
+    /// reallocating. If `capacity` is 0, the hash map will not allocate.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use bevy_utils::{AhashMap, AhashExt};
+    /// let mut map: AhashMap<&str, i32> = AhashMap::with_capacity(10);
+    /// ```
+    #[inline]
+    fn with_capacity(capacity: usize) -> Self {
+        AhashMap::with_capacity_and_hasher(capacity, RandomState::default())
+    }
+}
+
+/// A std hash set implementing Ahash, a high speed keyed hashing algorithm
+/// intended for use in in-memory hashmaps.
+///
+/// Ahash is designed for performance and is NOT cryptographically secure.
+pub type AhashSet<K> = std::collections::HashSet<K, RandomState>;
+
+impl<K> AhashExt for AhashSet<K> {
+    /// Creates an empty `HashSet` with Ahash.
+    ///
+    /// The hash set is initially created with a capacity of 0, so it will not
+    /// allocate until it is first inserted into.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use bevy_utils::{AhashSet, AhashExt};
+    /// let set: AhashSet<i32> = AhashSet::new();
+    /// ```
+    #[inline]
+    fn new() -> Self {
+        Default::default()
+    }
+
+    /// Creates an empty `HashSet` with the specified capacity with Ahash.
+    ///
+    /// The hash set will be able to hold at least `capacity` elements without
+    /// reallocating. If `capacity` is 0, the hash set will not allocate.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use bevy_utils::{AhashSet, AhashExt};
+    /// let set: AhashSet<i32> = AhashSet::with_capacity(10);
+    /// assert!(set.capacity() >= 10);
+    /// ```
+    #[inline]
+    fn with_capacity(capacity: usize) -> Self {
+        AhashSet::with_capacity_and_hasher(capacity, RandomState::default())
     }
 }
