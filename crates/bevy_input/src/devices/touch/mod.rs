3dcd78f42d3ff5cf67a60156627f459865f5ae86
Frey <freylint@tuta.io>
2020-11-05 12:10:42 -0600

    Move types and mod to device module.

--- crates/bevy_input/src/devices/touch/mod.rs
+++ crates/bevy_input/src/devices/touch/mod.rs
@@ -1,11 +1,11 @@
 pub mod codes;
+pub mod device;
 pub mod events;
 pub mod state;
 pub mod systems;
-pub mod types;
 
 pub use codes::*;
+pub use device::*;
 pub use events::*;
 pub use state::*;
 pub use systems::*;
-pub use types::*;
