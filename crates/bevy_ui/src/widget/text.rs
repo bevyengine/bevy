eb587b2f10f4a5c2a4c6b0fa272db89a006fb6cc
marius851000 <marius851000@gmail.com>
2020-11-22 20:57:52 +0100

    do not spend time drawing text with is_visible = false (#893)
    do not draw text with is_visible = false

--- crates/bevy_ui/src/widget/text.rs
+++ crates/bevy_ui/src/widget/text.rs
@@ -153,6 +153,10 @@ pub fn draw_text_system(
     let vertex_buffer_descriptor = font_quad.get_vertex_buffer_descriptor();
 
     for (entity, mut draw, text, node, global_transform) in query.iter_mut() {
+        if !draw.is_visible {
+            continue;
+        }
+
         if let Some(text_glyphs) = text_pipeline.get_glyphs(&entity) {
             let position = global_transform.translation - (node.size / 2.0).extend(0.0);
 
