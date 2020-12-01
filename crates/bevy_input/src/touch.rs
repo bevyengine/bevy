fdda5a34ddf9aff2f89e616289eb79536b760811
Amber Kowalski <amberkowalski03@gmail.com>
2020-11-30 22:51:23 -0600

    Switch method of iteration

--- crates/bevy_input/src/touch.rs
+++ crates/bevy_input/src/touch.rs
@@ -138,10 +138,8 @@ impl Touches {
         self.just_pressed.contains_key(&id)
     }
 
-    pub fn iter_just_pressed(&self) -> impl Iterator<Item = &Touch> + '_ {
-        self.just_pressed
-            .iter()
-            .map(move |(id, _)| self.pressed.get(id).unwrap())
+    pub fn iter_just_pressed(&self) -> impl Iterator<Item = &Touch> {
+        self.just_pressed.values()
     }
 
     pub fn get_released(&self, id: u64) -> Option<&Touch> {
@@ -152,10 +150,8 @@ impl Touches {
         self.just_released.contains_key(&id)
     }
 
-    pub fn iter_just_released(&self) -> impl Iterator<Item = &Touch> + '_ {
-        self.just_released
-            .iter()
-            .map(move |(id, _)| self.just_released.get(id).unwrap())
+    pub fn iter_just_released(&self) -> impl Iterator<Item = &Touch> {
+        self.just_released.values()
     }
 
     pub fn just_cancelled(&self, id: u64) -> bool {
@@ -163,9 +159,7 @@ impl Touches {
     }
 
     pub fn iter_just_cancelled(&self) -> impl Iterator<Item = &Touch> + '_ {
-        self.just_cancelled
-            .iter()
-            .map(move |(id, _)| self.just_cancelled.get(id).unwrap())
+        self.just_cancelled.values()
     }
 
     fn process_touch_event(&mut self, event: &TouchInput) {
