8df4bb1e35c09964fcd18dd435656ebc6799ea7e
Robert Swain <robert.swain@gmail.com>
2020-12-02 16:42:15 +0100

    Display average frame time

--- examples/ui/text_debug.rs
+++ examples/ui/text_debug.rs
@@ -140,10 +140,17 @@ fn change_text_system(
             }
         }
 
+        let mut frame_time = time.delta_seconds_f64();
+        if let Some(frame_time_diagnostic) = diagnostics.get(FrameTimeDiagnosticsPlugin::FRAME_TIME) {
+            if let Some(frame_time_avg) = frame_time_diagnostic.average() {
+                frame_time = frame_time_avg;
+            }
+        }
+
         text.value = format!(
             "This text changes in the bottom right - {:.1} fps, {:.3} ms/frame",
             fps,
-            time.delta_seconds_f64() * 1000.0,
+            frame_time * 1000.0,
         );
     }
 }
