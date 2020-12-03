d82c281bce20496ba95d3fbedb4c7ac6eb169ccb
Nathan Jeffords <njeffords@comtechefdata.com>
2020-12-02 20:29:09 -0800

    update `window_settings` example to demonstrate using `alt`+`enter` to switching between windows mode and border-less full screen mode

--- examples/window/window_settings.rs
+++ examples/window/window_settings.rs
@@ -1,4 +1,8 @@
-use bevy::prelude::*;
+use bevy::{
+    input::keyboard::{modifiers::ALT, KeyModifiers},
+    prelude::*,
+    window::WindowMode,
+};
 
 /// This example illustrates how to customize the default window settings
 fn main() {
@@ -14,6 +18,7 @@ fn main() {
         .add_plugins(DefaultPlugins)
         .add_system(change_title)
         .add_system(toggle_cursor)
+        .add_system(toggle_fullscreen)
         .run();
 }
 
@@ -32,5 +37,20 @@ fn toggle_cursor(input: Res<Input<KeyCode>>, mut windows: ResMut<Windows>) {
     if input.just_pressed(KeyCode::Space) {
         window.set_cursor_lock_mode(!window.cursor_locked());
         window.set_cursor_visibility(!window.cursor_visible());
+    }
+}
+
+fn toggle_fullscreen(
+    key_codes: Res<Input<KeyCode>>,
+    key_modifiers: Res<KeyModifiers>,
+    mut windows: ResMut<Windows>,
+) {
+    if key_codes.just_pressed(KeyCode::Return) && *key_modifiers == ALT {
+        let window = windows.get_primary_mut().unwrap();
+        match window.mode() {
+            WindowMode::BorderlessFullscreen => window.set_mode(WindowMode::Windowed),
+            WindowMode::Windowed => window.set_mode(WindowMode::BorderlessFullscreen),
+            _ => (),
+        }
     }
 }
