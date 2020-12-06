c3431e1e934c02df5000f4c8ef799a118a42c562
Skye Im <fluffyterran@gmail.com>
2020-12-06 19:13:55 +0100

    Fix examples/2d: Bundle -> Components

--- examples/2d/sprite_sheet.rs
+++ examples/2d/sprite_sheet.rs
@@ -31,8 +31,8 @@ fn setup(
     let texture_atlas = TextureAtlas::from_grid(texture_handle, Vec2::new(24.0, 24.0), 7, 1);
     let texture_atlas_handle = texture_atlases.add(texture_atlas);
     commands
-        .spawn(Camera2dBundle::default())
-        .spawn(SpriteSheetBundle {
+        .spawn(Camera2dComponents::default())
+        .spawn(SpriteSheetComponents {
             texture_atlas: texture_atlas_handle,
             transform: Transform::from_scale(Vec3::splat(6.0)),
             ..Default::default()
