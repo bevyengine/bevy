57332ccc8f6710cc73e447fcfb3b92e27193838c
François Mockers <mockersf@gmail.com>
2020-12-18 02:30:25 +0100

    asset count diagnostic

--- examples/diagnostics/log_diagnostics.rs
+++ examples/diagnostics/log_diagnostics.rs
@@ -15,5 +15,7 @@ fn main() {
         // .add_plugin(bevy::wgpu::diagnostic::WgpuResourceDiagnosticsPlugin::default())
         // Uncomment this to add an entity count diagnostics:
         // .add_plugin(bevy::diagnostic::EntityCountDiagnosticsPlugin::default())
+        // Uncomment this to add an asset count diagnostics:
+        // .add_plugin(bevy::asset::diagnostic::AssetCountDiagnosticsPlugin::<Texture>::default())
         .run();
 }
