3231e2f26c0e7ead161f1f1ada765b7c0b2cfefa
Schell Carl Scivally <efsubenovex@gmail.com>
2021-01-05 13:03:59 -0800

    render mask logic uses unwrap_or_default, better docs, fmt

--- crates/bevy_render/src/camera/visible_entities.rs
+++ crates/bevy_render/src/camera/visible_entities.rs
@@ -24,37 +24,46 @@ impl VisibleEntities {
     }
 }
 
-/// A mask that describes which rendering group an entity belongs to.
+/// A mask describes which rendering group an entity belongs to.
+///
 /// Cameras with this component will only render entities with a matching
 /// mask.
+///
+/// The [`Default`] instance of `RenderingMask` returns a mask that contains
+/// no groups.
 #[derive(Copy, Clone, Debug, Reflect, PartialEq, Eq, PartialOrd, Ord)]
 #[reflect(Component)]
 pub struct RenderingMask(pub u32);
 
 impl Default for RenderingMask {
     fn default() -> Self {
-        RenderingMask(1)
+        RenderingMask(0)
     }
 }
 
 impl RenderingMask {
+    /// Create new `RenderingMask` with the given rendering group set.
     pub fn group(n: u8) -> Self {
-        RenderingMask::default().with_group(n)
+        RenderingMask::default().set_group(n)
     }
 
-    pub fn with_group(mut self, group: u8) -> Self {
+    /// Set the given group on the mask.
+    /// This may be called multiple times to allow an entity to belong
+    /// to multiple rendering groups.
+    pub fn set_group(mut self, group: u8) -> Self {
         self.0 |= 1 << group;
         self
     }
 
-    pub fn without_group(mut self, group: u8) -> Self {
+    /// Unset the given rendering group from the mask.
+    pub fn unset_group(mut self, group: u8) -> Self {
         self.0 |= 0 << group;
         self
     }
 
-    /// Determine if a `RenderingMask` matches.
+    /// Determine if a `RenderingMask` matches another.
     /// `RenderingMask`s match if the first mask contains any of the groups
-    /// in the second, or if both masks are `0`.
+    /// in the other, or if both masks contain no groups.
     pub fn matches(&self, other: &RenderingMask) -> bool {
         ((self.0 & other.0) > 0) || (self.0 == 0 && other.0 == 0)
     }
@@ -72,8 +81,12 @@ mod rendering_mask_tests {
         assert!(RenderingMask::group(0).matches(&RenderingMask(1)));
         // a mask will match another mask containing any similar groups
         assert!(RenderingMask::group(0)
-            .with_group(3)
+            .set_group(3)
             .matches(&RenderingMask::group(3)));
+        // default masks match each other
+        assert!(RenderingMask::default().matches(&RenderingMask::default()));
+        // masks with differing groups do not match
+        assert_eq!(RenderingMask::group(0).matches(&RenderingMask::group(1)), false);
     }
 }
 
@@ -95,17 +108,13 @@ pub fn visible_entities_system(
 
         let mut no_transform_order = 0.0;
         let mut transparent_entities = Vec::new();
-        for (entity, visible, maybe_ent_mask) in visible_query.iter() {
+        for (entity, visible, maybe_entity_mask) in visible_query.iter() {
             if !visible.is_visible {
                 continue;
             }
 
-            let camera_mask = maybe_camera_mask
-                .copied()
-                .unwrap_or_else(|| RenderingMask(0));
-            let entity_mask = maybe_ent_mask
-                .copied()
-                .unwrap_or_else(|| RenderingMask(0));
+            let camera_mask = maybe_camera_mask.copied().unwrap_or_default();
+            let entity_mask = maybe_entity_mask.copied().unwrap_or_default();
             if !camera_mask.matches(&entity_mask) {
                 continue;
             }
