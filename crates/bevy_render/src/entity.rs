b0ea4fabf97e5a2233bf6f4bcab7e7e87bf077fa
Jasen Borisov <jamadazi@protonmail.ch>
2021-01-21 19:03:15 +0100

    add general (scaled) orthographic camera bundle
    FIXME: does the same "far" trick from Camera2DBundle make any sense here?

--- crates/bevy_render/src/entity.rs
+++ crates/bevy_render/src/entity.rs
@@ -1,5 +1,8 @@
 use crate::{
-    camera::{Camera, OrthographicProjection, PerspectiveProjection, VisibleEntities},
+    camera::{
+        Camera, OrthographicProjection, PerspectiveProjection, ScaledOrthographicProjection,
+        VisibleEntities,
+    },
     pipeline::RenderPipelines,
     prelude::Visible,
     render_graph::base,
@@ -23,6 +26,8 @@ pub struct MeshBundle {
 }
 
 /// A component bundle for "3d camera" entities
+///
+/// Uses a perspective projection.
 #[derive(Bundle)]
 pub struct Camera3dBundle {
     pub camera: Camera,
@@ -48,6 +53,10 @@ impl Default for Camera3dBundle {
 }
 
 /// A component bundle for "2d camera" entities
+///
+/// Uses an orthographic projection mapped to window pixels.
+///
+/// Use this for pixel-art / sprite-based 2D games.
 #[derive(Bundle)]
 pub struct Camera2dBundle {
     pub camera: Camera,
@@ -68,6 +77,43 @@ impl Default for Camera2dBundle {
                 ..Default::default()
             },
             orthographic_projection: OrthographicProjection {
+                far,
+                ..Default::default()
+            },
+            visible_entities: Default::default(),
+            transform: Transform::from_xyz(0.0, 0.0, far - 0.1),
+            global_transform: Default::default(),
+        }
+    }
+}
+
+/// A component bundle for "orthographic camera" entities
+///
+/// Uses a normalized orthographic projection.
+///
+/// Use this for CAD-like 3D views or non-pixel-oriented 2D games.
+#[derive(Bundle)]
+pub struct CameraOrthoBundle {
+    pub camera: Camera,
+    pub orthographic_projection: ScaledOrthographicProjection,
+    pub visible_entities: VisibleEntities,
+    pub transform: Transform,
+    pub global_transform: GlobalTransform,
+}
+
+impl Default for CameraOrthoBundle {
+    fn default() -> Self {
+        // FIXME does this same far trick from `Camera2DBundle` make any sense here?
+
+        // we want 0 to be "closest" and +far to be "farthest" in 2d, so we offset
+        // the camera's translation by far and use a right handed coordinate system
+        let far = 1000.0;
+        CameraOrthoBundle {
+            camera: Camera {
+                name: Some(base::camera::CAMERA_2D.to_string()),
+                ..Default::default()
+            },
+            orthographic_projection: ScaledOrthographicProjection {
                 far,
                 ..Default::default()
             },
