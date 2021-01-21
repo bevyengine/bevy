5383ca6c1584b77ed04b37d1f7206ff51c6cfa39
Will Crichton <wcrichto@cs.stanford.edu>
2021-01-20 21:10:42 -0500

    Enable dynamic mutable access to component data

--- crates/bevy_reflect/src/impls/bevy_ecs.rs
+++ crates/bevy_reflect/src/impls/bevy_ecs.rs
@@ -10,6 +10,7 @@ pub struct ReflectComponent {
     add_component: fn(&mut World, resources: &Resources, Entity, &dyn Reflect),
     apply_component: fn(&mut World, Entity, &dyn Reflect),
     reflect_component: unsafe fn(&Archetype, usize) -> &dyn Reflect,
+    reflect_component_mut: unsafe fn(&Archetype, usize) -> &mut dyn Reflect,
     copy_component: fn(&World, &mut World, &Resources, Entity, Entity),
 }
 
@@ -36,6 +37,17 @@ impl ReflectComponent {
         entity_index: usize,
     ) -> &'a dyn Reflect {
         (self.reflect_component)(archetype, entity_index)
+    }
+
+    /// # Safety
+    /// This does not do bound checks on entity_index. You must make sure entity_index is within bounds before calling.
+    /// To avoid having multiple mutable pointers to the same data, this method can only be called in a thread-local system.
+    pub unsafe fn reflect_component_mut<'a>(
+        &self,
+        archetype: &'a Archetype,
+        entity_index: usize,
+    ) -> &'a mut dyn Reflect {
+        (self.reflect_component_mut)(archetype, entity_index)
     }
 
     pub fn copy_component(
@@ -85,6 +97,13 @@ impl<C: Component + Reflect + FromResources> FromType<C> for ReflectComponent {
                     // the type has been looked up by the caller, so this is safe
                     let ptr = archetype.get::<C>().unwrap().as_ptr().add(index);
                     ptr.as_ref().unwrap()
+                }
+            },
+            reflect_component_mut: |archetype, index| {
+                unsafe {
+                    // the type has been looked up by the caller, so this is safe
+                    let ptr = archetype.get::<C>().unwrap().as_ptr().add(index);
+                    &mut *ptr
                 }
             },
         }
