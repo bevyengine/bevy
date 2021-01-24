ba60fd4fc3573a704affb0c1f02499a8762a9822
tigregalis <anak.harimau@gmail.com>
2021-01-24 19:10:45 +0800

    clippy

--- crates/bevy_text/src/lib.rs
+++ crates/bevy_text/src/lib.rs
@@ -21,9 +21,7 @@ pub use text::*;
 pub use text2d::*;
 
 pub mod prelude {
-    pub use crate::{
-        Font, Text, Text2dBundle, TextAlignment, TextError, TextSection, TextStyle,
-    };
+    pub use crate::{Font, Text, Text2dBundle, TextAlignment, TextError, TextSection, TextStyle};
     pub use glyph_brush_layout::{HorizontalAlign, VerticalAlign};
 }
 
