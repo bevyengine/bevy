b24bb8d000f0240311e5a9a83c6bc96ab805dad3
Jasen Borisov <jamadazi@protonmail.ch>
2021-01-26 19:08:26 +0100

    rename CameraUiBundle to UiCameraBundle

--- crates/bevy_ui/src/entity.rs
+++ crates/bevy_ui/src/entity.rs
@@ -164,7 +164,7 @@ impl Default for ButtonBundle {
 }
 
 #[derive(Bundle, Debug)]
-pub struct CameraUiBundle {
+pub struct UiCameraBundle {
     pub camera: Camera,
     pub orthographic_projection: OrthographicProjection,
     pub visible_entities: VisibleEntities,
@@ -172,12 +172,12 @@ pub struct CameraUiBundle {
     pub global_transform: GlobalTransform,
 }
 
-impl Default for CameraUiBundle {
+impl Default for UiCameraBundle {
     fn default() -> Self {
         // we want 0 to be "closest" and +far to be "farthest" in 2d, so we offset
         // the camera's translation by far and use a right handed coordinate system
         let far = 1000.0;
-        CameraUiBundle {
+        UiCameraBundle {
             camera: Camera {
                 name: Some(crate::camera::CAMERA_UI.to_string()),
                 ..Default::default()
