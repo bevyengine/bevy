eb3c8ed8835dee5924e6fa71a34f727768aaa130
Zicklag <zicklag@katharostech.com>
2021-01-28 10:10:06 -0600

    Update Scene Example to Use scn.ron File

--- examples/scene/scene.rs
+++ examples/scene/scene.rs
@@ -49,7 +49,7 @@ impl FromResources for ComponentB {
 
 fn load_scene_system(asset_server: Res<AssetServer>, mut scene_spawner: ResMut<SceneSpawner>) {
     // Scenes are loaded just like any other asset.
-    let scene_handle: Handle<DynamicScene> = asset_server.load("scenes/load_scene_example.scn");
+    let scene_handle: Handle<DynamicScene> = asset_server.load("scenes/load_scene_example.scn.ron");
 
     // SceneSpawner can "spawn" scenes. "Spawning" a scene creates a new instance of the scene in the World with new entity ids.
     // This guarantees that it will not overwrite existing entities.
