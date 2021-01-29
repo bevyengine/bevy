e54bd2b1775e1833515a848f06948b3667ea3a48
Will Crichton <wcrichto@cs.stanford.edu>
2021-01-28 21:20:44 -0500

    Add support for gltf::Material::unlit

--- crates/bevy_gltf/src/loader.rs
+++ crates/bevy_gltf/src/loader.rs
@@ -298,6 +298,7 @@ fn load_material(material: &Material, load_context: &mut LoadContext) -> Handle<
         LoadedAsset::new(StandardMaterial {
             albedo: Color::rgba(color[0], color[1], color[2], color[3]),
             albedo_texture: texture_handle,
+            shaded: !material.unlit(),
             ..Default::default()
         })
         .with_dependencies(dependencies),
