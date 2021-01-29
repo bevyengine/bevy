6d7ae6d720147534ce45731f1fed6b28c6e6882d
Will Crichton <wcrichto@cs.stanford.edu>
2021-01-28 22:07:14 -0500

    Formatting

--- crates/bevy_gltf/src/loader.rs
+++ crates/bevy_gltf/src/loader.rs
@@ -298,8 +298,8 @@ fn load_material(material: &Material, load_context: &mut LoadContext) -> Handle<
         LoadedAsset::new(StandardMaterial {
             albedo: Color::rgba(color[0], color[1], color[2], color[3]),
             albedo_texture: texture_handle,
-            shaded: !material.unlit()
-          })
+            shaded: !material.unlit(),
+        })
         .with_dependencies(dependencies),
     )
 }
