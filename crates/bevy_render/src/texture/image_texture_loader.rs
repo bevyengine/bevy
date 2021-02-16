7c8106da904e3002f2b65918338fc8b80e2fc189
François Mockers <mockersf@gmail.com>
2021-02-16 03:30:53 +0100

    common way to load texture from bytes with either extension or mime type

--- crates/bevy_render/src/texture/image_texture_loader.rs
+++ crates/bevy_render/src/texture/image_texture_loader.rs
@@ -1,7 +1,8 @@
-use super::image_texture_conversion::image_to_texture;
+use super::{image_texture_conversion::image_to_texture, Texture};
 use anyhow::Result;
 use bevy_asset::{AssetLoader, LoadContext, LoadedAsset};
 use bevy_utils::BoxedFuture;
+use thiserror::Error;
 
 /// Loader for images that can be read by the `image` crate.
 #[derive(Clone, Default)]
@@ -16,30 +17,17 @@ impl AssetLoader for ImageTextureLoader {
         load_context: &'a mut LoadContext,
     ) -> BoxedFuture<'a, Result<()>> {
         Box::pin(async move {
-            // Find the image type we expect. A file with the extension "png" should
-            // probably load as a PNG.
-
+            // use the file extension for the image type
             let ext = load_context.path().extension().unwrap().to_str().unwrap();
 
-            let img_format = image::ImageFormat::from_extension(ext)
-                .ok_or_else(|| {
-                    format!(
-                    "Unexpected image format {:?} for file {}, this is an error in `bevy_render`.",
-                    ext,
-                    load_context.path().display()
-                )
-                })
-                .unwrap();
+            let dyn_img = buffer_to_texture(bytes, ImageType::Extension(ext)).map_err(|err| {
+                FileTextureError {
+                    error: err,
+                    path: format!("{}", load_context.path().display()),
+                }
+            })?;
 
-            // Load the image in the expected format.
-            // Some formats like PNG allow for R or RG textures too, so the texture
-            // format needs to be determined. For RGB textures an alpha channel
-            // needs to be added, so the image data needs to be converted in those
-            // cases.
-
-            let dyn_img = image::load_from_memory_with_format(bytes, img_format)?;
-
-            load_context.set_default_asset(LoadedAsset::new(image_to_texture(dyn_img)));
+            load_context.set_default_asset(LoadedAsset::new(dyn_img));
             Ok(())
         })
     }
@@ -47,6 +35,68 @@ impl AssetLoader for ImageTextureLoader {
     fn extensions(&self) -> &[&str] {
         FILE_EXTENSIONS
     }
+}
+
+/// An error that occurs when loading a texture from a file
+#[derive(Error, Debug)]
+pub struct FileTextureError {
+    error: TextureError,
+    path: String,
+}
+impl std::fmt::Display for FileTextureError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::result::Result<(), std::fmt::Error> {
+        write!(
+            f,
+            "Error reading image file {}: {}, this is an error in `bevy_render`.",
+            self.path, self.error
+        )
+    }
+}
+
+/// An error that occurs when loading a texture
+#[derive(Error, Debug)]
+pub enum TextureError {
+    #[error("invalid image mime type")]
+    InvalidImageMimeType(String),
+    #[error("invalid image extension")]
+    InvalidImageExtension(String),
+    #[error("failed to load an image")]
+    ImageError(#[from] image::ImageError),
+}
+
+/// Type of a raw image buffer
+pub enum ImageType<'a> {
+    /// Mime type of an image, for example `"image/png"`
+    MimeType(&'a str),
+    /// Extension of an image file, for example `"png"`
+    Extension(&'a str),
+}
+
+/// Load a bytes buffer in a [`Texture`], according to type `image_type`, using the `image` crate`
+pub fn buffer_to_texture(buffer: &[u8], image_type: ImageType) -> Result<Texture, TextureError> {
+    let format = match image_type {
+        ImageType::MimeType(mime_type) => match mime_type {
+            "image/png" => Ok(image::ImageFormat::Png),
+            "image/vnd-ms.dds" => Ok(image::ImageFormat::Dds),
+            "image/x-targa" => Ok(image::ImageFormat::Tga),
+            "image/x-tga" => Ok(image::ImageFormat::Tga),
+            "image/jpeg" => Ok(image::ImageFormat::Jpeg),
+            "image/bmp" => Ok(image::ImageFormat::Bmp),
+            "image/x-bmp" => Ok(image::ImageFormat::Bmp),
+            _ => Err(TextureError::InvalidImageMimeType(mime_type.to_string())),
+        },
+        ImageType::Extension(extension) => image::ImageFormat::from_extension(extension)
+            .ok_or_else(|| TextureError::InvalidImageMimeType(extension.to_string())),
+    }?;
+
+    // Load the image in the expected format.
+    // Some formats like PNG allow for R or RG textures too, so the texture
+    // format needs to be determined. For RGB textures an alpha channel
+    // needs to be added, so the image data needs to be converted in those
+    // cases.
+
+    let dyn_img = image::load_from_memory_with_format(buffer, format)?;
+    Ok(image_to_texture(dyn_img))
 }
 
 #[cfg(test)]
