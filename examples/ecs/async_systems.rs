a3c84da1d34eb52f2c084fa7d7911a44cbb8bc39
TheRawMeatball <therawmeatball@gmail.com>
2021-02-24 23:33:30 +0300

    cargo fmt

--- examples/ecs/async_systems.rs
+++ examples/ecs/async_systems.rs
@@ -24,31 +24,31 @@ type Access<'a> = (
     Res<'a, ExecutionTime>,
 );
 
-async fn async_system(
-    mut accessor: Accessor<Access<'_>>,
-) {
+async fn async_system(mut accessor: Accessor<Access<'_>>) {
     fn sync_operation(msg: &str, execution_time: f32) {
         println!("{} @ {}", msg, execution_time);
     }
 
     let wait_duration = accessor
-        .access(|(greet_msg, mut next_delay, time, execution_time): Access<'_>| {
-            next_delay.0 += 1.;
-            if next_delay.0 > time.delta_seconds() {
-                sync_operation(&greet_msg.0, execution_time.0);
-                next_delay.0 - time.delta_seconds()
-            } else {
-                // We had a lag spike, and the frame time exceeded our waiting.
-                let mut timer = time.delta_seconds();
-                while timer > next_delay.0 {
-                    timer -= next_delay.0;
-                    // Do multiple operations to catch up.
-                    // (This is an example so we don't actually have anything to do)
-                    sync_operation(&greet_msg.0, execution_time.0)
+        .access(
+            |(greet_msg, mut next_delay, time, execution_time): Access<'_>| {
+                next_delay.0 += 1.;
+                if next_delay.0 > time.delta_seconds() {
+                    sync_operation(&greet_msg.0, execution_time.0);
+                    next_delay.0 - time.delta_seconds()
+                } else {
+                    // We had a lag spike, and the frame time exceeded our waiting.
+                    let mut timer = time.delta_seconds();
+                    while timer > next_delay.0 {
+                        timer -= next_delay.0;
+                        // Do multiple operations to catch up.
+                        // (This is an example so we don't actually have anything to do)
+                        sync_operation(&greet_msg.0, execution_time.0)
+                    }
+                    timer
                 }
-                timer
-            }
-        })
+            },
+        )
         .await;
 
     futures_timer::Delay::new(Duration::from_secs_f32(wait_duration)).await;
