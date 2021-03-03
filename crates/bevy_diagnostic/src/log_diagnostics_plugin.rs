e61d7920e30c4fd0da43726875eb21b7b53d52eb
Daniel McNab <36049421+DJMcNab@users.noreply.github.com>
2021-03-03 03:43:15 +0000

    Add suffixes to diagnostics and other cleanup (#1505)
    Also a few related clean ups to diagnostics
    Old look (from the log_diagnostics_example):
    <details>
    <summary>Old look hidden to avoid clutter </summary>
    
    ![image](https://user-images.githubusercontent.com/36049421/108776774-bc349080-755a-11eb-8569-d4832abf6bc3.png)
    
    </details>
    New look: 
    
    ![image](https://user-images.githubusercontent.com/36049421/108776587-82638a00-755a-11eb-96eb-539026d59bcb.png)
    
    In particular, notice that the width of the diagnostics has been significantly reduced - within vscode the value no longer wraps on my 1920 width monitor. The value is still 105 columns wide, so there is room for improvement however.

--- crates/bevy_diagnostic/src/log_diagnostics_plugin.rs
+++ crates/bevy_diagnostic/src/log_diagnostics_plugin.rs
@@ -28,6 +28,10 @@ impl Default for LogDiagnosticsPlugin {
     }
 }
 
+/// The width which diagnostic names will be printed as
+/// Plugin names should not be longer than this value
+pub(crate) const MAX_LOG_NAME_WIDTH: usize = 32;
+
 impl Plugin for LogDiagnosticsPlugin {
     fn build(&self, app: &mut bevy_app::AppBuilder) {
         app.insert_resource(LogDiagnosticsState {
@@ -58,11 +62,25 @@ impl LogDiagnosticsPlugin {
         if let Some(value) = diagnostic.value() {
             if let Some(average) = diagnostic.average() {
                 info!(
-                    "{:<65}: {:<10.6}  (avg {:.6})",
-                    diagnostic.name, value, average
+                    target: "bevy diagnostic",
+                    "{:<name_width$}: {:>12} (avg {:>})",
+                    diagnostic.name,
+                    // Suffix is only used for 's' as in seconds currently,
+                    // so we reserve one column for it
+                    format!("{:.6}{:1}", value, diagnostic.suffix),
+                    // Do not reserve one column for the suffix in the average
+                    // The ) hugging the value is more aesthetically pleasing
+                    format!("{:.6}{:}", average, diagnostic.suffix),
+                    name_width = MAX_LOG_NAME_WIDTH,
                 );
             } else {
-                info!("{:<65}: {:<10.6}", diagnostic.name, value);
+                info!(
+                    target: "bevy diagnostic",
+                    "{:<name_width$}: {:>}",
+                    diagnostic.name,
+                    format!("{:.6}{:}", value, diagnostic.suffix),
+                    name_width = MAX_LOG_NAME_WIDTH,
+                );
             }
         }
     }
