67b178675a875072a25d24f6d7e4047705bb505d
François Mockers <mockersf@gmail.com>
2021-03-06 14:31:06 +0100

    fix flip of contributor bird

--- examples/2d/contributors.rs
+++ examples/2d/contributors.rs
@@ -74,8 +74,7 @@ fn setup(
         // some sprites should be flipped
         let flipped = rnd.gen_bool(0.5);
 
-        let mut transform = Transform::from_xyz(pos.0, pos.1, 0.0);
-        transform.scale.x *= if flipped { -1.0 } else { 1.0 };
+        let transform = Transform::from_xyz(pos.0, pos.1, 0.0);
 
         commands
             .spawn((Contributor { color: col },))
@@ -87,6 +86,7 @@ fn setup(
                 sprite: Sprite {
                     size: Vec2::new(1.0, 1.0) * SPRITE_SIZE,
                     resize_mode: SpriteResizeMode::Manual,
+                    flip_x: flipped,
                     ..Default::default()
                 },
                 material: materials.add(ColorMaterial {
