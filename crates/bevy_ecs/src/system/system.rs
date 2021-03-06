b02c73aeb42fa52285f5036b9a36d1bb31c58d6c
Alice Cecile <alice.i.cecile@gmail.com>
2021-03-06 14:47:00 -0500

    Basic introduction to how systems work in Bevy

--- crates/bevy_ecs/src/system/system.rs
+++ crates/bevy_ecs/src/system/system.rs
@@ -17,6 +17,11 @@ impl SystemId {
 }
 
 /// An ECS system that can be added to a [Schedule](crate::schedule::Schedule)
+///
+/// Systems are just ordinary Rust functions, each of whose parameters implement the [IntoSystem] trait.
+/// You can add systems to your app by including `.add_system(my_system.system())` in your `AppBuilder`.
+/// In Bevy apps, the scheduler will automatically run systems in parallel, coordinate resource access
+/// and call your systems each pass of the main loop.
 pub trait System: Send + Sync + 'static {
     type In;
     type Out;
