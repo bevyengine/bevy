37e08f91358ad65c10d5ece737e10db3b75587c5
Carter Anderson <mcanders1@gmail.com>
2021-03-10 12:15:27 -0800

    Add Time system label

--- crates/bevy_core/src/lib.rs
+++ crates/bevy_core/src/lib.rs
@@ -17,13 +17,23 @@ pub mod prelude {
 }
 
 use bevy_app::prelude::*;
-use bevy_ecs::{entity::Entity, prelude::IntoExclusiveSystem, system::IntoSystem};
+use bevy_ecs::{
+    entity::Entity,
+    schedule::{ExclusiveSystemDescriptorCoercion, SystemLabel},
+    system::{IntoExclusiveSystem, IntoSystem},
+};
 use bevy_utils::HashSet;
 use std::ops::Range;
 
 /// Adds core functionality to Apps.
 #[derive(Default)]
 pub struct CorePlugin;
+
+#[derive(Debug, PartialEq, Eq, Clone, Hash, SystemLabel)]
+pub enum CoreSystem {
+    /// Updates the elapsed time. Any system that interacts with [Time] component should run after this.
+    Time,
+}
 
 impl Plugin for CorePlugin {
     fn build(&self, app: &mut AppBuilder) {
@@ -45,8 +55,10 @@ impl Plugin for CorePlugin {
             .register_type::<Range<f32>>()
             .register_type::<Timer>()
             // time system is added as an "exclusive system" to ensure it runs before other systems in CoreStage::First
-            // this also ensures that it runs before other exclusive systems added after CorePlugin
-            .add_system_to_stage(CoreStage::First, time_system.exclusive_system())
+            .add_system_to_stage(
+                CoreStage::First,
+                time_system.exclusive_system().label(CoreSystem::Time),
+            )
             .add_startup_system_to_stage(StartupStage::PostStartup, entity_labels_system.system())
             .add_system_to_stage(CoreStage::PostUpdate, entity_labels_system.system());
 
