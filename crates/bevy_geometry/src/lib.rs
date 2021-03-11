e2fd3d2739d474ecb8f8059969e31c25b308a0e1
Aevyrie Roessler <aevyrie@gmail.com>
2021-03-11 11:11:18 -0800

    derives

--- crates/bevy_geometry/src/lib.rs
+++ crates/bevy_geometry/src/lib.rs
@@ -2,22 +2,26 @@ use bevy_math::{Quat, Vec3};
 
 pub trait Primitive3d {}
 
+#[derive(Copy, Clone, PartialEq, Debug)]
 pub struct Sphere {
     pub origin: Vec3,
     pub radius: f32,
 }
 
+#[derive(Copy, Clone, PartialEq, Debug)]
 pub struct Box {
     pub maximums: Vec3,
     pub minimums: Vec3,
     pub orientation: Quat,
 }
 
+#[derive(Copy, Clone, PartialEq, Debug)]
 pub struct AxisAlignedBox {
     pub maximums: Vec3,
     pub minimums: Vec3,
 }
 
+#[derive(Copy, Clone, PartialEq, Debug)]
 pub struct Plane {
     pub point: Vec3,
     pub normal: Vec3,
