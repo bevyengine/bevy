aec3808ac5de890b7a99bcd4224b3a94945ef847
Jonas Matser <github@jonasmatser.nl>
2021-03-20 09:46:00 +0100

    Fix shader_reflect test BindGroupDescriptor

--- crates/bevy_render/src/shader/shader_reflect.rs
+++ crates/bevy_render/src/shader/shader_reflect.rs
@@ -378,10 +378,7 @@ mod tests {
                             name: "CameraViewProj".into(),
                             bind_type: BindType::Uniform {
                                 has_dynamic_offset: false,
-                                property: UniformProperty::Struct(vec![
-                                    UniformProperty::Mat4,
-                                    UniformProperty::Vec4
-                                ]),
+                                property: UniformProperty::Struct(vec![UniformProperty::Mat4,]),
                             },
                             shader_stage: BindingShaderStage::VERTEX,
                         }]
