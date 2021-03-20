7720542a4800bb7b2654a1574711bb5d059994c8
Jonas Matser <github@jonasmatser.nl>
2021-03-20 09:41:00 +0100

    Remove remaining CameraPos uniforms

--- crates/bevy_render/src/shader/shader_reflect.rs
+++ crates/bevy_render/src/shader/shader_reflect.rs
@@ -321,7 +321,6 @@ mod tests {
             layout(location = 0) out vec4 v_Position;
             layout(set = 0, binding = 0) uniform CameraViewProj {
                 mat4 ViewProj;
-                vec4 CameraPos;
             };
             layout(set = 1, binding = 0) uniform texture2D Texture;
 
