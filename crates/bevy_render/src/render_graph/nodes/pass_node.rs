f2a8f0518abe98cc86c080ca1b02b597e26fdd4d
Zhixing Zhang <me@neoto.xin>
2021-03-22 20:21:21 +0100

    RenderCommand

--- crates/bevy_render/src/render_graph/nodes/pass_node.rs
+++ crates/bevy_render/src/render_graph/nodes/pass_node.rs
@@ -313,6 +313,13 @@ where
                         );
                         draw_state.set_bind_group(index, bind_group);
                     }
+                    RenderCommand::SetPushConstants {
+                        stages,
+                        offset,
+                        data,
+                    } => {
+                        render_pass.set_push_constants(stages, offset, &*data);
+                    }
                 }
             }
         });
