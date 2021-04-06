9534a2de46b614f44a066df16f2b29c98b2a3013
Jakob Hellermann <jakob.hellermann@protonmail.com>
2021-04-06 20:17:43 +0200

    normalize normals (duh)

--- crates/bevy_render/src/mesh/mesh.rs
+++ crates/bevy_render/src/mesh/mesh.rs
@@ -465,7 +465,7 @@ impl Mesh {
 
 fn face_normal(a: [f32; 3], b: [f32; 3], c: [f32; 3]) -> [f32; 3] {
     let (a, b, c) = (Vec3::from(a), Vec3::from(b), Vec3::from(c));
-    (b - a).cross(c - a).into()
+    (b - a).cross(c - a).normalize().into()
 }
 
 fn remove_resource_save(
