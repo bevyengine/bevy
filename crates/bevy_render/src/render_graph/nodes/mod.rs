a920a8f1218d69f214b1ce00c1a80798c2cd23d1
Mariusz Kryński <mrk@sed.pl>
2021-04-18 00:55:34 +0200

    move TextureNode to separate module

--- crates/bevy_render/src/render_graph/nodes/mod.rs
+++ crates/bevy_render/src/render_graph/nodes/mod.rs
@@ -3,6 +3,7 @@ mod pass_node;
 mod render_resources_node;
 mod shared_buffers_node;
 mod texture_copy_node;
+mod texture_node;
 mod window_swapchain_node;
 mod window_texture_node;
 
@@ -11,5 +12,6 @@ pub use pass_node::*;
 pub use render_resources_node::*;
 pub use shared_buffers_node::*;
 pub use texture_copy_node::*;
+pub use texture_node::*;
 pub use window_swapchain_node::*;
 pub use window_texture_node::*;
--- examples/3d/render_to_texture.rs
+++ examples/3d/render_to_texture.rs
@@ -1,5 +1,3 @@
-use std::borrow::Cow;
-
 use bevy::{
     prelude::*,
     reflect::TypeUuid,
@@ -11,16 +9,20 @@ use bevy::{
         },
         render_graph::{
             base::{node::MAIN_PASS, MainPass},
-            CameraNode, Node, PassNode, RenderGraph, ResourceSlotInfo,
+            CameraNode, PassNode, RenderGraph, TextureNode,
         },
-        renderer::{RenderResourceId, RenderResourceType},
         texture::{
             Extent3d, SamplerDescriptor, TextureDescriptor, TextureDimension, TextureFormat,
-            TextureUsage, SAMPLER_ASSET_INDEX, TEXTURE_ASSET_INDEX,
+            TextureUsage,
         },
     },
     window::WindowId,
 };
+
+pub struct FirstPass;
+/// this component indicates what entities should rotate
+struct Rotator;
+struct Cube;
 
 pub const RENDER_TEXTURE_HANDLE: HandleUntyped =
     HandleUntyped::weak_from_u64(Texture::TYPE_UUID, 13378939762009864029);
@@ -31,11 +33,11 @@ pub const FIRST_PASS: &str = "first_pass";
 pub const FIRST_PASS_CAMERA: &str = "first_pass_camera";
 
 pub trait RenderToTextureGraphBuilder {
-    fn add_render_to_texture_graph(&mut self, active_cameras: &mut ActiveCameras) -> &mut Self;
+    fn add_render_to_texture_graph(&mut self, size: Extent3d) -> &mut Self;
 }
 
 impl RenderToTextureGraphBuilder for RenderGraph {
-    fn add_render_to_texture_graph(&mut self, active_cameras: &mut ActiveCameras) -> &mut Self {
+    fn add_render_to_texture_graph(&mut self, size: Extent3d) -> &mut Self {
         let mut pass_node = PassNode::<&FirstPass>::new(PassDescriptor {
             color_attachments: vec![RenderPassColorAttachmentDescriptor {
                 attachment: TextureAttachment::Input("color_attachment".to_string()),
@@ -55,19 +57,18 @@ impl RenderToTextureGraphBuilder for RenderGraph {
             }),
             sample_count: 1,
         });
+
         pass_node.add_camera(FIRST_PASS_CAMERA);
 
         self.add_node(FIRST_PASS, pass_node);
         self.add_system_node(FIRST_PASS_CAMERA, CameraNode::new(FIRST_PASS_CAMERA));
-
-        active_cameras.add(FIRST_PASS_CAMERA);
         self.add_node_edge(FIRST_PASS_CAMERA, FIRST_PASS).unwrap();
 
         self.add_node(
             TEXTURE_NODE,
             TextureNode::new(
                 TextureDescriptor {
-                    size: Extent3d::new(512, 512, 1),
+                    size,
                     mip_level_count: 1,
                     sample_count: 1,
                     dimension: TextureDimension::D2,
@@ -83,7 +84,7 @@ impl RenderToTextureGraphBuilder for RenderGraph {
             DEPTH_TEXTURE_NODE,
             TextureNode::new(
                 TextureDescriptor {
-                    size: Extent3d::new(512, 512, 1),
+                    size,
                     mip_level_count: 1,
                     sample_count: 1,
                     dimension: TextureDimension::D2,
@@ -116,74 +117,6 @@ impl RenderToTextureGraphBuilder for RenderGraph {
     }
 }
 
-/// this component indicates what entities should rotate
-struct Rotator;
-struct Cube;
-
-#[derive(Default)]
-pub struct FirstPass;
-
-pub struct TextureNode {
-    pub texture_descriptor: TextureDescriptor,
-    pub sampler_descriptor: Option<SamplerDescriptor>,
-    pub handle: Option<HandleUntyped>,
-}
-
-impl TextureNode {
-    pub const TEXTURE: &'static str = "texture";
-
-    pub fn new(
-        texture_descriptor: TextureDescriptor,
-        sampler_descriptor: Option<SamplerDescriptor>,
-        handle: Option<HandleUntyped>,
-    ) -> Self {
-        Self {
-            texture_descriptor,
-            sampler_descriptor,
-            handle,
-        }
-    }
-}
-
-impl Node for TextureNode {
-    fn output(&self) -> &[ResourceSlotInfo] {
-        static OUTPUT: &[ResourceSlotInfo] = &[ResourceSlotInfo {
-            name: Cow::Borrowed(TextureNode::TEXTURE),
-            resource_type: RenderResourceType::Texture,
-        }];
-        OUTPUT
-    }
-
-    fn update(
-        &mut self,
-        _world: &World,
-        render_context: &mut dyn bevy::render::renderer::RenderContext,
-        _input: &bevy::render::render_graph::ResourceSlots,
-        output: &mut bevy::render::render_graph::ResourceSlots,
-    ) {
-        if output.get(0).is_none() {
-            let render_resource_context = render_context.resources_mut();
-            let texture_id = render_resource_context.create_texture(self.texture_descriptor);
-            if let Some(handle) = &self.handle {
-                render_resource_context.set_asset_resource_untyped(
-                    handle.clone(),
-                    RenderResourceId::Texture(texture_id),
-                    TEXTURE_ASSET_INDEX,
-                );
-                if let Some(sampler_descriptor) = self.sampler_descriptor {
-                    let sampler_id = render_resource_context.create_sampler(&sampler_descriptor);
-                    render_resource_context.set_asset_resource_untyped(
-                        handle.clone(),
-                        RenderResourceId::Sampler(sampler_id),
-                        SAMPLER_ASSET_INDEX,
-                    );
-                }
-            }
-            output.set(0, RenderResourceId::Texture(texture_id));
-        }
-    }
-}
-
 /// rotates the inner cube (first pass)
 fn rotator_system(time: Res<Time>, mut query: Query<&mut Transform, With<Rotator>>) {
     for mut transform in query.iter_mut() {
@@ -204,7 +137,12 @@ fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
+    mut render_graph: ResMut<RenderGraph>,
+    mut active_cameras: ResMut<ActiveCameras>,
 ) {
+    let size = Extent3d::new(512, 512, 1);
+    render_graph.add_render_to_texture_graph(size);
+
     let cube_handle = meshes.add(Mesh::from(shape::Cube { size: 4.0 }));
     let cube_material_handle = materials.add(StandardMaterial {
         base_color: Color::rgb(0.8, 0.7, 0.6),
@@ -224,6 +162,7 @@ fn setup(
         .insert(Rotator)
         .insert(FirstPass)
         .remove::<MainPass>();
+
     // light
     commands.spawn_bundle(PointLightBundle {
         transform: Transform::from_translation(Vec3::new(0.0, 0.0, 10.0)),
@@ -241,9 +180,11 @@ fn setup(
             .looking_at(Vec3::default(), Vec3::Y),
         ..Default::default()
     };
-    first_pass_camera.camera.window = WindowId::new();
+    active_cameras.add(FIRST_PASS_CAMERA);
+
+    // do not use main window size / aspect for calculation of projection matrix
     let camera_projection = &mut first_pass_camera.perspective_projection;
-    camera_projection.update(512.0, 512.0);
+    camera_projection.update(size.width as f32, size.height as f32);
     first_pass_camera.camera.projection_matrix = camera_projection.get_projection_matrix();
     first_pass_camera.camera.depth_calculation = camera_projection.depth_calculation();
 
@@ -291,13 +232,6 @@ fn main() {
     app.add_plugins(DefaultPlugins)
         .add_startup_system(setup.system())
         .add_system(cube_rotator_system.system())
-        .add_system(rotator_system.system());
-    {
-        let world_cell = app.world_mut().cell();
-        let mut render_graph = world_cell.get_resource_mut::<RenderGraph>().unwrap();
-        let mut active_cameras = world_cell.get_resource_mut::<ActiveCameras>().unwrap();
-        render_graph.add_render_to_texture_graph(&mut active_cameras);
-    }
-
-    app.run();
+        .add_system(rotator_system.system())
+        .run();
 }
