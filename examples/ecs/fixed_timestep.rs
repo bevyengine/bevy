b163425efe368d1494ff561e7d5842412f1dfda1
François Mockers <mockersf@gmail.com>
2021-04-22 22:18:48 +0200

    change back warn to info

--- examples/ecs/fixed_timestep.rs
+++ examples/ecs/fixed_timestep.rs
@@ -35,13 +35,13 @@ fn frame_update(mut last_time: Local<f64>, time: Res<Time>) {
 }
 
 fn fixed_update(mut last_time: Local<f64>, time: Res<Time>, fixed_timesteps: Res<FixedTimesteps>) {
-    warn!(
+    info!(
         "fixed_update: {}",
         time.seconds_since_startup() - *last_time,
     );
 
     let fixed_timestep = fixed_timesteps.get(LABEL).unwrap();
-    warn!(
+    info!(
         "  overstep_percentage: {}",
         fixed_timestep.overstep_percentage()
     );
