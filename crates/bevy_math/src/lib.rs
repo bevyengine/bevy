e54efb34be32d1dbaee935c166c804bc185c54e8
Daniel Borges <msk@lywenn.eu.org>
2021-04-27 01:42:01 +0200

    export smooth damp max in prelude

--- crates/bevy_math/src/lib.rs
+++ crates/bevy_math/src/lib.rs
@@ -10,6 +10,6 @@ pub use smooth::*;
 pub mod prelude {
     pub use crate::{
         BVec2, BVec3, BVec4, FaceToward, IVec2, IVec3, IVec4, Mat3, Mat4, Quat, Rect, Size,
-        SmoothDamp, UVec2, UVec3, UVec4, Vec2, Vec3, Vec4,
+        SmoothDamp, SmoothDampMax, UVec2, UVec3, UVec4, Vec2, Vec3, Vec4,
     };
 }
