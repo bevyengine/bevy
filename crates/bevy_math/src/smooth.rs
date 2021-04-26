ece16673a88670b3150370f312bd6b0d14058b83
Daniel Borges <msk@lywenn.eu.org>
2021-04-27 01:55:57 +0200

    more macros for even less code duplication

--- crates/bevy_math/src/smooth.rs
+++ crates/bevy_math/src/smooth.rs
@@ -74,8 +74,8 @@ pub trait SmoothDampMax {
         Self: Sized;
 }
 
-macro_rules! impl_smooth_damp_max_scalar {
-    ($t:ty) => {
+macro_rules! impl_smooth_damp_max {
+    ($t:ty, $f:ty, $clamp:expr) => {
         impl SmoothDampMax for $t {
             fn smooth_damp_max(
                 from: $t,
@@ -85,9 +85,9 @@ macro_rules! impl_smooth_damp_max_scalar {
                 smooth_time: f32,
                 delta_time: f32,
             ) -> ($t, $t) {
-                let max_speed = <$t>::max(max_speed as $t, 0.0001); // ensure max speed is positive and non-zero
-                let smooth_time = <$t>::max(smooth_time as $t, 0.0001); // ensure smooth_time is positive and non-zero
-                let delta_time = delta_time as $t;
+                let max_speed = <$f>::max(max_speed as $f, 0.0001); // ensure max speed is positive and non-zero
+                let smooth_time = <$f>::max(smooth_time as $f, 0.0001); // ensure smooth_time is positive and non-zero
+                let delta_time = delta_time as $f;
 
                 // from game programming gems 4, chapter 1.10
                 let omega = 2.0 / smooth_time;
@@ -97,7 +97,7 @@ macro_rules! impl_smooth_damp_max_scalar {
                 // let exp = 1.0 / (1.0 + x * (1.0 + x * (0.48 + 0.235 * x))); // TODO: profile me, both in debug & release
 
                 let max = max_speed * delta_time;
-                let change = <$t>::clamp(from - to, -max, max);
+                let change = $clamp(from, to, max); //<$t>::clamp(from - to, -max, max);
 
                 let temp = (velocity + omega * change) * delta_time;
 
@@ -110,43 +110,7 @@ macro_rules! impl_smooth_damp_max_scalar {
     };
 }
 
-impl_smooth_damp_max_scalar! {f32}
-impl_smooth_damp_max_scalar! {f64}
-
-macro_rules! impl_smooth_damp_max_vec {
-    ($t:ty) => {
-        impl SmoothDampMax for $t {
-            fn smooth_damp_max(
-                from: $t,
-                to: $t,
-                velocity: $t,
-                max_speed: f32,
-                smooth_time: f32,
-                delta_time: f32,
-            ) -> ($t, $t) {
-                let max_speed = f32::max(max_speed, 0.0001); // ensure max speed is positive and non-zero
-                let smooth_time = f32::max(smooth_time, 0.0001); // ensure smooth_time is positive and non-zero
-
-                // from game programming gems 4, chapter 1.10
-                let omega = 2.0 / smooth_time;
-                let x = omega * delta_time;
-
-                let exp = 1.0 / (1.0 + x + 0.48 * x * x + 0.235 * x * x * x);
-                // let exp = 1.0 / (1.0 + x * (1.0 + x * (0.48 + 0.235 * x))); // TODO: profile me, both in debug & release
-
-                let max = max_speed * delta_time;
-                let change = (from - to).clamp_length_max(max);
-
-                let temp = (velocity + omega * change) * delta_time;
-
-                (
-                    to + (change + temp) * exp,      // position
-                    (velocity - omega * temp) * exp, // velocity
-                )
-            }
-        }
-    };
-}
-
-impl_smooth_damp_max_vec! {Vec2}
-impl_smooth_damp_max_vec! {Vec3}
+impl_smooth_damp_max! {f32, f32, |from, to, max:f32| { f32::clamp(from - to, -max, max) }}
+impl_smooth_damp_max! {f64, f64, |from, to, max:f64| { f64::clamp(from - to, -max, max) }}
+impl_smooth_damp_max! {Vec2, f32, |from:Vec2, to, max| { (from - to).clamp_length_max(max)}}
+impl_smooth_damp_max! {Vec3, f32, |from:Vec3, to, max| { (from - to).clamp_length_max(max)}}
