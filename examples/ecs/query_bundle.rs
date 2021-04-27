d9020d5838827197afe7ce82fb31239a73620bb8
CGMossa <cgmossa@gmail.com>
2021-04-27 13:52:19 +0200

    Fix typo and logging instead of wild printing.

--- examples/ecs/query_bundle.rs
+++ examples/ecs/query_bundle.rs
@@ -1,9 +1,19 @@
-use bevy::{ecs::schedule::RunOnce, prelude::*};
+use bevy::{
+    // diagnostic::LogDiagnosticsPlugin,
+    ecs::schedule::RunOnce,
+    log::{LogPlugin, LogSettings},
+    prelude::*,
+};
 
 fn main() {
     App::build()
-        .insert_resource(bevy::app::ScheduleRunnerSettings::run_once())
+        .insert_resource(LogSettings {
+            level: bevy::log::Level::DEBUG,
+            ..Default::default()
+        })
+        .add_plugin(LogPlugin)
         .add_plugins(MinimalPlugins)
+        .insert_resource(bevy::app::ScheduleRunnerSettings::run_once())
         .add_startup_system(setup.system())
         .add_stage("diagnostic", SystemStage::single_threaded())
         .add_system_to_stage(
@@ -30,7 +40,7 @@ struct DummyToo(usize);
 #[derive(Debug, Bundle)]
 struct DummyBundle {
     dummy_component: Dummy,
-    // dummy_too_component: DummyToo,
+    dummy_too_component: DummyToo,
 }
 
 /// Sets up entites with [Dummy] component as part of a bundle and isolated.
@@ -39,21 +49,21 @@ fn setup(mut commands: Commands) {
 
     commands.spawn().insert_bundle(DummyBundle {
         dummy_component: Dummy(222),
-        // dummy_too_component: DummyToo(333),
+        dummy_too_component: DummyToo(333),
     });
 }
 
 fn query_component_without_bundle(query: Query<&Dummy>) {
-    println!("Show all components");
+    debug!("Show all components");
     // this will necessarily have to print both components.
     query.iter().for_each(|x| {
-        dbg!(x);
+        debug!("{:?}", x);
     });
 }
 fn test_query_bundle(query: Query<&Dummy, WithBundle<DummyBundle>>) {
-    println!("Print component initated from bundle.");
+    debug!("Print component initiated from bundle.");
     // this should only print `Dummy(222)`.
     query.iter().for_each(|x| {
-        dbg!(x);
+        debug!("{:?}", x);
     });
 }
