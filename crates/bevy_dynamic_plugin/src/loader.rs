e5c21f4ddd0916215fa84a45cf994d8481099c3c
bjorn3 <bjorn3@users.noreply.github.com>
2021-04-29 14:08:19 +0200

    Fix soundness of bevy_dynamic_plugin
    * Make everything unsafe as it allows loading arbitrary code with
      potentially incompatible function signatures or init constructors
      doing bad things.
    * Ensure that the dynamic plugin is not unloaded at the end of the
      load_plugin call. This was previously already the cause because of a
      `print!("");` inside the output of `#[derive(DynamicPlugin)]` causing
      thread local storage to be accessed and thus preventing unloading with
      most libc implementations.

--- crates/bevy_dynamic_plugin/src/loader.rs
+++ crates/bevy_dynamic_plugin/src/loader.rs
@@ -2,23 +2,32 @@ use libloading::{Library, Symbol};
 
 use bevy_app::{AppBuilder, CreatePlugin, Plugin};
 
-/// Dynamically links a plugin a the given path. The plugin must export the [CreatePlugin] function.
-pub fn dynamically_load_plugin(path: &str) -> (Library, Box<dyn Plugin>) {
-    unsafe {
-        let lib = Library::new(path).unwrap();
-        let func: Symbol<CreatePlugin> = lib.get(b"_create_plugin").unwrap();
-        let plugin = Box::from_raw(func());
-        (lib, plugin)
-    }
+/// Dynamically links a plugin a the given path. The plugin must export a function with the
+/// [`CreatePlugin`] signature named `_bevy_create_plugin`.
+///
+/// # Safety
+///
+/// The specified plugin must be linked against the exact same libbevy.so as this program.
+/// In addition the `_bevy_create_plugin` symbol must not be manually created, but instead created
+/// by deriving `DynamicPlugin` on a unit struct implementing [`Plugin`].
+pub unsafe fn dynamically_load_plugin(path: &str) -> (Library, Box<dyn Plugin>) {
+    let lib = Library::new(path).unwrap();
+    let func: Symbol<CreatePlugin> = lib.get(b"_bevy_create_plugin").unwrap();
+    let plugin = Box::from_raw(func());
+    (lib, plugin)
 }
 
 pub trait DynamicPluginExt {
-    fn load_plugin(&mut self, path: &str) -> &mut Self;
+    /// # Safety
+    ///
+    /// Same as [`dynamically_load_plugin`].
+    unsafe fn load_plugin(&mut self, path: &str) -> &mut Self;
 }
 
 impl DynamicPluginExt for AppBuilder {
-    fn load_plugin(&mut self, path: &str) -> &mut Self {
-        let (_lib, plugin) = dynamically_load_plugin(path);
+    unsafe fn load_plugin(&mut self, path: &str) -> &mut Self {
+        let (lib, plugin) = dynamically_load_plugin(path);
+        std::mem::forget(lib); // Ensure that the library is not automatically unloaded
         plugin.build(self);
         self
     }
