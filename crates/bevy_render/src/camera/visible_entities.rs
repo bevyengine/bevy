3ca4b6f9e4683a4cb9a4a90636f7ef3ba9cf7969
Yoh Deadfall <yoh.deadfall@hotmail.com>
2021-05-03 14:59:59 +0200

    Fixed test

--- crates/bevy_render/src/camera/visible_entities.rs
+++ crates/bevy_render/src/camera/visible_entities.rs
@@ -356,7 +356,7 @@ mod test {
         let mut world = World::default();
         let mut update_stage = SystemStage::parallel();
         update_stage.add_system(parent_update_system.system());
-        update_stage.add_system(visible_entities_system.system());
+        update_stage.add_system(visible_effective_system.system());
 
         let mut schedule = Schedule::default();
         schedule.add_stage("update", update_stage);
