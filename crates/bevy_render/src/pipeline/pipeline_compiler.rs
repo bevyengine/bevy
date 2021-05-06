fcdce33bbac40e5fe022cdb2de6d19a3bf6016f7
Jonas Matser <github@jonasmatser.nl>
2021-05-07 00:00:00 +0200

    Fixes incorrect pipeline_compiler step_mode

--- crates/bevy_render/src/pipeline/pipeline_compiler.rs
+++ crates/bevy_render/src/pipeline/pipeline_compiler.rs
@@ -205,7 +205,7 @@ impl PipelineCompiler {
 
         // the vertex buffer descriptor that will be used for this pipeline
         let mut compiled_vertex_buffer_descriptor = VertexBufferLayout {
-            step_mode: InputStepMode::Vertex,
+            step_mode: mesh_vertex_buffer_layout.step_mode,
             stride: mesh_vertex_buffer_layout.stride,
             ..Default::default()
         };
