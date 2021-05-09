fbec101f26887ccc31750614d42e899fd7fca258
Felipe Jorge <felipe.jorge.pereira@gmail.com>
2021-05-09 01:53:04 -0300

    fix samples

--- examples/animations/smooth_curves.rs
+++ examples/animations/smooth_curves.rs
@@ -52,7 +52,7 @@ fn setup(
         vec![0.0, 1.0, 1.3, 1.6, 1.7, 1.8, 1.9, 2.0],
         vec![3.0, 0.0, 1.0, 0.0, 0.5, 0.0, 0.25, 0.0],
         TangentControl::Auto,
-        Interpolation::CatmullRom,
+        Interpolation::Hermite,
     )
     .unwrap();
     // Create timer
