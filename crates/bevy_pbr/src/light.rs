9a78b7119be1ceb3e0f732fa6d69d8b0e2662ec3
Daniel Borges <msk@lywenn.eu.org>
2021-05-09 10:42:09 +0200

    tabs -> spaces

--- crates/bevy_pbr/src/light.rs
+++ crates/bevy_pbr/src/light.rs
@@ -63,19 +63,19 @@ impl PointLightUniform {
 ///
 /// | Illuminance (lux) | Surfaces illuminated by                        |
 /// |-------------------|------------------------------------------------|
-/// | 0.0001 	        | Moonless, overcast night sky (starlight)       |
-/// | 0.002 	        | Moonless clear night sky with airglow          |
+/// | 0.0001            | Moonless, overcast night sky (starlight)       |
+/// | 0.002             | Moonless clear night sky with airglow          |
 /// | 0.05–0.3 	        | Full moon on a clear night                     |
-/// | 3.4 	            | Dark limit of civil twilight under a clear sky |
-/// | 20–50 	        | Public areas with dark surroundings            |
-/// | 50 	            | Family living room lights                      |
-/// | 80 	            | Office building hallway/toilet lighting        |
-/// | 100 	            | Very dark overcast day                         |
-/// | 150 	            | Train station platforms                        |
-/// | 320–500 	        | Office lighting                                |
-/// | 400 	            | Sunrise or sunset on a clear day.              |
-/// | 1000 	            | Overcast day; typical TV studio lighting       |
-/// | 10,000–25,000 	| Full daylight (not direct sun)                 |
+/// | 3.4               | Dark limit of civil twilight under a clear sky |
+/// | 20–50             | Public areas with dark surroundings            |
+/// | 50                | Family living room lights                      |
+/// | 80                | Office building hallway/toilet lighting        |
+/// | 100               | Very dark overcast day                         |
+/// | 150               | Train station platforms                        |
+/// | 320–500           | Office lighting                                |
+/// | 400               | Sunrise or sunset on a clear day.              |
+/// | 1000              | Overcast day; typical TV studio lighting       |
+/// | 10,000–25,000     | Full daylight (not direct sun)                 |
 /// | 32,000–100,000    | Direct sunlight                                |
 ///
 /// Source: [Wikipedia](https://en.wikipedia.org/wiki/Lux)
