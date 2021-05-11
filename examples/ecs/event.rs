e1142ad7ee9d490a4eb5041682cad127253a6f11
Alice <alice.i.cecile@gmail.com>
2021-05-11 18:41:48 -0400

    Move event.rs example to FixedTimeStep
    Consistency with event_consumer and improves simplicity

--- examples/ecs/event.rs
+++ examples/ecs/event.rs
@@ -1,3 +1,4 @@
+use bevy::core::FixedTimestep;
 use bevy::prelude::*;
 
 /// This example creates a new event, a system that triggers the event once per second,
@@ -6,8 +7,11 @@ fn main() {
     App::build()
         .add_plugins(DefaultPlugins)
         .add_event::<MyEvent>()
-        .init_resource::<EventTriggerState>()
-        .add_system(event_trigger_system.system())
+        .add_system(
+            event_trigger_system
+                .system()
+                .with_run_criteria(FixedTimestep::step(1.0)),
+        )
         .add_system(event_listener_system.system())
         .run();
 }
@@ -16,29 +20,11 @@ struct MyEvent {
     pub message: String,
 }
 
-struct EventTriggerState {
-    event_timer: Timer,
-}
-
-impl Default for EventTriggerState {
-    fn default() -> Self {
-        EventTriggerState {
-            event_timer: Timer::from_seconds(1.0, true),
-        }
-    }
-}
-
 // sends MyEvent every second
-fn event_trigger_system(
-    time: Res<Time>,
-    mut state: ResMut<EventTriggerState>,
-    mut my_events: EventWriter<MyEvent>,
-) {
-    if state.event_timer.tick(time.delta()).finished() {
-        my_events.send(MyEvent {
-            message: "MyEvent just happened!".to_string(),
-        });
-    }
+fn event_trigger_system(mut my_events: EventWriter<MyEvent>) {
+    my_events.send(MyEvent {
+        message: "MyEvent just happened!".to_string(),
+    });
 }
 
 // prints events as they come in
