6f814136dd4c3d60a07bc658596a334835cd4265
GreyGoo <zsw_shelter@live.com>
2021-05-16 04:13:00 +0800

    added test and fix unused field

--- crates/bevy_tasks/src/priority_executor.rs
+++ crates/bevy_tasks/src/priority_executor.rs
@@ -1,5 +1,4 @@
 use std::future::Future;
-use std::sync::atomic::AtomicUsize;
 
 use async_executor::{Executor, Task};
 use event_listener::Event;
@@ -17,7 +16,6 @@ pub enum Priority {
 pub struct PriorityExecutor<'a> {
     ex: [Executor<'a>; 3],
     event: Event,
-    worker: AtomicUsize,
 }
 
 impl<'a> Default for PriorityExecutor<'a> {
@@ -32,7 +30,6 @@ impl<'a> PriorityExecutor<'a> {
         PriorityExecutor {
             ex: [Executor::new(), Executor::new(), Executor::new()],
             event: Event::new(),
-            worker: AtomicUsize::new(0),
         }
     }
 
@@ -64,5 +61,135 @@ impl<'a> PriorityExecutor<'a> {
 
     pub fn tick(&self) {
         self.event.notify(usize::MAX);
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use std::sync::{Arc, RwLock};
+    use std::thread;
+    use std::time::Duration;
+
+    use futures_lite::future;
+    use instant::Instant;
+
+    use super::*;
+
+    #[inline(never)]
+    async fn a_task(duration: Duration, priority: Priority, counters: Arc<RwLock<[usize; 3]>>) {
+        let start = Instant::now();
+        while Instant::now() - start < duration {}
+        counters.write().unwrap()[priority as usize] += 1;
+    }
+
+    #[test]
+    fn test_priority_executor() {
+        let tasks_completed: Arc<RwLock<[usize; 3]>> = Arc::new(RwLock::new([0, 0, 0]));
+        let mut tasks_dispatched: [usize; 3] = [0, 0, 0];
+        let executor = PriorityExecutor::new();
+        let executor = Arc::new(executor);
+        let (shutdown_tx, shutdown_rx) = async_channel::unbounded::<()>();
+
+        let num_cpus = num_cpus::get();
+        let mut job_handles = vec![];
+
+        for _ in 0..num_cpus {
+            let executor = executor.clone();
+            let shutdown_rx = shutdown_rx.clone();
+            // Spawn a thread running the executor forever.
+
+            let job_handle = thread::spawn(move || {
+                let shutdown_future = executor.run(shutdown_rx.recv());
+                // Use unwrap_err because we expect a Closed error
+                future::block_on(shutdown_future).unwrap_err();
+                //println!("worker thread {} exits", x)
+            });
+            job_handles.push(job_handle);
+        }
+
+        let mut tasks: Vec<(Task<_>, Priority)> = Vec::new();
+        let priority_choice = [
+            Priority::FinishWithinFrame,
+            Priority::AcrossFrame,
+            Priority::IO,
+        ];
+
+        let time_choice = [
+            Duration::new(0, 500_000),
+            Duration::new(0, 50_000_000),
+            Duration::new(0, 500_000_000),
+        ];
+
+        let epochs: [u32; 3] = [num_cpus as u32 * 30 / 4, num_cpus as u32 * 2 / 4, 1];
+        let task_periods: [u32; 3] = [1, 6, 36];
+
+        let mut task_period_counter = [0, 0, 0];
+        let mut frame_counter = 0;
+
+        loop {
+            if frame_counter >= 60 {
+                break;
+            }
+
+            for i in 0..3 {
+                task_period_counter[i] += 1;
+                if task_period_counter[i] < task_periods[i] {
+                    continue;
+                }
+                let priority = priority_choice[i];
+                let time = time_choice[i];
+                let epoch = epochs[i];
+
+                tasks_dispatched[priority as usize] += epoch as usize;
+                for _ in 0..epoch {
+                    tasks.push((
+                        executor.spawn(priority, a_task(time, priority, tasks_completed.clone())),
+                        priority,
+                    ));
+                }
+                task_period_counter[i] = 0;
+            }
+
+            frame_counter += 1;
+            executor.tick();
+
+            let mut i = 0;
+            while i < tasks.len() {
+                match tasks[i].1 {
+                    Priority::FinishWithinFrame => {
+                        let task = tasks.swap_remove(i);
+                        future::block_on(task.0);
+                    }
+                    Priority::AcrossFrame => {
+                        i += 1;
+                    }
+                    Priority::IO => {
+                        i += 1;
+                    }
+                }
+            }
+            let tasks_completed = *tasks_completed.read().unwrap();
+
+            assert_eq!(tasks_completed[0], tasks_dispatched[0]);
+        }
+        for task in tasks.drain(..) {
+            future::block_on(async { task.0.await });
+        }
+        shutdown_tx.close();
+        for (task_completed, task_dispatched) in tasks_completed
+            .read()
+            .unwrap()
+            .iter()
+            .zip(tasks_dispatched.iter())
+        {
+            assert_eq!(*task_completed, *task_dispatched)
+        }
+
+        // if the test does not finish, there is potential deadlock
+        for job_handle in job_handles.drain(..) {
+            job_handle
+                .join()
+                .expect("Task thread panicked while executing.");
+        }
     }
 }
