271e3b53d38265ca726cb710b90264141fbe021a
the-notable <the-notable@users.noreply.github.com>
2021-05-15 20:48:13 -0700

    Update examples/tasks/async_compute.rs
    Co-authored-by: Nathan Ward <43621845+NathanSWard@users.noreply.github.com>

--- examples/tasks/async_compute.rs
+++ examples/tasks/async_compute.rs
@@ -1,5 +1,4 @@
-use bevy::prelude::*;
-use bevy::tasks::{AsyncComputeTaskPool, Task};
+use bevy::{prelude::*, tasks::{AsyncComputeTaskPool, Task};
 use std::time::{Instant, Duration};
 use rand::Rng;
 use futures_lite::future;
