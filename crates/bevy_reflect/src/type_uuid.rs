071965996be31b44170bef1cddf01950cdce4244
Nathan Ward <nathansward@comcast.net>
2021-05-17 20:28:50 +0000

    revert supporting generics for deriving TypeUuid (#2204)
    This reverts some of the changes made in #2044 as supporting generics for a `#[derive(TypeUuid)]` should not work as each generic instantiation would have the same uuid.
    
    Stems from [this conversation](https://github.com/bevyengine/bevy/pull/2044#issuecomment-841743135)

--- crates/bevy_reflect/src/type_uuid.rs
+++ crates/bevy_reflect/src/type_uuid.rs
@@ -22,25 +22,3 @@ where
         std::any::type_name::<Self>()
     }
 }
-
-#[cfg(test)]
-mod test {
-    use super::*;
-
-    #[derive(TypeUuid)]
-    #[uuid = "af6466c2-a9f4-11eb-bcbc-0242ac130002"]
-    struct TestDeriveStruct<T>
-    where
-        T: Clone,
-    {
-        _value: T,
-    }
-
-    fn test_impl_type_uuid(_: &impl TypeUuid) {}
-
-    #[test]
-    fn test_generic_type_uuid_derive() {
-        let test_struct = TestDeriveStruct { _value: 42 };
-        test_impl_type_uuid(&test_struct);
-    }
-}
