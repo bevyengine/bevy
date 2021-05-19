76ce2beaf4ecc8eeabb0ff4bc5f3117fa8f10028
AmrDhaliwal <amritdhaliwalz@gmail.com>
2021-05-19 13:30:13 -0700

    Added mouse inputs to the generic_systems example.

--- examples/ecs/generic_systems.rs
+++ examples/ecs/generic_systems.rs
@@ -1,20 +1,26 @@
 use bevy::prelude::*;
 use bevy::ecs::component::Component;
 
-// When entities spawn in call an event
-
 struct A;
 struct B;
 
-fn spawn_entities<T: Component>(mut commands: Commands, q: Query<Entity, With<T>>) {
-    q.for_each(|entity| { commands.entity(entity).spawn();});
-    info!("Entity {:?} spawned in!")
+pub fn spawn_entities<T: Component>(mut commands: Commands,
+    mouse_button_inputs: Res<Input<MouseButton>>,) {
+    for _ in 0..10 {
+        let id = commands.spawn().insert(T).id();
+        if !mouse_button_inputs.just_pressed(MouseButton::Left) {
+            return info!("Spawned entity {:?} with component {}", id, std::any::type_name::<T>());
+        }
+        if !mouse_button_inputs.just_pressed(MouseButton::Right) {
+            return info!("Spawned entity {:?} with component {}", id, std::any::type_name::<T>());
+        }
+    }
 }
-
 
 
 fn main() {
     App::build()
+        .add_plugins(DefaultPlugins)
         .add_system(spawn_entities::<A>.system())
         .add_system(spawn_entities::<B>.system())
         .run();
