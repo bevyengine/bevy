993cb239ddc46f3cc340ee653660cae3a671ae2e
AmrDhaliwal <amritdhaliwalz@gmail.com>
2021-05-27 11:40:45 -0700

    Updated the generic system example to be able to us const generics.

--- examples/ecs/generic_systems.rs
+++ examples/ecs/generic_systems.rs
@@ -4,21 +4,23 @@ use bevy::ecs::component::Component;
 struct A;
 struct B;
 
-pub fn spawn_entities<T: Component>(mut commands: Commands,
-    mouse_button_inputs: Res<Input<MouseButton>>,) {
-    for _ in 0..10 {
-        let id = commands.spawn().insert(T).id();
-        info!("Spawned entity {:?} with component {}", id, std::any::type_name::<T>());
+fn spawn_entities_on_click<T: Component, const N: usize>(
+    mut cmds: Commands,
+    mouse_button_inputs: Res<Input<MouseButton>>,
+) {
+    if mouse_button_inputs.just_pressed(MouseButton::Left) {
+        for _ in 0..N {
+            cmds.spawn().insert(T).id();
+        }
+        info!("spawned {} entities with component {}", N, std::any::type_name::<T>());
     }
-    mouse_button_inputs.just_pressed(MouseButton::Left);
-    mouse_button_inputs.just_pressed(MouseButton::Right);
 }
 
 
 fn main() {
     App::build()
         .add_plugins(DefaultPlugins)
-        .add_system(spawn_entities::<A>.system())
-        .add_system(spawn_entities::<B>.system())
+        .add_system(spawn_entities_on_click::<A>.system())
+        .add_system(spawn_entities_on_click::<B>.system())
         .run();
 }
