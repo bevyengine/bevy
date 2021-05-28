911b603b1d173eb92a69a5d32f5c032e98e85e53
AmrDhaliwal <amritdhaliwalz@gmail.com>
2021-05-28 11:10:26 -0700

    Moved the fn main() to the top of the file and constrained T on default.

--- examples/ecs/generic_systems.rs
+++ examples/ecs/generic_systems.rs
@@ -1,21 +1,4 @@
-use bevy::prelude::*;
-use bevy::ecs::component::Component;
-
-struct A;
-struct B;
-
-fn spawn_entities_on_click<T: Component, const N: usize>(
-    mut cmds: Commands,
-    mouse_button_inputs: Res<Input<MouseButton>>,
-) {
-    if mouse_button_inputs.just_pressed(MouseButton::Left) {
-        for _ in 0..N {
-            cmds.spawn().insert(T).id();
-        }
-        info!("spawned {} entities with component {}", N, std::any::type_name::<T>());
-    }
-}
-
+use bevy::{prelude::*, ecs::component::Component};
 
 fn main() {
     App::build()
@@ -24,3 +7,22 @@ fn main() {
         .add_system(spawn_entities_on_click::<B>.system())
         .run();
 }
+
+struct A;
+struct B;
+
+fn spawn_entities_on_click<T: Component, const N: usize>(
+    mut cmds: Commands,
+    mouse_button_inputs: Res<Input<MouseButton>>,
+)
+where
+    T: Component + Default
+    {
+    if mouse_button_inputs.just_pressed(MouseButton::Left) {
+        for _ in 0..N {
+            cmds.spawn().insert(T::default()).id();
+        }
+        info!("spawned {} entities with component {}", N, std::any::type_name::<T>());
+    }
+}
+
