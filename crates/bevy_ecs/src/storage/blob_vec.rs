1f849d76e26106401272014c0506bba6a90b6265
s33n <sean.kaufman92@gmail.com>
2021-06-02 06:08:12 -0700

    fixes dropping empty BlobVec

--- crates/bevy_ecs/src/storage/blob_vec.rs
+++ crates/bevy_ecs/src/storage/blob_vec.rs
@@ -199,12 +199,13 @@ impl BlobVec {
 impl Drop for BlobVec {
     fn drop(&mut self) {
         self.clear();
-        if self.item_layout.size() > 0 {
+        let array_layout = array_layout(&self.item_layout, self.capacity)
+            .expect("array layout should be valid");
+        if array_layout.size() > 0 {
             unsafe {
                 std::alloc::dealloc(
                     self.get_ptr().as_ptr(),
-                    array_layout(&self.item_layout, self.capacity)
-                        .expect("array layout should be valid"),
+                    array_layout,
                 );
                 std::alloc::dealloc(self.swap_scratch.as_ptr(), self.item_layout);
             }
