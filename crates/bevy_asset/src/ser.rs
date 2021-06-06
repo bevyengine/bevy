2d8f84946d77ad35c9033f08bd51ad5ae92c6280
Felipe Jorge <felipe.jorge.pereira@gmail.com>
2021-06-06 00:12:34 -0300

    serialize local asset references

--- crates/bevy_asset/src/ser.rs
+++ crates/bevy_asset/src/ser.rs
@@ -1,8 +1,9 @@
 use std::cell::Cell;
 
+use bevy_reflect::Uuid;
 use serde::{Deserialize, Deserializer, Serialize, Serializer};
 
-use crate::{Asset, AssetServer, Handle};
+use crate::{Asset, AssetServer, Handle, HandleId, HandleUntyped};
 
 ///////////////////////////////////////////////////////////////////////////////
 
@@ -10,27 +11,44 @@ thread_local! {
     static ASSET_SERVER: Cell<Option<AssetServer>> = Cell::new(None);
 }
 
+#[derive(Serialize, Deserialize)]
+enum AssetRef {
+    Default,
+    /// Used for static handles like the `PBR_PIPELINE_HANDLE` or a embedded assets
+    Local(Uuid, u64),
+    /// Loads form a file
+    External(String),
+}
+
 impl<T: Asset> Serialize for Handle<T> {
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
     where
         S: Serializer,
     {
-        let path = ASSET_SERVER.with(|cell| {
-            let server = cell.replace(None);
-            let path = server.as_ref().and_then(|server| {
-                // TODO: `get_handle_path` does absolutely nothing issue #1290
-                server.get_handle_path(self).map(|asset_path| {
-                    let mut path = asset_path.path().to_string_lossy().to_string();
-                    if let Some(label) = asset_path.label() {
-                        path.push('#');
-                        path.push_str(label);
-                    }
-                    path
-                })
+        let path = ASSET_SERVER
+            .with(|cell| {
+                let server = cell.replace(None);
+                let path = server.as_ref().and_then(|server| {
+                    // TODO: `get_handle_path` does absolutely nothing issue #1290
+                    server
+                        .get_handle_path(self)
+                        .map(|asset_path| {
+                            let mut path = asset_path.path().to_string_lossy().to_string();
+                            if let Some(label) = asset_path.label() {
+                                path.push('#');
+                                path.push_str(label);
+                            }
+                            path
+                        })
+                        .and_then(|path| Some(AssetRef::External(path)))
+                });
+                cell.replace(server);
+                path
+            })
+            .unwrap_or_else(|| match &self.id {
+                HandleId::Id(type_uuid, id) => AssetRef::Local(*type_uuid, *id),
+                HandleId::AssetPathId(_) => AssetRef::Default,
             });
-            cell.replace(server);
-            path
-        });
 
         path.serialize(serializer)
     }
@@ -41,16 +59,21 @@ impl<'de, T: Asset> Deserialize<'de> for Handle<T> {
     where
         D: Deserializer<'de>,
     {
-        Ok(Option::<String>::deserialize(deserializer)?
-            .and_then(|path| {
-                ASSET_SERVER.with(|cell| {
-                    let server = cell.replace(None);
-                    let handle = server.as_ref().map(|server| server.load(path.as_str()));
-                    cell.replace(server);
-                    handle
-                })
-            })
-            .unwrap_or_default())
+        match AssetRef::deserialize(deserializer)? {
+            AssetRef::Default => Ok(Handle::default()),
+            AssetRef::Local(type_uuid, id) => {
+                Ok(HandleUntyped::weak_from_u64(type_uuid, id).typed())
+            }
+            AssetRef::External(path) => ASSET_SERVER.with(|cell| {
+                let server = cell.replace(None);
+                let handle = server
+                    .as_ref()
+                    .map(|server| server.load(path.as_str()))
+                    .unwrap_or_default();
+                cell.replace(server);
+                Ok(handle)
+            }),
+        }
     }
 }
 
