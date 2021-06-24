b7c3a48e56a6a76cd8fcd0341fba3e53ac01bc1e
james7132 <contact@jamessliu.com>
2021-06-24 02:47:20 -0700

    Add SystemGraph implementation

--- crates/bevy_ecs/src/schedule/mod.rs
+++ crates/bevy_ecs/src/schedule/mod.rs
@@ -12,6 +12,7 @@ mod stage;
 mod state;
 mod system_container;
 mod system_descriptor;
+mod system_graph;
 mod system_set;
 
 pub use executor::*;
@@ -23,6 +24,7 @@ pub use stage::*;
 pub use state::*;
 pub use system_container::*;
 pub use system_descriptor::*;
+pub use system_graph::*;
 pub use system_set::*;
 
 use std::fmt::Debug;
