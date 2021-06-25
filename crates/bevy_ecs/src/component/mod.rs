6df3e41e2123c22c193dc10c7bb33996f36395d8
NathanW <nathansward@comcast.net>
2021-06-25 12:31:54 -0600

    fix imports... again

--- crates/bevy_ecs/src/component/mod.rs
+++ crates/bevy_ecs/src/component/mod.rs
@@ -32,7 +32,7 @@ impl<T: Send + Sync + 'static> Component for T {}
 /// The [`StorageType`] for a component is normally configured via `World::register_component`.
 ///
 /// ```
-/// # use bevy_ecs::{prelude::*, component::ComponentDescriptor};
+/// # use bevy_ecs::{prelude::*, component::*};
 ///
 /// struct A;
 ///
