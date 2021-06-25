6237c9fefe753b3b39e28a530cc537ba8361e69b
james7132 <contact@jamessliu.com>
2021-06-25 01:33:59 -0700

    Use separate graph and node counters

--- crates/bevy_ecs/src/schedule/system_graph.rs
+++ crates/bevy_ecs/src/schedule/system_graph.rs
@@ -8,7 +8,7 @@ use std::{
     sync::atomic::{AtomicU32, Ordering},
 };
 
-static NEXT_NODE_ID: AtomicU32 = AtomicU32::new(0);
+static NEXT_GRAPH_ID: AtomicU32 = AtomicU32::new(0);
 
 /// A builder for creating graphs of dependent parallel execution within a `SystemStage`.
 ///
@@ -86,12 +86,18 @@ static NEXT_NODE_ID: AtomicU32 = AtomicU32::new(0);
 /// # Cloning
 /// This type is backed by a Rc, so cloning it will still point to the same logical
 /// underlying graph.
-#[derive(Clone, Default)]
-pub struct SystemGraph(Rc<RefCell<HashMap<NodeId, SystemDescriptor>>>);
+#[derive(Clone)]
+pub struct SystemGraph {
+    id: u32,
+    nodes: Rc<RefCell<HashMap<NodeId, SystemDescriptor>>>,
+}
 
 impl SystemGraph {
     pub fn new() -> Self {
-        Self::default()
+        Self {
+            id: NEXT_GRAPH_ID.fetch_add(1, Ordering::Relaxed),
+            nodes: Default::default(),
+        }
     }
 
     /// Creates a root graph node without any dependencies. A graph can have multiple distinct
@@ -102,16 +108,25 @@ impl SystemGraph {
 
     /// Checks if two graphs instances point to the same logical underlying graph.
     pub fn is_same_graph(&self, other: &Self) -> bool {
-        Rc::ptr_eq(&self.0, &other.0)
+        self.id == other.id
     }
 
     fn create_node(&self, mut system: SystemDescriptor) -> SystemGraphNode {
-        let id = NodeId(NEXT_NODE_ID.fetch_add(1, Ordering::Relaxed));
+        let mut nodes = self.nodes.borrow_mut();
+        assert!(
+            nodes.len() <= u32::MAX as usize,
+            "Cannot add more than {} systems to a SystemGraph",
+            u32::MAX
+        );
+        let id = NodeId {
+            graph_id: self.id,
+            node_id: nodes.len() as u32,
+        };
         match &mut system {
             SystemDescriptor::Parallel(descriptor) => descriptor.labels.push(id.dyn_clone()),
             SystemDescriptor::Exclusive(descriptor) => descriptor.labels.push(id.dyn_clone()),
         }
-        self.0.borrow_mut().insert(id, system);
+        nodes.insert(id, system);
         SystemGraphNode {
             id,
             graph: self.clone(),
@@ -119,7 +134,7 @@ impl SystemGraph {
     }
 
     fn add_dependency(&self, src: NodeId, dst: NodeId) {
-        if let Some(system) = self.0.borrow_mut().get_mut(&dst) {
+        if let Some(system) = self.nodes.borrow_mut().get_mut(&dst) {
             match system {
                 SystemDescriptor::Parallel(descriptor) => descriptor.after.push(src.dyn_clone()),
                 SystemDescriptor::Exclusive(descriptor) => descriptor.after.push(src.dyn_clone()),
@@ -140,7 +155,7 @@ impl SystemGraph {
 impl From<SystemGraph> for SystemSet {
     fn from(graph: SystemGraph) -> Self {
         let mut system_set = SystemSet::new();
-        for (_, system) in graph.0.borrow_mut().drain() {
+        for (_, system) in graph.nodes.borrow_mut().drain() {
             system_set = system_set.with_system(system);
         }
         system_set
@@ -233,7 +248,10 @@ pub trait SystemGraphJoinExt: Sized + IntoIterator<Item = SystemGraphNode> {
 impl<T: IntoIterator<Item = SystemGraphNode>> SystemGraphJoinExt for T {}
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-struct NodeId(u32);
+struct NodeId {
+    graph_id: u32,
+    node_id: u32,
+}
 
 impl SystemLabel for NodeId {
     fn dyn_clone(&self) -> Box<dyn SystemLabel> {
