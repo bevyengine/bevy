27d8f1f592588f75414eb7aa8b7e25570c0e8fde
james7132 <contact@jamessliu.com>
2021-06-25 16:03:16 -0700

    Add tuple implementations for SystemJoin

--- crates/bevy_ecs/src/lib.rs
+++ crates/bevy_ecs/src/lib.rs
@@ -26,8 +26,8 @@ pub mod prelude {
         schedule::{
             AmbiguitySetLabel, ExclusiveSystemDescriptorCoercion, ParallelSystemDescriptorCoercion,
             RunCriteria, RunCriteriaDescriptorCoercion, RunCriteriaLabel, RunCriteriaPiping,
-            Schedule, Stage, StageLabel, State, SystemGraph, SystemGraphJoinExt, SystemGroup,
-            SystemLabel, SystemSet, SystemStage,
+            Schedule, Stage, StageLabel, State, SystemGraph, SystemGroup, SystemJoin, SystemLabel,
+            SystemSet, SystemStage,
         },
         system::{
             Commands, In, IntoChainSystem, IntoExclusiveSystem, IntoSystem, Local, NonSend,
