68e1a201cf2911a00495e5a46a11e2d5c6955e3c
Jacob Edie <jacobedie1@gmail.com>
2021-06-29 11:51:12 -0400

    added move_sprite example
    fixed line endings
    
    added space for CI
    
    changed to enum
    
    fixed formating
    
    fix formating again
    
    added transform to start.
    
    removed system

---
+++ examples/2d/move_sprite.rs
@@ -0,0 +1,45 @@
+use bevy::prelude::*;
+
+fn main() {
+    App::build()
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(setup)
+        .add_system(sprite_movement)
+        .run();
+}
+
+enum Direction {
+    Up,
+    Down,
+}
+
+fn setup(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    let texture_handle = asset_server.load("branding/icon.png");
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+    commands
+        .spawn_bundle(SpriteBundle {
+            material: materials.add(texture_handle.into()),
+            transform: Transform::from_xyz(100., 0., 0.),
+            ..Default::default()
+        })
+        .insert(Direction::Up);
+}
+
+fn sprite_movement(time: Res<Time>, mut sprite_position: Query<(&mut Direction, &mut Transform)>) {
+    for (mut logo, mut transform) in sprite_position.iter_mut() {
+        match *logo {
+            Direction::Up => transform.translation.y += 150. * time.delta_seconds(),
+            Direction::Down => transform.translation.y -= 150. * time.delta_seconds(),
+        }
+
+        if transform.translation.y > 200. {
+            *logo = Direction::Down;
+        } else if transform.translation.y < -200. {
+            *logo = Direction::Up;
+        }
+    }
+}
