02375f6a329c38802d10d6b082ee2a0d37b694ba
François Mockers <mockersf@gmail.com>
2021-07-04 22:41:19 +0200

    system yeet

--- crates/bevy_ecs/src/system/commands.rs
+++ crates/bevy_ecs/src/system/commands.rs
@@ -7,7 +7,7 @@ use crate::{
 use bevy_utils::tracing::debug;
 use std::marker::PhantomData;
 
-use super::System;
+use super::{IntoSystem, System};
 
 /// A [`World`] mutation.
 pub trait Command: Send + Sync + 'static {
@@ -181,9 +181,9 @@ impl<'a> Commands<'a> {
     }
 
     /// Run a one-off [`System`].
-    pub fn run_system(&mut self, system: impl System<In = (), Out = ()>) {
+    pub fn run_system<Params>(&mut self, system: impl IntoSystem<(), (), Params>) {
         self.queue.push(RunSystem {
-            system: Box::new(system),
+            system: Box::new(system.system()),
         });
     }
 
