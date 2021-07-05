29a92a88f52831ac659100cfae51271b3fdf3ede
MinerSebas <scherthan_sebastian@web.de>
2021-07-05 13:59:48 +0200

    Change the label from an enum to a unit type

--- crates/bevy_app/src/lib.rs
+++ crates/bevy_app/src/lib.rs
@@ -18,8 +18,8 @@ pub use schedule_runner::*;
 pub mod prelude {
     #[doc(hidden)]
     pub use crate::{
-        app::App, app_builder::AppBuilder, CoreSchedule, CoreStage, DynamicPlugin, Plugin,
-        PluginGroup, StartupStage,
+        app::App, app_builder::AppBuilder, CoreStage, DynamicPlugin, Plugin, PluginGroup,
+        StartupSchedule, StartupStage,
     };
 }
 
@@ -42,11 +42,9 @@ pub enum CoreStage {
 }
 
 /// The names of the default App schedules
+/// Runs once at the beginning of the app.
 #[derive(Debug, Hash, PartialEq, Eq, Clone, StageLabel)]
-pub enum CoreSchedule {
-    /// Runs once at the beginning of the app.
-    Startup,
-}
+pub struct StartupSchedule;
 
 /// The names of the default App startup stages
 #[derive(Debug, Hash, PartialEq, Eq, Clone, StageLabel)]
