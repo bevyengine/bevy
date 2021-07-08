565a69aa3c53a22c3b54ba1076be82291a4a292a
Federico Rinaldi <gisquerin@gmail.com>
2021-07-08 16:47:24 +0200

    Correct concurrent mutable component access

--- crates/bevy_ecs/src/system/query.rs
+++ crates/bevy_ecs/src/system/query.rs
@@ -49,7 +49,8 @@ use thiserror::Error;
 /// The following example similar to the previous one, with the exception of `ComponentA`
 /// being accessed mutably here. Both mutable and immutable accesses are allowed in the same
 /// query. Two systems cannot be executed in parallel if both access a certain component and
-/// at least one of the accesses is mutable.
+/// at least one of the accesses is mutable, unless, for table-based storage type, said component
+/// lives in different archetypes.
 ///
 /// ```
 /// # use bevy_ecs::system::IntoSystem;
