a9bc51eface1d66cd6d0ec4609497e8d90b57c78
Federico Rinaldi <gisquerin@gmail.com>
2021-07-11 14:15:04 +0200

    Fix typo

--- crates/bevy_ecs/src/system/query.rs
+++ crates/bevy_ecs/src/system/query.rs
@@ -46,7 +46,7 @@ use thiserror::Error;
 ///
 /// ## Mutable component access
 ///
-/// The following example similar to the previous one, with the exception of `ComponentA`
+/// The following example is similar to the previous one, with the exception of `ComponentA`
 /// being accessed mutably here. Both mutable and immutable accesses are allowed in the same
 /// query. Two systems cannot be executed in parallel if both access a certain component and
 /// at least one of the accesses is mutable, unless, for table-based storage type, said component
