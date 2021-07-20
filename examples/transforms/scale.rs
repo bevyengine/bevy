2bc355aa8bb1130871f3a8f213eb85b450e461b5
Mizu <5565979+l4desu-mizu@users.noreply.github.com>
2021-07-21 00:02:13 +0200

    improve readability for scaling example
    * add inline comments for explanation
    * use multiple systems with single goals over one that does it all

--- examples/transforms/scale.rs
+++ examples/transforms/scale.rs
@@ -1,57 +1,94 @@
+use bevy::math::Vec3Swizzles;
 use bevy::prelude::*;
 
+// Define a component to keep information for the scaled object.
 struct Scaling {
-    scale_axis: Vec3,
+    scale_direction: Vec3,
+    scale_speed: f32,
+    max_element_size: f32,
+    min_element_size: f32,
+}
+
+// Implement a simple initialisation.
+impl Scaling {
+    fn new() -> Self {
+        Scaling {
+            scale_direction: Vec3::X,
+            scale_speed: 2.0,
+            max_element_size: 5.0,
+            min_element_size: 1.0,
+        }
+    }
 }
 
 fn main() {
     App::build()
         .add_plugins(DefaultPlugins)
         .add_startup_system(setup.system())
+        .add_system(change_scale_direction.system())
         .add_system(scale_cube.system())
         .run();
 }
 
-fn scale_cube(mut cubes: Query<(&mut Transform, &mut Scaling)>, timer: Res<Time>) {
-    for (mut transform, mut cube) in cubes.iter_mut() {
-        if transform.scale.length() >= 10.0 {
-            cube.scale_axis *= -1.0;
-        } else if transform.scale.length() < Vec3::ONE.length() {
-            // switch to next axis and expand
-            transform.scale = Vec3::ONE;
-            cube.scale_axis *= -1.0;
-            cube.scale_axis = Vec3::from((cube.scale_axis.z, cube.scale_axis.x, cube.scale_axis.y));
-        }
-        transform.scale += cube.scale_axis * timer.delta_seconds();
-    }
-}
-
+// Startup system to setup the scene and spawn all relevant entities.
 fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
+    // Spawn a cube to scale.
+    commands
+        .spawn_bundle(PbrBundle {
+            mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
+            material: materials.add(Color::WHITE.into()),
+            transform: Transform::from_translation(Vec3::ZERO),
+            ..Default::default()
+        })
+        .insert(Scaling::new());
+
+    // Spawn a camera looking at the entities to show what's happening in this example.
     commands.spawn_bundle(PerspectiveCameraBundle {
         transform: Transform::from_xyz(0.0, 10.0, 20.0).looking_at(Vec3::ZERO, Vec3::Y),
         ..Default::default()
     });
 
+    // Add a light source for better 3d visibility.
     commands.spawn_bundle(PointLightBundle {
         transform: Transform::from_translation(Vec3::ONE * 3.0),
         ..Default::default()
     });
+}
 
-    commands
-        .spawn_bundle(PbrBundle {
-            mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
-            material: materials.add(StandardMaterial {
-                base_color: Color::WHITE,
-                ..Default::default()
-            }),
-            transform: Transform::from_translation(Vec3::ZERO),
-            ..Default::default()
-        })
-        .insert(Scaling {
-            scale_axis: Vec3::X * 2.0,
-        });
+// This system will check if a scaled entity went above or below the entities scaling bounds
+// and change the direction of the scaling vector.
+fn change_scale_direction(mut cubes: Query<(&mut Transform, &mut Scaling)>) {
+    for (mut transform, mut cube) in cubes.iter_mut() {
+        // If an entity scaled beyond the maximum of its size in any dimension
+        // the scaling vector is flipped so the scaling is gradually reverted.
+        // Additionally, to ensure the condition does not trigger again we floor the elements to
+        // their next full value, which should be max_element_size at max.
+        if transform.scale.max_element() > cube.max_element_size {
+            cube.scale_direction *= -1.0;
+            transform.scale = transform.scale.floor();
+        }
+        // If an entity scaled beyond the minimum of its size in any dimension
+        // the scaling vector is also flipped.
+        // Additionally the Values are ceiled to be min_element_size at least
+        // and the scale direction is flipped.
+        // This way the entity will change the dimension in which it is scaled any time it
+        // reaches its min_element_size.
+        if transform.scale.min_element() < cube.min_element_size {
+            cube.scale_direction *= -1.0;
+            transform.scale = transform.scale.ceil();
+            cube.scale_direction = cube.scale_direction.zxy();
+        }
+    }
+}
+
+// This system will scale any entity with assigned Scaling in each direction
+// by cycling through the directions to scale.
+fn scale_cube(mut cubes: Query<(&mut Transform, &Scaling)>, timer: Res<Time>) {
+    for (mut transform, cube) in cubes.iter_mut() {
+        transform.scale += cube.scale_direction * cube.scale_speed * timer.delta_seconds();
+    }
 }
