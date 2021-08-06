7a741743d255fdce4e06c16f8684eb9a71c48581
Dimev <49782454+Dimev@users.noreply.github.com>
2021-08-07 01:50:58 +0200

    Added StorageTextureAccess to the exposed wgpu things

--- pipelined/bevy_render2/src/render_resource/mod.rs
+++ pipelined/bevy_render2/src/render_resource/mod.rs
@@ -25,7 +25,7 @@ pub use wgpu::{
     MultisampleState, Operations, PipelineLayoutDescriptor, PolygonMode, PrimitiveState,
     PrimitiveTopology, RenderPassColorAttachment, RenderPassDepthStencilAttachment,
     RenderPassDescriptor, RenderPipelineDescriptor, SamplerDescriptor, ShaderFlags, ShaderModule,
-    ShaderModuleDescriptor, ShaderSource, ShaderStage, StencilFaceState, StencilState,
+    ShaderModuleDescriptor, ShaderSource, ShaderStage, StencilFaceState, StencilState, StorageTextureAccess,
     TextureAspect, TextureDescriptor, TextureDimension, TextureFormat, TextureSampleType,
     TextureUsage, TextureViewDescriptor, TextureViewDimension, VertexAttribute, VertexBufferLayout,
     VertexFormat, VertexState,
