0df284e9c1055c48e828154d781ee9a0581060aa
Brandon Janson <bjanson28@gmail.com>
2021-08-14 22:23:20 +1000

    add zoom to orthographic example

--- examples/3d/orthographic.rs
+++ examples/3d/orthographic.rs
@@ -1,10 +1,17 @@
-use bevy::prelude::*;
+use bevy::{input::mouse::MouseWheel, prelude::*, render::camera::OrthographicProjection};
+
+const ZOOM_SPEED: f32 = 0.001;
+const MIN_ZOOM: f32 = 1.0;
+const MAX_ZOOM: f32 = 30.0;
+
+struct MainCamera;
 
 fn main() {
     App::new()
         .insert_resource(Msaa { samples: 4 })
         .add_plugins(DefaultPlugins)
         .add_startup_system(setup)
+        .add_system(zoom_system)
         .run();
 }
 
@@ -12,7 +19,7 @@ fn main() {
 fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
+    mut materials: ResMut<Assets<StandardMaterial>>
 ) {
     // set up the camera
     let mut camera = OrthographicCameraBundle::new_3d();
@@ -20,7 +27,9 @@ fn setup(
     camera.transform = Transform::from_xyz(5.0, 5.0, 5.0).looking_at(Vec3::ZERO, Vec3::Y);
 
     // camera
-    commands.spawn_bundle(camera);
+    commands
+        .spawn_bundle(camera)
+        .insert(MainCamera);
 
     // plane
     commands.spawn_bundle(PbrBundle {
@@ -58,4 +67,31 @@ fn setup(
         transform: Transform::from_xyz(3.0, 8.0, 5.0),
         ..Default::default()
     });
+}
+
+fn zoom_system(
+    mut whl: EventReader<MouseWheel>,
+    mut cam: Query<(&mut Transform, &mut OrthographicProjection), With<MainCamera>>,
+    windows: Res<Windows>
+) {
+    let delta_zoom: f32 = whl.iter().map(|e| e.y).sum();
+    if delta_zoom == 0. {
+        return;
+    }
+
+    if let Ok((mut pos, mut cam)) = cam.single_mut() {
+        let window = windows.get_primary().unwrap();
+        let window_size = Vec2::new(window.width(), window.height());
+        let mouse_normalized_screen_pos =
+            (window.cursor_position().unwrap() / window_size) * 2. - Vec2::ONE;
+        let mouse_world_pos = pos.translation.truncate()
+            + mouse_normalized_screen_pos * Vec2::new(cam.right, cam.top) * cam.scale;
+
+        cam.scale -= ZOOM_SPEED * delta_zoom * cam.scale;
+        cam.scale = cam.scale.clamp(MIN_ZOOM, MAX_ZOOM);
+
+        pos.translation = (mouse_world_pos
+            - mouse_normalized_screen_pos * Vec2::new(cam.right, cam.top) * cam.scale)
+            .extend(pos.translation.z);
+    }
 }
