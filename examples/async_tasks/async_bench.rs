accee1bb4e1b3ea8c628199c0fd4ec02badc594f
hanabi1224 <harlowmoo@gmail.com>
2021-08-20 03:47:42 +0800

    noop waker solution

--- examples/async_tasks/async_bench.rs
+++ examples/async_tasks/async_bench.rs
@@ -3,9 +3,14 @@ use bevy::{
     prelude::*,
     tasks::{AsyncComputeTaskPool, Task},
 };
-use futures_lite::future::{block_on, poll_once};
+use core::task::Context;
+use futures_lite::{
+    future::{block_on, poll_once},
+    Future,
+};
 use std::{
-    sync::{Arc, RwLock},
+    sync::{Arc, LockResult, RwLock, RwLockReadGuard},
+    task::{Poll, Waker},
     time::{Duration, Instant},
 };
 
@@ -23,13 +28,11 @@ struct FrameCounter {
 // Run with release build: cargo run --release --example async_bench
 // Example output:
 // windows:
-// [handle_tasks]        n_frames executed: 64, avg fps: 11.2(target:120), duration: 5.704s
-// [handle_tasks_par]    n_frames executed: 121, avg fps: 20.8(target:120), duration: 5.805s
-// [handle_tasks_par_2]  n_frames executed: 81, avg fps: 14.0(target:120), duration: 5.769s
-// linux:
-// [handle_tasks]        n_frames executed: 215, avg fps: 16.2(target:120), duration: 13.307s
-// [handle_tasks_par]    n_frames executed: 332, avg fps: 26.2(target:120), duration: 12.675s
-// [handle_tasks_par_2]  n_frames executed: 252, avg fps: 22.1(target:120), duration: 11.389s
+// [no_poll_once]        n_frames executed: 238, avg fps: 39.3(target:120), duration: 6.048s
+// [noop_waker]          n_frames executed: 161, avg fps: 25.7(target:120), duration: 6.253s
+// [handle_tasks]        n_frames executed: 54, avg fps: 9.4(target:120), duration: 5.743s
+// [handle_tasks_par]    n_frames executed: 124, avg fps: 21.5(target:120), duration: 5.767s
+// [handle_tasks_par_2]  n_frames executed: 90, avg fps: 15.4(target:120), duration: 5.835s
 fn main() {
     App::new()
         .insert_resource(ScheduleRunnerSettings::run_loop(Duration::from_secs_f64(
@@ -40,6 +43,16 @@ fn main() {
         .add_startup_system(spawn_tasks_no_poll_once)
         .add_system_to_stage(CoreStage::First, count_frame)
         .add_system(handle_tasks_no_poll_once)
+        .run();
+    App::new()
+        .insert_resource(ScheduleRunnerSettings::run_loop(Duration::from_secs_f64(
+            FRAME_STEP,
+        )))
+        .insert_resource(FrameCounter { n_frames: 0 })
+        .add_plugins(MinimalPlugins)
+        .add_startup_system(spawn_tasks_noop_waker)
+        .add_system_to_stage(CoreStage::First, count_frame)
+        .add_system(handle_tasks_noop_waker)
         .run();
     for handle_tasks_system in [handle_tasks, handle_tasks_par, handle_tasks_par_2] {
         App::new()
@@ -171,42 +184,42 @@ fn print_statistics(name: &str, frame_counter: &Res<FrameCounter>, time: &Res<Ti
     );
 }
 
-#[derive(Clone)]
+#[derive(Debug)]
 struct TaskWrapper<T> {
-    pub result: Arc<RwLock<Option<T>>>,
+    result: Arc<RwLock<Option<T>>>,
+    _task: Task<()>,
 }
 
 impl<T> TaskWrapper<T> {
-    pub fn new() -> Self {
+    pub fn new(result: Arc<RwLock<Option<T>>>, task: Task<()>) -> Self {
         Self {
-            result: Arc::new(RwLock::new(None)),
+            result,
+            _task: task,
         }
     }
 
-    pub async fn register(&mut self, t: impl std::future::Future<Output = T>) {
-        let ret = t.await;
-        let mut lock = self.result.write().unwrap();
-        *lock = Some(ret);
+    pub fn poll(&self) -> LockResult<RwLockReadGuard<Option<T>>> {
+        self.result.read()
     }
 }
 
 fn spawn_tasks_no_poll_once(mut commands: Commands, thread_pool: Res<AsyncComputeTaskPool>) {
     for step in 0..N_STEPS {
         for _i in 0..N_TASKS {
-            let wrapper = TaskWrapper::<()>::new();
-            let mut wrapper_clone = wrapper.clone();
+            let result: Arc<RwLock<Option<()>>> = Arc::new(RwLock::new(None));
+            let result_clone = result.clone();
             let task = thread_pool.spawn(async move {
-                wrapper_clone
-                    .register(async move {
-                        let start_time = Instant::now();
-                        let duration = Duration::from_secs_f32(TASK_DURATION_SEC * (step as f32));
-                        while Instant::now() - start_time < duration {
-                            futures_timer::Delay::new(Duration::from_secs_f32(0.1)).await;
-                        }
-                    })
-                    .await;
+                let start_time = Instant::now();
+                let duration = Duration::from_secs_f32(TASK_DURATION_SEC * (step as f32));
+                while Instant::now() - start_time < duration {
+                    futures_timer::Delay::new(Duration::from_secs_f32(0.1)).await;
+                }
+                // println!("spawn_tasks_no_poll_once");
+                let mut locked = result_clone.write().unwrap();
+                *locked = Some(());
             });
-            commands.spawn().insert(wrapper.clone()).insert(task);
+            let wrapper = TaskWrapper::new(result, task);
+            commands.spawn().insert(wrapper);
         }
     }
 }
@@ -221,16 +234,75 @@ fn handle_tasks_no_poll_once(
     let mut n_tasks = 0;
     for (entity, task) in transform_tasks.iter() {
         n_tasks += 1;
-        let lock = task.result.read().unwrap();
-        if lock.is_some() {
-            commands
-                .entity(entity)
-                .remove::<TaskWrapper<()>>()
-                .remove::<Task<()>>();
+        let locked = task.poll().unwrap();
+        if locked.is_some() {
+            commands.entity(entity).remove::<TaskWrapper<()>>();
         }
     }
     if n_tasks == 0 {
-        print_statistics("handle_tasks_no_poll_once", &frame_counter, &time);
+        print_statistics("no_poll_once", &frame_counter, &time);
+        app_exit_events.send(AppExit);
+    }
+}
+
+lazy_static::lazy_static! {
+    static ref NOOP_WAKER: Waker = noop_waker::noop_waker();
+}
+
+#[derive(Debug)]
+struct TaskWrapper2<T> {
+    task: Task<T>,
+}
+
+impl<T: Clone> TaskWrapper2<T> {
+    pub fn new(task: Task<T>) -> Self {
+        Self { task }
+    }
+
+    pub fn poll(&mut self) -> Option<T> {
+        let f = &mut self.task;
+        futures_lite::pin!(f);
+        let mut noop_ctx = Context::from_waker(&NOOP_WAKER);
+        match f.poll(&mut noop_ctx) {
+            Poll::Ready(o) => Some(o.clone()),
+            Poll::Pending => None,
+        }
+    }
+}
+
+fn spawn_tasks_noop_waker(mut commands: Commands, thread_pool: Res<AsyncComputeTaskPool>) {
+    for step in 0..N_STEPS {
+        for _i in 0..N_TASKS {
+            let task = thread_pool.spawn(async move {
+                let start_time = Instant::now();
+                let duration = Duration::from_secs_f32(TASK_DURATION_SEC * (step as f32));
+                while Instant::now() - start_time < duration {
+                    futures_timer::Delay::new(Duration::from_secs_f32(0.1)).await;
+                }
+                // println!("spawn_tasks_noop_waker");
+            });
+            let wrapper = TaskWrapper2::<()>::new(task);
+            commands.spawn().insert(wrapper);
+        }
+    }
+}
+
+fn handle_tasks_noop_waker(
+    mut commands: Commands,
+    mut transform_tasks: Query<(Entity, &mut TaskWrapper2<()>)>,
+    mut app_exit_events: EventWriter<AppExit>,
+    time: Res<Time>,
+    frame_counter: Res<FrameCounter>,
+) {
+    let mut n_tasks = 0;
+    for (entity, mut task) in transform_tasks.iter_mut() {
+        n_tasks += 1;
+        if task.poll().is_some() {
+            commands.entity(entity).remove::<TaskWrapper2<()>>();
+        }
+    }
+    if n_tasks == 0 {
+        print_statistics("noop_waker", &frame_counter, &time);
         app_exit_events.send(AppExit);
     }
 }
