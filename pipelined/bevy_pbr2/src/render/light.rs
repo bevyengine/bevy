38d6e6341619e7805f94ac3ca91266e1af24f376
Robert Swain <robert.swain@gmail.com>
2021-08-25 11:20:13 +0200

    bevy_pbr2: Use a separate shader for the shadow pass

--- pipelined/bevy_pbr2/src/render/light.rs
+++ pipelined/bevy_pbr2/src/render/light.rs
@@ -14,12 +14,14 @@ use bevy_render2::{
     render_phase::{Draw, DrawFunctions, RenderPhase, TrackedRenderPass},
     render_resource::*,
     renderer::{RenderContext, RenderDevice},
+    shader::Shader,
     texture::*,
     view::{ExtractedView, ViewUniformOffset},
 };
 use bevy_transform::components::GlobalTransform;
 use crevice::std140::AsStd140;
 use std::num::NonZeroU32;
+use wgpu::ShaderModel;
 
 pub struct ExtractedAmbientLight {
     color: Color,
@@ -93,6 +95,7 @@ pub const DIRECTIONAL_SHADOW_LAYERS: u32 = MAX_DIRECTIONAL_LIGHTS as u32;
 pub const SHADOW_FORMAT: TextureFormat = TextureFormat::Depth32Float;
 
 pub struct ShadowShaders {
+    pub shader_module: ShaderModule,
     pub pipeline: RenderPipeline,
     pub view_layout: BindGroupLayout,
     pub point_light_sampler: Sampler,
@@ -104,6 +107,8 @@ impl FromWorld for ShadowShaders {
     fn from_world(world: &mut World) -> Self {
         let render_device = world.get_resource::<RenderDevice>().unwrap();
         let pbr_shaders = world.get_resource::<PbrShaders>().unwrap();
+        let shader = Shader::from_wgsl(include_str!("depth.wgsl"));
+        let shader_module = render_device.create_shader_module(&shader);
 
         let view_layout = render_device.create_bind_group_layout(&BindGroupLayoutDescriptor {
             entries: &[
@@ -157,7 +162,7 @@ impl FromWorld for ShadowShaders {
                         },
                     ],
                 }],
-                module: &pbr_shaders.shader_module,
+                module: &shader_module,
                 entry_point: "vertex",
             },
             fragment: None,
@@ -191,6 +196,7 @@ impl FromWorld for ShadowShaders {
         });
 
         ShadowShaders {
+            shader_module,
             pipeline,
             view_layout,
             point_light_sampler: render_device.create_sampler(&SamplerDescriptor {
