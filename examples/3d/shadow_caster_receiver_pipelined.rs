8716efd9129a7135bc7f0895c2f0bd3ced7be4c7
Carter Anderson <mcanders1@gmail.com>
2021-08-25 12:34:34 -0700

    Remove camera controls from example

--- examples/3d/shadow_caster_receiver_pipelined.rs
+++ examples/3d/shadow_caster_receiver_pipelined.rs
@@ -1,15 +1,14 @@
 use bevy::{
-    core::Time,
     ecs::prelude::*,
-    input::{mouse::MouseMotion, Input},
-    math::{EulerRot, Mat4, Quat, Vec2, Vec3},
+    input::Input,
+    math::{EulerRot, Mat4, Vec3},
     pbr2::{
         DirectionalLight, DirectionalLightBundle, NotShadowCaster, NotShadowReceiver, PbrBundle,
         PointLight, PointLightBundle, StandardMaterial,
     },
     prelude::{App, Assets, Handle, KeyCode, Transform},
     render2::{
-        camera::{Camera, OrthographicProjection, PerspectiveCameraBundle},
+        camera::{OrthographicProjection, PerspectiveCameraBundle},
         color::Color,
         mesh::{shape, Mesh},
     },
@@ -19,10 +18,6 @@ use bevy::{
 fn main() {
     println!(
         "Controls:
-    WSAD   - forward/back/strafe left/right
-    LShift - 'run'
-    E      - up
-    Q      - down
     C      - toggle shadow casters (i.e. casters become not, and not casters become casters)
     R      - toggle shadow receivers (i.e. receivers become not, and not receivers become receivers)
     L      - switch between directional and point lights"
@@ -32,7 +27,6 @@ fn main() {
         .add_startup_system(setup)
         .add_system(toggle_light)
         .add_system(toggle_shadows)
-        .add_system(camera_controller)
         .run();
 }
 
@@ -125,13 +119,11 @@ fn setup(
     });
 
     // camera
-    commands
-        .spawn_bundle(PerspectiveCameraBundle {
-            transform: Transform::from_xyz(-5.0, 5.0, 5.0)
-                .looking_at(Vec3::new(-1.0, 1.0, 0.0), Vec3::Y),
-            ..Default::default()
-        })
-        .insert(CameraController::default());
+    commands.spawn_bundle(PerspectiveCameraBundle {
+        transform: Transform::from_xyz(-5.0, 5.0, 5.0)
+            .looking_at(Vec3::new(-1.0, 1.0, 0.0), Vec3::Y),
+        ..Default::default()
+    });
 }
 
 fn toggle_light(
@@ -185,123 +177,6 @@ fn toggle_shadows(
         }
         for entity in queries.q3().iter() {
             commands.entity(entity).insert(NotShadowReceiver);
-        }
-    }
-}
-
-struct CameraController {
-    pub enabled: bool,
-    pub sensitivity: f32,
-    pub key_forward: KeyCode,
-    pub key_back: KeyCode,
-    pub key_left: KeyCode,
-    pub key_right: KeyCode,
-    pub key_up: KeyCode,
-    pub key_down: KeyCode,
-    pub key_run: KeyCode,
-    pub walk_speed: f32,
-    pub run_speed: f32,
-    pub friction: f32,
-    pub pitch: f32,
-    pub yaw: f32,
-    pub velocity: Vec3,
-}
-
-impl Default for CameraController {
-    fn default() -> Self {
-        Self {
-            enabled: true,
-            sensitivity: 0.5,
-            key_forward: KeyCode::W,
-            key_back: KeyCode::S,
-            key_left: KeyCode::A,
-            key_right: KeyCode::D,
-            key_up: KeyCode::E,
-            key_down: KeyCode::Q,
-            key_run: KeyCode::LShift,
-            walk_speed: 10.0,
-            run_speed: 30.0,
-            friction: 0.5,
-            pitch: 0.0,
-            yaw: 0.0,
-            velocity: Vec3::ZERO,
-        }
-    }
-}
-
-fn camera_controller(
-    time: Res<Time>,
-    mut mouse_events: EventReader<MouseMotion>,
-    key_input: Res<Input<KeyCode>>,
-    mut query: Query<(&mut Transform, &mut CameraController), With<Camera>>,
-) {
-    let dt = time.delta_seconds();
-
-    // Handle mouse input
-    let mut mouse_delta = Vec2::ZERO;
-    for mouse_event in mouse_events.iter() {
-        mouse_delta += mouse_event.delta;
-    }
-
-    for (mut transform, mut options) in query.iter_mut() {
-        if !options.enabled {
-            continue;
-        }
-
-        // Handle key input
-        let mut axis_input = Vec3::ZERO;
-        if key_input.pressed(options.key_forward) {
-            axis_input.z += 1.0;
-        }
-        if key_input.pressed(options.key_back) {
-            axis_input.z -= 1.0;
-        }
-        if key_input.pressed(options.key_right) {
-            axis_input.x += 1.0;
-        }
-        if key_input.pressed(options.key_left) {
-            axis_input.x -= 1.0;
-        }
-        if key_input.pressed(options.key_up) {
-            axis_input.y += 1.0;
-        }
-        if key_input.pressed(options.key_down) {
-            axis_input.y -= 1.0;
-        }
-
-        // Apply movement update
-        if axis_input != Vec3::ZERO {
-            let max_speed = if key_input.pressed(options.key_run) {
-                options.run_speed
-            } else {
-                options.walk_speed
-            };
-            options.velocity = axis_input.normalize() * max_speed;
-        } else {
-            let friction = options.friction.clamp(0.0, 1.0);
-            options.velocity *= 1.0 - friction;
-            if options.velocity.length_squared() < 1e-6 {
-                options.velocity = Vec3::ZERO;
-            }
-        }
-        let forward = transform.forward();
-        let right = transform.right();
-        transform.translation += options.velocity.x * dt * right
-            + options.velocity.y * dt * Vec3::Y
-            + options.velocity.z * dt * forward;
-
-        if mouse_delta != Vec2::ZERO {
-            // Apply look update
-            let (pitch, yaw) = (
-                (options.pitch - mouse_delta.y * 0.5 * options.sensitivity * dt).clamp(
-                    -0.99 * std::f32::consts::FRAC_PI_2,
-                    0.99 * std::f32::consts::FRAC_PI_2,
-                ),
-                options.yaw - mouse_delta.x * options.sensitivity * dt,
-            );
-            transform.rotation = Quat::from_euler(EulerRot::ZYX, 0.0, yaw, pitch);
-            options.pitch = pitch;
-            options.yaw = yaw;
         }
     }
 }
