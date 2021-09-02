3383bf07edb5dc013e08cb6cd3da8ef577077658
Afonso Lage <lage.afonso@gmail.com>
2021-09-02 19:35:11 -0300

    Explain which components/resources the systems are conflicting on.

--- crates/bevy_ecs/src/schedule/stage.rs
+++ crates/bevy_ecs/src/schedule/stage.rs
@@ -506,7 +506,7 @@ impl SystemStage {
             for (idx, (index_a, index_b, conflicts)) in ambiguities.drain(..).enumerate() {
                 writeln!(
                     string,
-                    "{}.{} - {:?} and {:?}",
+                    "{}.{} - {:?} and {:?} conflicts on the following components/resources:",
                     output_prefix,
                     idx,
                     systems[index_a].name(),
@@ -518,7 +518,7 @@ impl SystemStage {
                         .iter()
                         .map(|id| world.components().get_info(*id).unwrap().name())
                         .collect::<Vec<_>>();
-                    writeln!(string, "    conflicts: {:?}", names).unwrap();
+                    writeln!(string, "{:?}", names).unwrap();
                 }
             }
         }
