d678368b368f24c55ab9be416c928cf00c6d9272
Ïvar Källström <ivar.kallstrom@gmail.com>
2021-09-02 23:05:25 +0200

    added paramset to prelude

--- crates/bevy_ecs/src/lib.rs
+++ crates/bevy_ecs/src/lib.rs
@@ -32,7 +32,8 @@ pub mod prelude {
         },
         system::{
             Commands, ConfigurableSystem, In, IntoChainSystem, IntoExclusiveSystem, IntoSystem,
-            Local, NonSend, NonSendMut, Query, QuerySet, ParamSet, RemovedComponents, Res, ResMut, System,
+            Local, NonSend, NonSendMut, ParamSet, Query, QuerySet, RemovedComponents, Res, ResMut,
+            System,
         },
         world::{FromWorld, Mut, World},
     };
