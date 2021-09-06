639c82de0ea41d0598801525c3fd237558bad9ad
Bytebuddha <patrickisgreene@protonmail.com>
2021-09-06 13:58:54 -0500

    Run rustfmt

--- crates/bevy_diagnostic/src/diagnostic.rs
+++ crates/bevy_diagnostic/src/diagnostic.rs
@@ -120,11 +120,11 @@ impl Diagnostic {
         self.max_history_length
     }
 
-    pub fn values(&self) -> impl Iterator<Item=&f64> {
-        self.history.iter().map(|x|&x.value)
+    pub fn values(&self) -> impl Iterator<Item = &f64> {
+        self.history.iter().map(|x| &x.value)
     }
 
-    pub fn measurements(&self) -> impl Iterator<Item=&DiagnosticMeasurement> {
+    pub fn measurements(&self) -> impl Iterator<Item = &DiagnosticMeasurement> {
         self.history.iter()
     }
 }
