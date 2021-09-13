cd63208ba3b2cab90f28fdd8c5e3eab58726583b
B-Janson <brandon@canva.com>
2021-09-13 21:28:11 +1000

    Apply suggestions from code review
    Co-authored-by: Alice Cecile <alice.i.cecile@gmail.com>

--- examples/ecs/generic_system.rs
+++ examples/ecs/generic_system.rs
@@ -20,6 +20,7 @@ fn main() {
         .add_system_set(SystemSet::on_update(AppState::MainMenu).with_system(transition_to_in_game))
         // add the cleanup systems
         .add_system_set(
+            // Pass in the types your system should operate on using the ::<T> (turbofish) syntax
             SystemSet::on_exit(AppState::MainMenu).with_system(cleanup_system::<MenuClose>),
         )
         .add_system_set(
@@ -58,6 +59,8 @@ fn transition_to_in_game(mut state: ResMut<State<AppState>>, keyboard_input: Res
     }
 }
 
+// Type arguments on functions come after the function name, but before ordinary arguments.
+// Here, the `Component` trait is a trait bound on T, our generic type
 fn cleanup_system<T: Component>(mut commands: Commands, query: Query<Entity, With<T>>) {
     for e in query.iter() {
         commands.entity(e).despawn_recursive();
