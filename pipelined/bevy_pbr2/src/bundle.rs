e25b1da84c16df5d99b48fd16c230976ef97ef07
Robert Swain <robert.swain@gmail.com>
2021-09-14 20:31:58 +0200

    Add CubeFrustaVisibleEntities for use with cube maps

--- pipelined/bevy_pbr2/src/bundle.rs
+++ pipelined/bevy_pbr2/src/bundle.rs
@@ -32,6 +32,29 @@ impl Default for PbrBundle {
     }
 }
 
+#[derive(Clone, Debug, Default)]
+pub struct CubeFrustaVisibleEntities {
+    cube_frusta_visible_entities: [VisibleEntities; 6],
+}
+
+impl CubeFrustaVisibleEntities {
+    pub fn get(&self, i: usize) -> &VisibleEntities {
+        &self.cube_frusta_visible_entities[i]
+    }
+
+    pub fn get_mut(&mut self, i: usize) -> &mut VisibleEntities {
+        &mut self.cube_frusta_visible_entities[i]
+    }
+
+    pub fn iter(&self) -> impl DoubleEndedIterator<Item = &VisibleEntities> {
+        self.cube_frusta_visible_entities.iter()
+    }
+
+    pub fn iter_mut(&mut self) -> impl DoubleEndedIterator<Item = &mut VisibleEntities> {
+        self.cube_frusta_visible_entities.iter_mut()
+    }
+}
+
 /// A component bundle for "point light" entities
 #[derive(Debug, Bundle, Default)]
 pub struct PointLightBundle {
