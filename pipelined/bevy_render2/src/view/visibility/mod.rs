087fd7bcd0dbcb35ca1e492cf68d24eae941fb9e
Robert Swain <robert.swain@gmail.com>
2021-09-14 18:39:56 +0200

    Add visibility types and systems

--- pipelined/bevy_render2/src/view/visibility/mod.rs
+++ pipelined/bevy_render2/src/view/visibility/mod.rs
@@ -1,3 +1,184 @@
 mod render_layers;
 
 pub use render_layers::*;
+
+use bevy_app::{CoreStage, Plugin};
+use bevy_asset::{Assets, Handle};
+use bevy_ecs::prelude::*;
+use bevy_reflect::Reflect;
+use bevy_transform::{components::GlobalTransform, TransformSystem};
+
+use crate::{
+    camera::{CameraProjection, OrthographicProjection, PerspectiveProjection},
+    mesh::Mesh,
+    primitives::{Aabb, Frustum},
+};
+
+/// User indication of whether an entity is visible
+#[derive(Clone)]
+pub struct Visibility {
+    pub is_visible: bool,
+}
+
+impl Default for Visibility {
+    fn default() -> Self {
+        Self { is_visible: true }
+    }
+}
+
+/// Algorithmically-computed indication of whether an entity is visible and should be extracted for rendering
+#[derive(Clone)]
+pub struct ComputedVisibility {
+    pub is_visible: bool,
+}
+
+impl Default for ComputedVisibility {
+    fn default() -> Self {
+        Self { is_visible: true }
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct VisibleEntity {
+    pub entity: Entity,
+}
+
+#[derive(Clone, Default, Debug, Reflect)]
+#[reflect(Component)]
+pub struct VisibleEntities {
+    #[reflect(ignore)]
+    pub entities: Vec<VisibleEntity>,
+}
+
+impl VisibleEntities {
+    pub fn iter(&self) -> impl DoubleEndedIterator<Item = &VisibleEntity> {
+        self.entities.iter()
+    }
+}
+
+#[derive(Debug, Hash, PartialEq, Eq, Clone, SystemLabel)]
+pub enum VisibilitySystems {
+    CalculateBounds,
+    UpdateOrthographicFrusta,
+    UpdatePerspectiveFrusta,
+    CheckVisibility,
+}
+
+pub struct VisibilityPlugin;
+
+impl Plugin for VisibilityPlugin {
+    fn build(&self, app: &mut bevy_app::App) {
+        use VisibilitySystems::*;
+
+        app.add_system_to_stage(
+            CoreStage::PostUpdate,
+            calculate_bounds.label(CalculateBounds),
+        )
+        .add_system_to_stage(
+            CoreStage::PostUpdate,
+            update_frusta::<OrthographicProjection>
+                .label(UpdateOrthographicFrusta)
+                .after(TransformSystem::TransformPropagate),
+        )
+        .add_system_to_stage(
+            CoreStage::PostUpdate,
+            update_frusta::<PerspectiveProjection>
+                .label(UpdatePerspectiveFrusta)
+                .after(TransformSystem::TransformPropagate),
+        )
+        .add_system_to_stage(
+            CoreStage::PostUpdate,
+            check_visibility
+                .label(CheckVisibility)
+                .after(CalculateBounds)
+                .after(UpdateOrthographicFrusta)
+                .after(UpdatePerspectiveFrusta)
+                .after(TransformSystem::TransformPropagate),
+        );
+    }
+}
+
+pub fn calculate_bounds(
+    mut commands: Commands,
+    meshes: Res<Assets<Mesh>>,
+    without_aabb: Query<(Entity, &Handle<Mesh>), Without<Aabb>>,
+) {
+    for (entity, mesh_handle) in without_aabb.iter() {
+        if let Some(mesh) = meshes.get(mesh_handle) {
+            if let Some(aabb) = mesh.compute_aabb() {
+                commands.entity(entity).insert(aabb);
+            }
+        }
+    }
+}
+
+pub fn update_frusta<T: CameraProjection + Send + Sync + 'static>(
+    mut views: Query<(&GlobalTransform, &T, &mut Frustum)>,
+) {
+    for (transform, projection, mut frustum) in views.iter_mut() {
+        let view_projection =
+            projection.get_projection_matrix() * transform.compute_matrix().inverse();
+        *frustum = Frustum::from_view_projection(
+            &view_projection,
+            &transform.translation,
+            &transform.back(),
+            projection.far(),
+        );
+    }
+}
+
+pub fn check_visibility(
+    mut view_query: Query<(
+        &mut VisibleEntities,
+        Option<&RenderLayers>,
+        Option<&Frustum>,
+    )>,
+    mut visible_entity_query: Query<(
+        Entity,
+        &Visibility,
+        &mut ComputedVisibility,
+        Option<&RenderLayers>,
+    )>,
+    bounded_entity_query: Query<(&Aabb, &GlobalTransform)>,
+) {
+    let mut first_view = true;
+    for (mut visible_entities, maybe_view_mask, maybe_frustum) in view_query.iter_mut() {
+        visible_entities.entities.clear();
+        let view_mask = maybe_view_mask.copied().unwrap_or_default();
+
+        for (entity, visibility, mut computed_visibility, maybe_entity_mask) in
+            visible_entity_query.iter_mut()
+        {
+            // Initialize the computed visibility to false for the first view, then it is inherited for subsequent views
+            if first_view {
+                computed_visibility.is_visible = false;
+            }
+
+            if !visibility.is_visible {
+                continue;
+            }
+
+            let entity_mask = maybe_entity_mask.copied().unwrap_or_default();
+            if !view_mask.intersects(&entity_mask) {
+                continue;
+            }
+
+            // If we have an aabb, transform, and frustum, do frustum culling
+            if let Ok((aabb, transform)) = bounded_entity_query.get(entity) {
+                if let Some(frustum) = maybe_frustum {
+                    if !frustum.intersects_obb(aabb, &transform.compute_matrix()) {
+                        continue;
+                    }
+                }
+            }
+
+            computed_visibility.is_visible = true;
+            visible_entities.entities.push(VisibleEntity { entity });
+        }
+
+        // TODO: check for big changes in visible entities len() vs capacity() (ex: 2x) and resize
+        // to prevent holding unneeded memory
+
+        first_view = false;
+    }
+}
