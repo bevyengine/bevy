132ce8f051ebfb92bca9c77fc6a191d4362f9cb3
Mirko Rainer <mirkorainer@outlook.com>
2021-11-22 06:31:40 -0500

    Regex match titles of error docs. iterate them and print. Works so far!

--- tools/ci/src/main.rs
+++ tools/ci/src/main.rs
@@ -1,10 +1,29 @@
-use xshell::{cmd, pushd};
+use std::path::Path;
+
+use regex::Regex;
+use xshell::{cmd, pushd, read_dir};
 
 fn main() {
     // When run locally, results may differ from actual CI runs triggered by
     // .github/workflows/ci.yml
     // - Official CI runs latest stable
     // - Local runs use whatever the default Rust is locally
+
+    // Iterate through error codes and add them to the table of contents in the README.md
+    // This Regex matches a 'B' followed by four digits with a '.md' file extension
+    let error_doc_title_pattern =
+        Regex::new(r"B\d\d\d\d\.md").expect("Failed to create Regex, check the syntax.");
+    let path = Path::new("../../errors"); // Traverse up and into the errors directory
+    let errors_docs_dir = read_dir(path).expect("Failed to read errors docs dir.");
+    errors_docs_dir.iter().for_each(|doc_path| {
+        let file_name = doc_path
+            .file_name()
+            .expect("Failed to parse file name from doc_path");
+        for regex_capture in error_doc_title_pattern.captures_iter(file_name.to_str().unwrap()) {
+            // We should only have one capture
+            println!("{}", &regex_capture[0]);
+        }
+    });
 
     // See if any code needs to be formatted
     cmd!("cargo fmt --all -- --check")
