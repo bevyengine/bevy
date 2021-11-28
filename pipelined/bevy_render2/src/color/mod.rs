3de391be21666c90661066ca395c63813352b4bb
François <mockersf@gmail.com>
2021-11-28 10:40:42 +0000

    fix calls to as_rgba_linear (#3200)
    # Objective
    
    - After #3192, some places where `as_rgba_linear` was called were doing too many conversions
    
    ## Solution
    
    - Fix the conversions

--- pipelined/bevy_render2/src/color/mod.rs
+++ pipelined/bevy_render2/src/color/mod.rs
@@ -633,12 +633,21 @@ impl From<Vec4> for Color {
 
 impl From<Color> for wgpu::Color {
     fn from(color: Color) -> Self {
-        let color = color.as_rgba_linear();
-        wgpu::Color {
-            r: color.r() as f64,
-            g: color.g() as f64,
-            b: color.b() as f64,
-            a: color.a() as f64,
+        if let Color::RgbaLinear {
+            red,
+            green,
+            blue,
+            alpha,
+        } = color.as_rgba_linear()
+        {
+            wgpu::Color {
+                r: red as f64,
+                g: green as f64,
+                b: blue as f64,
+                a: alpha as f64,
+            }
+        } else {
+            unreachable!()
         }
     }
 }
