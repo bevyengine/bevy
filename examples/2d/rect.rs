05216f08aa7217e6ca0f01b73429449409e91d75
r00ster <r00ster91@protonmail.com>
2021-12-16 17:30:31 +0100

    Use `custom_size`

--- examples/2d/rect.rs
+++ examples/2d/rect.rs
@@ -10,12 +10,9 @@ fn main() {
 fn setup(mut commands: Commands, mut materials: ResMut<Assets<ColorMaterial>>) {
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
     commands.spawn_bundle(SpriteBundle {
-        transform: Transform {
-            scale: Vec3::new(50.0, 50.0, 0.0),
-            ..Default::default()
-        },
         sprite: Sprite {
             color: Color::rgb(0.25, 0.25, 0.75),
+            custom_size: Some(Vec2::new(50.0, 50.0)), 
             ..Default::default()
         },
     });
