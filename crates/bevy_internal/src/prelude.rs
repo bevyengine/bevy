85afcacf8f0aa86e1541135fb63db3c6838e9d30
TheRawMeatball <therawmeatball@gmail.com>
2021-12-26 10:53:02 +0300

    remove excess

--- crates/bevy_internal/src/prelude.rs
+++ crates/bevy_internal/src/prelude.rs
@@ -1,8 +1,8 @@
 #[doc(hidden)]
 pub use crate::{
     app::prelude::*, asset::prelude::*, core::prelude::*, ecs::prelude::*, log::prelude::*,
-    math::prelude::*, render::prelude::Color, render::prelude::*, transform::prelude::*,
-    window::prelude::*, DefaultPlugins, MinimalPlugins,
+    math::prelude::*, render::prelude::*, transform::prelude::*, window::prelude::*,
+    DefaultPlugins, MinimalPlugins,
 };
 
 pub use bevy_derive::bevy_main;
