959a845704426d2f967c3898239e552b9d6bf9c9
davier <bricedavier@gmail.com>
2021-12-27 19:46:25 +0000

    bevy_ui: register Overflow type (#3443)
    I forgot to register the new `Overflow` type in #3296.

--- crates/bevy_ui/src/lib.rs
+++ crates/bevy_ui/src/lib.rs
@@ -55,6 +55,7 @@ impl Plugin for UiPlugin {
             .register_type::<Node>()
             // NOTE: used by Style::aspect_ratio
             .register_type::<Option<f32>>()
+            .register_type::<Overflow>()
             .register_type::<PositionType>()
             .register_type::<Size<f32>>()
             .register_type::<Size<Val>>()
