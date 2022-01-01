7078e51f694bd62a531b183461358eab6ff59491
kdecay <kdecaymusic@protonmail.com>
2022-01-01 20:44:56 +0100

    Updated the bevy_dylib doc and added doc warning.

--- crates/bevy_dylib/src/lib.rs
+++ crates/bevy_dylib/src/lib.rs
@@ -1,12 +1,26 @@
+#![warn(missing_docs)]
 #![allow(clippy::single_component_path_imports)]
 
 //! Forces dynamic linking of Bevy.
 //!
 //! Dynamically linking Bevy makes the "link" step much faster. This can be achieved by adding
-//! `bevy_dylib` as dependency and `#[allow(unused_imports)] use bevy_dylib` to `main.rs`. It is
-//! recommended to disable the `bevy_dylib` dependency in release mode by adding
-//! `#[cfg(debug_assertions)]` to the `use` statement. Otherwise you will have to ship `libstd.so`
-//! and `libbevy_dylib.so` with your game.
+//! `bevy_dylib` as a dependency and adding the following code to the `main.rs` file:
+//!
+//! ```rust
+//! #[allow(unused_imports)]
+//! use bevy_dylib;
+//! ```
+//!
+//! It is recommended to disable the `bevy_dylib` dependency in release mode by adding the
+//! following code to the `use` statement:
+//!
+//! ```rust
+//! #[allow(unused_imports)]
+//! #[cfg(debug_assertions)] // new
+//! use bevy_dylib;
+//! ```
+//!
+//! If you don't do this you will have to ship `libstd.so` and `libbevy_dylib.so` with your game.
 
 // Force linking of the main bevy crate
 #[allow(unused_imports)]
