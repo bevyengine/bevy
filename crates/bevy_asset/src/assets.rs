d9cc1883c9255caabc011f53f6f33c0ae27c7d3a
François <8672791+mockersf@users.noreply.github.com>
2022-01-05 20:52:45 +0100

    check if resource for asset already exists before adding it

--- crates/bevy_asset/src/assets.rs
+++ crates/bevy_asset/src/assets.rs
@@ -274,16 +274,20 @@ impl AddAsset for App {
     where
         T: Asset,
     {
-        let assets = {
-            let asset_server = self.world.get_resource::<AssetServer>().unwrap();
-            asset_server.register_asset_type::<T>()
-        };
+        if !self.world.contains_resource::<Assets<T>>() {
+            let assets = {
+                let asset_server = self.world.get_resource::<AssetServer>().unwrap();
+                asset_server.register_asset_type::<T>()
+            };
 
-        self.insert_resource(assets)
-            .add_system_to_stage(AssetStage::AssetEvents, Assets::<T>::asset_event_system)
-            .add_system_to_stage(AssetStage::LoadAssets, update_asset_storage_system::<T>)
-            .register_type::<Handle<T>>()
-            .add_event::<AssetEvent<T>>()
+            self.insert_resource(assets)
+                .add_system_to_stage(AssetStage::AssetEvents, Assets::<T>::asset_event_system)
+                .add_system_to_stage(AssetStage::LoadAssets, update_asset_storage_system::<T>)
+                .register_type::<Handle<T>>()
+                .add_event::<AssetEvent<T>>()
+        } else {
+            self
+        }
     }
 
     fn init_asset_loader<T>(&mut self) -> &mut Self
