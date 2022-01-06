b673c51e2061e333ed6c37763f54f8e536238cd2
Daniel Bearden <danbear0@gmail.com>
2022-01-06 23:16:47 +0000

    Bevy app docs (#3539)
    # Objective
    
    Achieve 100% documentation coverage for bevy_app crate.
    See #3492 
    
    ## Solution
    
    - Add #![warn(missing_docs)] to crate root
    - Add doc comments to public items
    - Add doc comment to bevy_utils::define_label macro trait

--- crates/bevy_app/src/plugin.rs
+++ crates/bevy_app/src/plugin.rs
@@ -6,10 +6,14 @@ use std::any::Any;
 /// Plugins configure an [`App`](crate::App). When an [`App`](crate::App) registers
 /// a plugin, the plugin's [`Plugin::build`] function is run.
 pub trait Plugin: Any + Send + Sync {
+    /// Configures the [`App`] to which this plugin is added.
     fn build(&self, app: &mut App);
+    /// Configures a name for the [`Plugin`]. Primarily for debugging.
     fn name(&self) -> &str {
         std::any::type_name::<Self>()
     }
 }
 
+/// Type representing an unsafe function that returns a mutable pointer to a [`Plugin`].
+/// Used for dynamically loading plugins. See bevy_dynamic_plugin/src/loader.rs#dynamically_load_plugin
 pub type CreatePlugin = unsafe fn() -> *mut dyn Plugin;
