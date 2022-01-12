73ab0641d45a02a01328a71035312f0d189c80db
Daniel Wiesenberg <weasy@hotmail.de>
2022-01-12 20:47:18 +0100

    Move asset_listener and State into Plugin and remove loading text manipulation, because `loading_screen` query does not work because of system ordering.

--- examples/asset/loading_screen.rs
+++ examples/asset/loading_screen.rs
@@ -1,68 +1,22 @@
-use bevy::{gltf::Gltf, prelude::*};
+use bevy::prelude::*;
 
 /// This example illustrates, how a loading screen can be implemented. It has an animated spinner and listens for an `AssetEvent`.
-
-#[derive(Clone, Eq, PartialEq, Debug, Hash)]
-enum GameState {
-    Loading,
-    Playing,
-}
 
 fn main() {
     App::new()
         .insert_resource(Msaa { samples: 4 })
         .add_plugins(DefaultPlugins)
         // Loading screen plugin and its `asset_listening_system`
-        .add_plugin(plugin::SimpleLoadingScreenPlugin {
-            loading_state: GameState::Loading,
-        })
-        .add_system_set(
-            SystemSet::on_update(GameState::Loading).with_system(asset_listening_system),
-        )
+        .add_plugin(plugin::SimpleLoadingScreenPlugin)
         // system that sets up everything for our game
         .add_startup_system(setup_game)
         // system that will be run after everything is set up
-        .add_system_set(SystemSet::on_enter(GameState::Playing).with_system(game_system))
+        .add_system_set(SystemSet::on_enter(plugin::LoadingState::Done).with_system(game_system))
         .run();
 }
 
-// System that listens for our `AssetEvent` and changes our game state after we finished loading our asset
-fn asset_listening_system(
-    // if you have different asset, either use another `EventReader` for your particular type of asset,
-    // or use `AssetServer::get_load_state` with a resource in which you track the handles of your assets.
-    mut asset_events: EventReader<AssetEvent<Gltf>>,
-    mut state: ResMut<State<GameState>>,
-) {
-    for event in asset_events.iter() {
-        match event {
-            AssetEvent::Created { handle: _ } => {
-                info!("Done loading, switch state to `GameState::Playing`");
-                // in this case, we know that we only had one asset (FlightHelmet.gltf#Scene0) to load,
-                // so we can switch our game state from `GameState::Loading` over to `GameState::Playing`
-                let _ = state.overwrite_set(GameState::Playing);
-            }
-            // we don't care about these events in our example
-            AssetEvent::Modified { handle: _ } => {}
-            AssetEvent::Removed { handle: _ } => {}
-        }
-    }
-}
-
 // Here we prepare our gaming scene, meaning we setup our ingame camera and load our asset.
-fn setup_game(
-    mut commands: Commands,
-    asset_server: Res<AssetServer>,
-    loading_screen: Query<&Children, With<plugin::LoadingScreen>>,
-    mut text_query: Query<&mut Text, With<plugin::LoadingScreenContent>>,
-) {
-    // Change our default loading screen text to something more descriptive
-    let children = loading_screen.single();
-    if let Some(child) = children.first() {
-        if let Ok(mut loading_text) = text_query.get_mut(*child) {
-            loading_text.sections[0].value = "Loading \"FlightHelmet\"".to_string();
-        }
-    }
-
+fn setup_game(mut commands: Commands, asset_server: Res<AssetServer>) {
     // actually load our asset
     commands.spawn_scene(asset_server.load("models/FlightHelmet/FlightHelmet.gltf#Scene0"));
     // spawn our camera
@@ -99,25 +53,28 @@ fn game_system(_commands: Commands, _asset_server: Res<AssetServer>) {
 // You can use this as a template for your own project/game. Don't forget, that you also need a system,
 // like `asset_listening_system`, to track your loading progress and to transition into your playing state.
 pub mod plugin {
-    use bevy::{
-        ecs::schedule::{StateData, SystemSet},
-        prelude::*,
-    };
+    use bevy::{ecs::schedule::SystemSet, gltf::Gltf, prelude::*};
 
-    pub struct SimpleLoadingScreenPlugin<S: StateData> {
-        pub loading_state: S,
+    #[derive(Clone, Eq, PartialEq, Debug, Hash)]
+    pub enum LoadingState {
+        Loading,
+        Done,
     }
 
-    impl<S: StateData> Plugin for SimpleLoadingScreenPlugin<S> {
+    pub struct SimpleLoadingScreenPlugin;
+
+    impl Plugin for SimpleLoadingScreenPlugin {
         fn build(&self, app: &mut App) {
-            app.add_state(self.loading_state.clone())
+            app.add_state(LoadingState::Loading)
                 .add_startup_system(setup_loading_screen)
                 .add_system_set(
-                    SystemSet::on_update(self.loading_state.clone()).with_system(animate_spinner),
+                    SystemSet::on_update(LoadingState::Loading).with_system(animate_spinner),
                 )
                 .add_system_set(
-                    SystemSet::on_exit(self.loading_state.clone())
-                        .with_system(close_loading_screen),
+                    SystemSet::on_update(LoadingState::Loading).with_system(asset_listening_system),
+                )
+                .add_system_set(
+                    SystemSet::on_exit(LoadingState::Loading).with_system(close_loading_screen),
                 );
         }
     }
@@ -144,7 +101,7 @@ pub mod plugin {
                     flex_direction: FlexDirection::ColumnReverse,
                     ..Default::default()
                 },
-                color: Color::rgba(0.1, 0.1, 0.1, 1.0).into(),
+                color: Color::rgba(0.14, 0.14, 0.15, 1.0).into(),
                 ..Default::default()
             })
             // add the stuff we want to see on the loading screen as children
@@ -195,6 +152,29 @@ pub mod plugin {
             .insert(LoadingScreen);
     }
 
+    // System that listens for our `AssetEvent` and changes our game state after we finished loading our asset
+    // INFO: you need to change this if you have different assets 🙂
+    fn asset_listening_system(
+        // if you have different assets, either use another `EventReader` for your particular type of asset,
+        // or use `AssetServer::get_load_state` with a resource in which you track the handles of your assets.
+        mut asset_events: EventReader<AssetEvent<Gltf>>,
+        mut state: ResMut<State<LoadingState>>,
+    ) {
+        for event in asset_events.iter() {
+            match event {
+                AssetEvent::Created { handle: _ } => {
+                    info!("Done loading, switch state to `LoadingState::Done`");
+                    // in this case, we know that we only had one asset (FlightHelmet.gltf#Scene0) to load,
+                    // so we can switch our game state from `LoadingState::Loading` over to `LoadingState::Done`
+                    let _ = state.overwrite_set(LoadingState::Done);
+                }
+                // we don't care about these events in our example
+                AssetEvent::Modified { handle: _ } => {}
+                AssetEvent::Removed { handle: _ } => {}
+            }
+        }
+    }
+
     // Animates aka rotates the bevy bird `UiIamge` clockwise.
     fn animate_spinner(
         time: Res<Time>,
@@ -215,10 +195,11 @@ pub mod plugin {
         }
     }
 
-    // Close our loading screen. This system is called when we exit `GameState::Loading`.
+    // Close our loading screen. This system is called when we exit `LoadingState::Loading`.
     fn close_loading_screen(
         mut loading_screen: Query<(&mut Style, &Children), With<LoadingScreen>>,
         mut text_query: Query<&mut Text, With<LoadingScreenContent>>,
+        mut images_query: Query<&mut Transform, (With<UiImage>, With<LoadingScreenContent>)>,
     ) {
         // We only have one loading screen, so we can use `single_mut()` instead of `iter_mut()`
         let (mut style, children) = loading_screen.single_mut();
@@ -228,6 +209,11 @@ pub mod plugin {
             // Reset the text to our default text
             if let Ok(mut loading_text) = text_query.get_mut(*child) {
                 loading_text.sections[0].value = LOADING_SCREEN_DEFAULT_TEXT.to_string();
+            }
+            // Actually get the `Transform` of our child from all `UiImage`s
+            if let Ok(mut transform) = images_query.get_mut(*child) {
+                // Reset to the default rotation
+                transform.rotation = Quat::default();
             }
         }
     }
