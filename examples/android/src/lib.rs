dd9cc437466f829d8c1835733e765d3de752fe7a
Igor Shaposhnik <ishaposhnik@icloud.com>
2022-01-12 14:18:51 +0300

    Move android example into its own crate

---
+++ examples/android/src/lib.rs
@@ -0,0 +1,57 @@
+use bevy::{
+    prelude::*,
+    render::options::{Backends, WgpuOptions},
+    window::WindowMode,
+};
+
+// the `bevy_main` proc_macro generates the required ios boilerplate
+#[bevy_main]
+fn main() {
+    App::new()
+        .insert_resource(WindowDescriptor {
+            vsync: true,
+            resizable: false,
+            mode: WindowMode::BorderlessFullscreen,
+            ..Default::default()
+        })
+        .insert_resource(WgpuOptions {
+            backends: Some(Backends::all()),
+            ..Default::default()
+        })
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(setup)
+        .add_system(animate_sprite_system)
+        .run();
+}
+
+fn animate_sprite_system(
+    time: Res<Time>,
+    texture_atlases: Res<Assets<TextureAtlas>>,
+    mut query: Query<(&mut Timer, &mut TextureAtlasSprite, &Handle<TextureAtlas>)>,
+) {
+    for (mut timer, mut sprite, texture_atlas_handle) in query.iter_mut() {
+        timer.tick(time.delta());
+        if timer.finished() {
+            let texture_atlas = texture_atlases.get(texture_atlas_handle).unwrap();
+            sprite.index = (sprite.index + 1) % texture_atlas.textures.len();
+        }
+    }
+}
+
+fn setup(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut texture_atlases: ResMut<Assets<TextureAtlas>>,
+) {
+    let texture_handle = asset_server.load("textures/rpg/chars/gabe/gabe-idle-run.png");
+    let texture_atlas = TextureAtlas::from_grid(texture_handle, Vec2::new(24.0, 24.0), 7, 1);
+    let texture_atlas_handle = texture_atlases.add(texture_atlas);
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+    commands
+        .spawn_bundle(SpriteSheetBundle {
+            texture_atlas: texture_atlas_handle,
+            transform: Transform::from_scale(Vec3::splat(6.0)),
+            ..Default::default()
+        })
+        .insert(Timer::from_seconds(0.1, true));
+}
