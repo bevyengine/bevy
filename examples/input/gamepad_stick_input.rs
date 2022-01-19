fe53252f988afe46a9c7ad3a1997db36c8340cc2
Michael Dorst <m@mdorst.net>
2022-01-18 22:34:52 -0800

    Add coordinate text to `gamepad_stick_input` example

--- examples/input/gamepad_stick_input.rs
+++ examples/input/gamepad_stick_input.rs
@@ -1,10 +1,15 @@
 use bevy::prelude::*;
 
 const WINDOW_SIZE: f32 = 300.0;
-const CROSSHAIR_SIZE: f32 = 32.0;
+const CROSSHAIR_SIZE: f32 = 24.0;
+const FONT: &str = "fonts/FiraMono-Medium.ttf";
+const FONT_SIZE: f32 = 18.0;
 
 #[derive(Component)]
 struct Crosshair;
+
+#[derive(Component)]
+struct CoordinateText;
 
 fn main() {
     App::new()
@@ -17,30 +22,35 @@ fn main() {
         .insert_resource(ClearColor(Color::GRAY))
         .add_plugins(DefaultPlugins)
         .add_startup_system(setup)
-        .add_system(move_crosshair)
+        .add_system(update_position)
         .run();
 }
 
-fn move_crosshair(
-    mut query: Query<&mut Transform, With<Crosshair>>,
+fn update_position(
+    mut xform_query: Query<&mut Transform, With<Crosshair>>,
+    mut text_query: Query<&mut Text, With<CoordinateText>>,
     gamepads: Res<Gamepads>,
     axes: Res<Axis<GamepadAxis>>,
 ) {
-    let mut xform = query.single_mut();
+    let mut xform = xform_query.single_mut();
+    let mut text = text_query.single_mut();
     for gamepad in gamepads.iter() {
-        let left_stick_x = axes
+        // We only use input from the left stick.
+        let x = axes
             .get(GamepadAxis(*gamepad, GamepadAxisType::LeftStickX))
             .unwrap();
-        let left_stick_y = axes
+        let y = axes
             .get(GamepadAxis(*gamepad, GamepadAxisType::LeftStickY))
             .unwrap();
-        xform.translation.x = left_stick_x * WINDOW_SIZE / 2.0;
-        xform.translation.y = left_stick_y * WINDOW_SIZE / 2.0;
+        xform.translation.x = x * WINDOW_SIZE / 2.0;
+        xform.translation.y = y * WINDOW_SIZE / 2.0;
+        text.sections[0].value = format!("({:6.3}, {:6.3})", x, y);
     }
 }
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
-    // Spawn camera
+    // Spawn cameras
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
 
     // Spawn crosshair
@@ -55,4 +65,23 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
             ..Default::default()
         })
         .insert(Crosshair);
+
+    // Spawn text
+    let font = asset_server.load(FONT);
+    let text_style = TextStyle {
+        font,
+        font_size: FONT_SIZE,
+        color: Color::BLACK,
+    };
+    let text_alignment = TextAlignment {
+        vertical: VerticalAlign::Bottom,
+        horizontal: HorizontalAlign::Left,
+    };
+    commands
+        .spawn_bundle(Text2dBundle {
+            text: Text::with_section("(0.000, 0.000)", text_style, text_alignment),
+            transform: Transform::from_xyz(-WINDOW_SIZE / 2.0, -WINDOW_SIZE / 2.0, 0.0),
+            ..Default::default()
+        })
+        .insert(CoordinateText);
 }
