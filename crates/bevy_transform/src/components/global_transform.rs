48ce2d0e5cbd6dff478eb7791cababda993c602f
rsk <rskbox@gmail.com>
2022-01-30 18:18:49 +0300

    fix tranformation order, should be scale -> rotate -> translate

--- crates/bevy_transform/src/components/global_transform.rs
+++ crates/bevy_transform/src/components/global_transform.rs
@@ -213,8 +213,8 @@ impl GlobalTransform {
     /// Returns a [`Vec3`] of this [`Transform`] applied to `value`.
     #[inline]
     pub fn mul_vec3(&self, mut value: Vec3) -> Vec3 {
-        value = self.rotation * value;
         value = self.scale * value;
+        value = self.rotation * value;
         value += self.translation;
         value
     }
