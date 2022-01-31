f2a22b9b9412dc4fe2da83514f53941706731665
Aaron Loucks <aloucks@cofront.net>
2022-01-30 21:08:56 -0500

    Remove extra space in PresentMode docs

--- crates/bevy_window/src/window.rs
+++ crates/bevy_window/src/window.rs
@@ -25,7 +25,7 @@ pub enum PresentMode {
     /// The presentation engine does **not** wait for a vertical blanking period and
     /// the request is presented immediately. This is a low-latency presentation mode,
     /// but visible tearing may be observed. Will fallback to `Fifo` if unavailable on the
-    /// selected  platform and backend. Not optimal for mobile.
+    /// selected platform and backend. Not optimal for mobile.
     Immediate = 0,
     /// The presentation engine waits for the next vertical blanking period to update
     /// the current image, but frames may be submitted without delay. This is a low-latency
