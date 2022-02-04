57028b5d38bbe03debe5fb5b94190a0d06936553
KDecay <KDecayMusic@protonmail.com>
2022-02-04 20:06:57 +0100

    ButtonState instead of ElementState

--- crates/bevy_input/src/system.rs
+++ crates/bevy_input/src/system.rs
@@ -17,7 +17,7 @@ pub fn exit_on_esc_system(
 ) {
     for event in keyboard_input_events.iter() {
         if let Some(key_code) = event.key_code {
-            if event.state == ElementState::Pressed && key_code == KeyCode::Escape {
+            if event.state == ButtonState::Pressed && key_code == KeyCode::Escape {
                 app_exit_events.send_default();
             }
         }
