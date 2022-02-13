a661a5acfe898671654e397e86a8bc5da99c0553
dataphract <dataphract@gmail.com>
2022-02-13 13:26:56 -0600

    fix: make quoting style consistent

--- crates/bevy_render/src/render_graph/context.rs
+++ crates/bevy_render/src/render_graph/context.rs
@@ -207,11 +207,11 @@ impl<'a> RenderGraphContext<'a> {
 
 #[derive(Error, Debug, Eq, PartialEq)]
 pub enum RunSubGraphError {
-    #[error("attempted to run sub-graph \"{0}\", but it does not exist")]
+    #[error("attempted to run sub-graph `{0}`, but it does not exist")]
     MissingSubGraph(Cow<'static, str>),
-    #[error("attempted to pass inputs to sub-graph \"{0}\", which has no input slots")]
+    #[error("attempted to pass inputs to sub-graph `{0}`, which has no input slots")]
     SubGraphHasNoInputs(Cow<'static, str>),
-    #[error("sub graph (name: '{graph_name:?}') could not be run because slot '{slot_name}' at index {slot_index} has no value")]
+    #[error("sub graph (name: `{graph_name:?}`) could not be run because slot `{slot_name}` at index {slot_index} has no value")]
     MissingInput {
         slot_index: usize,
         slot_name: Cow<'static, str>,
@@ -231,7 +231,7 @@ pub enum RunSubGraphError {
 pub enum OutputSlotError {
     #[error("output slot `{0:?}` does not exist")]
     InvalidSlot(SlotLabel),
-    #[error("attempted to output a value of type {actual} to output slot {label:?}, which has type {expected}")]
+    #[error("attempted to output a value of type `{actual}` to output slot `{label:?}`, which has type `{expected}`")]
     MismatchedSlotType {
         label: SlotLabel,
         expected: SlotType,
@@ -241,9 +241,9 @@ pub enum OutputSlotError {
 
 #[derive(Error, Debug, Eq, PartialEq)]
 pub enum InputSlotError {
-    #[error("input slot {0:?} does not exist")]
+    #[error("input slot `{0:?}` does not exist")]
     InvalidSlot(SlotLabel),
-    #[error("attempted to retrieve a value of type {actual} from input slot {label:?}, which has type {expected}")]
+    #[error("attempted to retrieve a value of type `{actual}` from input slot `{label:?}`, which has type `{expected}`")]
     MismatchedSlotType {
         label: SlotLabel,
         expected: SlotType,
