3667ac404bf586f71f640fddf139c3bc57ed9ba6
TheRawMeatball <therawmeatball@gmail.com>
2022-02-14 20:08:08 +0300

    Add Local support to exclusive systems

--- crates/bevy_ecs/src/system/exclusive_system.rs
+++ crates/bevy_ecs/src/system/exclusive_system.rs
@@ -1,6 +1,9 @@
+use bevy_ecs_macros::all_tuples;
+
 use crate::{
     archetype::ArchetypeGeneration,
-    system::{check_system_change_tick, BoxedSystem, IntoSystem},
+    prelude::{FromWorld, Local},
+    system::{check_system_change_tick, BoxedSystem, IntoSystem, Resource},
     world::World,
 };
 use std::borrow::Cow;
@@ -15,57 +18,74 @@ pub trait ExclusiveSystem: Send + Sync + 'static {
     fn check_change_tick(&mut self, change_tick: u32);
 }
 
-pub struct ExclusiveSystemFn<F> {
+pub struct ExclusiveSystemFn<F, L> {
     func: F,
     name: Cow<'static, str>,
     last_change_tick: u32,
+    locals: Option<L>,
 }
 
-impl<F> ExclusiveSystem for ExclusiveSystemFn<F>
-where
-    F: FnMut(&mut World) + Send + Sync + 'static,
-{
-    fn name(&self) -> Cow<'static, str> {
-        self.name.clone()
-    }
+macro_rules! impl_exclusive_system {
+    ($($t:ident),*) => {
+        impl<F, $($t: FromWorld + Resource,)*> ExclusiveSystem for ExclusiveSystemFn<F, ($($t,)*)>
+        where
+            F: FnMut(&mut World, $(Local<$t>,)*) + Send + Sync + 'static,
+        {
+            fn name(&self) -> Cow<'static, str> {
+                self.name.clone()
+            }
 
-    fn run(&mut self, world: &mut World) {
-        // The previous value is saved in case this exclusive system is run by another exclusive
-        // system
-        let saved_last_tick = world.last_change_tick;
-        world.last_change_tick = self.last_change_tick;
+            fn run(&mut self, world: &mut World) {
+                // The previous value is saved in case this exclusive system is run by another exclusive
+                // system
+                let saved_last_tick = world.last_change_tick;
+                world.last_change_tick = self.last_change_tick;
 
-        (self.func)(world);
+                let ($($t,)*) = self.locals.as_mut().unwrap();
+                (self.func)(world, $(Local(&mut $t),)*);
 
-        let change_tick = world.change_tick.get_mut();
-        self.last_change_tick = *change_tick;
-        *change_tick += 1;
+                let change_tick = world.change_tick.get_mut();
+                self.last_change_tick = *change_tick;
+                *change_tick += 1;
 
-        world.last_change_tick = saved_last_tick;
-    }
+                world.last_change_tick = saved_last_tick;
+            }
 
-    fn initialize(&mut self, _: &mut World) {}
+            fn initialize(&mut self, _world: &mut World) {
+                self.locals = Some(($($t::from_world(_world),)*));
+            }
 
-    fn check_change_tick(&mut self, change_tick: u32) {
-        check_system_change_tick(&mut self.last_change_tick, change_tick, self.name.as_ref());
-    }
+            fn check_change_tick(&mut self, change_tick: u32) {
+                check_system_change_tick(
+                    &mut self.last_change_tick,
+                    change_tick,
+                    self.name.as_ref(),
+                );
+            }
+        }
+
+
+        impl<F, $($t: FromWorld + Resource,)*> IntoExclusiveSystem<&mut World, ExclusiveSystemFn<F, ($($t,)*)>> for F
+        where
+            F: FnMut(&mut World, $(Local<$t>,)*) + Send + Sync + 'static,
+        {
+            fn exclusive_system(self) -> ExclusiveSystemFn<F, ($($t,)*)> {
+                ExclusiveSystemFn {
+                    func: self,
+                    name: core::any::type_name::<F>().into(),
+                    last_change_tick: 0,
+                    locals: None,
+                }
+            }
+        }
+
+    };
 }
+
+all_tuples!(impl_exclusive_system, 0, 12, L);
 
 pub trait IntoExclusiveSystem<Params, SystemType> {
     fn exclusive_system(self) -> SystemType;
-}
-
-impl<F> IntoExclusiveSystem<&mut World, ExclusiveSystemFn<F>> for F
-where
-    F: FnMut(&mut World) + Send + Sync + 'static,
-{
-    fn exclusive_system(self) -> ExclusiveSystemFn<F> {
-        ExclusiveSystemFn {
-            func: self,
-            name: core::any::type_name::<F>().into(),
-            last_change_tick: 0,
-        }
-    }
 }
 
 pub struct ExclusiveSystemCoerced {
--- crates/bevy_ecs/src/system/system_param.rs
+++ crates/bevy_ecs/src/system/system_param.rs
@@ -625,7 +625,7 @@ impl<'w, 's> SystemParamFetch<'w, 's> for WorldState {
 /// // Note how the read local is still 0 due to the locals not being shared.
 /// assert_eq!(read_system.run((), world), 0);
 /// ```
-pub struct Local<'a, T: Resource>(&'a mut T);
+pub struct Local<'a, T: Resource>(pub(crate) &'a mut T);
 
 // SAFE: Local only accesses internal state
 unsafe impl<T: Resource> ReadOnlySystemParamFetch for LocalState<T> {}
