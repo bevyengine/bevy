a41fe022a6c26e6942e3bb4ea64e326aa95be934
Aevyrie Roessler <aevyrie@gmail.com>
2022-02-17 21:23:51 -0800

    Improve example clarity

--- examples/window/low_power.rs
+++ examples/window/low_power.rs
@@ -1,35 +1,52 @@
 use bevy::{
     prelude::*,
-    window::{PresentMode, RequestRedraw},
+    window::RequestRedraw,
     winit::{ControlFlow, WinitConfig},
 };
 
-/// This example illustrates how to run in low power mode, useful for making desktop applications.
-/// The app will only update when there is an event (resize, mouse input, etc.), or you send a
-/// redraw request.
+/// This example illustrates how to run a winit window in a reactive, low power mode, useful for
+/// making desktop applications or any other program that doesn't need to be running the main loop
+/// non-stop. The app will only update when there is an event (resize, mouse input, etc.), or a
+/// redraw request is sent to force an update.
+///
+/// When the window is minimized, de-focused, or not receiving input, it will use almost zero power.
 fn main() {
     App::new()
         .insert_resource(WinitConfig {
             control_flow: ControlFlow::Wait,
             ..Default::default()
         })
+        .insert_resource(ManuallyRedraw(false))
         .add_plugins(DefaultPlugins)
         .add_startup_system(setup)
+        .add_system(toggle_mode)
         .add_system(rotate)
-        // Try uncommenting this system to manually request a redraw every frame:
-        //.add_system(request_redraw)
         .run();
 }
 
-fn request_redraw(mut event: EventWriter<RequestRedraw>) {
-    event.send(RequestRedraw);
+struct ManuallyRedraw(bool);
+
+/// This system runs every update and switches between two modes when the left mouse button is
+/// clicked:
+/// 1) Continuously sending redraw requests
+/// 2) Only updating the app when an input is received
+fn toggle_mode(
+    mut event: EventWriter<RequestRedraw>,
+    mut redraw: ResMut<ManuallyRedraw>,
+    mouse_button_input: Res<Input<MouseButton>>,
+) {
+    if mouse_button_input.just_pressed(MouseButton::Left) {
+        redraw.0 = !redraw.0;
+    }
+    if redraw.0 {
+        event.send(RequestRedraw);
+    }
 }
 
 fn rotate(mut cube_transform: Query<&mut Transform, With<Rotator>>) {
     for mut transform in cube_transform.iter_mut() {
         transform.rotate(Quat::from_rotation_x(0.05));
-        transform.rotate(Quat::from_rotation_y(0.05));
-        transform.rotate(Quat::from_rotation_z(0.05));
+        transform.rotate(Quat::from_rotation_y(0.08));
     }
 }
 
@@ -42,15 +59,18 @@ fn setup(
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
     mut event: EventWriter<RequestRedraw>,
+    asset_server: Res<AssetServer>,
 ) {
+    let mesh = meshes.add(Mesh::from(shape::Cube { size: 0.5 }));
+    let material = materials.add(Color::rgb(0.8, 0.7, 0.6).into());
     // Spawn a big block of cubes
-    for i in -5..5 {
-        for j in -5..5 {
-            for k in -5..5 {
+    for i in -3..=3 {
+        for j in -3..=3 {
+            for k in -3..=3 {
                 commands
                     .spawn_bundle(PbrBundle {
-                        mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
-                        material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
+                        mesh: mesh.clone(),
+                        material: material.clone(),
                         transform: Transform::from_xyz(i as f32, j as f32, k as f32),
                         ..Default::default()
                     })
@@ -74,4 +94,30 @@ fn setup(
         ..Default::default()
     });
     event.send(RequestRedraw);
+    // UI camera
+    commands.spawn_bundle(UiCameraBundle::default());
+    // Text with one section
+    commands.spawn_bundle(TextBundle {
+        style: Style {
+            align_self: AlignSelf::FlexEnd,
+            position_type: PositionType::Absolute,
+            position: Rect {
+                bottom: Val::Px(5.0),
+                right: Val::Px(15.0),
+                ..Default::default()
+            },
+            ..Default::default()
+        },
+        // Use the `Text::with_section` constructor
+        text: Text::with_section(
+            "Click left mouse button to toggle continuous redraw requests",
+            TextStyle {
+                font: asset_server.load("fonts/FiraSans-Bold.ttf"),
+                font_size: 50.0,
+                color: Color::WHITE,
+            },
+            TextAlignment::default(),
+        ),
+        ..Default::default()
+    });
 }
