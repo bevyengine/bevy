6d3a6c0af93fe580a1a2df5501269ba7f1717476
Gabriel Bourgeois <gabriel.bourgeoisv4si@gmail.com>
2022-02-19 12:21:23 -0500

    Change UI example to showcase new Border component

--- examples/ui/ui.rs
+++ examples/ui/ui.rs
@@ -34,42 +34,34 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
                     style: Style {
                         size: Size::new(Val::Px(200.0), Val::Percent(100.0)),
                         border: Rect::all(Val::Px(2.0)),
+                        align_items: AlignItems::FlexEnd,
                         ..Default::default()
                     },
-                    color: Color::rgb(0.65, 0.65, 0.65).into(),
+                    color: Color::rgb(0.15, 0.15, 0.15).into(),
+                    border: Border {
+                        color: Color::rgb(0.65, 0.65, 0.65),
+                        width: 2.0,
+                    },
                     ..Default::default()
                 })
                 .with_children(|parent| {
-                    // left vertical fill (content)
-                    parent
-                        .spawn_bundle(NodeBundle {
-                            style: Style {
-                                size: Size::new(Val::Percent(100.0), Val::Percent(100.0)),
-                                align_items: AlignItems::FlexEnd,
-                                ..Default::default()
-                            },
-                            color: Color::rgb(0.15, 0.15, 0.15).into(),
+                    // text
+                    parent.spawn_bundle(TextBundle {
+                        style: Style {
+                            margin: Rect::all(Val::Px(5.0)),
                             ..Default::default()
-                        })
-                        .with_children(|parent| {
-                            // text
-                            parent.spawn_bundle(TextBundle {
-                                style: Style {
-                                    margin: Rect::all(Val::Px(5.0)),
-                                    ..Default::default()
-                                },
-                                text: Text::with_section(
-                                    "Text Example",
-                                    TextStyle {
-                                        font: asset_server.load("fonts/FiraSans-Bold.ttf"),
-                                        font_size: 30.0,
-                                        color: Color::WHITE,
-                                    },
-                                    Default::default(),
-                                ),
-                                ..Default::default()
-                            });
-                        });
+                        },
+                        text: Text::with_section(
+                            "Text Example",
+                            TextStyle {
+                                font: asset_server.load("fonts/FiraSans-Bold.ttf"),
+                                font_size: 30.0,
+                                color: Color::WHITE,
+                            },
+                            Default::default(),
+                        ),
+                        ..Default::default()
+                    });
                 });
             // right vertical fill
             parent
