cf3e3ea35dbb59ee29d63f9cd363b8a12a7047e3
Jupp56 <nick.gruessner@outlook.de>
2022-02-23 13:29:07 +0100

    additional typo fix

--- crates/bevy_ecs/src/system/query.rs
+++ crates/bevy_ecs/src/system/query.rs
@@ -316,7 +316,7 @@ where
     /// # Arguments
     ///
     ///* `task_pool` - The [`TaskPool`] to use
-    ///* `batch_size` - The Number of batches to spawn
+    ///* `batch_size` - The number of batches to spawn
     ///* `f` - The function to run on each item in the query
     #[inline]
     pub fn par_for_each(
