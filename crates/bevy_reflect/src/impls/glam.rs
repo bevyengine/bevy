4c4c36f3789d2da8a3901a2fc30ecd4431480942
james7132 <contact@jamessliu.com>
2022-02-23 23:42:57 -0800

    Implement Reflect for Vec3A

--- crates/bevy_reflect/src/impls/glam.rs
+++ crates/bevy_reflect/src/impls/glam.rs
@@ -1,7 +1,7 @@
 use crate as bevy_reflect;
 use crate::ReflectDeserialize;
 use bevy_reflect_derive::{impl_from_reflect_value, impl_reflect_value};
-use glam::{IVec2, IVec3, IVec4, Mat3, Mat4, Quat, UVec2, UVec3, UVec4, Vec2, Vec3, Vec4};
+use glam::{IVec2, IVec3, IVec4, Mat3, Mat4, Quat, UVec2, UVec3, UVec4, Vec2, Vec3, Vec3A, Vec4};
 
 impl_reflect_value!(IVec2(PartialEq, Serialize, Deserialize));
 impl_reflect_value!(IVec3(PartialEq, Serialize, Deserialize));
@@ -11,6 +11,7 @@ impl_reflect_value!(UVec3(PartialEq, Serialize, Deserialize));
 impl_reflect_value!(UVec4(PartialEq, Serialize, Deserialize));
 impl_reflect_value!(Vec2(PartialEq, Serialize, Deserialize));
 impl_reflect_value!(Vec3(PartialEq, Serialize, Deserialize));
+impl_reflect_value!(Vec3A(PartialEq, Serialize, Deserialize));
 impl_reflect_value!(Vec4(PartialEq, Serialize, Deserialize));
 impl_reflect_value!(Mat3(PartialEq, Serialize, Deserialize));
 impl_reflect_value!(Mat4(PartialEq, Serialize, Deserialize));
@@ -24,6 +25,7 @@ impl_from_reflect_value!(UVec3);
 impl_from_reflect_value!(UVec4);
 impl_from_reflect_value!(Vec2);
 impl_from_reflect_value!(Vec3);
+impl_from_reflect_value!(Vec3A);
 impl_from_reflect_value!(Vec4);
 impl_from_reflect_value!(Mat3);
 impl_from_reflect_value!(Mat4);
