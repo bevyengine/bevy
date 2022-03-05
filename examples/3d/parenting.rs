a731a62904e2d8a68bfe8cbb032f55b18baaac6d
David Taralla <davidtaralla@gmail.com>
2022-03-05 15:18:03 -0500

    Reword rules of transform inheritance
    Make it more future-proof.
    
    Co-authored-by: Alice Cecile <alice.i.cecile@gmail.com>

--- examples/3d/parenting.rs
+++ examples/3d/parenting.rs
@@ -2,7 +2,7 @@ use bevy::prelude::*;
 
 /// This example illustrates how to create parent->child relationships between entities how parent
 /// transforms are propagated to their descendants.
-/// Any entity with a `Transform` _and_ a `GlobalTransform` can be a parent.
+///  `Transform` can be inherited from any entity with both the `Transform` and `GlobalTransform` components.
 fn main() {
     App::new()
         .insert_resource(Msaa { samples: 4 })
