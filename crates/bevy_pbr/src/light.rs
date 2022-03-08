29b5ed0b7ca0dd7ee907e70c055d28bc3a58f0bb
robtfm <50659922+robtfm@users.noreply.github.com>
2022-03-08 11:58:01 +0000

    add basic tests

--- crates/bevy_pbr/src/light.rs
+++ crates/bevy_pbr/src/light.rs
@@ -1260,3 +1260,67 @@ pub fn check_light_mesh_visibility(
         }
     }
 }
+
+mod test {
+    use super::*;
+
+    #[allow(dead_code)]
+    fn test_cluster_tiling(config: ClusterConfig, screen_size: UVec2) -> Clusters {
+        let dims = config.dimensions_for_screen_size(screen_size);
+
+        // note: near & far do not affect tiling
+        let clusters = Clusters::from_screen_size_and_dimensions(screen_size, dims, 5.0, 1000.0);
+
+        // check we cover the screen
+        assert!(clusters.tile_size.x * clusters.axis_slices.x >= screen_size.x);
+        assert!(clusters.tile_size.y * clusters.axis_slices.y >= screen_size.y);
+        // check a smaller number of clusters would not cover the screen
+        assert!(clusters.tile_size.x * (clusters.axis_slices.x - 1) < screen_size.x);
+        assert!(clusters.tile_size.y * (clusters.axis_slices.y - 1) < screen_size.y);
+        // check a smaller tilesize would not cover the screen
+        assert!((clusters.tile_size.x - 1) * clusters.axis_slices.x < screen_size.x);
+        assert!((clusters.tile_size.y - 1) * clusters.axis_slices.y < screen_size.y);
+        // check we don't have more clusters than pixels
+        assert!(clusters.axis_slices.x <= screen_size.x);
+        assert!(clusters.axis_slices.y <= screen_size.y);
+
+        clusters
+    }
+
+    #[test]
+    // check tiling for small screen sizes
+    fn test_default_cluster_setup_small_screensizes() {
+        for x in 1..100 {
+            for y in 1..100 {
+                let screen_size = UVec2::new(x, y);
+                let clusters = test_cluster_tiling(ClusterConfig::default(), screen_size);
+                assert!(
+                    clusters.axis_slices.x * clusters.axis_slices.y * clusters.axis_slices.z
+                        <= 4096
+                );
+            }
+        }
+    }
+
+    #[test]
+    // check tiling for long thin screen sizes
+    fn test_default_cluster_setup_small_x() {
+        for x in 1..10 {
+            for y in 1..5000 {
+                let screen_size = UVec2::new(x, y);
+                let clusters = test_cluster_tiling(ClusterConfig::default(), screen_size);
+                assert!(
+                    clusters.axis_slices.x * clusters.axis_slices.y * clusters.axis_slices.z
+                        <= 4096
+                );
+
+                let screen_size = UVec2::new(y, x);
+                let clusters = test_cluster_tiling(ClusterConfig::default(), screen_size);
+                assert!(
+                    clusters.axis_slices.x * clusters.axis_slices.y * clusters.axis_slices.z
+                        <= 4096
+                );
+            }
+        }
+    }
+}
