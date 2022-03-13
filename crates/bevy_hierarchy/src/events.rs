42d0ad328b0e49b9c6ec95d0e3b474f27afba889
james7132 <contact@jamessliu.com>
2022-03-13 04:19:44 -0700

    Document new events

--- crates/bevy_hierarchy/src/events.rs
+++ crates/bevy_hierarchy/src/events.rs
@@ -1,20 +1,33 @@
 use bevy_ecs::prelude::Entity;
 
+/// An event that is fired whenever an [`Entity`] is added as a child
+/// to a new parent.
 #[derive(Clone)]
 pub struct ChildAdded {
-    pub parent: Entity,
+    /// The child that added
     pub child: Entity,
+    /// The parent the child was added to
+    pub parent: Entity,
 }
 
+/// An event that is fired whenever an child [`Entity`] is removed from
+/// to parent.
 #[derive(Clone)]
 pub struct ChildRemoved {
-    pub parent: Entity,
+    /// The child that removed
     pub child: Entity,
+    /// The parent the child was removed from
+    pub parent: Entity,
 }
 
+/// An event that is fired whenever an child [`Entity`] is moved to
+/// a new parent.
 #[derive(Clone)]
 pub struct ChildMoved {
+    /// The child that moved
     pub child: Entity,
+    /// The parent the child was removed from
     pub previous_parent: Entity,
+    /// The parent the child was added to
     pub new_parent: Entity,
 }
