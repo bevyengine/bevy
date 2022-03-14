115fea3a28ab573ed4e671a7ef3092c50e726d7f
james7132 <contact@jamessliu.com>
2022-03-14 11:48:39 -0700

    Fix build

--- crates/bevy_render/src/render_component.rs
+++ crates/bevy_render/src/render_component.rs
@@ -117,7 +117,9 @@ fn prepare_uniform_components<C: Component>(
             (
                 entity,
                 (DynamicUniformIndex::<C> {
-                    index: component_uniforms.uniforms.push(component.clone()),
+                    index: component_uniforms
+                        .uniforms
+                        .push_and_get_offset(component.clone()),
                     marker: PhantomData,
                 },),
             )
