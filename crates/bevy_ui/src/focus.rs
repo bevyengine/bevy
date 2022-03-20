9ec11c6c25fcb4c9aec5fd16bc75d0eeb8591c33
pubrrr <pubr@posteo.de>
2022-03-20 12:04:25 +0100

    document current behavior by unit tests

--- crates/bevy_ui/src/focus.rs
+++ crates/bevy_ui/src/focus.rs
@@ -4,7 +4,7 @@ use bevy_ecs::{
     entity::Entity,
     prelude::Component,
     reflect::ReflectComponent,
-    system::{Local, Query, Res},
+    system::{Local, Query, Res, Resource},
 };
 use bevy_input::{mouse::MouseButton, touch::Touches, Input};
 use bevy_math::Vec2;
@@ -59,8 +59,32 @@ pub struct State {
 /// The system that sets Interaction for all UI elements based on the mouse cursor activity
 #[allow(clippy::type_complexity)]
 pub fn ui_focus_system(
-    mut state: Local<State>,
+    state: Local<State>,
     windows: Res<Windows>,
+    mouse_button_input: Res<Input<MouseButton>>,
+    touches_input: Res<Touches>,
+    node_query: Query<(
+        Entity,
+        &Node,
+        &GlobalTransform,
+        Option<&mut Interaction>,
+        Option<&FocusPolicy>,
+        Option<&CalculatedClip>,
+    )>,
+) {
+    focus_ui(
+        state,
+        windows,
+        mouse_button_input,
+        touches_input,
+        node_query,
+    )
+}
+
+#[allow(clippy::type_complexity)]
+fn focus_ui<Cursor: CursorResource>(
+    mut state: Local<State>,
+    windows: Res<Cursor>,
     mouse_button_input: Res<Input<MouseButton>>,
     touches_input: Res<Touches>,
     mut node_query: Query<(
@@ -72,9 +96,7 @@ pub fn ui_focus_system(
         Option<&CalculatedClip>,
     )>,
 ) {
-    let cursor_position = windows
-        .get_primary()
-        .and_then(|window| window.cursor_position());
+    let cursor_position = windows.get_cursor_position();
 
     // reset entities that were both clicked and released in the last frame
     for entity in state.entities_to_reset.drain(..) {
@@ -172,6 +194,384 @@ pub fn ui_focus_system(
             if *interaction != Interaction::None {
                 *interaction = Interaction::None;
             }
+        }
+    }
+}
+
+trait CursorResource: Resource {
+    fn get_cursor_position(&self) -> Option<Vec2>;
+}
+
+impl CursorResource for Windows {
+    fn get_cursor_position(&self) -> Option<Vec2> {
+        self.get_primary()
+            .and_then(|window| window.cursor_position())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use bevy_app::App;
+    use bevy_ecs::event::Events;
+    use bevy_ecs::prelude::ParallelSystemDescriptorCoercion;
+    use bevy_input::touch::{touch_screen_input_system, TouchInput, TouchPhase};
+    use bevy_math::Vec3;
+
+    use super::*;
+
+    const NODE_SIZE: f32 = 5.0;
+
+    #[test]
+    fn test_sets_hovered_nodes() {
+        let test_sets = vec![
+            vec![(None, Interaction::None)],
+            vec![(Some((0., 0.)), Interaction::None)],
+            vec![(Some((10., 10.)), Interaction::Hovered)],
+            vec![
+                (Some((10., 10.)), Interaction::Hovered),
+                (Some((0., 0.)), Interaction::None),
+            ],
+            vec![
+                (Some((10., 10.)), Interaction::Hovered),
+                (None, Interaction::None),
+            ],
+        ];
+
+        for test_set in test_sets {
+            test_hovered_nodes(test_set);
+        }
+    }
+
+    fn test_hovered_nodes(test_set: Vec<(Option<(f32, f32)>, Interaction)>) {
+        let mut app = TestApp::new();
+        let entity = app.spawn_node_entity_at(10., 10.);
+
+        for (cursor_position, expected_interaction) in test_set {
+            app.set_cursor_position(cursor_position);
+
+            app.run_step();
+
+            let interaction = app.get_interaction(entity);
+            assert_eq!(
+                &expected_interaction, interaction,
+                "for position {:?}",
+                cursor_position,
+            );
+        }
+    }
+
+    #[test]
+    fn test_sets_clicked_nodes() {
+        let test_sets_mouse = vec![
+            vec![(None, Interaction::None)],
+            vec![(Some((0., 0.)), Interaction::None)],
+            vec![(Some((10., 10.)), Interaction::Clicked)],
+            vec![
+                (Some((10., 10.)), Interaction::Clicked),
+                (Some((0., 0.)), Interaction::Clicked),
+            ],
+            vec![
+                (Some((10., 10.)), Interaction::Clicked),
+                (None, Interaction::None),
+            ],
+        ];
+        let test_sets_touch = vec![
+            vec![(None, Interaction::None)],
+            vec![(Some((0., 0.)), Interaction::None)],
+            vec![(Some((10., 10.)), Interaction::Clicked)],
+            vec![
+                (Some((10., 10.)), Interaction::Clicked),
+                (Some((0., 0.)), Interaction::None),
+            ],
+            vec![
+                (Some((10., 10.)), Interaction::Clicked),
+                (None, Interaction::None),
+            ],
+        ];
+
+        for mouse_test_set in test_sets_mouse {
+            test_clicked_nodes(mouse_test_set, false);
+        }
+        for touch_test_set in test_sets_touch {
+            test_clicked_nodes(touch_test_set, true);
+        }
+    }
+
+    fn test_clicked_nodes(test_set: Vec<(Option<(f32, f32)>, Interaction)>, touch: bool) {
+        let mut app = TestApp::new();
+        let entity = app.spawn_node_entity_at(10., 10.);
+
+        for (cursor_position, expected_interaction) in test_set {
+            app.set_cursor_position(cursor_position);
+            if touch {
+                app.set_screen_touched();
+            } else {
+                app.set_mouse_clicked();
+            }
+
+            app.run_step();
+
+            let interaction = app.get_interaction(entity);
+            assert_eq!(
+                &expected_interaction, interaction,
+                "for position {:?}",
+                cursor_position,
+            );
+        }
+    }
+
+    #[test]
+    fn test_sets_click_stacked_nodes() {
+        let test_sets = vec![
+            (None, Interaction::None),
+            (Some(FocusPolicy::Block), Interaction::None),
+            (Some(FocusPolicy::Pass), Interaction::Clicked),
+        ];
+
+        for (focus_policy, expected_interaction) in test_sets {
+            test_click_stacked_nodes(focus_policy, expected_interaction);
+        }
+    }
+
+    fn test_click_stacked_nodes(
+        focus_policy: Option<FocusPolicy>,
+        expected_interaction: Interaction,
+    ) {
+        let mut app = TestApp::new();
+        let background_entity = app.spawn_node_entity_with_z_at(10., 10., 0., focus_policy);
+        let foreground_entity = app.spawn_node_entity_with_z_at(10., 10., 5., focus_policy);
+
+        app.set_cursor_position(Some((10., 10.)));
+        app.set_mouse_clicked();
+
+        app.run_step();
+
+        assert_eq!(
+            &Interaction::Clicked,
+            app.get_interaction(foreground_entity)
+        );
+        assert_eq!(
+            &expected_interaction,
+            app.get_interaction(background_entity)
+        );
+    }
+
+    #[test]
+    fn hover_one_node_then_click_the_other_where_both_overlap() {
+        let mut app = TestApp::new();
+        let background_node_position = 8.;
+        let background_entity = app.spawn_node_entity_with_z_at(
+            background_node_position,
+            background_node_position,
+            0.,
+            Some(FocusPolicy::Block),
+        );
+        let foreground_entity =
+            app.spawn_node_entity_with_z_at(10., 10., 5., Some(FocusPolicy::Block));
+
+        app.set_cursor_position(Some((6., 6.)));
+
+        app.run_step();
+
+        assert_eq!(&Interaction::None, app.get_interaction(foreground_entity));
+        assert_eq!(
+            &Interaction::Hovered,
+            app.get_interaction(background_entity)
+        );
+
+        app.set_cursor_position(Some((background_node_position, background_node_position)));
+        app.set_mouse_clicked();
+
+        app.run_step();
+
+        assert_eq!(
+            &Interaction::Clicked,
+            app.get_interaction(foreground_entity)
+        );
+        assert_eq!(&Interaction::None, app.get_interaction(background_entity));
+    }
+
+    #[test]
+    fn click_then_move_away_and_release_mouse_button() {
+        let mut app = TestApp::new();
+        let entity = app.spawn_node_entity_at(10., 10.);
+
+        app.set_cursor_position(Some((10., 10.)));
+        app.set_mouse_clicked();
+
+        app.run_step();
+        assert_eq!(&Interaction::Clicked, app.get_interaction(entity));
+
+        app.set_cursor_position(Some((0., 0.)));
+
+        app.run_step();
+        assert_eq!(&Interaction::Clicked, app.get_interaction(entity));
+
+        app.set_mouse_released();
+
+        app.run_step();
+        assert_eq!(&Interaction::None, app.get_interaction(entity));
+    }
+
+    #[test]
+    fn click_and_keep_pressed() {
+        let mut app = TestApp::new();
+        let entity = app.spawn_node_entity_at(10., 10.);
+        app.set_cursor_position(Some((10., 10.)));
+        app.set_mouse_clicked();
+
+        app.run_step();
+        assert_eq!(&Interaction::Clicked, app.get_interaction(entity));
+
+        app.run_step();
+        assert_eq!(&Interaction::Clicked, app.get_interaction(entity));
+    }
+
+    #[test]
+    fn click_and_release() {
+        let mut app = TestApp::new();
+        let entity = app.spawn_node_entity_at(10., 10.);
+        app.set_cursor_position(Some((10., 10.)));
+
+        app.set_mouse_clicked();
+        app.run_step();
+        assert_eq!(&Interaction::Clicked, app.get_interaction(entity));
+
+        app.set_mouse_released();
+        app.run_step();
+        assert_eq!(&Interaction::Hovered, app.get_interaction(entity));
+    }
+
+    #[test]
+    fn click_and_release_in_single_frame() {
+        let mut app = TestApp::new();
+        let entity = app.spawn_node_entity_at(10., 10.);
+        app.set_cursor_position(Some((10., 10.)));
+
+        app.set_mouse_clicked();
+        app.set_mouse_released();
+        app.run_step();
+        assert_eq!(&Interaction::Clicked, app.get_interaction(entity));
+
+        app.run_step();
+        assert_eq!(&Interaction::Hovered, app.get_interaction(entity));
+    }
+
+    struct TestApp {
+        app: App,
+    }
+
+    impl TestApp {
+        fn new() -> TestApp {
+            let mut app = App::new();
+            app.init_resource::<Input<MouseButton>>()
+                .init_resource::<Touches>()
+                .add_event::<TouchInput>()
+                .add_system(focus_ui::<WindowsDouble>.label("under_test"))
+                .add_system(touch_screen_input_system.before("under_test"));
+
+            TestApp { app }
+        }
+
+        fn set_cursor_position(&mut self, cursor_position: Option<(f32, f32)>) {
+            let cursor_position = cursor_position.map(|(x, y)| Vec2::new(x, y));
+            self.app.insert_resource(WindowsDouble { cursor_position });
+        }
+
+        fn set_screen_touched(&mut self) {
+            self.app
+                .world
+                .get_resource_mut::<Events<TouchInput>>()
+                .unwrap()
+                .send(TouchInput {
+                    phase: TouchPhase::Ended,
+                    position: Default::default(),
+                    force: None,
+                    id: 0,
+                })
+        }
+
+        fn set_mouse_clicked(&mut self) {
+            let mut mouse_input = self
+                .app
+                .world
+                .get_resource_mut::<Input<MouseButton>>()
+                .unwrap();
+            mouse_input.press(MouseButton::Left);
+        }
+
+        fn set_mouse_released(&mut self) {
+            let mut mouse_input = self
+                .app
+                .world
+                .get_resource_mut::<Input<MouseButton>>()
+                .unwrap();
+            mouse_input.release(MouseButton::Left);
+        }
+
+        fn spawn_node_entity_at(&mut self, x: f32, y: f32) -> Entity {
+            self.app
+                .world
+                .spawn()
+                .insert(GlobalTransform {
+                    translation: Vec3::new(x, y, 0.0),
+                    ..GlobalTransform::default()
+                })
+                .insert(Node {
+                    size: Vec2::new(NODE_SIZE, NODE_SIZE),
+                })
+                .insert(Interaction::None)
+                .id()
+        }
+
+        fn spawn_node_entity_with_z_at(
+            &mut self,
+            x: f32,
+            y: f32,
+            z: f32,
+            focus_policy: Option<FocusPolicy>,
+        ) -> Entity {
+            let mut entity = self.app.world.spawn();
+            if let Some(focus_policy) = focus_policy {
+                entity.insert(focus_policy);
+            }
+
+            entity
+                .insert(GlobalTransform {
+                    translation: Vec3::new(x, y, z),
+                    ..GlobalTransform::default()
+                })
+                .insert(Node {
+                    size: Vec2::new(NODE_SIZE, NODE_SIZE),
+                })
+                .insert(Interaction::None)
+                .id()
+        }
+
+        fn run_step(&mut self) {
+            self.app.schedule.run_once(&mut self.app.world);
+
+            let mut mouse_input = self
+                .app
+                .world
+                .get_resource_mut::<Input<MouseButton>>()
+                .unwrap();
+            mouse_input.clear();
+        }
+
+        fn get_interaction(&self, entity: Entity) -> &Interaction {
+            self.app.world.get::<Interaction>(entity).unwrap()
+        }
+    }
+
+    #[derive(Debug, Default)]
+    struct WindowsDouble {
+        cursor_position: Option<Vec2>,
+    }
+
+    impl CursorResource for WindowsDouble {
+        fn get_cursor_position(&self) -> Option<Vec2> {
+            self.cursor_position
         }
     }
 }
