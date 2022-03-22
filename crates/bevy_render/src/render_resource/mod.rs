0a4136d2661a2258ea63904dae0159fe23a896b5
Robert Swain <robert.swain@gmail.com>
2022-03-22 21:23:50 +0000

    Add a helper for storage buffers similar to `UniformVec` (#4079)
    # Objective
    
    - Add a helper for storage buffers similar to `UniformVec`
    
    ## Solution
    
    - Add a `StorageBuffer<T, U>` where `T` is the main body of the shader struct without any final variable-sized array member, and `U` is the type of the items in a variable-sized array.
    - Use `()` as the type for unwanted parts, e.g. `StorageBuffer<(), Vec4>::default()` would construct a binding that would work with `struct MyType { data: array<vec4<f32>>; }` in WGSL and `StorageBuffer<MyType, ()>::default()` would work with `struct MyType { ... }` in WGSL as long as there are no variable-sized arrays.
    - Std430 requires that there is at most one variable-sized array in a storage buffer, that if there is one it is the last member of the binding, and that it has at least one item. `StorageBuffer` handles all of these constraints.

--- crates/bevy_render/src/render_resource/mod.rs
+++ crates/bevy_render/src/render_resource/mod.rs
@@ -6,6 +6,7 @@ mod pipeline;
 mod pipeline_cache;
 mod pipeline_specializer;
 mod shader;
+mod storage_buffer;
 mod texture;
 mod uniform_vec;
 
@@ -17,6 +18,7 @@ pub use pipeline::*;
 pub use pipeline_cache::*;
 pub use pipeline_specializer::*;
 pub use shader::*;
+pub use storage_buffer::*;
 pub use texture::*;
 pub use uniform_vec::*;
 
