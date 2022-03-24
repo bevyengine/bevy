8dc269abe65181d91a7404fbef3d32655d7f99ec
Alice Cecile <alice.i.cecile@gmail.com>
2022-03-24 19:56:44 -0400

    Tweak constants for the AESTHETIC

--- examples/game/breakout.rs
+++ examples/game/breakout.rs
@@ -13,8 +13,8 @@ const TIME_STEP: f32 = 1.0 / 60.0;
 // These constants are defined in `Transform` units.
 // Using the default 2D camera they correspond 1:1 with screen pixels.
 // The `const_vec3!` macros are needed as functions that operate on floats cannot be constant in Rust.
-const PADDLE_SIZE: Vec3 = const_vec3!([120.0, 30.0, 0.0]);
-const GAP_BETWEEN_PADDLE_AND_FLOOR: f32 = 100.0;
+const PADDLE_SIZE: Vec3 = const_vec3!([120.0, 20.0, 0.0]);
+const GAP_BETWEEN_PADDLE_AND_FLOOR: f32 = 60.0;
 const PADDLE_SPEED: f32 = 500.0;
 // How close can the paddle get to the wall
 const PADDLE_PADDING: f32 = 10.0;
@@ -35,17 +35,17 @@ const TOP_WALL: f32 = 300.;
 
 const BRICK_SIZE: Vec2 = const_vec2!([100., 30.]);
 // These values are exact
-const GAP_BETWEEN_PADDLE_AND_BRICKS: f32 = 120.0;
+const GAP_BETWEEN_PADDLE_AND_BRICKS: f32 = 270.0;
 const GAP_BETWEEN_BRICKS: f32 = 5.0;
 // These values are lower bounds, as the number of bricks is computed
-const GAP_BETWEEN_BRICKS_AND_CEILING: f32 = 50.0;
+const GAP_BETWEEN_BRICKS_AND_CEILING: f32 = 20.0;
 const GAP_BETWEEN_BRICKS_AND_SIDES: f32 = 20.0;
 
 const SCOREBOARD_FONT_SIZE: f32 = 40.0;
 const SCOREBOARD_TEXT_PADDING: Val = Val::Px(5.0);
 
 const BACKGROUND_COLOR: Color = Color::rgb(0.9, 0.9, 0.9);
-const PADDLE_COLOR: Color = Color::rgb(0.5, 0.5, 1.0);
+const PADDLE_COLOR: Color = Color::rgb(0.3, 0.3, 0.7);
 const BALL_COLOR: Color = Color::rgb(1.0, 0.5, 0.5);
 const BRICK_COLOR: Color = Color::rgb(0.5, 0.5, 1.0);
 const WALL_COLOR: Color = Color::rgb(0.8, 0.8, 0.8);
