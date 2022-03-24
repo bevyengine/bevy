5eb8a376beb41de6d5092eb399fe3026a779783d
Alice Cecile <alice.i.cecile@gmail.com>
2022-03-24 19:31:33 -0400

    Enforce correct system ordering

--- examples/game/breakout.rs
+++ examples/game/breakout.rs
@@ -61,9 +61,9 @@ fn main() {
         .add_system_set(
             SystemSet::new()
                 .with_run_criteria(FixedTimestep::step(TIME_STEP as f64))
-                .with_system(move_paddle)
                 .with_system(check_for_collisions)
-                .with_system(apply_velocity),
+                .with_system(move_paddle.before(check_for_collisions))
+                .with_system(apply_velocity.before(check_for_collisions)),
         )
         .add_system(update_scoreboard)
         .add_system(bevy::input::system::exit_on_esc_system)
