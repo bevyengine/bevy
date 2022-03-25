823c8c4d3ae568e67966e169bba0d9170ac7955b
Nathan Pinard <bytemuck@gmail.com>
2022-03-24 22:08:55 -0400

    added another example of closure + fixed closures themselves

--- examples/ecs/system_closure.rs
+++ examples/ecs/system_closure.rs
@@ -4,12 +4,18 @@ fn main() {
     let f = |value: String| {
         move |mut arg: Local<String>| {
             *arg = value.clone();
-            info!("MyWrapper is : {:?}", arg);
+            info!("The value is : {:?}", arg);
         }
     };
 
     App::new()
         .add_plugin(LogPlugin)
-        .add_startup_system(f("Hello, World!".into()))
+        .add_startup_system(f("Hello, Closure 1!".into()))
+        .add_startup_system(Box::new(|value: String| {
+            move |mut arg: Local<String>| {
+                *arg = value.clone();
+                info!("The value is : {:?}", arg);
+            }
+        })("Hello, Closure 2!".to_string()))
         .run();
 }
