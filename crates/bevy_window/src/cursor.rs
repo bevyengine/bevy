faa5318d6b226eaf1e9af559d7d9c54f081fd8e7
Arnav Choubey <56453634+x-52@users.noreply.github.com>
2022-03-25 15:45:18 -0400

    Link to `winit` in `bevy_window::CursorIcon`

--- crates/bevy_window/src/cursor.rs
+++ crates/bevy_window/src/cursor.rs
@@ -1,7 +1,7 @@
 /// The icon to display for a window's cursor
 /// Examples of all of these cursors can be found [here](https://www.w3schools.com/cssref/playit.asp?filename=playcss_cursor)
-/// This `enum` is simply a copy of a similar `enum` found in `winit`.
-/// `winit`, in turn, simply used cursor types avilable in the browser.
+/// This `enum` is simply a copy of a similar `enum` found in [`winit`](https://docs.rs/winit/latest/winit/window/enum.CursorIcon.html).
+/// `winit`, in turn, mostly copied cursor types avilable in the browser.
 #[derive(Debug, Hash, PartialEq, Eq, Clone, Copy)]
 pub enum CursorIcon {
     /// The platform-dependent default cursor.
