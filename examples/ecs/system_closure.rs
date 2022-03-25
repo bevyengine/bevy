8940d1602c9aaea57581912276ceced35c195dad
Nathan Pinard <bytemuck@gmail.com>
2022-03-24 21:55:50 -0400

    updated examples/system_closure.rs

--- examples/ecs/system_closure.rs
+++ examples/ecs/system_closure.rs
@@ -1,19 +1,15 @@
 use bevy::{prelude::*, log::LogPlugin};
 
-#[derive(Default)]
-struct MyConfig(u32);
+fn main() {
+    let f = |value: String| {
+        move |mut arg: Local<String>| {
+            *arg = value.clone();
+            info!("MyWrapper is : {:?}", arg);
+        }
+    };
 
-
-fn configure_local_example(value: String) -> impl FnMut(Local<String>) {
-    move |mut arg| {
-        *arg = value.clone();
-        info!("MyWrapper is : {:?}", arg);
-    }
-}
-
-fn main() {   
     App::new()
         .add_plugin(LogPlugin)
-        .add_startup_system(configure_local_example("Hello, World!".into()))
+        .add_startup_system(f("Hello, World!".into()))
         .run();
 }
