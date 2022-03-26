2d7a807308800bdfa47f05593e0b89545157ba94
Arnav Choubey <56453634+x-52@users.noreply.github.com>
2022-03-26 09:20:39 -0400

    Change a comment in crates/bevy_window/src/raw_window_handle.rs
    Co-authored-by: Alice Cecile <alice.i.cecile@gmail.com>

--- crates/bevy_window/src/raw_window_handle.rs
+++ crates/bevy_window/src/raw_window_handle.rs
@@ -26,7 +26,7 @@ impl RawWindowHandleWrapper {
 // https://github.com/rust-windowing/raw-window-handle/issues/59
 unsafe impl Send for RawWindowHandleWrapper {}
 unsafe impl Sync for RawWindowHandleWrapper {}
-#[allow(missing_docs)] // Almost nobody uses this struct.
+#[doc(hidden)] // Add docs later; this is an obscure implementation detail
 pub struct HasRawWindowHandleWrapper(RawWindowHandle);
 
 // SAFE: the caller has validated that this is a valid context to get RawWindowHandle
