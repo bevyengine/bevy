94c67c53bb6a45d1a9e7437190c490eada1380a0
Alice Cecile <alice.i.cecile@gmail.com>
2022-03-30 11:46:14 -0400

    Add internal_ambiguities dev example

---
+++ examples/tools/internal_ambiguities.rs
@@ -0,0 +1,11 @@
+//! Reports any Bevy-internal system-order ambiguities
+//! This is primarily useful for engine development
+
+use bevy::prelude::*;
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .insert_resource(ReportExecutionOrderAmbiguities::ReportInternal)
+        .run()
+}
