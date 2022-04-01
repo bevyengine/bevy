bevyengine/bevy

commit bf6de8962287050369cd98605490bdd7770c87b4

use marker components for cameras instead of name strings (#3635)
**Problem**
- whenever you want more than one of the builtin cameras (for example multiple windows, split screen, portals), you need to add a render graph node that executes the correct sub graph, extract the camera into the render world and add the correct `RenderPhase<T>` components
- querying for the 3d camera is annoying because you need to compare the camera's name to e.g. `CameraPlugin::CAMERA_3d`

**Solution**
- Introduce the marker types `Camera3d`, `Camera2d` and `CameraUi`
-> `Query<&mut Transform, With<Camera3d>>` works
- `PerspectiveCameraBundle::new_3d()` and `PerspectiveCameraBundle::<Camera3d>::default()` contain the `Camera3d` marker
- `OrthographicCameraBundle::new_3d()` has `Camera3d`, `OrthographicCameraBundle::new_2d()` has `Camera2d`
- remove `ActiveCameras`, `ExtractedCameraNames`
- run 2d, 3d and ui passes for every camera of their respective marker
-> no custom setup for multiple windows example needed

**Open questions**
- do we need a replacement for `ActiveCameras`? What about a component `ActiveCamera { is_active: bool }` similar to `Visibility`?

Co-authored-by: Carter Anderson <mcanders1@gmail.com>

examples/window/multiple_windows.rs
@@ -1,54 +1,19 @@
 use bevy::{
-    core_pipeline::{draw_3d_graph, node, AlphaMask3d, Opaque3d, Transparent3d},
     prelude::*,
-    render::{
-        camera::{ActiveCameras, ExtractedCameraNames, RenderTarget},
-        render_graph::{Node, NodeRunError, RenderGraph, RenderGraphContext, SlotValue},
-        render_phase::RenderPhase,
-        renderer::RenderContext,
-        RenderApp, RenderStage,
-    },
+    render::camera::RenderTarget,
     window::{CreateWindow, PresentMode, WindowId},
 };
 
 /// This example creates a second window and draws a mesh from two different cameras, one in each window
 fn main() {
-    let mut app = App::new();
-    app.add_plugins(DefaultPlugins)
+    App::new()
+        .add_plugins(DefaultPlugins)
         .add_startup_system(setup)
-        .add_startup_system(create_new_window);
-
-    let render_app = app.sub_app_mut(RenderApp);
-    render_app.add_system_to_stage(RenderStage::Extract, extract_secondary_camera_phases);
-    let mut graph = render_app.world.resource_mut::<RenderGraph>();
-    graph.add_node(SECONDARY_PASS_DRIVER, SecondaryCameraDriver);
-    graph
-        .add_node_edge(node::MAIN_PASS_DEPENDENCIES, SECONDARY_PASS_DRIVER)
-        .unwrap();
-    app.run();
+        .add_startup_system(create_new_window)
+        .run();
 }
 
-fn extract_secondary_camera_phases(mut commands: Commands, active_cameras: Res<ActiveCameras>) {
-    if let Some(secondary) = active_cameras.get(SECONDARY_CAMERA_NAME) {
-        if let Some(entity) = secondary.entity {
-            commands.get_or_spawn(entity).insert_bundle((
-                RenderPhase::<Opaque3d>::default(),
-                RenderPhase::<AlphaMask3d>::default(),
-                RenderPhase::<Transparent3d>::default(),
-            ));
-        }
-    }
-}
-
-const SECONDARY_CAMERA_NAME: &str = "Secondary";
-const SECONDARY_PASS_DRIVER: &str = "secondary_pass_driver";
-
-fn create_new_window(
-    mut create_window_events: EventWriter<CreateWindow>,
-
-    mut commands: Commands,
-    mut active_cameras: ResMut<ActiveCameras>,
-) {
+fn create_new_window(mut create_window_events: EventWriter<CreateWindow>, mut commands: Commands) {
     let window_id = WindowId::new();
 
     // sends out a "CreateWindow" event, which will be received by the windowing backend
@@ -62,37 +27,16 @@ fn create_new_window(
             ..default()
         },
     });
+
     // second window camera
     commands.spawn_bundle(PerspectiveCameraBundle {
         camera: Camera {
             target: RenderTarget::Window(window_id),
-            name: Some(SECONDARY_CAMERA_NAME.into()),
             ..default()
         },
         transform: Transform::from_xyz(6.0, 0.0, 0.0).looking_at(Vec3::ZERO, Vec3::Y),
         ..default()
     });
-
-    active_cameras.add(SECONDARY_CAMERA_NAME);
-}
-
-struct SecondaryCameraDriver;
-impl Node for SecondaryCameraDriver {
-    fn run(
-        &self,
-        graph: &mut RenderGraphContext,
-        _render_context: &mut RenderContext,
-        world: &World,
-    ) -> Result<(), NodeRunError> {
-        let extracted_cameras = world.resource::<ExtractedCameraNames>();
-        if let Some(camera_3d) = extracted_cameras.entities.get(SECONDARY_CAMERA_NAME) {
-            graph.run_sub_graph(
-                crate::draw_3d_graph::NAME,
-                vec![SlotValue::Entity(*camera_3d)],
-            )?;
-        }
-        Ok(())
-    }
 }
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {

crates/bevy_core_pipeline/src/lib.rs
@@ -23,7 +23,7 @@ use bevy_app::{App, Plugin};
 use bevy_core::FloatOrd;
 use bevy_ecs::prelude::*;
 use bevy_render::{
-    camera::{ActiveCameras, CameraPlugin, RenderTarget},
+    camera::{ActiveCamera, Camera2d, Camera3d, RenderTarget},
     color::Color,
     render_graph::{EmptyNode, RenderGraph, SlotInfo, SlotType},
     render_phase::{
@@ -367,23 +367,20 @@ pub fn extract_clear_color(
 
 pub fn extract_core_pipeline_camera_phases(
     mut commands: Commands,
-    active_cameras: Res<ActiveCameras>,
+    active_2d: Res<ActiveCamera<Camera2d>>,
+    active_3d: Res<ActiveCamera<Camera3d>>,
 ) {
-    if let Some(camera_2d) = active_cameras.get(CameraPlugin::CAMERA_2D) {
-        if let Some(entity) = camera_2d.entity {
-            commands
-                .get_or_spawn(entity)
-                .insert(RenderPhase::<Transparent2d>::default());
-        }
+    if let Some(entity) = active_2d.get() {
+        commands
+            .get_or_spawn(entity)
+            .insert(RenderPhase::<Transparent2d>::default());
     }
-    if let Some(camera_3d) = active_cameras.get(CameraPlugin::CAMERA_3D) {
-        if let Some(entity) = camera_3d.entity {
-            commands.get_or_spawn(entity).insert_bundle((
-                RenderPhase::<Opaque3d>::default(),
-                RenderPhase::<AlphaMask3d>::default(),
-                RenderPhase::<Transparent3d>::default(),
-            ));
-        }
+    if let Some(entity) = active_3d.get() {
+        commands.get_or_spawn(entity).insert_bundle((
+            RenderPhase::<Opaque3d>::default(),
+            RenderPhase::<AlphaMask3d>::default(),
+            RenderPhase::<Transparent3d>::default(),
+        ));
     }
 }
 

crates/bevy_core_pipeline/src/main_pass_driver.rs
@@ -1,6 +1,6 @@
 use bevy_ecs::world::World;
 use bevy_render::{
-    camera::{CameraPlugin, ExtractedCameraNames},
+    camera::{ActiveCamera, Camera2d, Camera3d},
     render_graph::{Node, NodeRunError, RenderGraphContext, SlotValue},
     renderer::RenderContext,
 };
@@ -14,18 +14,17 @@ impl Node for MainPassDriverNode {
         _render_context: &mut RenderContext,
         world: &World,
     ) -> Result<(), NodeRunError> {
-        let extracted_cameras = world.resource::<ExtractedCameraNames>();
-        if let Some(camera_2d) = extracted_cameras.entities.get(CameraPlugin::CAMERA_2D) {
+        if let Some(camera_2d) = world.resource::<ActiveCamera<Camera2d>>().get() {
             graph.run_sub_graph(
                 crate::draw_2d_graph::NAME,
-                vec![SlotValue::Entity(*camera_2d)],
+                vec![SlotValue::Entity(camera_2d)],
             )?;
         }
 
-        if let Some(camera_3d) = extracted_cameras.entities.get(CameraPlugin::CAMERA_3D) {
+        if let Some(camera_3d) = world.resource::<ActiveCamera<Camera3d>>().get() {
             graph.run_sub_graph(
                 crate::draw_3d_graph::NAME,
-                vec![SlotValue::Entity(*camera_3d)],
+                vec![SlotValue::Entity(camera_3d)],
             )?;
         }
 

crates/bevy_gltf/src/loader.rs
@@ -12,7 +12,7 @@ use bevy_pbr::{
 };
 use bevy_render::{
     camera::{
-        Camera, CameraPlugin, CameraProjection, OrthographicProjection, PerspectiveProjection,
+        Camera, Camera2d, Camera3d, CameraProjection, OrthographicProjection, PerspectiveProjection,
     },
     color::Color,
     mesh::{Indices, Mesh, VertexAttributeValues},
@@ -494,11 +494,10 @@ fn load_node(
                 };
 
                 node.insert(Camera {
-                    name: Some(CameraPlugin::CAMERA_2D.to_owned()),
                     projection_matrix: orthographic_projection.get_projection_matrix(),
                     ..Default::default()
                 });
-                node.insert(orthographic_projection);
+                node.insert(orthographic_projection).insert(Camera2d);
             }
             gltf::camera::Projection::Perspective(perspective) => {
                 let mut perspective_projection: PerspectiveProjection = PerspectiveProjection {
@@ -513,13 +512,13 @@ fn load_node(
                     perspective_projection.aspect_ratio = aspect_ratio;
                 }
                 node.insert(Camera {
-                    name: Some(CameraPlugin::CAMERA_3D.to_owned()),
                     projection_matrix: perspective_projection.get_projection_matrix(),
                     near: perspective_projection.near,
                     far: perspective_projection.far,
                     ..Default::default()
                 });
                 node.insert(perspective_projection);
+                node.insert(Camera3d);
             }
         }
     }

crates/bevy_render/src/camera/active_cameras.rs
@@ -1,73 +1,0 @@
-use super::Camera;
-use bevy_ecs::{
-    entity::Entity,
-    system::{Query, ResMut},
-};
-use bevy_utils::HashMap;
-
-#[derive(Debug, Default)]
-pub struct ActiveCamera {
-    pub name: String,
-    pub entity: Option<Entity>,
-}
-
-#[derive(Debug, Default)]
-pub struct ActiveCameras {
-    cameras: HashMap<String, ActiveCamera>,
-}
-
-impl ActiveCameras {
-    pub fn add(&mut self, name: &str) {
-        self.cameras.insert(
-            name.to_string(),
-            ActiveCamera {
-                name: name.to_string(),
-                ..Default::default()
-            },
-        );
-    }
-
-    pub fn get(&self, name: &str) -> Option<&ActiveCamera> {
-        self.cameras.get(name)
-    }
-
-    pub fn get_mut(&mut self, name: &str) -> Option<&mut ActiveCamera> {
-        self.cameras.get_mut(name)
-    }
-
-    pub fn remove(&mut self, name: &str) -> Option<ActiveCamera> {
-        self.cameras.remove(name)
-    }
-
-    pub fn iter(&self) -> impl Iterator<Item = &ActiveCamera> {
-        self.cameras.values()
-    }
-
-    pub fn iter_mut(&mut self) -> impl Iterator<Item = &mut ActiveCamera> {
-        self.cameras.values_mut()
-    }
-}
-
-pub fn active_cameras_system(
-    mut active_cameras: ResMut<ActiveCameras>,
-    query: Query<(Entity, &Camera)>,
-) {
-    for (name, active_camera) in &mut active_cameras.cameras {
-        if active_camera
-            .entity
-            .map_or(false, |entity| query.get(entity).is_err())
-        {
-            active_camera.entity = None;
-        }
-
-        if active_camera.entity.is_none() {
-            for (camera_entity, camera) in query.iter() {
-                if let Some(ref current_name) = camera.name {
-                    if current_name == name {
-                        active_camera.entity = Some(camera_entity);
-                    }
-                }
-            }
-        }
-    }
-}

crates/bevy_render/src/camera/bundle.rs
@@ -1,36 +1,48 @@
 use crate::{
-    camera::{
-        Camera, CameraPlugin, DepthCalculation, OrthographicProjection, PerspectiveProjection,
-        ScalingMode,
-    },
+    camera::{Camera, DepthCalculation, OrthographicProjection, PerspectiveProjection},
     primitives::Frustum,
     view::VisibleEntities,
 };
-use bevy_ecs::bundle::Bundle;
+use bevy_ecs::{bundle::Bundle, prelude::Component};
 use bevy_math::Vec3;
 use bevy_transform::components::{GlobalTransform, Transform};
 
-use super::CameraProjection;
+use super::{CameraProjection, ScalingMode};
+
+#[derive(Component, Default)]
+pub struct Camera3d;
+
+#[derive(Component, Default)]
+pub struct Camera2d;
 
 /// Component bundle for camera entities with perspective projection
 ///
 /// Use this for 3D rendering.
 #[derive(Bundle)]
-pub struct PerspectiveCameraBundle {
+pub struct PerspectiveCameraBundle<M: Component> {
     pub camera: Camera,
     pub perspective_projection: PerspectiveProjection,
     pub visible_entities: VisibleEntities,
     pub frustum: Frustum,
     pub transform: Transform,
     pub global_transform: GlobalTransform,
+    pub marker: M,
 }
 
-impl PerspectiveCameraBundle {
-    pub fn new_3d() -> Self {
-        Default::default()
+impl Default for PerspectiveCameraBundle<Camera3d> {
+    fn default() -> Self {
+        PerspectiveCameraBundle::new_3d()
     }
+}
 
-    pub fn with_name(name: &str) -> Self {
+impl PerspectiveCameraBundle<Camera3d> {
+    pub fn new_3d() -> Self {
+        PerspectiveCameraBundle::new()
+    }
+}
+
+impl<M: Component + Default> PerspectiveCameraBundle<M> {
+    pub fn new() -> Self {
         let perspective_projection = PerspectiveProjection::default();
         let view_projection = perspective_projection.get_projection_matrix();
         let frustum = Frustum::from_view_projection(
@@ -41,7 +53,6 @@ impl PerspectiveCameraBundle {
         );
         PerspectiveCameraBundle {
             camera: Camera {
-                name: Some(name.to_string()),
                 near: perspective_projection.near,
                 far: perspective_projection.far,
                 ..Default::default()
@@ -51,13 +62,8 @@ impl PerspectiveCameraBundle {
             frustum,
             transform: Default::default(),
             global_transform: Default::default(),
+            marker: M::default(),
         }
-    }
-}
-
-impl Default for PerspectiveCameraBundle {
-    fn default() -> Self {
-        PerspectiveCameraBundle::with_name(CameraPlugin::CAMERA_3D)
     }
 }
 
@@ -65,16 +71,47 @@ impl Default for PerspectiveCameraBundle {
 ///
 /// Use this for 2D games, isometric games, CAD-like 3D views.
 #[derive(Bundle)]
-pub struct OrthographicCameraBundle {
+pub struct OrthographicCameraBundle<M: Component> {
     pub camera: Camera,
     pub orthographic_projection: OrthographicProjection,
     pub visible_entities: VisibleEntities,
     pub frustum: Frustum,
     pub transform: Transform,
     pub global_transform: GlobalTransform,
+    pub marker: M,
 }
 
-impl OrthographicCameraBundle {
+impl OrthographicCameraBundle<Camera3d> {
+    pub fn new_3d() -> Self {
+        let orthographic_projection = OrthographicProjection {
+            scaling_mode: ScalingMode::FixedVertical,
+            depth_calculation: DepthCalculation::Distance,
+            ..Default::default()
+        };
+        let view_projection = orthographic_projection.get_projection_matrix();
+        let frustum = Frustum::from_view_projection(
+            &view_projection,
+            &Vec3::ZERO,
+            &Vec3::Z,
+            orthographic_projection.far(),
+        );
+        OrthographicCameraBundle {
+            camera: Camera {
+                near: orthographic_projection.near,
+                far: orthographic_projection.far,
+                ..Default::default()
+            },
+            orthographic_projection,
+            visible_entities: VisibleEntities::default(),
+            frustum,
+            transform: Default::default(),
+            global_transform: Default::default(),
+            marker: Camera3d,
+        }
+    }
+}
+
+impl OrthographicCameraBundle<Camera2d> {
     /// Create an orthographic projection camera to render 2D content.
     ///
     /// The projection creates a camera space where X points to the right of the screen,
@@ -112,7 +149,6 @@ impl OrthographicCameraBundle {
         );
         OrthographicCameraBundle {
             camera: Camera {
-                name: Some(CameraPlugin::CAMERA_2D.to_string()),
                 near: orthographic_projection.near,
                 far: orthographic_projection.far,
                 ..Default::default()
@@ -122,58 +158,7 @@ impl OrthographicCameraBundle {
             frustum,
             transform,
             global_transform: Default::default(),
-        }
-    }
-
-    pub fn new_3d() -> Self {
-        let orthographic_projection = OrthographicProjection {
-            scaling_mode: ScalingMode::FixedVertical,
-            depth_calculation: DepthCalculation::Distance,
-            ..Default::default()
-        };
-        let view_projection = orthographic_projection.get_projection_matrix();
-        let frustum = Frustum::from_view_projection(
-            &view_projection,
-            &Vec3::ZERO,
-            &Vec3::Z,
-            orthographic_projection.far(),
-        );
-        OrthographicCameraBundle {
-            camera: Camera {
-                name: Some(CameraPlugin::CAMERA_3D.to_string()),
-                near: orthographic_projection.near,
-                far: orthographic_projection.far,
-                ..Default::default()
-            },
-            orthographic_projection,
-            visible_entities: VisibleEntities::default(),
-            frustum,
-            transform: Default::default(),
-            global_transform: Default::default(),
-        }
-    }
-
-    pub fn with_name(name: &str) -> Self {
-        let orthographic_projection = OrthographicProjection::default();
-        let view_projection = orthographic_projection.get_projection_matrix();
-        let frustum = Frustum::from_view_projection(
-            &view_projection,
-            &Vec3::ZERO,
-            &Vec3::Z,
-            orthographic_projection.far(),
-        );
-        OrthographicCameraBundle {
-            camera: Camera {
-                name: Some(name.to_string()),
-                near: orthographic_projection.near,
-                far: orthographic_projection.far,
-                ..Default::default()
-            },
-            orthographic_projection,
-            visible_entities: VisibleEntities::default(),
-            frustum,
-            transform: Default::default(),
-            global_transform: Default::default(),
+            marker: Camera2d,
         }
     }
 }

crates/bevy_render/src/camera/camera.rs
@@ -1,16 +1,23 @@
+use std::marker::PhantomData;
+
 use crate::{
-    camera::CameraProjection, prelude::Image, render_asset::RenderAssets,
-    render_resource::TextureView, view::ExtractedWindows,
+    camera::CameraProjection,
+    prelude::Image,
+    render_asset::RenderAssets,
+    render_resource::TextureView,
+    view::{ExtractedView, ExtractedWindows, VisibleEntities},
+    RenderApp, RenderStage,
 };
+use bevy_app::{App, CoreStage, Plugin, StartupStage};
 use bevy_asset::{AssetEvent, Assets, Handle};
 use bevy_ecs::{
     component::Component,
     entity::Entity,
     event::EventReader,
-    prelude::{DetectChanges, QueryState},
+    prelude::{DetectChanges, QueryState, With},
     query::Added,
     reflect::ReflectComponent,
-    system::{QuerySet, Res},
+    system::{Commands, Query, QuerySet, Res, ResMut},
 };
 use bevy_math::{Mat4, UVec2, Vec2, Vec3};
 use bevy_reflect::{Reflect, ReflectDeserialize};
@@ -24,7 +31,6 @@ use wgpu::Extent3d;
 #[reflect(Component)]
 pub struct Camera {
     pub projection_matrix: Mat4,
-    pub name: Option<String>,
     #[reflect(ignore)]
     pub target: RenderTarget,
     #[reflect(ignore)]
@@ -202,4 +208,112 @@ pub fn camera_system<T: CameraProjection + Component>(
             }
         }
     }
+}
+
+pub struct CameraTypePlugin<T: Component + Default>(PhantomData<T>);
+
+impl<T: Component + Default> Default for CameraTypePlugin<T> {
+    fn default() -> Self {
+        Self(Default::default())
+    }
+}
+
+impl<T: Component + Default> Plugin for CameraTypePlugin<T> {
+    fn build(&self, app: &mut App) {
+        app.init_resource::<ActiveCamera<T>>()
+            .add_startup_system_to_stage(StartupStage::PostStartup, set_active_camera::<T>)
+            .add_system_to_stage(CoreStage::PostUpdate, set_active_camera::<T>);
+        if let Ok(render_app) = app.get_sub_app_mut(RenderApp) {
+            render_app.add_system_to_stage(RenderStage::Extract, extract_cameras::<T>);
+        }
+    }
+}
+
+/// The canonical source of the "active camera" of the given camera type `T`.
+#[derive(Debug)]
+pub struct ActiveCamera<T: Component> {
+    camera: Option<Entity>,
+    marker: PhantomData<T>,
+}
+
+impl<T: Component> Default for ActiveCamera<T> {
+    fn default() -> Self {
+        Self {
+            camera: Default::default(),
+            marker: Default::default(),
+        }
+    }
+}
+
+impl<T: Component> Clone for ActiveCamera<T> {
+    fn clone(&self) -> Self {
+        Self {
+            camera: self.camera,
+            marker: self.marker,
+        }
+    }
+}
+
+impl<T: Component> ActiveCamera<T> {
+    /// Sets the active camera to the given `camera` entity.
+    pub fn set(&mut self, camera: Entity) {
+        self.camera = Some(camera);
+    }
+
+    /// Returns the active camera, if it exists.
+    pub fn get(&self) -> Option<Entity> {
+        self.camera
+    }
+}
+
+pub fn set_active_camera<T: Component>(
+    mut active_camera: ResMut<ActiveCamera<T>>,
+    cameras: Query<Entity, With<T>>,
+) {
+    if active_camera.get().is_some() {
+        return;
+    }
+
+    if let Some(camera) = cameras.iter().next() {
+        active_camera.camera = Some(camera);
+    }
+}
+
+#[derive(Component, Debug)]
+pub struct ExtractedCamera {
+    pub target: RenderTarget,
+    pub physical_size: Option<UVec2>,
+}
+
+pub fn extract_cameras<M: Component + Default>(
+    mut commands: Commands,
+    windows: Res<Windows>,
+    images: Res<Assets<Image>>,
+    active_camera: Res<ActiveCamera<M>>,
+    query: Query<(&Camera, &GlobalTransform, &VisibleEntities), With<M>>,
+) {
+    if let Some(entity) = active_camera.get() {
+        if let Ok((camera, transform, visible_entities)) = query.get(entity) {
+            if let Some(size) = camera.target.get_physical_size(&windows, &images) {
+                commands.get_or_spawn(entity).insert_bundle((
+                    ExtractedCamera {
+                        target: camera.target.clone(),
+                        physical_size: camera.target.get_physical_size(&windows, &images),
+                    },
+                    ExtractedView {
+                        projection: camera.projection_matrix,
+                        transform: *transform,
+                        width: size.x.max(1),
+                        height: size.y.max(1),
+                        near: camera.near,
+                        far: camera.far,
+                    },
+                    visible_entities.clone(),
+                    M::default(),
+                ));
+            }
+        }
+    }
+
+    commands.insert_resource(active_camera.clone())
 }

commit 0ed08d6a15f48e5030fcfdcb5d417a84d315e69b

fix multiple_windows example (#4389)
The example was broken in #3635 when the `ActiveCamera` logic was introduced, after which there could only be one active `Camera3d` globally.
Ideally there could be one `Camera3d` per render target, not globally, but that isn't the case yet.

To fix the example, we need to
- don't use `Camera3d` twice, add a new `SecondWindowCamera3d` marker
- add the `CameraTypePlugin::<SecondWindowCamera3d>`
- extract the correct `RenderPhase`s
- add a 3d pass driver node for the secondary camera

Fixes #4378

Co-authored-by: Jakob Hellermann <hellermann@sipgate.de>

examples/window/multiple_windows.rs
@@ -1,6 +1,13 @@
 use bevy::{
+    core_pipeline::{self, AlphaMask3d, Opaque3d, Transparent3d},
     prelude::*,
-    render::camera::RenderTarget,
+    render::{
+        camera::{ActiveCamera, CameraTypePlugin, RenderTarget},
+        render_graph::{self, NodeRunError, RenderGraph, RenderGraphContext, SlotValue},
+        render_phase::RenderPhase,
+        renderer::RenderContext,
+        RenderApp, RenderStage,
+    },
     window::{CreateWindow, PresentMode, WindowId},
 };
 
@@ -8,10 +15,72 @@ use bevy::{
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .add_plugin(SecondWindowCameraPlugin)
         .add_startup_system(setup)
         .add_startup_system(create_new_window)
         .run();
 }
+
+struct SecondWindowCameraPlugin;
+impl Plugin for SecondWindowCameraPlugin {
+    fn build(&self, app: &mut App) {
+        // adds the `ActiveCamera<SecondWindowCamera3d>` resource and extracts the camera into the render world
+        app.add_plugin(CameraTypePlugin::<SecondWindowCamera3d>::default());
+
+        let render_app = app.sub_app_mut(RenderApp);
+
+        // add `RenderPhase<Opaque3d>`, `RenderPhase<AlphaMask3d>` and `RenderPhase<Transparent3d>` camera phases
+        render_app.add_system_to_stage(RenderStage::Extract, extract_second_camera_phases);
+
+        // add a render graph node that executes the 3d subgraph
+        let mut render_graph = render_app.world.resource_mut::<RenderGraph>();
+        let second_window_node = render_graph.add_node("second_window_cam", SecondWindowDriverNode);
+        render_graph
+            .add_node_edge(
+                core_pipeline::node::MAIN_PASS_DEPENDENCIES,
+                second_window_node,
+            )
+            .unwrap();
+        render_graph
+            .add_node_edge(core_pipeline::node::CLEAR_PASS_DRIVER, second_window_node)
+            .unwrap();
+    }
+}
+
+struct SecondWindowDriverNode;
+impl render_graph::Node for SecondWindowDriverNode {
+    fn run(
+        &self,
+        graph: &mut RenderGraphContext,
+        _: &mut RenderContext,
+        world: &World,
+    ) -> Result<(), NodeRunError> {
+        if let Some(camera) = world.resource::<ActiveCamera<SecondWindowCamera3d>>().get() {
+            graph.run_sub_graph(
+                core_pipeline::draw_3d_graph::NAME,
+                vec![SlotValue::Entity(camera)],
+            )?;
+        }
+
+        Ok(())
+    }
+}
+
+fn extract_second_camera_phases(
+    mut commands: Commands,
+    active: Res<ActiveCamera<SecondWindowCamera3d>>,
+) {
+    if let Some(entity) = active.get() {
+        commands.get_or_spawn(entity).insert_bundle((
+            RenderPhase::<Opaque3d>::default(),
+            RenderPhase::<AlphaMask3d>::default(),
+            RenderPhase::<Transparent3d>::default(),
+        ));
+    }
+}
+
+#[derive(Component, Default)]
+struct SecondWindowCamera3d;
 
 fn create_new_window(mut create_window_events: EventWriter<CreateWindow>, mut commands: Commands) {
     let window_id = WindowId::new();
@@ -35,7 +104,8 @@ fn create_new_window(mut create_window_events: EventWriter<CreateWindow>, mut co
             ..default()
         },
         transform: Transform::from_xyz(6.0, 0.0, 0.0).looking_at(Vec3::ZERO, Vec3::Y),
-        ..default()
+        marker: SecondWindowCamera3d,
+        ..PerspectiveCameraBundle::new()
     });
 }
 