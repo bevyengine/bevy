6880dfc4ed036d68fcdaf49e2fffd7c60d5e5a92
Nathan Pinard <bytemuck@gmail.com>
2022-04-04 20:33:31 -0400

    modified the closure, and added comment thoughts in complex_closure

--- examples/ecs/system_closure.rs
+++ examples/ecs/system_closure.rs
@@ -1,16 +1,19 @@
 use bevy::{log::LogPlugin, prelude::*};
 
 fn main() {
-    // create a non-capturing closure.
-    let non_capturing_closure = || {
-        info!("Hello from a non-capturing closure!");
+    // create a simple closure.
+    let simple_closure = || {
+        info!("Hello from a simple closure!");
     };
 
-    // create a capturing closure, with a Local value.
-    let capturing_closure = |value: String| {
+    // create a closure, with a Local value to be initialized.
+    let complex_closure = |value: String| {
         move |mut arg: Local<String>| {
             *arg = value.clone();
-            info!("Hello from a capturing closure! {:?}", arg);
+            info!("Hello from a complex closure! {:?}", arg);
+
+            // 'arg' will be saved between calls.
+            // you could also use an outside variable like presented in then inlined closures
         }
     };
 
@@ -18,18 +21,19 @@ fn main() {
 
     App::new()
         .add_plugin(LogPlugin)
-        // we can use a non-capturing closure as a system
-        .add_startup_system(non_capturing_closure)
-        // or we can use a capturing closure, and pass an argument to it
-        .add_startup_system(capturing_closure("foo".into()))
-        // we can also inline closure
-        .add_startup_system(|| {
-            info!("Hello from an inlined non-capturing closure!");
+        // we can use a closure as a system
+        .add_system(simple_closure)
+        // or we can use a more complex closure, and pass an argument to initialize a Local variable.
+        .add_system(complex_closure("foo".into()))
+        // we can also inline a closure
+        .add_system(|| {
+            info!("Hello from an inlined closure!");
         })
-        // or use variables outside the closure to initialize, for example, a Local variable
-        .add_startup_system(move |mut arg: Local<String>| {
-            *arg = outside_variable.clone();
-            info!("Hello from an inlined capturing closure! {:?}", arg);
+        // or use variables outside a closure
+        .add_system(move || {
+            info!("Hello from an inlined closure that captured the 'outside_variable'! {:?}", outside_variable);
+            // you can use outside_variable, or any other variables inside this closure.
+            // there states will be saved.
         })
         .run();
 }
