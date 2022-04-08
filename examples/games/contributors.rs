8500fac41456f55a493ed9b3edb35ce487d1f3c1
Rob Parrett <robparrett@gmail.com>
2022-04-08 07:24:37 -0700

    Remove unnecessary ref-deref

--- examples/games/contributors.rs
+++ examples/games/contributors.rs
@@ -185,8 +185,8 @@ fn select_system(
     }
 
     {
-        let entity = &contributor_selection.order[contributor_selection.idx];
-        if let Ok((contributor, mut sprite, mut transform)) = query.get_mut(*entity) {
+        let entity = contributor_selection.order[contributor_selection.idx];
+        if let Ok((contributor, mut sprite, mut transform)) = query.get_mut(entity) {
             deselect(&mut sprite, contributor, &mut *transform);
         }
     }
@@ -197,9 +197,9 @@ fn select_system(
         contributor_selection.idx = 0;
     }
 
-    let entity = &contributor_selection.order[contributor_selection.idx];
+    let entity = contributor_selection.order[contributor_selection.idx];
 
-    if let Ok((contributor, mut sprite, mut transform)) = query.get_mut(*entity) {
+    if let Ok((contributor, mut sprite, mut transform)) = query.get_mut(entity) {
         let mut text = text_query.single_mut();
         select(&mut sprite, contributor, &mut *transform, &mut *text);
     }
