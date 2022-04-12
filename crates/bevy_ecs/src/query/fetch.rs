7034c14a9b94f99aee103a080cbce7acbae9fb71
TheRawMeatball <therawmeatball@gmail.com>
2022-04-12 21:12:02 +0300

    silence warning

--- crates/bevy_ecs/src/query/fetch.rs
+++ crates/bevy_ecs/src/query/fetch.rs
@@ -1644,6 +1644,7 @@ macro_rules! impl_anytuple_fetch {
         }
 
         #[allow(non_snake_case)]
+        #[allow(unused_unit)]
         impl<$($name: WorldQuery),*> WorldQuery for AnyOf<($($name,)*)> {
             // type Fetch = AnyOf<($(($name::Fetch, bool),)*)>;
             // type ReadOnlyFetch = AnyOf<($(($name::ReadOnlyFetch, bool),)*)>;
