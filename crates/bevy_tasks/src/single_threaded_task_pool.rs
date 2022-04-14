fc2b30020c0065d090d3e7ccb6c21d6ca3d9abb5
Michael Hsu <myhsu@benjaminelectric.com>
2022-04-14 10:02:31 -0700

    change Arc to immutable reference

--- crates/bevy_tasks/src/single_threaded_task_pool.rs
+++ crates/bevy_tasks/src/single_threaded_task_pool.rs
@@ -67,9 +67,12 @@ impl TaskPool {
         let executor: &'env async_executor::LocalExecutor<'env> =
             unsafe { mem::transmute(executor) };
 
+        let results: Mutex<Vec<Arc<Mutex<Option<T>>>>> = Mutex::new(Vec::new());
+        let results: &'env Mutex<Vec<Arc<Mutex<Option<T>>>>> = unsafe { mem::transmute(&results) };
+
         let mut scope = Scope {
             executor,
-            results: Arc::new(Mutex::new(Vec::new())),
+            results,
             scope: PhantomData,
         };
 
@@ -126,7 +129,7 @@ impl FakeTask {
 pub struct Scope<'scope, 'env: 'scope, T> {
     executor: &'env async_executor::LocalExecutor<'env>,
     // Vector to gather results of all futures spawned during scope run
-    results: Arc<Mutex<Vec<Arc<Mutex<Option<T>>>>>>,
+    results: &'env Mutex<Vec<Arc<Mutex<Option<T>>>>>,
     scope: PhantomData<&'scope ()>,
 }
 
