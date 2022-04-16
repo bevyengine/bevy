148d5985f839d018133685f0258a8a1d75cbab5d
TheRawMeatball <therawmeatball@gmail.com>
2022-04-16 23:40:48 +0300

    denoise error

--- crates/bevy_ecs/src/query/mod.rs
+++ crates/bevy_ecs/src/query/mod.rs
@@ -239,9 +239,12 @@ mod tests {
         );
 
         let values: HashSet<[&A; 2]> = a_query_without_b.iter_combinations(&world).collect();
+        for val in &values {
+            let &[a, b] = val;
+        }
         assert_eq!(
             values,
-            [[&A(2), &A(3)], [&A(2), &A(4)], [&A(3), &A(4)],]
+            [[&A(2), &A(3)], [&A(2), &A(4)], [&A(3), &A(4)]]
                 .into_iter()
                 .collect::<HashSet<_>>()
         );
