a1c311852da1496623d4726d81a333f361e86fbf
Aevyrie Roessler <aevyrie@gmail.com>
2022-04-17 03:16:18 -0700

    Replace import with qualified path for unbounded channel
    Co-authored-by: Robert Swain <robert.swain@gmail.com>

--- crates/bevy_render/src/view/visibility/mod.rs
+++ crates/bevy_render/src/view/visibility/mod.rs
@@ -1,8 +1,4 @@
 mod render_layers;
-
-use bevy_math::Vec3A;
-use crossbeam_channel::unbounded;
-
 pub use render_layers::*;
 
 use bevy_app::{CoreStage, Plugin};
@@ -166,7 +162,7 @@ pub fn check_visibility(
         visible_entities.entities.clear();
         let view_mask = maybe_view_mask.copied().unwrap_or_default();
 
-        let (visible_entity_sender, visible_entity_receiver) = unbounded();
+        let (visible_entity_sender, visible_entity_receiver) = crossbeam_channel::unbounded();
 
         visible_entity_query.par_for_each_mut(
             &thread_pool,
