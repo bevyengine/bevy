58604962be1055b4fbecbea6a38d636a0460d1ab
PROMETHIA <electriccobras@gmail.com>
2022-04-19 11:01:47 -0400

    Testing impl_reflect_struct

--- crates/bevy_reflect/src/impls/glam.rs
+++ crates/bevy_reflect/src/impls/glam.rs
@@ -1,16 +1,17 @@
 use crate as bevy_reflect;
 use crate::ReflectDeserialize;
-use bevy_reflect_derive::{impl_from_reflect_value, impl_reflect_value};
+use crate::reflect::Reflect;
+use bevy_reflect_derive::{impl_from_reflect_value, impl_reflect_value, impl_reflect_struct};
 use glam::*;
 
-impl_reflect_value!(IVec2(PartialEq, Serialize, Deserialize));
+// impl_reflect_value!(IVec2(PartialEq, Serialize, Deserialize));
 impl_reflect_value!(IVec3(PartialEq, Serialize, Deserialize));
 impl_reflect_value!(IVec4(PartialEq, Serialize, Deserialize));
 impl_reflect_value!(UVec2(PartialEq, Serialize, Deserialize));
 impl_reflect_value!(UVec3(PartialEq, Serialize, Deserialize));
 impl_reflect_value!(UVec4(PartialEq, Serialize, Deserialize));
 impl_reflect_value!(Vec2(PartialEq, Serialize, Deserialize));
-impl_reflect_value!(Vec3(PartialEq, Serialize, Deserialize));
+// impl_reflect_value!(Vec3(PartialEq, Serialize, Deserialize));
 impl_reflect_value!(Vec3A(PartialEq, Serialize, Deserialize));
 impl_reflect_value!(Vec4(PartialEq, Serialize, Deserialize));
 impl_reflect_value!(DVec2(PartialEq, Serialize, Deserialize));
@@ -22,6 +23,27 @@ impl_reflect_value!(Quat(PartialEq, Serialize, Deserialize));
 impl_reflect_value!(DMat3(PartialEq, Serialize, Deserialize));
 impl_reflect_value!(DMat4(PartialEq, Serialize, Deserialize));
 impl_reflect_value!(DQuat(PartialEq, Serialize, Deserialize));
+
+/*
+==========================
+|     Updated impls      |
+==========================
+*/
+
+impl_reflect_struct!(
+    struct IVec2 {
+        x: i32,
+        y: i32
+    }
+);
+
+impl_reflect_struct!(
+    struct Vec3 {
+        x: f32,
+        y: f32,
+        z: f32
+    }
+);
 
 impl_from_reflect_value!(IVec2);
 impl_from_reflect_value!(IVec3);
@@ -42,3 +64,25 @@ impl_from_reflect_value!(Quat);
 impl_from_reflect_value!(DMat3);
 impl_from_reflect_value!(DMat4);
 impl_from_reflect_value!(DQuat);
+
+#[test]
+fn temp_test() {
+    let v = vec3(12.0, 0.0, 0.0);
+
+    let refl: &dyn Reflect = &v;
+
+    assert!(match refl.reflect_ref() {
+        bevy_reflect::ReflectRef::Struct(_) => true,
+        _ => false,
+    });
+
+    assert!(match refl.reflect_ref() {
+        bevy_reflect::ReflectRef::Struct(s) => s.field("x").is_some(),
+        _ => false
+    });
+
+    assert!(match refl.reflect_ref() {
+        bevy_reflect::ReflectRef::Struct(s) => s.field("x").unwrap().downcast_ref::<f32>().unwrap() == &12.0,
+        _ => false
+    });
+}
