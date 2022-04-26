bevyengine/bevy

commit 2abf5cc618957ed6a2acf6a496c0039513746cae

Clustered forward rendering (#3153)
# Objective

Implement clustered-forward rendering.

## Solution

~~FIXME - in the interest of keeping the merge train moving, I'm submitting this PR now before the description is ready. I want to add in some comments into the code with references for the various bits and pieces and I want to describe some of the key decisions I made here. I'll do that as soon as I can.~~ Anyone reviewing is welcome to add review comments where you want to know more about how something or other works.

* The summary of the technique is that the view frustum is divided into a grid of sub-volumes called clusters, point lights are tested against each of the clusters to see if they would affect that volume within the scene and if so, added to a list of lights affecting that cluster. Then when shading a fragment which is a point on the surface of a mesh within the scene, the point is mapped to a cluster and only the lights affecting that clusters are used in lighting calculations. This brings huge performance and scalability benefits as most of the time lights are placed so that there are not that many that overlap each other in terms of their sphere of influence, but there may be many distinct point lights visible in the scene. Doing all the lighting calculations for all visible lights in the scene for every pixel on the screen quickly becomes a performance limitation. Clustered forward rendering allows us to make an approximate list of lights that affect each pixel, indeed each surface in the scene (as it works along the view z axis too, unlike tiled/forward+).

pipelined/bevy_pbr2/Cargo.toml
@@ -24,6 +24,7 @@ bevy_reflect = { path = "../../crates/bevy_reflect", version = "0.5.0", features
 bevy_render2 = { path = "../bevy_render2", version = "0.5.0" }
 bevy_transform = { path = "../../crates/bevy_transform", version = "0.5.0" }
 bevy_utils = { path = "../../crates/bevy_utils", version = "0.5.0" }
+bevy_window = { path = "../../crates/bevy_window", version = "0.5.0" }
 
 # other
 bitflags = "1.2"

pipelined/bevy_core_pipeline/src/main_pass_3d.rs
@@ -59,7 +59,7 @@ impl Node for MainPass3dNode {
             let pass_descriptor = RenderPassDescriptor {
                 label: Some("main_opaque_pass_3d"),
                 // NOTE: The opaque pass clears and initializes the color
-                //       buffer as well as writing to it.
+                // buffer as well as writing to it.
                 color_attachments: &[target.get_color_attachment(Operations {
                     load: LoadOp::Clear(clear_color.0.into()),
                     store: true,
@@ -135,8 +135,8 @@ impl Node for MainPass3dNode {
                 depth_stencil_attachment: Some(RenderPassDepthStencilAttachment {
                     view: &depth.view,
                     // NOTE: For the transparent pass we load the depth buffer but do not write to it.
-                    //       As the opaque and alpha mask passes run first, opaque meshes can occlude
-                    //       transparent ones.
+                    // As the opaque and alpha mask passes run first, opaque meshes can occlude
+                    // transparent ones.
                     depth_ops: Some(Operations {
                         load: LoadOp::Load,
                         store: false,

pipelined/bevy_pbr2/src/lib.rs
@@ -60,6 +60,29 @@ impl Plugin for PbrPlugin {
             .init_resource::<DirectionalLightShadowMap>()
             .init_resource::<PointLightShadowMap>()
             .init_resource::<AmbientLight>()
+            .init_resource::<VisiblePointLights>()
+            .add_system_to_stage(
+                CoreStage::PostUpdate,
+                // NOTE: Clusters need to have been added before update_clusters is run so
+                // add as an exclusive system
+                add_clusters
+                    .exclusive_system()
+                    .label(SimulationLightSystems::AddClusters),
+            )
+            .add_system_to_stage(
+                CoreStage::PostUpdate,
+                // NOTE: Must come after add_clusters!
+                update_clusters
+                    .label(SimulationLightSystems::UpdateClusters)
+                    .after(TransformSystem::TransformPropagate),
+            )
+            .add_system_to_stage(
+                CoreStage::PostUpdate,
+                assign_lights_to_clusters
+                    .label(SimulationLightSystems::AssignLightsToClusters)
+                    .after(TransformSystem::TransformPropagate)
+                    .after(SimulationLightSystems::UpdateClusters),
+            )
             .add_system_to_stage(
                 CoreStage::PostUpdate,
                 update_directional_light_frusta
@@ -70,11 +93,12 @@ impl Plugin for PbrPlugin {
                 CoreStage::PostUpdate,
                 update_point_light_frusta
                     .label(SimulationLightSystems::UpdatePointLightFrusta)
-                    .after(TransformSystem::TransformPropagate),
+                    .after(TransformSystem::TransformPropagate)
+                    .after(SimulationLightSystems::AssignLightsToClusters),
             )
             .add_system_to_stage(
                 CoreStage::PostUpdate,
-                check_light_visibility
+                check_light_mesh_visibility
                     .label(SimulationLightSystems::CheckLightVisibility)
                     .after(TransformSystem::TransformPropagate)
                     .after(VisibilitySystems::CalculateBounds)
@@ -90,6 +114,10 @@ impl Plugin for PbrPlugin {
         render_app
             .add_system_to_stage(
                 RenderStage::Extract,
+                render::extract_clusters.label(RenderLightSystems::ExtractClusters),
+            )
+            .add_system_to_stage(
+                RenderStage::Extract,
                 render::extract_lights.label(RenderLightSystems::ExtractLights),
             )
             .add_system_to_stage(
@@ -99,6 +127,15 @@ impl Plugin for PbrPlugin {
                 render::prepare_lights
                     .exclusive_system()
                     .label(RenderLightSystems::PrepareLights),
+            )
+            .add_system_to_stage(
+                RenderStage::Prepare,
+                // this is added as an exclusive system because it contributes new views. it must run (and have Commands applied)
+                // _before_ the `prepare_views()` system is run. ideally this becomes a normal system when "stageless" features come out
+                render::prepare_clusters
+                    .exclusive_system()
+                    .label(RenderLightSystems::PrepareClusters)
+                    .after(RenderLightSystems::PrepareLights),
             )
             .add_system_to_stage(
                 RenderStage::Queue,
@@ -111,6 +148,7 @@ impl Plugin for PbrPlugin {
             .init_resource::<ShadowPipeline>()
             .init_resource::<DrawFunctions<Shadow>>()
             .init_resource::<LightMeta>()
+            .init_resource::<GlobalLightMeta>()
             .init_resource::<SpecializedPipelines<PbrPipeline>>()
             .init_resource::<SpecializedPipelines<ShadowPipeline>>();
 

pipelined/bevy_pbr2/src/render/mesh.rs
@@ -1,6 +1,6 @@
 use crate::{
-    GpuLights, LightMeta, NotShadowCaster, NotShadowReceiver, ShadowPipeline,
-    ViewLightsUniformOffset, ViewShadowBindings,
+    GlobalLightMeta, GpuLights, LightMeta, NotShadowCaster, NotShadowReceiver, ShadowPipeline,
+    ViewClusterBindings, ViewLightsUniformOffset, ViewShadowBindings,
 };
 use bevy_app::Plugin;
 use bevy_asset::{Assets, Handle, HandleUntyped};
@@ -234,6 +234,47 @@ impl FromWorld for MeshPipeline {
                     ty: BindingType::Sampler {
                         comparison: true,
                         filtering: true,
+                    },
+                    count: None,
+                },
+                // PointLights
+                BindGroupLayoutEntry {
+                    binding: 6,
+                    visibility: ShaderStages::FRAGMENT,
+                    ty: BindingType::Buffer {
+                        ty: BufferBindingType::Uniform,
+                        has_dynamic_offset: false,
+                        // NOTE: Static size for uniform buffers. GpuPointLight has a padded
+                        // size of 128 bytes, so 16384 / 128 = 128 point lights max
+                        min_binding_size: BufferSize::new(16384),
+                    },
+                    count: None,
+                },
+                // ClusteredLightIndexLists
+                BindGroupLayoutEntry {
+                    binding: 7,
+                    visibility: ShaderStages::FRAGMENT,
+                    ty: BindingType::Buffer {
+                        ty: BufferBindingType::Uniform,
+                        has_dynamic_offset: false,
+                        // NOTE: With 128 point lights max, indices need 7 bits. Use u8 for
+                        // convenience.
+                        min_binding_size: BufferSize::new(16384),
+                    },
+                    count: None,
+                },
+                // ClusterOffsetsAndCounts
+                BindGroupLayoutEntry {
+                    binding: 8,
+                    visibility: ShaderStages::FRAGMENT,
+                    ty: BindingType::Buffer {
+                        ty: BufferBindingType::Uniform,
+                        has_dynamic_offset: false,
+                        // NOTE: The offset needs to address 16384 indices, which needs 21 bits.
+                        // The count can be at most all 128 lights so 7 bits.
+                        // Pack the offset into the upper 24 bits and the count into the
+                        // lower 8 bits for convenience.
+                        min_binding_size: BufferSize::new(16384),
                     },
                     count: None,
                 },
@@ -514,20 +555,23 @@ pub struct MeshViewBindGroup {
     pub value: BindGroup,
 }
 
+#[allow(clippy::too_many_arguments)]
 pub fn queue_mesh_view_bind_groups(
     mut commands: Commands,
     render_device: Res<RenderDevice>,
     mesh_pipeline: Res<MeshPipeline>,
     shadow_pipeline: Res<ShadowPipeline>,
     light_meta: Res<LightMeta>,
+    global_light_meta: Res<GlobalLightMeta>,
     view_uniforms: Res<ViewUniforms>,
-    mut views: Query<(Entity, &ViewShadowBindings)>,
+    mut views: Query<(Entity, &ViewShadowBindings, &ViewClusterBindings)>,
 ) {
-    if let (Some(view_binding), Some(light_binding)) = (
+    if let (Some(view_binding), Some(light_binding), Some(point_light_binding)) = (
         view_uniforms.uniforms.binding(),
         light_meta.view_gpu_lights.binding(),
+        global_light_meta.gpu_point_lights.binding(),
     ) {
-        for (entity, view_shadow_bindings) in views.iter_mut() {
+        for (entity, view_shadow_bindings, view_cluster_bindings) in views.iter_mut() {
             let view_bind_group = render_device.create_bind_group(&BindGroupDescriptor {
                 entries: &[
                     BindGroupEntry {
@@ -559,6 +603,24 @@ pub fn queue_mesh_view_bind_groups(
                         resource: BindingResource::Sampler(
                             &shadow_pipeline.directional_light_sampler,
                         ),
+                    },
+                    BindGroupEntry {
+                        binding: 6,
+                        resource: point_light_binding.clone(),
+                    },
+                    BindGroupEntry {
+                        binding: 7,
+                        resource: view_cluster_bindings
+                            .cluster_light_index_lists
+                            .binding()
+                            .unwrap(),
+                    },
+                    BindGroupEntry {
+                        binding: 8,
+                        resource: view_cluster_bindings
+                            .cluster_offsets_and_counts
+                            .binding()
+                            .unwrap(),
                     },
                 ],
                 label: Some("mesh_view_bind_group"),

pipelined/bevy_pbr2/src/render/mesh_view_bind_group.wgsl
@@ -1,18 +1,20 @@
 [[block]]
 struct View {
     view_proj: mat4x4<f32>;
+    inverse_view: mat4x4<f32>;
     projection: mat4x4<f32>;
     world_position: vec3<f32>;
+    near: f32;
+    far: f32;
+    width: f32;
+    height: f32;
 };
 
 struct PointLight {
-    projection: mat4x4<f32>;
-    color: vec4<f32>;
-    position: vec3<f32>;
-    inverse_square_range: f32;
-    radius: f32;
-    near: f32;
-    far: f32;
+    // NOTE: [2][2] [2][3] [3][2] [3][3]
+    projection_lr: vec4<f32>;
+    color_inverse_square_range: vec4<f32>;
+    position_radius: vec4<f32>;
     // 'flags' is a bit field indicating various options. u32 is 32 bits so we have up to 32 options.
     flags: u32;
     shadow_depth_bias: f32;
@@ -36,12 +38,33 @@ let DIRECTIONAL_LIGHT_FLAGS_SHADOWS_ENABLED_BIT: u32 = 1u;
 [[block]]
 struct Lights {
     // NOTE: this array size must be kept in sync with the constants defined bevy_pbr2/src/render/light.rs
-    // TODO: this can be removed if we move to storage buffers for light arrays
-    point_lights: array<PointLight, 10>;
-    directional_lights: array<DirectionalLight, 1>;
+    directional_lights: array<DirectionalLight, 1u>;
     ambient_color: vec4<f32>;
-    n_point_lights: u32;
+    // x/y/z dimensions
+    cluster_dimensions: vec4<u32>;
+    // xy are vec2<f32>(cluster_dimensions.xy) / vec2<f32>(view.width, view.height)
+    // z is cluster_dimensions.z / log(far / near)
+    // w is cluster_dimensions.z * log(near) / log(far / near)
+    cluster_factors: vec4<f32>;
     n_directional_lights: u32;
+};
+
+[[block]]
+struct PointLights {
+    data: array<PointLight, 256u>;
+};
+
+[[block]]
+struct ClusterLightIndexLists {
+    // each u32 contains 4 u8 indices into the PointLights array
+    data: array<vec4<u32>, 1024u>;
+};
+
+[[block]]
+struct ClusterOffsetsAndCounts {
+    // each u32 contains a 24-bit index into ClusterLightIndexLists in the high 24 bits
+    // and an 8-bit count of the number of lights in the low 8 bits
+    data: array<vec4<u32>, 1024u>;
 };
 
 [[group(0), binding(0)]]
@@ -56,3 +79,9 @@ var point_shadow_textures_sampler: sampler_comparison;
 var directional_shadow_textures: texture_depth_2d_array;
 [[group(0), binding(5)]]
 var directional_shadow_textures_sampler: sampler_comparison;
+[[group(0), binding(6)]]
+var<uniform> point_lights: PointLights;
+[[group(0), binding(7)]]
+var<uniform> cluster_light_index_lists: ClusterLightIndexLists;
+[[group(0), binding(8)]]
+var<uniform> cluster_offsets_and_counts: ClusterOffsetsAndCounts;

pipelined/bevy_pbr2/src/render/mod.rs
@@ -249,7 +249,7 @@ pub fn queue_meshes(
 
         for visible_entity in &visible_entities.entities {
             if let Ok((material_handle, mesh_handle, mesh_uniform)) =
-                standard_material_meshes.get(visible_entity.entity)
+                standard_material_meshes.get(*visible_entity)
             {
                 if let Some(material) = render_materials.get(material_handle) {
                     let mut pbr_key = PbrPipelineKey {
@@ -275,37 +275,37 @@ pub fn queue_meshes(
                     match material.alpha_mode {
                         AlphaMode::Opaque => {
                             opaque_phase.add(Opaque3d {
-                                entity: visible_entity.entity,
+                                entity: *visible_entity,
                                 draw_function: draw_opaque_pbr,
                                 pipeline: pipeline_id,
                                 // NOTE: Front-to-back ordering for opaque with ascending sort means near should have the
-                                //       lowest sort key and getting further away should increase. As we have
-                                //       -z in front of the camera, values in view space decrease away from the
-                                //       camera. Flipping the sign of mesh_z results in the correct front-to-back ordering
+                                // lowest sort key and getting further away should increase. As we have
+                                // -z in front of the camera, values in view space decrease away from the
+                                // camera. Flipping the sign of mesh_z results in the correct front-to-back ordering
                                 distance: -mesh_z,
                             });
                         }
                         AlphaMode::Mask(_) => {
                             alpha_mask_phase.add(AlphaMask3d {
-                                entity: visible_entity.entity,
+                                entity: *visible_entity,
                                 draw_function: draw_alpha_mask_pbr,
                                 pipeline: pipeline_id,
                                 // NOTE: Front-to-back ordering for alpha mask with ascending sort means near should have the
-                                //       lowest sort key and getting further away should increase. As we have
-                                //       -z in front of the camera, values in view space decrease away from the
-                                //       camera. Flipping the sign of mesh_z results in the correct front-to-back ordering
+                                // lowest sort key and getting further away should increase. As we have
+                                // -z in front of the camera, values in view space decrease away from the
+                                // camera. Flipping the sign of mesh_z results in the correct front-to-back ordering
                                 distance: -mesh_z,
                             });
                         }
                         AlphaMode::Blend => {
                             transparent_phase.add(Transparent3d {
-                                entity: visible_entity.entity,
+                                entity: *visible_entity,
                                 draw_function: draw_transparent_pbr,
                                 pipeline: pipeline_id,
                                 // NOTE: Back-to-front ordering for transparent with ascending sort means far should have the
-                                //       lowest sort key and getting closer should increase. As we have
-                                //       -z in front of the camera, the largest distance is -far with values increasing toward the
-                                //       camera. As such we can just use mesh_z as the distance
+                                // lowest sort key and getting closer should increase. As we have
+                                // -z in front of the camera, the largest distance is -far with values increasing toward the
+                                // camera. As such we can just use mesh_z as the distance
                                 distance: mesh_z,
                             });
                         }

commit 328c26d02c50de0bc77f0d24a376f43ba89517b1

Add an example to test small window sizes (#3597)
# Objective

We keep getting issues where things break at small window sizes, e.g #3368 (caused by #3153), #3596 ('caused' by #3545)

## Solution

- Add a test that we can make small windows.


Currently, this fails on my machine with some quite scary vulkan errors: 
```
2022-01-08T22:55:13.770261Z ERROR wgpu_hal::vulkan::instance: VALIDATION [VUID-VkSwapchainCreateInfoKHR-imageExtent-01274 (0x7cd0911d)]
        Validation Error: [ VUID-VkSwapchainCreateInfoKHR-imageExtent-01274 ] Object 0: handle = 0x1adbd410a60, type = VK_OBJECT_TYPE_DEVICE; | MessageID = 0x7cd0911d | vkCreateSwapchainKHR() called with imageExtent = (225,60), which is outside the bounds returned by vkGetPhysicalDeviceSurfaceCapabilitiesKHR(): currentExtent = (225,56), minImageExtent = (225,56), maxImageExtent = (225,56). The Vulkan spec states: imageExtent must be between minImageExtent and maxImageExtent, inclusive, where minImageExtent and maxImageExtent are members of the VkSurfaceCapabilitiesKHR structure returned by vkGetPhysicalDeviceSurfaceCapabilitiesKHR for the surface (https://vulkan.lunarg.com/doc/view/1.2.198.1/windows/1.2-extensions/vkspec.html#VUID-VkSwapchainCreateInfoKHR-imageExtent-01274)
2022-01-08T22:55:13.770808Z ERROR wgpu_hal::vulkan::instance:   objects: (type: DEVICE, hndl: 0x1adbd410a60, name: ?)
2022-01-08T22:55:13.787403Z ERROR wgpu_hal::vulkan::instance: VALIDATION [VUID-VkSwapchainCreateInfoKHR-imageExtent-01274 (0x7cd0911d)]

.github/example-run/minimising.ron
@@ -1,0 +1,3 @@
+(
+    exit_after: Some(90)
+)

.github/example-run/resizing.ron
@@ -1,0 +1,4 @@
+(
+    // Ensures that the full cycle will run
+    exit_after: Some(410)
+)

Cargo.toml
@@ -615,6 +615,15 @@ path = "examples/window/transparent_window.rs"
 name = "window_settings"
 path = "examples/window/window_settings.rs"
 
+[[example]]
+name = "resizing"
+path = "tests/window/resizing.rs"
+
+[[example]]
+name = "minimising"
+path = "tests/window/minimising.rs"
+
+
 # Android
 [[example]]
 crate-type = ["cdylib"]

tests/window/minimising.rs
@@ -1,0 +1,75 @@
+//! A test to confirm that `bevy` allows minimising the window
+//! This is run in CI to ensure that this doesn't regress again.
+use bevy::prelude::*;
+
+fn main() {
+    // TODO: Combine this with `resizing` once multiple_windows is simpler than
+    // it is currently.
+    App::new()
+        .insert_resource(WindowDescriptor {
+            title: "Minimising".into(),
+            ..Default::default()
+        })
+        .add_plugins(DefaultPlugins)
+        .add_system(minimise_automatically)
+        .add_startup_system(setup_3d)
+        .add_startup_system(setup_2d)
+        .run();
+}
+
+fn minimise_automatically(mut windows: ResMut<Windows>, mut frames: Local<u32>) {
+    if *frames == 60 {
+        windows.get_primary_mut().unwrap().set_minimized(true);
+    } else {
+        *frames += 1;
+    }
+}
+
+/// A simple 3d scene, taken from the `3d_scene` example
+fn setup_3d(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    // plane
+    commands.spawn_bundle(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Plane { size: 5.0 })),
+        material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
+        ..default()
+    });
+    // cube
+    commands.spawn_bundle(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
+        material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
+        transform: Transform::from_xyz(0.0, 0.5, 0.0),
+        ..default()
+    });
+    // light
+    commands.spawn_bundle(PointLightBundle {
+        point_light: PointLight {
+            intensity: 1500.0,
+            shadows_enabled: true,
+            ..default()
+        },
+        transform: Transform::from_xyz(4.0, 8.0, 4.0),
+        ..default()
+    });
+    // camera
+    commands.spawn_bundle(PerspectiveCameraBundle {
+        transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+        ..default()
+    });
+}
+
+/// A simple 2d scene, taken from the `rect` example
+fn setup_2d(mut commands: Commands) {
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+    commands.spawn_bundle(SpriteBundle {
+        sprite: Sprite {
+            color: Color::rgb(0.25, 0.25, 0.75),
+            custom_size: Some(Vec2::new(50.0, 50.0)),
+            ..default()
+        },
+        ..default()
+    });
+}

tests/window/resizing.rs
@@ -1,0 +1,153 @@
+//! A test to confirm that `bevy` allows setting the window to arbitrary small sizes
+//! This is run in CI to ensure that this doesn't regress again.
+
+use bevy::{input::system::exit_on_esc_system, prelude::*};
+
+// The smallest size reached is 1x1, as X11 doesn't support windows with a 0 dimension
+// TODO: Add a check for platforms other than X11 for 0xk and kx0, despite those currently unsupported on CI.
+const MAX_WIDTH: u16 = 401;
+const MAX_HEIGHT: u16 = 401;
+const MIN_WIDTH: u16 = 1;
+const MIN_HEIGHT: u16 = 1;
+const RESIZE_STEP: u16 = 4;
+
+struct Dimensions {
+    width: u16,
+    height: u16,
+}
+
+fn main() {
+    App::new()
+        .insert_resource(WindowDescriptor {
+            width: MAX_WIDTH.try_into().unwrap(),
+            height: MAX_HEIGHT.try_into().unwrap(),
+            scale_factor_override: Some(1.),
+            title: "Resizing".into(),
+            ..Default::default()
+        })
+        .insert_resource(Dimensions {
+            width: MAX_WIDTH,
+            height: MAX_HEIGHT,
+        })
+        .add_plugins(DefaultPlugins)
+        .insert_resource(Phase::ContractingY)
+        .add_system(change_window_size)
+        .add_system(sync_dimensions)
+        .add_system(exit_on_esc_system)
+        .add_startup_system(setup_3d)
+        .add_startup_system(setup_2d)
+        .run();
+}
+
+enum Phase {
+    ContractingY,
+    ContractingX,
+    ExpandingY,
+    ExpandingX,
+}
+
+use Phase::*;
+
+fn change_window_size(
+    mut windows: ResMut<Dimensions>,
+    mut phase: ResMut<Phase>,
+    mut first_complete: Local<bool>,
+) {
+    // Put off rendering for one frame, as currently for a frame where
+    // resizing happens, nothing is presented.
+    // TODO: Debug and fix this if feasible
+    if !*first_complete {
+        *first_complete = true;
+        return;
+    }
+    let height = windows.height;
+    let width = windows.width;
+    match *phase {
+        Phase::ContractingY => {
+            if height <= MIN_HEIGHT {
+                *phase = ContractingX;
+            } else {
+                windows.height -= RESIZE_STEP;
+            }
+        }
+        Phase::ContractingX => {
+            if width <= MIN_WIDTH {
+                *phase = ExpandingY;
+            } else {
+                windows.width -= RESIZE_STEP;
+            }
+        }
+        Phase::ExpandingY => {
+            if height >= MAX_HEIGHT {
+                *phase = ExpandingX;
+            } else {
+                windows.height += RESIZE_STEP;
+            }
+        }
+        Phase::ExpandingX => {
+            if width >= MAX_WIDTH {
+                *phase = ContractingY;
+            } else {
+                windows.width += RESIZE_STEP;
+            }
+        }
+    }
+}
+
+fn sync_dimensions(dim: Res<Dimensions>, mut windows: ResMut<Windows>) {
+    if dim.is_changed() {
+        windows.get_primary_mut().unwrap().set_resolution(
+            dim.width.try_into().unwrap(),
+            dim.height.try_into().unwrap(),
+        );
+    }
+}
+
+/// A simple 3d scene, taken from the `3d_scene` example
+fn setup_3d(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    // plane
+    commands.spawn_bundle(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Plane { size: 5.0 })),
+        material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
+        ..default()
+    });
+    // cube
+    commands.spawn_bundle(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
+        material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
+        transform: Transform::from_xyz(0.0, 0.5, 0.0),
+        ..default()
+    });
+    // light
+    commands.spawn_bundle(PointLightBundle {
+        point_light: PointLight {
+            intensity: 1500.0,
+            shadows_enabled: true,
+            ..default()
+        },
+        transform: Transform::from_xyz(4.0, 8.0, 4.0),
+        ..default()
+    });
+    // camera
+    commands.spawn_bundle(PerspectiveCameraBundle {
+        transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+        ..default()
+    });
+}
+
+/// A simple 2d scene, taken from the `rect` example
+fn setup_2d(mut commands: Commands) {
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+    commands.spawn_bundle(SpriteBundle {
+        sprite: Sprite {
+            color: Color::rgb(0.25, 0.25, 0.75),
+            custom_size: Some(Vec2::new(50.0, 50.0)),
+            ..default()
+        },
+        ..default()
+    });
+}