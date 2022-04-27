be509bda422048357953eaad37da4d06d2293fff
Rob Parrett <robparrett@gmail.com>
2022-04-27 12:41:20 -0700

    Ditch the texture

--- examples/3d/shapes.rs
+++ examples/3d/shapes.rs
@@ -1,4 +1,7 @@
-use bevy::prelude::*;
+use bevy::{
+    prelude::*,
+    render::render_resource::{Extent3d, TextureDimension, TextureFormat},
+};
 
 fn main() {
     App::new()
@@ -17,11 +20,11 @@ const X_EXTENT: f32 = 14.;
 fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
+    mut images: ResMut<Assets<Image>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
-    asset_server: ResMut<AssetServer>,
 ) {
     let debug_material = materials.add(StandardMaterial {
-        base_color_texture: Some(asset_server.load("textures/uv_debug.png")),
+        base_color_texture: Some(images.add(uv_debug_texture())),
         ..default()
     });
 
@@ -83,4 +86,34 @@ fn rotate(mut query: Query<&mut Transform, With<Shape>>, time: Res<Time>) {
         transform.rotation = Quat::from_rotation_y(time.seconds_since_startup() as f32 / 2.)
             * Quat::from_rotation_x(-std::f32::consts::PI / 4.)
     }
+}
+
+/// Creates a colorful test pattern
+fn uv_debug_texture() -> Image {
+    const TEXTURE_SIZE: usize = 8;
+
+    let mut palette: [u8; 32] = [
+        255, 102, 159, 255, 255, 159, 102, 255, 236, 255, 102, 255, 121, 255, 102, 255, 102, 255,
+        198, 255, 102, 198, 255, 255, 121, 102, 255, 255, 236, 102, 255, 255,
+    ];
+
+    let mut texture_data = [0; TEXTURE_SIZE * TEXTURE_SIZE * 4];
+    for y in 0..TEXTURE_SIZE {
+        let offset = TEXTURE_SIZE * y * 4;
+        texture_data[offset..(offset + TEXTURE_SIZE * 4)].copy_from_slice(&palette);
+        palette.rotate_right(4);
+    }
+
+    let texture = Image::new_fill(
+        Extent3d {
+            width: TEXTURE_SIZE as u32,
+            height: TEXTURE_SIZE as u32,
+            depth_or_array_layers: 1,
+        },
+        TextureDimension::D2,
+        &texture_data,
+        TextureFormat::Rgba8UnormSrgb,
+    );
+
+    texture
 }
