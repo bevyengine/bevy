f30c6954624a84a243213d884d187013eba224a0
Yutao Yuan <infmagic2047reg@outlook.com>
2022-05-01 17:22:32 +0800

    Fix type parameter name conflicts of derive(Bundle)

--- crates/bevy_ecs/macros/src/lib.rs
+++ crates/bevy_ecs/macros/src/lib.rs
@@ -156,9 +156,9 @@ pub fn derive_bundle(input: TokenStream) -> TokenStream {
             }
 
             #[allow(unused_variables, unused_mut, non_snake_case)]
-            unsafe fn from_components<T, F>(ctx: &mut T, mut func: F) -> Self
+            unsafe fn from_components<__T, __F>(ctx: &mut __T, mut func: __F) -> Self
             where
-                F: FnMut(&mut T) -> #ecs_path::ptr::OwningPtr<'_>
+                __F: FnMut(&mut __T) -> #ecs_path::ptr::OwningPtr<'_>
             {
                 Self {
                     #(#field_from_components)*
