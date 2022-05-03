7ca88019ede0aba177a82bebcf1f14d6bcd33407
Jakob Hellermann <jakob.hellermann@protonmail.com>
2022-05-03 10:54:38 +0200

    add doc comment

--- crates/bevy_reflect/src/std_traits.rs
+++ crates/bevy_reflect/src/std_traits.rs
@@ -1,5 +1,8 @@
 use crate::{FromType, Reflect};
 
+/// A struct used to provide the default value of a type.
+///
+/// A [`ReflectDefault`] for type `T` can be obtained via [`FromType::from_type`].
 #[derive(Clone)]
 pub struct ReflectDefault {
     default: fn() -> Box<dyn Reflect>,
