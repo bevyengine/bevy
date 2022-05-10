9692e7a5a65f8db5793c1f9ef9ed2c76290d7c31
james7132 <contact@jamessliu.com>
2022-05-09 22:00:22 -0700

    Make it a child span

--- crates/bevy_ecs/src/query/state.rs
+++ crates/bevy_ecs/src/query/state.rs
@@ -847,7 +847,7 @@ impl<Q: WorldQuery, F: WorldQuery> QueryState<Q, F> {
         change_tick: u32,
     ) {
         #[cfg(feature = "trace")]
-        let span = bevy_utils::tracing::span::Span::current();
+        let span = bevy_utils::info_span!("par_for_each");
         // NOTE: If you are changing query iteration code, remember to update the following places, where relevant:
         // QueryIter, QueryIterationCursor, QueryState::for_each_unchecked_manual, QueryState::par_for_each_unchecked_manual
         task_pool.scope(|scope| {
