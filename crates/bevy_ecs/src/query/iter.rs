27d1734eb11af50ccccec863b5da8e66d31c1abe
james7132 <contact@jamessliu.com>
2022-05-12 13:09:07 -0700

    Mention ID alignment

--- crates/bevy_ecs/src/query/iter.rs
+++ crates/bevy_ecs/src/query/iter.rs
@@ -316,8 +316,8 @@ where
         QueryIterationCursor {
             fetch,
             filter,
-            // SAFE: The associated IDs are exactly the same size and the iterator
-            // has identical layouts and lifetimes.
+            // SAFE: The associated IDs are exactly the same size and alignment and 
+            // the iterator has identical layouts and lifetimes.
             id_iter: if Self::IS_DENSE {
                 std::mem::transmute(query_state.matched_table_ids.iter())
             } else {
