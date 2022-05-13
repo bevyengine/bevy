e35e11a6cfc3cf936843e4cd6224813da122750b
james7132 <contact@jamessliu.com>
2022-05-13 13:53:50 -0700

    Fix local tests for bevy_task

--- crates/bevy_tasks/examples/busy_behavior.rs
+++ crates/bevy_tasks/examples/busy_behavior.rs
@@ -1,13 +1,13 @@
 use bevy_tasks::TaskPoolBuilder;
 
-// This sample demonstrates creating a thread pool with 4 tasks and spawning 40 tasks that spin
-// for 100ms. It's expected to take about a second to run (assuming the machine has >= 4 logical
+// This sample demonstrates creating a thread pool with 4 compute threads and spawning 40 tasks that 
+// spin for 100ms. It's expected to take about a second to run (assuming the machine has >= 4 logical
 // cores)
 
 fn main() {
     let pool = TaskPoolBuilder::new()
         .thread_name("Busy Behavior ThreadPool".to_string())
-        .num_threads(4)
+        .compute_threads(4)
         .build();
 
     let t0 = instant::Instant::now();
