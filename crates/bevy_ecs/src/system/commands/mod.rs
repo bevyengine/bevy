858a96c882c0c1f94c62ad8b8098beb24e67ac61
jamescarterbell <38409055+jamescarterbell@users.noreply.github.com>
2022-05-17 13:42:37 -0400

    Update crates/bevy_ecs/src/system/commands/mod.rs
    Co-authored-by: Mike <mike.hsu@gmail.com>

--- crates/bevy_ecs/src/system/commands/mod.rs
+++ crates/bevy_ecs/src/system/commands/mod.rs
@@ -847,7 +847,7 @@ pub trait BoxableCommand {
 
 impl<T: Command> BoxableCommand for T {
     fn box_command(self) -> BoxedCommand {
-        /// SAFE: This function is only every called when the `command` bytes is the associated
+        /// SAFE: This function is only ever called when the `command` bytes is the associated
         /// [`Commands`] `T` type. Also this only reads the data via `read_unaligned` so unaligned
         /// accesses are safe.
         unsafe fn write_command<T: Command>(command: *mut u8, world: &mut World) {
