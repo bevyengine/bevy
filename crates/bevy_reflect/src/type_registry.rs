43dda0afa22c4c23210ecd5f5760fd66e7fac377
Gino Valente <gino.valente.code@gmail.com>
2022-05-18 16:59:53 -0700

    Remove unused import

--- crates/bevy_reflect/src/type_registry.rs
+++ crates/bevy_reflect/src/type_registry.rs
@@ -2,7 +2,6 @@ use crate::{Reflect, TypeInfo, Typed};
 use bevy_utils::{HashMap, HashSet};
 use downcast_rs::{impl_downcast, Downcast};
 use parking_lot::{RwLock, RwLockReadGuard, RwLockWriteGuard};
-use serde::de::Error;
 use serde::Deserialize;
 use std::{any::TypeId, fmt::Debug, sync::Arc};
 
