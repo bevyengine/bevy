2d15c42febe5082fe4a0a87e7a754d9d0959d6fc
Herbert Wolverson <herberticus@gmail.com>
2022-05-20 08:06:05 -0500

    Update example: the header mentions the vertex colors, use named colors rather than inscrutable f32 arrays.

--- examples/2d/mesh2d_vertex_color_texture.rs
+++ examples/2d/mesh2d_vertex_color_texture.rs
@@ -1,4 +1,5 @@
 //! Shows how to render a polygonal [`Mesh`], generated from a [`Quad`] primitive, in a 2D scene.
+//! Adds a texture and colored vertices, giving per-vertex tinting.
 
 use bevy::{prelude::*, sprite::MaterialMesh2dBundle};
 
@@ -19,12 +20,12 @@ fn setup(
     let texture_handle = asset_server.load("branding/banner.png");
     // Build a default quad mesh
     let mut mesh = Mesh::from(shape::Quad::default());
-    // Build vertex colors for the quad
+    // Build vertex colors for the quad. One entry per vertex (the corners of the quad)
     let vertex_colors: Vec<[f32; 4]> = vec![
-        [1.0, 0.0, 0.0, 1.0],
-        [0.0, 1.0, 0.0, 1.0],
-        [0.0, 0.0, 1.0, 1.0],
-        [1.0, 1.0, 1.0, 1.0],
+        Color::RED.as_rgba_f32(),
+        Color::GREEN.as_rgba_f32(),
+        Color::BLUE.as_rgba_f32(),
+        Color::WHITE.as_rgba_f32(),
     ];
     // Insert the vertex colors as an attribute
     mesh.insert_attribute(Mesh::ATTRIBUTE_COLOR, vertex_colors);
