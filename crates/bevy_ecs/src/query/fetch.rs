9ffb07c65166b8319f3deae139373ed415afbbfb
james7132 <contact@jamessliu.com>
2022-05-21 16:26:53 -0700

    Fix docs

--- crates/bevy_ecs/src/query/fetch.rs
+++ crates/bevy_ecs/src/query/fetch.rs
@@ -54,7 +54,7 @@ use std::{cell::UnsafeCell, marker::PhantomData};
 ///
 /// The derive macro implements [`WorldQuery`] for your type and declares an additional struct
 /// which will be used as an item for query iterators. The implementation also generates two other
-/// structs that implement [`Fetch`] and [`FetchState`] and are used as [`WorldQuery::Fetch`](WorldQueryGats::Fetch) and
+/// structs that implement [`Fetch`] and are used as [`WorldQuery::Fetch`](WorldQueryGats::Fetch) and
 /// [`WorldQuery::State`] associated types respectively.
 ///
 /// The derive macro requires every struct field to implement the `WorldQuery` trait.
@@ -328,7 +328,7 @@ pub trait Fetch<'world>: Sized {
     ///
     /// # Safety
     ///
-    /// `state` must have been initialized (via [`FetchState::init`]) using the same `world` passed
+    /// `state` must have been initialized (via [`Fetch::init_state`]) using the same `world` passed
     /// in to this function.
     unsafe fn init(
         world: &'world World,
