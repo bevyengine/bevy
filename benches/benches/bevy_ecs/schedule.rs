6301fd5567711f8bb707aaa142823842b70000a3
JoJoJet <joe102000@gmail.com>
2022-06-07 22:24:02 -0400

    move initialization code outside of the benchmark

--- benches/benches/bevy_ecs/schedule.rs
+++ benches/benches/bevy_ecs/schedule.rs
@@ -6,9 +6,24 @@ criterion_group!(benches, build_schedule);
 criterion_main!(benches);
 
 fn build_schedule(criterion: &mut Criterion) {
+    // empty system
+    fn sys() {}
+
+    // Use multiple different kinds of label to ensure that dynamic dispatch
+    // doesn't somehow get optimized away.
+    #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, SystemLabel)]
+    struct NumLabel(usize);
+    #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, SystemLabel)]
+    struct DummyLabel;
+
     let mut group = criterion.benchmark_group("build_schedule");
     group.warm_up_time(std::time::Duration::from_millis(500));
     group.measurement_time(std::time::Duration::from_secs(15));
+
+    // Method: generate a set of `graph_size` systems which have a One True Ordering.
+    // Add system to the stage with full constraints. Hopefully this should be maximimally
+    // difficult for bevy to figure out.
+    let labels: Vec<_> = (0..1000).map(NumLabel).collect();
 
     // Benchmark graphs of different sizes.
     for graph_size in [100, 500, 1000] {
@@ -17,52 +32,24 @@ fn build_schedule(criterion: &mut Criterion) {
             bencher.iter(|| {
                 let mut app = App::new();
                 for _ in 0..graph_size {
-                    // empty system
-                    fn sys() {}
                     app.add_system(sys);
                 }
+                app.run();
             });
         });
+
         // Benchmark with constraints.
         group.bench_function(format!("{graph_size}_schedule"), |bencher| {
             bencher.iter(|| {
-                // empty system
-                fn sys() {}
-
-                // Use multiple different kinds of label;
-                #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, SystemLabel)]
-                struct EvenLabel(usize);
-                #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, SystemLabel)]
-                struct OddLabel(usize);
-
-                // unique label
-                #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, SystemLabel)]
-                struct NumLabel(usize);
-
-                // Method: generate a set of `graph_size` systems which have a One True Ordering.
-                // Add system to the stage with full constraints. Hopefully this should be maximimally
-                // difficult for bevy to figure out.
-
                 let mut app = App::new();
+                app.add_system(sys.label(DummyLabel));
                 for i in 0..graph_size {
-                    let mut sys = if i % 2 == 0 {
-                        sys.label(EvenLabel(i))
-                    } else {
-                        sys.label(OddLabel(i))
-                    };
+                    let mut sys = sys.label(labels[i]).before(DummyLabel);
                     for a in 0..i {
-                        sys = if a % 2 == 0 {
-                            sys.after(EvenLabel(a))
-                        } else {
-                            sys.after(OddLabel(a))
-                        }
+                        sys = sys.after(labels[a]);
                     }
                     for b in i + 1..graph_size {
-                        sys = if b % 2 == 0 {
-                            sys.before(EvenLabel(b))
-                        } else {
-                            sys.before(OddLabel(b))
-                        }
+                        sys = sys.before(labels[b]);
                     }
                     app.add_system(sys);
                 }
