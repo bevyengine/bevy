5c22e3429a00e328d0329cf8aac6dd676f505489
Your Name <you@example.com>
2022-06-12 22:46:04 +0200

    Rewrite the test with assertion system function logic

--- tests/input.rs
+++ tests/input.rs
@@ -1,48 +1,38 @@
+use std::{
+    collections::VecDeque,
+    sync::{Arc, Mutex},
+};
+
 use bevy::{
     ecs::{
         event::{Event, Events},
-        query::{Fetch, WorldQuery, WorldQueryGats},
     },
     input::InputPlugin,
     input::{keyboard::KeyboardInput, ButtonState},
-    prelude::{App, Entity},
-    prelude::{Commands, Component, EventReader, KeyCode},
-    utils::HashSet,
+    prelude::{App},
+    prelude::{EventReader, KeyCode},
     window::{ReceivedCharacter, WindowId, WindowPlugin},
 };
 
-#[derive(Component)]
-struct CharComponent(char);
-fn spawn_entity_at_char_event_system(
-    mut commands: Commands,
-    mut char_input_events: EventReader<ReceivedCharacter>,
-) {
-    for event in char_input_events.iter() {
-        commands.spawn().insert(CharComponent(event.char));
-    }
-}
-
-#[derive(Component)]
-struct KeyComponent(KeyboardInput);
-fn spawn_entity_at_keyboard_event_system(
-    mut commands: Commands,
-    mut char_input_events: EventReader<KeyboardInput>,
-) {
-    for event in char_input_events.iter() {
-        commands.spawn().insert(KeyComponent(event.clone()));
-    }
-}
-
 #[test]
-fn test_input_received_character_single_button() {
+fn test_input_received_character() {
+    let inputs = Arc::new(Mutex::new(VecDeque::from(['a', 'B'])));
+    let inputs2 = inputs.clone();
+    let system = move |mut chat_input_events: EventReader<ReceivedCharacter>| {
+        for event in chat_input_events.iter() {
+            assert_eq!(
+                event.char,
+                inputs2.lock().unwrap().pop_front().expect("foo")
+            );
+        }
+    };
+
     let mut app = App::new();
-    app.add_plugin(WindowPlugin::default())
-        .add_system(spawn_entity_at_char_event_system);
+    app.add_plugin(WindowPlugin::default()).add_system(system);
 
+    // If no buttons are pressed, the iterator inside the system function is empty
     app.update();
-
-    let entities: Vec<Entity> = get_entities(&mut app);
-    assert_eq!(entities.len(), 0);
+    assert_eq!(inputs.lock().unwrap().len(), 2);
 
     send_event(
         &mut app,
@@ -52,13 +42,14 @@ fn test_input_received_character_single_button() {
         },
     );
 
+    // If a button is clicked, the iterator inside the system function returns one item
     app.update();
+    assert_eq!(inputs.lock().unwrap().len(), 1);
 
-    let chars: HashSet<_> = get_entities::<(Entity, &CharComponent)>(&mut app)
-        .into_iter()
-        .map(|(_, c)| c.0)
-        .collect();
-    assert_eq!(chars, HashSet::<_>::from_iter(['a'].into_iter()));
+    // If no buttons are pressed, the past event is forgot
+    // And in this iteration no events are received
+    app.update();
+    assert_eq!(inputs.lock().unwrap().len(), 1);
 
     send_event(
         &mut app,
@@ -68,57 +59,41 @@ fn test_input_received_character_single_button() {
         },
     );
 
+    // Another button is clicked, the vec is empty
     app.update();
-
-    let entities: Vec<(Entity, &CharComponent)> = get_entities(&mut app);
-    let chars: HashSet<_> = entities.into_iter().map(|(_, c)| c.0).collect();
-    assert_eq!(chars, HashSet::<_>::from_iter(['a', 'B'].into_iter()));
+    assert!(inputs.lock().unwrap().is_empty());
 }
 
 #[test]
-fn test_input_received_character_multiple_buttons_at_once() {
-    let mut app = App::new();
-    app.add_plugin(WindowPlugin::default())
-        .add_system(spawn_entity_at_char_event_system);
-
-    app.update();
-
-    let entities: Vec<Entity> = get_entities(&mut app);
-    assert_eq!(entities.len(), 0);
-
-    send_event(
-        &mut app,
-        ReceivedCharacter {
-            id: WindowId::primary(),
-            char: 'a',
+fn test_input_keyboard_input() {
+    let inputs = Arc::new(Mutex::new(VecDeque::from([
+        KeyboardInput {
+            key_code: Some(KeyCode::A),
+            scan_code: 0,
+            state: ButtonState::Pressed,
         },
-    );
-    send_event(
-        &mut app,
-        ReceivedCharacter {
-            id: WindowId::primary(),
-            char: 'B',
+        KeyboardInput {
+            key_code: Some(KeyCode::B),
+            scan_code: 0,
+            state: ButtonState::Released,
         },
-    );
+    ])));
+    let inputs2 = inputs.clone();
+    let system = move |mut chat_input_events: EventReader<KeyboardInput>| {
+        for event in chat_input_events.iter() {
+            assert_eq!(
+                event.clone(),
+                inputs2.lock().unwrap().pop_front().expect("foo")
+            );
+        }
+    };
 
-    app.update();
-
-    let entities: Vec<(Entity, &CharComponent)> = get_entities(&mut app);
-    let chars: HashSet<_> = entities.into_iter().map(|(_, c)| c.0).collect();
-    assert_eq!(chars, HashSet::<_>::from_iter(['a', 'B'].into_iter()));
-}
-
-#[test]
-fn test_input_received_keyboard_single_button() {
     let mut app = App::new();
+    app.add_plugin(InputPlugin::default()).add_system(system);
 
-    app.add_plugin(InputPlugin::default())
-        .add_system(spawn_entity_at_keyboard_event_system);
-
+    // If no buttons are pressed, the iterator inside the system function is empty
     app.update();
-
-    let entities: Vec<Entity> = get_entities(&mut app);
-    assert_eq!(entities.len(), 0);
+    assert_eq!(inputs.lock().unwrap().len(), 2);
 
     send_event(
         &mut app,
@@ -129,30 +104,76 @@ fn test_input_received_keyboard_single_button() {
         },
     );
 
+    // If a button is clicked, the iterator inside the system function returns one item
     app.update();
+    assert_eq!(inputs.lock().unwrap().len(), 1);
 
-    let entities: Vec<(Entity, &KeyComponent)> = get_entities(&mut app);
-    let keys: HashSet<_> = entities
-        .into_iter()
-        .map(|(_, c)| (c.0.key_code, c.0.state))
-        .collect();
-    assert_eq!(
-        keys,
-        HashSet::<_>::from_iter([(Some(KeyCode::A), ButtonState::Pressed)].into_iter())
+    // If no buttons are pressed, the past event is forgot
+    // And in this iteration no events are received
+    app.update();
+    assert_eq!(inputs.lock().unwrap().len(), 1);
+
+    send_event(
+        &mut app,
+        KeyboardInput {
+            key_code: Some(KeyCode::B),
+            scan_code: 0,
+            state: ButtonState::Released,
+        },
     );
+
+    // Another button is clicked, the vec is empty
+    app.update();
+    assert!(inputs.lock().unwrap().is_empty());
 }
 
 #[test]
-fn test_input_received_keyboard_multiple_button_at_once() {
+fn test_input_multiple_received_char_and_keyboard_input_are_independent() {
+    let expected_received_chars = Arc::new(Mutex::new(VecDeque::from(['a', 'B'])));
+    let expected_keyboard_inputs = Arc::new(Mutex::new(VecDeque::from([
+        KeyboardInput {
+            key_code: Some(KeyCode::A),
+            scan_code: 0,
+            state: ButtonState::Pressed,
+        },
+        KeyboardInput {
+            key_code: Some(KeyCode::B),
+            scan_code: 0,
+            state: ButtonState::Released,
+        },
+    ])));
+    let expected_received_chars2 = expected_received_chars.clone();
+    let expected_keyboard_inputs2 = expected_keyboard_inputs.clone();
+    let keyboard_system = move |mut chat_input_events: EventReader<KeyboardInput>| {
+        for event in chat_input_events.iter() {
+            assert_eq!(
+                event.clone(),
+                expected_keyboard_inputs2
+                    .lock()
+                    .unwrap()
+                    .pop_front()
+                    .expect("foo")
+            );
+        }
+    };
+    let received_char_system = move |mut chat_input_events: EventReader<ReceivedCharacter>| {
+        for event in chat_input_events.iter() {
+            assert_eq!(
+                event.char,
+                expected_received_chars2
+                    .lock()
+                    .unwrap()
+                    .pop_front()
+                    .expect("foo")
+            );
+        }
+    };
+
     let mut app = App::new();
-
     app.add_plugin(InputPlugin::default())
-        .add_system(spawn_entity_at_keyboard_event_system);
-
-    app.update();
-
-    let entities: Vec<Entity> = get_entities(&mut app);
-    assert_eq!(entities.len(), 0);
+        .add_plugin(WindowPlugin::default())
+        .add_system(keyboard_system)
+        .add_system(received_char_system);
 
     send_event(
         &mut app,
@@ -170,45 +191,11 @@ fn test_input_received_keyboard_multiple_button_at_once() {
             state: ButtonState::Released,
         },
     );
-
-    app.update();
-
-    let entities: Vec<(Entity, &KeyComponent)> = get_entities(&mut app);
-    let keys: HashSet<_> = entities
-        .into_iter()
-        .map(|(_, c)| (c.0.key_code, c.0.state))
-        .collect();
-    assert_eq!(
-        keys,
-        HashSet::<_>::from_iter(
-            [
-                (Some(KeyCode::A), ButtonState::Pressed),
-                (Some(KeyCode::B), ButtonState::Released)
-            ]
-            .into_iter()
-        )
-    );
-}
-
-#[test]
-fn test_input_event_should_be_handler_just_once() {
-    let mut app = App::new();
-    app.add_plugin(WindowPlugin::default())
-        .add_plugin(InputPlugin::default())
-        .add_system(spawn_entity_at_keyboard_event_system)
-        .add_system(spawn_entity_at_char_event_system);
-
-    app.update();
-
-    let entities: Vec<Entity> = get_entities(&mut app);
-    assert_eq!(entities.len(), 0);
-
     send_event(
         &mut app,
-        KeyboardInput {
-            key_code: Some(KeyCode::A),
-            scan_code: 0,
-            state: ButtonState::Pressed,
+        ReceivedCharacter {
+            id: WindowId::primary(),
+            char: 'a',
         },
     );
     send_event(
@@ -219,26 +206,11 @@ fn test_input_event_should_be_handler_just_once() {
         },
     );
 
+    // Multiple events can be sent multiple time per `update`.
+    // The iterator inside the system function returns 2 events
     app.update();
-
-    let entities: Vec<Entity> = get_entities(&mut app);
-    assert_eq!(entities.len(), 2);
-
-    (0..10).for_each(|_| app.update());
-
-    let entities: Vec<Entity> = get_entities(&mut app);
-    assert_eq!(entities.len(), 2);
-}
-
-fn get_entities<'a, T: WorldQuery>(app: &'a mut App) -> Vec<T>
-where
-    Vec<T>: FromIterator<<<T as WorldQueryGats<'a>>::ReadOnlyFetch as Fetch<'a>>::Item>,
-{
-    let world = &mut app.world;
-    let mut query = world.query::<T>();
-    let iter = query.iter(world);
-
-    iter.collect()
+    assert_eq!(expected_keyboard_inputs.lock().unwrap().len(), 0);
+    assert_eq!(expected_received_chars.lock().unwrap().len(), 0);
 }
 
 fn send_event<E: Event>(app: &mut App, event: E) {
