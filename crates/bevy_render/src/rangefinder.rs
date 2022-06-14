d2a1d253157e0f0ae568156c2e2383dfe18ce780
Robin KAY <komadori@gekkou.co.uk>
2022-06-14 19:44:40 +0100

    Improve documentation.

--- crates/bevy_render/src/rangefinder.rs
+++ crates/bevy_render/src/rangefinder.rs
@@ -2,12 +2,13 @@ use bevy_math::{Mat4, Vec4};
 
 use crate::view::ExtractedView;
 
+/// A distance calculator for the draw order of [PhaseItems](crate::render_phase::PhaseItem).
 pub struct ViewRangefinder3d {
     inverse_view_row_2: Vec4,
 }
 
-/// A helper for calculating the draw order of meshes.
 impl ViewRangefinder3d {
+    /// Creates a rangefinder for a view
     pub fn from_view(view: &ExtractedView) -> ViewRangefinder3d {
         let inverse_view_matrix = view.transform.compute_matrix().inverse();
         ViewRangefinder3d {
@@ -15,11 +16,11 @@ impl ViewRangefinder3d {
         }
     }
 
-    /// Calculates the view-space Z value for a mesh's origin
-    pub fn distance(&self, mesh_transform: &Mat4) -> f32 {
+    /// Calculates the distance, or view-space Z value, for a transform
+    pub fn distance(&self, transform: &Mat4) -> f32 {
         // NOTE: row 2 of the inverse view matrix dotted with column 3 of the model matrix
         // gives the z component of translation of the mesh in view-space
-        self.inverse_view_row_2.dot(mesh_transform.col(3))
+        self.inverse_view_row_2.dot(transform.col(3))
     }
 }
 
