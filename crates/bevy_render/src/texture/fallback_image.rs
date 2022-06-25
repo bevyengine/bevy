cc3b9eea611cb5379272a34e3cb8e47443116525
Carter Anderson <mcanders1@gmail.com>
2022-06-25 16:22:10 -0700

    Use create_texture_with_data

--- crates/bevy_render/src/texture/fallback_image.rs
+++ crates/bevy_render/src/texture/fallback_image.rs
@@ -1,11 +1,8 @@
-use crate::{
-    render_resource::*,
-    texture::{DefaultImageSampler, TextureFormatPixelInfo},
-};
+use crate::{render_resource::*, texture::DefaultImageSampler};
 use bevy_derive::Deref;
 use bevy_ecs::prelude::FromWorld;
 use bevy_math::Vec2;
-use wgpu::{Extent3d, ImageCopyTexture, Origin3d, TextureDimension, TextureFormat};
+use wgpu::{Extent3d, TextureDimension, TextureFormat};
 
 use crate::{
     prelude::Image,
@@ -27,35 +24,16 @@ impl FromWorld for FallbackImage {
             &[255u8; 4],
             TextureFormat::bevy_default(),
         );
-        let texture = render_device.create_texture(&image.texture_descriptor);
+        let texture = render_device.create_texture_with_data(
+            render_queue,
+            &image.texture_descriptor,
+            &image.data,
+        );
+        let texture_view = texture.create_view(&TextureViewDescriptor::default());
         let sampler = match image.sampler_descriptor {
             ImageSampler::Default => (**default_sampler).clone(),
             ImageSampler::Descriptor(descriptor) => render_device.create_sampler(&descriptor),
         };
-
-        let format_size = image.texture_descriptor.format.pixel_size();
-        render_queue.write_texture(
-            ImageCopyTexture {
-                texture: &texture,
-                mip_level: 0,
-                origin: Origin3d::ZERO,
-                aspect: TextureAspect::All,
-            },
-            &image.data,
-            ImageDataLayout {
-                offset: 0,
-                bytes_per_row: Some(
-                    std::num::NonZeroU32::new(
-                        image.texture_descriptor.size.width * format_size as u32,
-                    )
-                    .unwrap(),
-                ),
-                rows_per_image: None,
-            },
-            image.texture_descriptor.size,
-        );
-
-        let texture_view = texture.create_view(&TextureViewDescriptor::default());
         Self(GpuImage {
             texture,
             texture_view,
