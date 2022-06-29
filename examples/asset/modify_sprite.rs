3ce8b8fa18eefcc55c7fd0d865e84993148e9bf4
loui <louisgjohnson@gmail.com>
2022-06-29 20:48:05 +1000

    removed old examples - updated modify sprite to match new bevy api

--- examples/asset/modify_sprite.rs
+++ examples/asset/modify_sprite.rs
@@ -1,20 +1,15 @@
-use bevy::{
-    core::{Time, Timer},
-    ecs::prelude::*,
-    math::Vec3,
-    prelude::{App, AssetServer, Assets, Handle, Transform},
-    render2::{camera::OrthographicCameraBundle, color::Color, texture::Image},
-    sprite2::{PipelinedSpriteBundle, Sprite},
-    PipelinedDefaultPlugins,
-};
+use bevy::prelude::*;
 
 struct BevyLogoLight {
     handle: Handle<Image>,
 }
 
+#[derive(Component, Deref, DerefMut)]
+struct AnimationTimer(Timer);
+
 fn main() {
     App::new()
-        .add_plugins(PipelinedDefaultPlugins)
+        .add_plugins(DefaultPlugins)
         .add_startup_system(setup)
         .add_system(change_texture)
         .add_system(change_color)
@@ -24,9 +19,9 @@ fn main() {
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     let texture_handle = asset_server.load("branding/bevy_logo_dark.png");
     let texture_handle_2: Handle<Image> = asset_server.load("branding/bevy_logo_light.png");
-    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+    commands.spawn_bundle(Camera2dBundle::default());
     commands
-        .spawn_bundle(PipelinedSpriteBundle {
+        .spawn_bundle(SpriteBundle {
             texture: texture_handle.clone(),
             transform: Transform {
                 translation: Vec3::new(1., 1., 1.),
@@ -39,7 +34,7 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
             },
             ..Default::default()
         })
-        .insert(Timer::from_seconds(1.5, false));
+        .insert(AnimationTimer(Timer::from_seconds(1f32, false)));
 
     commands.insert_resource(BevyLogoLight {
         handle: texture_handle_2,
@@ -49,9 +44,9 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
 fn change_texture(
     time: Res<Time>,
     bevy_logo_light: Res<BevyLogoLight>,
-    mut query: Query<(&mut Timer, &mut Handle<Image>)>,
+    mut query: Query<(&mut AnimationTimer, &mut Handle<Image>)>,
 ) {
-    for (mut timer, mut handle) in &mut query.iter_mut() {
+    for (mut timer, mut handle) in query.iter_mut() {
         timer.tick(time.delta());
         if timer.finished() {
             *handle = bevy_logo_light.handle.clone();
@@ -59,17 +54,9 @@ fn change_texture(
     }
 }
 
-fn change_color(
-    time: Res<Time>,
-    mut sprite_assets: Res<Assets<Sprite>>,
-    mut query: Query<(&mut Timer, &mut Handle<Image>)>,
-) {
-    let (timer, sprite) = query.iter_mut().next().unwrap();
+fn change_color(mut query: Query<(&mut AnimationTimer, &mut Sprite)>) {
+    let (timer, mut sprite) = query.iter_mut().next().unwrap();
     if timer.finished() {
-        sprite_assets.get_mut(sprite).unwrap().color = Color::WHITE;
+        sprite.color = Color::RED;
     }
-
-    // for (mut timer, mut handle) in &mut query.iter_mut() {
-    //     timer.tick(time.delta());
-    // }
 }
