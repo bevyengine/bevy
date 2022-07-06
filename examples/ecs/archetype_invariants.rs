9bb0cf42cf768ccca735e1a95a18638daf754223
plof27 <rosepeck1997@gmail.com>
2022-07-06 11:19:18 -0400

    Clean up example

--- examples/ecs/archetype_invariants.rs
+++ examples/ecs/archetype_invariants.rs
@@ -23,27 +23,31 @@ fn main() {
         // use (MyComponent, ) to construct a bundle with a single item.
         // This invariant ensures that Player and Camera can never be found together.
         .add_archetype_invariant(ArchetypeInvariant::<(Player,), (Camera,)>::forbids())
-        // This invariant ensures that the presence of a `GlobalTransform` component implies the existence of a `Transform` component.
-        .add_archetype_invariant(ArchetypeInvariant::<(GlobalTransform,), (Transform,)>::requires())
-        // Note that the converse statement isn't automatically true!
-        // With only the above invariant, a entity with only `Transform` is valid.
-        // To fix this, swap the order of the generic types and add a new invariant.
-        .add_archetype_invariant(ArchetypeInvariant::<(Transform,), (GlobalTransform,)>::requires())
+        // This invariant ensures that the `GlobalTransform` component is always found with the `Transform` component, and vice versa.
+        .add_archetype_invariant(ArchetypeInvariant::<(GlobalTransform, Transform)>::atomic())
+        // This invariant ensures that the `Player` compoenent is always found with  the `Life` component.
+        // This requirement is only in one direction: it is possible to have entities which have `Life`, but not `Player` (like enemies).
+        .add_archetype_invariant(ArchetypeInvariant::<(Player,), (Life,)>::requires())
         // The `disjoint` invariant ensures that at most one component from the bundle is present on a given entity.
-        // This way, an entity can never be an animal and a vegetable at once.
+        // This way, an entity never belongs to more than one RPG class at once.
         // This is useful for creating groups of components that behave conceptually similarly to an enum.
-        .add_archetype_invariant(ArchetypeInvariant::<(Animal, Vegetable, Mineral)>::disjoint())
-        // You can also specify custom invariants by constructing `ArchetypeInvariant` directly.
-        // This invariant ensures that all entities have at least one component from the bundle given.
-        // Combined with the above invariant, this means that every entity has exactly one of (Animal, Vegetable, Mineral).
+        .add_archetype_invariant(ArchetypeInvariant::<(Archer, Swordsman, Mage)>::disjoint())
+        // This invariant indicates that any entity with the `Player` component always has
+        // at least one component in the `(Archer, Swordsman, Mage)` bundle.
+        // We could use a type alias to improve clarity and avoid errors caused by duplication:
+        //   type Class = (Archer, Swordsman, Mage);
+        .add_archetype_invariant(
+            ArchetypeInvariant::<(Player,), (Archer, Swordsman, Mage)>::requires_one(),
+        )
+        // You can also specify custom invariants by constructing `ArchetypeInvariant direcly.
+        // This invariant specifies that the `Node` component cannot appear on any entity in our world.
+        // We're not using bevy_ui in our App, so this component should never show up.
         .add_archetype_invariant(ArchetypeInvariant {
-            // This statement is always true, and so matches all entities regardless of their archetype.
-            premise: ArchetypeStatement::always_true(),
-            // ArchetypeStatement::AnyOf evaluates to true when at least one of the components is present on the entity.
-            consequence: ArchetypeStatement::<(Animal, Vegetable, Mineral)>::any_of(),
+            premise: ArchetypeStatement::<(Node,)>::all_of(),
+            consequence: ArchetypeStatement::always_false(),
         })
-        .add_startup_system(spawn_vegetable)
-        .add_system(position_vegetable)
+        .add_startup_system(spawn_player)
+        .add_system(position_player)
         .run();
 }
 
@@ -51,33 +55,36 @@ fn main() {
 struct Player;
 
 #[derive(Component)]
-struct Animal;
+struct Life;
 
 #[derive(Component)]
-struct Vegetable;
+struct Archer;
 
 #[derive(Component)]
-struct Mineral;
+struct Swordsman;
 
-fn spawn_vegetable(mut commands: Commands) {
-    commands.spawn().insert(Vegetable);
+#[derive(Component)]
+struct Mage;
+
+fn spawn_player(mut commands: Commands) {
+    commands.spawn().insert_bundle((Player, Mage));
 }
 
-fn position_vegetable(mut commands: Commands, query: Query<Entity, With<Vegetable>>) {
-    let vegetable_entity = query.single();
+fn position_player(mut commands: Commands, query: Query<Entity, (With<Player>, Added<Player>)>) {
+    let player_entity = query.single();
 
     // Because of our invariants, these components need to be added together.
     // Adding them separately (as in the broken code below) will cause the entity to briefly enter an invalid state,
     // where it has only one of the two components.
     commands
-        .entity(vegetable_entity)
+        .entity(player_entity)
         .insert_bundle((GlobalTransform::default(), Transform::default()));
 
-    // Adding the components one at a time panics
-    // Track this limitation at https://github.com/bevyengine/bevy/issues/5074
+    // Adding the components one at a time panics.
+    // Track this limitation at https://github.com/bevyengine/bevy/issues/5074.
     /*
     commands
-        .entity(vegetable_entity)
+        .entity(player_entity)
         .insert(GlobalTransform::default())
         .insert(Transform::default());
     */
