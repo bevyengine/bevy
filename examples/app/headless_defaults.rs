126338288a0ba16630e86740a8c8767f359570f5
Christian Legnitto <christian@legnitto.com>
2022-07-09 15:35:30 -0400

    Make `headless_defaults` example actually headless
    Fixes https://github.com/bevyengine/bevy/issues/5260

--- examples/app/headless_defaults.rs
+++ examples/app/headless_defaults.rs
@@ -1,7 +1,7 @@
 //! An application that runs with default plugins, but without an actual renderer.
 //! This can be very useful for integration tests or CI.
 
-use bevy::{prelude::*, render::settings::WgpuSettings};
+use bevy::{app::AppExit, prelude::*, render::settings::WgpuSettings, window::WindowSettings};
 
 fn main() {
     App::new()
@@ -9,6 +9,21 @@ fn main() {
             backends: None,
             ..default()
         })
+        .insert_resource(WindowSettings {
+            add_primary_window: false,
+            exit_on_all_closed: false,
+            ..default()
+        })
         .add_plugins(DefaultPlugins)
+        .add_system(do_something)
         .run();
+}
+
+// Normally Bevy exits when all windows are closed.
+//
+// When running in headless mode there are no windows so
+// you must manually send an [`bevy::app::AppExit`] event.
+fn do_something(mut app_exit_events: EventWriter<AppExit>) {
+    info!("Successfully ran! Exiting...");
+    app_exit_events.send(AppExit);
 }
