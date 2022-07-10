df4fe42a3a3dd47e9275050cffcaaa7f16fdc421
Zicklag <zicklag@katharostech.com>
2022-07-10 10:05:53 -0500

    Apply suggestions from code review
    Co-authored-by: Federico Rinaldi <gisquerin@gmail.com>

--- crates/bevy_utils/src/float_ord.rs
+++ crates/bevy_utils/src/float_ord.rs
@@ -6,12 +6,12 @@ use std::{
 
 /// A wrapper for floats that implements [`Ord`], [`Eq`], and [`Hash`] traits.
 ///
-/// This is a work around for the fact that the EEE 754-2008 standard,
+/// This is a work around for the fact that the IEEE 754-2008 standard,
 /// implemented by Rust's [`f32`] type,
 /// doesn't define an ordering for [`NaN`](f32::NAN),
 /// and `NaN` is not considered equal to any other `NaN`.
 ///
-/// Wrapping a float with [`FloatOrd`] breaks conformance with the standard
+/// Wrapping a float with `FloatOrd` breaks conformance with the standard
 /// by sorting `NaN` as less than all other numbers and equal to any other `NaN`.
 #[derive(Debug, Copy, Clone, PartialOrd)]
 pub struct FloatOrd(pub f32);
