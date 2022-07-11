3cdd33308dd0c7c37b0c53da9e3122cb6c13fd4e
harudagondi <giogdeasis@gmail.com>
2022-07-11 23:48:42 +0800

    Allow iter combinations on custom world queries

--- crates/bevy_ecs/src/query/fetch.rs
+++ crates/bevy_ecs/src/query/fetch.rs
@@ -1458,6 +1458,7 @@ macro_rules! impl_tuple_fetch {
 /// `Query<AnyOf<(&A, &B, &mut C)>>` is equivalent to `Query<(Option<&A>, Option<&B>, Option<&mut C>), (Or(With<A>, With<B>, With<C>)>`.
 /// Each of the components in `T` is returned as an `Option`, as with `Option<A>` queries.
 /// Entities are guaranteed to have at least one of the components in `T`.
+#[derive(Clone)]
 pub struct AnyOf<T>(T);
 
 macro_rules! impl_anytuple_fetch {
