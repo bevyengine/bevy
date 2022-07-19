7dcfaaef67ee4d4000af9b8e8ec6c32784229d68
Jakob Hellermann <jakob.hellermann@protonmail.com>
2022-07-19 23:00:34 +0000

    bevy_reflect: `ReflectFromPtr` to create `&dyn Reflect` from a `*const ()` (#4475)
    # Objective
    
    https://github.com/bevyengine/bevy/pull/4447 adds functions that can fetch resources/components as `*const ()` ptr by providing the `ComponentId`. This alone is not enough for them to be usable safely with reflection, because there is no general way to go from the raw pointer to a `&dyn Reflect` which is the pointer + a pointer to the VTable of the `Reflect` impl.
    
    By adding a `ReflectFromPtr` type that is included in the type type registration when deriving `Reflect`, safe functions can be implemented in scripting languages that don't assume a type layout and can access the component data via reflection:
    
    ```rust
    #[derive(Reflect)]
    struct StringResource {
        value: String
    }
    ```
    
    ```lua
    local res_id = world:resource_id_by_name("example::StringResource")
    local res = world:resource(res_id)
    
    print(res.value)
    ```
    
    ## Solution
    
    1. add a `ReflectFromPtr` type with a `FromType<T: Reflect>` implementation and the following methods:
    - `     pub unsafe fn as_reflect_ptr<'a>(&self, val: Ptr<'a>) -> &'a dyn Reflect`
    - `     pub unsafe fn as_reflect_ptr_mut<'a>(&self, val: PtrMut<'a>) -> &'a mud dyn Reflect`
    
    Safety requirements of the methods are that you need to check that the `ReflectFromPtr` was constructed for the correct type.
    
    2. add that type to the `TypeRegistration` in the `GetTypeRegistration` impl generated by `#[derive(Reflect)]`.
    This is different to other reflected traits because it doesn't need `#[reflect(ReflectReflectFromPtr)]` which IMO should be there by default.
    
    Co-authored-by: Jakob Hellermann <hellermann@sipgate.de>
    Co-authored-by: Carter Anderson <mcanders1@gmail.com>

--- crates/bevy_reflect/src/impls/std.rs
+++ crates/bevy_reflect/src/impls/std.rs
@@ -1,4 +1,4 @@
-use crate as bevy_reflect;
+use crate::{self as bevy_reflect, ReflectFromPtr};
 use crate::{
     map_apply, map_partial_eq, Array, ArrayInfo, ArrayIter, DynamicMap, FromReflect, FromType,
     GetTypeRegistration, List, ListInfo, Map, MapInfo, MapIter, Reflect, ReflectDeserialize,
@@ -175,6 +175,7 @@ impl<T: FromReflect + for<'de> Deserialize<'de>> GetTypeRegistration for Vec<T> 
     fn get_type_registration() -> TypeRegistration {
         let mut registration = TypeRegistration::of::<Vec<T>>();
         registration.insert::<ReflectDeserialize>(FromType::<Vec<T>>::from_type());
+        registration.insert::<ReflectFromPtr>(FromType::<Vec<T>>::from_type());
         registration
     }
 }
@@ -326,8 +327,9 @@ where
     V: FromReflect + Clone + for<'de> Deserialize<'de>,
 {
     fn get_type_registration() -> TypeRegistration {
-        let mut registration = TypeRegistration::of::<Self>();
-        registration.insert::<ReflectDeserialize>(FromType::<Self>::from_type());
+        let mut registration = TypeRegistration::of::<HashMap<K, V>>();
+        registration.insert::<ReflectDeserialize>(FromType::<HashMap<K, V>>::from_type());
+        registration.insert::<ReflectFromPtr>(FromType::<HashMap<K, V>>::from_type());
         registration
     }
 }
@@ -575,6 +577,7 @@ impl GetTypeRegistration for Cow<'static, str> {
     fn get_type_registration() -> TypeRegistration {
         let mut registration = TypeRegistration::of::<Cow<'static, str>>();
         registration.insert::<ReflectDeserialize>(FromType::<Cow<'static, str>>::from_type());
+        registration.insert::<ReflectFromPtr>(FromType::<Cow<'static, str>>::from_type());
         registration.insert::<ReflectSerialize>(FromType::<Cow<'static, str>>::from_type());
         registration
     }
