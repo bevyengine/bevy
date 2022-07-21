30084c278b5d612ee39d698074c9b1329dfbd16a
JoJoJet <joe102000@gmail.com>
2022-07-21 14:05:22 -0400

    Revert "don't time system-adding"
    This reverts commit 8bf7503e83a205774176c3c7c744151ae7024e66.

--- benches/benches/bevy_ecs/scheduling/schedule.rs
+++ benches/benches/bevy_ecs/scheduling/schedule.rs
@@ -251,12 +251,10 @@ pub fn build_schedule(criterion: &mut Criterion) {
     group.bench_function("schedule 10 plugins", |bencher| {
         let mut world = World::new();
         bencher.iter_batched(
-            || {
+            || experiment!(0, 1, 2, 3, 4, 5, 6, 7, 8, 9),
+            |experiment| {
                 let mut stage = SystemStage::parallel();
-                experiment!(0, 1, 2, 3, 4, 5, 6, 7, 8, 9).write_to(&mut stage);
-                stage
-            },
-            |mut stage| {
+                experiment.write_to(&mut stage);
                 stage.run(&mut world);
             },
             criterion::BatchSize::SmallInput,
@@ -267,16 +265,15 @@ pub fn build_schedule(criterion: &mut Criterion) {
         let mut world = World::new();
         bencher.iter_batched(
             || {
-                let mut stage = SystemStage::parallel();
                 experiment!(
                     0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
                     22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41,
                     42, 43, 44, 45, 46, 47, 48, 49,
                 )
-                .write_to(&mut stage);
-                stage
             },
-            |mut stage| {
+            |experiment| {
+                let mut stage = SystemStage::parallel();
+                experiment.write_to(&mut stage);
                 stage.run(&mut world);
             },
             criterion::BatchSize::SmallInput,
@@ -287,7 +284,6 @@ pub fn build_schedule(criterion: &mut Criterion) {
         let mut world = World::new();
         bencher.iter_batched(
             || {
-                let mut stage = SystemStage::parallel();
                 experiment!(
                     0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
                     22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41,
@@ -295,10 +291,10 @@ pub fn build_schedule(criterion: &mut Criterion) {
                     62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81,
                     82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99,
                 )
-                .write_to(&mut stage);
-                stage
             },
-            |mut stage| {
+            |experiment| {
+                let mut stage = SystemStage::parallel();
+                experiment.write_to(&mut stage);
                 stage.run(&mut world);
             },
             criterion::BatchSize::SmallInput,
