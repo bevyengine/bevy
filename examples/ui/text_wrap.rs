fda9ca8b6e7ec312c749c1116e31b2f295945cf4
Louis Johnson <louisgjohnson@gmail.com>
2022-08-05 16:37:03 +1000

    update formatting on example

--- examples/ui/text_wrap.rs
+++ examples/ui/text_wrap.rs
@@ -39,29 +39,35 @@ fn spawn_left_panel(parent: &mut ChildBuilder, asset_server: &Res<AssetServer>) 
             ..default()
         })
         .with_children(|parent| {
-            parent
-                .spawn_bundle(TextBundle::from_section(
-                        "This is a super long text, that I would hope would get wrapped. It should only fill half the container size",
-                        TextStyle {
-                            font: asset_server.load("fonts/FiraSans-Bold.ttf"),
-                            font_size: 20.0,
-                            color: Color::WHITE,
-                        }
-                    ).with_style(Style {
-                        max_size: Size::new(Val::Percent(50.0), Val::Undefined),
-                        ..default()
-                    }));
-            parent
-                    .spawn_bundle(TextBundle::from_section(
-                            "This is another super long text, that I would hope would get wrapped. It should fill the whole container",
-                            TextStyle {
-                                font: asset_server.load("fonts/FiraSans-Bold.ttf"),
-                                font_size: 20.0,
-                                color: Color::GRAY,
-                            }
-                        ).with_style(Style {
-                            max_size: Size::new(Val::Percent(100.0), Val::Undefined),
-                            ..default()
-                        }));
-                });
+            parent.spawn_bundle(
+                TextBundle::from_section(
+                    "This is a super long text, that I would hope would get wrapped. ".to_owned()
+                        + "It should only fill half the container size",
+                    TextStyle {
+                        font: asset_server.load("fonts/FiraSans-Bold.ttf"),
+                        font_size: 20.0,
+                        color: Color::WHITE,
+                    },
+                )
+                .with_style(Style {
+                    max_size: Size::new(Val::Percent(50.0), Val::Undefined),
+                    ..default()
+                }),
+            );
+            parent.spawn_bundle(
+                TextBundle::from_section(
+                    "This is another super long text, that I would hope would get wrapped. ".to_owned()
+                        + "It should fill the whole container",
+                    TextStyle {
+                        font: asset_server.load("fonts/FiraSans-Bold.ttf"),
+                        font_size: 20.0,
+                        color: Color::GRAY,
+                    },
+                )
+                .with_style(Style {
+                    max_size: Size::new(Val::Percent(100.0), Val::Undefined),
+                    ..default()
+                }),
+            );
+        });
 }
