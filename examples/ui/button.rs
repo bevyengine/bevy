02a195c6976d9b4f7cb2ef20b3b7867d7a9740f4
Nicola Papale <nico@nicopap.ch>
2022-08-05 13:04:56 +0200

    Add simple button navigation example

--- examples/ui/button.rs
+++ examples/ui/button.rs
@@ -7,6 +7,7 @@ fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         // Only run the app when there is user input. This will significantly reduce CPU/GPU use.
+        // Note that this will not work with gamepad input.
         .insert_resource(WinitSettings::desktop_app())
         .add_startup_system(setup)
         .add_system(button_color.after(NavRequestSystem))
