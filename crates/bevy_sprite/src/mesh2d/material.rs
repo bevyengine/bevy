ea4f78c20679654945b36be3351db373b3a11fcb
Timo Kösters <timo@koesters.xyz>
2022-08-08 08:43:46 +0200

    Fix shader syntax

--- crates/bevy_sprite/src/mesh2d/material.rs
+++ crates/bevy_sprite/src/mesh2d/material.rs
@@ -103,11 +103,11 @@ use crate::{
 ///     color: vec4<f32>;
 /// };
 ///
-/// [[group(1), binding(0)]]
+/// @group(1) @binding(0)
 /// var<uniform> material: CustomMaterial;
-/// [[group(1), binding(1)]]
+/// @group(1) @binding(1)
 /// var color_texture: texture_2d<f32>;
-/// [[group(1), binding(2)]]
+/// @group(1) @binding(2)
 /// var color_sampler: sampler;
 /// ```
 pub trait Material2d: AsBindGroup + Send + Sync + Clone + TypeUuid + Sized + 'static {
