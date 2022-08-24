3bdf3cc579d900a9ace8ff18e1a1f6bbc7db770b
Tristan Guichaoua <tguichaoua.dev@gmail.com>
2022-08-24 14:46:04 +0200

    fix check-doc

--- crates/bevy_ecs/src/system/system_param.rs
+++ crates/bevy_ecs/src/system/system_param.rs
@@ -666,7 +666,7 @@ impl<'w, 's> SystemParamFetch<'w, 's> for WorldState {
 /// assert_eq!(read_system.run((), world), 0);
 /// write_system.run((), world);
 /// // Note how the read local is still 0 due to the locals not being shared.
-/// assert_eq!(read_system.run((), world, RunMeta::run()), 0);
+/// assert_eq!(read_system.run((), world), 0);
 /// ```
 ///
 /// N.B. A [`Local`]s value cannot be read or written to outside of the containing system.
