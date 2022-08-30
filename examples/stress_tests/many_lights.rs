65252bb87a0ca8da2805649f5be821f853afcd99
ira <JustTheCoolDude@gmail.com>
2022-08-30 19:52:11 +0000

    Consistently use `PI` to specify angles in examples. (#5825)
    Examples inconsistently use either `TAU`, `PI`, `FRAC_PI_2` or `FRAC_PI_4`.
    Often in odd ways and without `use`ing the constants, making it difficult to parse.
    
     * Use `PI` to specify angles.
     * General code-quality improvements.
     * Fix borked `hierarchy` example.
    
    
    Co-authored-by: devil-ira <justthecooldude@gmail.com>

--- examples/stress_tests/many_lights.rs
+++ examples/stress_tests/many_lights.rs
@@ -1,6 +1,8 @@
 //! Simple benchmark to test rendering many point lights.
 //! Run with `WGPU_SETTINGS_PRIO=webgl2` to restrict to uniform buffers and max 256 lights.
 
+use std::f64::consts::PI;
+
 use bevy::{
     diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin},
     math::{DVec2, DVec3},
@@ -113,7 +115,7 @@ fn setup(
 const EPSILON: f64 = 0.36;
 fn fibonacci_spiral_on_sphere(golden_ratio: f64, i: usize, n: usize) -> DVec2 {
     DVec2::new(
-        2.0 * std::f64::consts::PI * (i as f64 / golden_ratio),
+        PI * 2. * (i as f64 / golden_ratio),
         (1.0 - 2.0 * (i as f64 + EPSILON) / (n as f64 - 1.0 + 2.0 * EPSILON)).acos(),
     )
 }
