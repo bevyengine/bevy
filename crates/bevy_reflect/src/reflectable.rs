88cd4a24901285c8f571924d4ea69e246bc52846
Gino Valente <49806985+MrGVSV@users.noreply.github.com>
2022-08-30 21:15:00 -0700

    Apply suggestions from code review
    Co-authored-by: Alice Cecile <alice.i.cecile@gmail.com>

--- crates/bevy_reflect/src/reflectable.rs
+++ crates/bevy_reflect/src/reflectable.rs
@@ -3,9 +3,9 @@ use crate::{GetTypeRegistration, Reflect, TypePath, Typed};
 /// A catch-all trait that is bound by the core reflection traits,
 /// useful for simpler reflection-based generic type bounds.
 ///
-/// > __Note:__ You do _not_ need to implement this trait manually.
-/// > It is automatically implemented for all types that implement its supertraits.
-/// > And these supertraits are all automatically derived with the [`Reflect` derive].
+/// You do _not_ need to implement this trait manually.
+/// It is automatically implemented for all types that implement its supertraits.
+/// And these supertraits are all automatically derived with the [`Reflect` derive macro].
 ///
 /// This should namely be used to bound generic arguments to the necessary traits for reflection.
 /// Doing this has the added benefit of reducing migration costs, as a change to the required traits
@@ -27,7 +27,7 @@ use crate::{GetTypeRegistration, Reflect, TypePath, Typed};
 /// }
 /// ```
 ///
-/// [`Reflect` derive]: bevy_reflect_derive::Reflect
+/// [`Reflect` derive macro]: bevy_reflect_derive::Reflect
 pub trait Reflectable: Reflect + GetTypeRegistration + Typed + TypePath {}
 
 impl<T: Reflect + GetTypeRegistration + Typed + TypePath> Reflectable for T {}
