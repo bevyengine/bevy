0d6dc2a6787375144cada30d89804b334871773b
Acid <alexvermaat@gmail.com>
2022-09-02 15:41:09 +0200

    Update to ron 0.8

--- crates/bevy_scene/src/dynamic_scene.rs
+++ crates/bevy_scene/src/dynamic_scene.rs
@@ -141,7 +141,6 @@ where
     S: Serialize,
 {
     let pretty_config = ron::ser::PrettyConfig::default()
-        .decimal_floats(true)
         .indentor("  ".to_string())
         .new_line("\n".to_string());
     ron::ser::to_string_pretty(&serialize, pretty_config)
