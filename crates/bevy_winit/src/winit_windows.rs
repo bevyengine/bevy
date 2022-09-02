c353c39c490abcc0b5fcb8e1ca781411d093d513
devil-ira <justthecooldude@gmail.com>
2022-09-02 15:26:18 +0200

    yeet

--- crates/bevy_winit/src/winit_windows.rs
+++ crates/bevy_winit/src/winit_windows.rs
@@ -264,9 +264,3 @@ pub fn get_best_videomode(monitor: &winit::monitor::MonitorHandle) -> winit::mon
 
     modes.first().unwrap().clone()
 }
-
-// WARNING: this only works under the assumption that wasm runtime is single threaded
-#[cfg(target_arch = "wasm32")]
-unsafe impl Send for WinitWindows {}
-#[cfg(target_arch = "wasm32")]
-unsafe impl Sync for WinitWindows {}
