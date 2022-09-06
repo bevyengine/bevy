bd68ba1c3cf8d67b69c24a29172663ce63ed3abf
Al M <hello@yrns.org>
2022-09-06 20:03:40 +0000

    make TextLayoutInfo a Component (#4460)
    # Objective
    
    Make `TextLayoutInfo` more accessible as a component, rather than internal to `TextPipeline`. I am working on a plugin that manipulates these and there is no (mutable) access to them right now.
    
    ## Solution
    
    This changes `TextPipeline::queue_text` to return `TextLayoutInfo`'s rather than storing them in a map internally. `text2d_system` and `text_system` now take the returned `TextLayoutInfo` and store it as a component of the entity. I considered adding an accessor to `TextPipeline` (e.g. `get_glyphs_mut`) but this seems like it might be a little faster, and also has the added benefit of cleaning itself up when entities are removed. Right now nothing is ever removed from the glyphs map.
    
    ## Changelog
    
    Removed `DefaultTextPipeline`. `TextPipeline` no longer has a generic key type. `TextPipeline::queue_text` returns `TextLayoutInfo` directly.
    
    ## Migration Guide
    
    This might break a third-party crate? I could restore the orginal TextPipeline API as a wrapper around what's in this PR.

--- crates/bevy_text/src/lib.rs
+++ crates/bevy_text/src/lib.rs
@@ -28,12 +28,10 @@ pub mod prelude {
 
 use bevy_app::prelude::*;
 use bevy_asset::AddAsset;
-use bevy_ecs::{entity::Entity, schedule::ParallelSystemDescriptorCoercion};
+use bevy_ecs::schedule::ParallelSystemDescriptorCoercion;
 use bevy_render::{RenderApp, RenderStage};
 use bevy_sprite::SpriteSystem;
 use bevy_window::ModifiesWindows;
-
-pub type DefaultTextPipeline = TextPipeline<Entity>;
 
 #[derive(Default)]
 pub struct TextPlugin;
@@ -48,7 +46,7 @@ impl Plugin for TextPlugin {
             .register_type::<VerticalAlign>()
             .register_type::<HorizontalAlign>()
             .init_asset_loader::<FontLoader>()
-            .insert_resource(DefaultTextPipeline::default())
+            .insert_resource(TextPipeline::default())
             .add_system_to_stage(
                 CoreStage::PostUpdate,
                 update_text2d_layout.after(ModifiesWindows),
