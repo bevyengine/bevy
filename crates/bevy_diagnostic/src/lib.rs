59ec59c6401aa221c555f49291f58afc1ff61f24
l1npengtul <l1npengtul@protonmail.com>
2022-09-08 11:23:41 +0900

    add system information plugin and update relevant examples

--- crates/bevy_diagnostic/src/lib.rs
+++ crates/bevy_diagnostic/src/lib.rs
@@ -2,12 +2,14 @@ mod diagnostic;
 mod entity_count_diagnostics_plugin;
 mod frame_time_diagnostics_plugin;
 mod log_diagnostics_plugin;
+mod system_information_diagnostics_plugin;
+
+use bevy_app::prelude::*;
 pub use diagnostic::*;
 pub use entity_count_diagnostics_plugin::EntityCountDiagnosticsPlugin;
 pub use frame_time_diagnostics_plugin::FrameTimeDiagnosticsPlugin;
 pub use log_diagnostics_plugin::LogDiagnosticsPlugin;
-
-use bevy_app::prelude::*;
+pub use system_information_diagnostics_plugin::SystemInformationDiagnosticsPlugin;
 
 /// Adds core diagnostics resources to an App.
 #[derive(Default)]
---
+++ crates/bevy_diagnostic/src/system_information_diagnostics_plugin.rs
@@ -0,0 +1,64 @@
+use crate::{Diagnostic, DiagnosticId, Diagnostics};
+use bevy_app::{App, Plugin};
+use bevy_ecs::prelude::{ResMut, Resource};
+use sysinfo::{CpuExt, System, SystemExt};
+
+#[derive(Resource)]
+pub struct SystemInfo {
+    pub sys: System,
+}
+
+impl Default for SystemInfo {
+    fn default() -> Self {
+        SystemInfo {
+            sys: System::new_all(),
+        }
+    }
+}
+
+/// Adds a System Information Diagnostic, specifically "cpu_usage" (%) and "mem_usage" (%)
+#[derive(Default)]
+pub struct SystemInformationDiagnosticsPlugin;
+
+impl Plugin for SystemInformationDiagnosticsPlugin {
+    fn build(&self, app: &mut App) {
+        app.insert_resource(SystemInfo::default());
+        app.add_startup_system(Self::setup_system)
+            .add_system(Self::diagnostic_system);
+    }
+}
+
+impl SystemInformationDiagnosticsPlugin {
+    pub const CPU_USAGE: DiagnosticId =
+        DiagnosticId::from_u128(78494871623549551581510633532637320956);
+    pub const MEM_USAGE: DiagnosticId =
+        DiagnosticId::from_u128(42846254859293759601295317811892519825);
+
+    pub fn setup_system(mut diagnostics: ResMut<Diagnostics>) {
+        diagnostics.add(Diagnostic::new(Self::CPU_USAGE, "cpu_usage", 20).with_suffix("%"));
+        diagnostics.add(Diagnostic::new(Self::MEM_USAGE, "mem_usage", 20).with_suffix("%"));
+    }
+
+    pub fn diagnostic_system(
+        mut diagnostics: ResMut<Diagnostics>,
+        mut susinfo: ResMut<SystemInfo>,
+    ) {
+        susinfo.sys.refresh_cpu();
+        susinfo.sys.refresh_memory();
+        let current_cpu_usage = {
+            let mut usage = 0.0;
+            let cpus = susinfo.sys.cpus();
+            for cpu in cpus {
+                usage += cpu.cpu_usage(); // note for future maintainers: this returns a value from 0.0 to 100.0
+            }
+            // average
+            usage / cpus.len() as f32
+        };
+        let total_mem = susinfo.sys.total_memory() as f64 / 1073741824_f64; // `memory()` fns return a value in bytes
+        let used_mem = susinfo.sys.used_memory() as f64 / 1073741824_f64;
+        let current_used_mem = used_mem / total_mem * 100.0;
+
+        diagnostics.add_measurement(Self::CPU_USAGE, || current_cpu_usage as f64);
+        diagnostics.add_measurement(Self::MEM_USAGE, || current_used_mem as f64);
+    }
+}
