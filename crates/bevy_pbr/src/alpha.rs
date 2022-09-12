902c3a9418b41a895ab341b6b318f0b527b6ba7d
Nicola Papale <nico@nicopap.ch>
2022-09-12 19:49:27 +0200

    Improve AlphaMode Mask documentation

--- crates/bevy_pbr/src/alpha.rs
+++ crates/bevy_pbr/src/alpha.rs
@@ -2,6 +2,7 @@ use bevy_ecs::{component::Component, reflect::ReflectComponent};
 use bevy_reflect::std_traits::ReflectDefault;
 use bevy_reflect::Reflect;
 
+// TODO: add discussion about performance.
 /// Sets how a PBR material's base color alpha channel is used for transparency.
 #[derive(Component, Debug, Default, Reflect, Copy, Clone, PartialEq)]
 #[reflect(Component, Default)]
@@ -9,8 +10,12 @@ pub enum AlphaMode {
     /// Base color alpha values are overridden to be fully opaque (1.0).
     #[default]
     Opaque,
-    /// An alpha cutoff must be supplied where alpha values >= the cutoff
-    /// will be fully opaque (1.0) and < will be fully transparent (0.0).
+    /// Reduce transparency to fully opaque or fully transparent
+    /// based on a threshold.
+    ///
+    /// The base color texture pixels with an alpha channel lower than the
+    /// provided value will be fully transparent while pixels with an alpha
+    /// greater than the provided value will be fully opaque.
     Mask(f32),
     /// The base color alpha value defines the opacity of the color.
     /// Standard alpha-blending is used to blend the fragment's color
