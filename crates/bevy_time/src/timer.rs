a264e50e4d965c6f3c327d15de320298401b103f
Sergi-Ferrez <61662926+Sergi-Ferrez@users.noreply.github.com>
2022-09-13 20:15:18 +0200

    Fix float equality assert

--- crates/bevy_time/src/timer.rs
+++ crates/bevy_time/src/timer.rs
@@ -348,10 +348,12 @@ impl Timer {
     /// # Examples
     /// ```
     /// # use bevy_time::*;
+    /// use std::cmp::Ordering;
     /// use std::time::Duration;
     /// let mut timer = Timer::from_seconds(2.0, false);
     /// timer.tick(Duration::from_secs_f32(0.5));
-    /// assert_eq!(timer.remaining_secs(), 1.5);
+    /// let result = timer.remaining_secs().total_cmp(&1.5);
+    /// assert_eq!(Ordering::Equal, result);
     /// ```
     #[inline]
     pub fn remaining_secs(&self) -> f32 {
