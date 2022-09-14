e4ad01d8fa67a83e532025e827a4bb5b0f170753
Michael Hsu <mike.hsu@gmail.com>
2022-09-14 14:48:29 -0700

    fix rebase error

--- crates/bevy_ecs/src/schedule/executor_parallel.rs
+++ crates/bevy_ecs/src/schedule/executor_parallel.rs
@@ -236,7 +236,7 @@ impl ParallelExecutor {
                 if system_data.is_send {
                     scope.spawn(task);
                 } else {
-                    scope.spawn_local(task);
+                    scope.spawn_on_scope(task);
                 }
 
                 #[cfg(test)]
