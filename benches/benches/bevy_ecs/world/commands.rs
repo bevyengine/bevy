07ae8dc83c52f7c3135f899c2615cfaff57f92d2
Nilirad <gisquerin@gmail.com>
2022-09-14 19:14:08 +0200

    Missed one :P

--- benches/benches/bevy_ecs/world/commands.rs
+++ benches/benches/bevy_ecs/world/commands.rs
@@ -238,7 +238,8 @@ pub fn get_or_spawn(criterion: &mut Criterion) {
             for i in 0..10_000 {
                 commands
                     .get_or_spawn(Entity::from_raw(i))
-                    .insert((Matrix::default(), Vec3::default()));
+                    .expect("Entity not found.")
+                    .insert_bundle((Matrix::default(), Vec3::default()));
             }
             command_queue.apply(&mut world);
         });
