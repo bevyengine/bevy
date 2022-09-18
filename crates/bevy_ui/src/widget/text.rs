1e19863dd9f6a27a7174773794635a1e47b330f4
targrub <targrub@gmail.com>
2022-09-17 20:53:58 -0400

    Fixing clippy errors and other errors from running `cargo run -p ci`

--- crates/bevy_ui/src/widget/text.rs
+++ crates/bevy_ui/src/widget/text.rs
@@ -113,9 +113,9 @@ pub fn text_system(
                 scale_factor,
                 text.alignment,
                 node_size,
-                &mut *font_atlas_set_storage,
-                &mut *texture_atlases,
-                &mut *textures,
+                &mut font_atlas_set_storage,
+                &mut texture_atlases,
+                &mut textures,
             ) {
                 Err(TextError::NoSuchFont) => {
                     // There was an error processing the text layout, let's add this entity to the
