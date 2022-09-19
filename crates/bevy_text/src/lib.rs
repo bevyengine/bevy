b6efe0f318b93330376cd44849281e919eec4a53
xtr3m3nerd <xtr3m3nerd@gmail.com>
2022-09-19 16:12:12 +0000

    Limit FontAtlasSets  (#5708)
    # Objective
    
    Fixes #5636
    Summary: The FontAtlasSet caches generated font textures per font size. Since font size can be any arbitrary floating point number it is possible for the user to generate thousands of font texture inadvertently by changing the font size over time. This results in a memory leak as these generated font textures fill the available memory. 
    
    ## Solution
    
    We limit the number of possible font sizes that we will cache and throw an error if the user attempts to generate more. This error encourages the user to use alternative, less performance intensive methods to accomplish the same goal. If the user requires more font sizes and the alternative solutions wont work there is now a TextSettings Resource that the user can set to configure this limit. 
    
    ---
    
    ## Changelog
    
    The number of cached font sizes per font is now limited with a default limit of 100 font sizes per font. This limit is configurable via the new TextSettings struct.

--- crates/bevy_text/src/lib.rs
+++ crates/bevy_text/src/lib.rs
@@ -28,13 +28,33 @@ pub mod prelude {
 
 use bevy_app::prelude::*;
 use bevy_asset::AddAsset;
-use bevy_ecs::schedule::ParallelSystemDescriptorCoercion;
+use bevy_ecs::{schedule::ParallelSystemDescriptorCoercion, system::Resource};
 use bevy_render::{RenderApp, RenderStage};
 use bevy_sprite::SpriteSystem;
 use bevy_window::ModifiesWindows;
+use std::num::NonZeroUsize;
 
 #[derive(Default)]
 pub struct TextPlugin;
+
+/// [`TextPlugin`] settings
+#[derive(Resource)]
+pub struct TextSettings {
+    /// Maximum number of font atlases supported in a ['FontAtlasSet']
+    pub max_font_atlases: NonZeroUsize,
+    /// Allows font size to be set dynamically exceeding the amount set in max_font_atlases.
+    /// Note each font size has to be generated which can have a strong performance impact.
+    pub allow_dynamic_font_size: bool,
+}
+
+impl Default for TextSettings {
+    fn default() -> Self {
+        Self {
+            max_font_atlases: NonZeroUsize::new(16).unwrap(),
+            allow_dynamic_font_size: false,
+        }
+    }
+}
 
 impl Plugin for TextPlugin {
     fn build(&self, app: &mut App) {
@@ -46,6 +66,7 @@ impl Plugin for TextPlugin {
             .register_type::<VerticalAlign>()
             .register_type::<HorizontalAlign>()
             .init_asset_loader::<FontLoader>()
+            .init_resource::<TextSettings>()
             .insert_resource(TextPipeline::default())
             .add_system_to_stage(
                 CoreStage::PostUpdate,
