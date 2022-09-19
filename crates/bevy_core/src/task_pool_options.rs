f2ad11104dec0e3bbd93ad70c9fdea9d61bfa1aa
James Liu <contact@jamessliu.com>
2022-09-19 15:46:03 +0000

    Swap out num_cpus for std::thread::available_parallelism (#4970)
    # Objective
    As of Rust 1.59, `std::thread::available_parallelism` has been stabilized. As of Rust 1.61, the API matches `num_cpus::get` by properly handling Linux's cgroups and other sandboxing mechanisms.
    
    As bevy does not have an established MSRV, we can replace `num_cpus` in `bevy_tasks` and reduce our dependency tree by one dep.
    
    ## Solution
    Replace `num_cpus` with `std::thread::available_parallelism`. Wrap it to have a fallback in the case it errors out and have it operate in the same manner as `num_cpus` did.
    
    This however removes `physical_core_count` from the API, though we are currently not using it in any way in first-party crates.
    
    ---
    
    ## Changelog
    Changed: `bevy_tasks::logical_core_count` -> `bevy_tasks::available_parallelism`.
    Removed: `bevy_tasks::physical_core_count`.
    
    ## Migration Guide
    `bevy_tasks::logical_core_count` and `bevy_tasks::physical_core_count` have been removed. `logical_core_count` has been replaced with `bevy_tasks::available_parallelism`, which works identically. If `bevy_tasks::physical_core_count` is required, the `num_cpus` crate can be used directly, as these two were just aliases for `num_cpus` APIs.

--- crates/bevy_core/src/task_pool_options.rs
+++ crates/bevy_core/src/task_pool_options.rs
@@ -94,8 +94,8 @@ impl DefaultTaskPoolOptions {
 
     /// Inserts the default thread pools into the given resource map based on the configured values
     pub fn create_default_pools(&self) {
-        let total_threads =
-            bevy_tasks::logical_core_count().clamp(self.min_total_threads, self.max_total_threads);
+        let total_threads = bevy_tasks::available_parallelism()
+            .clamp(self.min_total_threads, self.max_total_threads);
         trace!("Assigning {} cores to default task pools", total_threads);
 
         let mut remaining_threads = total_threads;
