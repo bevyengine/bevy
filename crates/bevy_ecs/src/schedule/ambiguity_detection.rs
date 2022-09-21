253f40485c7f225830786c1937a72209b36c3124
JoJoJet <joe102000@gmail.com>
2022-09-21 15:57:41 -0400

    Add `SystemOrderAmbiguity` struct

--- crates/bevy_ecs/src/schedule/ambiguity_detection.rs
+++ crates/bevy_ecs/src/schedule/ambiguity_detection.rs
@@ -5,74 +5,195 @@ use crate::component::ComponentId;
 use crate::schedule::{SystemContainer, SystemStage};
 use crate::world::World;
 
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub struct SystemOrderAmbiguity {
+    // Note: In order for comparisons to work correctly,
+    // `system_names` and `conflicts` must be sorted at all times.
+    system_names: [String; 2],
+    conflicts: Vec<String>,
+    pub segment: SystemStageSegment,
+}
+
+/// Which part of a [`SystemStage`] was a [`SystemOrderAmbiguity`] detected in?
+#[derive(Debug, PartialEq, Eq, Clone, Copy, Hash)]
+pub enum SystemStageSegment {
+    Parallel,
+    ExclusiveAtStart,
+    ExclusiveBeforeCommands,
+    ExclusiveAtEnd,
+}
+
+impl SystemStageSegment {
+    pub fn desc(&self) -> &'static str {
+        match self {
+            SystemStageSegment::Parallel => "Parallel systems",
+            SystemStageSegment::ExclusiveAtStart => "Exclusive systems at start of stage",
+            SystemStageSegment::ExclusiveBeforeCommands => {
+                "Exclusive systems before commands of stage"
+            }
+            SystemStageSegment::ExclusiveAtEnd => "Exclusive systems at end of stage",
+        }
+    }
+}
+
+impl SystemOrderAmbiguity {
+    fn from_raw(
+        system_a_index: usize,
+        system_b_index: usize,
+        component_ids: Vec<ComponentId>,
+        segment: SystemStageSegment,
+        stage: &SystemStage,
+        world: &World,
+    ) -> Self {
+        use crate::schedule::graph_utils::GraphNode;
+        use SystemStageSegment::*;
+
+        // TODO: blocked on https://github.com/bevyengine/bevy/pull/4166
+        // We can't grab the system container generically, because .parallel_systems()
+        // and the exclusive equivalent return a different type,
+        // and SystemContainer is not object-safe
+        let (system_a_name, system_b_name) = match segment {
+            Parallel => {
+                let system_container = stage.parallel_systems();
+                (
+                    system_container[system_a_index].name(),
+                    system_container[system_b_index].name(),
+                )
+            }
+            ExclusiveAtStart => {
+                let system_container = stage.exclusive_at_start_systems();
+                (
+                    system_container[system_a_index].name(),
+                    system_container[system_b_index].name(),
+                )
+            }
+            ExclusiveBeforeCommands => {
+                let system_container = stage.exclusive_before_commands_systems();
+                (
+                    system_container[system_a_index].name(),
+                    system_container[system_b_index].name(),
+                )
+            }
+            ExclusiveAtEnd => {
+                let system_container = stage.exclusive_at_end_systems();
+                (
+                    system_container[system_a_index].name(),
+                    system_container[system_b_index].name(),
+                )
+            }
+        };
+
+        let mut system_names = [system_a_name.to_string(), system_b_name.to_string()];
+        system_names.sort();
+
+        let mut conflicts: Vec<_> = component_ids
+            .iter()
+            .map(|id| world.components().get_info(*id).unwrap().name().to_owned())
+            .collect();
+        conflicts.sort();
+
+        Self {
+            system_names,
+            conflicts,
+            segment,
+        }
+    }
+}
+
 impl SystemStage {
     /// Logs execution order ambiguities between systems. System orders must be fresh.
     pub fn report_ambiguities(&self, world: &World) {
         debug_assert!(!self.systems_modified);
         use std::fmt::Write;
-        fn write_display_names_of_pairs(
-            string: &mut String,
-            systems: &[impl SystemContainer],
-            mut ambiguities: Vec<(usize, usize, Vec<ComponentId>)>,
-            world: &World,
-        ) {
-            for (index_a, index_b, conflicts) in ambiguities.drain(..) {
-                writeln!(
-                    string,
-                    " -- {:?} and {:?}",
-                    systems[index_a].name(),
-                    systems[index_b].name()
-                )
-                .unwrap();
-                if !conflicts.is_empty() {
-                    let names = conflicts
-                        .iter()
-                        .map(|id| world.components().get_info(*id).unwrap().name())
-                        .collect::<Vec<_>>();
-                    writeln!(string, "    conflicts: {:?}", names).unwrap();
-                }
-            }
-        }
-        let parallel = find_ambiguities(&self.parallel);
-        let at_start = find_ambiguities(&self.exclusive_at_start);
-        let before_commands = find_ambiguities(&self.exclusive_before_commands);
-        let at_end = find_ambiguities(&self.exclusive_at_end);
-        if !(parallel.is_empty()
-            && at_start.is_empty()
-            && before_commands.is_empty()
-            && at_end.is_empty())
-        {
+        let ambiguities = self.ambiguities(world);
+        if !ambiguities.is_empty() {
             let mut string = "Execution order ambiguities detected, you might want to \
 						add an explicit dependency relation between some of these systems:\n"
                 .to_owned();
-            if !parallel.is_empty() {
-                writeln!(string, " * Parallel systems:").unwrap();
-                write_display_names_of_pairs(&mut string, &self.parallel, parallel, world);
+
+            let mut last_segment_kind = None;
+
+            for SystemOrderAmbiguity {
+                system_names: [system_a, system_b],
+                conflicts,
+                segment,
+            } in &ambiguities
+            {
+                // If the ambiguity occurred in a different segment than the previous one, write a header for the segment.
+                if last_segment_kind != Some(segment) {
+                    writeln!(string, " * {}:", segment.desc()).unwrap();
+                    last_segment_kind = Some(segment);
+                }
+
+                writeln!(string, " -- {:?} and {:?}", system_a, system_b).unwrap();
+
+                if !conflicts.is_empty() {
+                    writeln!(string, "    conflicts: {conflicts:?}").unwrap();
+                }
             }
-            if !at_start.is_empty() {
-                writeln!(string, " * Exclusive systems at start of stage:").unwrap();
-                write_display_names_of_pairs(
-                    &mut string,
-                    &self.exclusive_at_start,
-                    at_start,
-                    world,
-                );
-            }
-            if !before_commands.is_empty() {
-                writeln!(string, " * Exclusive systems before commands of stage:").unwrap();
-                write_display_names_of_pairs(
-                    &mut string,
-                    &self.exclusive_before_commands,
-                    before_commands,
-                    world,
-                );
-            }
-            if !at_end.is_empty() {
-                writeln!(string, " * Exclusive systems at end of stage:").unwrap();
-                write_display_names_of_pairs(&mut string, &self.exclusive_at_end, at_end, world);
-            }
+
             info!("{}", string);
         }
+    }
+
+    fn ambiguities(&self, world: &World) -> Vec<SystemOrderAmbiguity> {
+        let parallel = find_ambiguities(&self.parallel).into_iter().map(
+            |(system_a_index, system_b_index, component_ids)| {
+                SystemOrderAmbiguity::from_raw(
+                    system_a_index,
+                    system_b_index,
+                    component_ids.to_vec(),
+                    SystemStageSegment::Parallel,
+                    self,
+                    world,
+                )
+            },
+        );
+
+        let at_start = find_ambiguities(&self.exclusive_at_start).into_iter().map(
+            |(system_a_index, system_b_index, component_ids)| {
+                SystemOrderAmbiguity::from_raw(
+                    system_a_index,
+                    system_b_index,
+                    component_ids,
+                    SystemStageSegment::ExclusiveAtStart,
+                    self,
+                    world,
+                )
+            },
+        );
+
+        let before_commands = find_ambiguities(&self.exclusive_before_commands)
+            .into_iter()
+            .map(|(system_a_index, system_b_index, component_ids)| {
+                SystemOrderAmbiguity::from_raw(
+                    system_a_index,
+                    system_b_index,
+                    component_ids,
+                    SystemStageSegment::ExclusiveBeforeCommands,
+                    self,
+                    world,
+                )
+            });
+
+        let at_end = find_ambiguities(&self.exclusive_at_end).into_iter().map(
+            |(system_a_index, system_b_index, component_ids)| {
+                SystemOrderAmbiguity::from_raw(
+                    system_a_index,
+                    system_b_index,
+                    component_ids,
+                    SystemStageSegment::ExclusiveAtEnd,
+                    self,
+                    world,
+                )
+            },
+        );
+
+        at_start
+            .chain(parallel)
+            .chain(before_commands)
+            .chain(at_end)
+            .collect()
     }
 }
 
