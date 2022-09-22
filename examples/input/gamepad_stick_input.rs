397c68639ba7d209b6a54e042aca5d3d880a717d
targrub <targrub@gmail.com>
2022-09-22 13:46:58 -0400

    Removed as much windows code as possible.
    Left in setting the window title, though.

--- examples/input/gamepad_stick_input.rs
+++ examples/input/gamepad_stick_input.rs
@@ -1,5 +1,6 @@
 use bevy::{input::gamepad::GamepadSettings, prelude::*};
 
+const FULL_RANGE_SIZE: f32 = 300.0;
 const CROSSHAIR_SIZE: f32 = 16.0;
 const FONT: &str = "fonts/FiraMono-Medium.ttf";
 const FONT_SIZE: f32 = 18.0;
@@ -12,20 +13,8 @@ struct Crosshair;
 #[derive(Component)]
 struct CoordinateText;
 
-#[derive(Resource)]
-struct VisualizationSize {
-    size: f32,
-}
-
-impl Default for VisualizationSize {
-    fn default() -> Self {
-        VisualizationSize { size: 300.0 }
-    }
-}
-
 fn main() {
     App::new()
-        .insert_resource(VisualizationSize::default())
         .insert_resource(ClearColor(DEADZONE_COLOR))
         .add_plugins(DefaultPlugins)
         .add_startup_system(setup)
@@ -38,7 +27,6 @@ fn update_position(
     mut text_query: Query<&mut Text, With<CoordinateText>>,
     gamepads: Res<Gamepads>,
     axes: Res<Axis<GamepadAxis>>,
-    visualization_size: Res<VisualizationSize>,
 ) {
     let mut transform = crosshair_query.single_mut();
     let mut text = text_query.single_mut();
@@ -56,8 +44,8 @@ fn update_position(
                 axis_type: GamepadAxisType::LeftStickY,
             })
             .unwrap();
-        transform.translation.x = x * visualization_size.size / 2.0;
-        transform.translation.y = y * visualization_size.size / 2.0;
+        transform.translation.x = x * FULL_RANGE_SIZE / 2.0;
+        transform.translation.y = y * FULL_RANGE_SIZE / 2.0;
         text.sections[0].value = format!("({:6.3}, {:6.3})", x, y);
     }
 }
@@ -66,31 +54,10 @@ fn setup(
     mut commands: Commands,
     asset_server: Res<AssetServer>,
     gamepad_settings: Res<GamepadSettings>,
-    mut visualization_window: ResMut<VisualizationSize>,
     mut windows: ResMut<Windows>,
 ) {
     let window = windows.get_primary_mut().unwrap();
     window.set_title("Gamepad Stick Input".to_owned());
-
-    // The monitor's size
-    let physical_width = window.physical_width();
-    let physical_height = window.physical_height();
-
-    // The scaling factor the OS uses to account for pixel density or user preference
-    let scale_factor = window.scale_factor();
-
-    // The resizing constraints that limit the resolution we can set
-    let constraints = window.resize_constraints();
-
-    let min_physical_dimension: f32 =
-        (physical_height.min(physical_width) as f64 * scale_factor) as f32;
-    let min_constraint_dimension = constraints.max_height.min(constraints.max_width);
-
-    // Keep the size of the window within constraints
-    let min_dimension = min_physical_dimension.min(min_constraint_dimension);
-
-    window.set_resolution(min_dimension, min_dimension);
-    visualization_window.size = min_dimension;
 
     // Spawn camera
     commands.spawn_bundle(Camera2dBundle::default());
@@ -119,10 +86,10 @@ fn setup(
     let deadzone_midpoint = (deadzone_lowerbound + deadzone_upperbound) / 2.0;
     let livezone_size = livezone_upperbound - livezone_lowerbound;
     let deadzone_size = deadzone_upperbound - deadzone_lowerbound;
-    let livezone_box_midpoint = livezone_midpoint * min_dimension / 2.0;
-    let deadzone_box_midpoint = deadzone_midpoint * min_dimension / 2.0;
-    let livezone_box_size = livezone_size * min_dimension / 2.0;
-    let deadzone_box_size = deadzone_size * min_dimension / 2.0;
+    let livezone_box_midpoint = livezone_midpoint * FULL_RANGE_SIZE / 2.0;
+    let deadzone_box_midpoint = deadzone_midpoint * FULL_RANGE_SIZE / 2.0;
+    let livezone_box_size = livezone_size * FULL_RANGE_SIZE / 2.0;
+    let deadzone_box_size = deadzone_size * FULL_RANGE_SIZE / 2.0;
     // For text placement
     let livezone_lower_left_corner = (livezone_box_midpoint - livezone_box_size) / 2.0;
 
