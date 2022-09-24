b91945b54ddf9664d1ed89a33a872ab2700626c2
Boutillier <boris.boutillier@gmail.com>
2022-09-24 12:58:06 +0000

    Merge TextureAtlas::from_grid_with_padding into TextureAtlas::from_grid through option arguments (#6057)
    This is an adoption of #3775
    This merges `TextureAtlas` `from_grid_with_padding` into `from_grid` , adding optional padding and optional offset.
    Since the orignal PR, the offset had already been added to from_grid_with_padding through #4836 
    
    ## Changelog
    
    - Added `padding` and `offset` arguments to  `TextureAtlas::from_grid`
    - Removed `TextureAtlas::from_grid_with_padding`
    
    ## Migration Guide
    
    `TextureAtlas::from_grid_with_padding` was merged into `from_grid` which takes two additional parameters for padding and an offset.
    ```
    // 0.8
    TextureAtlas::from_grid(texture_handle, Vec2::new(24.0, 24.0), 7, 1);
    // 0.9
    TextureAtlas::from_grid(texture_handle, Vec2::new(24.0, 24.0), 7, 1, None, None)
    
    // 0.8
    TextureAtlas::from_grid_with_padding(texture_handle, Vec2::new(24.0, 24.0), 7, 1, Vec2::new(4.0, 4.0));
    // 0.9
    TextureAtlas::from_grid(texture_handle, Vec2::new(24.0, 24.0), 7, 1, Some(Vec2::new(4.0, 4.0)), None)
    ```
    
    Co-authored-by: olefish <88390729+oledfish@users.noreply.github.com>

--- examples/2d/sprite_sheet.rs
+++ examples/2d/sprite_sheet.rs
@@ -39,7 +39,8 @@ fn setup(
     mut texture_atlases: ResMut<Assets<TextureAtlas>>,
 ) {
     let texture_handle = asset_server.load("textures/rpg/chars/gabe/gabe-idle-run.png");
-    let texture_atlas = TextureAtlas::from_grid(texture_handle, Vec2::new(24.0, 24.0), 7, 1);
+    let texture_atlas =
+        TextureAtlas::from_grid(texture_handle, Vec2::new(24.0, 24.0), 7, 1, None, None);
     let texture_atlas_handle = texture_atlases.add(texture_atlas);
     commands.spawn(Camera2dBundle::default());
     commands.spawn((
