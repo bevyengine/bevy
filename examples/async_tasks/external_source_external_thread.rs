9c7c0cc56b9fedec718bda9ee5a04e126d39f1a8
rustui <90625190+rustui@users.noreply.github.com>
2022-09-28 21:22:21 +0800

    StreamReceiver does not need to be mutable
    ResMut -> Res

--- examples/async_tasks/external_source_external_thread.rs
+++ examples/async_tasks/external_source_external_thread.rs
@@ -46,7 +46,7 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
 }
 
 // This system reads from the receiver and sends events to Bevy
-fn read_stream(receiver: ResMut<StreamReceiver>, mut events: EventWriter<StreamEvent>) {
+fn read_stream(receiver: Res<StreamReceiver>, mut events: EventWriter<StreamEvent>) {
     for from_stream in receiver.try_iter() {
         events.send(StreamEvent(from_stream));
     }
