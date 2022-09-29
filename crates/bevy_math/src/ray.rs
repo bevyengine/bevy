dedd6b75ee3ac0f59af1fb62b3974614254ca3a0
devil-ira <justthecooldude@gmail.com>
2022-09-29 21:20:55 +0200

    Remove `#[repr(C)]` and add comment

--- crates/bevy_math/src/ray.rs
+++ crates/bevy_math/src/ray.rs
@@ -2,7 +2,6 @@ use crate::Vec3;
 use serde::{Deserialize, Serialize};
 
 /// A ray is an infinite line starting at `origin`, going in `direction`.
-#[repr(C)]
 #[derive(Default, Clone, Copy, Debug, Serialize, Deserialize, PartialEq)]
 pub struct Ray {
     /// The origin of the ray.
--- crates/bevy_render/src/camera/camera.rs
+++ crates/bevy_render/src/camera/camera.rs
@@ -229,6 +229,7 @@ impl Camera {
         let ndc = viewport_position * 2. / target_size - Vec2::ONE;
 
         let world_near_plane = self.ndc_to_world(camera_transform, ndc.extend(1.))?;
+        // Using EPSILON because passing an ndc with Z = 0 returns NaNs.
         let world_far_plane = self.ndc_to_world(camera_transform, ndc.extend(f32::EPSILON))?;
 
         Some(Ray {
