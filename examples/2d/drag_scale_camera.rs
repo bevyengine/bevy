64c05440676d07319d7e03edc574e0dd46da75e8
Aitesedba <yiconghui@gmail.com>
2022-10-03 19:43:24 +0100

    Added Drag_scale_camera as an example

---
+++ examples/2d/drag_scale_camera.rs
@@ -0,0 +1,73 @@
+//! How to change the transform and scale of the camera. Affect camera via dragging and scrolling
+
+use bevy::{input::mouse::MouseWheel, prelude::*, sprite::MaterialMesh2dBundle};
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(setup)
+        .add_system(camera_scale)
+        .add_system(camer_drag)
+        .insert_resource(ClearColor(Color::RED))
+        .run();
+}
+
+fn camer_drag(
+    mouse_button_input: Res<Input<MouseButton>>,
+    mut query: Query<(&mut Transform, &mut OrthographicProjection)>,
+
+    winfo: Res<Windows>,
+    mut prev_position: Local<Vec2>,
+) {
+    if let Some(cursor_position) = winfo.get_primary().unwrap().cursor_position() {
+        if mouse_button_input.pressed(MouseButton::Left) {
+            for (mut transf, cam) in query.iter_mut() {
+                // Calculate the delta in mouse position when it has been held down.
+                let delta: Vec2 = (*prev_position - cursor_position) * cam.scale;
+
+                //Applying the motion Delta to the camera's transform
+                *transf = Transform::from_translation(transf.mul_vec3(delta.extend(0.0)));
+            }
+        }
+        *prev_position = cursor_position;
+    }
+}
+fn camera_scale(
+    mut mouse_wheel_events: EventReader<MouseWheel>,
+    mut query: Query<&mut OrthographicProjection, With<Camera>>,
+) {
+    // Query for mouse scroll events, and will apply delta scroll to camera's scale
+    for event in mouse_wheel_events.iter() {
+        for mut que in query.iter_mut() {
+            let mut log_scale = que.scale.ln();
+            log_scale += event.y * 0.5;
+            que.scale = log_scale.exp();
+        }
+    }
+}
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    commands.spawn_bundle(Camera2dBundle::default());
+
+    let square_size = 10;
+
+    // Quick loop to generate array of circles as a reference point for background
+    for x in -square_size..square_size {
+        for y in -square_size..square_size {
+            commands.spawn_bundle(MaterialMesh2dBundle {
+                mesh: meshes.add(shape::Circle::new(10.).into()).into(),
+                material: materials.add(ColorMaterial::from(Color::BLUE)),
+                transform: Transform::from_translation(Vec3::new(
+                    (x as f32) * 100.0,
+                    (y as f32) * 100.0,
+                    0.,
+                )),
+                ..default()
+            });
+        }
+    }
+}
