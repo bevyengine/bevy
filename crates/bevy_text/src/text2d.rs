14093cc85678e0bede192f4e54a8e646443c49f7
François <mockersf@gmail.com>
2022-10-06 00:06:21 +0200

    make Handle::<T>::id private

--- crates/bevy_text/src/text2d.rs
+++ crates/bevy_text/src/text2d.rs
@@ -134,7 +134,7 @@ pub fn extract_text2d_sprite(
                 color,
                 rect,
                 custom_size: None,
-                image_handle_id: handle.id,
+                image_handle_id: handle.id(),
                 flip_x: false,
                 flip_y: false,
                 anchor: Anchor::Center.as_vec(),
