2171d1beeef781c71dc3b47359013680dd38d95b
Alice Cecile <alice.i.cecile@gmail.com>
2022-10-10 19:21:58 -0400

    Rename system chaining to system piping

--- examples/ecs/system_chaining.rs
+++ examples/ecs/system_chaining.rs
@@ -7,7 +7,7 @@ use bevy::prelude::*;
 fn main() {
     App::new()
         .insert_resource(Message("42".to_string()))
-        .add_system(parse_message_system.chain(handler_system))
+        .add_system(parse_message_system.pipe(handler_system))
         .run();
 }
 
