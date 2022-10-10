4d3034dfd741a8f096e5289c599a97a350008fac
Pietrek14 <dawidekpe@gmail.com>
2022-10-10 23:44:24 +0200

    fixed the example

--- examples/ui/slider.rs
+++ examples/ui/slider.rs
@@ -1,7 +1,7 @@
 //! This example illustrates how to create a button that changes color and text based on its
 //! interaction state.
 
-use bevy::{prelude::*, winit::WinitSettings};
+use bevy::{prelude::*, ui::widget::Slider, winit::WinitSettings};
 
 fn main() {
     App::new()
@@ -9,6 +9,7 @@ fn main() {
         // Only run the app when there is user input. This will significantly reduce CPU/GPU use.
         .insert_resource(WinitSettings::desktop_app())
         .add_startup_system(setup)
+        .add_system(update)
         .run();
 }
 
@@ -16,31 +17,51 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     // ui camera
     commands.spawn(Camera2dBundle::default());
     commands
-        .spawn(SliderBundle {
+        .spawn(NodeBundle {
             style: Style {
-                size: Size::new(Val::Px(200.), Val::Px(20.)),
-                // center slider
-                margin: UiRect::all(Val::Auto),
+                size: Size::new(Val::Percent(100.), Val::Percent(100.)),
+                flex_direction: FlexDirection::ColumnReverse,
+                justify_content: JustifyContent::Center,
+                align_items: AlignItems::Center,
                 ..default()
             },
-            background_color: Color::rgb(0.8, 0.8, 0.8).into(),
             ..default()
         })
         .with_children(|parent| {
-            parent.spawn(SliderHandleBundle {
-                style: Style {
-                    size: Size::new(Val::Px(15.), Val::Px(20.)),
+            parent
+                .spawn(SliderBundle {
+                    style: Style {
+                        size: Size::new(Val::Px(200.), Val::Px(20.)),
+                        margin: UiRect::bottom(Val::Px(25.)),
+                        ..default()
+                    },
+                    background_color: Color::rgb(0.8, 0.8, 0.8).into(),
                     ..default()
+                })
+                .with_children(|parent| {
+                    parent.spawn(SliderHandleBundle {
+                        style: Style {
+                            size: Size::new(Val::Px(15.), Val::Px(20.)),
+                            ..default()
+                        },
+                        ..default()
+                    });
+                });
+
+            parent.spawn(TextBundle::from_section(
+                "0",
+                TextStyle {
+                    font: asset_server.load("fonts/FiraSans-Bold.ttf"),
+                    font_size: 40.0,
+                    color: Color::rgb(0.9, 0.9, 0.9),
                 },
-                ..default()
-            });
+            ));
         });
-    commands.spawn(TextBundle::from_section(
-        "0",
-        TextStyle {
-            font: asset_server.load("fonts/FiraSans-Bold.ttf"),
-            font_size: 40.0,
-            color: Color::rgb(0.9, 0.9, 0.9),
-        },
-    ));
+}
+
+fn update(slider_query: Query<&Slider>, mut text_query: Query<&mut Text>) {
+    let slider = slider_query.single();
+    let mut text = text_query.single_mut();
+
+    text.sections[0].value = format!("{}", slider.get_value().round());
 }
