c0a93aa7a4d612cb3cdc3ef63c089ed032bed513
Alice Cecile <alice.i.cecile@gmail.com>
2022-10-11 15:21:12 +0000

    Rename system chaining to system piping (#6230)
    # Objective
    
    > System chaining is a confusing name: it implies the ability to construct non-linear graphs, and suggests a sense of system ordering that is only incidentally true. Instead, it actually works by passing data from one system to the next, much like the pipe operator.
    
    > In the accepted [stageless RFC](https://github.com/bevyengine/rfcs/blob/main/rfcs/45-stageless.md), this concept is renamed to piping, and "system chaining" is used to construct groups of systems with ordering dependencies between them.
    
    Fixes #6225.
    
    ## Changelog
    
    System chaining has been renamed to system piping to improve clarity (and free up the name for new ordering APIs). 
    
    ## Migration Guide
    
    The `.chain(handler_system)` method on systems is now `.pipe(handler_system)`.
    The `IntoChainSystem` trait is now `IntoPipeSystem`, and the `ChainSystem` struct is now `PipeSystem`.

--- examples/ecs/system_piping.rs
+++ examples/ecs/system_piping.rs
@@ -1,5 +1,5 @@
-//! Illustrates how to make a single system from multiple functions running in sequence and sharing
-//! their inputs and outputs.
+//! Illustrates how to make a single system from multiple functions running in sequence,
+//! passing the output of the first into the input of the next.
 
 use anyhow::Result;
 use bevy::prelude::*;
@@ -7,7 +7,7 @@ use bevy::prelude::*;
 fn main() {
     App::new()
         .insert_resource(Message("42".to_string()))
-        .add_system(parse_message_system.chain(handler_system))
+        .add_system(parse_message_system.pipe(handler_system))
         .run();
 }
 
