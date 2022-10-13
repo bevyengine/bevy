c0d528a1928db81ca3b456c2a17143f57a8d6223
JMS55 <47158642+JMS55@users.noreply.github.com>
2022-10-13 14:19:52 -0400

    Misc fixes

--- examples/3d/3d_scene.rs
+++ examples/3d/3d_scene.rs
@@ -99,10 +99,10 @@ fn setup(
         ..default()
     });
     // camera
-    commands.spawn((Camera3dBundle {
+    commands.spawn(Camera3dBundle {
         transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
         ..default()
-    },));
+    });
 }
 
 #[derive(Component)]
