1e7cb757a6e4bd3565edf94c7d3643bf680f4e87
JMS55 <47158642+JMS55@users.noreply.github.com>
2022-10-13 13:49:21 -0400

    Remove PrepassSettings from example/3d_scene.rs

--- examples/3d/3d_scene.rs
+++ examples/3d/3d_scene.rs
@@ -1,7 +1,6 @@
 //! A simple 3D scene with light shining over a cube sitting on a plane.
 
 use bevy::{
-    core_pipeline::core_3d::PrepassSettings,
     prelude::*,
     reflect::TypeUuid,
     render::render_resource::{AsBindGroup, ShaderRef},
@@ -100,13 +99,10 @@ fn setup(
         ..default()
     });
     // camera
-    commands.spawn((
-        Camera3dBundle {
-            transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
-            ..default()
-        },
-        PrepassSettings::default(),
-    ));
+    commands.spawn((Camera3dBundle {
+        transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+        ..default()
+    },));
 }
 
 #[derive(Component)]
