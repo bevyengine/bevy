c2476ed9976654a9d1957e617b11aad561655b00
james7132 <contact@jamessliu.com>
2022-10-17 19:53:34 -0700

    Update safety docs on get_ticks

--- crates/bevy_ecs/src/storage/resource.rs
+++ crates/bevy_ecs/src/storage/resource.rs
@@ -36,7 +36,10 @@ impl ResourceData {
     pub fn get_ticks(&self) -> Option<&ComponentTicks> {
         self.column
             .get_ticks(0)
-            // SAFETY: If the first row exists, a valid ticks value has been written.
+            // SAFETY: 
+            //  - This borrow's lifetime is bounded by the lifetime on self.
+            //  - A read-only borrow on self can only exist while a mutable borrow doesn't 
+            //    exist.
             .map(|ticks| unsafe { ticks.deref() })
     }
 
