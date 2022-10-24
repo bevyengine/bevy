48ca0e96cfafee1823ea3d05892465d0d4aafb88
Mike <mike.hsu@gmail.com>
2022-10-24 13:46:40 +0000

    tick local executor (#6121)
    # Objective
    
    - #4466 broke local tasks running.
    - Fixes https://github.com/bevyengine/bevy/issues/6120
    
    ## Solution
    
    - Add system for ticking local executors on main thread into bevy_core where the tasks pools are initialized.
    - Add ticking local executors into thread executors
    
    ## Changelog
    
    - tick all thread local executors in task pool.
    
    ## Notes
    
    - ~~Not 100% sure about this PR. Ticking the local executor for the main thread in scope feels a little kludgy as it requires users of bevy_tasks to be calling scope periodically for those tasks to make progress.~~ took this out in favor of a system that ticks the local executors.

--- crates/bevy_tasks/src/task_pool.rs
+++ crates/bevy_tasks/src/task_pool.rs
@@ -8,7 +8,7 @@ use std::{
 };
 
 use concurrent_queue::ConcurrentQueue;
-use futures_lite::{future, pin};
+use futures_lite::{future, pin, FutureExt};
 
 use crate::Task;
 
@@ -117,9 +117,16 @@ impl TaskPool {
 
                 thread_builder
                     .spawn(move || {
-                        let shutdown_future = ex.run(shutdown_rx.recv());
-                        // Use unwrap_err because we expect a Closed error
-                        future::block_on(shutdown_future).unwrap_err();
+                        TaskPool::LOCAL_EXECUTOR.with(|local_executor| {
+                            let tick_forever = async move {
+                                loop {
+                                    local_executor.tick().await;
+                                }
+                            };
+                            let shutdown_future = ex.run(tick_forever.or(shutdown_rx.recv()));
+                            // Use unwrap_err because we expect a Closed error
+                            future::block_on(shutdown_future).unwrap_err();
+                        });
                     })
                     .expect("Failed to spawn thread.")
             })
@@ -313,6 +320,24 @@ impl TaskPool {
         T: 'static,
     {
         Task::new(TaskPool::LOCAL_EXECUTOR.with(|executor| executor.spawn(future)))
+    }
+
+    /// Runs a function with the local executor. Typically used to tick
+    /// the local executor on the main thread as it needs to share time with
+    /// other things.
+    ///
+    /// ```rust
+    /// use bevy_tasks::TaskPool;
+    ///
+    /// TaskPool::new().with_local_executor(|local_executor| {
+    ///     local_executor.try_tick();
+    /// });
+    /// ```
+    pub fn with_local_executor<F, R>(&self, f: F) -> R
+    where
+        F: FnOnce(&async_executor::LocalExecutor) -> R,
+    {
+        Self::LOCAL_EXECUTOR.with(f)
     }
 }
 
--- crates/bevy_tasks/src/usages.rs
+++ crates/bevy_tasks/src/usages.rs
@@ -109,3 +109,29 @@ impl Deref for IoTaskPool {
         &self.0
     }
 }
+
+/// Used by `bevy_core` to tick the global tasks pools on the main thread.
+/// This will run a maximum of 100 local tasks per executor per call to this function.
+#[cfg(not(target_arch = "wasm32"))]
+pub fn tick_global_task_pools_on_main_thread() {
+    COMPUTE_TASK_POOL
+        .get()
+        .unwrap()
+        .with_local_executor(|compute_local_executor| {
+            ASYNC_COMPUTE_TASK_POOL
+                .get()
+                .unwrap()
+                .with_local_executor(|async_local_executor| {
+                    IO_TASK_POOL
+                        .get()
+                        .unwrap()
+                        .with_local_executor(|io_local_executor| {
+                            for _ in 0..100 {
+                                compute_local_executor.try_tick();
+                                async_local_executor.try_tick();
+                                io_local_executor.try_tick();
+                            }
+                        });
+                });
+        });
+}
