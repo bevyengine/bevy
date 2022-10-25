c6f27eb0547ce909fa01ff063ec9a3e531c14564
Carter Weinberg <weinbergcarter@gmail.com>
2022-10-25 00:19:23 +0000

    Add More Description to the Iter Combinations Documentation (#6260)
    # Objective
    
    I was trying to implement a collision system for my game, and believed that the iter_combinations method might be what I need. But I couldn't find a simple explanation of what a combination was in Bevy and thought it could use some more explanation. 
    
    ## Solution
    
    I added some description to the documentation that can hopefully further elaborate on what a combination is. 
    
    I also changed up the docs for the method because a combination is a different thing than a permutation but the Bevy docs seemed to use them interchangeably.

--- crates/bevy_ecs/src/query/iter.rs
+++ crates/bevy_ecs/src/query/iter.rs
@@ -219,11 +219,21 @@ where
 
 /// An iterator over `K`-sized combinations of query items without repetition.
 ///
-/// In this context, a combination is an unordered subset of `K` elements.
-/// The number of combinations depend on how `K` relates to the number of entities matching the [`Query`] (called `N`):
+/// A combination is an arrangement of a collection of items where order does not matter.
+///
+/// `K` is the number of items that make up each subset, and the number of items returned by the iterator.
+/// `N` is the number of total entities output by the query.
+///
+/// For example, given the list [1, 2, 3, 4], where `K` is 2, the combinations without repeats are
+/// [1, 2], [1, 3], [1, 4], [2, 3], [2, 4], [3, 4].
+/// And in this case, `N` would be defined as 4 since the size of the input list is 4.
+///
+/// The number of combinations depend on how `K` relates to the number of entities matching the [`Query`]:
 /// - if `K = N`, only one combination exists,
 /// - if `K < N`, there are <sub>N</sub>C<sub>K</sub> combinations (see the [performance section] of `Query`),
 /// - if `K > N`, there are no combinations.
+///
+/// The output combination is not guaranteed to have any order of iteration.
 ///
 /// # Usage
 ///
