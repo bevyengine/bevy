76cbcba4680c516976fc840f4bed9b495bc279a3
amiani <amianijohns@gmail.com>
2022-10-31 07:24:28 -0400

    add serialize feature to bevy_transform

--- crates/bevy_transform/src/components/transform.rs
+++ crates/bevy_transform/src/components/transform.rs
@@ -35,6 +35,7 @@ use std::ops::Mul;
 /// [`global_vs_local_translation`]: https://github.com/bevyengine/bevy/blob/latest/examples/transforms/global_vs_local_translation.rs
 /// [`transform`]: https://github.com/bevyengine/bevy/blob/latest/examples/transforms/transform.rs
 #[derive(Component, Debug, PartialEq, Clone, Copy, Reflect, FromReflect)]
+#[cfg_attr(feature = "serialize", derive(serde::Serialize, serde::Deserialize))]
 #[reflect(Component, Default, PartialEq)]
 pub struct Transform {
     /// Position of the entity. In 2d, the last value of the `Vec3` is used for z-ordering.
