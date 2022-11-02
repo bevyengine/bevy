4b7bbfef9e2fd6fa5f7ef5d1fa74b679848226f4
james7132 <contact@jamessliu.com>
2022-11-02 16:30:05 -0700

    Revert bevy_transform changes

--- crates/bevy_transform/src/systems.rs
+++ crates/bevy_transform/src/systems.rs
@@ -318,7 +318,10 @@ mod test {
         let mut temp = World::new();
         let mut app = App::new();
 
-        app.add_system(transform_propagate_system);
+        // FIXME: Parallel executors seem to have some odd interaction with the other
+        // tests in this crate. Using single_threaded until a root cause can be found.
+        app.add_stage("single", SystemStage::single_threaded())
+            .add_system_to_stage("single", transform_propagate_system);
 
         fn setup_world(world: &mut World) -> (Entity, Entity) {
             let mut grandchild = Entity::from_raw(0);
