da5b9ef36c0e3c638d20ab91fa1cf0cee240432b
James Liu <contact@jamessliu.com>
2022-11-02 13:47:55 -0700

    Remove unnecessary stage from test.

--- crates/bevy_transform/src/systems.rs
+++ crates/bevy_transform/src/systems.rs
@@ -318,8 +318,7 @@ mod test {
         let mut temp = World::new();
         let mut app = App::new();
 
-        app.add_stage("single", SystemStage::parallel())
-            .add_system_to_stage("single", transform_propagate_system);
+        app.add_system_to_stage("single", transform_propagate_system);
 
         fn setup_world(world: &mut World) -> (Entity, Entity) {
             let mut grandchild = Entity::from_raw(0);
