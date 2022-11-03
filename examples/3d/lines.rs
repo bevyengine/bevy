262b3fc40d7c4171dd585f7127cfb54573edb170
ira <JustTheCoolDude@gmail.com>
2022-11-03 12:38:47 +0000

    Fix `mesh.wgsl` error for meshes without normals (#6439)
    # Objective
    Split `model` assignment out of `#ifdef VERTEX_NORMALS`.
    Remove outdated code/comments talking about required mesh attributes. 
    
    Co-authored-by: devil-ira <justthecooldude@gmail.com>

--- examples/3d/lines.rs
+++ examples/3d/lines.rs
@@ -95,12 +95,9 @@ pub struct LineList {
 impl From<LineList> for Mesh {
     fn from(line: LineList) -> Self {
         let mut vertices = vec![];
-        let mut normals = vec![];
         for (start, end) in line.lines {
             vertices.push(start.to_array());
             vertices.push(end.to_array());
-            normals.push(Vec3::ZERO.to_array());
-            normals.push(Vec3::ZERO.to_array());
         }
 
         // This tells wgpu that the positions are list of lines
@@ -108,8 +105,6 @@ impl From<LineList> for Mesh {
         let mut mesh = Mesh::new(PrimitiveTopology::LineList);
 
         mesh.insert_attribute(Mesh::ATTRIBUTE_POSITION, vertices);
-        // Normals are currently required by bevy, but they aren't used by the [`LineMaterial`]
-        mesh.insert_attribute(Mesh::ATTRIBUTE_NORMAL, normals);
         mesh
     }
 }
@@ -123,10 +118,8 @@ pub struct LineStrip {
 impl From<LineStrip> for Mesh {
     fn from(line: LineStrip) -> Self {
         let mut vertices = vec![];
-        let mut normals = vec![];
         for pos in line.points {
             vertices.push(pos.to_array());
-            normals.push(Vec3::ZERO.to_array());
         }
 
         // This tells wgpu that the positions are a list of points
@@ -134,8 +127,6 @@ impl From<LineStrip> for Mesh {
         let mut mesh = Mesh::new(PrimitiveTopology::LineStrip);
 
         mesh.insert_attribute(Mesh::ATTRIBUTE_POSITION, vertices);
-        // Normals are currently required by bevy, but they aren't used by the [`LineMaterial`]
-        mesh.insert_attribute(Mesh::ATTRIBUTE_NORMAL, normals);
         mesh
     }
 }
