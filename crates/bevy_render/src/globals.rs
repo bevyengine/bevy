40743d20432d44626bdc11482ffe41d7e733978b
Rob Parrett <robparrett@gmail.com>
2022-11-04 11:05:04 -0700

    Fix typo

--- crates/bevy_render/src/globals.rs
+++ crates/bevy_render/src/globals.rs
@@ -41,7 +41,7 @@ pub struct GlobalsUniform {
     /// Frame count since the start of the app.
     /// It wraps to zero when it reaches the maximum value of a u32.
     frame_count: u32,
-    /// Webgl2 structs must be 16 byte aligned.
+    /// WebGL2 structs must be 16 byte aligned.
     #[cfg(feature = "webgl")]
     _wasm_padding: f32,
 }
