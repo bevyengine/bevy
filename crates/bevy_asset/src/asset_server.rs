d407e95ccd5fad73ba11bc1f4a116a1d68f48f9d
targrub <targrub@gmail.com>
2022-11-05 10:53:02 -0400

    Fix `clippy::useless_let_if_seq` / `clippy::equatable_if_let`

--- crates/bevy_asset/src/asset_server.rs
+++ crates/bevy_asset/src/asset_server.rs
@@ -512,7 +512,7 @@ impl AssetServer {
             let asset_sources = self.server.asset_sources.read();
             let asset_lifecycles = self.server.asset_lifecycles.read();
             for potential_free in potential_frees.drain(..) {
-                if let Some(&0) = ref_counts.get(&potential_free) {
+                if ref_counts.get(&potential_free) == Some(&0) {
                     let type_uuid = match potential_free {
                         HandleId::Id(type_uuid, _) => Some(type_uuid),
                         HandleId::AssetPathId(id) => asset_sources
