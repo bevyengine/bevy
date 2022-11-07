8e69ca9940e642e503209d39ae83db27c5687ae0
James Liu <contact@jamessliu.com>
2022-11-07 02:50:46 -0800

    Fix docs.
    Co-authored-by: François <mockersf@gmail.com>

--- crates/bevy_animation/src/util.rs
+++ crates/bevy_animation/src/util.rs
@@ -1,5 +1,5 @@
 /// Steps between two different discrete values of any clonable type.
-/// Returns a copy of `b` if `t >= 1.0`, otherwise returns a copy of `b`.
+/// Returns a copy of `b` if `t >= 1.0`, otherwise returns a copy of `a`.
 #[inline]
 pub(crate) fn step_unclamped<T>(a: T, b: T, t: f32) -> T {
     if t >= 1.0 {
