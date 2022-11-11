ad38f8867294a015b56cc5a6f3489a796b918855
devil-ira <justthecooldude@gmail.com>
2022-11-11 14:06:23 +0100

    inline

--- crates/bevy_debug_draw/src/debug_draw.rs
+++ crates/bevy_debug_draw/src/debug_draw.rs
@@ -31,11 +31,13 @@ impl Default for DebugDraw {
 
 impl DebugDraw {
     /// Draw a line from `start` to `end`.
+    #[inline]
     pub fn line(&mut self, start: Vec3, end: Vec3, color: Color) {
         self.line_gradient(start, end, color, color);
     }
 
     /// Draw a line from `start` to `end`.
+    #[inline]
     pub fn line_gradient(&mut self, start: Vec3, end: Vec3, start_color: Color, end_color: Color) {
         self.positions.extend([start.to_array(), end.to_array()]);
         self.colors.extend([
@@ -45,11 +47,13 @@ impl DebugDraw {
     }
 
     /// Draw a line from `start` to `start + vector`.
+    #[inline]
     pub fn ray(&mut self, start: Vec3, vector: Vec3, color: Color) {
         self.ray_gradient(start, vector, color, color);
     }
 
     /// Draw a line from `start` to `start + vector`.
+    #[inline]
     pub fn ray_gradient(
         &mut self,
         start: Vec3,
@@ -61,6 +65,7 @@ impl DebugDraw {
     }
 
     /// Draw a circle at `position` with the flat side facing `normal`.
+    #[inline]
     pub fn circle(&mut self, position: Vec3, normal: Vec3, radius: f32, color: Color) {
         let rotation = Quat::from_rotation_arc(Vec3::Z, normal);
         self.positions
@@ -80,6 +85,7 @@ impl DebugDraw {
     }
 
     /// Draw a sphere.
+    #[inline]
     pub fn sphere(&mut self, position: Vec3, radius: f32, color: Color) {
         self.circle(position, Vec3::X, radius, color);
         self.circle(position, Vec3::Y, radius, color);
@@ -87,6 +93,7 @@ impl DebugDraw {
     }
 
     /// Draw a rectangle.
+    #[inline]
     pub fn rect(&mut self, position: Vec3, rotation: Quat, size: Vec2, color: Color) {
         let half_size = size / 2.;
         let tl = (position + rotation * vec3(-half_size.x, half_size.y, 0.)).to_array();
@@ -99,6 +106,7 @@ impl DebugDraw {
     }
 
     /// Draw a box.
+    #[inline]
     pub fn cuboid(&mut self, position: Vec3, rotation: Quat, size: Vec3, color: Color) {
         let half_size = size / 2.;
         // Front
@@ -121,11 +129,13 @@ impl DebugDraw {
     }
 
     /// Draw a line from `start` to `end`.
+    #[inline]
     pub fn line_2d(&mut self, start: Vec2, end: Vec2, color: Color) {
         self.line_gradient_2d(start, end, color, color);
     }
 
     /// Draw a line from `start` to `end`.
+    #[inline]
     pub fn line_gradient_2d(
         &mut self,
         start: Vec2,
@@ -137,11 +147,13 @@ impl DebugDraw {
     }
 
     /// Draw a line from `start` to `start + vector`.
+    #[inline]
     pub fn ray_2d(&mut self, start: Vec2, vector: Vec2, color: Color) {
-        self.ray(start.extend(0.), vector.extend(0.), color);
+        self.ray_gradient_2d(start, vector, color, color);
     }
 
     /// Draw a line from `start` to `start + vector`.
+    #[inline]
     pub fn ray_gradient_2d(
         &mut self,
         start: Vec2,
@@ -153,11 +165,13 @@ impl DebugDraw {
     }
 
     // Draw a circle.
+    #[inline]
     pub fn circle_2d(&mut self, position: Vec2, radius: f32, color: Color) {
         self.circle(position.extend(0.), Vec3::Z, radius, color);
     }
 
     /// Draw a rectangle.
+    #[inline]
     pub fn rect_2d(&mut self, position: Vec2, rotation: f32, size: Vec2, color: Color) {
         self.rect(
             position.extend(0.),
@@ -168,12 +182,14 @@ impl DebugDraw {
     }
 
     /// Clear everything drawn up to this point, this frame.
+    #[inline]
     pub fn clear(&mut self) {
         self.positions.clear();
         self.colors.clear();
     }
 
     /// Take the positions and colors data from `self` and overwrite the `mesh`'s vertex positions and colors.
+    #[inline]
     pub fn update_mesh(&mut self, mesh: &mut Mesh) {
         mesh.insert_attribute(
             Mesh::ATTRIBUTE_POSITION,
