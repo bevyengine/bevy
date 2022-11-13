374605172a8bf80d6354217ad4914dbc7320e0df
james7132 <contact@jamessliu.com>
2022-11-13 02:57:23 -0800

    Make bevy_ecs no_std

--- crates/bevy_ecs/src/lib.rs
+++ crates/bevy_ecs/src/lib.rs
@@ -1,8 +1,12 @@
 #![warn(clippy::undocumented_unsafe_blocks)]
 #![doc = include_str!("../README.md")]
 
+#[no_std]
+
 #[cfg(target_pointer_width = "16")]
 compile_error!("bevy_ecs cannot safely compile for a 16-bit platform.");
+
+extern crate alloc;
 
 pub mod archetype;
 pub mod bundle;
