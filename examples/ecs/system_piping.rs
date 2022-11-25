d7e967a7c822a32b2daea19be2e18bae29892712
Edvin Kjell <edvin.kjell@hotmail.com>
2022-11-25 13:24:55 +0100

    Modified ``system_piping.rs`` example

--- examples/ecs/system_piping.rs
+++ examples/ecs/system_piping.rs
@@ -4,10 +4,22 @@
 use anyhow::Result;
 use bevy::prelude::*;
 
+use bevy::utils::tracing::Level;
+use bevy::log::LogPlugin;
+
+
 fn main() {
     App::new()
-        .insert_resource(Message("42".to_string()))
+        .insert_resource(Message("42A".to_string()))
+        .add_plugin(LogPlugin {
+            level: Level::TRACE,
+            filter: "".to_string(),
+        })
         .add_system(parse_message_system.pipe(handler_system))
+        .add_system(data_pipe_system.pipe(system_adapter::info))
+        .add_system(parse_message_system.pipe(system_adapter::dbg))
+        .add_system(warning_pipe_system.pipe(system_adapter::warn))
+        .add_system(parse_message_system.pipe(system_adapter::error))
         .run();
 }
 
@@ -26,5 +38,17 @@ fn handler_system(In(result): In<Result<usize>>) {
     match result {
         Ok(value) => println!("parsed message: {value}"),
         Err(err) => println!("encountered an error: {err:?}"),
+    }
+}
+
+fn data_pipe_system(message: Res<Message>) -> String {
+    message.0.clone()
+}
+
+fn warning_pipe_system(message: Res<Message>) -> Option<String> {
+
+    match message.0.as_str() {
+        "No warning" => None,
+        x => Some(x.to_string()),
     }
 }
