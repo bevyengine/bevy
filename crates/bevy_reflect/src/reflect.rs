708e320c41aca44b7229096b302c1ab3480d859e
Feyo Korenhof <feyokorenhof@gmail.com>
2022-11-26 20:22:53 +0100

    added documentation draft for try_apply

--- crates/bevy_reflect/src/reflect.rs
+++ crates/bevy_reflect/src/reflect.rs
@@ -159,6 +159,53 @@ pub trait Reflect: Any + Send + Sync {
     /// - If `T` is a value type and `self` cannot be downcast to `T`
     fn apply(&mut self, value: &dyn Reflect);
 
+    /// Tries to apply a reflected value to this value and returns a result.
+    ///
+    /// Functions the same as the `apply` function but returns an error instead of
+    /// panicking.
+    ///
+    /// Note: the value you call this function on will end up being partially mutated
+    /// if this function runs into an error along the way (types with nested values like lists).
+    ///
+    /// If a type implements a subtrait of `Reflect`, then the semantics of this
+    /// method are as follows:
+    /// - If `T` is a [`Struct`], then the value of each named field of `value` is
+    ///   applied to the corresponding named field of `self`. Fields which are
+    ///   not present in both structs are ignored.
+    /// - If `T` is a [`TupleStruct`] or [`Tuple`], then the value of each
+    ///   numbered field is applied to the corresponding numbered field of
+    ///   `self.` Fields which are not present in both values are ignored.
+    /// - If `T` is an [`Enum`], then the variant of `self` is `updated` to match
+    ///   the variant of `value`. The corresponding fields of that variant are
+    ///   applied from `value` onto `self`. Fields which are not present in both
+    ///   values are ignored.
+    /// - If `T` is a [`List`] or [`Array`], then each element of `value` is applied
+    ///   to the corresponding element of `self`. Up to `self.len()` items are applied,
+    ///   and excess elements in `value` are appended to `self`.
+    /// - If `T` is a [`Map`], then for each key in `value`, the associated
+    ///   value is applied to the value associated with the same key in `self`.
+    ///   Keys which are not present in `self` are inserted.
+    /// - If `T` is none of these, then `value` is downcast to `T`, cloned, and
+    ///   assigned to `self`.
+    ///
+    /// Note that `Reflect` must be implemented manually for [`List`]s and
+    /// [`Map`]s in order to achieve the correct semantics, as derived
+    /// implementations will have the semantics for [`Struct`], [`TupleStruct`], [`Enum`]
+    /// or none of the above depending on the kind of type. For lists and maps, use the
+    /// [`list_apply`] and [`map_apply`] helper functions when implementing this method.
+    ///
+    /// [`list_apply`]: crate::list_apply
+    /// [`map_apply`]: crate::map_apply
+    ///
+    /// # Errors
+    ///
+    /// Derived implementations of this method will return an error:
+    /// - If the type of `value` is not of the same kind as `T` (e.g. if `T` is
+    ///   a `List`, while `value` is a `Struct`).
+    /// - If `T` is any complex type and the corresponding fields or elements of
+    ///   `self` and `value` are not of the same type.
+    /// - If `T` is a value type and `self` cannot be downcast to `T`
+
     fn try_apply(&mut self, value: &dyn Reflect) -> Result<(), ApplyError>;
 
     /// Performs a type-checked assignment of a reflected value to this value.
