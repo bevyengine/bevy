ddc4b1e9d05ff9b00e3ef46ad9989c026c7afec3
JoJoJet <21144246+JoJoJet@users.noreply.github.com>
2022-11-26 03:07:42 -0500

    add a word

--- crates/bevy_ecs/src/world/mod.rs
+++ crates/bevy_ecs/src/world/mod.rs
@@ -1401,7 +1401,7 @@ impl World {
     /// Reads the current change tick of this world.
     ///
     /// This does the same thing as [`Self::read_change_tick`], only this method
-    /// is more efficient since it does not require atomics.
+    /// is more efficient since it does not require atomic synchronization.
     #[inline]
     pub fn change_tick(&mut self) -> u32 {
         *self.change_tick.get_mut()
