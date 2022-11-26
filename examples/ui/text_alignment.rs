7bb6e52908256be7dd6f3708e2109ffd22129abc
Ickshonpe <david.curthoys@googlemail.com>
2022-11-26 01:12:38 +0000

    simplified example

--- examples/ui/text_alignment.rs
+++ examples/ui/text_alignment.rs
@@ -18,62 +18,31 @@ fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_startup_system(setup)
-        .add_system(update)
         .run();
 }
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn(Camera2dBundle::default());
-    commands
-        .spawn(NodeBundle {
-            style: Style {
-                align_items: AlignItems::Center,
-                justify_content: JustifyContent::Center,
-                size: Size::new(Val::Percent(100.), Val::Percent(100.)),
-                ..Default::default()
-            },
-            ..Default::default()
-        })
-        .with_children(|builder| {
-            builder
-                .spawn(NodeBundle {
-                    style: Style {
-                        size: Size::new(Val::Percent(80.), Val::Percent(80.)),
-                        ..Default::default()
-                    },
-                    background_color: BackgroundColor(Color::NAVY),
+    for x in 0..3 {
+        for y in 0..3 {
+            let alignment = ALIGNMENTS[x + 3 * y];
+            commands.spawn((
+                TextBundle::from_section(
+                    format!("{:?}\n{:?}", alignment.vertical, alignment.horizontal), 
+                     TextStyle {
+                        font: asset_server.load("fonts/FiraSans-Bold.ttf"),
+                        font_size: 24.0,
+                        color: Color::WHITE,
+                     }
+                )
+                .with_text_alignment(alignment)
+                .with_style(Style {
+                    position_type: PositionType::Absolute,
+                    position: UiRect { left: Val::Px(x as f32 * 210. + 5.), top: Val::Px(y as f32 * 210. + 5.), ..Default::default() },
+                    size: Size::new(Val::Px(200.), Val::Px(200.)),
                     ..Default::default()
-                })
-                .with_children(|builder| {
-                    builder.spawn(TextBundle {
-                        text: Text::from_section(
-                            "".to_string(),
-                            TextStyle {
-                                font: asset_server.load("fonts/FiraSans-Bold.ttf"),
-                                font_size: 24.0,
-                                color: Color::WHITE,
-                            },
-                        ),
-                        ..Default::default()
-                    });
-                });
-        });
-}
-
-pub fn update(
-    mut t: Local<f32>,
-    mut i: Local<usize>,
-    time: Res<Time>,
-    mut text_query: Query<&mut Text>,
-) {
-    *t -= time.delta_seconds();
-    if *t <= 0. {
-        *t = 0.5;
-        *i = (*i + 1) % ALIGNMENTS.len();
-        let mut text = text_query.single_mut();
-        text.alignment = ALIGNMENTS[*i];
-        text.sections[0].value = format!(
-            "{:?}-{:?}",
-            text.alignment.vertical, text.alignment.horizontal
-        );
+                }),
+                BackgroundColor(Color::NAVY)
+            ));
+        }
     }
 }
