e7de76b27d942f5c858a5b0568e8b4cb574f97b7
Ickshonpe <david.curthoys@googlemail.com>
2022-11-26 01:13:08 +0000

    cargo fmt

--- examples/ui/text_alignment.rs
+++ examples/ui/text_alignment.rs
@@ -27,21 +27,25 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
             let alignment = ALIGNMENTS[x + 3 * y];
             commands.spawn((
                 TextBundle::from_section(
-                    format!("{:?}\n{:?}", alignment.vertical, alignment.horizontal), 
-                     TextStyle {
+                    format!("{:?}\n{:?}", alignment.vertical, alignment.horizontal),
+                    TextStyle {
                         font: asset_server.load("fonts/FiraSans-Bold.ttf"),
                         font_size: 24.0,
                         color: Color::WHITE,
-                     }
+                    },
                 )
                 .with_text_alignment(alignment)
                 .with_style(Style {
                     position_type: PositionType::Absolute,
-                    position: UiRect { left: Val::Px(x as f32 * 210. + 5.), top: Val::Px(y as f32 * 210. + 5.), ..Default::default() },
+                    position: UiRect {
+                        left: Val::Px(x as f32 * 210. + 5.),
+                        top: Val::Px(y as f32 * 210. + 5.),
+                        ..Default::default()
+                    },
                     size: Size::new(Val::Px(200.), Val::Px(200.)),
                     ..Default::default()
                 }),
-                BackgroundColor(Color::NAVY)
+                BackgroundColor(Color::NAVY),
             ));
         }
     }
