16b94dcff9b40614495967cfd3fec289fb185556
james7132 <contact@jamessliu.com>
2022-11-27 01:04:06 -0800

    Undo test changes to empty.rs

--- examples/app/empty.rs
+++ examples/app/empty.rs
@@ -2,17 +2,6 @@
 
 use bevy::prelude::*;
 
-#[derive(Component)]
-struct A(f32);
-
-#[derive(Component)]
-struct B(f32);
-
-#[no_mangle]
-fn test(mut query: Query<(&mut A, &B)>) {
-    query.iter_mut().for_each(|(mut a, b)| {
-        a.0 += b.0;
-    });
+fn main() {
+    App::new().run()
 }
-
-fn main() {}
