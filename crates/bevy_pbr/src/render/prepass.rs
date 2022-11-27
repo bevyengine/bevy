294ed2a3f6674d0162ea296a28bc1502a7db594d
JMS55 <47158642+JMS55@users.noreply.github.com>
2022-11-27 13:48:46 -0500

    Add missing system to stage

--- crates/bevy_pbr/src/render/prepass.rs
+++ crates/bevy_pbr/src/render/prepass.rs
@@ -95,6 +95,7 @@ where
         render_app
             .add_system_to_stage(RenderStage::Extract, extract_camera_prepass_components)
             .add_system_to_stage(RenderStage::Prepare, prepare_prepass_textures)
+            .add_system_to_stage(RenderStage::Prepare, prepare_previous_view_projection_uniforms)
             .add_system_to_stage(RenderStage::Queue, queue_prepass_view_bind_group::<M>)
             .add_system_to_stage(RenderStage::Queue, queue_prepass_material_meshes::<M>)
             .add_system_to_stage(RenderStage::PhaseSort, sort_phase_system::<Opaque3dPrepass>)
