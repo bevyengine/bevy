2f2df97dcc1bd4c69f01a13fcf1f03d00e881033
james7132 <contact@jamessliu.com>
2022-11-27 23:16:34 -0800

    Getting it working

--- crates/bevy_animation/src/lib.rs
+++ crates/bevy_animation/src/lib.rs
@@ -8,8 +8,10 @@ use bevy_app::{App, CoreStage, Plugin};
 use bevy_asset::{AddAsset, Assets, Handle};
 use bevy_core::Name;
 use bevy_ecs::{
+    prelude::*,
     change_detection::DetectChanges,
     entity::Entity,
+    storage::SparseSet,
     prelude::Component,
     reflect::ReflectComponent,
     schedule::IntoSystemDescriptor,
@@ -21,6 +23,10 @@ use bevy_reflect::{FromReflect, Reflect, TypeUuid};
 use bevy_time::Time;
 use bevy_transform::{prelude::Transform, TransformSystem};
 use bevy_utils::{tracing::warn, HashMap};
+use bevy_tasks::{ParallelSlice, ComputeTaskPool};
+use smallvec::{smallvec, SmallVec};
+use thread_local::ThreadLocal;
+use std::cell::Cell;
 
 #[allow(missing_docs)]
 pub mod prelude {
@@ -63,15 +69,26 @@ pub struct EntityPath {
 #[derive(Reflect, FromReflect, Clone, TypeUuid, Debug, Default)]
 #[uuid = "d81b7179-0448-4eb0-89fe-c067222725bf"]
 pub struct AnimationClip {
-    curves: HashMap<EntityPath, Vec<VariableCurve>>,
+    curves: Vec<Vec<VariableCurve>>,
+    paths: HashMap<EntityPath, usize>,
     duration: f32,
 }
 
 impl AnimationClip {
     #[inline]
     /// Hashmap of the [`VariableCurve`]s per [`EntityPath`].
-    pub fn curves(&self) -> &HashMap<EntityPath, Vec<VariableCurve>> {
+    pub fn curves(&self) -> &Vec<Vec<VariableCurve>> {
         &self.curves
+    }
+
+    #[inline]
+    pub fn get_curves(&self, bone_id: usize) -> Option<&'_ Vec<VariableCurve>> {
+        self.curves.get(bone_id)
+    }
+
+    #[inline]
+    pub fn get_curves_by_path(&self, path: &EntityPath) -> Option<&'_ Vec<VariableCurve>> {
+        self.paths.get(path).and_then(|id| self.curves.get(*id))
     }
 
     /// Duration of the clip, represented in seconds
@@ -86,7 +103,13 @@ impl AnimationClip {
         self.duration = self
             .duration
             .max(*curve.keyframe_timestamps.last().unwrap_or(&0.0));
-        self.curves.entry(path).or_default().push(curve);
+        if let Some(bone_id) = self.paths.get(&path) {
+            self.curves[*bone_id].push(curve);
+        } else {
+            let idx = self.curves.len();
+            self.curves.push(vec![curve]);
+            self.paths.insert(path, idx);
+        }
     }
 }
 
@@ -181,117 +204,179 @@ impl AnimationPlayer {
     }
 }
 
+#[derive(Clone)]
+pub struct BoneBinding {
+    root: Entity,
+    bone_id: usize,
+    elapsed: f32,
+}
+
+#[derive(Resource, Default)]
+pub struct BoneBindings {
+    // Entity must be globally unique.
+    bindings: Vec<(Entity, SmallVec<[BoneBinding; 2]>)>,
+}
+
+fn find_bone(root: Entity, path: &EntityPath, children: &Query<&Children>, names: &Query<&Name>) -> Option<Entity> {
+    // PERF: finding the target entity can be optimised
+    let mut current_entity = root;
+    // Ignore the first name, it is the root node which we already have
+    for part in path.parts.iter().skip(1) {
+        let mut found = false;
+        let children = children.get(current_entity).ok()?;
+        for child in children.deref() {
+            if let Ok(name) = names.get(*child) {
+                if name == part {
+                    // Found a children with the right name, continue to the next part
+                    current_entity = *child;
+                    found = true;
+                    break;
+                }
+            }
+        }
+        if !found {
+            warn!("Entity not found for path {:?} on part {:?}", path, part);
+            return None;
+        }
+    }
+    Some(current_entity)
+}
+
+pub fn bind_bones(
+    time: Res<Time>,
+    animations: Res<Assets<AnimationClip>>,
+    names: Query<&Name>,
+    children: Query<&Children>,
+    mut animation_players: Query<(Entity, &mut AnimationPlayer)>,
+    mut queues: Local<ThreadLocal<Cell<Vec<(Entity, BoneBinding)>>>>,
+    mut dedup: Local<SparseSet<Entity, usize>>,
+    mut bone_bindings: ResMut<BoneBindings>,
+) {
+    bone_bindings.bindings.clear();
+    animation_players.par_for_each_mut(100, |(root, mut player)| {
+        let Some(animation_clip) = animations.get(&player.animation_clip) else { return };
+        // Continue if paused unless the `AnimationPlayer` was changed
+        // This allow the animation to still be updated if the player.elapsed field was manually updated in pause
+        if player.paused && !player.is_changed() {
+            return;
+        }
+        if !player.paused {
+            player.elapsed += time.delta_seconds() * player.speed;
+        }
+        let mut elapsed = player.elapsed;
+        if player.repeat {
+            elapsed %= animation_clip.duration;
+        }
+        if elapsed < 0.0 {
+            elapsed += animation_clip.duration;
+        }
+        let queue_cell = queues.get_or_default();
+        let mut queue = queue_cell.take();
+        for (path, bone_id) in &animation_clip.paths {
+            if let Some(target) = find_bone(root, &path, &children, &names) { 
+                queue.push((target, BoneBinding { root, bone_id: *bone_id, elapsed }));
+            }
+        }
+        queue_cell.set(queue);
+    });
+    bone_bindings.bindings.clear();
+    for queue in queues.iter_mut() {
+        for (entity, binding) in queue.get_mut() {
+            if let Some(idx) = dedup.get(*entity) {
+                bone_bindings.bindings[*idx].1.push(binding.clone());
+            } else {
+                let idx = bone_bindings.bindings.len();
+                bone_bindings.bindings.push((*entity, smallvec![binding.clone()]));
+                dedup.insert(*entity, idx);
+            }
+        }
+        queue.get_mut().clear();
+    }
+    dedup.clear();
+}
+
 /// System that will play all animations, using any entity with a [`AnimationPlayer`]
 /// and a [`Handle<AnimationClip>`] as an animation root
 pub fn animation_player(
-    time: Res<Time>,
     animations: Res<Assets<AnimationClip>>,
-    mut animation_players: Query<(Entity, &mut AnimationPlayer)>,
-    names: Query<&Name>,
-    mut transforms: Query<&mut Transform>,
-    children: Query<&Children>,
+    animation_players: Query<&AnimationPlayer>,
+    transforms: Query<&mut Transform>,
+    mut bone_bindings: ResMut<BoneBindings>,
 ) {
-    for (entity, mut player) in &mut animation_players {
-        if let Some(animation_clip) = animations.get(&player.animation_clip) {
-            // Continue if paused unless the `AnimationPlayer` was changed
-            // This allow the animation to still be updated if the player.elapsed field was manually updated in pause
-            if player.paused && !player.is_changed() {
-                continue;
-            }
-            if !player.paused {
-                player.elapsed += time.delta_seconds() * player.speed;
-            }
-            let mut elapsed = player.elapsed;
-            if player.repeat {
-                elapsed %= animation_clip.duration;
-            }
-            if elapsed < 0.0 {
-                elapsed += animation_clip.duration;
-            }
-            'entity: for (path, curves) in &animation_clip.curves {
-                // PERF: finding the target entity can be optimised
-                let mut current_entity = entity;
-                // Ignore the first name, it is the root node which we already have
-                for part in path.parts.iter().skip(1) {
-                    let mut found = false;
-                    if let Ok(children) = children.get(current_entity) {
-                        for child in children.deref() {
-                            if let Ok(name) = names.get(*child) {
-                                if name == part {
-                                    // Found a children with the right name, continue to the next part
-                                    current_entity = *child;
-                                    found = true;
-                                    break;
-                                }
-                            }
-                        }
-                    }
-                    if !found {
-                        warn!("Entity not found for path {:?} on part {:?}", path, part);
-                        continue 'entity;
-                    }
-                }
-                if let Ok(mut transform) = transforms.get_mut(current_entity) {
-                    for curve in curves {
-                        // Some curves have only one keyframe used to set a transform
-                        if curve.keyframe_timestamps.len() == 1 {
-                            match &curve.keyframes {
-                                Keyframes::Rotation(keyframes) => transform.rotation = keyframes[0],
-                                Keyframes::Translation(keyframes) => {
-                                    transform.translation = keyframes[0];
-                                }
-                                Keyframes::Scale(keyframes) => transform.scale = keyframes[0],
-                            }
-                            continue;
-                        }
-
-                        // Find the current keyframe
-                        // PERF: finding the current keyframe can be optimised
-                        let step_start = match curve
-                            .keyframe_timestamps
-                            .binary_search_by(|probe| probe.partial_cmp(&elapsed).unwrap())
-                        {
-                            Ok(n) if n >= curve.keyframe_timestamps.len() - 1 => continue, // this curve is finished
-                            Ok(i) => i,
-                            Err(0) => continue, // this curve isn't started yet
-                            Err(n) if n > curve.keyframe_timestamps.len() - 1 => continue, // this curve is finished
-                            Err(i) => i - 1,
-                        };
-                        let ts_start = curve.keyframe_timestamps[step_start];
-                        let ts_end = curve.keyframe_timestamps[step_start + 1];
-                        let lerp = (elapsed - ts_start) / (ts_end - ts_start);
-
-                        // Apply the keyframe
+    if bone_bindings.bindings.is_empty() {
+        return;
+    }
+    let task_pool = &*ComputeTaskPool::get();
+    let thread_count = task_pool.thread_num().max(1);
+    let batch_size = bone_bindings.bindings.len() / thread_count;
+    bone_bindings.bindings.par_chunk_map(task_pool, batch_size, |chunk| {
+        for (target, bindings) in chunk {
+            // SAFETY: bind_bones ensures that every binding in BoneBindings is unique.
+            let Ok(mut transform) = (unsafe { transforms.get_unchecked(*target) }) else { continue };
+            for binding in bindings {
+                let Ok(animator) = animation_players.get(binding.root) else { continue };
+                let Some(animation_clip) = animations.get(&animator.animation_clip) else { continue };
+                let Some(curves) = animation_clip.get_curves(binding.bone_id) else { continue };
+                for curve in curves {
+                    // Some curves have only one keyframe used to set a transform
+                    if curve.keyframe_timestamps.len() == 1 {
                         match &curve.keyframes {
-                            Keyframes::Rotation(keyframes) => {
-                                let rot_start = keyframes[step_start];
-                                let mut rot_end = keyframes[step_start + 1];
-                                // Choose the smallest angle for the rotation
-                                if rot_end.dot(rot_start) < 0.0 {
-                                    rot_end = -rot_end;
-                                }
-                                // Rotations are using a spherical linear interpolation
-                                transform.rotation =
-                                    rot_start.normalize().slerp(rot_end.normalize(), lerp);
-                            }
+                            Keyframes::Rotation(keyframes) => transform.rotation = keyframes[0],
                             Keyframes::Translation(keyframes) => {
-                                let translation_start = keyframes[step_start];
-                                let translation_end = keyframes[step_start + 1];
-                                let result = translation_start.lerp(translation_end, lerp);
-                                transform.translation = result;
+                                transform.translation = keyframes[0];
                             }
-                            Keyframes::Scale(keyframes) => {
-                                let scale_start = keyframes[step_start];
-                                let scale_end = keyframes[step_start + 1];
-                                let result = scale_start.lerp(scale_end, lerp);
-                                transform.scale = result;
+                            Keyframes::Scale(keyframes) => transform.scale = keyframes[0],
+                        }
+                        continue;
+                    }
+
+                    // Find the current keyframe
+                    // PERF: finding the current keyframe can be optimised
+                    let step_start = match curve
+                        .keyframe_timestamps
+                        .binary_search_by(|probe| probe.partial_cmp(&binding.elapsed).unwrap())
+                    {
+                        Ok(n) if n >= curve.keyframe_timestamps.len() - 1 => continue, // this curve is finished
+                        Ok(i) => i,
+                        Err(0) => continue, // this curve isn't started yet
+                        Err(n) if n > curve.keyframe_timestamps.len() - 1 => continue, // this curve is finished
+                        Err(i) => i - 1,
+                    };
+                    let ts_start = curve.keyframe_timestamps[step_start];
+                    let ts_end = curve.keyframe_timestamps[step_start + 1];
+                    let lerp = (binding.elapsed - ts_start) / (ts_end - ts_start);
+
+                    // Apply the keyframe
+                    match &curve.keyframes {
+                        Keyframes::Rotation(keyframes) => {
+                            let rot_start = keyframes[step_start];
+                            let mut rot_end = keyframes[step_start + 1];
+                            // Choose the smallest angle for the rotation
+                            if rot_end.dot(rot_start) < 0.0 {
+                                rot_end = -rot_end;
                             }
+                            // Rotations are using a spherical linear interpolation
+                            transform.rotation =
+                                rot_start.normalize().slerp(rot_end.normalize(), lerp);
+                        }
+                        Keyframes::Translation(keyframes) => {
+                            let translation_start = keyframes[step_start];
+                            let translation_end = keyframes[step_start + 1];
+                            let result = translation_start.lerp(translation_end, lerp);
+                            transform.translation = result;
+                        }
+                        Keyframes::Scale(keyframes) => {
+                            let scale_start = keyframes[step_start];
+                            let scale_end = keyframes[step_start + 1];
+                            let result = scale_start.lerp(scale_end, lerp);
+                            transform.scale = result;
                         }
                     }
                 }
             }
         }
-    }
+    });
+    bone_bindings.bindings.clear();
 }
 
 /// Adds animation support to an app
@@ -303,6 +388,11 @@ impl Plugin for AnimationPlugin {
         app.add_asset::<AnimationClip>()
             .register_asset_reflect::<AnimationClip>()
             .register_type::<AnimationPlayer>()
+            .init_resource::<BoneBindings>()
+            .add_system_to_stage(
+                CoreStage::PostUpdate,
+                bind_bones.before(animation_player),
+            )
             .add_system_to_stage(
                 CoreStage::PostUpdate,
                 animation_player.before(TransformSystem::TransformPropagate),
