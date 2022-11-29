857baec2aa981c50e25942d8b0fb370ee1e75a47
Michael Hsu <mike.hsu@gmail.com>
2022-11-28 19:46:17 -0800

    move local.with inside catch_unwind

--- crates/bevy_tasks/src/task_pool.rs
+++ crates/bevy_tasks/src/task_pool.rs
@@ -117,23 +117,23 @@ impl TaskPool {
 
                 thread_builder
                     .spawn(move || {
-                        TaskPool::LOCAL_EXECUTOR.with(|local_executor| {
-                            loop {
-                                let res = std::panic::catch_unwind(|| {
+                        loop {
+                            let res = std::panic::catch_unwind(|| {
+                                TaskPool::LOCAL_EXECUTOR.with(|local_executor| {
                                     let tick_forever = async move {
                                         loop {
                                             local_executor.tick().await;
                                         }
                                     };
                                     future::block_on(ex.run(tick_forever.or(shutdown_rx.recv())))
-                                });
-                                if let Ok(value) = res {
-                                    // Use unwrap_err because we expect a Closed error
-                                    value.unwrap_err();
-                                    break;
-                                }
+                                })
+                            });
+                            if let Ok(value) = res {
+                                // Use unwrap_err because we expect a Closed error
+                                value.unwrap_err();
+                                break;
                             }
-                        });
+                        }
                     })
                     .expect("Failed to spawn thread.")
             })
