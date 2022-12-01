392a43e0d0635180851b19e2b44dc78721ebe98b
JMS55 <47158642+JMS55@users.noreply.github.com>
2022-11-30 19:33:37 -0500

    Improve SSAO example

--- examples/3d/ssao.rs
+++ examples/3d/ssao.rs
@@ -125,7 +125,7 @@ fn update(
     time: Res<Time>,
 ) {
     let mut sphere = sphere.single_mut();
-    sphere.translation.y = (time.elapsed_seconds() / 2.0).sin();
+    sphere.translation.y = (time.elapsed_seconds() / 1.7).sin() * 0.7;
 
     let (camera_entity, ssao_settings, temporal_jitter) = camera.single();
 
