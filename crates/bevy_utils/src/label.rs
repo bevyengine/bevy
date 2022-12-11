26d614591523acdb58682378041b93cc3158d455
James Liu <contact@jamessliu.com>
2022-12-11 18:46:42 +0000

    Document remaining members of bevy_utils (#6897)
    # Objective
    Partially address #3492. 
    
    ## Solution
    Document the remaining undocumented members of `bevy_utils` and set `warn(missing_docs)` on the crate level. Also enabled `clippy::undocumented_unsafe_blocks` as a warning on the crate to keep it in sync with `bevy_ecs`'s warnings.

--- crates/bevy_utils/src/label.rs
+++ crates/bevy_utils/src/label.rs
@@ -5,9 +5,16 @@ use std::{
     hash::{Hash, Hasher},
 };
 
+/// An object safe version of [`Eq`]. This trait is automatically implemented
+/// for any `'static` type that implements `Eq`.
 pub trait DynEq: Any {
+    /// Casts the type to `dyn Any`.
     fn as_any(&self) -> &dyn Any;
 
+    /// This method tests for `self` and `other` values to be equal.
+    ///
+    /// Implementers should avoid returning `true` when the underlying types are
+    /// not the same.
     fn dyn_eq(&self, other: &dyn DynEq) -> bool;
 }
 
@@ -27,9 +34,15 @@ where
     }
 }
 
+/// An object safe version of [`Hash`]. This trait is automatically implemented
+/// for any `'static` type that implements `Hash`.
 pub trait DynHash: DynEq {
+    /// Casts the type to `dyn Any`.
     fn as_dyn_eq(&self) -> &dyn DynEq;
 
+    /// Feeds this value into the given [`Hasher`].
+    ///
+    /// [`Hash`]: std::hash::Hasher
     fn dyn_hash(&self, state: &mut dyn Hasher);
 }
 
