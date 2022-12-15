de6a4da89d629dfd6b1930b2a0722e1e4620955f
Nick Fagerlund <nick.fagerlund@gmail.com>
2022-12-14 16:06:15 -0800

    Target the one noisy `tracy.frame_mark` event more precisely
    This converts the previous Target-based filter to a closure filter that only
    removes the one problematic event, allowing any other events that might get
    added to bevy_render::renderer over time.

--- crates/bevy_log/src/lib.rs
+++ crates/bevy_log/src/lib.rs
@@ -158,11 +158,10 @@ impl Plugin for LogPlugin {
             // bevy_render::renderer logs a `tracy.frame_mark` event every frame
             // at Level::INFO. Formatted logs should omit it.
             #[cfg(feature = "tracing-tracy")]
-            let fmt_layer = fmt_layer.with_filter(
-                tracing_subscriber::filter::Targets::new()
-                    .with_target("bevy_render::renderer", Level::ERROR)
-                    .with_default(Level::TRACE),
-            );
+            let fmt_layer =
+                fmt_layer.with_filter(tracing_subscriber::filter::FilterFn::new(|meta| {
+                    meta.fields().field("tracy.frame_mark").is_none()
+                }));
 
             let subscriber = subscriber.with(fmt_layer);
 
