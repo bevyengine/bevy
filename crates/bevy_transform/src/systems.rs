bf2682da145c9ed12cbccc9db396a6f5f5570353
JoJoJet <21144246+JoJoJet@users.noreply.github.com>
2022-12-21 14:43:05 -0500

    change another 'children' to 'descendants'

--- crates/bevy_transform/src/systems.rs
+++ crates/bevy_transform/src/systems.rs
@@ -49,7 +49,7 @@ pub fn propagate_transforms(
                 // SAFETY: Assuming the hierarchy is consistent, we can be sure that each `child` entity
                 // will be unique across each invocation of this loop body.
                 // Since this is the only place where `transform_query` gets used, `child` will not be borrowed anywhere else.
-                // Also, since `child` only has one ancestor, none of its children will be borrowed by other invocations of the loop.
+                // Also, since `child` only has one ancestor, none of its descendants will be borrowed by other invocations of the loop.
                 unsafe {
                     propagate_recursive(
                         &global_transform,
