ad3ce4058053e29e998fde2a5081c3ee363ae662
JMS55 <47158642+JMS55@users.noreply.github.com>
2022-12-23 13:20:11 -0500

    Misc cleanup

--- crates/bevy_pbr/src/ssao/mod.rs
+++ crates/bevy_pbr/src/ssao/mod.rs
@@ -1,10 +1,3 @@
-// Ground Truth-based Ambient Occlusion (GTAO)
-// Paper: https://www.activision.com/cdn/research/Practical_Real_Time_Strategies_for_Accurate_Indirect_Occlusion_NEW%20VERSION_COLOR.pdf
-// Presentation: https://blog.selfshadow.com/publications/s2016-shading-course/activision/s2016_pbs_activision_occlusion.pdf
-
-// Source code heavily based on XeGTAO v1.30 from Intel
-// https://github.com/GameTechDev/XeGTAO/blob/0d177ce06bfa642f64d8af4de1197ad1bcb862d4/Source/Rendering/Shaders/XeGTAO.hlsli
-
 use crate::MAX_DIRECTIONAL_LIGHTS;
 use bevy_app::{App, Plugin};
 use bevy_asset::{load_internal_asset, HandleUntyped};
