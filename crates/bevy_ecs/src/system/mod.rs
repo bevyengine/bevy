d8299dc75f60a8a52a2750b52932233cfc993723
JoJoJet <21144246+JoJoJet@users.noreply.github.com>
2022-12-23 19:06:31 -0500

    update docs to use `SystemTypeIdLabel` by default

--- crates/bevy_ecs/src/system/mod.rs
+++ crates/bevy_ecs/src/system/mod.rs
@@ -41,9 +41,11 @@
 //!
 //! - **System Stages:** They determine hard execution synchronization boundaries inside of
 //!   which systems run in parallel by default.
-//! - **Labeling:** First, systems are labeled upon creation by calling `.label()`. Then,
-//!   methods such as `.before()` and `.after()` are appended to systems to determine
-//!   execution order in respect to other systems.
+//! - **Labels:** Systems may be ordered within a stage using the methods `.before()` and `.after()`,
+//!   which order systems based on their [`SystemLabel`]s. Each system is implicitly labeled with
+//!   its `fn` type, and custom labels may be added by calling `.label()`.
+//!
+//! [`SystemLabel`]: crate::schedule::SystemLabel
 //!
 //! ## Example
 //!
