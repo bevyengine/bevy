a28e8ee7768039dc972d8e8fe5a2219e3e42a8f6
Devin Leamy <devinleamy@gmail.com>
2022-12-25 20:15:00 -0700

    Updated the examples with the new events

--- examples/input/gamepad_input_events.rs
+++ examples/input/gamepad_input_events.rs
@@ -1,7 +1,7 @@
 //! Iterates and prints gamepad input and connection events.
 
 use bevy::{
-    input::gamepad::{GamepadEvent, GamepadEventType},
+    input::gamepad::{GamepadAxisChangedEvent, GamepadButtonChangedEvent, GamepadConnectionEvent},
     prelude::*,
 };
 
@@ -12,27 +12,24 @@ fn main() {
         .run();
 }
 
-fn gamepad_events(mut gamepad_event: EventReader<GamepadEvent>) {
-    for event in gamepad_event.iter() {
-        match event.event_type {
-            GamepadEventType::Connected(_) => {
-                info!("{:?} Connected", event.gamepad);
-            }
-            GamepadEventType::Disconnected => {
-                info!("{:?} Disconnected", event.gamepad);
-            }
-            GamepadEventType::ButtonChanged(button_type, value) => {
-                info!(
-                    "{:?} of {:?} is changed to {}",
-                    button_type, event.gamepad, value
-                );
-            }
-            GamepadEventType::AxisChanged(axis_type, value) => {
-                info!(
-                    "{:?} of {:?} is changed to {}",
-                    axis_type, event.gamepad, value
-                );
-            }
-        }
+fn gamepad_events(
+    mut gamepad_connection_events: EventReader<GamepadConnectionEvent>,
+    mut gamepad_axis_events: EventReader<GamepadAxisChangedEvent>,
+    mut gamepad_button_events: EventReader<GamepadButtonChangedEvent>,
+) {
+    for connection_event in gamepad_connection_events.iter() {
+        info!("{:?}", connection_event);
+    }
+    for axis_event in gamepad_axis_events.iter() {
+        info!(
+            "{:?} of {:?} is changed to {}",
+            axis_event.axis_type, axis_event.gamepad, axis_event.value
+        );
+    }
+    for button_event in gamepad_button_events.iter() {
+        info!(
+            "{:?} of {:?} is changed to {}",
+            button_event.button_type, button_event.gamepad, button_event.value
+        );
     }
 }
