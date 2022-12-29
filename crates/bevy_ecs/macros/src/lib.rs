27e0f9eb35d32251fa5c62b960954514000280e1
JoJoJet <21144246+JoJoJet@users.noreply.github.com>
2022-12-28 23:25:13 -0500

    add a comment

--- crates/bevy_ecs/macros/src/lib.rs
+++ crates/bevy_ecs/macros/src/lib.rs
@@ -533,6 +533,7 @@ pub fn derive_system_param(input: TokenStream) -> TokenStream {
                 {}
             }
         }
+        // For unconditionally mutable params, we simply do not implement `ReadOnlySystemParam`.
         ParamAccess::Mutable => quote! {},
     };
 
