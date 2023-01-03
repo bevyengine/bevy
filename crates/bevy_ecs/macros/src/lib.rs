1b55e24d16f250ea33eec9ffd18eda9e9841cca1
CatThingy <andyc.andychen@gmail.com>
2023-01-03 15:59:29 -0800

    Add `Event` derive

--- crates/bevy_ecs/macros/src/lib.rs
+++ crates/bevy_ecs/macros/src/lib.rs
@@ -1,6 +1,7 @@
 extern crate proc_macro;
 
 mod component;
+mod event;
 mod fetch;
 mod set;
 mod states;
@@ -480,6 +481,11 @@ pub fn derive_system_set(input: TokenStream) -> TokenStream {
 
 pub(crate) fn bevy_ecs_path() -> syn::Path {
     BevyManifest::default().get_path("bevy_ecs")
+}
+
+#[proc_macro_derive(Event)]
+pub fn derive_event(input: TokenStream) -> TokenStream {
+    event::derive_event(input)
 }
 
 #[proc_macro_derive(Resource)]
