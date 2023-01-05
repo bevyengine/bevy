062b466952df1b0208abd911d0eae5aa5c0ba2b1
A-Walrus <ofek.r.guy@gmail.com>
2023-01-05 09:01:03 +0200

    Fix doc comment "Turbo" -> "Extreme"

--- crates/bevy_core_pipeline/src/fxaa/mod.rs
+++ crates/bevy_core_pipeline/src/fxaa/mod.rs
@@ -47,7 +47,7 @@ pub struct Fxaa {
 
     /// Use lower sensitivity for a sharper, faster, result.
     /// Use higher sensitivity for a slower, smoother, result.
-    /// Ultra and Turbo settings can result in significant smearing and loss of detail.
+    /// Ultra and Extreme settings can result in significant smearing and loss of detail.
 
     /// The minimum amount of local contrast required to apply algorithm.
     pub edge_threshold: Sensitivity,
