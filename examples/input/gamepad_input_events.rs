b2d5cdd4988ff7a86fb974f106217ba9b3f2d85f
Devin Leamy <devinleamy@gmail.com>
2023-01-05 10:05:45 -0700

    Extended example to showcase reading ordered events

--- examples/input/gamepad_input_events.rs
+++ examples/input/gamepad_input_events.rs
@@ -1,7 +1,9 @@
 //! Iterates and prints gamepad input and connection events.
 
 use bevy::{
-    input::gamepad::{GamepadAxisChangedEvent, GamepadButtonChangedEvent, GamepadConnectionEvent},
+    input::gamepad::{
+        GamepadAxisChangedEvent, GamepadButtonChangedEvent, GamepadConnectionEvent, GamepadEvent,
+    },
     prelude::*,
 };
 
@@ -9,6 +11,7 @@ fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_system(gamepad_events)
+        .add_system(gamepad_ordered_events)
         .run();
 }
 
@@ -31,5 +34,18 @@ fn gamepad_events(
             "{:?} of {:?} is changed to {}",
             button_event.button_type, button_event.gamepad, button_event.value
         );
+    }
+}
+
+// If you require in-frame relative event ordering, you can also read the `Gamepad` event
+// stream directly. For standard use-cases, reading the events individually or using the
+// `Input<T>` or `Axis<T>` resources is preferable.
+fn gamepad_ordered_events(mut gamepad_events: EventReader<GamepadEvent>) {
+    for gamepad_event in gamepad_events.iter() {
+        match gamepad_event {
+            GamepadEvent::Connection(connection_event) => info!("{:?}", connection_event),
+            GamepadEvent::Button(button_event) => info!("{:?}", button_event),
+            GamepadEvent::Axis(axis_event) => info!("{:?}", axis_event),
+        }
     }
 }
