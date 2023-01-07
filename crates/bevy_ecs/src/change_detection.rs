47e6b1b9121a473a633bc615d6694662e1b4f786
JoJoJet <21144246+JoJoJet@users.noreply.github.com>
2023-01-07 09:55:43 -0500

    port over `with_type`

--- crates/bevy_ecs/src/change_detection.rs
+++ crates/bevy_ecs/src/change_detection.rs
@@ -441,6 +441,18 @@ impl<'a> MutUntyped<'a> {
     pub fn as_ref(&self) -> Ptr<'_> {
         self.value.as_ref()
     }
+
+    /// Transforms this [`MutUntyped`] into a [`Mut<T>`] with the same lifetime.
+    ///
+    /// # Safety
+    /// * Must be valid for writes and aligned for `T`.
+    /// * Must point to an initialized value of `T`.
+    pub unsafe fn with_type<T>(self) -> Mut<'a, T> {
+        Mut {
+            value: self.value.deref_mut(),
+            ticks: self.ticks,
+        }
+    }
 }
 
 impl<'a> DetectChanges for MutUntyped<'a> {
