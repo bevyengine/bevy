05f076acb09f0d2ba4577b813560f53951806f01
JoJoJet <21144246+JoJoJet@users.noreply.github.com>
2023-01-11 21:08:21 -0500

    remove a now-useless comment

--- crates/bevy_ecs/src/system/commands/command_queue.rs
+++ crates/bevy_ecs/src/system/commands/command_queue.rs
@@ -10,7 +10,6 @@ struct CommandMeta {
     offset: usize,
     /// SAFETY: The `value` must point to a value of type `T: Command`,
     /// where `T` is some specific type that was used to produce this metadata.
-    /// The pointer need not be aligned.
     apply_command: unsafe fn(value: OwningPtr<Unaligned>, world: &mut World),
 }
 
