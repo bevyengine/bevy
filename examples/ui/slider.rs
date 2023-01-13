65bb68c87a6ea1b775f79839c028a068a5caaabd
Pietrek14 <dawidekpe@gmail.com>
2023-01-13 23:44:56 +0100

    added a stepped slider to the example

--- examples/ui/slider.rs
+++ examples/ui/slider.rs
@@ -6,9 +6,23 @@ fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_startup_system(setup)
-        .add_system(update)
+        .add_system(update_slider::<RegularSlider, RegularSliderOutput>)
+        .add_system(update_slider::<SteppedSlider, SteppedSliderOutput>)
         .run();
 }
+
+// Marker components
+#[derive(Component)]
+struct RegularSlider;
+
+#[derive(Component)]
+struct SteppedSlider;
+
+#[derive(Component)]
+struct RegularSliderOutput;
+
+#[derive(Component)]
+struct SteppedSliderOutput;
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     // ui camera
@@ -25,40 +39,106 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
             ..default()
         })
         .with_children(|parent| {
-            // Adding the slider
             parent
-                .spawn(SliderBundle {
+                .spawn(NodeBundle {
                     style: Style {
-                        size: Size::new(Val::Px(200.), Val::Px(20.)),
+                        flex_direction: FlexDirection::Column,
+                        justify_content: JustifyContent::Center,
+                        align_items: AlignItems::Center,
                         margin: UiRect::bottom(Val::Px(25.)),
                         ..default()
                     },
-                    background_color: Color::rgb(0.8, 0.8, 0.8).into(),
                     ..default()
                 })
                 .with_children(|parent| {
-                    // Adding the slider handle
-                    parent.spawn(SliderHandleBundle {
-                        style: Style {
-                            size: Size::new(Val::Px(15.), Val::Px(20.)),
+                    // Adding the slider
+                    parent
+                        .spawn(SliderBundle {
+                            style: Style {
+                                size: Size::new(Val::Px(200.), Val::Px(20.)),
+                                margin: UiRect::bottom(Val::Px(15.)),
+                                ..default()
+                            },
+                            background_color: Color::rgb(0.8, 0.8, 0.8).into(),
                             ..default()
-                        },
-                        ..default()
-                    });
+                        })
+                        .insert(RegularSlider)
+                        .with_children(|parent| {
+                            // Adding the slider handle
+                            parent.spawn(SliderHandleBundle {
+                                style: Style {
+                                    size: Size::new(Val::Px(15.), Val::Px(20.)),
+                                    ..default()
+                                },
+                                ..default()
+                            });
+                        });
+
+                    parent
+                        .spawn(TextBundle::from_section(
+                            "0",
+                            TextStyle {
+                                font: asset_server.load("fonts/FiraSans-Bold.ttf"),
+                                font_size: 40.0,
+                                color: Color::rgb(0.9, 0.9, 0.9),
+                            },
+                        ))
+                        .insert(RegularSliderOutput);
                 });
 
-            parent.spawn(TextBundle::from_section(
-                "0",
-                TextStyle {
-                    font: asset_server.load("fonts/FiraSans-Bold.ttf"),
-                    font_size: 40.0,
-                    color: Color::rgb(0.9, 0.9, 0.9),
-                },
-            ));
+            parent
+                .spawn(NodeBundle {
+                    style: Style {
+                        flex_direction: FlexDirection::Column,
+                        justify_content: JustifyContent::Center,
+                        align_items: AlignItems::Center,
+                        ..default()
+                    },
+                    ..default()
+                })
+                .with_children(|parent| {
+                    // Adding the stepped slider
+                    parent
+                        .spawn(SliderBundle {
+                            style: Style {
+                                size: Size::new(Val::Px(200.), Val::Px(20.)),
+                                margin: UiRect::bottom(Val::Px(15.)),
+                                ..default()
+                            },
+                            background_color: Color::rgb(0.8, 0.8, 0.8).into(),
+                            slider: Slider::new(50., 150.).with_step(10.),
+                            ..default()
+                        })
+                        .insert(SteppedSlider)
+                        .with_children(|parent| {
+                            // Adding the slider handle
+                            parent.spawn(SliderHandleBundle {
+                                style: Style {
+                                    size: Size::new(Val::Px(15.), Val::Px(20.)),
+                                    ..default()
+                                },
+                                ..default()
+                            });
+                        });
+
+                    parent
+                        .spawn(TextBundle::from_section(
+                            "0",
+                            TextStyle {
+                                font: asset_server.load("fonts/FiraSans-Bold.ttf"),
+                                font_size: 40.0,
+                                color: Color::rgb(0.9, 0.9, 0.9),
+                            },
+                        ))
+                        .insert(SteppedSliderOutput);
+                });
         });
 }
 
-fn update(slider_query: Query<&Slider>, mut text_query: Query<&mut Text>) {
+fn update_slider<SliderMarker: Component, OutputMarker: Component>(
+    slider_query: Query<&Slider, With<SliderMarker>>,
+    mut text_query: Query<&mut Text, With<OutputMarker>>,
+) {
     let slider = slider_query.single();
     let mut text = text_query.single_mut();
 
