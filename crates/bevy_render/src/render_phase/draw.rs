369eaec41d0c1a80a21fd58cdf5d07f63b167e52
Kurt Kühnert <kurt@kuehnert.dev>
2023-01-16 10:18:25 +0100

    documented usage of SRes::into_inner on the RenderCommand trait

--- crates/bevy_render/src/render_phase/draw.rs
+++ crates/bevy_render/src/render_phase/draw.rs
@@ -159,6 +159,12 @@ impl<P: PhaseItem> DrawFunctions<P> {
 pub trait RenderCommand<P: PhaseItem> {
     /// Specifies the general ECS data (e.g. resources) required by [`RenderCommand::render`].
     ///
+    /// Due to lifetime limitations of the `Deref` trait,
+    /// [`SRes::into_inner`](bevy_ecs::system::lifetimeless::SRes::into_inner) must be called on
+    /// each [`SRes`](bevy_ecs::system::lifetimeless::SRes) reference in the
+    /// [`RenderCommand::render`] method, instead of being automatically dereferenced as is the
+    /// case in normal `systems`.
+    ///
     /// All parameters have to be read only.
     type Param: SystemParam + 'static;
     /// Specifies the ECS data of the view entity required by [`RenderCommand::render`].
