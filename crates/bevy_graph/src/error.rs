6c9e16896981c257430acba1a351674db2a03a40
DasLixou <einlixou@gmail.com>
2023-01-16 15:43:21 +0100

    thiserror and document Idx types

--- crates/bevy_graph/src/error.rs
+++ crates/bevy_graph/src/error.rs
@@ -1,11 +1,18 @@
+use thiserror::Error;
+
 use crate::graphs::keys::{EdgeIdx, NodeIdx};
 
-#[derive(Debug)]
+#[derive(Debug, Error)]
 pub enum GraphError {
+    #[error("node by given NodeIdx `{0:?}` doesn't exist in this graph")]
     NodeIdxDoesntExist(NodeIdx),
+    #[error("edge by given EdgeIdx `{0:?}` doesn't exist in this graph")]
     EdgeIdxDoesntExist(EdgeIdx),
+    #[error("edge between nodes `{0:?}` and `{1:?}` doesn't exist in this graph")]
     EdgeBetweenDoesntExist(NodeIdx, NodeIdx),
+    #[error("edge between nodes `{0:?}` and `{1:?}` is already preset in this graph")]
     EdgeBetweenAlreadyExists(NodeIdx, NodeIdx),
+    #[error("cannot create an edge between node `{0:?}` and itself in a SimpleGraph. use MultiGraph instead")]
     EdgeBetweenSameNode(NodeIdx),
 }
 
--- crates/bevy_graph/src/graphs/keys.rs
+++ crates/bevy_graph/src/graphs/keys.rs
@@ -5,7 +5,9 @@ use crate::error::GraphResult;
 use super::Graph;
 
 new_key_type! {
+    /// a key that holds an index to a node in a graph
     pub struct NodeIdx;
+    /// a key that holds an index to an edge in a graph
     pub struct EdgeIdx;
 }
 
