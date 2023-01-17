5b72c8601fdde32d0cf6215949c13c9611960ed1
JoJoJet <21144246+JoJoJet@users.noreply.github.com>
2023-01-16 20:40:09 -0500

    replace `FunctionSystem` with a type specialized for commands

--- crates/bevy_ecs/src/system/commands/system_command.rs
+++ crates/bevy_ecs/src/system/commands/system_command.rs
@@ -1,9 +1,11 @@
-use crate::system::{IntoSystem, SystemParam};
+use std::marker::PhantomData;
+
+use crate::{
+    prelude::World,
+    system::{IntoSystem, SystemMeta, SystemParam, SystemParamFunction},
+};
 
 use super::{Command, IntoCommand};
-
-#[doc(hidden)]
-pub struct IsSystemCommand;
 
 pub trait CommandParam: SystemParam {}
 
@@ -14,5 +16,52 @@ where
     type Command = T::System;
     fn into_command(this: Self) -> Self::Command {
         IntoSystem::into_system(this)
+    }
+}
+
+#[doc(hidden)]
+pub struct IsSystemCommand;
+
+pub struct SystemCommand<Param, Marker, F>
+where
+    Param: CommandParam,
+{
+    func: F,
+    system_meta: SystemMeta,
+    // NOTE: PhantomData<fn()-> T> gives this safe Send/Sync impls
+    marker: PhantomData<fn() -> (Param, Marker)>,
+}
+
+impl<Param, Marker, F> IntoCommand<(IsSystemCommand, Param, Marker)> for F
+where
+    Param: CommandParam + 'static,
+    Marker: 'static,
+    F: SystemParamFunction<(), (), Param, Marker> + Send + Sync + 'static,
+{
+    type Command = SystemCommand<Param, Marker, F>;
+    fn into_command(func: Self) -> Self::Command {
+        SystemCommand {
+            func,
+            system_meta: SystemMeta::new::<F>(),
+            marker: PhantomData,
+        }
+    }
+}
+
+impl<Param, Marker, F> Command for SystemCommand<Param, Marker, F>
+where
+    Param: CommandParam + 'static,
+    Marker: 'static,
+    F: SystemParamFunction<(), (), Param, Marker> + Send + Sync + 'static,
+{
+    fn write(mut self, world: &mut World) {
+        let change_tick = world.change_tick();
+
+        let mut param_state = Param::init_state(world, &mut self.system_meta);
+        let params =
+            // SAFETY: We have exclusive world access.
+            unsafe { Param::get_param(&mut param_state, &self.system_meta, world, change_tick) };
+        self.func.run((), params);
+        Param::apply(&mut param_state, &self.system_meta, world);
     }
 }
