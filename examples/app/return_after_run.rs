f0c504947ce653068a424979faf226c1e990818d
Stephen Martindale <stephenmartindale@users.noreply.github.com>
2023-01-18 23:02:38 +0000

    Docs: App::run() might never return; effect of WinitSettings::return_from_run. (#7228)
    # Objective
    
    See:
    
    - https://github.com/bevyengine/bevy/issues/7067#issuecomment-1381982285
      - (This does not fully close that issue in my opinion.)
    - https://discord.com/channels/691052431525675048/1063454009769340989
    
    ## Solution
    
    This merge request adds documentation:
    
    1. Alert users to the fact that `App::run()` might never return and code placed after it might never be executed.
    2. Makes `winit::WinitSettings::return_from_run` discoverable.
    3. Better explains why `winit::WinitSettings::return_from_run` is discouraged and better links to up-stream docs. on that topic.
    4. Adds notes to the `app/return_after_run.rs` example which otherwise promotes a feature that carries caveats.
    
    Furthermore, w.r.t `winit::WinitSettings::return_from_run`:
    
    - Broken links to `winit` docs are fixed.
    - Links now point to BOTH `EventLoop::run()` and `EventLoopExtRunReturn::run_return()` which are the salient up-stream pages and make more sense, taken together.
    - Collateral damage: "Supported platforms" heading; disambiguation of "run" → `App::run()`; links.
       
    ## Future Work
    
    I deliberately structured the "`run()` might not return" section under `App::run()` to allow for alternative patterns (e.g. `AppExit` event, `WindowClosed` event) to be listed or mentioned, beneath it, in the future.

--- examples/app/return_after_run.rs
+++ examples/app/return_after_run.rs
@@ -1,4 +1,15 @@
 //! Shows how to return to the calling function after a windowed Bevy app has exited.
+//!
+//! In windowed *Bevy* applications, executing code below a call to `App::run()` is
+//! not recommended because `App::run()` might never return.
+//!
+//! This example demonstrates the use of `WinitSettings::return_from_run` to
+//! require that `App::run()` *does* return but this is not recommended. Be sure
+//! to read the documentation on both `App::run()` and `WinitSettings::return_from_run`
+//! for caveats and further details:
+//!
+//! - <https://docs.rs/bevy/latest/bevy/app/struct.App.html#method.run>
+//! - <https://docs.rs/bevy/latest/bevy/winit/struct.WinitSettings.html#structfield.return_from_run>
 
 use bevy::{prelude::*, winit::WinitSettings};
 
