9503a66a43dbf6cbecb2c9e04c1a50441158fc24
Jakob Hellermann <jakob.hellermann@protonmail.com>
2023-01-19 21:35:42 +0100

    add get_non_send_resource[_mut]_by_id

--- crates/bevy_ecs/src/world/interior_mutable_world.rs
+++ crates/bevy_ecs/src/world/interior_mutable_world.rs
@@ -184,6 +184,29 @@ impl<'w> InteriorMutableWorld<'w> {
         self.0.get_non_send_resource()
     }
 
+    /// Gets a `!Send` resource to the resource with the id [`ComponentId`] if it exists.
+    /// The returned pointer must not be used to modify the resource, and must not be
+    /// dereferenced after the immutable borrow of the [`World`] ends.
+    ///
+    /// **You should prefer to use the typed API [`InteriorMutableWorld::get_non_send_resource`] where possible and only
+    /// use this in cases where the actual types are not known at compile time.**
+    ///
+    /// # Panics
+    /// This function will panic if it isn't called from the same thread that the resource was inserted from.
+    ///
+    /// # Safety
+    /// It is the callers responsibility to ensure that
+    /// - the [`InteriorMutableWorld`] has permission to access the resource
+    /// - no other mutable references to the resource exist at the same time
+    #[inline]
+    pub unsafe fn get_non_send_resource_by_id(&self, component_id: ComponentId) -> Option<Ptr<'_>> {
+        self.0
+            .storages
+            .non_send_resources
+            .get(component_id)?
+            .get_data()
+    }
+
     /// Gets a mutable reference to the resource of the given type if it exists
     ///
     /// # Safety
@@ -243,6 +266,41 @@ impl<'w> InteriorMutableWorld<'w> {
         let component_id = self.0.components.get_resource_id(TypeId::of::<R>())?;
         // SAFETY: component_id matches `R`, rest is promised by caller
         unsafe { self.get_non_send_mut_with_id(component_id) }
+    }
+
+    /// Gets a `!Send` resource to the resource with the id [`ComponentId`] if it exists.
+    /// The returned pointer may be used to modify the resource, as long as the mutable borrow
+    /// of the [`World`] is still valid.
+    ///
+    /// **You should prefer to use the typed API [`InteriorMutableWorld::get_non_send_resource_mut`] where possible and only
+    /// use this in cases where the actual types are not known at compile time.**
+    ///
+    /// # Panics
+    /// This function will panic if it isn't called from the same thread that the resource was inserted from.
+    ///
+    /// # Safety
+    /// It is the callers responsibility to ensure that
+    /// - the [`InteriorMutableWorld`] has permission to access the resource mutably
+    /// - no other references to the resource exist at the same time
+    #[inline]
+    pub unsafe fn get_non_send_resource_mut_by_id(
+        &mut self,
+        component_id: ComponentId,
+    ) -> Option<MutUntyped<'_>> {
+        let change_tick = self.read_change_tick();
+        let (ptr, ticks) = self.0.get_non_send_with_ticks(component_id)?;
+
+        let ticks =
+            // SAFETY: This function has exclusive access to the world so nothing aliases `ticks`.
+            // - index is in-bounds because the column is initialized and non-empty
+            // - no other reference to the ticks of the same row can exist at the same time
+            unsafe { TicksMut::from_tick_cells(ticks, self.last_change_tick(), change_tick) };
+
+        Some(MutUntyped {
+            // SAFETY: This function has exclusive access to the world so nothing aliases `ptr`.
+            value: unsafe { ptr.assert_unique() },
+            ticks,
+        })
     }
 }
 
