6c0b1e829fe6547361b2f29a99227a35ff420f53
François <mockersf@gmail.com>
2023-01-20 10:26:58 +0100

    clippy

--- crates/bevy_ui/src/focus.rs
+++ crates/bevy_ui/src/focus.rs
@@ -182,7 +182,7 @@ pub fn ui_focus_system(
         .find_map(|window_ref| {
             windows.get(window_ref.entity()).ok().and_then(|window| {
                 window.cursor_position().map(|mut cursor_pos| {
-                    cursor_pos.y = window.height() as f32 - cursor_pos.y;
+                    cursor_pos.y = window.height() - cursor_pos.y;
                     cursor_pos
                 })
             })
