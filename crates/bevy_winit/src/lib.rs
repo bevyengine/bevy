60aea1e4249dec279251dd43896a9b6b2a81e504
François <mockersf@gmail.com>
2023-01-20 10:22:28 +0100

    break feedback loop when moving cursor

--- crates/bevy_winit/src/lib.rs
+++ crates/bevy_winit/src/lib.rs
@@ -396,7 +396,9 @@ pub fn winit_runner(mut app: App) {
                             window.resolution.physical_height() as f64 - position.y,
                         );
 
-                        window.set_physical_cursor_position(Some(physical_position));
+                        window
+                            .bypass_change_detection()
+                            .set_physical_cursor_position(Some(physical_position));
 
                         cursor_events.cursor_moved.send(CursorMoved {
                             window: window_entity,
@@ -412,7 +414,9 @@ pub fn winit_runner(mut app: App) {
                     WindowEvent::CursorLeft { .. } => {
                         // Component
                         if let Ok((mut window, _)) = window_query.get_mut(window_entity) {
-                            window.set_physical_cursor_position(None);
+                            window
+                                .bypass_change_detection()
+                                .set_physical_cursor_position(None);
                         }
 
                         cursor_events.cursor_left.send(CursorLeft {
