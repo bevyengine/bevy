ee88d17a95613f3bf9f831337d5cf8549c3a045c
DasLixou <einlixou@gmail.com>
2023-01-23 18:46:06 +0100

    Work on customizing algos

--- crates/bevy_graph/src/algos/bfs.rs
+++ crates/bevy_graph/src/algos/bfs.rs
@@ -11,16 +11,19 @@ use crate::{
 ///
 /// when `d` is the distance between a node and the startnode,
 /// it will evaluate every node with `d=1`, then continue with `d=2` and so on.
-pub struct BreadthFirstSearch<'g, N, E, G: Graph<N, E>> {
+pub struct BreadthFirstSearch<'g, N, E: 'g, G: Graph<N, E>, I: Iterator<Item = EdgeRef<'g, E>>> {
     graph: &'g G,
     queue: VecDeque<NodeIdx>,
     visited: HashSet<NodeIdx>,
+    visitor: fn(&'g G, NodeIdx) -> I,
     phantom: PhantomData<(N, E)>,
 }
 
-impl<'g, N, E, G: Graph<N, E>> BreadthFirstSearch<'g, N, E, G> {
-    /// Creates a new `BreadthFirstSearch` with a start node
-    pub fn new(graph: &'g G, start: NodeIdx) -> Self {
+impl<'g, N, E: 'g, G: Graph<N, E>, I: Iterator<Item = EdgeRef<'g, E>>>
+    BreadthFirstSearch<'g, N, E, G, I>
+{
+    /// Creates a new `BreadthFirstSearch` with a start node and a custom visitor
+    pub fn custom(graph: &'g G, start: NodeIdx, visitor: fn(&'g G, NodeIdx) -> I) -> Self {
         let node_count = graph.node_count();
         let mut queue = VecDeque::with_capacity(node_count);
         let mut visited = HashSet::with_capacity(node_count);
@@ -32,17 +35,28 @@ impl<'g, N, E, G: Graph<N, E>> BreadthFirstSearch<'g, N, E, G> {
             graph,
             queue,
             visited,
+            visitor,
             phantom: PhantomData,
         }
     }
+}
 
-    /// Creates a new `BreadthFirstSearch` wrapped inside an `NodesByIdx` iterator
+impl<'g, N, E: 'g, G: Graph<N, E>> BreadthFirstSearch<'g, N, E, G, G::OutgoingEdgesOf<'g>> {
+    /// Creates a new `BreadthFirstSearch` with a start node and the default visitor of `outgoing`
+    #[inline]
+    pub fn new(graph: &'g G, start: NodeIdx) -> Self {
+        Self::custom(graph, start, |graph, index| graph.outgoing_edges_of(index))
+    }
+
+    /// Creates a new `BreadthFirstSearch` with a start node and the default visitor of `outgoing` wrapped inside an `NodesByIdx` iterator
+    #[inline]
     pub fn new_ref(graph: &'g G, start: NodeIdx) -> iters::NodesByIdx<'g, N, NodeIdx, Self> {
         let inner = Self::new(graph, start);
         iters::NodesByIdx::from_graph(inner, graph)
     }
 
-    /// Creates a new `BreadthFirstSearch` wrapped inside an `NodesByIdxMut` iterator
+    /// Creates a new `BreadthFirstSearch` with a start node and the default visitor of `outgoing` wrapped inside an `NodesByIdxMut` iterator
+    #[inline]
     pub fn new_mut(graph: &'g mut G, start: NodeIdx) -> iters::NodesByIdxMut<'g, N, NodeIdx, Self> {
         unsafe {
             // SAFETY: We know `NodesByIdxMut` doesn't intercept (deletes nodes) at all.
@@ -54,12 +68,14 @@ impl<'g, N, E, G: Graph<N, E>> BreadthFirstSearch<'g, N, E, G> {
     }
 }
 
-impl<'g, N, E, G: Graph<N, E>> Iterator for BreadthFirstSearch<'g, N, E, G> {
+impl<'g, N, E: 'g, G: Graph<N, E>, I: Iterator<Item = EdgeRef<'g, E>>> Iterator
+    for BreadthFirstSearch<'g, N, E, G, I>
+{
     type Item = NodeIdx;
 
     fn next(&mut self) -> Option<Self::Item> {
         if let Some(node) = self.queue.pop_front() {
-            for EdgeRef(_, dst, _) in self.graph.outgoing_edges_of(node) {
+            for EdgeRef(_, dst, _) in (self.visitor)(self.graph, node) {
                 if !self.visited.contains(&dst) {
                     self.visited.insert(dst);
                     self.queue.push_back(dst);
--- crates/bevy_graph/src/algos/dfs.rs
+++ crates/bevy_graph/src/algos/dfs.rs
@@ -10,16 +10,19 @@ use crate::{
 /// Implementation of the [`DFS` algorithm](https://www.geeksforgeeks.org/depth-first-search-or-dfs-for-a-graph/)
 ///
 /// it will evaluate every node from the start as deep as it can and then continue at the next sibling node from the top.
-pub struct DepthFirstSearch<'g, N, E, G: Graph<N, E>> {
+pub struct DepthFirstSearch<'g, N, E: 'g, G: Graph<N, E>, I: Iterator<Item = EdgeRef<'g, E>>> {
     graph: &'g G,
     stack: Vec<NodeIdx>,
     visited: HashSet<NodeIdx>,
+    visitor: fn(&'g G, NodeIdx) -> I,
     phantom: PhantomData<(N, E)>,
 }
 
-impl<'g, N, E, G: Graph<N, E>> DepthFirstSearch<'g, N, E, G> {
-    /// Creates a new `DepthFirstSearch` with a start node
-    pub fn new(graph: &'g G, start: NodeIdx) -> Self {
+impl<'g, N, E: 'g, G: Graph<N, E>, I: Iterator<Item = EdgeRef<'g, E>>>
+    DepthFirstSearch<'g, N, E, G, I>
+{
+    /// Creates a new `DepthFirstSearch` with a start node and a custom visitor
+    pub fn custom(graph: &'g G, start: NodeIdx, visitor: fn(&'g G, NodeIdx) -> I) -> Self {
         let node_count = graph.node_count();
         let mut stack = Vec::with_capacity(node_count);
         let mut visited = HashSet::with_capacity(node_count);
@@ -31,17 +34,26 @@ impl<'g, N, E, G: Graph<N, E>> DepthFirstSearch<'g, N, E, G> {
             graph,
             stack,
             visited,
+            visitor,
             phantom: PhantomData,
         }
     }
+}
 
-    /// Creates a new `DepthFirstSearch` wrapped inside an `NodesByIdx` iterator
+impl<'g, N, E: 'g, G: Graph<N, E>> DepthFirstSearch<'g, N, E, G, G::OutgoingEdgesOf<'g>> {
+    /// Creates a new `DepthFirstSearch` with a start node and the default visitor of `outgoing`
+    #[inline]
+    pub fn new(graph: &'g G, start: NodeIdx) -> Self {
+        Self::custom(graph, start, |graph, index| graph.outgoing_edges_of(index))
+    }
+
+    /// Creates a new `DepthFirstSearch` with a start node and the default visitor of `outgoing` wrapped inside an `NodesByIdx` iterator
     pub fn new_ref(graph: &'g G, start: NodeIdx) -> iters::NodesByIdx<'g, N, NodeIdx, Self> {
         let inner = Self::new(graph, start);
         iters::NodesByIdx::from_graph(inner, graph)
     }
 
-    /// Creates a new `BreadthFirstSearch` wrapped inside an `NodesByIdxMut` iterator
+    /// Creates a new `DepthFirstSearch` with a start node and the default visitor of `outgoing` wrapped inside an `NodesByIdxMut` iterator
     pub fn new_mut(graph: &'g mut G, start: NodeIdx) -> iters::NodesByIdxMut<'g, N, NodeIdx, Self> {
         unsafe {
             // SAFETY: We know `NodesByIdxMut` doesn't intercept (deletes nodes) at all.
@@ -53,12 +65,14 @@ impl<'g, N, E, G: Graph<N, E>> DepthFirstSearch<'g, N, E, G> {
     }
 }
 
-impl<'g, N, E, G: Graph<N, E>> Iterator for DepthFirstSearch<'g, N, E, G> {
+impl<'g, N, E: 'g, G: Graph<N, E>, I: Iterator<Item = EdgeRef<'g, E>>> Iterator
+    for DepthFirstSearch<'g, N, E, G, I>
+{
     type Item = NodeIdx;
 
     fn next(&mut self) -> Option<Self::Item> {
         if let Some(node) = self.stack.pop() {
-            for EdgeRef(_, dst, _) in self.graph.outgoing_edges_of(node) {
+            for EdgeRef(_, dst, _) in (self.visitor)(self.graph, node) {
                 if !self.visited.contains(&dst) {
                     self.visited.insert(dst);
                     self.stack.push(dst);
