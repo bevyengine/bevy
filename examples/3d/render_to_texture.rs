d66484154022c82de659e60dbd2891db811cf55d
tigregalis <anak.harimau@gmail.com>
2023-01-25 18:54:59 +0800

    update render_to_texture example to use constructor

--- examples/3d/render_to_texture.rs
+++ examples/3d/render_to_texture.rs
@@ -5,13 +5,7 @@ use std::f32::consts::PI;
 use bevy::{
     core_pipeline::clear_color::ClearColorConfig,
     prelude::*,
-    render::{
-        camera::RenderTarget,
-        render_resource::{
-            Extent3d, TextureDescriptor, TextureDimension, TextureFormat, TextureUsages,
-        },
-        view::RenderLayers,
-    },
+    render::{camera::RenderTarget, view::RenderLayers},
 };
 
 fn main() {
@@ -37,30 +31,8 @@ fn setup(
     mut materials: ResMut<Assets<StandardMaterial>>,
     mut images: ResMut<Assets<Image>>,
 ) {
-    let size = Extent3d {
-        width: 512,
-        height: 512,
-        ..default()
-    };
-
     // This is the texture that will be rendered to.
-    let mut image = Image {
-        texture_descriptor: TextureDescriptor {
-            label: None,
-            size,
-            dimension: TextureDimension::D2,
-            format: TextureFormat::Bgra8UnormSrgb,
-            mip_level_count: 1,
-            sample_count: 1,
-            usage: TextureUsages::TEXTURE_BINDING
-                | TextureUsages::COPY_DST
-                | TextureUsages::RENDER_ATTACHMENT,
-        },
-        ..default()
-    };
-
-    // fill image.data with zeroes
-    image.resize(size);
+    let image = Image::new_target_texture(512, 512);
 
     let image_handle = images.add(image);
 
