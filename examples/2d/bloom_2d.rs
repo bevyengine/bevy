95ad4d792c6fb7fbe18a25081f430fe34748b365
JMS55 <47158642+JMS55@users.noreply.github.com>
2023-02-02 13:27:31 -0500

    Add bloom_2d example controls

--- examples/2d/bloom_2d.rs
+++ examples/2d/bloom_2d.rs
@@ -1,12 +1,17 @@
 //! Illustrates bloom post-processing in 2d.
 
-use bevy::{core_pipeline::bloom::BloomSettings, prelude::*, sprite::MaterialMesh2dBundle};
+use bevy::{
+    core_pipeline::bloom::{BloomCompositeMode, BloomSettings},
+    prelude::*,
+    sprite::MaterialMesh2dBundle,
+};
 
 fn main() {
     App::new()
         .insert_resource(ClearColor(Color::DARK_GRAY))
         .add_plugins(DefaultPlugins)
         .add_startup_system(setup)
+        .add_system(update_bloom_settings)
         .run();
 }
 
@@ -57,4 +62,143 @@ fn setup(
         transform: Transform::from_translation(Vec3::new(200., 0., 0.)),
         ..default()
     });
+
+    // UI
+    commands.spawn(
+        TextBundle::from_section(
+            "",
+            TextStyle {
+                font: asset_server.load("fonts/FiraMono-Medium.ttf"),
+                font_size: 18.0,
+                color: Color::WHITE,
+            },
+        )
+        .with_style(Style {
+            position_type: PositionType::Absolute,
+            position: UiRect {
+                bottom: Val::Px(10.0),
+                left: Val::Px(10.0),
+                ..default()
+            },
+            ..default()
+        }),
+    );
+}
+
+// ------------------------------------------------------------------------------------------------
+
+fn update_bloom_settings(
+    mut camera: Query<(Entity, Option<&mut BloomSettings>), With<Camera>>,
+    mut text: Query<&mut Text>,
+    mut commands: Commands,
+    keycode: Res<Input<KeyCode>>,
+    time: Res<Time>,
+) {
+    let bloom_settings = camera.single_mut();
+    let mut text = text.single_mut();
+    let text = &mut text.sections[0].value;
+
+    match bloom_settings {
+        (entity, Some(mut bloom_settings)) => {
+            *text = "BloomSettings (Toggle: Space)\n".to_string();
+            text.push_str(&format!("(Q/A) Intensity: {}\n", bloom_settings.intensity));
+            text.push_str(&format!("(W/S) LF boost: {}\n", bloom_settings.lf_boost));
+            text.push_str(&format!(
+                "(E/D) LF boost curv.: {}\n",
+                bloom_settings.lf_boost_curvature
+            ));
+            text.push_str(&format!(
+                "(R/F) High-pass freq.: {}\n",
+                bloom_settings.high_pass_frequency
+            ));
+            text.push_str(&format!(
+                "(T/G) Mode: {}\n",
+                match bloom_settings.composite_mode {
+                    BloomCompositeMode::EnergyConserving => "Energy-conserving",
+                    BloomCompositeMode::Additive => "Additive",
+                }
+            ));
+            text.push_str(&format!(
+                "(Y/H) Threshold: {}\n",
+                bloom_settings.prefilter_settings.threshold
+            ));
+            text.push_str(&format!(
+                "(U/J) Threshold softness: {}\n",
+                bloom_settings.prefilter_settings.threshold_softness
+            ));
+
+            if keycode.just_pressed(KeyCode::Space) {
+                commands.entity(entity).remove::<BloomSettings>();
+            }
+
+            let dt = time.delta_seconds();
+
+            if keycode.pressed(KeyCode::A) {
+                bloom_settings.intensity -= dt / 10.0;
+            }
+            if keycode.pressed(KeyCode::Q) {
+                bloom_settings.intensity += dt / 10.0;
+            }
+            bloom_settings.intensity = bloom_settings.intensity.clamp(0.0, 1.0);
+
+            if keycode.pressed(KeyCode::S) {
+                bloom_settings.lf_boost -= dt / 10.0;
+            }
+            if keycode.pressed(KeyCode::W) {
+                bloom_settings.lf_boost += dt / 10.0;
+            }
+            bloom_settings.lf_boost = bloom_settings.lf_boost.clamp(0.0, 1.0);
+
+            if keycode.pressed(KeyCode::D) {
+                bloom_settings.lf_boost_curvature -= dt / 10.0;
+            }
+            if keycode.pressed(KeyCode::E) {
+                bloom_settings.lf_boost_curvature += dt / 10.0;
+            }
+            bloom_settings.lf_boost_curvature = bloom_settings.lf_boost_curvature.clamp(0.0, 1.0);
+
+            if keycode.pressed(KeyCode::F) {
+                bloom_settings.high_pass_frequency -= dt / 10.0;
+            }
+            if keycode.pressed(KeyCode::R) {
+                bloom_settings.high_pass_frequency += dt / 10.0;
+            }
+            bloom_settings.high_pass_frequency = bloom_settings.high_pass_frequency.clamp(0.0, 1.0);
+
+            if keycode.pressed(KeyCode::G) {
+                bloom_settings.composite_mode = BloomCompositeMode::Additive;
+            }
+            if keycode.pressed(KeyCode::T) {
+                bloom_settings.composite_mode = BloomCompositeMode::EnergyConserving;
+            }
+
+            if keycode.pressed(KeyCode::H) {
+                bloom_settings.prefilter_settings.threshold -= dt;
+            }
+            if keycode.pressed(KeyCode::Y) {
+                bloom_settings.prefilter_settings.threshold += dt;
+            }
+            bloom_settings.prefilter_settings.threshold =
+                bloom_settings.prefilter_settings.threshold.max(0.0);
+
+            if keycode.pressed(KeyCode::J) {
+                bloom_settings.prefilter_settings.threshold_softness -= dt / 10.0;
+            }
+            if keycode.pressed(KeyCode::U) {
+                bloom_settings.prefilter_settings.threshold_softness += dt / 10.0;
+            }
+            bloom_settings.prefilter_settings.threshold_softness = bloom_settings
+                .prefilter_settings
+                .threshold_softness
+                .clamp(0.0, 1.0);
+        }
+
+        (entity, None) => {
+            *text = "Bloom: Off (Toggle: Space)".to_string();
+
+            if keycode.just_pressed(KeyCode::Space) {
+                commands.entity(entity).insert(BloomSettings::default());
+            }
+        }
+    }
 }
