66888a0e257325a3da35c21d4d629df4ed15fb80
TGR <account@thomasreddy.com>
2023-02-03 09:40:42 +0000

    Update texture.rs

--- examples/3d/texture.rs
+++ examples/3d/texture.rs
@@ -21,7 +21,7 @@ pub fn update_mesh_uvs_for_number_of_tiles(mesh: &mut Mesh, number_of_tiles: (f3
 
 // Update a planes's UVs so that the applied texture tiles with the calculated number of tiles,
 // given the intended size of the texture in bevy units.
-pub fn update_plane_uvs_for_world_space_texture_size(
+pub fn update_quad_uvs_for_world_space_texture_size(
     mesh: &mut Mesh,
     world_space_texture_size: (f32, f32),
 ) {
@@ -29,27 +29,14 @@ pub fn update_plane_uvs_for_world_space_texture_size(
     if let (
         Some(VertexAttributeValues::Float32x2(uvs)),
         Some(VertexAttributeValues::Float32x3(positions)),
-        Some(VertexAttributeValues::Float32x3(normals)),
     ) = (
         attributes.remove(&Mesh::ATTRIBUTE_UV_0.id),
         attributes.remove(&Mesh::ATTRIBUTE_POSITION.id),
-        attributes.remove(&Mesh::ATTRIBUTE_NORMAL.id),
     ) {
-        for ((position, uv), normal) in positions.iter().zip(uvs).zip(normals) {
-            let x = normal[0];
-            let y = normal[1];
-            let z = normal[2];
-
-            if x > y && x > z {
-                uv[0] = position[1] / world_space_texture_size.0;
-                uv[1] = -position[2] / world_space_texture_size.1;
-            } else if y > x && y > z {
-                uv[0] = position[0] / world_space_texture_size.0;
-                uv[1] = -position[2] / world_space_texture_size.1;
-            } else {
-                uv[0] = position[0] / world_space_texture_size.0;
-                uv[1] = -position[1] / world_space_texture_size.1;
-            }
+        for (position, uv) in positions.iter().zip(uvs) {
+            uv[0] = position[0] / world_space_texture_size.0;
+            // If you are using this to change the UVs of a shape::Plane, change this to -position[2]
+            uv[1] = -position[1] / world_space_texture_size.1;
         }
     }
 }
