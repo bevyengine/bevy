14f3c5ceebbacef67093b4160fdc3ba3276e4314
SpeedRoll <sonicedge2@gmail.com>
2023-02-07 01:10:37 +0530

    doc comment changes

--- crates/bevy_render/src/render_resource/uniform_buffer.rs
+++ crates/bevy_render/src/render_resource/uniform_buffer.rs
@@ -102,7 +102,7 @@ impl<T: ShaderType + WriteInto> UniformBuffer<T> {
         self.label.as_deref()
     }
 
-    /// Set the buffer usage of the buffer.
+    /// Adds more [`BufferUsages`] to the buffer.
     ///
     /// This method only allows addition of flags to the default usage flags.
     ///
@@ -223,7 +223,7 @@ impl<T: ShaderType + WriteInto> DynamicUniformBuffer<T> {
         self.label.as_deref()
     }
 
-    /// Set the buffer usage of the buffer.
+    /// Adds more [`BufferUsages`] to the buffer.
     ///
     /// This method only allows addition of flags to the default usage flags.
     ///
