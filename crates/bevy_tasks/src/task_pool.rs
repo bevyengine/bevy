a7fa621effc0826f604e730f224d7d17c0b162d4
Mike Hsu <mike.hsu@gmail.com>
2023-02-07 21:16:23 -0800

    create a new scope executor for every scope

--- crates/bevy_tasks/src/task_pool.rs
+++ crates/bevy_tasks/src/task_pool.rs
@@ -112,7 +112,6 @@ pub struct TaskPool {
 impl TaskPool {
     thread_local! {
         static LOCAL_EXECUTOR: async_executor::LocalExecutor<'static> = async_executor::LocalExecutor::new();
-        static THREAD_EXECUTOR: ThreadExecutor<'static> = ThreadExecutor::new();
     }
 
     /// Create a `TaskPool` with the default configuration.
@@ -275,9 +274,8 @@ impl TaskPool {
         F: for<'scope> FnOnce(&'scope Scope<'scope, 'env, T>),
         T: Send + 'static,
     {
-        Self::THREAD_EXECUTOR.with(|scope_executor| {
-            self.scope_with_executor_inner(true, scope_executor, scope_executor, f)
-        })
+        let scope_executor = ThreadExecutor::new();
+        self.scope_with_executor_inner(true, &scope_executor, &scope_executor, f)
     }
 
     /// This allows passing an external executor to spawn tasks on. When you pass an external executor
@@ -299,25 +297,24 @@ impl TaskPool {
         F: for<'scope> FnOnce(&'scope Scope<'scope, 'env, T>),
         T: Send + 'static,
     {
-        Self::THREAD_EXECUTOR.with(|scope_executor| {
-            // If a `external_executor` is passed use that. Otherwise get the executor stored
-            // in the `THREAD_EXECUTOR` thread local.
-            if let Some(external_executor) = external_executor {
-                self.scope_with_executor_inner(
-                    tick_task_pool_executor,
-                    external_executor,
-                    scope_executor,
-                    f,
-                )
-            } else {
-                self.scope_with_executor_inner(
-                    tick_task_pool_executor,
-                    scope_executor,
-                    scope_executor,
-                    f,
-                )
-            }
-        })
+        let scope_executor = ThreadExecutor::new();
+        // If a `external_executor` is passed use that. Otherwise get the executor stored
+        // in the `THREAD_EXECUTOR` thread local.
+        if let Some(external_executor) = external_executor {
+            self.scope_with_executor_inner(
+                tick_task_pool_executor,
+                external_executor,
+                &scope_executor,
+                f,
+            )
+        } else {
+            self.scope_with_executor_inner(
+                tick_task_pool_executor,
+                &scope_executor,
+                &scope_executor,
+                f,
+            )
+        }
     }
 
     fn scope_with_executor_inner<'env, F, T>(
