0f8bdb5c8e728b62cd1e0f3dc8ac782424e9c9d3
Rob Parrett <robparrett@gmail.com>
2023-02-20 15:21:45 -0700

    Fix some more typos

--- crates/bevy_pbr/src/light.rs
+++ crates/bevy_pbr/src/light.rs
@@ -2173,7 +2173,7 @@ mod test {
         // check a smaller number of clusters would not cover the screen
         assert!(clusters.tile_size.x * (clusters.dimensions.x - 1) < screen_size.x);
         assert!(clusters.tile_size.y * (clusters.dimensions.y - 1) < screen_size.y);
-        // check a smaller tilesize would not cover the screen
+        // check a smaller tile size would not cover the screen
         assert!((clusters.tile_size.x - 1) * clusters.dimensions.x < screen_size.x);
         assert!((clusters.tile_size.y - 1) * clusters.dimensions.y < screen_size.y);
         // check we don't have more clusters than pixels
