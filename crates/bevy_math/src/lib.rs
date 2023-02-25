18aa56f987d17d32cf2ff397b9e549e8ff6a659d
Aevyrie Roessler <aevyrie@gmail.com>
2023-02-25 03:36:03 -0800

    general purpose cubic curves

--- crates/bevy_math/src/lib.rs
+++ crates/bevy_math/src/lib.rs
@@ -6,16 +6,11 @@
 
 #![warn(missing_docs)]
 
-mod bezier;
-mod catmull_rom;
+mod cubic_splines;
 mod ray;
 mod rect;
 
-pub use catmull_rom::CatmullRom;
-pub use bezier::{
-    generic as generic_bezier, Bezier, CubicBezier2d, CubicBezier3d, CubicBezierEasing,
-    QuadraticBezier2d, QuadraticBezier3d,
-};
+pub use cubic_splines::*;
 pub use ray::Ray;
 pub use rect::Rect;
 
@@ -23,9 +18,9 @@ pub use rect::Rect;
 pub mod prelude {
     #[doc(hidden)]
     pub use crate::{
-        BVec2, BVec3, BVec4, Bezier, CubicBezier2d, CubicBezier3d, CubicBezierEasing, EulerRot,
-        IVec2, IVec3, IVec4, Mat2, Mat3, Mat4, QuadraticBezier2d, QuadraticBezier3d, Quat, Ray,
-        Rect, UVec2, UVec3, UVec4, Vec2, Vec3, Vec4,
+        BSpline, BVec2, BVec3, BVec4, Bezier, CardinalSpline, CubicGenerator, EulerRot, Hermite,
+        IVec2, IVec3, IVec4, Mat2, Mat3, Mat4, Quat, Ray, Rect, UVec2, UVec3, UVec4, Vec2, Vec3,
+        Vec4,
     };
 }
 
