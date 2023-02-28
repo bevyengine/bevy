adc811be9bdbbb63cac6c96ea1d98aafb36a6471
Ickshonpe <david.curthoys@googlemail.com>
2023-02-28 12:17:12 +0000

    Draw the text using text2d and trigger recomputations by modifying the text's bounds.

--- examples/stress_tests/text_pipeline.rs
+++ examples/stress_tests/text_pipeline.rs
@@ -1,12 +1,12 @@
 //! Text pipeline benchmark.
 //!
 //! Continuously recomputes a large `Text` component with 100 sections.
-//! No rendering.
+
 use bevy::{
     diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin},
     prelude::*,
     text::{
-        BreakLineOn, FontAtlasSet, FontAtlasWarning, TextPipeline, TextSettings, YAxisOrientation,
+        BreakLineOn, Text2dBounds,
     },
     window::{PresentMode, WindowPlugin},
 };
@@ -22,64 +22,52 @@ fn main() {
         }))
         .add_plugin(FrameTimeDiagnosticsPlugin::default())
         .add_plugin(LogDiagnosticsPlugin::default())
-        .init_resource::<TestText>()
-        .add_system(text_pipe)
+        .add_startup_system(spawn)
+        .add_system(update_text_bounds)
         .run();
 }
 
-#[derive(Resource)]
-struct TestText {
-    pub sections: Vec<TextSection>,
-}
-
-impl FromWorld for TestText {
-    fn from_world(world: &mut World) -> Self {
-        let asset_server = world.resource::<AssetServer>();
-        let mut sections = Vec::new();
-        for i in 1..=50 {
-            sections.push(TextSection {
-                value: "Hello World!".repeat(i),
+fn spawn(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+) {
+    commands.spawn(Camera2dBundle::default());
+    let sections =
+        (1..=50).flat_map(|i| [
+            TextSection {
+                value: "text".repeat(i),
                 style: TextStyle {
                     font: asset_server.load("fonts/FiraMono-Medium.ttf"),
-                    font_size: (10 + i % 10) as f32,
-                    ..Default::default()
+                    font_size: (4 + i % 10) as f32,
+                    color: Color::BLUE,
                 },
-            });
-            sections.push(TextSection {
-                value: "So long, Earth?".repeat(i),
+            },
+            TextSection {
+                value: "pipeline".repeat(i),
                 style: TextStyle {
                     font: asset_server.load("fonts/FiraSans-Bold.ttf"),
-                    font_size: (11 + i % 15) as f32,
-                    ..Default::default()
+                    font_size: (4 + i % 11) as f32,
+                    color: Color::YELLOW,
                 },
-            });
-        }
-        TestText { sections }
-    }
+            }]
+        ).collect::<Vec<_>>();
+    commands.spawn(Text2dBundle {
+        text: Text {
+            sections,
+            alignment: TextAlignment::Center,
+            linebreak_behaviour: BreakLineOn::AnyCharacter,
+        },
+        ..Default::default()
+    });
 }
 
-fn text_pipe(
-    mut text_pipeline: ResMut<TextPipeline>,
-    fonts: Res<Assets<Font>>,
-    mut font_atlas_set_storage: ResMut<Assets<FontAtlasSet>>,
-    mut texture_atlases: ResMut<Assets<TextureAtlas>>,
-    mut textures: ResMut<Assets<Image>>,
-    text_settings: Res<TextSettings>,
-    mut font_atlas_warning: ResMut<FontAtlasWarning>,
-    test_text: Res<TestText>,
+// changing the bounds of the text will cause a recomputation
+fn update_text_bounds(
+    time: Res<Time>,
+    mut text_bounds_query: Query<&mut Text2dBounds>,
 ) {
-    let _ = text_pipeline.queue_text(
-        &fonts,
-        &test_text.sections,
-        2.5,
-        TextAlignment::Center,
-        BreakLineOn::AnyCharacter,
-        Vec2::new(1000.0, f32::INFINITY),
-        &mut font_atlas_set_storage,
-        &mut texture_atlases,
-        &mut textures,
-        text_settings.as_ref(),
-        &mut font_atlas_warning,
-        YAxisOrientation::BottomToTop,
-    );
+    let width = (1. + time.elapsed_seconds().sin()) * 600.0;
+    for mut text_bounds in text_bounds_query.iter_mut() {
+        text_bounds.size.x = width;
+    }
 }
