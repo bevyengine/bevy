57779cb2caef5c0925b5efb720dc4464fdf53d6c
shuo <shuoli84@gmail.com>
2023-03-02 21:36:08 +0800

    use ENV to pass ci features

--- .github/example-run/load_gltf.sh
+++ .github/example-run/load_gltf.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-exec cargo run --example load_gltf --features "bevy_ci_testing,trace,trace_chrome,debug_asset_server"
+exec cargo run --example load_gltf --features $CI_FEATURES --features "debug_asset_server"
