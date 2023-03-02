f3f02d86798a954d824ffe1c8834c6621b9487d3
Ickshonpe <david.curthoys@googlemail.com>
2023-03-02 23:40:21 +0000

    fixed positions

--- examples/3d/tonemapping.rs
+++ examples/3d/tonemapping.rs
@@ -72,11 +72,8 @@ fn setup(
         )
         .with_style(Style {
             position_type: PositionType::Absolute,
-            position: UiRect {
-                top: Val::Px(10.0),
-                left: Val::Px(10.0),
-                ..default()
-            },
+            top: Val::Px(10.0),
+            left: Val::Px(10.0),
             ..default()
         }),
     );
