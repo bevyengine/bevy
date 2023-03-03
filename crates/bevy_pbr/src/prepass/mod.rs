554c9a084c9f91ae5cabfc60eef192dddca5da99
JMS55 <47158642+JMS55@users.noreply.github.com>
2023-03-03 18:38:18 -0500

    Update crates/bevy_pbr/src/prepass/mod.rs
    Co-authored-by: Edgar Geier <geieredgar@gmail.com>

--- crates/bevy_pbr/src/prepass/mod.rs
+++ crates/bevy_pbr/src/prepass/mod.rs
@@ -399,11 +399,12 @@ where
         }
 
         // The fragment shader is only used when the normal prepass or motion vectors prepass
-        // is enabled or the material uses alpha cutoff values
-        let fragment_required = key
-            .mesh_key
-            .intersects(MeshPipelineKey::ALPHA_MASK | MeshPipelineKey::BLEND_PREMULTIPLIED_ALPHA)
-            || !targets.is_empty();
+        // is enabled or the material uses alpha cutoff values and doesn't rely on the standard 
+        // prepass shader
+        let fragment_required = !targets.is_empty()
+            || ((key.mesh_key.contains(MeshPipelineKey::ALPHA_MASK)
+                || blend_key == MeshPipelineKey::BLEND_PREMULTIPLIED_ALPHA)
+                && self.material_fragment_shader.is_some())
 
         let fragment = fragment_required.then(|| {
             // Use the fragment shader from the material
