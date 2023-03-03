f046a0601f88e0406e4c3eefb175640d9eb72f7a
shuo <shuoli84@gmail.com>
2023-03-04 00:03:35 +0800

    Use Image::default for 1 pixel white texture directly

--- crates/bevy_sprite/src/mesh2d/mesh.rs
+++ crates/bevy_sprite/src/mesh2d/mesh.rs
@@ -208,12 +208,7 @@ impl FromWorld for Mesh2dPipeline {
         });
         // A 1x1x1 'all 1.0' texture to use as a dummy texture to use in place of optional StandardMaterial textures
         let dummy_white_gpu_image = {
-            let image = Image::new_fill(
-                Extent3d::default(),
-                TextureDimension::D2,
-                &[255u8; 4],
-                TextureFormat::bevy_default(),
-            );
+            let image = Image::default();
             let texture = render_device.create_texture(&image.texture_descriptor);
             let sampler = match image.sampler_descriptor {
                 ImageSampler::Default => (**default_sampler).clone(),
