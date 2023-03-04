666226f8720a7f5ebe46ddb11979d9e45c453304
Robert Swain <robert.swain@gmail.com>
2023-03-04 16:00:55 +0100

    Make Bevy logo movable in lighting example
    Co-authored-by: robtfm <50659922+robtfm@users.noreply.github.com>

--- examples/3d/lighting.rs
+++ examples/3d/lighting.rs
@@ -65,17 +65,21 @@ fn setup(
     // Bevy logo to demonstrate alpha mask shadows
     let mut transform = Transform::from_xyz(-2.2, 0.5, 1.0);
     transform.rotate_y(PI / 8.);
-    commands.spawn(PbrBundle {
-        mesh: meshes.add(Mesh::from(shape::Quad::new(Vec2::new(2.0, 0.5)))),
-        transform,
-        material: materials.add(StandardMaterial {
-            base_color_texture: Some(asset_server.load("branding/bevy_logo_light.png")),
-            perceptual_roughness: 1.0,
-            alpha_mode: AlphaMode::Mask(0.5),
-            cull_mode: None,
+    commands.spawn((
+        PbrBundle {
+            mesh: meshes.add(Mesh::from(shape::Quad::new(Vec2::new(2.0, 0.5)))),
+            transform,
+            material: materials.add(StandardMaterial {
+                base_color_texture: Some(asset_server.load("branding/bevy_logo_light.png")),
+                perceptual_roughness: 1.0,
+                alpha_mode: AlphaMode::Mask(0.5),
+                cull_mode: None,
+                ..default()
+            }),
             ..default()
-        }),
-        ..default()
+        },
+        Movable,
+    ));
     });
 
     // cube
