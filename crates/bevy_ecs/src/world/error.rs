bfbffd3ab4ef3f988bf99aa6d1c33938e0fd4c6a
Christian Hughes <xdotdash@gmail.com>
2023-03-10 14:10:54 -0600

    Add World::try_run_schedule

---
+++ crates/bevy_ecs/src/world/error.rs
@@ -0,0 +1,8 @@
+use thiserror::Error;
+
+use crate::schedule::BoxedScheduleLabel;
+
+/// The error type returned by `World::try_run_schedule` if the provided schedule does not exist.
+#[derive(Error, Debug)]
+#[error("The schedule with the label {0:?} was not found.")]
+pub struct TryRunScheduleError(pub BoxedScheduleLabel);
