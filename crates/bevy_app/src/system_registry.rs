f565203961d772544c3b2a58be83217f1acbefd6
Alejandro Pascual Pozo <alejandro.pascual.pozo@gmail.com>
2023-03-10 12:37:47 +0100

    remove run_systems_by_set

--- crates/bevy_app/src/system_registry.rs
+++ crates/bevy_app/src/system_registry.rs
@@ -3,6 +3,17 @@ use bevy_ecs::prelude::*;
 use bevy_ecs::system::{SystemId, SystemRegistryError};
 
 impl App {
+    /// Register a system with any number of [`SystemLabel`]s.
+    ///
+    /// Calls [`SystemRegistry::register_system`](bevy_ecs::system::SystemRegistry::register_system).
+    pub fn register_system<M, S: IntoSystem<(), (), M> + 'static>(
+        &mut self,
+        system: S,
+    ) -> &mut Self {
+        self.world.register_system(system);
+        self
+    }
+
     /// Runs the supplied system on the [`World`] a single time.
     ///
     /// Calls [`SystemRegistry::run_system`](bevy_ecs::system::SystemRegistry::run_system).
