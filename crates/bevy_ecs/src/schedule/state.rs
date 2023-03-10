11cb4e37e7cd656d7e244d4b2c6f0e7b7fd01115
Christian Hughes <xdotdash@gmail.com>
2023-03-10 14:18:01 -0600

    Remove unused import

--- crates/bevy_ecs/src/schedule/state.rs
+++ crates/bevy_ecs/src/schedule/state.rs
@@ -4,7 +4,7 @@ use std::mem;
 
 use crate as bevy_ecs;
 use crate::change_detection::DetectChangesMut;
-use crate::schedule::{ScheduleLabel, Schedules, SystemSet};
+use crate::schedule::{ScheduleLabel, SystemSet};
 use crate::system::Resource;
 use crate::world::World;
 
