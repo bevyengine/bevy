466df592113c4a162b67e9042908d587aa187a1a
IceSentry <IceSentry@users.noreply.github.com>
2023-03-15 01:49:15 -0400

    Update examples/shader/post_process_pass.rs
    Co-authored-by: JMS55 <47158642+JMS55@users.noreply.github.com>

--- examples/shader/post_process_pass.rs
+++ examples/shader/post_process_pass.rs
@@ -86,7 +86,7 @@ impl Plugin for PostProcessPlugin {
         );
 
         // We now need to add an edge between our node and the nodes from bevy
-        // to make sure our node is scheduled correctly.
+        // to make sure our node is ordered correctly relative to other nodes.
         //
         // Here we want our effect to run after tonemapping and before the end of the main pass post processing
         core_3d_graph.add_node_edge(core_3d::graph::node::TONEMAPPING, PostProcessNode::NAME);
