609b099e7c5ff945050b9dee2b7ffb30f03d5544
Christian Hughes <9044780+ItsDoot@users.noreply.github.com>
2023-03-16 20:22:54 -0500

    Add World::try_run_schedule (#8028)
    Co-authored-by: James Liu <contact@jamessliu.com>

--- crates/bevy_app/src/app.rs
+++ crates/bevy_app/src/app.rs
@@ -358,15 +358,10 @@ impl App {
                     .run_if(in_state(variant)),
             );
         }
-        // These are different for loops to avoid conflicting access to self
-        for variant in S::variants() {
-            if self.get_schedule(OnEnter(variant.clone())).is_none() {
-                self.add_schedule(OnEnter(variant.clone()), Schedule::new());
-            }
-            if self.get_schedule(OnExit(variant.clone())).is_none() {
-                self.add_schedule(OnExit(variant), Schedule::new());
-            }
-        }
+
+        // The OnEnter, OnExit, and OnTransition schedules are lazily initialized
+        // (i.e. when the first system is added to them), and World::try_run_schedule is used to fail
+        // gracefully if they aren't present.
 
         self
     }
