3b7c419dac6e664558d98c4b000f1bd32f97278d
Piotr Siuszko <siuszko@zoho.com>
2023-03-22 19:37:22 +0100

    Update ui/button example

--- examples/ui/button.rs
+++ examples/ui/button.rs
@@ -27,6 +27,10 @@ fn button_system(
     for (interaction, mut color, children) in &mut interaction_query {
         let mut text = text_query.get_mut(children[0]).unwrap();
         match *interaction {
+            Interaction::Released => {
+                text.sections[0].value = "Released".to_string();
+                *color = PRESSED_BUTTON.into();
+            }
             Interaction::Clicked => {
                 text.sections[0].value = "Press".to_string();
                 *color = PRESSED_BUTTON.into();
@@ -35,7 +39,7 @@ fn button_system(
                 text.sections[0].value = "Hover".to_string();
                 *color = HOVERED_BUTTON.into();
             }
-            _ => {
+            Interaction::None => {
                 text.sections[0].value = "Button".to_string();
                 *color = NORMAL_BUTTON.into();
             }
