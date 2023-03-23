617c53139fa735dba8ded84a320fb7b095c68429
Eric Walker <eric.walker@gmail.com>
2023-03-22 21:40:16 -0600

    Clarify wording of comment

--- crates/bevy_render/src/extract_param.rs
+++ crates/bevy_render/src/extract_param.rs
@@ -12,8 +12,8 @@ use std::ops::{Deref, DerefMut};
 /// contained in [`MainWorld`]. This parameter only works for systems run
 /// during the [`ExtractSchedule`](crate::ExtractSchedule).
 ///
-/// This requires that the contained [`SystemParam`] not mutate the world, as it
-/// uses a read-only reference to [`MainWorld`] internally.
+/// As a result, the contained [`SystemParam`] must not mutate the world, as it uses a
+/// read-only reference to [`MainWorld`] internally.
 ///
 /// ## Context
 ///
