a5ee1ab6471017ba3e31b6d67591ae0ab707f5a2
kjolnyr <kjolnyr@protonmail.com>
2023-03-24 05:40:50 +0100

    Removed impl Lerp for Color + adapted the example

--- examples/animation/linear_interpolation.rs
+++ examples/animation/linear_interpolation.rs
@@ -5,12 +5,7 @@ use std::f32::consts::PI;
 use bevy::prelude::*;
 
 #[derive(Component)]
-pub enum LerpExample {
-    Translation(Vec3, Vec3),
-    Rotation(Quat, Quat),
-    Scale(f32, f32),
-    Color(Color, Color),
-}
+pub struct Sphere;
 
 fn main() {
     App::new()
@@ -25,47 +20,15 @@ fn setup(
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
-
-    // Spawning cubes to experiment on
+    // Spawning a sphere to experiment on
     commands.spawn((
         PbrBundle {
             mesh: meshes.add(shape::Cube::default().into()),
             material: materials.add(Color::ORANGE.into()),
-            transform: Transform::from_xyz(-6., 2., 0.)
-                .with_rotation(Quat::from_rotation_x(-PI / 4.)),
+            transform: Transform::from_xyz(0., 2., 0.),
             ..default()
         },
-        LerpExample::Translation(Vec3::new(-6., 2., 0.), Vec3::new(-6., 4., 0.)),
-    ));
-    commands.spawn((
-        PbrBundle {
-            mesh: meshes.add(shape::Cube::default().into()),
-            material: materials.add(Color::ORANGE.into()),
-            transform: Transform::from_xyz(-2., 2., 0.)
-                .with_rotation(Quat::from_rotation_x(-PI / 4.)),
-            ..default()
-        },
-        LerpExample::Rotation(Quat::from_rotation_x(-PI / 4.), Quat::from_rotation_z(PI)),
-    ));
-    commands.spawn((
-        PbrBundle {
-            mesh: meshes.add(shape::Cube::default().into()),
-            material: materials.add(Color::ORANGE.into()),
-            transform: Transform::from_xyz(2., 2., 0.)
-                .with_rotation(Quat::from_rotation_x(-PI / 4.)),
-            ..default()
-        },
-        LerpExample::Scale(1., 2.),
-    ));
-    commands.spawn((
-        PbrBundle {
-            mesh: meshes.add(shape::Cube::default().into()),
-            material: materials.add(Color::ORANGE.into()),
-            transform: Transform::from_xyz(6., 2., 0.)
-                .with_rotation(Quat::from_rotation_x(-PI / 4.)),
-            ..default()
-        },
-        LerpExample::Color(Color::ORANGE, Color::PURPLE),
+        Sphere,
     ));
 
     // Some light to see something
@@ -94,34 +57,18 @@ fn setup(
     });
 }
 
-pub fn linear_interpolate(
-    time: Res<Time>,
-    mut query: Query<(&mut Transform, &mut Handle<StandardMaterial>, &LerpExample)>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
-) {
+pub fn linear_interpolate(time: Res<Time>, mut query: Query<&mut Transform, With<Sphere>>) {
     // s lies between 0 and 1
     let s = (time.elapsed_seconds().sin() + 1.) / 2.;
 
-    for (mut transform, handle, lerp_example) in &mut query {
-        match lerp_example {
-            LerpExample::Translation(origin, target) => {
-                // Linear interpolation on Vec (Vec2 or Vec3, they are used the same way)
-                transform.translation = origin.lerp(*target, s);
-            }
-            LerpExample::Rotation(origin, target) => {
-                // Spherical linear interpolation on Quat
-                transform.rotation = origin.slerp(*target, s);
-            }
-            LerpExample::Scale(origin, target) => {
-                // Lerp on f32 directly
-                transform.scale = Vec3::splat(origin.lerp(*target, s))
-            }
-            LerpExample::Color(origin, target) => {
-                let Some(mut mat) = materials.get_mut(&handle) else { continue; };
+    for mut transform in &mut query {
+        // // Linear interpolation on Vec3 (lerp on Vec2 is the same format)
+        transform.translation = Vec3::new(0., 2., 0.).lerp(Vec3::new(0., 4., 0.), s);
 
-                // Lerp on colors
-                mat.base_color = origin.lerp(*target, s);
-            }
-        }
+        // Spherical linear interpolation on Quat
+        transform.rotation = Quat::from_rotation_x(-PI / 4.).slerp(Quat::from_rotation_z(PI), s);
+
+        // Lerp on f32 directly
+        transform.scale = Vec3::splat((1.).lerp(2., s));
     }
 }
