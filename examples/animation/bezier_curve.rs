948f4417bed6a0d9fa9b65abdd9d21b4744bfdd8
Kjolnyr <105886300+Kjolnyr@users.noreply.github.com>
2023-03-24 18:07:31 +0100

    Adding a line showing the cuve
    Co-authored-by: ira <JustTheCoolDude@gmail.com>

--- examples/animation/bezier_curve.rs
+++ examples/animation/bezier_curve.rs
@@ -82,10 +82,16 @@ fn setup(
     });
 }
 
-pub fn animate_cube(time: Res<Time>, mut query: Query<(&mut Transform, &CubeCurve)>) {
+pub fn animate_cube(
+    time: Res<Time>,
+    mut query: Query<(&mut Transform, &CubeCurve)>,
+    mut gizmos: Gizmos,
+) {
     let step = (time.elapsed_seconds().sin() + 1.) / 2.;
 
     for (mut transform, cube_curve) in &mut query {
+        // Draw the curve
+        gizmos.linestrip(cube_curve.0.iter_positions(50), Color::WHITE);
         // position takes a point from the curve where 0 is the initial point
         // and 1 is the last point
         transform.translation = cube_curve.0.position(step);
