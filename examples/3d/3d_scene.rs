007f20ea199ef8775bcb8ac0c599ec049183471a
Visclodev <72014629+Visclodev@users.noreply.github.com>
2023-03-26 13:23:36 +0200

    Update 3d_scene.rs

--- examples/3d/3d_scene.rs
+++ examples/3d/3d_scene.rs
@@ -5,7 +5,7 @@ use bevy::prelude::*;
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_systems(Startup, setup)
+        .add_startup_system(setup)
         .run();
 }
 
