57f8ec1031db3d22e9837d9bae0f2c308873f29f
Mossa <cgmossa@gmail.com>
2023-03-28 14:25:21 +0200

    Test of the macro. QUESTION: Where should this be located?

---
+++ crates/bevy_ecs/tests/system_param.rs
@@ -0,0 +1,66 @@
+use bevy_ecs::prelude::*;
+use bevy_ecs::system::SystemParam;
+
+#[allow(dead_code)]
+#[derive(Debug, Resource)]
+struct PlayerName(String);
+
+#[derive(Debug, Component)]
+struct Companion;
+
+#[allow(dead_code)]
+#[derive(Debug, SystemParam)]
+struct UsualLifetime<'w> {
+    player_name: Res<'w, PlayerName>,
+}
+
+#[allow(dead_code)]
+#[derive(Debug, SystemParam)]
+struct UsualLifetimes<'w, 's> {
+    player_name: Res<'w, PlayerName>,
+    companions: Query<'w, 's, &'static Companion>,
+}
+
+#[allow(dead_code)]
+#[derive(Debug, SystemParam)]
+struct UnusualLifetime<'world> {
+    player_name: Res<'world, PlayerName>,
+}
+
+#[allow(dead_code)]
+#[derive(Debug, SystemParam)]
+struct UnusualLifetimes<'world, 'state> {
+    player_name: Res<'world, PlayerName>,
+    companions: Query<'world, 'state, &'static Companion>,
+}
+
+#[allow(dead_code)]
+#[derive(Debug, SystemParam)]
+struct MixLifetimes<'w, 'state> {
+    player_name: Res<'w, PlayerName>,
+    companions: Query<'w, 'state, &'static Companion>,
+}
+
+// must fail to compile
+// #[allow(dead_code)]
+// #[derive(Debug, SystemParam)]
+// struct DuplicateWorldLifetime<'w, 'world> {
+//     player_name: Res<'w, PlayerName>
+// }
+
+// must fail to compile
+// #[allow(dead_code)]
+// #[derive(Debug, SystemParam)]
+// struct DuplicateStateLifetime<'s, 'cache> {
+//     player_name: Local<'cache, String>
+// }
+
+
+// UNIMPLEMENTED, but should succeed
+// #[allow(dead_code)]
+// #[derive(Debug, SystemParam)]
+// struct ExtraLifetimes<'w, 'a, 'b> {
+//     player_name: Res<'w, PlayerName>,
+//     // companions: Query<'w, 'state, &'static Companion>,
+//     _s: PhantomData<&'b &'a ()>,
+// }
