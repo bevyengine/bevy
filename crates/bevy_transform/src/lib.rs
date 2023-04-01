09d669af5ba442676fda413139f31d9a24c6d3a0
devil-ira <justthecooldude@gmail.com>
2023-04-01 22:47:50 +0200

    Add `Transform2dBundle`

--- crates/bevy_transform/src/lib.rs
+++ crates/bevy_transform/src/lib.rs
@@ -77,6 +77,63 @@ impl From<Transform> for TransformBundle {
         Self::from_transform(transform)
     }
 }
+
+/// A [`Bundle`] of the [`Transform2d`] and [`GlobalTransform2d`]
+/// [`Component`](bevy_ecs::component::Component)s, which describe the position of an entity.
+///
+/// * To place or move an entity, you should set its [`Transform2d`].
+/// * To get the global transform of an entity, you should get its [`GlobalTransform2d`].
+/// * For transform hierarchies to work correctly, you must have both a [`Transform2d`] and a [`GlobalTransform2d`].
+///   * You may use the [`TransformBundle`] to guarantee this.
+///
+/// ## [`Transform2d`] and [`GlobalTransform2d`]
+///
+/// [`Transform2d`] is the position of an entity relative to its parent position, or the reference
+/// frame if it doesn't have a parent.
+///
+/// [`GlobalTransform2d`] is the position of an entity relative to the reference frame.
+///
+/// [`GlobalTransform2d`] is updated from [`Transform2d`] by systems in the system set
+/// [`TransformPropagate`](crate::TransformSystem::TransformPropagate).
+///
+/// This system runs during [`PostUpdate`](bevy_app::PostUpdate). If you
+/// update the [`Transform2d`] of an entity in this schedule or after, you will notice a 1 frame lag
+/// before the [`GlobalTransform2d`] is updated.
+#[derive(Bundle, Clone, Copy, Debug, Default)]
+pub struct Transform2dBundle {
+    /// The transform of the entity.
+    pub local: Transform2d,
+    /// The global transform of the entity.
+    pub global: GlobalTransform2d,
+}
+
+impl Transform2dBundle {
+    /// An identity [`TransformBundle2d`] with no translation, rotation, and a scale of 1 on all axes.
+    pub const IDENTITY: Self = Transform2dBundle {
+        local: Transform2d::IDENTITY,
+        global: GlobalTransform2d::IDENTITY,
+    };
+
+    /// Creates a new [`TransformBundle2d`] from a [`Transform2d`].
+    ///
+    /// This initializes [`GlobalTransform2d`] as identity, to be updated later by the
+    /// [`PostUpdate`](bevy_app::PostUpdate) schedule.
+    #[inline]
+    pub const fn from_transform(transform: Transform2d) -> Self {
+        Transform2dBundle {
+            local: transform,
+            ..Self::IDENTITY
+        }
+    }
+}
+
+impl From<Transform2d> for Transform2dBundle {
+    #[inline]
+    fn from(transform: Transform2d) -> Self {
+        Self::from_transform(transform)
+    }
+}
+
 /// Set enum for the systems relating to transform propagation
 #[derive(Debug, Hash, PartialEq, Eq, Clone, SystemSet)]
 pub enum TransformSystem {
