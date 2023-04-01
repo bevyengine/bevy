3215c0fff0c6d1e597b93243d95781425de4bf4a
JMS55 <47158642+JMS55@users.noreply.github.com>
2023-04-01 11:18:54 -0400

    Check for admin privileges

--- tools/nvidia-gpu-lock.sh
+++ tools/nvidia-gpu-lock.sh
@@ -3,7 +3,10 @@
 graphics=$(nvidia-smi --query-supported-clocks=graphics --format=csv | sed -n 2p | tr -d -c 0-9)
 memory=$(nvidia-smi --query-supported-clocks=memory --format=csv | sed -n 2p | tr -d -c 0-9)
 
-nvidia-smi --lock-gpu-clocks=$graphics
+if ! nvidia-smi --lock-gpu-clocks=$graphics; then
+    echo -e "\nRerun with administrator privileges."
+    exit
+fi
 nvidia-smi --lock-memory-clocks=$memory
 
 function reset {
