e588952a8ff673cc9c7d8a2e7560d4e5e267be5b
devil-ira <justthecooldude@gmail.com>
2023-04-01 23:14:44 +0200

    Fix incorrect `transform_point` docs Who could have possibly messed this up? *whistles*

--- crates/bevy_transform/src/components/global_transform.rs
+++ crates/bevy_transform/src/components/global_transform.rs
@@ -185,7 +185,7 @@ impl GlobalTransform {
 
     /// Transforms the given `point`, applying shear, scale, rotation and translation.
     ///
-    /// This moves `point` into the local space of this [`GlobalTransform`].
+    /// This moves the `point` from the local space of this entity into global space.
     #[inline]
     pub fn transform_point(&self, point: Vec3) -> Vec3 {
         self.0.transform_point3(point)
