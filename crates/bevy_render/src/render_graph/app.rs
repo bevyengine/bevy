9a36d58db4d50d9b4fcc064a886e18a61d6858de
IceSentry <c.giguere42@gmail.com>
2023-04-03 20:34:37 -0400

    add single edge variant

--- crates/bevy_render/src/render_graph/app.rs
+++ crates/bevy_render/src/render_graph/app.rs
@@ -19,6 +19,13 @@ pub trait RenderGraphApp {
         sub_graph_name: &'static str,
         edges: &[&'static str],
     ) -> &mut Self;
+    /// Add node edge to the specified graph
+    fn add_render_graph_edge(
+        &mut self,
+        sub_graph_name: &'static str,
+        output_edge: &'static str,
+        input_edge: &'static str,
+    ) -> &mut Self;
 }
 
 impl RenderGraphApp for App {
@@ -47,6 +54,20 @@ impl RenderGraphApp for App {
         );
         let graph = render_graph.sub_graph_mut(sub_graph_name);
         graph.add_node_edges(edges);
+        self
+    }
+
+    fn add_render_graph_edge(
+        &mut self,
+        sub_graph_name: &'static str,
+        output_edge: &'static str,
+        input_edge: &'static str,
+    ) -> &mut Self {
+        let mut render_graph = self.world.get_resource_mut::<RenderGraph>().expect(
+            "RenderGraph not found. Make sure you are using add_render_graph_node on the RenderApp",
+        );
+        let graph = render_graph.sub_graph_mut(sub_graph_name);
+        graph.add_node_edge(output_edge, input_edge);
         self
     }
 }
