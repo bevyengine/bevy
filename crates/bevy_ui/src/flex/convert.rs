accd08ec8763d1e8b83560b446d46db444e07622
ickshonpe <david.curthoys@googlemail.com>
2023-04-13 21:03:03 +0100

    Cargo fmt --all

--- crates/bevy_ui/src/flex/convert.rs
+++ crates/bevy_ui/src/flex/convert.rs
@@ -270,12 +270,8 @@ mod tests {
                 height: Val::Percent(0.),
             },
         };
-        let viewport_values = LayoutContext::new(
-            1.0,
-            bevy_math::Vec2::new(800., 600.),
-            false,
-            false,
-        );
+        let viewport_values =
+            LayoutContext::new(1.0, bevy_math::Vec2::new(800., 600.), false, false);
         let taffy_style = from_style(&viewport_values, &bevy_style);
         assert_eq!(taffy_style.display, taffy::style::Display::Flex);
         assert_eq!(taffy_style.position, taffy::style::Position::Absolute);
@@ -405,12 +401,7 @@ mod tests {
     #[test]
     fn test_into_length_percentage() {
         use taffy::style::LengthPercentage;
-        let context = LayoutContext::new(
-            2.0,
-            bevy_math::Vec2::new(800., 600.),
-            false,
-            false,
-        );
+        let context = LayoutContext::new(2.0, bevy_math::Vec2::new(800., 600.), false, false);
         let cases = [
             (Val::Auto, LengthPercentage::Points(0.)),
             (Val::Percent(1.), LengthPercentage::Percent(0.01)),
