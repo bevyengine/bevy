29e3e33c4a807d1c1c21a6f44706b89cc1219a16
JoJoJet <21144246+JoJoJet@users.noreply.github.com>
2023-04-14 08:24:27 -0400

    revert fixed timestep changes

--- crates/bevy_time/src/fixed_timestep.rs
+++ crates/bevy_time/src/fixed_timestep.rs
@@ -106,9 +106,15 @@ pub fn run_fixed_update_schedule(world: &mut World) {
     let mut fixed_time = world.resource_mut::<FixedTime>();
     fixed_time.tick(delta_time);
 
-    // Run the schedule until we run out of accumulated time
-    while world.resource_mut::<FixedTime>().expend().is_ok() {
-        let _ = world.try_run_schedule(FixedUpdate);
+    // Run the schedule until we run out of accumulated timelet mut check_again = true;
+    while check_again {
+        let mut fixed_time = world.resource_mut::<FixedTime>();
+        let fixed_time_run = fixed_time.expend().is_ok();
+        if fixed_time_run {
+            let _ = world.try_run_schedule(FixedUpdate);
+        } else {
+            check_again = false;
+        }
     }
 }
 
