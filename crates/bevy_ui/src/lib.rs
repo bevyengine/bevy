19c8bf6eeeb8a6ae606b6eaa2895b1418c0d17a4
InnocentusLime <InnocentusLime.help@gmail.com>
2023-04-17 22:38:19 +0300

    Added register for ZIndex

--- crates/bevy_ui/src/lib.rs
+++ crates/bevy_ui/src/lib.rs
@@ -94,6 +94,7 @@ impl Plugin for UiPlugin {
             .register_type::<Interaction>()
             .register_type::<JustifyContent>()
             .register_type::<Node>()
+            .register_type::<ZIndex>()
             // NOTE: used by Style::aspect_ratio
             .register_type::<Option<f32>>()
             .register_type::<Overflow>()
