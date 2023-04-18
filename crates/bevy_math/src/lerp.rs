4382cc189210addef1d29d900a38cfe62022638c
Kjolnyr <kjolnyr@protonmail.com>
2023-04-18 12:00:25 +0200

    Better comments

--- crates/bevy_math/src/lerp.rs
+++ crates/bevy_math/src/lerp.rs
@@ -1,13 +1,17 @@
 /// Provides a trait to implement the linear interpolation function.
+/// type `Scalar` is used to interpolate between `self` and `rhs`
+/// it should provide a way to range from `self` to `rhs` linearily
+/// and be able to extrapolate if the user input a value less than min
+/// or greater than max.
 pub trait Lerp {
     /// The Scalar type used in the linear interpolation function.
     type Scalar;
 
     /// Performs a linear interpolation between `self` and `rhs` based on the value `s`.
     ///
-    /// When `s` is `0.0`, the result will be equal to `self`.  When `s` is `1.0`, the result
-    /// will be equal to `rhs`. When `s` is outside of range `[0, 1]`, the result is linearly
-    /// extrapolated.
+    /// If `Scalar` is f32 or f64, when `s` is `0.0`, the result will be equal to `self`.
+    /// When `s` is `1.0`, the result will be equal to `rhs`. When `s` is outside of range `[0, 1]`,
+    /// the result is linearly extrapolated.
     fn lerp(self, rhs: Self, s: Self::Scalar) -> Self;
 }
 
