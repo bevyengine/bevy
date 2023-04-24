890309b1950d257b780ea67ca224fca49abd67d8
radiish <thesethskigamer@gmail.com>
2023-04-24 19:58:32 +0000

    `Reflect` in `std_traits`

--- crates/bevy_reflect/src/std_traits.rs
+++ crates/bevy_reflect/src/std_traits.rs
@@ -1,20 +1,20 @@
-use crate::{FromType, PartialReflect};
+use crate::{FromType, Reflect};
 
 /// A struct used to provide the default value of a type.
 ///
 /// A [`ReflectDefault`] for type `T` can be obtained via [`FromType::from_type`].
 #[derive(Clone)]
 pub struct ReflectDefault {
-    default: fn() -> Box<dyn PartialReflect>,
+    default: fn() -> Box<dyn Reflect>,
 }
 
 impl ReflectDefault {
-    pub fn default(&self) -> Box<dyn PartialReflect> {
+    pub fn default(&self) -> Box<dyn Reflect> {
         (self.default)()
     }
 }
 
-impl<T: PartialReflect + Default> FromType<T> for ReflectDefault {
+impl<T: Reflect + Default> FromType<T> for ReflectDefault {
     fn from_type() -> Self {
         ReflectDefault {
             default: || Box::<T>::default(),
