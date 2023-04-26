8ac3ef127ba73d2dfc14935a57fbd4ee499b3151
Marco Buono <thecoreh@gmail.com>
2023-04-25 22:20:50 -0300

    Clear motion vector to 0.0, fixing artifacts against the background

--- crates/bevy_core_pipeline/src/prepass/node.rs
+++ crates/bevy_core_pipeline/src/prepass/node.rs
@@ -82,9 +82,9 @@ impl Node for PrepassNode {
                 view: &view_motion_vectors_texture.default_view,
                 resolve_target: None,
                 ops: Operations {
-                    // Blue channel doesn't matter, but set to 1.0 for possible faster clear
+                    // Blue channel doesn't matter, but set to 0.0 for possible faster clear
                     // https://gpuopen.com/performance/#clears
-                    load: LoadOp::Clear(Color::rgb_linear(1.0, 1.0, 1.0).into()),
+                    load: LoadOp::Clear(Color::rgb_linear(0.0, 0.0, 0.0).into()),
                     store: true,
                 },
             },
