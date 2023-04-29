c2b47f7a46895ecfa63c4935d62bc82e08f468cf
tigregalis <anak.harimau@gmail.com>
2023-04-30 01:52:29 +0800

    make text update lazier in tonemapping example

--- examples/3d/tonemapping.rs
+++ examples/3d/tonemapping.rs
@@ -492,8 +492,15 @@ fn update_ui(
     }
     text.clear();
     if *hide_ui {
+        if !text_query.single().sections[0].value.is_empty() {
+            // single_mut() always triggers change detection,
+            // so only access if text actually needs changing
+            text_query.single_mut().sections[0].value.clear();
+        }
         return;
     }
+
+    let mut text = String::with_capacity(text_query.single().sections[0].value.len());
 
     let scn = current_scene.0;
     text.push_str("(H) Hide UI\n\n");
@@ -597,6 +604,12 @@ fn update_ui(
 
     if current_scene.0 == 1 {
         text.push_str("(Enter) Reset all to scene recommendation\n");
+    }
+
+    if text != text_query.single().sections[0].value {
+        // single_mut() always triggers change detection,
+        // so only access if text actually changed
+        text_query.single_mut().sections[0].value = text;
     }
 }
 
