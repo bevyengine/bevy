f5ca55d4af4bedc1133d17780b19f95483b30a15
Robert Swain <robert.swain@gmail.com>
2023-05-02 00:32:03 +0200

    Clarify logic around max_storage_buffers_per_shader_stage
    Use == 0 instead of < 1.

--- crates/bevy_render/src/render_resource/gpu_list.rs
+++ crates/bevy_render/src/render_resource/gpu_list.rs
@@ -81,7 +81,7 @@ impl<T: GpuListable> GpuList<T> {
         BindGroupLayoutEntry {
             binding,
             visibility,
-            ty: if device.limits().max_storage_buffers_per_shader_stage < 1 {
+            ty: if device.limits().max_storage_buffers_per_shader_stage == 0 {
                 BindingType::Buffer {
                     ty: BufferBindingType::Uniform,
                     has_dynamic_offset: true,
@@ -109,7 +109,7 @@ impl<T: GpuListable> GpuList<T> {
 
     pub fn batch_size(device: &RenderDevice) -> Option<u32> {
         let limits = device.limits();
-        if limits.max_storage_buffers_per_shader_stage < 1 {
+        if limits.max_storage_buffers_per_shader_stage == 0 {
             Some(BatchedUniformBuffer::<T>::batch_size(&limits) as u32)
         } else {
             None
