bed9c9da7724b19462ec466f714274367285f671
ickshonpe <david.curthoys@googlemail.com>
2023-05-11 12:44:25 +0100

    Removed `WinitSettings::desktop_app()`

--- examples/ui/node_order.rs
+++ examples/ui/node_order.rs
@@ -4,8 +4,6 @@ use bevy::{prelude::*, winit::WinitSettings};
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        // Only run the app when there is user input. This will significantly reduce CPU/GPU use.
-        .insert_resource(WinitSettings::desktop_app())
         .add_systems(Startup, setup)
         .add_systems(Update, (update_ordered_buttons, update_direction_button))
         .run();
