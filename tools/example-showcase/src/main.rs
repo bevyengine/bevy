599ab0bfb3e0131e4d62cb791be21d436069d10d
François <mockersf@gmail.com>
2023-05-19 14:05:21 +0200

    can not optimize for size

--- tools/example-showcase/src/main.rs
+++ tools/example-showcase/src/main.rs
@@ -54,6 +54,10 @@ enum Action {
         #[arg(long)]
         /// Enable hacks for Bevy website integration
         website_hacks: bool,
+
+        #[arg(long)]
+        /// Optimize the wasm file for size with wasm-opt
+        optimize_size: bool,
     },
 }
 
@@ -248,6 +252,7 @@ header_message = \"Examples (WebGPU)\"
         Action::BuildWebGPUExamples {
             content_folder,
             website_hacks,
+            optimize_size,
         } => {
             let examples_to_build = parse_examples();
 
@@ -290,12 +295,21 @@ header_message = \"Examples (WebGPU)\"
 
                 let sh = Shell::new().unwrap();
                 let example = &to_build.technical_name;
-                cmd!(
-                    sh,
-                    "cargo run -p build-wasm-example -- --api webgpu {example} --optimize-size"
-                )
-                .run()
-                .unwrap();
+                if optimize_size {
+                    cmd!(
+                        sh,
+                        "cargo run -p build-wasm-example -- --api webgpu {example} --optimize-size"
+                    )
+                    .run()
+                    .unwrap();
+                } else {
+                    cmd!(
+                        sh,
+                        "cargo run -p build-wasm-example -- --api webgpu {example}"
+                    )
+                    .run()
+                    .unwrap();
+                }
 
                 let category_path = root_path.join(&to_build.category);
                 let _ = fs::create_dir_all(&category_path);
@@ -312,10 +326,17 @@ header_message = \"Examples (WebGPU)\"
                     Path::new("examples/wasm/target/wasm_example.js"),
                     &example_path.join("wasm_example.js"),
                 );
-                let _ = fs::rename(
-                    Path::new("examples/wasm/target/wasm_example_bg.wasm.optimized"),
-                    &example_path.join("wasm_example_bg.wasm"),
-                );
+                if optimize_size {
+                    let _ = fs::rename(
+                        Path::new("examples/wasm/target/wasm_example_bg.wasm.optimized"),
+                        &example_path.join("wasm_example_bg.wasm"),
+                    );
+                } else {
+                    let _ = fs::rename(
+                        Path::new("examples/wasm/target/wasm_example_bg.wasm"),
+                        &example_path.join("wasm_example_bg.wasm"),
+                    );
+                }
                 pb.inc();
             }
             pb.finish_print("done");
