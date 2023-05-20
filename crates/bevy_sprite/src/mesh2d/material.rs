be9cda020ba2aa06667c099bda88151f9fad3a02
Tin Rabzelj <tin@flinect.com>
2023-05-20 09:18:36 +0200

    Make Material2d pipeline utils public

--- crates/bevy_sprite/src/mesh2d/material.rs
+++ crates/bevy_sprite/src/mesh2d/material.rs
@@ -432,7 +432,7 @@ pub struct PreparedMaterial2d<T: Material2d> {
 }
 
 #[derive(Resource)]
-struct ExtractedMaterials2d<M: Material2d> {
+pub struct ExtractedMaterials2d<M: Material2d> {
     extracted: Vec<(Handle<M>, M)>,
     removed: Vec<Handle<M>>,
 }
@@ -458,7 +458,7 @@ impl<T: Material2d> Default for RenderMaterials2d<T> {
 
 /// This system extracts all created or modified assets of the corresponding [`Material2d`] type
 /// into the "render world".
-fn extract_materials_2d<M: Material2d>(
+pub fn extract_materials_2d<M: Material2d>(
     mut commands: Commands,
     mut events: Extract<EventReader<AssetEvent<M>>>,
     assets: Extract<Res<Assets<M>>>,
@@ -505,7 +505,7 @@ impl<M: Material2d> Default for PrepareNextFrameMaterials<M> {
 
 /// This system prepares all assets of the corresponding [`Material2d`] type
 /// which where extracted this frame for the GPU.
-fn prepare_materials_2d<M: Material2d>(
+pub fn prepare_materials_2d<M: Material2d>(
     mut prepare_next_frame: Local<PrepareNextFrameMaterials<M>>,
     mut extracted_assets: ResMut<ExtractedMaterials2d<M>>,
     mut render_materials: ResMut<RenderMaterials2d<M>>,
