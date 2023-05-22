76509f25dced63f1a6555fe8d16ec374fbe9e44a
ickshonpe <david.curthoys@googlemail.com>
2023-05-22 22:36:27 +0100

    Require a full update on window created events

--- crates/bevy_ui/src/layout/mod.rs
+++ crates/bevy_ui/src/layout/mod.rs
@@ -217,7 +217,8 @@ pub fn ui_layout_system(
     windows: Query<(Entity, &Window)>,
     ui_scale: Res<UiScale>,
     mut scale_factor_events: EventReader<WindowScaleFactorChanged>,
-    mut resize_events: EventReader<bevy_window::WindowResized>,
+    mut window_resize_events: EventReader<bevy_window::WindowResized>,
+    mut window_created_events: EventReader<bevy_window::WindowCreated>,
     mut ui_surface: ResMut<UiSurface>,
     root_node_query: Query<Entity, (With<Node>, Without<Parent>)>,
     style_query: Query<(Entity, Ref<Style>), With<Node>>,
@@ -263,9 +264,12 @@ pub fn ui_layout_system(
             return;
         };
 
-    let resized = resize_events
+        let primary_window_modified = window_resize_events
         .iter()
-        .any(|resized_window| resized_window.window == primary_window_entity);
+        .any(|resized_window| resized_window.window == primary_window_entity)
+        || window_created_events
+            .iter()
+            .any(|window_created| window_created.window == primary_window_entity);
 
     // update window root nodes
     for (entity, window) in windows.iter() {
@@ -276,7 +280,7 @@ pub fn ui_layout_system(
 
     let layout_context = LayoutContext::new(scale_factor, physical_size);
 
-    if !scale_factor_events.is_empty() || ui_scale.is_changed() || resized {
+    if !scale_factor_events.is_empty() || ui_scale.is_changed() || primary_window_modified {
         scale_factor_events.clear();
         // update all nodes
         for (entity, style) in style_query.iter() {
