be5b7056ac101d2e6f84239ffd296d82fa3679fc
Peter Hayman <peteyhayman@gmail.com>
2023-05-24 14:51:15 +1000

    remove comment from a different pr.

--- crates/bevy_render/src/camera/camera.rs
+++ crates/bevy_render/src/camera/camera.rs
@@ -542,7 +542,6 @@ impl NormalizedRenderTarget {
 /// [`OrthographicProjection`]: crate::camera::OrthographicProjection
 /// [`PerspectiveProjection`]: crate::camera::PerspectiveProjection
 /// [`Projection`]: crate::camera::Projection
-/// [`CoreSet::PostUpdate`]: bevy_app::CoreSet::PostUpdate
 #[allow(clippy::too_many_arguments)]
 pub fn camera_system<T: CameraProjection + Component>(
     mut window_resized_events: EventReader<WindowResized>,
