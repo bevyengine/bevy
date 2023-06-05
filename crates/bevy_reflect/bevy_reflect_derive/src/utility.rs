ebbab1ed4e89655937e66fc1233c93fc61def9ef
Jamie Ridding <jamie.ridding@hotmail.co.uk>
2023-06-05 23:25:43 +0100

    Add a comment explaining the reason for seemingly spurious parentheses.

--- crates/bevy_reflect/bevy_reflect_derive/src/utility.rs
+++ crates/bevy_reflect/bevy_reflect_derive/src/utility.rs
@@ -155,6 +155,11 @@ pub(crate) fn extend_where_clause(
     } else {
         quote!()
     };
+
+    // The nested parentheses here are required to properly scope HRTBs coming
+    // from field types to the type itself, as the compiler will scope them to
+    // the whole bound by default, resulting in a failure to prove trait
+    // adherence.
     generic_where_clause.extend(quote! {
         #((#active_types): #active_trait_bounds,)*
         #((#ignored_types): #ignored_trait_bounds,)*
