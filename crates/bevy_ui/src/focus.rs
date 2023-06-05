afdff7adfed07c04937391633f51dcaca1405bf3
ickshonpe <david.curthoys@googlemail.com>
2023-06-05 23:20:01 +0100

    Modified `ui_focus_system` to support multiple windows.

--- crates/bevy_ui/src/focus.rs
+++ crates/bevy_ui/src/focus.rs
@@ -13,7 +13,7 @@ use bevy_math::Vec2;
 use bevy_reflect::{
     FromReflect, Reflect, ReflectDeserialize, ReflectFromReflect, ReflectSerialize,
 };
-use bevy_render::{camera::NormalizedRenderTarget, prelude::Camera, view::ComputedVisibility};
+use bevy_render::{prelude::Camera, view::ComputedVisibility};
 use bevy_transform::components::GlobalTransform;
 
 use bevy_window::{PrimaryWindow, Window};
@@ -139,169 +139,184 @@ pub struct NodeQuery {
 #[allow(clippy::too_many_arguments)]
 pub fn ui_focus_system(
     mut state: Local<State>,
-    camera: Query<(&Camera, Option<&UiCameraConfig>)>,
-    windows: Query<&Window>,
+    camera_query: Query<(Entity, &Camera, Option<&UiCameraConfig>)>,
+    windows: Query<(Entity, &Window)>,
     mouse_button_input: Res<Input<MouseButton>>,
     touches_input: Res<Touches>,
     ui_stacks: Res<UiStacks>,
     mut node_query: Query<NodeQuery>,
-    primary_window: Query<Entity, With<PrimaryWindow>>,
+    primary_window_query: Query<Entity, With<PrimaryWindow>>,
 ) {
-    // let primary_window = primary_window.iter().next();
+    let primary_window = primary_window_query.get_single().ok();
+    let cursor_state = {
+        let mut cursor_state = None;
+        for (window_entity, window) in windows.iter() {
+            if let Some(position) = window.cursor_position() {
+                cursor_state = Some((window_entity, position));
+                break;
+            }
+        }
+        cursor_state
+    }
+    .or_else(|| {
+        touches_input
+            .first_pressed_position()
+            .and_then(|position| primary_window.map(|primary_window| (primary_window, position)))
+    });
 
-    // // reset entities that were both clicked and released in the last frame
-    // for entity in state.entities_to_reset.drain(..) {
-    //     if let Ok(mut interaction) = node_query.get_component_mut::<Interaction>(entity) {
-    //         *interaction = Interaction::None;
-    //     }
-    // }
+    let (window_cameras, cursor_position) =
+        if let Some((pointed_window, cursor_position)) = cursor_state {
+            (
+                camera_query
+                    .iter()
+                    .filter(|(_, _, config)| {
+                        matches!(config, Some(&UiCameraConfig { show_ui: true, .. }) | None)
+                    })
+                    .filter_map(|(camera_entity, camera, _)| match camera.target {
+                        bevy_render::camera::RenderTarget::Window(window_ref) => {
+                            if window_ref
+                                .normalize(primary_window)
+                                .map(|normalized_window_ref| {
+                                    normalized_window_ref.entity() == pointed_window
+                                })
+                                .unwrap_or(false)
+                            {
+                                Some(camera_entity)
+                            } else {
+                                None
+                            }
+                        }
+                        bevy_render::camera::RenderTarget::Image(_) => None,
+                    })
+                    .collect(),
+                Some(cursor_position),
+            )
+        } else {
+            (vec![], None)
+        };
 
-    // let mouse_released =
-    //     mouse_button_input.just_released(MouseButton::Left) || touches_input.any_just_released();
-    // if mouse_released {
-    //     for node in node_query.iter_mut() {
-    //         if let Some(mut interaction) = node.interaction {
-    //             if *interaction == Interaction::Clicked {
-    //                 *interaction = Interaction::None;
-    //             }
-    //         }
-    //     }
-    // }
+    let mouse_released =
+        mouse_button_input.just_released(MouseButton::Left) || touches_input.any_just_released();
+    if mouse_released {
+        for node in node_query.iter_mut() {
+            if let Some(mut interaction) = node.interaction {
+                if *interaction == Interaction::Clicked {
+                    *interaction = Interaction::None;
+                }
+            }
+        }
+    }
 
-    // let mouse_clicked =
-    //     mouse_button_input.just_pressed(MouseButton::Left) || touches_input.any_just_pressed();
+    let mouse_clicked =
+        mouse_button_input.just_pressed(MouseButton::Left) || touches_input.any_just_pressed();
 
-    // let is_ui_disabled =
-    //     |camera_ui| matches!(camera_ui, Some(&UiCameraConfig { show_ui: false, .. }));
+    // prepare an iterator that contains all the nodes that have the cursor in their rect,
+    // from the top node to the bottom one. this will also reset the interaction to `None`
+    // for all nodes encountered that are no longer hovered.
+    let mut hovered_nodes = vec![];
+    for ui_stack in ui_stacks.stacks.iter() {
+        if window_cameras.contains(&ui_stack.camera_entity) {
+            // reverse the iterator to traverse the tree from closest nodes to furthest
+            for &entity in ui_stack.uinodes.iter().rev() {
+                if let Ok(node) = node_query.get_mut(entity) {
+                    // Nodes that are not rendered should not be interactable
+                    if let Some(computed_visibility) = node.computed_visibility {
+                        if !computed_visibility.is_visible() {
+                            // Reset their interaction to None to avoid strange stuck state
+                            if let Some(mut interaction) = node.interaction {
+                                // We cannot simply set the interaction to None, as that will trigger change detection repeatedly
+                                interaction.set_if_neq(Interaction::None);
+                            }
 
-    // let cursor_position = camera
-    //     .iter()
-    //     .filter(|(_, camera_ui)| !is_ui_disabled(*camera_ui))
-    //     .filter_map(|(camera, _)| {
-    //         if let Some(NormalizedRenderTarget::Window(window_ref)) =
-    //             camera.target.normalize(primary_window)
-    //         {
-    //             Some(window_ref)
-    //         } else {
-    //             None
-    //         }
-    //     })
-    //     .find_map(|window_ref| {
-    //         windows
-    //             .get(window_ref.entity())
-    //             .ok()
-    //             .and_then(|window| window.cursor_position())
-    //     })
-    //     .or_else(|| touches_input.first_pressed_position());
+                            continue;
+                        }
+                    }
 
-    // // prepare an iterator that contains all the nodes that have the cursor in their rect,
-    // // from the top node to the bottom one. this will also reset the interaction to `None`
-    // // for all nodes encountered that are no longer hovered.
-    // let mut hovered_nodes = ui_stack
-    //     .uinodes
-    //     .iter()
-    //     // reverse the iterator to traverse the tree from closest nodes to furthest
-    //     .rev()
-    //     .filter_map(|entity| {
-    //         if let Ok(node) = node_query.get_mut(*entity) {
-    //             // Nodes that are not rendered should not be interactable
-    //             if let Some(computed_visibility) = node.computed_visibility {
-    //                 if !computed_visibility.is_visible() {
-    //                     // Reset their interaction to None to avoid strange stuck state
-    //                     if let Some(mut interaction) = node.interaction {
-    //                         // We cannot simply set the interaction to None, as that will trigger change detection repeatedly
-    //                         interaction.set_if_neq(Interaction::None);
-    //                     }
+                    let position = node.global_transform.translation();
+                    let ui_position = position.truncate();
+                    let extents = node.node.size() / 2.0;
+                    let mut min = ui_position - extents;
+                    if let Some(clip) = node.calculated_clip {
+                        min = Vec2::max(min, clip.clip.min);
+                    }
 
-    //                     return None;
-    //                 }
-    //             }
+                    // The mouse position relative to the node
+                    // (0., 0.) is the top-left corner, (1., 1.) is the bottom-right corner
+                    let relative_cursor_position = cursor_position.map(|cursor_position| {
+                        Vec2::new(
+                            (cursor_position.x - min.x) / node.node.size().x,
+                            (cursor_position.y - min.y) / node.node.size().y,
+                        )
+                    });
 
-    //             let position = node.global_transform.translation();
-    //             let ui_position = position.truncate();
-    //             let extents = node.node.size() / 2.0;
-    //             let mut min = ui_position - extents;
-    //             if let Some(clip) = node.calculated_clip {
-    //                 min = Vec2::max(min, clip.clip.min);
-    //             }
+                    // If the current cursor position is within the bounds of the node, consider it for
+                    // clicking
+                    let relative_cursor_position_component = RelativeCursorPosition {
+                        normalized: relative_cursor_position,
+                    };
 
-    //             // The mouse position relative to the node
-    //             // (0., 0.) is the top-left corner, (1., 1.) is the bottom-right corner
-    //             let relative_cursor_position = cursor_position.map(|cursor_position| {
-    //                 Vec2::new(
-    //                     (cursor_position.x - min.x) / node.node.size().x,
-    //                     (cursor_position.y - min.y) / node.node.size().y,
-    //                 )
-    //             });
+                    let contains_cursor = relative_cursor_position_component.mouse_over();
 
-    //             // If the current cursor position is within the bounds of the node, consider it for
-    //             // clicking
-    //             let relative_cursor_position_component = RelativeCursorPosition {
-    //                 normalized: relative_cursor_position,
-    //             };
+                    // Save the relative cursor position to the correct component
+                    if let Some(mut node_relative_cursor_position_component) =
+                        node.relative_cursor_position
+                    {
+                        *node_relative_cursor_position_component =
+                            relative_cursor_position_component;
+                    }
 
-    //             let contains_cursor = relative_cursor_position_component.mouse_over();
+                    if contains_cursor {
+                        hovered_nodes.push(entity);
+                    } else {
+                        if let Some(mut interaction) = node.interaction {
+                            if *interaction == Interaction::Hovered || (cursor_position.is_none()) {
+                                interaction.set_if_neq(Interaction::None);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 
-    //             // Save the relative cursor position to the correct component
-    //             if let Some(mut node_relative_cursor_position_component) =
-    //                 node.relative_cursor_position
-    //             {
-    //                 *node_relative_cursor_position_component = relative_cursor_position_component;
-    //             }
+    let mut hovered_nodes = hovered_nodes.into_iter();
 
-    //             if contains_cursor {
-    //                 Some(*entity)
-    //             } else {
-    //                 if let Some(mut interaction) = node.interaction {
-    //                     if *interaction == Interaction::Hovered || (cursor_position.is_none()) {
-    //                         interaction.set_if_neq(Interaction::None);
-    //                     }
-    //                 }
-    //                 None
-    //             }
-    //         } else {
-    //             None
-    //         }
-    //     })
-    //     .collect::<Vec<Entity>>()
-    //     .into_iter();
+    // set Clicked or Hovered on top nodes. as soon as a node with a `Block` focus policy is detected,
+    // the iteration will stop on it because it "captures" the interaction.
+    let mut iter = node_query.iter_many_mut(hovered_nodes.by_ref());
+    while let Some(node) = iter.fetch_next() {
+        if let Some(mut interaction) = node.interaction {
+            if mouse_clicked {
+                // only consider nodes with Interaction "clickable"
+                if *interaction != Interaction::Clicked {
+                    *interaction = Interaction::Clicked;
+                    // if the mouse was simultaneously released, reset this Interaction in the next
+                    // frame
+                    if mouse_released {
+                        state.entities_to_reset.push(node.entity);
+                    }
+                }
+            } else if *interaction == Interaction::None {
+                *interaction = Interaction::Hovered;
+            }
+        }
 
-    // // set Clicked or Hovered on top nodes. as soon as a node with a `Block` focus policy is detected,
-    // // the iteration will stop on it because it "captures" the interaction.
-    // let mut iter = node_query.iter_many_mut(hovered_nodes.by_ref());
-    // while let Some(node) = iter.fetch_next() {
-    //     if let Some(mut interaction) = node.interaction {
-    //         if mouse_clicked {
-    //             // only consider nodes with Interaction "clickable"
-    //             if *interaction != Interaction::Clicked {
-    //                 *interaction = Interaction::Clicked;
-    //                 // if the mouse was simultaneously released, reset this Interaction in the next
-    //                 // frame
-    //                 if mouse_released {
-    //                     state.entities_to_reset.push(node.entity);
-    //                 }
-    //             }
-    //         } else if *interaction == Interaction::None {
-    //             *interaction = Interaction::Hovered;
-    //         }
-    //     }
-
-    //     match node.focus_policy.unwrap_or(&FocusPolicy::Block) {
-    //         FocusPolicy::Block => {
-    //             break;
-    //         }
-    //         FocusPolicy::Pass => { /* allow the next node to be hovered/clicked */ }
-    //     }
-    // }
-    // // reset `Interaction` for the remaining lower nodes to `None`. those are the nodes that remain in
-    // // `moused_over_nodes` after the previous loop is exited.
-    // let mut iter = node_query.iter_many_mut(hovered_nodes);
-    // while let Some(node) = iter.fetch_next() {
-    //     if let Some(mut interaction) = node.interaction {
-    //         // don't reset clicked nodes because they're handled separately
-    //         if *interaction != Interaction::Clicked {
-    //             interaction.set_if_neq(Interaction::None);
-    //         }
-    //     }
-    // }
+        match node.focus_policy.unwrap_or(&FocusPolicy::Block) {
+            FocusPolicy::Block => {
+                break;
+            }
+            FocusPolicy::Pass => { /* allow the next node to be hovered/clicked */ }
+        }
+    }
+    // reset `Interaction` for the remaining lower nodes to `None`. those are the nodes that remain in
+    // `moused_over_nodes` after the previous loop is exited.
+    let mut iter = node_query.iter_many_mut(hovered_nodes);
+    while let Some(node) = iter.fetch_next() {
+        if let Some(mut interaction) = node.interaction {
+            // don't reset clicked nodes because they're handled separately
+            if *interaction != Interaction::Clicked {
+                interaction.set_if_neq(Interaction::None);
+            }
+        }
+    }
 }
