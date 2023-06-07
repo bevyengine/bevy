b13648c7403b1fe6903c261efc29aa486a33c82f
Raffaele Ragni <raffaele.ragni@gmail.com>
2023-06-07 23:09:33 +0200

    import

--- crates/bevy_core_pipeline/src/tonemapping/mod.rs
+++ crates/bevy_core_pipeline/src/tonemapping/mod.rs
@@ -2,7 +2,7 @@ use crate::fullscreen_vertex_shader::fullscreen_shader_vertex_state;
 use bevy_app::prelude::*;
 use bevy_asset::{load_internal_asset, Assets, Handle, HandleUntyped};
 use bevy_ecs::prelude::*;
-use bevy_reflect::{FromReflect, Reflect, TypeUuid};
+use bevy_reflect::{FromReflect, Reflect, ReflectFromReflect, TypeUuid};
 use bevy_render::camera::Camera;
 use bevy_render::extract_component::{ExtractComponent, ExtractComponentPlugin};
 use bevy_render::extract_resource::{ExtractResource, ExtractResourcePlugin};
