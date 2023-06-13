1ba3698660cd8d1cc3d43670c26fcbc3e2956a30
JoJoJet <21144246+JoJoJet@users.noreply.github.com>
2023-06-13 13:48:47 -0400

    fix the doctest

--- crates/bevy_ecs/src/system/adapter_system.rs
+++ crates/bevy_ecs/src/system/adapter_system.rs
@@ -35,7 +35,7 @@ use crate::world::unsafe_world_cell::UnsafeWorldCell;
 ///     }
 /// }
 /// # let mut world = World::new();
-/// # let mut system = NotSystem::new(IntoSystem::into_system(|| false));
+/// # let mut system = NotSystem::new(NotMarker, IntoSystem::into_system(|| false));
 /// # system.initialize(&mut world);
 /// # assert!(system.run((), &mut world));
 /// ```
