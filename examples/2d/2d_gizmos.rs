001b3eb97c3506fe299453e50f98987243824da1
ira <JustTheCoolDude@gmail.com>
2023-06-13 08:49:47 +0200

    Instanced line rendering for gizmos based on `bevy_polyline` (#8427)
    # Objective
    
    Adopt code from
    [bevy_polyline](https://github.com/ForesightMiningSoftwareCorporation/bevy_polyline)
    for gizmo line-rendering.
    This adds configurable width and perspective rendering for the lines.
    
    Many thanks to @mtsr for the initial work on bevy_polyline. Thanks to
    @aevyrie for maintaining it, @nicopap for adding the depth_bias feature
    and the other
    [contributors](https://github.com/ForesightMiningSoftwareCorporation/bevy_polyline/graphs/contributors)
    for squashing bugs and keeping bevy_polyline up-to-date.
    
    #### Before
    
    ![Before](https://user-images.githubusercontent.com/29694403/232831591-a8e6ed0c-3a09-4413-80fa-74cb8e0d33dd.png)
    #### After - with line perspective
    
    ![After](https://user-images.githubusercontent.com/29694403/232831692-ba7cbeb7-e63a-4f8e-9b1b-1b80c668f149.png)
    
    Line perspective is not on by default because with perspective there is
    no default line width that works for every scene.
    
    <details><summary>After - without line perspective</summary>
    <p>
    
    ![After - no
    perspective](https://user-images.githubusercontent.com/29694403/232836344-0dbfb4c8-09b7-4cf5-95f9-a4c26f38dca3.png)
    
    </p>
    </details>
    
    Somewhat unexpectedly, the performance is improved with this PR.
    At 200,000 lines in many_gizmos I get ~110 FPS on main and ~200 FPS with
    this PR.
    I'm guessing this is a CPU side difference as I would expect the
    rendering technique to be more expensive on the GPU to some extent, but
    I am not entirely sure.
    
    ---------
    
    Co-authored-by: Jonas Matser <github@jonasmatser.nl>
    Co-authored-by: Aevyrie <aevyrie@gmail.com>
    Co-authored-by: Nicola Papale <nico@nicopap.ch>
    Co-authored-by: Nicola Papale <nicopap@users.noreply.github.com>

--- examples/2d/2d_gizmos.rs
+++ examples/2d/2d_gizmos.rs
@@ -8,12 +8,21 @@ fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, setup)
-        .add_systems(Update, system)
+        .add_systems(Update, (system, update_config))
         .run();
 }
 
-fn setup(mut commands: Commands) {
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn(Camera2dBundle::default());
+    // text
+    commands.spawn(TextBundle::from_section(
+        "Hold 'Left' or 'Right' to change the line width",
+        TextStyle {
+            font: asset_server.load("fonts/FiraMono-Medium.ttf"),
+            font_size: 24.,
+            color: Color::WHITE,
+        },
+    ));
 }
 
 fn system(mut gizmos: Gizmos, time: Res<Time>) {
@@ -44,4 +53,13 @@ fn system(mut gizmos: Gizmos, time: Res<Time>) {
     // Arcs default amount of segments is linerarly interpolated between
     // 1 and 32, using the arc length as scalar.
     gizmos.arc_2d(Vec2::ZERO, sin / 10., PI / 2., 350., Color::ORANGE_RED);
+}
+
+fn update_config(mut config: ResMut<GizmoConfig>, keyboard: Res<Input<KeyCode>>, time: Res<Time>) {
+    if keyboard.pressed(KeyCode::Right) {
+        config.line_width += 5. * time.delta_seconds();
+    }
+    if keyboard.pressed(KeyCode::Left) {
+        config.line_width -= 5. * time.delta_seconds();
+    }
 }
