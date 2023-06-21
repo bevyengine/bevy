c39e02cefb52ab102b27d4c7039ecea7e1e561da
ickshonpe <david.curthoys@googlemail.com>
2023-06-22 00:50:29 +0100

    Improved UI render batching (#8793)
    # Objective
    
    `prepare_uinodes` creates a new `UiBatch` whenever the texture changes,
    when most often it's just queuing untextured quads. Instead of switching
    textures, we can reduce the number of batches generated significantly by
    adding a condition to the fragment shader so that it only multiplies by
    the `textureSample` value when drawing a textured quad.
    
    # Solution
    
    Add a `mode` field to `UiVertex`.
    In `prepare_uinodes` set `mode` to 0 if the quad is textured or 1 if
    untextured.
    Add a condition to the fragment shader that only multiplies by the
    `color` value from `textureSample` if `mode` is set to 1.
    
    ---
    
    ## Changelog
    * Added a `mode` field to `UiVertex`, and added an extra `u32` vertex
    attribute to the shader and vertex buffer layout.
    * In `prepare_uinodes` mode is set to 0 for the vertices of textured
    quads, and 1 if untextured.
    * Added a condition to the fragment shader in `ui.wgsl` that only
    multiplies by the `color` value from `textureSample` if the mode is
    equal to 0.

--- crates/bevy_ui/src/render/mod.rs
+++ crates/bevy_ui/src/render/mod.rs
@@ -552,6 +552,7 @@ struct UiVertex {
     pub position: [f32; 3],
     pub uv: [f32; 2],
     pub color: [f32; 4],
+    pub mode: u32,
 }
 
 #[derive(Resource)]
@@ -585,6 +586,9 @@ pub struct UiBatch {
     pub z: f32,
 }
 
+const TEXTURED_QUAD: u32 = 0;
+const UNTEXTURED_QUAD: u32 = 1;
+
 pub fn prepare_uinodes(
     mut commands: Commands,
     render_device: Res<RenderDevice>,
@@ -601,20 +605,33 @@ pub fn prepare_uinodes(
 
     let mut start = 0;
     let mut end = 0;
-    let mut current_batch_handle = Default::default();
+    let mut current_batch_image = DEFAULT_IMAGE_HANDLE.typed();
     let mut last_z = 0.0;
+
+    #[inline]
+    fn is_textured(image: &Handle<Image>) -> bool {
+        image.id() != DEFAULT_IMAGE_HANDLE.id()
+    }
+
     for extracted_uinode in &extracted_uinodes.uinodes {
-        if current_batch_handle != extracted_uinode.image {
-            if start != end {
-                commands.spawn(UiBatch {
-                    range: start..end,
-                    image: current_batch_handle,
-                    z: last_z,
-                });
-                start = end;
+        let mode = if is_textured(&extracted_uinode.image) {
+            if current_batch_image.id() != extracted_uinode.image.id() {
+                if is_textured(&current_batch_image) && start != end {
+                    commands.spawn(UiBatch {
+                        range: start..end,
+                        image: current_batch_image,
+                        z: last_z,
+                    });
+                    start = end;
+                }
+                current_batch_image = extracted_uinode.image.clone_weak();
             }
-            current_batch_handle = extracted_uinode.image.clone_weak();
-        }
+            TEXTURED_QUAD
+        } else {
+            // Untextured `UiBatch`es are never spawned within the loop.
+            // If all the `extracted_uinodes` are untextured a single untextured UiBatch will be spawned after the loop terminates.
+            UNTEXTURED_QUAD
+        };
 
         let mut uinode_rect = extracted_uinode.rect;
 
@@ -672,7 +689,7 @@ pub fn prepare_uinodes(
                 continue;
             }
         }
-        let uvs = if current_batch_handle.id() == DEFAULT_IMAGE_HANDLE.id() {
+        let uvs = if mode == UNTEXTURED_QUAD {
             [Vec2::ZERO, Vec2::X, Vec2::ONE, Vec2::Y]
         } else {
             let atlas_extent = extracted_uinode.atlas_size.unwrap_or(uinode_rect.max);
@@ -717,6 +734,7 @@ pub fn prepare_uinodes(
                 position: positions_clipped[i].into(),
                 uv: uvs[i].into(),
                 color,
+                mode,
             });
         }
 
@@ -728,7 +746,7 @@ pub fn prepare_uinodes(
     if start != end {
         commands.spawn(UiBatch {
             range: start..end,
-            image: current_batch_handle,
+            image: current_batch_image,
             z: last_z,
         });
     }
