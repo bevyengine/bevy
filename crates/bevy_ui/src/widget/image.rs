83745d228256d93da3461342828e16a813c83ca4
ickshonpe <david.curthoys@googlemail.com>
2023-06-22 21:15:47 +0100

    `bevy_ui::widget::image` Checking for changed scale factor is required, unless the `image_size.size` is set to the scaled image size, which doesn't seem desirable.

--- crates/bevy_ui/src/widget/image.rs
+++ crates/bevy_ui/src/widget/image.rs
@@ -1,6 +1,5 @@
-
 use crate::{
-    measurement::AvailableSpace, ContentSize, Measure, Node, UiImage, UiTextureAtlasImage, UiScale,
+    measurement::AvailableSpace, ContentSize, Measure, Node, UiImage, UiScale, UiTextureAtlasImage,
 };
 use bevy_asset::{Assets, Handle};
 
@@ -10,7 +9,7 @@ use bevy_ecs::{
     prelude::Component,
     query::With,
     reflect::ReflectComponent,
-    system::{Query, Res},
+    system::{Local, Query, Res},
 };
 use bevy_math::Vec2;
 use bevy_reflect::{std_traits::ReflectDefault, FromReflect, Reflect, ReflectFromReflect};
@@ -76,6 +75,7 @@ impl Measure for ImageMeasure {
 
 /// Updates content size of the node based on the image provided
 pub fn update_image_content_size_system(
+    mut previous_combined_scale_factor: Local<f64>,
     windows: Query<&Window, With<PrimaryWindow>>,
     ui_scale: Res<UiScale>,
     textures: Res<Assets<Image>>,
@@ -100,8 +100,8 @@ pub fn update_image_content_size_system(
                 texture.texture_descriptor.size.width as f32,
                 texture.texture_descriptor.size.height as f32,
             );
-            // Update only if size has changed to avoid needless layout calculations
-            if size != image_size.size {
+            // Update only if size or scale factor has changed to avoid needless layout calculations
+            if size != image_size.size || combined_scale_factor != *previous_combined_scale_factor {
                 image_size.size = size;
                 content_size.set(ImageMeasure {
                     // multiply the image size by the scale factor to get the physical size
@@ -110,10 +110,13 @@ pub fn update_image_content_size_system(
             }
         }
     }
+
+    *previous_combined_scale_factor = combined_scale_factor;
 }
 
 /// Updates content size of the node based on the texture atlas sprite
 pub fn update_atlas_content_size_system(
+    mut previous_combined_scale_factor: Local<f64>,
     windows: Query<&Window, With<PrimaryWindow>>,
     ui_scale: Res<UiScale>,
     atlases: Res<Assets<TextureAtlas>>,
@@ -141,18 +144,20 @@ pub fn update_atlas_content_size_system(
         .map(|window| window.resolution.scale_factor())
         .unwrap_or(1.)
         * ui_scale.scale;
+
     for (mut content_size, atlas, atlas_image, mut image_size) in &mut atlas_query {
         if let Some(atlas) = atlases.get(atlas) {
             let size = atlas.textures[atlas_image.index].size();
-
-            // Update only if size has changed to avoid needless layout calculations
-            if size != image_size.size {
+            // Update only if size or scale factor has changed to avoid needless layout calculations
+            if size != image_size.size || combined_scale_factor != *previous_combined_scale_factor {
                 image_size.size = size;
-                content_size.set(ImageMeasure { 
+                content_size.set(ImageMeasure {
                     // multiply the image size by the scale factor to get the physical size
-                    size: size * combined_scale_factor as f32 
+                    size: size * combined_scale_factor as f32,
                 });
             }
         }
     }
+
+    *previous_combined_scale_factor = combined_scale_factor;
 }
