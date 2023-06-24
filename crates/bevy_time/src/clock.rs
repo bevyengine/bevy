1298507170e2bc837d316da13472e722c37b6250
Nuutti Kotivuori <naked@iki.fi>
2023-06-24 13:51:16 +0000

    Improve clock to not have public data members

--- crates/bevy_time/src/clock.rs
+++ crates/bevy_time/src/clock.rs
@@ -1,25 +1,35 @@
 use bevy_reflect::{FromReflect, Reflect};
 use bevy_utils::Duration;
 
-#[derive(Default, Debug, Copy, Clone, Reflect, FromReflect)]
+#[derive(Debug, Copy, Clone, Reflect, FromReflect)]
 pub struct Clock {
-    pub wrap_seconds: u64,
-    pub delta: Duration,
-    pub delta_seconds: f32,
-    pub delta_seconds_f64: f64,
-    pub elapsed: Duration,
-    pub elapsed_seconds: f32,
-    pub elapsed_seconds_f64: f64,
-    pub elapsed_wrapped: Duration,
-    pub elapsed_seconds_wrapped: f32,
-    pub elapsed_seconds_wrapped_f64: f64,
+    wrap_seconds: u64,
+    delta: Duration,
+    delta_seconds: f32,
+    delta_seconds_f64: f64,
+    elapsed: Duration,
+    elapsed_seconds: f32,
+    elapsed_seconds_f64: f64,
+    elapsed_wrapped: Duration,
+    elapsed_seconds_wrapped: f32,
+    elapsed_seconds_wrapped_f64: f64,
 }
 
 impl Clock {
+    const DEFAULT_WRAP_SECONDS: u64 = 3600; // 1 hour
+
     pub fn new(wrap_seconds: u64) -> Clock {
         Clock {
             wrap_seconds,
-            ..Default::default()
+            delta: Duration::ZERO,
+            delta_seconds: 0.0,
+            delta_seconds_f64: 0.0,
+            elapsed: Duration::ZERO,
+            elapsed_seconds: 0.0,
+            elapsed_seconds_f64: 0.0,
+            elapsed_wrapped: Duration::ZERO,
+            elapsed_seconds_wrapped: 0.0,
+            elapsed_seconds_wrapped_f64: 0.0,
         }
     }
 
@@ -37,5 +47,48 @@ impl Clock {
 
     pub fn advance_to(&mut self, elapsed: Duration) {
         self.advance_by(elapsed - self.elapsed)
+    }
+
+    pub fn delta(&self) -> Duration {
+        self.delta
+    }
+
+    pub fn delta_seconds(&self) -> f32 {
+        self.delta_seconds
+    }
+
+    pub fn delta_seconds_f64(&self) -> f64 {
+        self.delta_seconds_f64
+    }
+
+    pub fn elapsed_wrapped(&self) -> Duration {
+        self.elapsed_wrapped
+    }
+
+    pub fn elapsed_seconds_wrapped(&self) -> f32 {
+        self.elapsed_seconds_wrapped
+    }
+
+    pub fn elapsed_seconds_wrapped_f64(&self) -> f64 {
+        self.elapsed_seconds_wrapped_f64
+    }
+
+    pub fn wrap_period(&self) -> Duration {
+        Duration::from_secs(self.wrap_seconds)
+    }
+
+    pub fn set_wrap_period(&mut self, wrap_period: Duration) {
+        assert!(!wrap_period.is_zero(), "division by zero");
+        assert_eq!(wrap_period.subsec_nanos(), 0, "wrap period must be integral seconds");
+        self.wrap_seconds = wrap_period.as_secs();
+        self.elapsed_wrapped = Duration::new(self.elapsed.as_secs() % self.wrap_seconds, self.elapsed.subsec_nanos());
+        self.elapsed_seconds_wrapped = self.elapsed_wrapped.as_secs_f32();
+        self.elapsed_seconds_wrapped_f64 = self.elapsed_wrapped.as_secs_f64();        
+    }
+}
+
+impl Default for Clock {
+    fn default() -> Self {
+        Self::new(Self::DEFAULT_WRAP_SECONDS)
     }
 }
