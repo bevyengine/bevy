d33f5c759c33e657dcd3dc66f2fb828a21221162
James Liu <contact@jamessliu.com>
2023-07-08 21:22:15 -0700

    Add optional single-threaded feature to bevy_ecs/bevy_tasks (#6690)
    # Objective
    Fixes #6689.
    
    ## Solution
    Add `single-threaded` as an optional non-default feature to `bevy_ecs`
    and `bevy_tasks` that:
     
     - disable the `ParallelExecutor` as a default runner
     - disables the multi-threaded `TaskPool`
    - internally replace `QueryParIter::for_each` calls with
    `Query::for_each`.
    
    Removed the `Mutex` and `Arc` usage in the single-threaded task pool.
    
    
    ![image](https://user-images.githubusercontent.com/3137680/202833253-dd2d520f-75e6-4c7b-be2d-5ce1523cbd38.png)
    
    ## Future Work/TODO
    Create type aliases for `Mutex`, `Arc` that change to single-threaaded
    equivalents where possible.
    
    ---
    
    ## Changelog
    Added: Optional default feature `multi-theaded` to that enables
    multithreaded parallelism in the engine. Disabling it disables all
    multithreading in exchange for higher single threaded performance. Does
    nothing on WASM targets.
    
    ---------
    
    Co-authored-by: Carter Anderson <mcanders1@gmail.com>

--- crates/bevy_ecs/src/query/par_iter.rs
+++ crates/bevy_ecs/src/query/par_iter.rs
@@ -1,5 +1,4 @@
 use crate::{component::Tick, world::unsafe_world_cell::UnsafeWorldCell};
-use bevy_tasks::ComputeTaskPool;
 use std::ops::Range;
 
 use super::{QueryItem, QueryState, ROQueryItem, ReadOnlyWorldQuery, WorldQuery};
@@ -34,6 +33,8 @@ pub struct BatchingStrategy {
     /// increase the scheduling overhead for the iteration.
     ///
     /// Defaults to 1.
+    ///
+    /// [`ComputeTaskPool`]: bevy_tasks::ComputeTaskPool
     pub batches_per_thread: usize,
 }
 
@@ -148,23 +149,36 @@ impl<'w, 's, Q: WorldQuery, F: ReadOnlyWorldQuery> QueryParIter<'w, 's, Q, F> {
     /// [`ComputeTaskPool`]: bevy_tasks::ComputeTaskPool
     #[inline]
     unsafe fn for_each_unchecked<FN: Fn(QueryItem<'w, Q>) + Send + Sync + Clone>(&self, func: FN) {
-        let thread_count = ComputeTaskPool::get().thread_num();
-        if thread_count <= 1 {
+        #[cfg(any(target = "wasm32", not(feature = "multi-threaded")))]
+        {
             self.state
                 .for_each_unchecked_manual(self.world, func, self.last_run, self.this_run);
-        } else {
-            // Need a batch size of at least 1.
-            let batch_size = self.get_batch_size(thread_count).max(1);
-            self.state.par_for_each_unchecked_manual(
-                self.world,
-                batch_size,
-                func,
-                self.last_run,
-                self.this_run,
-            );
+        }
+        #[cfg(all(not(target = "wasm32"), feature = "multi-threaded"))]
+        {
+            let thread_count = bevy_tasks::ComputeTaskPool::get().thread_num();
+            if thread_count <= 1 {
+                self.state.for_each_unchecked_manual(
+                    self.world,
+                    func,
+                    self.last_run,
+                    self.this_run,
+                );
+            } else {
+                // Need a batch size of at least 1.
+                let batch_size = self.get_batch_size(thread_count).max(1);
+                self.state.par_for_each_unchecked_manual(
+                    self.world,
+                    batch_size,
+                    func,
+                    self.last_run,
+                    self.this_run,
+                );
+            }
         }
     }
 
+    #[cfg(all(not(target = "wasm32"), feature = "multi-threaded"))]
     fn get_batch_size(&self, thread_count: usize) -> usize {
         if self.batching_strategy.batch_size_limits.is_empty() {
             return self.batching_strategy.batch_size_limits.start;
