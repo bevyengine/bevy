e5ea269f38510887f5ce61fd6f89bd69a57412b2
fgrust <freezy.gem0918@outlook.com>
2023-07-10 13:35:52 -0400

    remove `repr(transparent)` from non-sized struct

--- crates/bevy_ptr/src/lib.rs
+++ crates/bevy_ptr/src/lib.rs
@@ -10,12 +10,10 @@ use core::{
 
 /// Used as a type argument to [`Ptr`], [`PtrMut`] and [`OwningPtr`] to specify that the pointer is aligned.
 #[derive(Copy, Clone)]
-#[repr(transparent)]
 pub struct Aligned;
 
 /// Used as a type argument to [`Ptr`], [`PtrMut`] and [`OwningPtr`] to specify that the pointer is not aligned.
 #[derive(Copy, Clone)]
-#[repr(transparent)]
 pub struct Unaligned;
 
 /// Trait that is only implemented for [`Aligned`] and [`Unaligned`] to work around the lack of ability
