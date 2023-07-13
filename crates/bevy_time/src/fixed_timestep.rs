aa8969cee07f79cf5414d17c7a99cb341170c573
SpecificProtagonist <specificprotagonist@posteo.org>
2023-07-13 23:06:30 +0200

    fix typo in doc comment

--- crates/bevy_time/src/fixed_timestep.rs
+++ crates/bevy_time/src/fixed_timestep.rs
@@ -65,7 +65,7 @@ impl FixedTime {
 
     /// Expends one `period` of accumulated time.
     ///
-    /// [`Err(FixedUpdateError`)] will be returned if there is
+    /// [`Err(FixedUpdateError)`] will be returned if there is
     /// not enough accumulated time to span an entire period.
     pub fn expend(&mut self) -> Result<(), FixedUpdateError> {
         if let Some(new_value) = self.accumulated.checked_sub(self.period) {
