b1739799a0513487c8ca7936f06116e55c95309b
66OJ66 <hi0obxud@anonaddy.me>
2023-07-14 17:02:08 +0000

    If RenderDevice is absent, don't support any compressed formats

--- crates/bevy_render/src/texture/image_texture_loader.rs
+++ crates/bevy_render/src/texture/image_texture_loader.rs
@@ -82,7 +82,7 @@ impl FromWorld for ImageTextureLoader {
         let supported_compressed_formats = match world.get_resource::<RenderDevice>() {
             Some(render_device) => CompressedImageFormats::from_features(render_device.features()),
 
-            None => CompressedImageFormats::all(),
+            None => CompressedImageFormats::NONE,
         };
         Self {
             supported_compressed_formats,
