d9c89d9319ebe0f5a223b9452e0945c6a1a9c8c3
Trashtalk <trashtalk217@gmail.com>
2023-07-20 21:49:27 +0200

    Fixed a CI error in bevy_app

--- crates/bevy_app/src/system_registry.rs
+++ crates/bevy_app/src/system_registry.rs
@@ -1,21 +1,9 @@
 use crate::App;
 use bevy_ecs::{
     prelude::*,
-    system::{SystemId, SystemRegistryError},
 };
 
 impl App {
-    /// Register a system.
-    ///
-    /// Calls [`SystemRegistry::register_system`](bevy_ecs::system::SystemRegistry::register_system).
-    pub fn register_system<M, S: IntoSystem<(), (), M> + 'static>(
-        &mut self,
-        system: S,
-    ) -> &mut Self {
-        self.world.register_system(system);
-        self
-    }
-
     /// Runs the supplied system on the [`World`] a single time.
     ///
     /// Calls [`SystemRegistry::run_system`](bevy_ecs::system::SystemRegistry::run_system).
@@ -23,13 +11,5 @@ impl App {
     pub fn run_system<M, S: IntoSystem<(), (), M> + 'static>(&mut self, system: S) -> &mut Self {
         self.world.run_system(system);
         self
-    }
-
-    /// Run the systems corresponding to the label stored in the provided [`Callback`]
-    ///
-    /// Calls [`SystemRegistry::run_callback`](bevy_ecs::system::SystemRegistry::run_callback).
-    #[inline]
-    pub fn run_system_by_id(&mut self, system_id: SystemId) -> Result<(), SystemRegistryError> {
-        self.world.run_system_by_id(system_id)
     }
 }
