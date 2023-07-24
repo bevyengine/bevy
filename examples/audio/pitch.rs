deeadd8d533afbbe1563f9937898eaf98d320179
Чай Хуа Пу <basilefff@gmail.com>
2023-07-24 23:24:09 +0400

    fixed formatting

--- examples/audio/pitch.rs
+++ examples/audio/pitch.rs
@@ -22,23 +22,32 @@ fn setup(mut commands: Commands) {
     commands.insert_resource(PitchFrequency(220.0));
 }
 
-fn play_pitch(mut pitch_assets: ResMut<Assets<Pitch>>, frequency: Res<PitchFrequency>, mut events: EventReader<PlayPitch>, mut commands: Commands) {
+fn play_pitch(
+    mut pitch_assets: ResMut<Assets<Pitch>>,
+    frequency: Res<PitchFrequency>,
+    mut events: EventReader<PlayPitch>,
+    mut commands: Commands,
+) {
     for _ in events.iter() {
         info!("playing pitch with frequency: {}", frequency.0);
         commands.spawn(PitchBundle {
             source: pitch_assets.add(Pitch::new(frequency.0, Duration::new(1, 0))),
-            settings: PlaybackSettings::DESPAWN
+            settings: PlaybackSettings::DESPAWN,
         });
         info!("number of pitch assets: {}", pitch_assets.len());
     }
 }
 
-fn keyboard_input_system(keyboard_input: Res<Input<KeyCode>>, mut frequency: ResMut<PitchFrequency>, mut events: EventWriter<PlayPitch>) {
+fn keyboard_input_system(
+    keyboard_input: Res<Input<KeyCode>>,
+    mut frequency: ResMut<PitchFrequency>,
+    mut events: EventWriter<PlayPitch>,
+) {
     if keyboard_input.just_pressed(KeyCode::Up) {
-        frequency.0 *= 2.0f32.powf(1.0/12.0);
+        frequency.0 *= 2.0f32.powf(1.0 / 12.0);
     }
     if keyboard_input.just_pressed(KeyCode::Down) {
-        frequency.0 /= 2.0f32.powf(1.0/12.0);
+        frequency.0 /= 2.0f32.powf(1.0 / 12.0);
     }
     if keyboard_input.just_pressed(KeyCode::Space) {
         events.send(PlayPitch);
