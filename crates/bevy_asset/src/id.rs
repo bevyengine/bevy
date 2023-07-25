b8241365f792cffd18c1bc3c6ba548ef8a026dc0
Carter Anderson <mcanders1@gmail.com>
2023-07-24 21:04:17 -0700

    Remove unnecessary serialize / deserialize impls from AssetId

--- crates/bevy_asset/src/id.rs
+++ crates/bevy_asset/src/id.rs
@@ -1,6 +1,5 @@
 use crate::{Asset, AssetIndex, Handle, UntypedHandle};
 use bevy_reflect::{Reflect, Uuid};
-use serde::{Deserialize, Serialize};
 use std::{
     any::TypeId,
     fmt::{Debug, Display},
@@ -12,7 +11,7 @@ use std::{
 /// lifetime of the Asset. This means it _can_ point to an [`Asset`] that no longer exists.
 ///
 /// For an "untyped" / "generic-less" id, see [`UntypedAssetId`].
-#[derive(Reflect, Serialize, Deserialize)]
+#[derive(Reflect)]
 pub enum AssetId<A: Asset> {
     /// A small / efficient runtime identifier that can be used to efficiently look up an asset stored in [`Assets`]. This is
     /// the "default" identifier used for assets. The alternative(s) (ex: [`AssetId::Uuid`]) will only be used if assets are
