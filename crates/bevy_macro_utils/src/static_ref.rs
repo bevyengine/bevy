a501af7fc0251b8c5df823fe5e42fee6b2f041fa
Edgar Geier <geieredgar@gmail.com>
2023-08-05 19:26:59 +0200

    Simplify `static_ref_impl` again

--- crates/bevy_macro_utils/src/static_ref.rs
+++ crates/bevy_macro_utils/src/static_ref.rs
@@ -4,18 +4,13 @@ use syn::spanned::Spanned;
 
 pub fn static_ref_impl(input: &syn::DeriveInput) -> TokenStream {
     match &input.data {
-        syn::Data::Struct(data) => match &data.fields {
-            syn::Fields::Named(fields) if fields.named.is_empty() => {
+        syn::Data::Struct(data) => {
+            if data.fields.is_empty() {
                 quote! { ::std::option::Option::Some(&Self{}) }
-            }
-            syn::Fields::Unnamed(fields) if fields.unnamed.is_empty() => {
-                quote! { ::std::option::Option::Some(&Self()) }
-            }
-            syn::Fields::Unit => quote! { ::std::option::Option::Some(&Self) },
-            _ => {
+            } else {
                 quote! { ::std::option::Option::None }
             }
-        },
+        }
         syn::Data::Enum(data) => {
             let variants: Vec<_> = data
                 .variants
@@ -23,21 +18,10 @@ pub fn static_ref_impl(input: &syn::DeriveInput) -> TokenStream {
                 .filter_map(|variant| {
                     let span = variant.span();
                     let variant_ident = variant.ident.clone();
-                    match &variant.fields {
-                        syn::Fields::Named(fields) if fields.named.is_empty() => {
-                            Some(quote_spanned! { span => Self::#variant_ident{} => ::std::option::Option::Some(&Self::#variant_ident{}), })
-                        }
-                        syn::Fields::Unnamed(fields) if fields.unnamed.is_empty() => {
-                            // TODO: Simplify using `Self` after rustc 1.72.0 is released.
-                            let ident = input.ident.clone();
-                            let (_, ty_generics, _) = input.generics.split_for_impl();
-                            let turbofish = ty_generics.as_turbofish();
-                            Some(quote_spanned! { span => Self::#variant_ident() => ::std::option::Option::Some(&#ident #turbofish::#variant_ident()), })
-                        }
-                        syn::Fields::Unit => {
-                            Some(quote_spanned! { span => Self::#variant_ident => ::std::option::Option::Some(&Self::#variant_ident), })
-                        }
-                        _ => None
+                    if variant.fields.is_empty() {
+                        Some(quote_spanned! { span => Self::#variant_ident{} => ::std::option::Option::Some(&Self::#variant_ident{}), })
+                    } else {
+                        None
                     }
                 })
                 .collect();
