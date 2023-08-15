51f7fbdefca775f160cdd38878b838d3b99dff47
IDEDARY <IDEDARY@gmail.com>
2023-08-15 09:42:45 +0200

    Fixed comments as suggested by  Selene-Amanita

--- crates/bevy_render/src/color/mod.rs
+++ crates/bevy_render/src/color/mod.rs
@@ -368,7 +368,7 @@ impl Color {
         )
     }
 
-    /// Get red
+    /// Converts a Color to variant Color::Rgba and return red in sRGB colorspace
     pub fn r(&self) -> f32 {
         match self.as_rgba() {
             Color::Rgba { red, .. } => red,
@@ -376,7 +376,7 @@ impl Color {
         }
     }
 
-    /// Get green
+    /// Converts a Color to variant Color::Rgba and return green in sRGB colorspace
     pub fn g(&self) -> f32 {
         match self.as_rgba() {
             Color::Rgba { green, .. } => green,
@@ -384,7 +384,7 @@ impl Color {
         }
     }
 
-    /// Get blue
+    /// Converts a Color to variant Color::Rgba and return blue in sRGB colorspace
     pub fn b(&self) -> f32 {
         match self.as_rgba() {
             Color::Rgba { blue, .. } => blue,
@@ -392,7 +392,7 @@ impl Color {
         }
     }
 
-    /// Set red
+    /// Converts a Color to variant Color::Rgba and set red
     pub fn set_r(&mut self, r: f32) -> &mut Self {
         *self = self.as_rgba();
         match self {
@@ -402,14 +402,14 @@ impl Color {
         self
     }
 
-    /// Returns this color with red set to a new value
+    /// Converts a Color to variant Color::Rgba and return this color with red set to a new value
     #[must_use]
     pub fn with_r(mut self, r: f32) -> Self {
         self.set_r(r);
         self
     }
 
-    /// Set green
+    /// Converts a Color to variant Color::Rgba and set green
     pub fn set_g(&mut self, g: f32) -> &mut Self {
         *self = self.as_rgba();
         match self {
@@ -419,14 +419,14 @@ impl Color {
         self
     }
 
-    /// Returns this color with green set to a new value
+    /// Converts a Color to variant Color::Rgba and return this color with green set to a new value
     #[must_use]
     pub fn with_g(mut self, g: f32) -> Self {
         self.set_g(g);
         self
     }
 
-    /// Set blue
+    /// Converts a Color to variant Color::Rgba and set blue
     pub fn set_b(&mut self, b: f32) -> &mut Self {
         *self = self.as_rgba();
         match self {
@@ -436,14 +436,14 @@ impl Color {
         self
     }
 
-    /// Returns this color with blue set to a new value
+    /// Converts a Color to variant Color::Rgba and return this color with blue set to a new value
     #[must_use]
     pub fn with_b(mut self, b: f32) -> Self {
         self.set_b(b);
         self
     }
 
-    /// Get hue
+    /// Converts a Color to variant Color::Hsla and return hue
     pub fn h(&self) -> f32 {
         match self.as_hsla() {
             Color::Hsla { hue, .. } => hue,
@@ -451,7 +451,7 @@ impl Color {
         }
     }
 
-    /// Get saturation
+    /// Converts a Color to variant Color::Hsla and return saturation
     pub fn s(&self) -> f32 {
         match self.as_hsla() {
             Color::Hsla { saturation, .. } => saturation,
@@ -459,7 +459,7 @@ impl Color {
         }
     }
 
-    /// Get lightness
+    /// Converts a Color to variant Color::Hsla and return lightness
     pub fn l(&self) -> f32 {
         match self.as_hsla() {
             Color::Hsla { lightness, .. } => lightness,
@@ -467,7 +467,7 @@ impl Color {
         }
     }
 
-    /// Set hue
+    /// Converts a Color to variant Color::Hsla and set hue
     pub fn set_h(&mut self, h: f32) -> &mut Self {
         *self = self.as_hsla();
         match self {
@@ -477,14 +477,14 @@ impl Color {
         self
     }
 
-    /// Returns this color with hue set to a new value
+    /// Converts a Color to variant Color::Hsla and return this color with hue set to a new value
     #[must_use]
     pub fn with_h(mut self, h: f32) -> Self {
         self.set_h(h);
         self
     }
 
-    /// Set saturation
+    /// Converts a Color to variant Color::Hsla and set saturation
     pub fn set_s(&mut self, s: f32) -> &mut Self {
         *self = self.as_hsla();
         match self {
@@ -494,14 +494,14 @@ impl Color {
         self
     }
 
-    /// Returns this color with saturation set to a new value
+    /// Converts a Color to variant Color::Hsla and return this color with saturation set to a new value
     #[must_use]
     pub fn with_s(mut self, s: f32) -> Self {
         self.set_s(s);
         self
     }
 
-    /// Set lightness
+    /// Converts a Color to variant Color::Hsla and set lightness
     pub fn set_l(&mut self, l: f32) -> &mut Self {
         *self = self.as_hsla();
         match self {
@@ -511,7 +511,7 @@ impl Color {
         self
     }
 
-    /// Returns this color with lightness set to a new value
+    /// Converts a Color to variant Color::Hsla and return this color with lightness set to a new value
     #[must_use]
     pub fn with_l(mut self, l: f32) -> Self {
         self.set_l(l);
