80269303d6def4100fd3889f40e8883b1fd9ef3c
JMS55 <47158642+JMS55@users.noreply.github.com>
2023-08-17 13:13:33 -0700

    Improve macro

--- crates/bevy_render/src/render_resource/bind_group_macros.rs
+++ crates/bevy_render/src/render_resource/bind_group_macros.rs
@@ -1,35 +1,40 @@
+#[doc(hidden)]
+#[macro_export]
+macro_rules! bind_group_descriptor_acc {
+    (@d texture($e:expr)) => ($crate::render_resource::BindingResource::TextureView($e));
+    (@d sampler($e:expr)) => ($crate::render_resource::BindingResource::Sampler($e));
+    (@d buffer($e:expr)) => ($e);
+    (@d $_:ident($e:expr)) => (compile_error!("only 'texture', 'sampler', and 'buffer' bindings are supported"));
+
+    (@acc $index:ident, [$($acc:tt)*] $fn:ident ($e:expr) $(, $($rest:tt)* )?) => {
+        $crate::bind_group_descriptor_acc!(@acc $index,
+            [
+                $($acc)*
+                $crate::render_resource::BindGroupEntry {
+                    binding: {$index += 1; $index-1},
+                    resource: $crate::bind_group_descriptor_acc!(@d $fn($e)),
+                },
+            ] $($($rest)*)?
+        )
+    };
+
+    // Nothing left
+    (@acc $index:ident,
+        [$($output:tt)*] $(,)?
+    ) => (
+        [ $($output)* ]
+    );
+}
+
 /// TODO: Docs
 #[macro_export]
 macro_rules! bind_group_descriptor {
-    (@entry, texture, $binding_index: expr, $resource_value: expr) => {
-        $crate::render_resource::BindGroupEntry {
-            binding: $binding_index,
-            resource: $crate::render_resource::BindingResource::TextureView($resource_value),
-        }
-    };
-    (@entry, sampler, $binding_index: expr, $resource_value: expr) => {
-        $crate::render_resource::BindGroupEntry {
-            binding: $binding_index,
-            resource: $crate::render_resource::BindingResource::Sampler($resource_value),
-        }
-    };
-    (@entry, buffer, $binding_index: expr, $resource_value: expr) => {
-        $crate::render_resource::BindGroupEntry {
-            binding: $binding_index,
-            resource: $resource_value,
-        }
-    };
-    (@entry, $any_other:ident, $binding_index: expr, $resource_value: expr) => {
-        compile_error!("only 'texture', 'sampler', and 'buffer' are supported")
-    };
-    ($label:expr, $layout:expr, $($entry_kind: ident ($resource_value:expr)),*,) => {{
-        let mut i = 0;
+    ($label:expr, $layout:expr, $($fun:tt)*) => {{
+        let mut index = 0;
         &$crate::render_resource::BindGroupDescriptor {
             label: Some($label),
             layout: $layout,
-            entries: &[ $(
-                bind_group_descriptor!(@entry, $entry_kind, { i += 1; i - 1}, $resource_value),
-            )* ],
+            entries: &$crate::bind_group_descriptor_acc!(@acc index, [] $($fun)*)
         }
     }};
 }
