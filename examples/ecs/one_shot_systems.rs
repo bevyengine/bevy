1963e6399e0111956d3dbdf29b73dab536d6d76a
Trashtalk <trashtalk217@gmail.com>
2023-08-23 22:25:43 +0200

    Remove outdated comments

--- examples/ecs/one_shot_systems.rs
+++ examples/ecs/one_shot_systems.rs
@@ -14,11 +14,7 @@ use bevy::{
 fn main() {
     App::new()
         .add_systems(Startup, (count_entities, setup))
-        // One shot systems are interchangeable with ordinarily scheduled systems.
         .add_systems(PostUpdate, count_entities)
-        // One-shot systems can be used to build complex abstractions
-        // to match the needs of your design.
-        // Here, we model a very simple component-linked callback architecture.
         .add_systems(Update, evaluate_callbacks)
         .run();
 }
@@ -38,9 +34,6 @@ struct Triggered;
 
 fn setup(mut system_registry: ResMut<SystemRegistry>, mut commands: Commands) {
     commands.spawn((
-        // The Callback component is defined in bevy_ecs,
-        // but wrapping this (or making your own customized variant) is easy.
-        // Just stored a boxed SystemLabel!
         Callback(system_registry.register(button_pressed)),
         Triggered,
     ));
@@ -62,10 +55,6 @@ fn slider_toggled() {
 /// This could be done in an exclusive system rather than using `Commands` if preferred.
 fn evaluate_callbacks(query: Query<&Callback, With<Triggered>>, mut commands: Commands) {
     for callback in query.iter() {
-        // Because we don't have access to the type information of the callbacks
-        // we have to use the layer of indirection provided by system labels.
-        // Note that if we had registered multiple systems with the same label,
-        // they would all be evaluated here.
         commands.run_system_by_id(callback.0);
     }
 }
