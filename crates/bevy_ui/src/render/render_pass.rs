c6a9cd6b5d469a1d7d2789b5282339aa7539d334
ickshonpe <david.curthoys@googlemail.com>
2023-08-28 23:29:32 +0100

    removed unused imports

--- crates/bevy_ui/src/render/render_pass.rs
+++ crates/bevy_ui/src/render/render_pass.rs
@@ -12,7 +12,6 @@ use bevy_render::{
     renderer::*,
     view::*,
 };
-use bevy_utils::FloatOrd;
 
 pub struct UiPassNode {
     ui_view_query: QueryState<
@@ -87,7 +86,7 @@ impl Node for UiPassNode {
 }
 
 pub struct TransparentUi {
-    pub sort_key: FloatOrd,
+    pub sort_key: u32,
     pub entity: Entity,
     pub pipeline: CachedRenderPipelineId,
     pub draw_function: DrawFunctionId,
@@ -95,7 +94,7 @@ pub struct TransparentUi {
 }
 
 impl PhaseItem for TransparentUi {
-    type SortKey = FloatOrd;
+    type SortKey = u32;
 
     #[inline]
     fn entity(&self) -> Entity {
