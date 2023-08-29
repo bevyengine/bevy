e88aaca2658d51b12471d223ee3031502aeaf176
Giacomo Stevanato <giaco.stevanato@gmail.com>
2023-08-29 17:42:47 +0200

    Add documentation to from_reflect_or_world

--- crates/bevy_ecs/src/reflect/component.rs
+++ crates/bevy_ecs/src/reflect/component.rs
@@ -294,11 +294,21 @@ impl<C: Component + Reflect + FromReflect> FromType<C> for ReflectComponent {
     }
 }
 
+/// Creates a `T` from a `&dyn Reflect`.
+///
+/// The first approach uses `T`'s implementation of `FromReflect`.
+/// If this fails, it falls back to default-initializing a new instance of `T` using its
+/// `ReflectFromWorld` data from the `world`'s `AppTypeRegistry` and `apply`ing the
+/// `&dyn Reflect` on it.
+///
+/// Panics if both approaches fail.
 fn from_reflect_or_world<T: FromReflect>(reflected: &dyn Reflect, world: &mut World) -> T {
     if let Some(value) = T::from_reflect(reflected) {
         return value;
     }
 
+    // Clone the `ReflectFromWorld` because it's cheap and "frees"
+    // the borrow of `world` so that it can be passed to `from_world`.
     let reflect_from_world = world
         .resource::<AppTypeRegistry>()
         .read()
