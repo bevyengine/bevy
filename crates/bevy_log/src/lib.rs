ba78be96a9d3963078d1f3ceb1f0e6aed60a5364
IceSentry <IceSentry@users.noreply.github.com>
2023-09-05 02:09:22 -0400

    Update crates/bevy_log/src/lib.rs
    Co-authored-by: François <mockersf@gmail.com>

--- crates/bevy_log/src/lib.rs
+++ crates/bevy_log/src/lib.rs
@@ -257,12 +257,14 @@ impl Plugin for LogPlugin {
 
             let file_appender_layer = if let Some(settings) = &self.file_appender_settings {
                 if settings.use_panic_hook {
+                    let old_handler = panic::take_hook();
                     std::panic::set_hook(Box::new(|panic_info| {
                         if let Some(s) = panic_info.payload().downcast_ref::<&str>() {
                             error!("panic occurred: {s:?}");
                         } else {
                             error!("panic occurred");
                         }
+                        old_handler(panic_info);
                     }));
                 }
 
