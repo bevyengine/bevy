f793ba9eac6f3d71c1ddf060157ded28abac58e3
ickshonpe <david.curthoys@googlemail.com>
2023-09-09 11:59:19 +0100

    Use the `scale_value` function from `Text2d` to apply the inverse scaling for the UI text widget.

--- crates/bevy_ui/src/widget/text.rs
+++ crates/bevy_ui/src/widget/text.rs
@@ -13,7 +13,7 @@ use bevy_render::texture::Image;
 use bevy_sprite::TextureAtlas;
 use bevy_text::{
     BreakLineOn, Font, FontAtlasSets, FontAtlasWarning, Text, TextError, TextLayoutInfo,
-    TextMeasureInfo, TextPipeline, TextSettings, YAxisOrientation,
+    TextMeasureInfo, TextPipeline, TextSettings, YAxisOrientation, scale_value,
 };
 use bevy_window::{PrimaryWindow, Window};
 use taffy::style::AvailableSpace;
@@ -153,6 +153,7 @@ fn queue_text(
     textures: &mut Assets<Image>,
     text_settings: &TextSettings,
     scale_factor: f64,
+    inverse_scale_factor: f64,
     text: &Text,
     node: Ref<Node>,
     mut text_flags: Mut<TextFlags>,
@@ -190,7 +191,8 @@ fn queue_text(
                 panic!("Fatal error when processing text: {e}.");
             }
             Ok(mut info) => {
-                info.logical_size /= scale_factor as f32;
+                info.logical_size.x = scale_value(info.logical_size.x, inverse_scale_factor);
+                info.logical_size.y = scale_value(info.logical_size.y, inverse_scale_factor);
                 *text_layout_info = info;
                 text_flags.needs_recompute = false;
             }
@@ -227,7 +229,7 @@ pub fn text_system(
         .unwrap_or(1.);
 
     let scale_factor = ui_scale.0 * window_scale_factor;
-
+    let inverse_scale_factor = scale_factor.recip();
     if *last_scale_factor == scale_factor {
         // Scale factor unchanged, only recompute text for modified text nodes
         for (node, text, text_layout_info, text_flags) in text_query.iter_mut() {
@@ -241,6 +243,7 @@ pub fn text_system(
                     &mut textures,
                     &text_settings,
                     scale_factor,
+                    inverse_scale_factor,
                     text,
                     node,
                     text_flags,
@@ -262,6 +265,7 @@ pub fn text_system(
                 &mut textures,
                 &text_settings,
                 scale_factor,
+                inverse_scale_factor,
                 text,
                 node,
                 text_flags,
