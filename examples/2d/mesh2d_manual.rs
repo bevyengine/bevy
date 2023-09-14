89dcd4ba44ea06879fc7f49511d34002ff5bd682
Robert Swain <robert.swain@gmail.com>
2023-09-15 01:16:03 +0200

    Update examples/2d/mesh2d_manual.rs
    Co-authored-by: robtfm <50659922+robtfm@users.noreply.github.com>

--- examples/2d/mesh2d_manual.rs
+++ examples/2d/mesh2d_manual.rs
@@ -148,6 +148,9 @@ impl SpecializedRenderPipeline for ColoredMesh2dPipeline {
             false => TextureFormat::bevy_default(),
         };
 
+        // meshes typically live in bind group 2. because we are using bindgroup 1
+        // we need to add MESH_BINDGROUP_1 shader def so that the bindings are correctly
+        // linked in the shader
         let shader_defs = vec!["MESH_BINDGROUP_1".into()];
 
         RenderPipelineDescriptor {
