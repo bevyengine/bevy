519265b52071e69bf884f813f65c8af1b539ec6a
Bruce Mitchener <bruce.mitchener@gmail.com>
2023-09-19 00:06:49 +0700

    Improve doc formatting.

--- crates/bevy_utils/src/label.rs
+++ crates/bevy_utils/src/label.rs
@@ -73,8 +73,8 @@ macro_rules! define_boxed_label {
     ($label_trait_name:ident) => {
         /// A strongly-typed label.
         pub trait $label_trait_name: 'static + Send + Sync + ::std::fmt::Debug {
-            /// Return's the [TypeId] of this label, or the the ID of the
-            /// wrappped label type for `Box<dyn
+            /// Return's the [`TypeId`] of this label, or the the ID of the
+            /// wrapped label type for `Box<dyn
             #[doc = stringify!($label_trait_name)]
             /// >`
             ///
