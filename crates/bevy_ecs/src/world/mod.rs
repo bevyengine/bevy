d9fb6ec56cd32a2eb470d3c56642e4d1309e32e6
Rob Parrett <robparrett@gmail.com>
2023-09-17 17:00:59 -0700

    Remove some old references to CoreSet

--- crates/bevy_ecs/src/world/mod.rs
+++ crates/bevy_ecs/src/world/mod.rs
@@ -885,8 +885,8 @@ impl World {
     /// of detection to be recorded.
     ///
     /// When using `bevy_ecs` as part of the full Bevy engine, this method is added as a system to the
-    /// main app, to run during `CoreSet::Last`, so you don't need to call it manually. When using
-    /// `bevy_ecs` as a separate standalone crate however, you need to call this manually.
+    /// main app, to run during `Last`, so you don't need to call it manually. When using `bevy_ecs`
+    /// as a separate standalone crate however, you need to call this manually.
     ///
     /// ```
     /// # use bevy_ecs::prelude::*;
