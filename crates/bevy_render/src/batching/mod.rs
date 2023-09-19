0bddc59506ae6ea1de1e9e23e72def36102a393b
Nicola Papale <nico@nicopap.ch>
2023-09-19 15:03:54 +0200

    prev over old

--- crates/bevy_render/src/batching/mod.rs
+++ crates/bevy_render/src/batching/mod.rs
@@ -85,10 +85,10 @@ pub fn batch_and_prepare_render_phase<I: CachedRenderPipelinePhaseItem, F: GetBa
             let batch_data = process_item(i);
             (i.batch_range_mut(), batch_data)
         });
-        items.reduce(|(start_range, old_batch_meta), (range, batch_meta)| {
-            if batch_meta.is_some() && old_batch_meta == batch_meta {
+        items.reduce(|(start_range, prev_batch_meta), (range, batch_meta)| {
+            if batch_meta.is_some() && prev_batch_meta == batch_meta {
                 start_range.end = range.end;
-                (start_range, old_batch_meta)
+                (start_range, prev_batch_meta)
             } else {
                 (range, batch_meta)
             }
