dda752d6869fb67b27f7c6e407ceb692b7b47e4a
Zachary Harrold <zac@harrold.com.au>
2023-09-25 19:52:34 +1000

    Removed `query_get_component` Benchmark

--- benches/benches/bevy_ecs/world/world_get.rs
+++ benches/benches/bevy_ecs/world/world_get.rs
@@ -300,61 +300,6 @@ pub fn query_get_component_simple(criterion: &mut Criterion) {
     group.finish();
 }
 
-pub fn query_get_component(criterion: &mut Criterion) {
-    let mut group = criterion.benchmark_group("query_get_component");
-    group.warm_up_time(std::time::Duration::from_millis(500));
-    group.measurement_time(std::time::Duration::from_secs(4));
-
-    for entity_count in RANGE.map(|i| i * 10_000) {
-        group.bench_function(format!("{}_entities_table", entity_count), |bencher| {
-            let mut world = World::default();
-            let mut entities: Vec<_> = world
-                .spawn_batch((0..entity_count).map(|_| Table::default()))
-                .collect();
-            entities.shuffle(&mut deterministic_rand());
-            let mut query = SystemState::<Query<&Table>>::new(&mut world);
-            let query = query.get(&world);
-
-            bencher.iter(|| {
-                let mut count = 0;
-                for comp in entities
-                    .iter()
-                    .flat_map(|&e| query.get(e))
-                {
-                    black_box(comp);
-                    count += 1;
-                    black_box(count);
-                }
-                assert_eq!(black_box(count), entity_count);
-            });
-        });
-        group.bench_function(format!("{}_entities_sparse", entity_count), |bencher| {
-            let mut world = World::default();
-            let mut entities: Vec<_> = world
-                .spawn_batch((0..entity_count).map(|_| Sparse::default()))
-                .collect();
-            entities.shuffle(&mut deterministic_rand());
-            let mut query = SystemState::<Query<&Sparse>>::new(&mut world);
-            let query = query.get(&world);
-
-            bencher.iter(|| {
-                let mut count = 0;
-                for comp in entities
-                    .iter()
-                    .flat_map(|&e| query.get(e))
-                {
-                    black_box(comp);
-                    count += 1;
-                    black_box(count);
-                }
-                assert_eq!(black_box(count), entity_count);
-            });
-        });
-    }
-
-    group.finish();
-}
-
 pub fn query_get(criterion: &mut Criterion) {
     let mut group = criterion.benchmark_group("query_get");
     group.warm_up_time(std::time::Duration::from_millis(500));
