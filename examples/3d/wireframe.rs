c9a012f4c16f9147de8cad8802c3459fd35aff57
IceSentry <c.giguere42@gmail.com>
2023-10-01 16:35:44 -0400

    improve example

--- examples/3d/wireframe.rs
+++ examples/3d/wireframe.rs
@@ -3,40 +3,38 @@
 use bevy::{
     pbr::wireframe::{Wireframe, WireframeConfig, WireframePlugin},
     prelude::*,
-    render::{render_resource::WgpuFeatures, settings::WgpuSettings, RenderPlugin},
+    render::{
+        render_resource::WgpuFeatures,
+        settings::{RenderCreation, WgpuSettings},
+        RenderPlugin,
+    },
 };
 
 fn main() {
     App::new()
         .add_plugins((
             DefaultPlugins.set(RenderPlugin {
-                render_creation: WgpuSettings {
+                render_creation: RenderCreation::Automatic(WgpuSettings {
+                    // WARN this is a native only feature. It will not work with webgl or webgpu
                     features: WgpuFeatures::POLYGON_MODE_LINE,
                     ..default()
-                }
-                .into(),
+                }),
+                ..default()
             }),
+            // You need to add this plugin to enable wireframe rendering
             WireframePlugin,
         ))
         .add_systems(Startup, setup)
+        .add_systems(Update, toggle_global_wireframe)
         .run();
 }
 
 /// set up a simple 3D scene
 fn setup(
     mut commands: Commands,
-    mut wireframe_config: ResMut<WireframeConfig>,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
-    // To draw the wireframe on all entities, set this to 'true'
-    wireframe_config.global = false;
-    // plane
-    commands.spawn(PbrBundle {
-        mesh: meshes.add(shape::Plane::from_size(5.0).into()),
-        material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
-        ..default()
-    });
     // cube
     commands.spawn((
         PbrBundle {
@@ -48,6 +46,14 @@ fn setup(
         // This enables wireframe drawing on this entity
         Wireframe,
     ));
+
+    // plane
+    commands.spawn(PbrBundle {
+        mesh: meshes.add(shape::Plane::from_size(5.0).into()),
+        material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
+        ..default()
+    });
+
     // light
     commands.spawn(PointLightBundle {
         transform: Transform::from_xyz(4.0, 8.0, 4.0),
@@ -58,4 +64,14 @@ fn setup(
         transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
         ..default()
     });
+}
+
+fn toggle_global_wireframe(
+    mut wireframe_config: ResMut<WireframeConfig>,
+    keyboard_input: Res<Input<KeyCode>>,
+) {
+    if keyboard_input.just_pressed(KeyCode::T) {
+        // To draw the wireframe on all entities, set this to 'true'
+        wireframe_config.global = !wireframe_config.global;
+    }
 }
