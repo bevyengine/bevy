d38af0166b15079e267ff592f272f5a3bc9e9290
François <mockersf@gmail.com>
2023-10-06 21:56:50 +0200

    ignore complexity lint in example

--- examples/2d/mesh2d_manual.rs
+++ examples/2d/mesh2d_manual.rs
@@ -2,6 +2,7 @@
 //! pipeline for 2d meshes.
 //! It doesn't use the [`Material2d`] abstraction, but changes the vertex buffer to include vertex color.
 //! Check out the "mesh2d" example for simpler / higher level 2d meshes.
+#![allow(clippy::type_complexity)]
 
 use bevy::{
     core_pipeline::core_2d::Transparent2d,
