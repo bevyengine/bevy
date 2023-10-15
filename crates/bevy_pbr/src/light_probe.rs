cb739cfcb53db2a9446795f9f75f30f658a6f611
Patrick Walton <pcwalton@mimiga.net>
2023-10-14 22:09:45 -0700

    Better documentation for light probes

--- crates/bevy_pbr/src/light_probe.rs
+++ crates/bevy_pbr/src/light_probe.rs
@@ -15,9 +15,19 @@ use smallvec::SmallVec;
 
 use crate::{environment_map::EnvironmentMapLightId, EnvironmentMapLight};
 
+/// Adds support for light probes, cuboid bounding regions that apply global
+/// illumination to objects within them.
 pub struct LightProbePlugin;
 
 /// A cuboid region that provides global illumination to all meshes inside it.
+///
+/// A mesh is considered inside the light probe if the mesh's origin is
+/// contained within the cuboid centered at the light probe's transform with
+/// width, height, and depth equal to double the value of `half_extents`.
+///
+/// Note that a light probe will have no effect unless the entity contains some
+/// kind of illumination. At present, the only supported type of illumination is
+/// the [EnvironmentMapLight].
 #[derive(Component, Debug, Clone, Copy, Reflect)]
 #[reflect(Component, Default)]
 pub struct LightProbe {
@@ -28,12 +38,22 @@ pub struct LightProbe {
 /// Which light probes this mesh must take into account.
 #[derive(Component, Debug, Clone)]
 pub struct AppliedLightProbes {
+    /// The ID of the single light probe that this mesh reflects.
     pub reflection_probe: EnvironmentMapLightId,
 }
 
+// Information about the light probe that applies to each mesh.
+//
+// This is a transient structure only used by the [apply_light_probes] system.
 struct LightProbeApplicationInfo {
+    // Maps from the light probe's space into world space; i.e. the opposite of the light probe's
+    // [GlobalTransform].
     inverse_transform: Mat4,
+
+    // The half-extents of the light probe.
     half_extents: Vec3A,
+
+    // The ID of the light probe.
     light_probes: AppliedLightProbes,
 }
 
@@ -61,15 +81,19 @@ impl Plugin for LightProbePlugin {
     }
 }
 
+/// Determines which light probe applies to each mesh and attaches
+/// [AppliedLightProbes] components to them as appropriate.
 pub fn apply_light_probes(
     mut commands: Commands,
     mesh_query: Query<(Entity, &GlobalTransform), With<Handle<Mesh>>>,
     light_probe_query: Query<(&LightProbe, &EnvironmentMapLight, &GlobalTransform)>,
 ) {
+    // If there are no meshes, we can just bail.
     if mesh_query.is_empty() {
         return;
     }
 
+    // Gather up information about all light probes in the scene.
     let mut light_probes: SmallVec<[LightProbeApplicationInfo; 4]> = SmallVec::new();
     for (light_probe, environment_map_light, light_probe_transform) in light_probe_query.iter() {
         light_probes.push(LightProbeApplicationInfo {
@@ -81,13 +105,21 @@ pub fn apply_light_probes(
         })
     }
 
+    // For each mesh and each light probe…
     'outer: for (mesh_entity, mesh_transform) in mesh_query.iter() {
         for light_probe_info in &light_probes {
+            // Determine the mesh center in the light probe's object space.
+            // (This makes it easier to test whether the mesh is inside the
+            // light probe's AABB.)
             let probe_space_mesh_center: Vec3A = (light_probe_info.inverse_transform
                 * mesh_transform.translation_vec3a().extend(1.0))
             .xyz()
             .into();
 
+            // If the mesh is inside the AABB of the light probe, add the
+            // [AppliedLightProbes] component. Note that at present we naïvely
+            // consider the first bounding light probe to be the one that
+            // matches.
             if (-light_probe_info.half_extents)
                 .cmple(probe_space_mesh_center)
                 .all()
@@ -101,5 +133,8 @@ pub fn apply_light_probes(
                 continue 'outer;
             }
         }
+
+        // If we got here, no light probe applies, so remove the component.
+        commands.entity(mesh_entity).remove::<AppliedLightProbes>();
     }
 }
