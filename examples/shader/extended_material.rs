aadeef5bb6ec14f288d232b342b4597aaaae487f
robtfm <50659922+robtfm@users.noreply.github.com>
2023-10-17 16:34:47 +0100

    reword a comment

--- examples/shader/extended_material.rs
+++ examples/shader/extended_material.rs
@@ -37,12 +37,12 @@ fn setup(
             base: StandardMaterial {
                 base_color: Color::RED,
                 // can be used in forward or deferred mode.
-                opaque_render_method: OpaqueRendererMethod::Forward,
+                opaque_render_method: OpaqueRendererMethod::Auto,
                 // in deferred mode, only the PbrInput can be modified (uvs, color and other material properties),
                 // in forward mode, the output can also be modified after lighting is applied.
                 // see the fragment shader `extended_material.wgsl` for more info.
-                // Note: to run in deferred mode, you must also add a `DeferredPrepass` component to the camera.
-                // opaque_render_method: OpaqueRendererMethod::Deferred,
+                // Note: to run in deferred mode, you must also add a `DeferredPrepass` component to the camera and either
+                // change the above to `OpaqueRendererMethod::Deferred` or add the `DefaultOpaqueRendererMethod` resource. 
                 ..Default::default()
             },
             extension: MyExtension { quantize_steps: 3 },
