b6923e424f758917d874672dcc8094778027f4d7
Marco Buono <thecoreh@gmail.com>
2023-10-19 23:09:57 -0300

    Rename `mesh_View_layout.rs` to `mesh_view_bindings.rs`

--- crates/bevy_pbr/src/render/mod.rs
+++ crates/bevy_pbr/src/render/mod.rs
@@ -2,7 +2,7 @@ mod fog;
 mod light;
 pub(crate) mod mesh;
 mod mesh_bindings;
-mod mesh_view_layout;
+mod mesh_view_bindings;
 mod morph;
 mod skin;
 
@@ -10,5 +10,5 @@ pub use fog::*;
 pub use light::*;
 pub use mesh::*;
 pub use mesh_bindings::MeshLayouts;
-pub use mesh_view_layout::*;
+pub use mesh_view_bindings::*;
 pub use skin::{extract_skins, prepare_skins, SkinIndex, SkinUniform, MAX_JOINTS};
