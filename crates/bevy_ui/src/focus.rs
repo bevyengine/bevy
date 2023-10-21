1b5a278b0b4d1cc9b05c210ea291113fc0fa444c
Hennadii Chernyshchyk <genaloner@gmail.com>
2023-10-21 19:49:49 +0300

    Register click only if the button was pressed before

--- crates/bevy_ui/src/focus.rs
+++ crates/bevy_ui/src/focus.rs
@@ -163,12 +163,14 @@ pub fn ui_focus_system(
         }
     }
 
+    let mut was_pressed = false;
     let mouse_released =
         mouse_button_input.just_released(MouseButton::Left) || touches_input.any_just_released();
     if mouse_released {
         for node in &mut node_query {
             if let Some(mut interaction) = node.interaction {
                 if *interaction == Interaction::Pressed {
+                    was_pressed = true;
                     *interaction = Interaction::None;
                 }
             }
@@ -257,7 +259,7 @@ pub fn ui_focus_system(
 
                 if contains_cursor {
                     // Emit a click event only if mouse was released under the button
-                    if mouse_released {
+                    if mouse_released && was_pressed {
                         click_events.send(Clicked(*entity));
                     }
                     Some(*entity)
