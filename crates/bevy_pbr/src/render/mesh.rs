1562746b217a567b5c65a267bafd26b919f2377d
roman <carpowroma@gmail.com>
2023-10-22 19:16:30 +0300

    Sure, clippy

--- crates/bevy_pbr/src/render/mesh.rs
+++ crates/bevy_pbr/src/render/mesh.rs
@@ -375,7 +375,7 @@ impl FromWorld for MeshPipeline {
             let sampler = match image.sampler_descriptor {
                 ImageSampler::Default => (**default_sampler).clone(),
                 ImageSampler::Descriptor(ref descriptor) => {
-                    render_device.create_sampler(&descriptor)
+                    render_device.create_sampler(descriptor)
                 }
             };
 
