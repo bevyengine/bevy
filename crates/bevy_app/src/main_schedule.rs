a830530be4398fb0832992e5b4baefc316d1f1d0
Edgar Geier <geieredgar@gmail.com>
2023-10-25 23:39:23 +0200

    Replace all labels with interned labels (#7762)
    # Objective
    
    First of all, this PR took heavy inspiration from #7760 and #5715. It
    intends to also fix #5569, but with a slightly different approach.
    
    
    This also fixes #9335 by reexporting `DynEq`.
    
    ## Solution
    
    The advantage of this API is that we can intern a value without
    allocating for zero-sized-types and for enum variants that have no
    fields. This PR does this automatically in the `SystemSet` and
    `ScheduleLabel` derive macros for unit structs and fieldless enum
    variants. So this should cover many internal and external use cases of
    `SystemSet` and `ScheduleLabel`. In these optimal use cases, no memory
    will be allocated.
    
    - The interning returns a `Interned<dyn SystemSet>`, which is just a
    wrapper around a `&'static dyn SystemSet`.
    - `Hash` and `Eq` are implemented in terms of the pointer value of the
    reference, similar to my first approach of anonymous system sets in
    #7676.
    - Therefore, `Interned<T>` does not implement `Borrow<T>`, only `Deref`.
    - The debug output of `Interned<T>` is the same as the interned value.
    
    Edit: 
    - `AppLabel` is now also interned and the old
    `derive_label`/`define_label` macros were replaced with the new
    interning implementation.
    - Anonymous set ids are reused for different `Schedule`s, reducing the
    amount of leaked memory.
    
    ### Pros
    - `InternedSystemSet` and `InternedScheduleLabel` behave very similar to
    the current `BoxedSystemSet` and `BoxedScheduleLabel`, but can be copied
    without an allocation.
    - Many use cases don't allocate at all.
    - Very fast lookups and comparisons when using `InternedSystemSet` and
    `InternedScheduleLabel`.
    - The `intern` module might be usable in other areas.
    - `Interned{ScheduleLabel, SystemSet, AppLabel}` does implement
    `{ScheduleLabel, SystemSet, AppLabel}`, increasing ergonomics.
    
    ### Cons
    - Implementors of `SystemSet` and `ScheduleLabel` still need to
    implement `Hash` and `Eq` (and `Clone`) for it to work.
    
    ## Changelog
    
    ### Added
    
    - Added `intern` module to `bevy_utils`.
    - Added reexports of `DynEq` to `bevy_ecs` and `bevy_app`.
    
    ### Changed
    
    - Replaced `BoxedSystemSet` and `BoxedScheduleLabel` with
    `InternedSystemSet` and `InternedScheduleLabel`.
    - Replaced `impl AsRef<dyn ScheduleLabel>` with `impl ScheduleLabel`.
    - Replaced `AppLabelId` with `InternedAppLabel`.
    - Changed `AppLabel` to use `Debug` for error messages.
    - Changed `AppLabel` to use interning.
    - Changed `define_label`/`derive_label` to use interning. 
    - Replaced `define_boxed_label`/`derive_boxed_label` with
    `define_label`/`derive_label`.
    - Changed anonymous set ids to be only unique inside a schedule, not
    globally.
    - Made interned label types implement their label trait. 
    
    ### Removed
    
    - Removed `define_boxed_label` and `derive_boxed_label`. 
    
    ## Migration guide
    
    - Replace `BoxedScheduleLabel` and `Box<dyn ScheduleLabel>` with
    `InternedScheduleLabel` or `Interned<dyn ScheduleLabel>`.
    - Replace `BoxedSystemSet` and `Box<dyn SystemSet>` with
    `InternedSystemSet` or `Interned<dyn SystemSet>`.
    - Replace `AppLabelId` with `InternedAppLabel` or `Interned<dyn
    AppLabel>`.
    - Types manually implementing `ScheduleLabel`, `AppLabel` or `SystemSet`
    need to implement:
      - `dyn_hash` directly instead of implementing `DynHash`
      - `as_dyn_eq`
    - Pass labels to `World::try_schedule_scope`, `World::schedule_scope`,
    `World::try_run_schedule`. `World::run_schedule`, `Schedules::remove`,
    `Schedules::remove_entry`, `Schedules::contains`, `Schedules::get` and
    `Schedules::get_mut` by value instead of by reference.
    
    ---------
    
    Co-authored-by: Joseph <21144246+JoJoJet@users.noreply.github.com>
    Co-authored-by: Carter Anderson <mcanders1@gmail.com>

--- crates/bevy_app/src/main_schedule.rs
+++ crates/bevy_app/src/main_schedule.rs
@@ -1,6 +1,6 @@
 use crate::{App, Plugin};
 use bevy_ecs::{
-    schedule::{ExecutorKind, Schedule, ScheduleLabel},
+    schedule::{ExecutorKind, InternedScheduleLabel, Schedule, ScheduleLabel},
     system::{Local, Resource},
     world::{Mut, World},
 };
@@ -105,21 +105,21 @@ pub struct Last;
 #[derive(Resource, Debug)]
 pub struct MainScheduleOrder {
     /// The labels to run for the [`Main`] schedule (in the order they will be run).
-    pub labels: Vec<Box<dyn ScheduleLabel>>,
+    pub labels: Vec<InternedScheduleLabel>,
 }
 
 impl Default for MainScheduleOrder {
     fn default() -> Self {
         Self {
             labels: vec![
-                Box::new(First),
-                Box::new(PreUpdate),
-                Box::new(StateTransition),
-                Box::new(RunFixedUpdateLoop),
-                Box::new(Update),
-                Box::new(SpawnScene),
-                Box::new(PostUpdate),
-                Box::new(Last),
+                First.intern(),
+                PreUpdate.intern(),
+                StateTransition.intern(),
+                RunFixedUpdateLoop.intern(),
+                Update.intern(),
+                SpawnScene.intern(),
+                PostUpdate.intern(),
+                Last.intern(),
             ],
         }
     }
@@ -133,7 +133,7 @@ impl MainScheduleOrder {
             .iter()
             .position(|current| (**current).eq(&after))
             .unwrap_or_else(|| panic!("Expected {after:?} to exist"));
-        self.labels.insert(index + 1, Box::new(schedule));
+        self.labels.insert(index + 1, schedule.intern());
     }
 }
 
@@ -148,8 +148,8 @@ impl Main {
         }
 
         world.resource_scope(|world, order: Mut<MainScheduleOrder>| {
-            for label in &order.labels {
-                let _ = world.try_run_schedule(&**label);
+            for &label in &order.labels {
+                let _ = world.try_run_schedule(label);
             }
         });
     }
