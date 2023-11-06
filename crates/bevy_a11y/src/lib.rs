b086945b44964a4b342ba4c8ade7d6730ec39ca9
Jakob Hellermann <jakob.hellermann@protonmail.com>
2023-11-06 14:16:01 +0100

    ignore accessibility ambiguities

--- crates/bevy_a11y/src/lib.rs
+++ crates/bevy_a11y/src/lib.rs
@@ -106,6 +106,7 @@ impl Plugin for AccessibilityPlugin {
     fn build(&self, app: &mut bevy_app::App) {
         app.init_resource::<AccessibilityRequested>()
             .init_resource::<ManageAccessibilityUpdates>()
-            .init_resource::<Focus>();
+            .init_resource::<Focus>()
+            .allow_ambiguous_component::<AccessibilityNode>();
     }
 }
