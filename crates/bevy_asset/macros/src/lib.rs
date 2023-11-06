7b129e1878418fc828b39fbb417cd4cecc85221c
robtfm <50659922+robtfm@users.noreply.github.com>
2023-11-06 12:34:05 +0000

    derive asset for enums

--- crates/bevy_asset/macros/src/lib.rs
+++ crates/bevy_asset/macros/src/lib.rs
@@ -1,6 +1,6 @@
 use bevy_macro_utils::BevyManifest;
 use proc_macro::{Span, TokenStream};
-use quote::quote;
+use quote::{format_ident, quote};
 use syn::{parse_macro_input, Data, DeriveInput, Path};
 
 pub(crate) fn bevy_asset_path() -> syn::Path {
@@ -41,33 +41,101 @@ fn derive_dependency_visitor_internal(
     ast: &DeriveInput,
     bevy_asset_path: &Path,
 ) -> Result<proc_macro2::TokenStream, syn::Error> {
-    let mut field_visitors = Vec::new();
-    if let Data::Struct(data_struct) = &ast.data {
-        for field in &data_struct.fields {
-            if field
-                .attrs
-                .iter()
-                .any(|a| a.path().is_ident(DEPENDENCY_ATTRIBUTE))
-            {
-                if let Some(field_ident) = &field.ident {
-                    field_visitors.push(quote! {
-                        #bevy_asset_path::VisitAssetDependencies::visit_dependencies(&self.#field_ident, visit);
-                    });
-                }
-            }
-        }
-    } else {
-        return Err(syn::Error::new(
-            Span::call_site().into(),
-            "Asset derive currently only works on structs",
-        ));
-    }
-
     let struct_name = &ast.ident;
     let (impl_generics, type_generics, where_clause) = &ast.generics.split_for_impl();
 
+    let body = match &ast.data {
+        Data::Struct(data_struct) => {
+            let mut field_visitors = Vec::new();
+
+            for field in &data_struct.fields {
+                if field
+                    .attrs
+                    .iter()
+                    .any(|a| a.path().is_ident(DEPENDENCY_ATTRIBUTE))
+                {
+                    if let Some(field_ident) = &field.ident {
+                        field_visitors.push(quote! {
+                            #bevy_asset_path::VisitAssetDependencies::visit_dependencies(&self.#field_ident, visit);
+                        });
+                    }
+                }
+            }
+
+            quote! {
+                #(#field_visitors)*
+            }
+        }
+        Data::Enum(data_enum) => {
+            let mut any_case_required = false;
+            let cases = data_enum.variants.iter().map(|variant| {
+                let mut any_member_omitted = false;
+                let mut field_names = Vec::new();
+                let mut field_visitors = Vec::new();
+
+                for (i, field) in variant.fields.iter().enumerate() {
+                    if field
+                        .attrs
+                        .iter()
+                        .any(|a| a.path().is_ident(DEPENDENCY_ATTRIBUTE))
+                    {
+                        if let Some(field_ident) = &field.ident {
+                            field_names.push(quote!{ #field_ident });
+                            field_visitors.push(quote! {
+                                #bevy_asset_path::VisitAssetDependencies::visit_dependencies(#field_ident, visit);
+                            });
+                        } else {
+                            let name = format_ident!("member_{i}");
+                            field_names.push(quote!{ #name });
+                            field_visitors.push(quote! {
+                                #bevy_asset_path::VisitAssetDependencies::visit_dependencies(#name, visit);
+                            });
+                        }
+                    } else {
+                        any_member_omitted = true;
+                    }
+                }
+
+                if any_member_omitted {
+                    field_names.push(quote!{ .. });
+                }
+
+                let field_data = match variant.fields {
+                    syn::Fields::Named(_) => quote! { {#(#field_names,)*} },
+                    syn::Fields::Unnamed(_) => quote! { (#(#field_names,)*) },
+                    syn::Fields::Unit => quote! { },
+                };
+
+                any_case_required |= !field_visitors.is_empty();
+
+                let var_ident = &variant.ident;
+                quote! {
+                    Self::#var_ident #field_data => {
+                        #(#field_visitors)*
+                    }
+                }
+            }).collect::<Vec<_>>();
+
+            if !any_case_required {
+                quote! { }
+            } else {
+                quote! {
+                    match self {
+                        #(#cases)*
+                    }
+                }
+            }
+        }
+        Data::Union(_) => {
+            return Err(syn::Error::new(
+                Span::call_site().into(),
+                "Asset derive currently doesn't work on unions",
+            ));
+        }
+    };
+
     // prevent unused variable warning in case there are no dependencies
-    let visit = if field_visitors.is_empty() {
+    let visit = if body.is_empty() {
         quote! { _visit }
     } else {
         quote! { visit }
@@ -76,7 +144,7 @@ fn derive_dependency_visitor_internal(
     Ok(quote! {
         impl #impl_generics #bevy_asset_path::VisitAssetDependencies for #struct_name #type_generics #where_clause {
             fn visit_dependencies(&self, #visit: &mut impl FnMut(#bevy_asset_path::UntypedAssetId)) {
-                #(#field_visitors)*
+                #body
             }
         }
     })
