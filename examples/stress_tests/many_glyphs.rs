cbadc31d19e1d40841969ac503d49b3fff030f57
Rob Parrett <robparrett@gmail.com>
2023-11-09 15:05:32 -0700

    Use a consistent scale factor and resolution in stress tests (#10474)
    # Objective
    
    Related to #10472.
    
    Not having a hardcoded scale factor makes comparing results from these
    stress tests difficult.
    
    Contributors using high dpi screens may be rendering 4x as many pixels
    as others (or more). Stress tests may have different behavior when moved
    from one monitor in a dual setup to another. At very high resolutions,
    different parts of the engine / hardware are being stressed.
    
    1080p is also a far more common resolution for gaming.
    
    ## Solution
    
    Use a consistent 1080p with `scale_factor_override: 1.0` everywhere.
    
    In #9903, this sort of change was added specifically to `bevymark` and
    `many_cubes` but it makes sense to do it everywhere.
    
    ## Discussion
    
    - Maybe we should have a command line option, environment variable, or
    `CI_TESTING_CONFIG` option for 1080p / 1440p / 4k.
    
    - Will these look odd (small text?) when screenshotted and shown in the
    example showcase? The aspect ratio is the same, but they will be
    downscaled from 1080p instead of ~720p.
    
    - Maybe there are other window properties that should be consistent
    across stress tests. e.g. `resizable: false`.
    
    - Should we add a `stress_test_window(title)` helper or something?
    
    - Bevymark (pre-10472) was intentionally 800x600 to match "bunnymark", I
    believe. I don't personally think this is very important.

--- examples/stress_tests/many_glyphs.rs
+++ examples/stress_tests/many_glyphs.rs
@@ -9,7 +9,7 @@ use bevy::{
     diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin},
     prelude::*,
     text::{BreakLineOn, Text2dBounds},
-    window::{PresentMode, WindowPlugin},
+    window::{PresentMode, WindowPlugin, WindowResolution},
 };
 
 fn main() {
@@ -18,6 +18,7 @@ fn main() {
         DefaultPlugins.set(WindowPlugin {
             primary_window: Some(Window {
                 present_mode: PresentMode::AutoNoVsync,
+                resolution: WindowResolution::new(1920.0, 1080.0).with_scale_factor_override(1.0),
                 ..default()
             }),
             ..default()
