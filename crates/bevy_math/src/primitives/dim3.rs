020616958ecaec67f93779432ee91abe589c2248
Joona Aalto <jondolf.dev@gmail.com>
2023-11-18 18:03:53 +0200

    Add more helpers and constructors for 3D primitives

--- crates/bevy_math/src/primitives/dim3.rs
+++ crates/bevy_math/src/primitives/dim3.rs
@@ -1,4 +1,6 @@
-use super::Primitive3d;
+use std::f32::consts::PI;
+
+use super::{Circle, Primitive3d};
 use crate::Vec3;
 
 /// A normalized vector pointing in a direction in 3D space
@@ -34,6 +36,23 @@ pub struct Sphere {
 }
 impl Primitive3d for Sphere {}
 
+impl Sphere {
+    /// Get the diameter of the sphere
+    pub fn diameter(&self) -> f32 {
+        2.0 * self.radius
+    }
+
+    /// Get the surface area of the sphere
+    pub fn area(&self) -> f32 {
+        4.0 * PI * self.radius * self.radius
+    }
+
+    /// Get the volume of the sphere
+    pub fn volume(&self) -> f32 {
+        4.0 * std::f32::consts::FRAC_PI_3 * self.radius * self.radius * self.radius
+    }
+}
+
 /// An unbounded plane in 3D space. It forms a separating surface through the origin,
 /// stretching infinitely far
 #[derive(Clone, Copy, Debug)]
@@ -42,6 +61,22 @@ pub struct Plane3d {
     pub normal: Direction3d,
 }
 impl Primitive3d for Plane3d {}
+
+impl Plane3d {
+    /// Create a new `Plane3d` and get its translation from the origin based on three points.
+    /// The direction of the plane normal is determined by the winding order
+    /// of the triangle formed by the points.
+    ///
+    /// # Panics
+    ///
+    /// Panics if `a == b`, `b == c` or `a == c`.
+    pub fn from_points(&self, a: Vec3, b: Vec3, c: Vec3) -> (Self, Vec3) {
+        debug_assert!(a != b && b != c && a != c);
+        let normal = Direction3d::from((b - a).cross(c - a));
+        let translation = (a + b + c) / 3.0;
+        (Self { normal }, translation)
+    }
+}
 
 /// An infinite line along a direction in 3D space.
 ///
@@ -76,7 +111,9 @@ impl Segment3d {
 
     /// Get a line segment and translation from two points at each end of a line segment
     ///
-    /// Panics if point1 == point2
+    /// # Panics
+    ///
+    /// Panics if `point1 == point2`
     pub fn from_points(point1: Vec3, point2: Vec3) -> (Self, Vec3) {
         let diff = point2 - point1;
         let length = diff.length();
@@ -156,21 +193,45 @@ impl BoxedPolyline3d {
 #[derive(Clone, Copy, Debug)]
 pub struct Cuboid {
     /// Half of the width, height and depth of the cuboid
-    pub half_extents: Vec3,
+    pub half_size: Vec3,
 }
 impl Primitive3d for Cuboid {}
 
 impl Cuboid {
-    /// Create a cuboid from a full x, y and z length
+    /// Create a new `Cuboid` from a full x, y and z length
     pub fn new(x_length: f32, y_length: f32, z_length: f32) -> Self {
         Self::from_size(Vec3::new(x_length, y_length, z_length))
     }
 
-    /// Create a cuboid from a given full size
+    /// Create a new `Cuboid` from a given full size
     pub fn from_size(size: Vec3) -> Self {
         Self {
-            half_extents: size / 2.,
+            half_size: size / 2.,
         }
+    }
+
+    /// Create a new `Cuboid` from two corner points
+    pub fn from_corners(point1: Vec3, point2: Vec3) -> Self {
+        Self {
+            half_size: (point2 - point1).abs() / 2.0,
+        }
+    }
+
+    /// Get the size of the cuboid
+    pub fn size(&self) -> Vec3 {
+        2.0 * self.half_size
+    }
+
+    /// Get the surface area of the cuboid
+    pub fn area(&self) -> f32 {
+        8.0 * (self.half_size.x * self.half_size.y
+            + self.half_size.y * self.half_size.z
+            + self.half_size.x * self.half_size.z)
+    }
+
+    /// Get the volume of the cuboid
+    pub fn volume(&self) -> f32 {
+        8.0 * self.half_size.x * self.half_size.y * self.half_size.z
     }
 }
 
@@ -185,12 +246,41 @@ pub struct Cylinder {
 impl Primitive3d for Cylinder {}
 
 impl Cylinder {
-    /// Create a cylinder from a radius and full height
+    /// Create a new `Cylinder` from a radius and full height
     pub fn new(radius: f32, height: f32) -> Self {
         Self {
             radius,
             half_height: height / 2.,
         }
+    }
+
+    /// Get the base of the cylinder as a [`Circle`]
+    pub fn base(&self) -> Circle {
+        Circle {
+            radius: self.radius,
+        }
+    }
+
+    /// Get the surface area of the side of the cylinder,
+    /// also known as the lateral area
+    #[doc(alias = "side_area")]
+    pub fn lateral_area(&self) -> f32 {
+        4.0 * PI * self.radius * self.half_height
+    }
+
+    /// Get the surface area of one base of the cylinder
+    pub fn base_area(&self) -> f32 {
+        PI * self.radius * self.radius
+    }
+
+    /// Get the total surface area of the cylinder
+    pub fn area(&self) -> f32 {
+        2.0 * PI * self.radius * (self.radius + 2.0 * self.half_height)
+    }
+
+    /// Get the volume of the cylinder
+    pub fn volume(&self) -> f32 {
+        self.base_area() * 2.0 * self.half_height
     }
 }
 
@@ -214,6 +304,27 @@ impl Capsule {
             half_length: length / 2.0,
         }
     }
+
+    /// Get the part connecting the hemispherical ends
+    /// of the capsule as a [`Cylinder`]
+    pub fn to_cylinder(&self) -> Cylinder {
+        Cylinder {
+            radius: self.radius,
+            half_height: self.half_length,
+        }
+    }
+
+    /// Get the surface area of the capsule
+    pub fn area(&self) -> f32 {
+        // Simplified version of 2pi * r * (2r + h)
+        4.0 * PI * self.radius * (self.radius + self.half_length)
+    }
+
+    /// Get the volume of the capsule
+    pub fn volume(&self) -> f32 {
+        // Simplified version of 2pi * r * (2r + h)
+        4.0 * std::f32::consts::FRAC_PI_3 * self.radius * self.radius * self.radius
+    }
 }
 
 /// A cone primitive.
@@ -225,6 +336,44 @@ pub struct Cone {
     pub height: f32,
 }
 impl Primitive3d for Cone {}
+
+impl Cone {
+    /// Get the base of the cone as a [`Circle`]
+    pub fn base(&self) -> Circle {
+        Circle {
+            radius: self.radius,
+        }
+    }
+
+    /// Get the slant height of the cone, the length of the line segment
+    /// connecting a point on the base to the apex
+    #[doc(alias = "side_length")]
+    pub fn slant_height(&self) -> f32 {
+        self.radius.hypot(self.height)
+    }
+
+    /// Get the surface area of the side of the cone,
+    /// also known as the lateral area
+    #[doc(alias = "side_area")]
+    pub fn lateral_area(&self) -> f32 {
+        PI * self.radius * self.slant_height()
+    }
+
+    /// Get the surface area of the base of the cone
+    pub fn base_area(&self) -> f32 {
+        PI * self.radius * self.radius
+    }
+
+    /// Get the total surface area of the cone
+    pub fn area(&self) -> f32 {
+        self.base_area() + self.lateral_area()
+    }
+
+    /// Get the volume of the cone
+    pub fn volume(&self) -> f32 {
+        (self.base_area() * self.height) / 3.0
+    }
+}
 
 /// A conical frustum primitive.
 /// A conical frustum can be created
@@ -243,9 +392,37 @@ impl Primitive3d for ConicalFrustum {}
 /// A torus (AKA donut) primitive.
 #[derive(Clone, Copy, Debug)]
 pub struct Torus {
-    /// The radius of the overall shape
-    pub radius: f32,
-    /// The radius of the internal ring
-    pub ring_radius: f32,
+    /// The inner radius of the torus, the distance
+    /// from the center to the closest edge of the ring
+    pub inner_radius: f32,
+    /// The outer radius of the torus, the distance
+    /// from the center to the farthest part of the ring
+    pub outer_radius: f32,
 }
 impl Primitive3d for Torus {}
+
+impl Torus {
+    /// Get the minor radius of the torus.
+    /// This corresponds to half of the thickness
+    /// of the ring
+    #[doc(alias = "ring_half_thickness")]
+    pub fn minor_radius(&self) -> f32 {
+        (self.outer_radius - self.inner_radius) / 2.0
+    }
+
+    /// Get the major radius of the torus.
+    /// This corresponds to the distance from the center
+    /// of the torus to the middle of the ring
+    pub fn major_radius(&self) -> f32 {
+        self.outer_radius - self.minor_radius()
+    }
+    /// Get the surface area of the torus
+    pub fn area(&self) -> f32 {
+        4.0 * PI * PI * self.major_radius() * self.minor_radius()
+    }
+
+    /// Get the volume of the torus
+    pub fn volume(&self) -> f32 {
+        2.0 * PI * PI * self.major_radius() * self.minor_radius().powi(2)
+    }
+}
