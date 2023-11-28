d00e845ff50f8b06975780a4b49c5ac7cee8b18a
JMS55 <47158642+JMS55@users.noreply.github.com>
2023-11-28 11:11:58 -0500

    Missed a clippy lint

--- crates/bevy_pbr/src/material.rs
+++ crates/bevy_pbr/src/material.rs
@@ -809,6 +809,7 @@ pub fn extract_materials<M: Material>(
     let mut changed_assets = HashSet::default();
     let mut removed = Vec::new();
     for event in events.read() {
+        #[allow(clippy::match_same_arms)]
         match event {
             AssetEvent::Added { id } | AssetEvent::Modified { id } => {
                 changed_assets.insert(*id);
