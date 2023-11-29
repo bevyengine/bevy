878b886051465db47ed0061915a3d5ab41448320
robtfm <50659922+robtfm@users.noreply.github.com>
2023-11-29 23:38:44 +0000

    ignore doc code

--- crates/bevy_gltf/src/loader.rs
+++ crates/bevy_gltf/src/loader.rs
@@ -108,7 +108,7 @@ pub struct GltfLoader {
 /// Specifies optional settings for processing gltfs at load time. By default, all recognized contents of
 /// the gltf will be loaded.
 /// Example usage: to load a gltf but exclude the cameras, replace a call to `asset_server.load("my.gltf")` with
-/// ```
+/// ```no_run
 /// asset_server.load_with_settings(
 ///     "my.gltf",
 ///     |s: &mut GltfLoaderSettings| {
