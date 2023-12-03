9eb26425d6eced94d4c752d3932f6599ac6cdbec
Joona Aalto <jondolf.dev@gmail.com>
2023-12-03 18:33:38 +0200

    Implement `From<Triangle2d>` for `Mesh`

--- crates/bevy_render/src/mesh/primitive_meshes/mod.rs
+++ crates/bevy_render/src/mesh/primitive_meshes/mod.rs
@@ -1,5 +1,8 @@
 use super::{Indices, Mesh, PrimitiveTopology};
-use bevy_math::primitives::{Circle, Cuboid, Rectangle, RegularPolygon};
+use bevy_math::{
+    primitives::{Circle, Cuboid, Rectangle, RegularPolygon, Triangle2d, WindingOrder},
+    Vec2,
+};
 
 impl From<Cuboid> for Mesh {
     fn from(cuboid: Cuboid) -> Self {
@@ -146,5 +149,34 @@ impl MeshableCircle for Circle {
 impl From<Circle> for Mesh {
     fn from(circle: Circle) -> Self {
         circle.mesh(64)
+    }
+}
+
+impl From<Triangle2d> for Mesh {
+    fn from(triangle: Triangle2d) -> Self {
+        let [a, b, c] = triangle.vertices;
+        let max = a.min(b).min(c).abs().max(a.max(b).max(c)) * Vec2::new(1.0, -1.0);
+        let [norm_a, norm_b, norm_c] = [(a) / max, (b) / max, (c) / max];
+        let vertices = [
+            (a.extend(0.0), [0.0, 0.0, 1.0], norm_a / 2.0 + 0.5),
+            (b.extend(0.0), [0.0, 0.0, 1.0], norm_b / 2.0 + 0.5),
+            (c.extend(0.0), [0.0, 0.0, 1.0], norm_c / 2.0 + 0.5),
+        ];
+
+        let indices = if triangle.winding_order() == WindingOrder::CounterClockwise {
+            Indices::U32(vec![0, 1, 2])
+        } else {
+            Indices::U32(vec![0, 2, 1])
+        };
+
+        let positions: Vec<_> = vertices.iter().map(|(p, _, _)| *p).collect();
+        let normals: Vec<_> = vertices.iter().map(|(_, n, _)| *n).collect();
+        let uvs: Vec<_> = vertices.iter().map(|(_, _, uv)| *uv).collect();
+
+        Mesh::new(PrimitiveTopology::TriangleList)
+            .with_indices(Some(indices))
+            .with_inserted_attribute(Mesh::ATTRIBUTE_POSITION, positions)
+            .with_inserted_attribute(Mesh::ATTRIBUTE_NORMAL, normals)
+            .with_inserted_attribute(Mesh::ATTRIBUTE_UV_0, uvs)
     }
 }
