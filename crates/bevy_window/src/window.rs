b909d51dfcf66ee4c440d1de79fa8700e387c37b
Mateusz Wachowiak <mateusz_wachowiak@outlook.com>
2023-12-04 23:00:05 +0100

    Add comment about scale factor in `WindowMode`

--- crates/bevy_window/src/window.rs
+++ crates/bevy_window/src/window.rs
@@ -989,6 +989,11 @@ pub enum WindowMode {
     /// When setting this, the window's physical size will be modified to match the size
     /// of the current monitor resolution, and the logical size will follow based
     /// on the scale factor, see [`WindowResolution`].
+    ///
+    /// Note: As this mode respects the scale factor provided by the operating system,
+    /// the window's logical size may be different from its physical size.
+    /// If you want to avoid that behavior, you can use the [`WindowResolution::set_scale_factor_override`]
+    /// function to set the scale factor to 1.0.
     BorderlessFullscreen,
     /// The window should be in "true"/"legacy" Fullscreen mode.
     ///
@@ -1006,6 +1011,11 @@ pub enum WindowMode {
     /// After that, the window's physical size will be modified to match
     /// that monitor resolution, and the logical size will follow based on the
     /// scale factor, see [`WindowResolution`].
+    ///
+    /// Note: As this mode respects the scale factor provided by the operating system,
+    /// the window's logical size may be different from its physical size.
+    /// If you want to avoid that behavior, you can use the [`WindowResolution::set_scale_factor_override`]
+    /// function to set the scale factor to 1.0.
     Fullscreen,
 }
 
