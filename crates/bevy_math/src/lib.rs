d5c2da5c10adca471fe0b385d8616e200929491c
JohnTheCoolingFan <ivan8215145640@gmail.com>
2023-12-04 12:40:11 +0300

    Import CubicNurbs and CubicNurbsError in prelude

--- crates/bevy_math/src/lib.rs
+++ crates/bevy_math/src/lib.rs
@@ -22,7 +22,7 @@ pub mod prelude {
     pub use crate::{
         cubic_splines::{
             CubicBSpline, CubicBezier, CubicCardinalSpline, CubicGenerator, CubicHermite,
-            CubicSegment,
+            CubicNurbs, CubicNurbsError, CubicSegment,
         },
         primitives, BVec2, BVec3, BVec4, EulerRot, IRect, IVec2, IVec3, IVec4, Mat2, Mat3, Mat4,
         Quat, Ray, Rect, URect, UVec2, UVec3, UVec4, Vec2, Vec2Swizzles, Vec3, Vec3Swizzles, Vec4,
