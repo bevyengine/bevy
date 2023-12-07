46b5f50c34f84cf1f2ada3ee2250f9da33334530
Joona Aalto <jondolf.dev@gmail.com>
2023-12-07 23:03:12 +0200

    Fix camera position in 3d_shapes example

--- examples/3d/3d_shapes.rs
+++ examples/3d/3d_shapes.rs
@@ -82,7 +82,7 @@ fn setup(
     });
 
     commands.spawn(Camera3dBundle {
-        transform: Transform::from_xyz(2.0, 3., 6.0).looking_at(Vec3::new(2., 1., 0.), Vec3::Y),
+        transform: Transform::from_xyz(0.0, 5.0, 12.0).looking_at(Vec3::new(0., 1., 0.), Vec3::Y),
         ..default()
     });
 }
