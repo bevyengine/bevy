dd649101ce8ffee2a8c85b984c93ae44806cbb0b
James Liu <contact@jamessliu.com>
2023-12-23 08:25:46 -0800

    Fix step_unclamped
    Co-authored-by: irate <JustTheCoolDude@gmail.com>

--- crates/bevy_animation/src/util.rs
+++ crates/bevy_animation/src/util.rs
@@ -1,8 +1,8 @@
-/// Steps between two different discrete values of any clonable type.
-/// Returns a copy of `b` if `t >= 1.0`, otherwise returns a copy of `a`.
+/// Steps between two different discrete values of any type.
+/// Returns `a` if `t < 1.0`, otherwise returns `b`.
 #[inline]
 pub(crate) fn step_unclamped<T>(a: T, b: T, t: f32) -> T {
-    if t >= 1.0 {
+    if t < 1.0 {
         a
     } else {
         b
