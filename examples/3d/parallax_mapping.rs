524942bb261ee04962f0bb6e31a52faae6944676
irate-devil <justthecooldude@gmail.com>
2023-12-24 18:25:31 +0100

    Use `lerp` in examples

--- examples/3d/parallax_mapping.rs
+++ examples/3d/parallax_mapping.rs
@@ -99,8 +99,7 @@ fn update_parallax_depth_scale(
         let mut text = text.single_mut();
         for (_, mat) in materials.iter_mut() {
             let current_depth = mat.parallax_depth_scale;
-            let new_depth =
-                current_depth * (1.0 - DEPTH_CHANGE_RATE) + (target_depth.0 * DEPTH_CHANGE_RATE);
+            let new_depth = current_depth.lerp(target_depth.0, DEPTH_CHANGE_RATE);
             mat.parallax_depth_scale = new_depth;
             text.sections[0].value = format!("Parallax depth scale: {new_depth:.5}\n");
             if (new_depth - current_depth).abs() <= 0.000000001 {
