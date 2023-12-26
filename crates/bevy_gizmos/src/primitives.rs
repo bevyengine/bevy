5596ab60f4f9a7c85bf6c97b210707a7f50890af
aviac <aviac@mailbox.org>
2023-12-26 14:20:34 +0100

    feat: add gizmo trait impl for all 2D primitives
    Authored-by: RobWalt <robwalter96@gmail.com>

--- crates/bevy_gizmos/src/primitives.rs
+++ crates/bevy_gizmos/src/primitives.rs
@@ -1,5 +1,6 @@
 use bevy_math::primitives::{
-    Circle, Direction2d, Ellipse, Line2d, Plane2d, Primitive2d, Primitive3d, Segment2d, Sphere,
+    BoxedPolygon, BoxedPolyline2d, Circle, Direction2d, Ellipse, Line2d, Plane2d, Polygon,
+    Polyline2d, Primitive2d, Primitive3d, Rectangle, RegularPolygon, Segment2d, Sphere, Triangle2d,
 };
 use bevy_math::{Quat, Vec2, Vec3};
 use bevy_render::color::Color;
@@ -18,9 +19,9 @@ pub trait GizmoPrimitive3d<P: Primitive3d, D: PrimitiveDetailFor<P>> {
 
 // Direction 2D
 
-struct DirectionDetails {
-    position: Vec2,
-    color: Color,
+pub struct DirectionDetails {
+    pub position: Vec2,
+    pub color: Color,
 }
 impl PrimitiveDetailFor<Direction2d> for DirectionDetails {}
 
@@ -39,9 +40,9 @@ impl<'s> GizmoPrimitive2d<Direction2d, DirectionDetails> for Gizmos<'s> {
 
 // Circle 2D
 
-struct CircleDetails {
-    position: Vec2,
-    color: Color,
+pub struct CircleDetails {
+    pub position: Vec2,
+    pub color: Color,
 }
 impl PrimitiveDetailFor<Circle> for CircleDetails {}
 
@@ -54,9 +55,9 @@ impl<'s> GizmoPrimitive2d<Circle, CircleDetails> for Gizmos<'s> {
 
 // Ellipse 2D
 
-struct EllipseDetails {
-    position: Vec2,
-    color: Color,
+pub struct EllipseDetails {
+    pub position: Vec2,
+    pub color: Color,
 }
 impl PrimitiveDetailFor<Ellipse> for EllipseDetails {}
 
@@ -69,9 +70,9 @@ impl<'s> GizmoPrimitive2d<Ellipse, EllipseDetails> for Gizmos<'s> {
 
 // Line 2D
 
-struct LineDetails {
-    position: Vec2,
-    color: Color,
+pub struct LineDetails {
+    pub position: Vec2,
+    pub color: Color,
 }
 impl PrimitiveDetailFor<Line2d> for LineDetails {}
 
@@ -92,9 +93,9 @@ impl<'s> GizmoPrimitive2d<Line2d, LineDetails> for Gizmos<'s> {
 
 // Plane 2D
 
-struct PlaneDetails {
-    position: Vec2,
-    color: Color,
+pub struct PlaneDetails {
+    pub position: Vec2,
+    pub color: Color,
 }
 impl PrimitiveDetailFor<Plane2d> for PlaneDetails {}
 
@@ -111,9 +112,9 @@ impl<'s> GizmoPrimitive2d<Plane2d, PlaneDetails> for Gizmos<'s> {
 
 // Segment 2D
 
-struct SegmentDetails {
-    position: Vec2,
-    color: Color,
+pub struct SegmentDetails {
+    pub position: Vec2,
+    pub color: Color,
 }
 impl PrimitiveDetailFor<Segment2d> for SegmentDetails {}
 
@@ -129,12 +130,256 @@ impl<'s> GizmoPrimitive2d<Segment2d, SegmentDetails> for Gizmos<'s> {
     }
 }
 
+// Polyline 2D
+
+pub struct PolylineDetails {
+    pub position: Vec2,
+    pub rotation: Vec2,
+    pub color: Color,
+}
+impl<const N: usize> PrimitiveDetailFor<Polyline2d<N>> for PolylineDetails {}
+
+impl<'s, const N: usize> GizmoPrimitive2d<Polyline2d<N>, PolylineDetails> for Gizmos<'s> {
+    fn primitive_2d(&mut self, primitive: Polyline2d<N>, detail: PolylineDetails) {
+        let PolylineDetails {
+            position,
+            rotation,
+            color,
+        } = detail;
+        self.linestrip_2d(
+            primitive
+                .vertices
+                .into_iter()
+                .map(|vertex| vertex.rotate(rotation) + position),
+            color,
+        );
+    }
+}
+
+// BoxedPolyline 2D
+//
+// not sure here yet, maybe we should use a reference to some of the primitives instead since
+// cloning all the vertices for drawing might defeat its purpose if we pass in the primitive by
+// value
+
+pub struct BoxedPolylineDetails {
+    pub position: Vec2,
+    pub rotation: Vec2,
+    pub color: Color,
+}
+impl PrimitiveDetailFor<BoxedPolyline2d> for BoxedPolylineDetails {}
+
+impl<'s> GizmoPrimitive2d<BoxedPolyline2d, BoxedPolylineDetails> for Gizmos<'s> {
+    fn primitive_2d(&mut self, primitive: BoxedPolyline2d, detail: BoxedPolylineDetails) {
+        let BoxedPolylineDetails {
+            position,
+            rotation,
+            color,
+        } = detail;
+        self.linestrip_2d(
+            primitive
+                .vertices
+                .iter()
+                .map(|vertex| vertex.rotate(rotation) + position),
+            color,
+        );
+    }
+}
+
+// Triangle 2D
+//
+// NOTE: to self: it's a bit ambigious at the moment how the shapes are defined. We should define /
+// document somewhere whether polygons are expected to be closed/open by default. One other
+// possibility would be to handle this via some extra struct and make the user specify what they
+// provide, e.g.
+//
+// ```rust
+// enum Closedness {
+//   Closed,
+//   Open
+// }
+// ```
+//
+// but then we would also write code in the hot paths of gizmos that optionally handle these
+// configurations. It might be preemtive optimization to not go that route on the other hand as
+// well
+//
+// For now I'll assume that all of the primitives are provided in an open configurations. That
+// means that primitive.first != primitive.last
+
+pub struct TriangleDetails {
+    pub position: Vec2,
+    pub rotation: Vec2,
+    pub color: Color,
+}
+impl PrimitiveDetailFor<Triangle2d> for TriangleDetails {}
+
+impl<'s> GizmoPrimitive2d<Triangle2d, TriangleDetails> for Gizmos<'s> {
+    fn primitive_2d(&mut self, primitive: Triangle2d, detail: TriangleDetails) {
+        let TriangleDetails {
+            position,
+            rotation,
+            color,
+        } = detail;
+        let [a, b, c] = primitive.vertices;
+        self.primitive_2d(
+            Polyline2d {
+                vertices: [a, b, c, a],
+            },
+            PolylineDetails {
+                position,
+                rotation,
+                color,
+            },
+        );
+    }
+}
+
+// Rectangle 2D
+
+pub struct RectangleDetails {
+    pub position: Vec2,
+    pub rotation: Vec2,
+    pub color: Color,
+}
+impl PrimitiveDetailFor<Rectangle> for RectangleDetails {}
+
+impl<'s> GizmoPrimitive2d<Rectangle, RectangleDetails> for Gizmos<'s> {
+    fn primitive_2d(&mut self, primitive: Rectangle, detail: RectangleDetails) {
+        let RectangleDetails {
+            position,
+            rotation,
+            color,
+        } = detail;
+        let [a, b, c, d] =
+            [(1.0, 1.0), (1.0, -1.0), (-1.0, -1.0), (-1.0, 1.0)].map(|(sign_x, sign_y)| {
+                Vec2::new(
+                    primitive.half_width * sign_x,
+                    primitive.half_height * sign_y,
+                )
+            });
+        self.primitive_2d(
+            Polyline2d {
+                vertices: [a, b, c, d, a],
+            },
+            PolylineDetails {
+                position,
+                rotation,
+                color,
+            },
+        );
+    }
+}
+
+// Polygon 2D
+
+pub struct PolygonDetails {
+    pub position: Vec2,
+    pub rotation: Vec2,
+    pub color: Color,
+}
+impl<const N: usize> PrimitiveDetailFor<Polygon<N>> for PolygonDetails {}
+
+impl<'s, const N: usize> GizmoPrimitive2d<Polygon<N>, PolygonDetails> for Gizmos<'s> {
+    fn primitive_2d(&mut self, primitive: Polygon<N>, detail: PolygonDetails) {
+        let PolygonDetails {
+            position,
+            rotation,
+            color,
+        } = detail;
+        let closing_point = primitive.vertices.last().cloned();
+        self.linestrip_2d(
+            primitive
+                .vertices
+                .into_iter()
+                .chain(closing_point)
+                .map(|vertex| vertex.rotate(rotation) + position),
+            color,
+        );
+    }
+}
+
+// BoxedPolygon 2D
+
+pub struct BoxedPolygonDetails {
+    pub position: Vec2,
+    pub rotation: Vec2,
+    pub color: Color,
+}
+impl PrimitiveDetailFor<BoxedPolygon> for BoxedPolygonDetails {}
+
+impl<'s> GizmoPrimitive2d<BoxedPolygon, BoxedPolygonDetails> for Gizmos<'s> {
+    fn primitive_2d(&mut self, primitive: BoxedPolygon, detail: BoxedPolygonDetails) {
+        let BoxedPolygonDetails {
+            position,
+            rotation,
+            color,
+        } = detail;
+        let closing_point = primitive.vertices.last().cloned();
+        self.linestrip_2d(
+            primitive
+                .vertices
+                .iter()
+                .cloned()
+                .chain(closing_point)
+                .map(|vertex| vertex.rotate(rotation) + position),
+            color,
+        );
+    }
+}
+
+// RegularPolygon 2D
+
+pub struct RegularPolygonDetails {
+    pub position: Vec2,
+    pub rotation: Vec2,
+    pub color: Color,
+}
+
+impl Default for RegularPolygonDetails {
+    fn default() -> Self {
+        Self {
+            position: Vec2::ZERO,
+            rotation: Vec2::from_angle(0.0),
+            color: Color::default(),
+        }
+    }
+}
+
+impl PrimitiveDetailFor<RegularPolygon> for RegularPolygonDetails {}
+
+impl<'s> GizmoPrimitive2d<RegularPolygon, RegularPolygonDetails> for Gizmos<'s> {
+    fn primitive_2d(&mut self, primitive: RegularPolygon, detail: RegularPolygonDetails) {
+        let RegularPolygonDetails {
+            position,
+            rotation,
+            color,
+        } = detail;
+
+        fn regular_polygon_inner(radius: f32, segments: usize) -> impl Iterator<Item = [Vec2; 2]> {
+            (0..segments + 1).map(|i| [i, i + 1]).map(move |vals| {
+                vals.map(|i| {
+                    let angle = i as f32 * std::f32::consts::TAU / segments as f32;
+                    let (x, y) = angle.sin_cos();
+                    Vec2::new(x, y) * radius
+                })
+            })
+        }
+
+        regular_polygon_inner(primitive.circumcircle.radius, primitive.sides)
+            .map(|vertices| vertices.map(|vertex| vertex.rotate(rotation) + position))
+            .for_each(|[start, end]| {
+                self.line_2d(start, end, color);
+            });
+    }
+}
+
 // Sphere 3D
 
-struct SphereDetails {
-    position: Vec3,
-    rotation: Quat,
-    color: Color,
+pub struct SphereDetails {
+    pub position: Vec3,
+    pub rotation: Quat,
+    pub color: Color,
 }
 impl PrimitiveDetailFor<Sphere> for SphereDetails {}
 
