87d499c33a3f0e9bdaaef100f62690c097ff62f1
Joseph <21144246+JoJoJet@users.noreply.github.com>
2023-12-27 11:59:21 -0800

    Update a doctest

--- crates/bevy_ecs/src/system/commands/mod.rs
+++ crates/bevy_ecs/src/system/commands/mod.rs
@@ -630,8 +630,8 @@ impl<'w, 's> Commands<'w, 's> {
 /// # assert_schedule.run(&mut world);
 ///
 /// fn setup(mut commands: Commands) {
-///     commands.spawn_empty().add(CountName);
-///     commands.spawn_empty().add(CountName);
+///     commands.spawn_empty().add(count_name);
+///     commands.spawn_empty().add(count_name);
 /// }
 ///
 /// fn assert_names(named: Query<&Name>) {
