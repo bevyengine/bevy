5ad466f9ddf67dc7bbb9ba5dc6a64fe7f77f9a11
Joona Aalto <jondolf.dev@gmail.com>
2023-12-30 14:18:25 +0200

    Reflect more

--- crates/bevy_reflect/src/impls/angle.rs
+++ crates/bevy_reflect/src/impls/angle.rs
@@ -1,5 +1,11 @@
-use crate as bevy_reflect;
 use crate::prelude::ReflectDefault;
+use crate::{self as bevy_reflect, ReflectDeserialize, ReflectSerialize};
 use bevy_reflect_derive::impl_reflect_value;
 
-impl_reflect_value!(::bevy_math::Angle(Debug, Default));
+impl_reflect_value!(::bevy_math::Angle(
+    Debug,
+    Default,
+    PartialEq,
+    Serialize,
+    Deserialize,
+));
