8baefa1570363e11eb038a1e55529318ef683e1b
Adam <46227443+Adamkob12@users.noreply.github.com>
2024-01-01 08:55:07 -0800

    Implement Deref and DerefMut for In (#11104)
    # Objective
    
    Implement Deref and DerefMut for In<T>
    
    makes it so the user doesn't have to add ".0" in most cases

--- crates/bevy_ecs/src/system/mod.rs
+++ crates/bevy_ecs/src/system/mod.rs
@@ -308,6 +308,20 @@ pub fn assert_system_does_not_conflict<Out, Params, S: IntoSystem<(), Out, Param
     system.run((), &mut world);
 }
 
+impl<T> std::ops::Deref for In<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl<T> std::ops::DerefMut for In<T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use std::any::TypeId;
