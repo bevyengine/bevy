dd14f3a47730121a5715473fda843e4b30c3cea9
Patrick Walton <pcwalton@mimiga.net>
2024-01-02 12:38:47 -0800

    Implement lightmaps. (#10231)
    ![Screenshot](https://i.imgur.com/A4KzWFq.png)
    
    # Objective
    
    Lightmaps, textures that store baked global illumination, have been a
    mainstay of real-time graphics for decades. Bevy currently has no
    support for them, so this pull request implements them.
    
    ## Solution
    
    The new `Lightmap` component can be attached to any entity that contains
    a `Handle<Mesh>` and a `StandardMaterial`. When present, it will be
    applied in the PBR shader. Because multiple lightmaps are frequently
    packed into atlases, each lightmap may have its own UV boundaries within
    its texture. An `exposure` field is also provided, to control the
    brightness of the lightmap.
    
    Note that this PR doesn't provide any way to bake the lightmaps. That
    can be done with [The Lightmapper] or another solution, such as Unity's
    Bakery.
    
    ---
    
    ## Changelog
    
    ### Added
    * A new component, `Lightmap`, is available, for baked global
    illumination. If your mesh has a second UV channel (UV1), and you attach
    this component to the entity with that mesh, Bevy will apply the texture
    referenced in the lightmap.
    
    [The Lightmapper]: https://github.com/Naxela/The_Lightmapper
    
    ---------
    
    Co-authored-by: Carter Anderson <mcanders1@gmail.com>

--- crates/bevy_pbr/src/material.rs
+++ crates/bevy_pbr/src/material.rs
@@ -465,6 +465,7 @@ pub fn queue_material_meshes<M: Material>(
     mut render_mesh_instances: ResMut<RenderMeshInstances>,
     render_material_instances: Res<RenderMaterialInstances<M>>,
     images: Res<RenderAssets<Image>>,
+    render_lightmaps: Res<RenderLightmaps>,
     mut views: Query<(
         &ExtractedView,
         &VisibleEntities,
@@ -612,6 +613,13 @@ pub fn queue_material_meshes<M: Material>(
             }
 
             mesh_key |= alpha_mode_pipeline_key(material.properties.alpha_mode);
+
+            if render_lightmaps
+                .render_lightmaps
+                .contains_key(visible_entity)
+            {
+                mesh_key |= MeshPipelineKey::LIGHTMAPPED;
+            }
 
             let pipeline_id = pipelines.specialize(
                 &pipeline_cache,
