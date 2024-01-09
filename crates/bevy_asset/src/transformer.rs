756586858b1d7ac1ee464934c258e197cb0db167
thepackett <thepackett727@aol.com>
2024-01-08 18:38:52 -0600

    Removed experimental Noop AssetTransformer implementation.

--- crates/bevy_asset/src/transformer.rs
+++ crates/bevy_asset/src/transformer.rs
@@ -1,6 +1,5 @@
 use crate::{meta::Settings, Asset};
 use serde::{Deserialize, Serialize};
-use std::{convert::Infallible, marker::PhantomData};
 
 /// Transforms an [`Asset`] of a given [`AssetTransformer::AssetInput`] type to an [`Asset`] of [`AssetTransformer::AssetOutput`] type.
 pub trait AssetTransformer: Send + Sync + 'static {
@@ -18,33 +17,4 @@ pub trait AssetTransformer: Send + Sync + 'static {
         asset: Self::AssetInput,
         settings: &'a Self::Settings,
     ) -> Result<Self::AssetOutput, Box<dyn std::error::Error + Send + Sync + 'static>>;
-}
-
-/// An [`AssetTransformer`] implementation which returns the original [`Asset`] unchanged.
-/// This is useful when
-pub struct NoopAssetTransformer<A: Asset> {
-    marker: PhantomData<fn() -> A>,
-}
-
-impl<A: Asset> AssetTransformer for NoopAssetTransformer<A> {
-    type AssetInput = A;
-    type AssetOutput = A;
-    type Settings = ();
-    type Error = Infallible;
-
-    fn transform<'a>(
-        &'a self,
-        asset: Self::AssetInput,
-        _settings: &'a Self::Settings,
-    ) -> Result<Self::AssetOutput, Box<dyn std::error::Error + Send + Sync + 'static>> {
-        Ok(asset)
-    }
-}
-
-impl<A: Asset> Default for NoopAssetTransformer<A> {
-    fn default() -> Self {
-        NoopAssetTransformer {
-            marker: Default::default(),
-        }
-    }
 }
