c5bbbc6503c261c80be4993a6215f304c938d0f0
pablo-lua <lpablo1eu@gmail.com>
2024-01-09 21:08:00 -0300

    [fix] Clippy warning

--- crates/bevy_ui/src/debug_overlay/mod.rs
+++ crates/bevy_ui/src/debug_overlay/mod.rs
@@ -154,6 +154,7 @@ struct OutlineParam<'w, 's> {
     children: Query<'w, 's, &'static Children>,
     nodes: Query<'w, 's, NodesQuery>,
     view_visibility: Query<'w, 's, &'static ViewVisibility>,
+    ui_scale: Res<'w, UiScale>,
 }
 
 type CameraQuery<'w, 's> = Query<'w, 's, &'static Camera, With<DebugOverlayCamera>>;
@@ -166,7 +167,6 @@ fn outline_roots(
     window: Query<&Window, With<PrimaryWindow>>,
     nonprimary_windows: Query<&Window, Without<PrimaryWindow>>,
     options: Res<UiDebugOptions>,
-    ui_scale: Res<UiScale>,
 ) {
     if !options.enabled {
         return;
@@ -178,7 +178,7 @@ fn outline_roots(
         );
     }
     let window_scale = window.get_single().map_or(1., Window::scale_factor);
-    let scale_factor = window_scale * ui_scale.0;
+    let scale_factor = window_scale * outline.ui_scale.0;
 
     // We let the line be defined by the window scale alone
     let line_width = outline.gizmo_config.line_width / window_scale;
