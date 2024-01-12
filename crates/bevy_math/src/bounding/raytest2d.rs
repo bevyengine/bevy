e65139a30c2aac0394ef2064418df1bfebb75403
NiseVoid <nisevoid@gmail.com>
2024-01-12 01:56:10 +0100

    Add RayTest2d and RayTest3d

---
+++ crates/bevy_math/src/bounding/raytest2d.rs
@@ -0,0 +1,78 @@
+use super::{Aabb2d, BoundingCircle, IntersectsVolume};
+use crate::{primitives::Direction2d, Vec2};
+
+/// A raycast intersection test for 2D bounding volumes
+pub struct RayTest2d {
+    /// The origin of the ray
+    pub origin: Vec2,
+    /// The direction of the ray
+    pub dir: Direction2d,
+    /// The inverse direction of the ray
+    pub inv_dir: Vec2,
+    /// The maximum time of impact of the ray
+    pub max: f32,
+}
+
+impl RayTest2d {
+    /// Construct a [`RayTest2d`] from an origin, [`Direction2d`] and max time of impact.
+    pub fn new(origin: Vec2, dir: Direction2d, max: f32) -> Self {
+        Self {
+            origin,
+            inv_dir: Vec2::ONE / *dir,
+            dir,
+            max,
+        }
+    }
+
+    /// Get the time of impact for an intersection with an [`Aabb2d`], if any.
+    pub fn aabb_intersection_at(&self, aabb: &Aabb2d) -> Option<f32> {
+        let (min_x, max_x) = if self.inv_dir.x.is_sign_positive() {
+            (aabb.min.x, aabb.max.x)
+        } else {
+            (aabb.max.x, aabb.min.x)
+        };
+        let (min_y, max_y) = if self.inv_dir.y.is_sign_positive() {
+            (aabb.min.y, aabb.max.y)
+        } else {
+            (aabb.max.y, aabb.min.y)
+        };
+        let tmin_x = (min_x - self.origin.x) * self.inv_dir.x;
+        let tmin_y = (min_y - self.origin.y) * self.inv_dir.y;
+        let tmax_x = (max_x - self.origin.x) * self.inv_dir.x;
+        let tmax_y = (max_y - self.origin.y) * self.inv_dir.y;
+
+        let tmin = tmin_x.max(tmin_y).max(0.);
+        let tmax = tmax_y.min(tmax_x).min(self.max);
+
+        if tmin <= tmax {
+            Some(tmin)
+        } else {
+            None
+        }
+    }
+
+    /// Get the time of impact for an intersection with a [`BoundingCircle`], if any.
+    pub fn sphere_intersection_at(&self, sphere: &BoundingCircle) -> Option<f32> {
+        let oc = self.origin - sphere.center;
+        let b = oc.dot(*self.dir);
+        let qc = oc - b * *self.dir;
+        let h = sphere.radius().powi(2) - qc.dot(qc);
+        if h < 0. || h > -b {
+            None
+        } else {
+            Some(-b - h)
+        }
+    }
+}
+
+impl IntersectsVolume<Aabb2d> for RayTest2d {
+    fn intersects(&self, volume: &Aabb2d) -> bool {
+        self.aabb_intersection_at(volume).is_some()
+    }
+}
+
+impl IntersectsVolume<BoundingCircle> for RayTest2d {
+    fn intersects(&self, volume: &BoundingCircle) -> bool {
+        self.sphere_intersection_at(volume).is_some()
+    }
+}
---
+++ crates/bevy_math/src/bounding/raytest3d.rs
@@ -0,0 +1,85 @@
+use super::{Aabb3d, BoundingSphere, IntersectsVolume};
+use crate::{primitives::Direction3d, Vec3};
+
+/// A raycast intersection test for 3D bounding volumes
+pub struct RayTest3d {
+    /// The origin of the ray
+    pub origin: Vec3,
+    /// The direction of the ray
+    pub dir: Direction3d,
+    /// The inverse direction of the ray
+    pub inv_dir: Vec3,
+    /// The maximum time of impact of the ray
+    pub max: f32,
+}
+
+impl RayTest3d {
+    /// Construct a [`RayTest3d`] from an origin, [`Direction3d`] and max time of impact.
+    pub fn new(origin: Vec3, dir: Direction3d, max: f32) -> Self {
+        Self {
+            origin,
+            inv_dir: Vec3::ONE / *dir,
+            dir,
+            max,
+        }
+    }
+
+    /// Get the time of impact for an intersection with an [`Aabb3d`], if any.
+    pub fn aabb_intersection_at(&self, aabb: &Aabb3d) -> Option<f32> {
+        let (min_x, max_x) = if self.inv_dir.x.is_sign_positive() {
+            (aabb.min.x, aabb.max.x)
+        } else {
+            (aabb.max.x, aabb.min.x)
+        };
+        let (min_y, max_y) = if self.inv_dir.y.is_sign_positive() {
+            (aabb.min.y, aabb.max.y)
+        } else {
+            (aabb.max.y, aabb.min.y)
+        };
+        let (min_z, max_z) = if self.inv_dir.z.is_sign_positive() {
+            (aabb.min.z, aabb.max.z)
+        } else {
+            (aabb.max.z, aabb.min.z)
+        };
+        let tmin_x = (min_x - self.origin.x) * self.inv_dir.x;
+        let tmin_y = (min_y - self.origin.y) * self.inv_dir.y;
+        let tmin_z = (min_z - self.origin.z) * self.inv_dir.z;
+        let tmax_x = (max_x - self.origin.x) * self.inv_dir.x;
+        let tmax_y = (max_y - self.origin.y) * self.inv_dir.y;
+        let tmax_z = (max_z - self.origin.z) * self.inv_dir.z;
+
+        let tmin = tmin_x.max(tmin_y).max(tmin_z).max(0.);
+        let tmax = tmax_z.min(tmax_y).min(tmax_x).min(self.max);
+
+        if tmin <= tmax {
+            Some(tmin)
+        } else {
+            None
+        }
+    }
+
+    /// Get the time of impact for an intersection with a [`BoundingSphere`], if any.
+    pub fn sphere_intersection_at(&self, sphere: &BoundingSphere) -> Option<f32> {
+        let oc = self.origin - sphere.center;
+        let b = oc.dot(*self.dir);
+        let qc = oc - b * *self.dir;
+        let h = sphere.radius().powi(2) - qc.dot(qc);
+        if h < 0. || h > -b {
+            None
+        } else {
+            Some(-b - h)
+        }
+    }
+}
+
+impl IntersectsVolume<Aabb3d> for RayTest3d {
+    fn intersects(&self, volume: &Aabb3d) -> bool {
+        self.aabb_intersection_at(volume).is_some()
+    }
+}
+
+impl IntersectsVolume<BoundingSphere> for RayTest3d {
+    fn intersects(&self, volume: &BoundingSphere) -> bool {
+        self.sphere_intersection_at(volume).is_some()
+    }
+}
