dbb4b920d80d74dbbb24d5525a6cf90f528282e3
Mateusz Wachowiak <mateusz_wachowiak@outlook.com>
2024-01-15 22:24:04 +0100

    move ci_testing to bevy_dev_tools

--- crates/bevy_dev_tools/src/lib.rs
+++ crates/bevy_dev_tools/src/lib.rs
@@ -3,10 +3,17 @@
 //! aimed at improving developer experience.
 
 use bevy_app::prelude::*;
+#[cfg(feature = "bevy_ci_testing")]
+pub mod ci_testing;
 
 /// Adds developer tools to an App.
 pub struct DevToolsPlugin;
 
 impl Plugin for DevToolsPlugin {
-    fn build(&self, _app: &mut App) {}
+    fn build(&self, _app: &mut App) {
+        #[cfg(feature = "bevy_ci_testing")]
+        {
+            ci_testing::setup_app(_app);
+        }
+    }
 }
