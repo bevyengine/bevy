9d32bd9ad778446e3f0c5684834896d2ce41a8d4
Mateusz Wachowiak <mateusz_wachowiak@outlook.com>
2024-01-16 21:30:00 +0100

    add example how to turn off DevToolsPlugin

--- crates/bevy_dev_tools/src/lib.rs
+++ crates/bevy_dev_tools/src/lib.rs
@@ -7,6 +7,23 @@ use bevy_app::prelude::*;
 pub mod ci_testing;
 
 /// Enables developer tools in an [`App`].
+/// This plugin is part of the [`DefaultPlugins`](https://docs.rs/bevy/latest/bevy/struct.DefaultPlugins.html), and enabled by default.
+///
+/// It's recommended to disable it in a release. To disable it, you can either:
+///
+/// - Remove it when adding [`DefaultPlugins`](https://docs.rs/bevy/latest/bevy/struct.DefaultPlugins.html):
+/// ```no_run
+/// # use bevy_app::{App, NoopPluginGroup as DefaultPlugins, PluginGroup};
+/// # use bevy_dev_tools::{DevToolsPlugin};
+/// fn main() {
+///     App::new()
+///         .add_plugins(DefaultPlugins.build().disable::<DevToolsPlugin>())
+///         .run();
+/// }
+/// ```
+///
+/// - Disable the feature:
+/// Disable default features from Bevy, and don't enable the feature `bevy_dev_tools`.
 pub struct DevToolsPlugin;
 
 impl Plugin for DevToolsPlugin {
