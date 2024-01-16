294a078f8c9a874df3a342b59e327d1df8c93704
Mateusz Wachowiak <mateusz_wachowiak@outlook.com>
2024-01-16 21:12:41 +0100

    rephrase docs

--- crates/bevy_dev_tools/src/lib.rs
+++ crates/bevy_dev_tools/src/lib.rs
@@ -1,12 +1,12 @@
 #![warn(missing_docs)]
 //! This crate provides additional utilities for the [Bevy game engine](https://bevyengine.org),
-//! aimed at improving developer experience.
+//! focused on improving developer experience.
 
 use bevy_app::prelude::*;
 #[cfg(feature = "bevy_ci_testing")]
 pub mod ci_testing;
 
-/// Adds developer tools to an App.
+/// Enables developer tools in an [`App`].
 pub struct DevToolsPlugin;
 
 impl Plugin for DevToolsPlugin {
