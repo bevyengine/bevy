cfa4634a287367248c9db77e16c1e36bfbfde9a1
Lee-Orr <leeorr@gmail.com>
2024-01-19 15:32:34 -0500

    add example

---
+++ examples/ecs/derived_state.rs
@@ -0,0 +1,205 @@
+//! This example illustrates how to use [`DerivedStates`] for more complex state transitions.
+//!
+//! In this case, we're transitioning from a `Menu` state to an `InGame` state, with the addition
+//! of the capacity to pause and unpause if we are in game using the ESCAPE button on the keyboard.
+
+use bevy::prelude::*;
+use bevy_internal::ecs::schedule::DerivedStates;
+
+fn main() {
+    // We start the setup like we did in the states example.
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .init_state::<AppState>() 
+        .add_systems(Startup, setup)
+        .add_systems(OnEnter(AppState::Menu), setup_menu)
+        .add_systems(Update, menu.run_if(in_state(AppState::Menu)))
+        .add_systems(OnExit(AppState::Menu), cleanup_menu)
+
+        // We only want to run the [`setup_game`] function when we enter the [`AppState::InGame`] state, regardless
+        // of whether the game is paused or not. So we add an [`InGame`] derived state, and rely on that.
+        .derive_state::<InGame>()
+        .add_systems(OnEnter(InGame), setup_game)
+        // We want the color change and the toggle_pause systems to ignore the paused condition, so we can use the [`InGame`] derived
+        // state here as well.
+        .add_systems(Update, (toggle_pause,change_color).run_if(in_state(InGame)))
+
+        // However, we only want the movement to happen if we are not in a paused state, so here we
+        // use the full AppState: `AppState::InGame { paused: false }`.
+        .add_systems(
+            Update,
+            movement.run_if(in_state(AppState::InGame { paused: false}))
+        )
+        .add_systems(Update, log_transitions)
+        .run();
+}
+
+#[derive(Debug, Clone, Copy, Default, Eq, PartialEq, Hash, States)]
+enum AppState {
+    #[default]
+    Menu,
+    InGame {
+        paused: bool
+    },
+}
+
+#[derive(Debug, Clone, Copy, Eq, PartialEq, Hash, States)]
+struct InGame;
+
+impl DerivedStates for InGame {
+    type SourceStates = AppState;
+
+    fn derive(
+        sources: <<Self as DerivedStates>::SourceStates as bevy_internal::ecs::schedule::StateSet>::Optionals,
+    ) -> Option<Self> {
+        match sources {
+            Some(AppState::InGame { .. }) => Some(InGame),
+            _ => None
+        }
+    }
+}
+
+#[derive(Resource)]
+struct MenuData {
+    button_entity: Entity,
+}
+
+const NORMAL_BUTTON: Color = Color::rgb(0.15, 0.15, 0.15);
+const HOVERED_BUTTON: Color = Color::rgb(0.25, 0.25, 0.25);
+const PRESSED_BUTTON: Color = Color::rgb(0.35, 0.75, 0.35);
+
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
+}
+
+fn setup_menu(mut commands: Commands) {
+    let button_entity = commands
+        .spawn(NodeBundle {
+            style: Style {
+                // center button
+                width: Val::Percent(100.),
+                height: Val::Percent(100.),
+                justify_content: JustifyContent::Center,
+                align_items: AlignItems::Center,
+                ..default()
+            },
+            ..default()
+        })
+        .with_children(|parent| {
+            parent
+                .spawn(ButtonBundle {
+                    style: Style {
+                        width: Val::Px(150.),
+                        height: Val::Px(65.),
+                        // horizontally center child text
+                        justify_content: JustifyContent::Center,
+                        // vertically center child text
+                        align_items: AlignItems::Center,
+                        ..default()
+                    },
+                    background_color: NORMAL_BUTTON.into(),
+                    ..default()
+                })
+                .with_children(|parent| {
+                    parent.spawn(TextBundle::from_section(
+                        "Play",
+                        TextStyle {
+                            font_size: 40.0,
+                            color: Color::rgb(0.9, 0.9, 0.9),
+                            ..default()
+                        },
+                    ));
+                });
+        })
+        .id();
+    commands.insert_resource(MenuData { button_entity });
+}
+
+fn menu(
+    mut next_state: ResMut<NextState<AppState>>,
+    mut interaction_query: Query<
+        (&Interaction, &mut BackgroundColor),
+        (Changed<Interaction>, With<Button>),
+    >,
+) {
+    for (interaction, mut color) in &mut interaction_query {
+        match *interaction {
+            Interaction::Pressed => {
+                *color = PRESSED_BUTTON.into();
+                next_state.set(AppState::InGame { paused: false });
+            }
+            Interaction::Hovered => {
+                *color = HOVERED_BUTTON.into();
+            }
+            Interaction::None => {
+                *color = NORMAL_BUTTON.into();
+            }
+        }
+    }
+}
+
+fn cleanup_menu(mut commands: Commands, menu_data: Res<MenuData>) {
+    commands.entity(menu_data.button_entity).despawn_recursive();
+}
+
+fn setup_game(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn(SpriteBundle {
+        texture: asset_server.load("branding/icon.png"),
+        ..default()
+    });
+}
+
+const SPEED: f32 = 100.0;
+fn movement(
+    time: Res<Time>,
+    input: Res<ButtonInput<KeyCode>>,
+    mut query: Query<&mut Transform, With<Sprite>>,
+) {
+    for mut transform in &mut query {
+        let mut direction = Vec3::ZERO;
+        if input.pressed(KeyCode::ArrowLeft) {
+            direction.x -= 1.0;
+        }
+        if input.pressed(KeyCode::ArrowRight) {
+            direction.x += 1.0;
+        }
+        if input.pressed(KeyCode::ArrowUp) {
+            direction.y += 1.0;
+        }
+        if input.pressed(KeyCode::ArrowDown) {
+            direction.y -= 1.0;
+        }
+
+        if direction != Vec3::ZERO {
+            transform.translation += direction.normalize() * SPEED * time.delta_seconds();
+        }
+    }
+}
+
+fn toggle_pause(input: Res<ButtonInput<KeyCode>>, current_state: Res<State<AppState>>, mut next_state: ResMut<NextState<AppState>>) {
+    if input.just_pressed(KeyCode::Escape) {
+        let AppState::InGame { paused } = current_state.get() else {
+            return;
+        };
+        next_state.set(AppState::InGame { paused: !*paused });
+    }
+}
+
+fn change_color(time: Res<Time>, mut query: Query<&mut Sprite>) {
+    for mut sprite in &mut query {
+        sprite
+            .color
+            .set_b((time.elapsed_seconds() * 0.5).sin() + 2.0);
+    }
+}
+
+/// print when an `AppState` transition happens
+/// also serves as an example of how to use `StateTransitionEvent`
+fn log_transitions(mut transitions: EventReader<StateTransitionEvent<AppState>>) {
+    for transition in transitions.read() {
+        info!(
+            "transition: {:?} => {:?}",
+            transition.before, transition.after
+        );
+    }
+}
