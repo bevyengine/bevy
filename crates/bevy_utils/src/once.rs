e9b8c71da07ea03ac131d016a349911979eb6e80
Lee-Orr <lee-orr@users.noreply.github.com>
2024-01-19 01:07:41 -0500

    move once from bevy_log to bevy_utils, to allow for it's use in bevy_ecs (#11419)
    # Objective
    
    When working within `bevy_ecs`, we can't use the `log_once` macros due
    to their placement in `bevy_log` - which depends on `bevy_ecs`. All this
    create does is migrate those macros to the `bevy_utils` crate, while
    still re-exporting them in `bevy_log`.
    
    created to resolve this:
    https://github.com/bevyengine/bevy/pull/11417#discussion_r1458100211
    
    ---------
    
    Co-authored-by: François <mockersf@gmail.com>

--- crates/bevy_utils/src/once.rs
+++ crates/bevy_utils/src/once.rs
@@ -11,52 +11,52 @@ macro_rules! once {
     }};
 }
 
-/// Call [`trace!`](crate::trace) once per call site.
+/// Call [`trace!`](crate::tracing::trace) once per call site.
 ///
 /// Useful for logging within systems which are called every frame.
 #[macro_export]
 macro_rules! trace_once {
     ($($arg:tt)+) => ({
-        $crate::once!($crate::trace!($($arg)+))
+        $crate::once!($crate::tracing::trace!($($arg)+))
     });
 }
 
-/// Call [`debug!`](crate::debug) once per call site.
+/// Call [`debug!`](crate::tracing::debug) once per call site.
 ///
 /// Useful for logging within systems which are called every frame.
 #[macro_export]
 macro_rules! debug_once {
     ($($arg:tt)+) => ({
-        $crate::once!($crate::debug!($($arg)+))
+        $crate::once!($crate::tracing::debug!($($arg)+))
     });
 }
 
-/// Call [`info!`](crate::info) once per call site.
+/// Call [`info!`](crate::tracing::info) once per call site.
 ///
 /// Useful for logging within systems which are called every frame.
 #[macro_export]
 macro_rules! info_once {
     ($($arg:tt)+) => ({
-        $crate::once!($crate::info!($($arg)+))
+        $crate::once!($crate::tracing::info!($($arg)+))
     });
 }
 
-/// Call [`warn!`](crate::warn) once per call site.
+/// Call [`warn!`](crate::tracing::warn) once per call site.
 ///
 /// Useful for logging within systems which are called every frame.
 #[macro_export]
 macro_rules! warn_once {
     ($($arg:tt)+) => ({
-        $crate::once!($crate::warn!($($arg)+))
+        $crate::once!($crate::tracing::warn!($($arg)+))
     });
 }
 
-/// Call [`error!`](crate::error) once per call site.
+/// Call [`error!`](crate::tracing::error) once per call site.
 ///
 /// Useful for logging within systems which are called every frame.
 #[macro_export]
 macro_rules! error_once {
     ($($arg:tt)+) => ({
-        $crate::once!($crate::error!($($arg)+))
+        $crate::once!($crate::tracing::error!($($arg)+))
     });
 }
