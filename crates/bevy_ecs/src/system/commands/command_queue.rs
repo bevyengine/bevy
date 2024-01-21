528f866ed28897eb341ff416bed796256fe0b49e
Pixelstorm <thepixelstormer@gmail.com>
2024-01-21 16:38:03 +0000

    Impl debug manually rather than deriving it

--- crates/bevy_ecs/src/system/commands/command_queue.rs
+++ crates/bevy_ecs/src/system/commands/command_queue.rs
@@ -1,4 +1,4 @@
-use std::mem::MaybeUninit;
+use std::{fmt::Debug, mem::MaybeUninit};
 
 use bevy_ptr::{OwningPtr, Unaligned};
 use bevy_utils::tracing::warn;
@@ -24,7 +24,7 @@ struct CommandMeta {
 // entities/components/resources, and it's not currently possible to parallelize these
 // due to mutable [`World`] access, maximizing performance for [`CommandQueue`] is
 // preferred to simplicity of implementation.
-#[derive(Debug, Default)]
+#[derive(Default)]
 pub struct CommandQueue {
     // This buffer densely stores all queued commands.
     //
@@ -32,6 +32,19 @@ pub struct CommandQueue {
     // to store the command itself. To interpret these bytes, a pointer must
     // be passed to the corresponding `CommandMeta.apply_command_and_get_size` fn pointer.
     bytes: Vec<MaybeUninit<u8>>,
+}
+
+// CommandQueue needs to implement Debug manually, rather than deriving it, because the derived impl just prints
+// [core::mem::maybe_uninit::MaybeUninit<u8>, core::mem::maybe_uninit::MaybeUninit<u8>, ..] for every byte in the vec,
+// which gets extremely verbose very quickly, while also providing no useful information.
+// It is not possible to soundly print the values of the contained bytes, as some of them may be padding or uninitialized (#4863)
+// So instead, the manual impl just prints the length of vec.
+impl Debug for CommandQueue {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("CommandQueue")
+            .field("len", &self.bytes.len())
+            .finish_non_exhaustive()
+    }
 }
 
 // SAFETY: All commands [`Command`] implement [`Send`]
