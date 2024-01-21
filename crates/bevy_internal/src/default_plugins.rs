3e4e172480bf451f3a1e27ca86cfbc0c8fa01f9b
Doonv <58695417+doonv@users.noreply.github.com>
2024-01-21 20:59:55 +0200

    Add CorePipelinePlugin to DefaultPlugins

--- crates/bevy_internal/src/default_plugins.rs
+++ crates/bevy_internal/src/default_plugins.rs
@@ -30,6 +30,8 @@ plugin_group!(
         #[cfg(feature = "bevy_render")]
         #[custom(cfg(all(not(target_arch = "wasm32"), feature = "multi-threaded")))]
         bevy_render::pipelined_rendering:::PipelinedRenderingPlugin,
+        #[cfg(feature = "bevy_core_pipeline")]
+        bevy_core_pipeline:::CorePipelinePlugin,
         #[cfg(feature = "bevy_sprite")]
         bevy_sprite:::SpritePlugin,
         #[cfg(feature = "bevy_text")]
