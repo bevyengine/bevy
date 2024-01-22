ddb9ad4fc168d8fbbaed329661adde74f4ab3989
EngoDev <shalev.goo@gmail.com>
2024-01-22 22:05:45 +0200

    Added modified on_err benches

--- benches/benches/bevy_ecs/world/mod.rs
+++ benches/benches/bevy_ecs/world/mod.rs
@@ -12,6 +12,7 @@ criterion_group!(
     world_benches,
     empty_commands,
     spawn_commands,
+    spawn_commands_error_handler,
     insert_commands,
     fake_commands,
     zero_sized_commands,
