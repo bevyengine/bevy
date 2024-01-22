b2c9a980bfc70d96d72289d86c7b9223482a4562
Joona Aalto <jondolf.dev@gmail.com>
2024-01-22 22:34:27 +0200

    Fix doc example

--- crates/bevy_render/src/mesh/primitives/mod.rs
+++ crates/bevy_render/src/mesh/primitives/mod.rs
@@ -7,15 +7,15 @@
 //! ```
 //! # use bevy_asset::Assets;
 //! # use bevy_ecs::prelude::ResMut;
-//! # use bevy_math::primitives;
+//! # use bevy_math::prelude::Circle;
 //! # use bevy_render::prelude::*;
 //! #
 //! # fn setup(mut meshes: ResMut<Assets<Mesh>>) {
 //! // Create circle mesh with default configuration
-//! let circle = meshes.add(primitives::Circle { radius: 25.0 });
+//! let circle = meshes.add(Circle { radius: 25.0 });
 //!
 //! // Specify number of vertices
-//! let circle = meshes.add(primitivesCircle { radius: 25.0 }.mesh().resolution(64));
+//! let circle = meshes.add(Circle { radius: 25.0 }.mesh().resolution(64));
 //! # }
 //! ```
 //!
