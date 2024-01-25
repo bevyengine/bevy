bb4982ea6fed9b68d8cbc23ad7525ce1732b322c
atlas dostal <rodol@rivalrebels.com>
2024-01-25 01:15:49 -0800

    Tweak camera

--- examples/3d/animated_material.rs
+++ examples/3d/animated_material.rs
@@ -16,7 +16,7 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn((
         Camera3dBundle {
             transform: Transform::from_xyz(3.0, 1.0, 3.0)
-                .looking_at(Vec3::new(0.0, 0.2, 0.0), Vec3::Y),
+                .looking_at(Vec3::new(0.0, -0.5, 0.0), Vec3::Y),
             ..default()
         },
         EnvironmentMapLight {
