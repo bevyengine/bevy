b9c3b224ed9ff749d8312fdaff8673965f5153e6
Hullo I'm Jay <jason@blunderboffins.com>
2024-01-25 14:48:03 -0500

    Renaming and cleaning up the UV channel selection test.

--- examples/3d/uv_channel_selection_test.rs
+++ examples/3d/uv_channel_selection_test.rs
@@ -1,5 +1,6 @@
 //! This example demonstrates the use of the second UV channel for mapping various texture properties.
 //! The spheres on the left use UV0 and those on the right use UV1 (which is just a scaled copy).
+//! The spheres on the top test base colour and metal-roughness. Those on the bottom test normals and occlusion.
 
 use std::f32::consts::PI;
 
@@ -34,15 +35,13 @@ fn setup(
     asset_server: Res<AssetServer>,
 ) {
     let image_colour = Some(images.add(uv_debug_texture()));
-    let image_bw = Some(images.add(bw_checkerboard_texture()));
     let image_normal = Some(images.add(grid_normal_map_texture()));
     let image_metallic_roughness = Some(images.add(metallic_roughness_uv_texture()));
     let image_occlusion = Some(images.add(grid_occlusion_map_texture()));
 
-    // Material using UV0 (default).
+    // Material using UV0 (default) for base colour, metallic/roughness.
     let debug_material_1 = materials.add(StandardMaterial {
         base_color_texture: image_colour.clone(),
-        base_color_texture_uv_channel: 0,
         metallic: 1.0,
         perceptual_roughness: 1.0,
         metallic_roughness_texture: image_metallic_roughness.clone(),
@@ -61,7 +60,7 @@ fn setup(
         ..default()
     });
 
-    // Normal and occlusion, UV0.
+    // Normal and occlusion, UV0 (default).
     let debug_material_3 = materials.add(StandardMaterial {
         normal_map_texture: image_normal.clone(),
         occlusion_texture: image_occlusion.clone(),
@@ -80,20 +79,17 @@ fn setup(
     let mut new_mesh = Mesh::from(shape::UVSphere::default());
     new_mesh.generate_tangents().expect("Failed to generate tangents. Oh my.");
 
-    // Attempt to copy UVs from the first channel
+    // Attempt to copy UVs from the first channel.
     if let Some(uv0) = new_mesh.attribute(Mesh::ATTRIBUTE_UV_0).and_then(|attr| attr.as_float2()) {
         let uv1: Vec<[f32; 2]> = uv0.iter().map(|&[u, v]| [u * SCALE_FACTOR, v * SCALE_FACTOR]).collect();
         new_mesh.insert_attribute(Mesh::ATTRIBUTE_UV_1, uv1);
     } else {
         eprintln!("Failed to copy UVs!");
-        eprintln!("Available attributes:");
-        for (attribute_id, _) in new_mesh.attributes() {
-            eprintln!(" - {:?}", attribute_id);
-        }
     }
 
     let new_mesh_handle = meshes.add(new_mesh);
 
+    // Upper-left sphere (material 1).
     commands.spawn((
         PbrBundle {
             mesh: new_mesh_handle.clone(),
@@ -109,6 +105,7 @@ fn setup(
         Shape,
     ));
 
+    // Upper-right sphere (material 2).
     commands.spawn((
         PbrBundle {
             mesh: new_mesh_handle.clone(),
@@ -124,6 +121,7 @@ fn setup(
         Shape,
     ));
 
+    // Lower-left sphere (material 3).
     commands.spawn((
         PbrBundle {
             mesh: new_mesh_handle.clone(),
@@ -139,6 +137,7 @@ fn setup(
         Shape,
     ));
 
+    // Lower-right sphere (material 3).
     commands.spawn((
         PbrBundle {
             mesh: new_mesh_handle.clone(),
@@ -184,7 +183,7 @@ fn rotate(mut query: Query<&mut Transform, With<Shape>>, time: Res<Time>) {
     }
 }
 
-/// Creates a colorful test pattern
+/// Creates a colourful test pattern. Stolen from 3D shapes example.
 fn uv_debug_texture() -> Image {
     const TEXTURE_SIZE: usize = 8;
 
@@ -213,36 +212,7 @@ fn uv_debug_texture() -> Image {
     )
 }
 
-fn bw_checkerboard_texture() -> Image {
-    const TEXTURE_SIZE: usize = 8;
-    const BLACK: [u8; 4] = [0, 0, 0, 255]; // RGBA for black
-    const WHITE: [u8; 4] = [255, 255, 255, 255]; // RGBA for white
-
-    let mut texture_data = [0; TEXTURE_SIZE * TEXTURE_SIZE * 4];
-    for y in 0..TEXTURE_SIZE {
-        for x in 0..TEXTURE_SIZE {
-            let offset = (y * TEXTURE_SIZE + x) * 4;
-            if (x + y) % 2 == 0 {
-                texture_data[offset..offset + 4].copy_from_slice(&WHITE);
-            } else {
-                texture_data[offset..offset + 4].copy_from_slice(&BLACK);
-            }
-        }
-    }
-
-    Image::new_fill(
-        Extent3d {
-            width: TEXTURE_SIZE as u32,
-            height: TEXTURE_SIZE as u32,
-            depth_or_array_layers: 1,
-        },
-        TextureDimension::D2,
-        &texture_data,
-        TextureFormat::Rgba8UnormSrgb,
-        RenderAssetPersistencePolicy::Unload,
-    )
-}
-
+/// Creates a pattern for testing metallic-roughness. It alternatives shiny metal and rough non-metal.
 fn metallic_roughness_uv_texture() -> Image {
     const TEXTURE_SIZE: usize = 8;
 
@@ -277,6 +247,7 @@ fn metallic_roughness_uv_texture() -> Image {
     )
 }
 
+/// Creates a test normal map, which is just grooves along a grid pattern.
 fn grid_normal_map_texture() -> Image {
     const TEXTURE_SIZE: usize = 512;
     const GRID_SIZE: usize = 8;
@@ -324,6 +295,7 @@ fn grid_normal_map_texture() -> Image {
     )
 }
 
+/// Creates an occlusion test pattern, which is just a grid of black lines aligning with the normal pattern above.
 fn grid_occlusion_map_texture() -> Image {
     const TEXTURE_SIZE: usize = 512;
     const GRID_SIZE: usize = 8;
