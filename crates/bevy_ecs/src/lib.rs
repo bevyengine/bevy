dcc34992cb4996b7066667a7d34f2ae2fafbc8a8
Lee-Orr <leeorr@gmail.com>
2024-01-26 13:05:50 -0500

    fmt fix

--- crates/bevy_ecs/src/lib.rs
+++ crates/bevy_ecs/src/lib.rs
@@ -43,8 +43,8 @@ pub mod prelude {
         schedule::{
             apply_deferred, apply_state_transition, common_conditions::*, ComputedStates,
             Condition, IntoSystemConfigs, IntoSystemSet, IntoSystemSetConfigs, NextState, OnEnter,
-            OnExit, OnTransition, Schedule, Schedules, State, StateSet, StateTransitionEvent,
-            States, SystemSet, StateTransition
+            OnExit, OnTransition, Schedule, Schedules, State, StateSet, StateTransition,
+            StateTransitionEvent, States, SystemSet,
         },
         system::{
             Commands, Deferred, In, IntoSystem, Local, NonSend, NonSendMut, ParallelCommands,
