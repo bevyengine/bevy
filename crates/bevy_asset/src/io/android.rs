58d120d65ee1a108ef4ebb630686f670a1eed256
Ensar Sarajčić <es.ensar@gmail.com>
2024-01-26 10:40:02 +0100

    Use `is_none` instead of `map_or`
    Co-authored-by: Kanabenki <lucien.menassol@gmail.com>

--- crates/bevy_asset/src/io/android.rs
+++ crates/bevy_asset/src/io/android.rs
@@ -106,7 +106,7 @@ impl AssetReader for AndroidAssetReader {
                 .ok_or(AssetReaderError::NotFound(path.to_path_buf()))?;
             Ok(asset_manager
                 .open(&CString::new(path.to_str().unwrap()).unwrap())
-                .map_or(true, |_| false))
+                .is_none()
         })
     }
 }
