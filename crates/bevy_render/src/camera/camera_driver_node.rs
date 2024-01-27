87b8798f7a9fb81ff5fe3abe4729db8de206dd53
JMS55 <47158642+JMS55@users.noreply.github.com>
2024-01-26 20:21:36 -0800

    Increase frame count hack

--- crates/bevy_render/src/camera/camera_driver_node.rs
+++ crates/bevy_render/src/camera/camera_driver_node.rs
@@ -58,14 +58,14 @@ impl Node for CameraDriverNode {
             }
         }
 
-        // For the first few frames, no pipelines have finished compiling, and so no work will have been sent to the GPU,
+        // For the first several frames, no pipelines have finished compiling, and so no work will have been sent to the GPU,
         // causing error messages on startup. Scheduling some dummy work bypasses this.
-        let first_few_frames = world.resource::<FrameCount>().0 <= 5;
+        let first_several_frames = world.resource::<FrameCount>().0 <= 100;
 
         // wgpu (and some backends) require doing work for swap chains if you call `get_current_texture()` and `present()`
         // This ensures that Bevy doesn't crash, even when there are no cameras (and therefore no work submitted).
         for (id, window) in world.resource::<ExtractedWindows>().iter() {
-            if camera_windows.contains(id) && !first_few_frames {
+            if camera_windows.contains(id) && !first_several_frames {
                 continue;
             }
 
