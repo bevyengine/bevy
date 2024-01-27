da4daef912a21ee2d5784363e3bbde080925c98e
NiseVoid <nisevoid@gmail.com>
2024-01-27 19:05:54 +0100

    Apply review suggestions

--- crates/bevy_math/src/bounding/raytest2d.rs
+++ crates/bevy_math/src/bounding/raytest2d.rs
@@ -5,33 +5,33 @@ use crate::{primitives::Direction2d, Ray2d, Vec2};
 pub struct RayTest2d {
     /// The ray for the test
     pub ray: Ray2d,
-    /// The maximum time of impact of the ray
+    /// The maximum distance for the ray
     pub max: f32,
     /// The multiplicative inverse direction of the ray
-    dir_recip: Vec2,
+    direction_recip: Vec2,
 }
 
 impl RayTest2d {
-    /// Construct a [`RayTest2d`] from an origin, [`Direction2d`] and max time of impact.
+    /// Construct a [`RayTest2d`] from an origin, [`Direction2d`] and max distance.
     pub fn new(origin: Vec2, direction: Direction2d, max: f32) -> Self {
         Self::from_ray(Ray2d { origin, direction }, max)
     }
 
-    /// Construct a [`RayTest2d`] from a [`Ray2d`] and max time of impact.
+    /// Construct a [`RayTest2d`] from a [`Ray2d`] and max distance.
     pub fn from_ray(ray: Ray2d, max: f32) -> Self {
         Self {
             ray,
-            dir_recip: ray.direction.recip(),
+            direction_recip: ray.direction.recip(),
             max,
         }
     }
 
-    /// Get the cached multiplicate inverse of the direction of the ray
-    pub fn dir_recip(&self) -> Vec2 {
-        self.dir_recip
+    /// Get the cached multiplicative inverse of the direction of the ray.
+    pub fn direction_recip(&self) -> Vec2 {
+        self.direction_recip
     }
 
-    /// Get the time of impact for an intersection with an [`Aabb2d`], if any.
+    /// Get the distance of an intersection with an [`Aabb2d`], if any.
     pub fn aabb_intersection_at(&self, aabb: &Aabb2d) -> Option<f32> {
         let (min_x, max_x) = if self.ray.direction.x.is_sign_positive() {
             (aabb.min.x, aabb.max.x)
@@ -44,13 +44,13 @@ impl RayTest2d {
             (aabb.max.y, aabb.min.y)
         };
 
-        // Calculate the minimum/maximum time for each based on how much the direction goes that
-        // way. These values van get arbitrarily large, or even become NaN, which is handled by the
+        // Calculate the minimum/maximum time for each axis based on how much the direction goes that
+        // way. These values can get arbitrarily large, or even become NaN, which is handled by the
         // min/max operations below
-        let tmin_x = (min_x - self.ray.origin.x) * self.dir_recip.x;
-        let tmin_y = (min_y - self.ray.origin.y) * self.dir_recip.y;
-        let tmax_x = (max_x - self.ray.origin.x) * self.dir_recip.x;
-        let tmax_y = (max_y - self.ray.origin.y) * self.dir_recip.y;
+        let tmin_x = (min_x - self.ray.origin.x) * self.direction_recip.x;
+        let tmin_y = (min_y - self.ray.origin.y) * self.direction_recip.y;
+        let tmax_x = (max_x - self.ray.origin.x) * self.direction_recip.x;
+        let tmax_y = (max_y - self.ray.origin.y) * self.direction_recip.y;
 
         // An axis that is not relevant to the ray direction will be NaN. When one of the arguments
         // to min/max is NaN, the other argument is used.
@@ -66,7 +66,7 @@ impl RayTest2d {
         }
     }
 
-    /// Get the time of impact for an intersection with a [`BoundingCircle`], if any.
+    /// Get the distance of an intersection with a [`BoundingCircle`], if any.
     pub fn circle_intersection_at(&self, sphere: &BoundingCircle) -> Option<f32> {
         let offset = self.ray.origin - sphere.center;
         let projected = offset.dot(*self.ray.direction);
--- crates/bevy_math/src/bounding/raytest3d.rs
+++ crates/bevy_math/src/bounding/raytest3d.rs
@@ -5,33 +5,33 @@ use crate::{primitives::Direction3d, Ray3d, Vec3};
 pub struct RayTest3d {
     /// The ray for the test
     pub ray: Ray3d,
-    /// The maximum time of impact of the ray
+    /// The maximum distance for the ray
     pub max: f32,
     /// The multiplicative inverse direction of the ray
-    dir_recip: Vec3,
+    direction_recip: Vec3,
 }
 
 impl RayTest3d {
-    /// Construct a [`RayTest3d`] from an origin, [`Direction3d`] and max time of impact.
+    /// Construct a [`RayTest3d`] from an origin, [`Direction3d`] and max distance.
     pub fn new(origin: Vec3, direction: Direction3d, max: f32) -> Self {
         Self::from_ray(Ray3d { origin, direction }, max)
     }
 
-    /// Construct a [`RayTest3d`] from a [`Ray3d`] and max time of impact.
+    /// Construct a [`RayTest3d`] from a [`Ray3d`] and max distance.
     pub fn from_ray(ray: Ray3d, max: f32) -> Self {
         Self {
             ray,
-            dir_recip: ray.direction.recip(),
+            direction_recip: ray.direction.recip(),
             max,
         }
     }
 
-    /// Get the cached multiplicate inverse of the direction of the ray
-    pub fn dir_recip(&self) -> Vec3 {
-        self.dir_recip
+    /// Get the cached multiplicative inverse of the direction of the ray.
+    pub fn direction_recip(&self) -> Vec3 {
+        self.direction_recip
     }
 
-    /// Get the time of impact for an intersection with an [`Aabb3d`], if any.
+    /// Get the distance of an intersection with an [`Aabb3d`], if any.
     pub fn aabb_intersection_at(&self, aabb: &Aabb3d) -> Option<f32> {
         let (min_x, max_x) = if self.ray.direction.x.is_sign_positive() {
             (aabb.min.x, aabb.max.x)
@@ -49,15 +49,15 @@ impl RayTest3d {
             (aabb.max.z, aabb.min.z)
         };
 
-        // Calculate the minimum/maximum time for each based on how much the direction goes that
-        // way. These values van get arbitrarily large, or even become NaN, which is handled by the
+        // Calculate the minimum/maximum time for each axis based on how much the direction goes that
+        // way. These values can get arbitrarily large, or even become NaN, which is handled by the
         // min/max operations below
-        let tmin_x = (min_x - self.ray.origin.x) * self.dir_recip.x;
-        let tmin_y = (min_y - self.ray.origin.y) * self.dir_recip.y;
-        let tmin_z = (min_z - self.ray.origin.z) * self.dir_recip.z;
-        let tmax_x = (max_x - self.ray.origin.x) * self.dir_recip.x;
-        let tmax_y = (max_y - self.ray.origin.y) * self.dir_recip.y;
-        let tmax_z = (max_z - self.ray.origin.z) * self.dir_recip.z;
+        let tmin_x = (min_x - self.ray.origin.x) * self.direction_recip.x;
+        let tmin_y = (min_y - self.ray.origin.y) * self.direction_recip.y;
+        let tmin_z = (min_z - self.ray.origin.z) * self.direction_recip.z;
+        let tmax_x = (max_x - self.ray.origin.x) * self.direction_recip.x;
+        let tmax_y = (max_y - self.ray.origin.y) * self.direction_recip.y;
+        let tmax_z = (max_z - self.ray.origin.z) * self.direction_recip.z;
 
         // An axis that is not relevant to the ray direction will be NaN. When one of the arguments
         // to min/max is NaN, the other argument is used.
@@ -73,7 +73,7 @@ impl RayTest3d {
         }
     }
 
-    /// Get the time of impact for an intersection with a [`BoundingSphere`], if any.
+    /// Get the distance of an intersection with a [`BoundingSphere`], if any.
     pub fn sphere_intersection_at(&self, sphere: &BoundingSphere) -> Option<f32> {
         let offset = self.ray.origin - sphere.center;
         let projected = offset.dot(*self.ray.direction);
