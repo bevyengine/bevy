8f4589baa1838965b1459e20a88ef21b12cdd970
Alice Cecile <alice.i.cecil@gmail.com>
2024-01-27 23:18:22 -0500

    Add basic example

---
+++ examples/ecs/send_and_receive_events.rs
@@ -0,0 +1,152 @@
+//! From time-to-time, you may find that you want to both send and receive and event of the same type in a single system.
+//!
+//! Of course, this results in an error: the borrows of [`EventWriter`] and [`EventReader`] overlap,
+//! if and only if the [`Event`] type is the same.
+//! One system parameter borrows the [`Events`] resource mutably, and another system parameter borrows the [`Events`] resource immutably immutably.
+//! If Bevy allowed this, there would be Undefined Behavior!
+//!
+//! There are two ways to solve this problem:
+//!
+//! 1. Use [`ParamSet`] to check out the [`EventWriter`] and [`EventReader`] one at a time.
+//! 2. Use a [`Local`] [`ManualEventReader`] instead of an [`EventReader`], and use [`ResMut`] to access [`Events`].
+//!
+//! In the first case, you're being careful to only check out one of the [`EventWriter`] or [`EventReader`] at a time.
+//! By "temporally" seperating them, you avoid the overlap.
+//!
+//! In the second case, you only ever have one access to the underlying  [`Events`] resource at a time.
+//! But in exchange, you have to manually keep track of which events you've already read.
+//!
+//! Let's look at an example of each.
+
+use bevy::core::FrameCount;
+use bevy::ecs::event::ManualEventReader;
+use bevy::prelude::*;
+
+fn main() {
+    let mut app = App::new();
+    app.add_plugins(MinimalPlugins)
+        .add_event::<MyEvent>()
+        .add_systems(
+            Update,
+            (
+                send_events,
+                debug_events,
+                send_and_receive_param_set,
+                debug_events,
+                send_and_receive_local_event_reader,
+                debug_events,
+            )
+                .chain(),
+        );
+    // We're just going to run a few frames, so we can see and understand the output.
+    app.update();
+    // By running for longer than one frame, we can see that we're caching our cursor in the event queue properly.
+    app.update();
+}
+
+/// A dummy event type.
+#[derive(Debug, Clone, Event)]
+struct MyEvent {
+    re_emit_from_param_set: bool,
+    re_emit_from_local_event_reader: bool,
+    times_sent: u8,
+}
+
+/// A system that sends all combinations of events.
+fn send_events(mut events: EventWriter<MyEvent>, frame_count: Res<FrameCount>) {
+    info!("Sending events for frame {:?}", *frame_count);
+
+    events.send(MyEvent {
+        re_emit_from_param_set: false,
+        re_emit_from_local_event_reader: false,
+        times_sent: 1,
+    });
+    events.send(MyEvent {
+        re_emit_from_param_set: true,
+        re_emit_from_local_event_reader: false,
+        times_sent: 1,
+    });
+    events.send(MyEvent {
+        re_emit_from_param_set: false,
+        re_emit_from_local_event_reader: true,
+        times_sent: 1,
+    });
+    events.send(MyEvent {
+        re_emit_from_param_set: true,
+        re_emit_from_local_event_reader: true,
+        times_sent: 1,
+    });
+}
+
+/// A system that prints all events sent since the last time this system ran.
+///
+/// Note that some events will be printed twice, because they were sent twice.
+fn debug_events(mut events: EventReader<MyEvent>) {
+    for event in events.read() {
+        println!("{:?}", event);
+    }
+}
+
+/// A system that both sends and receives events using [`ParamSet`].
+fn send_and_receive_param_set(
+    // Note: we can make this even prettier using the `SystemParam` derive macro
+    // to make our own `SystemParam` type to put inside of the `ParamSet`.
+    mut param_set: ParamSet<(EventReader<MyEvent>, EventWriter<MyEvent>)>,
+    frame_count: Res<FrameCount>,
+) {
+    info!("Sending and receiving events for frame {:?}", *frame_count);
+
+    // We must collect the events to re-emit, because we can't access the writer while we're iterating over the reader.
+    let mut events_to_re_emit = Vec::new();
+
+    // This is p0, as the first parameter in the `ParamSet` is the reader.
+    for event in param_set.p0().read() {
+        if event.re_emit_from_param_set {
+            events_to_re_emit.push(event.clone());
+        }
+    }
+
+    // This is p1, as the second parameter in the `ParamSet` is the writer.
+    for event in events_to_re_emit {
+        param_set.p1().send(MyEvent {
+            times_sent: event.times_sent + 1,
+            // This is struct update syntax! Here, we're copying all of the fields from `event`,
+            // except for the `times_sent` field, which we're incrementing.
+            ..event
+        });
+    }
+}
+
+/// A system that both sends and receives events using a [`Local`] [`ManualEventReader`].
+fn send_and_receive_local_event_reader(
+    // The Local SystemParam stores state inside the system itself, rather than in the world.
+    // ManualEventReader<T> is the internal state of EventReader<T>, which tracks which events have been seen.
+    mut local_event_reader: Local<ManualEventReader<MyEvent>>,
+    // We can access the `Events` resource mutably, allowing us to both read and write its contents.
+    mut events: ResMut<Events<MyEvent>>,
+    frame_count: Res<FrameCount>,
+) {
+    info!("Sending and receiving events for frame {:?}", *frame_count);
+
+    // We must collect the events to re-emit, because we can't mutate events while we're iterating over the events.
+    let mut events_to_re_emit = Vec::new();
+
+    for event in local_event_reader.read(&events) {
+        if event.re_emit_from_local_event_reader {
+            // For simplicity, we're cloning the event.
+            // In this case, since we have mutable access to the `Events` resource,
+            // we could also just mutate the event in-place,
+            // or drain the event queue into our `events_to_re_emit` vector.
+            events_to_re_emit.push(event.clone());
+        }
+    }
+
+    for event in events_to_re_emit {
+        events.send(MyEvent {
+            times_sent: event.times_sent + 1,
+            // This is struct update syntax! Here, we're copying all of the fields from `event`,
+            // except for the `times_sent` field, which we're incrementing.
+            ..event
+        });
+    }
+}
