df761af49b0c414be6f8da071674e89ce9925680
radiish <cb.setho@gmail.com>
2024-01-30 14:39:01 +0000

    reflection: replace `impl_reflect_struct` with `impl_reflect` (#11437)
    # Objective
    
    - `impl_reflect_struct` doesn't cover tuple structs or enums.
    - Problem brought up [on
    Discord](https://discord.com/channels/691052431525675048/1002362493634629796/1190623345817960463).
    
    ## Solution
    
    - Replaces `impl_reflect_struct` with the new `impl_reflect` which works
    for tuple structs and enums too.
    
    ---
    
    ## Changelog
    
    - Internally in `bevy_reflect_derive`, we have a new `ReflectProvenance`
    type which is composed of `ReflectTraitToImpl` and `ReflectSource`.
    - `impl_reflect_struct` is gone and totally superseded by
    `impl_reflect`.
    
    ---------
    
    Co-authored-by: Gino Valente <49806985+MrGVSV@users.noreply.github.com>

--- crates/bevy_reflect/bevy_reflect_derive/src/derive_data.rs
+++ crates/bevy_reflect/bevy_reflect_derive/src/derive_data.rs
@@ -1,3 +1,5 @@
+use core::fmt;
+
 use crate::container_attributes::{FromReflectAttrs, ReflectTraits, TypePathAttrs};
 use crate::field_attributes::{parse_field_attrs, ReflectFieldAttr};
 use crate::type_path::parse_path_no_leading_colon;
@@ -140,10 +142,46 @@ enum ReflectMode {
     Value,
 }
 
+/// How the macro was invoked.
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+pub(crate) enum ReflectImplSource {
+    ImplRemoteType,
+    DeriveLocalType,
+}
+
+/// Which trait the macro explicitly implements.
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+pub(crate) enum ReflectTraitToImpl {
+    Reflect,
+    FromReflect,
+    TypePath,
+}
+
+/// The provenance of a macro invocation.
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+pub(crate) struct ReflectProvenance {
+    pub source: ReflectImplSource,
+    pub trait_: ReflectTraitToImpl,
+}
+
+impl fmt::Display for ReflectProvenance {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        use self::{ReflectImplSource as S, ReflectTraitToImpl as T};
+        let str = match (self.source, self.trait_) {
+            (S::ImplRemoteType, T::Reflect) => "`impl_reflect`",
+            (S::DeriveLocalType, T::Reflect) => "`#[derive(Reflect)]`",
+            (S::DeriveLocalType, T::FromReflect) => "`#[derive(FromReflect)]`",
+            (S::DeriveLocalType, T::TypePath) => "`#[derive(TypePath)]`",
+            (S::ImplRemoteType, T::FromReflect | T::TypePath) => unreachable!(),
+        };
+        f.write_str(str)
+    }
+}
+
 impl<'a> ReflectDerive<'a> {
     pub fn from_input(
         input: &'a DeriveInput,
-        is_from_reflect_derive: bool,
+        provenance: ReflectProvenance,
     ) -> Result<Self, syn::Error> {
         let mut traits = ReflectTraits::default();
         // Should indicate whether `#[reflect_value]` was used.
@@ -167,8 +205,7 @@ impl<'a> ReflectDerive<'a> {
                     }
 
                     reflect_mode = Some(ReflectMode::Normal);
-                    let new_traits =
-                        ReflectTraits::from_meta_list(meta_list, is_from_reflect_derive)?;
+                    let new_traits = ReflectTraits::from_meta_list(meta_list, provenance.trait_)?;
                     traits.merge(new_traits)?;
                 }
                 Meta::List(meta_list) if meta_list.path.is_ident(REFLECT_VALUE_ATTRIBUTE_NAME) => {
@@ -180,8 +217,7 @@ impl<'a> ReflectDerive<'a> {
                     }
 
                     reflect_mode = Some(ReflectMode::Value);
-                    let new_traits =
-                        ReflectTraits::from_meta_list(meta_list, is_from_reflect_derive)?;
+                    let new_traits = ReflectTraits::from_meta_list(meta_list, provenance.trait_)?;
                     traits.merge(new_traits)?;
                 }
                 Meta::Path(path) if path.is_ident(REFLECT_VALUE_ATTRIBUTE_NAME) => {
@@ -259,6 +295,16 @@ impl<'a> ReflectDerive<'a> {
         };
 
         let meta = ReflectMeta::new(type_path, traits);
+
+        if provenance.source == ReflectImplSource::ImplRemoteType
+            && meta.type_path_attrs().should_auto_derive()
+            && !meta.type_path().has_custom_path()
+        {
+            return Err(syn::Error::new(
+                meta.type_path().span(),
+                format!("a #[{TYPE_PATH_ATTRIBUTE_NAME} = \"...\"] attribute must be specified when using {provenance}")
+            ));
+        }
 
         #[cfg(feature = "documentation")]
         let meta = meta.with_docs(doc);
