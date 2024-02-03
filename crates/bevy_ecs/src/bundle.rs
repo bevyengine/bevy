21aa5fe2b68b8f87e3cfcdff467ce1a2ecdf6c78
SpecificProtagonist <specificprotagonist@posteo.org>
2024-02-04 00:47:04 +0100

    Use TypeIdMap whenever possible (#11684)
    Use `TypeIdMap<T>` instead of `HashMap<TypeId, T>`
    
    - ~~`TypeIdMap` was in `bevy_ecs`. I've kept it there because of
    #11478~~
    - ~~I haven't swapped `bevy_reflect` over because it doesn't depend on
    `bevy_ecs`, but I'd also be happy with moving `TypeIdMap` to
    `bevy_utils` and then adding a dependency to that~~
    - ~~this is a slight change in the public API of
    `DrawFunctionsInternal`, does this need to go in the changelog?~~
    
    ## Changelog
    - moved `TypeIdMap` to `bevy_utils`
    - changed `DrawFunctionsInternal::indices` to `TypeIdMap`
    
    ## Migration Guide
    
    - `TypeIdMap` now lives in `bevy_utils`
    - `DrawFunctionsInternal::indices` now uses a `TypeIdMap`.
    
    ---------
    
    Co-authored-by: Alice Cecile <alice.i.cecile@gmail.com>

--- crates/bevy_ecs/src/bundle.rs
+++ crates/bevy_ecs/src/bundle.rs
@@ -3,7 +3,7 @@
 //! This module contains the [`Bundle`] trait and some other helper types.
 
 pub use bevy_ecs_macros::Bundle;
-use bevy_utils::{HashMap, HashSet};
+use bevy_utils::{HashMap, HashSet, TypeIdMap};
 
 use crate::{
     archetype::{
@@ -14,7 +14,6 @@ use crate::{
     entity::{Entities, Entity, EntityLocation},
     query::DebugCheckedUnwrap,
     storage::{SparseSetIndex, SparseSets, Storages, Table, TableRow},
-    TypeIdMap,
 };
 use bevy_ptr::OwningPtr;
 use bevy_utils::all_tuples;
