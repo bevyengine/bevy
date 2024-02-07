55368a1446e5e72018a6b387f0be1eb32498734d
Sjael <jakeobrien44@gmail.com>
2024-02-06 16:29:09 -0800

    cleaned up example

--- examples/ui/ui_opacity.rs
+++ examples/ui/ui_opacity.rs
@@ -14,27 +14,29 @@ fn setup(mut commands: Commands) {
     // Camera
     commands.spawn(Camera2dBundle::default());
 
+    let text_style = TextStyle {
+        font_size: 30.0,
+        color: Color::WHITE,
+        ..Default::default()
+    };
     commands
         .spawn(NodeBundle {
             style: Style {
                 width: Val::Percent(100.0),
                 height: Val::Percent(100.0),
                 flex_direction: FlexDirection::Column,
-                justify_content: JustifyContent::Center,
+                padding: UiRect::all(Val::Px(18.)),
+                row_gap: Val::Px(10.),
                 ..default()
             },
             ..default()
         })
         .with_children(|parent| {
+            // 100% Opacity section
             parent
                 .spawn(NodeBundle {
                     style: Style {
                         flex_direction: FlexDirection::Column,
-                        justify_content: JustifyContent::FlexStart,
-                        margin: UiRect {
-                            bottom: Val::Px(10.),
-                            ..default()
-                        },
                         row_gap: Val::Px(10.),
                         padding: UiRect::all(Val::Px(18.)),
                         ..default()
@@ -45,41 +47,24 @@ fn setup(mut commands: Commands) {
                 })
                 .with_children(|parent| {
                     parent.spawn(TextBundle {
-                        text: Text::from_section(
-                            "Fully Opaque (100% black bg)",
-                            TextStyle {
-                                font_size: 30.0,
-                                color: Color::WHITE,
-                                ..Default::default()
-                            },
-                        ),
+                        text: Text::from_section("Fully Opaque (100% black bg)", text_style.clone()),
                         ..default()
                     });
                     parent.spawn(NodeBundle {
                         style: Style {
-                            flex_direction: FlexDirection::Column,
-                            justify_content: JustifyContent::FlexStart,
-                            margin: UiRect {
-                                bottom: Val::Px(10.),
-                                ..default()
-                            },
-                            row_gap: Val::Px(10.),
-                            padding: UiRect::all(Val::Px(18.)),
+                            height: Val::Px(36.),
                             ..default()
                         },
                         background_color: Color::GREEN.into(),
                         ..default()
                     });
                 });
+
+            // 50% Opacity section
             parent
                 .spawn(NodeBundle {
                     style: Style {
                         flex_direction: FlexDirection::Column,
-                        justify_content: JustifyContent::FlexStart,
-                        margin: UiRect {
-                            bottom: Val::Px(10.),
-                            ..default()
-                        },
                         row_gap: Val::Px(10.),
                         padding: UiRect::all(Val::Px(18.)),
                         ..default()
@@ -90,41 +75,24 @@ fn setup(mut commands: Commands) {
                 })
                 .with_children(|parent| {
                     parent.spawn(TextBundle {
-                        text: Text::from_section(
-                            "Half Opaque (50%)",
-                            TextStyle {
-                                font_size: 30.0,
-                                color: Color::WHITE,
-                                ..Default::default()
-                            },
-                        ),
+                        text: Text::from_section("Half Opaque (50%)", text_style.clone()),
                         ..default()
                     });
                     parent.spawn(NodeBundle {
                         style: Style {
-                            flex_direction: FlexDirection::Column,
-                            justify_content: JustifyContent::FlexStart,
-                            margin: UiRect {
-                                bottom: Val::Px(10.),
-                                ..default()
-                            },
-                            row_gap: Val::Px(10.),
-                            padding: UiRect::all(Val::Px(18.)),
+                            height: Val::Px(36.),
                             ..default()
                         },
                         background_color: Color::GREEN.into(),
                         ..default()
                     });
                 });
+
+            // Nested 50% Opacity section
             parent
                 .spawn(NodeBundle {
                     style: Style {
                         flex_direction: FlexDirection::Column,
-                        justify_content: JustifyContent::FlexStart,
-                        margin: UiRect {
-                            bottom: Val::Px(10.),
-                            ..default()
-                        },
                         row_gap: Val::Px(10.),
                         padding: UiRect::all(Val::Px(18.)),
                         ..default()
@@ -135,40 +103,22 @@ fn setup(mut commands: Commands) {
                 })
                 .with_children(|parent| {
                     parent.spawn(TextBundle {
-                        text: Text::from_section(
-                            "Half Opaque (50%)",
-                            TextStyle {
-                                font_size: 30.0,
-                                color: Color::WHITE,
-                                ..Default::default()
-                            },
-                        ),
+                        text: Text::from_section("Half Opaque (50%)", text_style.clone()),
                         ..default()
                     });
                     parent.spawn(NodeBundle {
                         style: Style {
-                            flex_direction: FlexDirection::Column,
-                            justify_content: JustifyContent::FlexStart,
-                            margin: UiRect {
-                                bottom: Val::Px(10.),
-                                ..default()
-                            },
-                            row_gap: Val::Px(10.),
-                            padding: UiRect::all(Val::Px(18.)),
+                            height: Val::Px(36.),
                             ..default()
                         },
                         background_color: Color::RED.into(),
                         ..default()
                     });
+                    // Child 1
                     parent
                         .spawn(NodeBundle {
                             style: Style {
                                 flex_direction: FlexDirection::Column,
-                                justify_content: JustifyContent::FlexStart,
-                                margin: UiRect {
-                                    bottom: Val::Px(10.),
-                                    ..default()
-                                },
                                 row_gap: Val::Px(10.),
                                 padding: UiRect::all(Val::Px(18.)),
                                 ..default()
@@ -181,39 +131,24 @@ fn setup(mut commands: Commands) {
                             parent.spawn(TextBundle {
                                 text: Text::from_section(
                                     "Half Opaque (75% black bg over 2 layers)",
-                                    TextStyle {
-                                        font_size: 30.0,
-                                        color: Color::WHITE,
-                                        ..Default::default()
-                                    },
+                                    text_style.clone(),
                                 ),
                                 ..default()
                             });
                             parent.spawn(NodeBundle {
                                 style: Style {
-                                    flex_direction: FlexDirection::Column,
-                                    justify_content: JustifyContent::FlexStart,
-                                    margin: UiRect {
-                                        bottom: Val::Px(10.),
-                                        ..default()
-                                    },
-                                    row_gap: Val::Px(10.),
-                                    padding: UiRect::all(Val::Px(18.)),
+                                    height: Val::Px(36.),
                                     ..default()
                                 },
                                 background_color: Color::PURPLE.into(),
                                 ..default()
                             });
                         });
+                    // Child 2
                     parent
                         .spawn(NodeBundle {
                             style: Style {
                                 flex_direction: FlexDirection::Column,
-                                justify_content: JustifyContent::FlexStart,
-                                margin: UiRect {
-                                    bottom: Val::Px(10.),
-                                    ..default()
-                                },
                                 row_gap: Val::Px(10.),
                                 padding: UiRect::all(Val::Px(18.)),
                                 ..default()
@@ -226,24 +161,13 @@ fn setup(mut commands: Commands) {
                             parent.spawn(TextBundle {
                                 text: Text::from_section(
                                     "Quarter Opaque (62.5% over 2 layers)",
-                                    TextStyle {
-                                        font_size: 30.0,
-                                        color: Color::WHITE,
-                                        ..Default::default()
-                                    },
+                                    text_style.clone(),
                                 ),
                                 ..default()
                             });
                             parent.spawn(NodeBundle {
                                 style: Style {
-                                    flex_direction: FlexDirection::Column,
-                                    justify_content: JustifyContent::FlexStart,
-                                    margin: UiRect {
-                                        bottom: Val::Px(10.),
-                                        ..default()
-                                    },
-                                    row_gap: Val::Px(10.),
-                                    padding: UiRect::all(Val::Px(18.)),
+                                    height: Val::Px(36.),
                                     ..default()
                                 },
                                 background_color: Color::BLUE.into(),
