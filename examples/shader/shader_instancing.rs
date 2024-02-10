0d3c60da05fca255c3827b02d21bd1304d26fc99
Patrick Walton <pcwalton@mimiga.net>
2024-02-09 17:05:27 -0800

    Rustfmt police

--- examples/shader/shader_instancing.rs
+++ examples/shader/shader_instancing.rs
@@ -259,12 +259,7 @@ impl<P: PhaseItem> RenderCommand<P> for DrawMeshInstanced {
         };
 
         pass.set_vertex_buffer(0, gpu_mesh.vertex_buffer.slice(..));
-        pass.set_vertex_buffer(
-            1,
-            instance_buffer
-                .buffer
-                .slice(..),
-        );
+        pass.set_vertex_buffer(1, instance_buffer.buffer.slice(..));
 
         match &gpu_mesh.buffer_info {
             GpuBufferInfo::Indexed {
