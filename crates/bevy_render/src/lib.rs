5e82f2d28e8a92fd38b58890bf6dbaaa003df0eb
Rob Parrett <robparrett@gmail.com>
2024-02-15 12:24:04 -0700

    Fix missed rename

--- crates/bevy_render/src/lib.rs
+++ crates/bevy_render/src/lib.rs
@@ -87,7 +87,7 @@ pub struct RenderPlugin {
     pub render_creation: RenderCreation,
     /// If `true`, disables asynchronous pipeline compilation.
     /// This has no effect on macOS, Wasm, or without the `multi-threaded` feature.
-    pub sync_pipelines: bool,
+    pub synchronous_pipeline_compilation: bool,
 }
 
 /// The labels of the default App rendering sets.
@@ -358,7 +358,10 @@ impl Plugin for RenderPlugin {
 
             render_app
                 .insert_resource(instance)
-                .insert_resource(PipelineCache::new(device.clone(), self.sync_pipelines))
+                .insert_resource(PipelineCache::new(
+                    device.clone(),
+                    self.synchronous_pipeline_compilation,
+                ))
                 .insert_resource(device)
                 .insert_resource(queue)
                 .insert_resource(render_adapter)
