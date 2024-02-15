02aea99bd7728c4b49aac4d3ae3e259d94fcad43
Rob Parrett <robparrett@gmail.com>
2024-02-15 12:21:40 -0700

    Update crates/bevy_render/src/lib.rs
    Co-authored-by: François <mockersf@gmail.com>

--- crates/bevy_render/src/lib.rs
+++ crates/bevy_render/src/lib.rs
@@ -86,7 +86,7 @@ use std::{
 pub struct RenderPlugin {
     pub render_creation: RenderCreation,
     /// If `true`, disables asynchronous pipeline compilation.
-    /// This has no effect on macOS, WASM, or without the `multi-threaded` feature.
+    /// This has no effect on macOS, Wasm, or without the `multi-threaded` feature.
     pub sync_pipelines: bool,
 }
 
