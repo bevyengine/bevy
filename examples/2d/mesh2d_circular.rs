794a611fa8d7d2b742fa9aed1528a144d1ec74e2
Alexis "spectria" Horizon <spectria.limina@gmail.com>
2024-02-16 16:05:23 -0500

    Add tests to bounding shapes for CircularSector.
    Also draw bounding boxes on the mesh example to visually confirm
    correctness.

--- examples/2d/mesh2d_circular.rs
+++ examples/2d/mesh2d_circular.rs
@@ -1,5 +1,6 @@
 //! Demonstrates UV mappings of the [`CircularSector`] and [`CircularSegment`] primitives.
-
+//!
+//! Also draws the bounding boxes and circles of the primitives.
 use std::f32::consts::PI;
 
 use bevy::{
@@ -7,13 +8,24 @@ use bevy::{
     render::mesh::{CircularMeshUvMode, CircularSectorMeshBuilder, CircularSegmentMeshBuilder},
     sprite::MaterialMesh2dBundle,
 };
+use bevy_internal::math::bounding::{Bounded2d, BoundingVolume};
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, setup)
+        .add_systems(
+            Update,
+            (
+                draw_bounds::<CircularSector>,
+                draw_bounds::<CircularSegment>,
+            ),
+        )
         .run();
 }
+
+#[derive(Component, Debug)]
+struct DrawBounds<Shape: Bounded2d + Send + Sync + 'static>(Shape);
 
 fn setup(
     mut commands: Commands,
@@ -47,19 +59,22 @@ fn setup(
         // We must rotate it both in the Transform and in the mesh's UV mappings.
         let sector_angle = -sector.half_angle();
         let sector_mesh =
-            CircularSectorMeshBuilder::new(sector).uv_mode(CircularMeshUvMode::Mask {
+            CircularSectorMeshBuilder::new(sector.clone()).uv_mode(CircularMeshUvMode::Mask {
                 angle: sector_angle,
             });
-        commands.spawn(MaterialMesh2dBundle {
-            mesh: meshes.add(sector_mesh).into(),
-            material: material.clone(),
-            transform: Transform {
-                translation: Vec3::new(FIRST_X + OFFSET * i as f32, 2.0 * UPPER_Y, 0.0),
-                rotation: Quat::from_rotation_z(sector_angle),
+        commands.spawn((
+            MaterialMesh2dBundle {
+                mesh: meshes.add(sector_mesh).into(),
+                material: material.clone(),
+                transform: Transform {
+                    translation: Vec3::new(FIRST_X + OFFSET * i as f32, 2.0 * UPPER_Y, 0.0),
+                    rotation: Quat::from_rotation_z(sector_angle),
+                    ..default()
+                },
                 ..default()
             },
-            ..default()
-        });
+            DrawBounds(sector),
+        ));
 
         let segment = CircularSegment::from_fraction(40.0, fraction);
         // For the circular segment, we will draw Bevy charging forward, which requires rotating the
@@ -71,18 +86,43 @@ fn setup(
         // so it is the negative of what you might otherwise expect.
         let segment_angle = -PI / 2.0;
         let segment_mesh =
-            CircularSegmentMeshBuilder::new(segment).uv_mode(CircularMeshUvMode::Mask {
+            CircularSegmentMeshBuilder::new(segment.clone()).uv_mode(CircularMeshUvMode::Mask {
                 angle: -segment_angle,
             });
-        commands.spawn(MaterialMesh2dBundle {
-            mesh: meshes.add(segment_mesh).into(),
-            material: material.clone(),
-            transform: Transform {
-                translation: Vec3::new(FIRST_X + OFFSET * i as f32, LOWER_Y, 0.0),
-                rotation: Quat::from_rotation_z(segment_angle),
+        commands.spawn((
+            MaterialMesh2dBundle {
+                mesh: meshes.add(segment_mesh).into(),
+                material: material.clone(),
+                transform: Transform {
+                    translation: Vec3::new(FIRST_X + OFFSET * i as f32, LOWER_Y, 0.0),
+                    rotation: Quat::from_rotation_z(segment_angle),
+                    ..default()
+                },
                 ..default()
             },
-            ..default()
-        });
+            DrawBounds(segment),
+        ));
+    }
+}
+
+fn draw_bounds<Shape: Bounded2d + Send + Sync + 'static>(
+    q: Query<(&DrawBounds<Shape>, &GlobalTransform)>,
+    mut gizmos: Gizmos,
+) {
+    for (shape, transform) in &q {
+        let (_, rotation, translation) = transform.to_scale_rotation_translation();
+        let translation = translation.truncate();
+        let rotation = rotation.to_euler(EulerRot::XYZ).2;
+
+        let aabb = shape.0.aabb_2d(translation, rotation);
+        gizmos.rect_2d(aabb.center(), 0.0, aabb.half_size() * 2.0, Color::RED);
+
+        let bounding_circle = shape.0.bounding_circle(translation, rotation);
+        dbg!(bounding_circle);
+        gizmos.circle_2d(
+            bounding_circle.center,
+            bounding_circle.radius(),
+            Color::BLUE,
+        );
     }
 }
