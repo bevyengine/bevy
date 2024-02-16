c6b3c07ea5af6a09f3e34c4d0064fa49c4cc376e
pablo-lua <lpablo1eu@gmail.com>
2024-02-16 01:28:27 -0300

    [add] Added new arrow to the gizmos 2d example

--- examples/2d/2d_gizmos.rs
+++ examples/2d/2d_gizmos.rs
@@ -128,6 +128,14 @@ fn draw_example_collection(
         Vec2::from_angle(sin / -10. + PI / 2.) * 50.,
         Color::YELLOW,
     );
+
+    {
+        // We can create more complex arrows using the arrow builder.
+        let mut arrow_builder =
+            gizmos.arrow_2d(Vec2::ZERO, Vec2::from_angle(sin / -10.) * 50., Color::GREEN);
+        arrow_builder.with_double_end();
+        arrow_builder.with_tip_length(10.);
+    };
 }
 
 fn draw_primitives(
