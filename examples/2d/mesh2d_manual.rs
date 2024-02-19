02917b0169779469402af22305ec6e00bfedf42f
james7132 <contact@jamessliu.com>
2024-02-19 12:55:24 -0800

    Fix up examples

--- examples/2d/mesh2d_manual.rs
+++ examples/2d/mesh2d_manual.rs
@@ -355,7 +355,7 @@ pub fn queue_colored_mesh2d(
         return;
     }
     // Iterate each view (a camera is a view)
-    for (visible_entities, mut transparent_phase, view) in &mut views {
+    for (visible_entities, transparent_phase, view) in &views {
         let draw_colored_mesh2d = transparent_draw_functions.read().id::<DrawColoredMesh2d>();
 
         let mesh_key = Mesh2dPipelineKey::from_msaa_samples(msaa.samples())
