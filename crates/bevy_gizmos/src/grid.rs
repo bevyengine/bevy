84819b67b88a898dde3650566a0043402a4fd240
Lynn Büttgenbach <62256001+solis-lumine-vorago@users.noreply.github.com>
2024-02-20 02:20:31 +0100

    Fix doc examples (again)

--- crates/bevy_gizmos/src/grid.rs
+++ crates/bevy_gizmos/src/grid.rs
@@ -130,7 +130,8 @@ impl<'w, 's, T: GizmoConfigGroup> Gizmos<'w, 's, T> {
     ///     gizmos.grid(
     ///         Vec3::ZERO,
     ///         Quat::IDENTITY,
-    ///         Uvec2::new(10, 10),
+    ///         UVec2::new(10, 10),
+    ///         Vec2::splat(2.),
     ///         Color::GREEN
     ///         )
     ///         .skew_x(0.25)
