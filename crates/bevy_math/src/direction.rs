63a4353aa55d17239db22af2258d791c664f60ec
Joona Aalto <jondolf.dev@gmail.com>
2024-02-20 23:51:51 +0200

    Add `Dir3A`

--- crates/bevy_math/src/direction.rs
+++ crates/bevy_math/src/direction.rs
@@ -292,6 +292,151 @@ impl approx::UlpsEq for Dir3 {
     }
 }
 
+/// A normalized SIMD vector pointing in a direction in 3D space.
+///
+/// This type stores a 16 byte aligned [`Vec3A`].
+#[derive(Clone, Copy, Debug, PartialEq)]
+#[cfg_attr(feature = "serialize", derive(serde::Serialize, serde::Deserialize))]
+pub struct Dir3A(Vec3A);
+impl Primitive3d for Dir3A {}
+
+impl Dir3A {
+    /// A unit vector pointing along the positive X axis.
+    pub const X: Self = Self(Vec3A::X);
+    /// A unit vector pointing along the positive Y axis.
+    pub const Y: Self = Self(Vec3A::Y);
+    /// A unit vector pointing along the positive Z axis.
+    pub const Z: Self = Self(Vec3A::Z);
+    /// A unit vector pointing along the negative X axis.
+    pub const NEG_X: Self = Self(Vec3A::NEG_X);
+    /// A unit vector pointing along the negative Y axis.
+    pub const NEG_Y: Self = Self(Vec3A::NEG_Y);
+    /// A unit vector pointing along the negative Z axis.
+    pub const NEG_Z: Self = Self(Vec3A::NEG_Z);
+
+    /// Create a direction from a finite, nonzero [`Vec3A`].
+    ///
+    /// Returns [`Err(InvalidDirectionError)`](InvalidDirectionError) if the length
+    /// of the given vector is zero (or very close to zero), infinite, or `NaN`.
+    pub fn new(value: Vec3A) -> Result<Self, InvalidDirectionError> {
+        Self::new_and_length(value).map(|(dir, _)| dir)
+    }
+
+    /// Create a [`Dir3A`] from a [`Vec3A`] that is already normalized.
+    ///
+    /// # Warning
+    ///
+    /// `value` must be normalized, i.e it's length must be `1.0`.
+    pub fn new_unchecked(value: Vec3A) -> Self {
+        debug_assert!(value.is_normalized());
+
+        Self(value)
+    }
+
+    /// Create a direction from a finite, nonzero [`Vec3A`], also returning its original length.
+    ///
+    /// Returns [`Err(InvalidDirectionError)`](InvalidDirectionError) if the length
+    /// of the given vector is zero (or very close to zero), infinite, or `NaN`.
+    pub fn new_and_length(value: Vec3A) -> Result<(Self, f32), InvalidDirectionError> {
+        let length = value.length();
+        let direction = (length.is_finite() && length > 0.0).then_some(value / length);
+
+        direction
+            .map(|dir| (Self(dir), length))
+            .ok_or(InvalidDirectionError::from_length(length))
+    }
+
+    /// Create a direction from its `x`, `y`, and `z` components.
+    ///
+    /// Returns [`Err(InvalidDirectionError)`](InvalidDirectionError) if the length
+    /// of the vector formed by the components is zero (or very close to zero), infinite, or `NaN`.
+    pub fn from_xyz(x: f32, y: f32, z: f32) -> Result<Self, InvalidDirectionError> {
+        Self::new(Vec3A::new(x, y, z))
+    }
+}
+
+impl TryFrom<Vec3A> for Dir3A {
+    type Error = InvalidDirectionError;
+
+    fn try_from(value: Vec3A) -> Result<Self, Self::Error> {
+        Self::new(value)
+    }
+}
+
+impl From<Dir3A> for Vec3A {
+    fn from(value: Dir3A) -> Self {
+        value.0
+    }
+}
+
+impl std::ops::Deref for Dir3A {
+    type Target = Vec3A;
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl std::ops::Neg for Dir3A {
+    type Output = Self;
+    fn neg(self) -> Self::Output {
+        Self(-self.0)
+    }
+}
+
+impl std::ops::Mul<f32> for Dir3A {
+    type Output = Vec3A;
+    fn mul(self, rhs: f32) -> Self::Output {
+        self.0 * rhs
+    }
+}
+
+impl std::ops::Mul<Dir3A> for Quat {
+    type Output = Dir3A;
+
+    /// Rotates the [`Dir3A`] using a [`Quat`].
+    fn mul(self, direction: Dir3A) -> Self::Output {
+        let rotated = self * *direction;
+
+        // Make sure the result is normalized.
+        // This can fail for non-unit quaternions.
+        debug_assert!(rotated.is_normalized());
+
+        Dir3A::new_unchecked(rotated)
+    }
+}
+
+#[cfg(feature = "approx")]
+impl approx::AbsDiffEq for Dir3A {
+    type Epsilon = f32;
+    fn default_epsilon() -> f32 {
+        f32::EPSILON
+    }
+    fn abs_diff_eq(&self, other: &Self, epsilon: f32) -> bool {
+        self.as_ref().abs_diff_eq(other.as_ref(), epsilon)
+    }
+}
+
+#[cfg(feature = "approx")]
+impl approx::RelativeEq for Dir3A {
+    fn default_max_relative() -> f32 {
+        f32::EPSILON
+    }
+    fn relative_eq(&self, other: &Self, epsilon: f32, max_relative: f32) -> bool {
+        self.as_ref()
+            .relative_eq(other.as_ref(), epsilon, max_relative)
+    }
+}
+
+#[cfg(feature = "approx")]
+impl approx::UlpsEq for Dir3A {
+    fn default_max_ulps() -> u32 {
+        4
+    }
+    fn ulps_eq(&self, other: &Self, epsilon: f32, max_ulps: u32) -> bool {
+        self.as_ref().ulps_eq(other.as_ref(), epsilon, max_ulps)
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -344,6 +489,34 @@ mod tests {
         assert!(
             (Quat::from_rotation_z(std::f32::consts::FRAC_PI_2) * Dir3::X)
                 .abs_diff_eq(Vec3::Y, 10e-6)
+        );
+    }
+
+    #[test]
+    fn dir3a_creation() {
+        assert_eq!(Dir3A::new(Vec3A::X * 12.5), Ok(Dir3A::X));
+        assert_eq!(
+            Dir3A::new(Vec3A::new(0.0, 0.0, 0.0)),
+            Err(InvalidDirectionError::Zero)
+        );
+        assert_eq!(
+            Dir3A::new(Vec3A::new(f32::INFINITY, 0.0, 0.0)),
+            Err(InvalidDirectionError::Infinite)
+        );
+        assert_eq!(
+            Dir3A::new(Vec3A::new(f32::NEG_INFINITY, 0.0, 0.0)),
+            Err(InvalidDirectionError::Infinite)
+        );
+        assert_eq!(
+            Dir3A::new(Vec3A::new(f32::NAN, 0.0, 0.0)),
+            Err(InvalidDirectionError::NaN)
+        );
+        assert_eq!(Dir3A::new_and_length(Vec3A::X * 6.5), Ok((Dir3A::X, 6.5)));
+
+        // Test rotation
+        assert!(
+            (Quat::from_rotation_z(std::f32::consts::FRAC_PI_2) * Dir3A::X)
+                .abs_diff_eq(Vec3A::Y, 10e-6)
         );
     }
 }
--- crates/bevy_reflect/src/impls/math/direction.rs
+++ crates/bevy_reflect/src/impls/math/direction.rs
@@ -4,3 +4,4 @@ use bevy_reflect_derive::impl_reflect_value;
 
 impl_reflect_value!(::bevy_math::Dir2(Debug, PartialEq, Serialize, Deserialize));
 impl_reflect_value!(::bevy_math::Dir3(Debug, PartialEq, Serialize, Deserialize));
+impl_reflect_value!(::bevy_math::Dir3A(Debug, PartialEq, Serialize, Deserialize));
