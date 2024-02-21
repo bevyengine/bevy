d15bc3f9a0fc7e7d6a87ae80595be4289788df12
BD103 <59022059+BD103@users.noreply.github.com>
2024-02-21 13:20:49 -0500

    feat: document all members of `bevy_dynamic_plugin`

--- crates/bevy_dynamic_plugin/src/lib.rs
+++ crates/bevy_dynamic_plugin/src/lib.rs
@@ -1,5 +1,23 @@
-// FIXME(3492): remove once docs are ready
-#![allow(missing_docs)]
+//! Bevy's dynamic plugin loading functionality.
+//!
+//! This crate allows loading dynamic libraries (`.dylib`, `.so`) that export a single
+//! [`Plugin`](bevy_app::Plugin). For usage, see [`dynamically_load_plugin`].
+//!
+//! Note that dynamic linking and loading is inheritly unsafe because it allows executing foreign
+//! code. Additionally, Rust does not have a stable ABI and may produce
+//! incompatible libraries across Rust versions, or even subsequent compilations. This will not work
+//! well in scenarios such as modding, but can work if the dynamic plugins and the main app are
+//! built at the same time, such as with DLCs.
+//!
+//! You may be interested in these safer alternatives:
+//!
+//! - [Bevy Assets - Scripting]: Scripting and modding libraries for Bevy
+//! - [`dextrous_developer`]: Hot reloading system
+//! - [`stabby`]: Stable Rust ABI
+//!
+//! [Bevy Assets - Scripting]: https://bevyengine.org/assets/#scripting
+//! [`dextrous_developer`]: https://github.com/lee-orr/dexterous_developer
+//! [`stabby`]: https://github.com/ZettaScaleLabs/stabby
 
 mod loader;
 
