c9d600547e75cc28a19ee124863445eb2fa7940f
BD103 <59022059+BD103@users.noreply.github.com>
2024-02-20 23:00:13 -0500

    feat: center mesh in `gltf_skinned_mesh` example

--- examples/animation/gltf_skinned_mesh.rs
+++ examples/animation/gltf_skinned_mesh.rs
@@ -20,7 +20,7 @@ fn main() {
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     // Create a camera
     commands.spawn(Camera3dBundle {
-        transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+        transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::new(0.0, 1.0, 0.0), Vec3::Y),
         ..default()
     });
 
