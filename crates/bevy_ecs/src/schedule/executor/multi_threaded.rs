f403b7b23eb1bfe81bd695fd788c38ee99fd3d4b
Chris Russell <8494645+chescock@users.noreply.github.com>
2024-02-23 14:30:15 -0500

    Update crates/bevy_ecs/src/schedule/executor/multi_threaded.rs
    Co-authored-by: Mike <mike.hsu@gmail.com>

--- crates/bevy_ecs/src/schedule/executor/multi_threaded.rs
+++ crates/bevy_ecs/src/schedule/executor/multi_threaded.rs
@@ -466,7 +466,7 @@ impl ExecutorState {
                 }
 
                 // SAFETY:
-                // - No other reference to this system exists.
+                // - Caller ensured no other reference to this system exists.
                 // - `can_run` has been called, which calls `update_archetype_component_access` with this system.
                 // - `can_run` returned true, so no systems with conflicting world access are running.
                 unsafe {
