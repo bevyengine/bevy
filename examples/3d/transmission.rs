cf8c4c7938aa5facfb6068a9218c26ac311dce51
Alice Cecile <alice.i.cecile@gmail.com>
2024-02-29 10:30:28 -0500

    Use color mixing in example
    Co-authored-by: Zachary Harrold <zac@harrold.com.au>

--- examples/3d/transmission.rs
+++ examples/3d/transmission.rs
@@ -316,7 +316,7 @@ fn setup(
             transform: Transform::from_xyz(-1.0, 1.7, 0.0),
             point_light: PointLight {
                 color: Color::from(
-                    LinearRgba::from(ANTIQUE_WHITE) * 0.8 + LinearRgba::from(ORANGE_RED) * 0.2,
+                    LinearRgba::from(ANTIQUE_WHITE).mix(&LinearRgba::from(ORANGE_RED), 0.2),
                 ),
                 intensity: 4_000.0,
                 radius: 0.2,
