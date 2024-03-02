fee8e5c907795cf4fbe5cdf16253be2fff7cec5f
Sludge <96552222+SludgePhD@users.noreply.github.com>
2024-03-02 22:56:29 +0100

    `#[reflect(Default)]` for Handle/Mesh2dHandle

--- crates/bevy_asset/src/handle.rs
+++ crates/bevy_asset/src/handle.rs
@@ -3,7 +3,7 @@ use crate::{
     UntypedAssetId,
 };
 use bevy_ecs::prelude::*;
-use bevy_reflect::{Reflect, TypePath};
+use bevy_reflect::{std_traits::ReflectDefault, Reflect, TypePath};
 use bevy_utils::{get_short_name, Uuid};
 use crossbeam_channel::{Receiver, Sender};
 use std::{
@@ -122,7 +122,7 @@ impl std::fmt::Debug for StrongHandle {
 ///
 /// [`Handle::Strong`] also provides access to useful [`Asset`] metadata, such as the [`AssetPath`] (if it exists).
 #[derive(Component, Reflect)]
-#[reflect(Component)]
+#[reflect(Default, Component)]
 pub enum Handle<A: Asset> {
     /// A "strong" reference to a live (or loading) [`Asset`]. If a [`Handle`] is [`Handle::Strong`], the [`Asset`] will be kept
     /// alive until the [`Handle`] is dropped. Strong handles also provide access to additional asset metadata.
