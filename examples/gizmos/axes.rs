4288d9410cf24a6551250846fa1c47a73c2dcb8a
Matthew Weatherley <weatherleymatthew@gmail.com>
2024-03-04 13:12:17 -0500

    Automatically size axes gizmo based on AABB, added another cube

--- examples/gizmos/axes.rs
+++ examples/gizmos/axes.rs
@@ -1,5 +1,6 @@
 //! This example demonstrates the implementation and behavior of the axes gizmo.
 use bevy::prelude::*;
+use bevy_internal::render::primitives::Aabb;
 use rand::random;
 use std::f32::consts::PI;
 
@@ -7,18 +8,14 @@ fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, setup)
-        .add_systems(Update, (move_cube, draw_axes, change_axis_length))
+        .add_systems(Update, (move_cubes, draw_axes))
         .run();
 }
 
 /// The `ShowAxes` component is attached to an entity to get the `draw_axes` system to
-/// display axes according to its Transform component. The `base_length` parameter
-/// controls the lengths of the axes before they are transformed.
+/// display axes according to its Transform component.
 #[derive(Component)]
-struct ShowAxes {
-    /// The base length for the axes that we will draw
-    base_length: f32,
-}
+struct ShowAxes;
 
 /// The `TransformTracking` component keeps track of the data we need to interpolate
 /// between two transforms in our example.
@@ -55,14 +52,28 @@ fn setup(
         ..default()
     });
 
-    // Action! (Our cube that's going to move)
+    // Action! (Our cubes that are going to move)
     commands.spawn((
         PbrBundle {
             mesh: meshes.add(Cuboid::new(1., 1., 1.)),
             material: materials.add(Color::srgb(0.8, 0.7, 0.6)),
             ..default()
         },
-        ShowAxes { base_length: 1. },
+        ShowAxes,
+        TransformTracking {
+            initial_transform: default(),
+            target_transform: random_transform(),
+            progress: 0,
+        },
+    ));
+
+    commands.spawn((
+        PbrBundle {
+            mesh: meshes.add(Cuboid::new(0.5, 0.5, 0.5)),
+            material: materials.add(Color::srgb(0.6, 0.7, 0.8)),
+            ..default()
+        },
+        ShowAxes,
         TransformTracking {
             initial_transform: default(),
             target_transform: random_transform(),
@@ -77,56 +88,32 @@ fn setup(
         transform: Transform::from_xyz(0., -2., 0.),
         ..default()
     });
-
-    // Instructions
-    commands.spawn(
-        TextBundle::from_section(
-            "Press Up/Down to increase/decrease the axes' base length.",
-            TextStyle {
-                font_size: 20.,
-                ..default()
-            },
-        )
-        .with_style(Style {
-            position_type: PositionType::Absolute,
-            top: Val::Px(20.),
-            left: Val::Px(20.),
-            ..default()
-        }),
-    );
 }
 
-// This system draws the axes based on the cube's transform.
-fn draw_axes(mut gizmos: Gizmos, query: Query<(&Transform, &ShowAxes)>) {
-    let (&transform, axis_data) = query.single();
-    gizmos.axes(transform, axis_data.base_length);
-}
-
-// This system handles keypresses to alter the axis base length
-fn change_axis_length(mut query: Query<&mut ShowAxes>, keyboard: Res<ButtonInput<KeyCode>>) {
-    let mut axis_data = query.single_mut();
-
-    if keyboard.pressed(KeyCode::ArrowUp) {
-        axis_data.base_length += 0.05;
-    } else if keyboard.pressed(KeyCode::ArrowDown) && axis_data.base_length > 0.05 {
-        axis_data.base_length -= 0.05;
+// This system draws the axes based on the cube's transform, with length based on the size of
+// the entity's axis-aligned bounding box (AABB).
+fn draw_axes(mut gizmos: Gizmos, query: Query<(&Transform, &Aabb), With<ShowAxes>>) {
+    for (&transform, &aabb) in &query {
+        let length = aabb.half_extents.length();
+        gizmos.axes(transform, length);
     }
 }
 
-// This system changes the cube's transform to interpolate between random transforms
-fn move_cube(mut query: Query<(&mut Transform, &mut TransformTracking)>) {
-    let (mut transform, mut tracking) = query.single_mut();
+// This system changes the cubes' transforms to interpolate between random transforms
+fn move_cubes(mut query: Query<(&mut Transform, &mut TransformTracking)>) {
+    for (mut transform, mut tracking) in &mut query {
+        let t = tracking.progress as f32 / 100.;
 
-    let t = tracking.progress as f32 / 100.;
+        *transform =
+            interpolate_transforms(tracking.initial_transform, tracking.target_transform, t);
 
-    *transform = interpolate_transforms(tracking.initial_transform, tracking.target_transform, t);
-
-    if tracking.progress < 100 {
-        tracking.progress += 1;
-    } else {
-        tracking.initial_transform = *transform;
-        tracking.target_transform = random_transform();
-        tracking.progress = 0;
+        if tracking.progress < 100 {
+            tracking.progress += 1;
+        } else {
+            tracking.initial_transform = *transform;
+            tracking.target_transform = random_transform();
+            tracking.progress = 0;
+        }
     }
 }
 
@@ -139,8 +126,8 @@ const TRANSLATION_BOUND_UPPER_Y: f32 = 1.;
 const TRANSLATION_BOUND_LOWER_Z: f32 = -2.;
 const TRANSLATION_BOUND_UPPER_Z: f32 = 6.;
 
-const SCALING_BOUND_LOWER_LOG: f32 = -1.;
-const SCALING_BOUND_UPPER_LOG: f32 = 1.;
+const SCALING_BOUND_LOWER_LOG: f32 = -1.2;
+const SCALING_BOUND_UPPER_LOG: f32 = 1.2;
 
 fn random_transform() -> Transform {
     Transform {
