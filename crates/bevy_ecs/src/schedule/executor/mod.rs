41436e63e581ccff38ad8d6724b5b88bf2719471
Vincent Junge <specificprotagonist@posteo.org>
2024-03-09 20:42:08 +0100

    damnit clippy

--- crates/bevy_ecs/src/schedule/executor/mod.rs
+++ crates/bevy_ecs/src/schedule/executor/mod.rs
@@ -126,7 +126,7 @@ pub(super) fn is_apply_deferred(system: &BoxedSystem) -> bool {
 /// extra frame in the backtrace.
 ///
 /// This is reliant on undocumented behavior in Rust's default panic handler, which checks the call stack for symbols
-/// containing the string __rust_begin_short_backtrace in their mangled name.
+/// containing the string `__rust_begin_short_backtrace` in their mangled name.
 mod __rust_begin_short_backtrace {
     use std::hint::black_box;
 
