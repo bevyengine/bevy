75a97df1684a8a64ec7e7a2a108ca935daba8015
Mateusz Wachowiak <mateusz_wachowiak@outlook.com>
2024-03-09 13:16:52 +0100

    fix doc link

--- crates/bevy_dev_tools/src/fps_overlay.rs
+++ crates/bevy_dev_tools/src/fps_overlay.rs
@@ -15,7 +15,7 @@ use bevy_ui::node_bundles::TextBundle;
 /// A plugin that adds an FPS overlay to the Bevy application.
 /// Warning: This plugin will add [`FrameTimeDiagnosticsPlugin`] if it wasn't added before.
 pub struct FpsOverlayPlugin {
-    /// Starting configuration of overlay, this can be later be changed through `[FpsOverlayConfig]` resource.
+    /// Starting configuration of overlay, this can be later be changed through [`FpsOverlayConfig`] resource.
     pub config: FpsOverlayConfig,
 }
 
