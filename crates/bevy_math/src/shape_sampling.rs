118a6fccbcbd39142c8a9fbdec17c52524744786
William Rose <william@theroses.me.uk>
2024-03-14 23:13:21 +0000

    Added a ?Sized to all the rng arguments

--- crates/bevy_math/src/shape_sampling.rs
+++ crates/bevy_math/src/shape_sampling.rs
@@ -18,7 +18,7 @@ pub trait ShapeSample {
     /// // Returns a Vec2 with both x and y between -1 and 1.
     /// println!("{:?}", square.sample_volume(&mut rand::thread_rng()));
     /// ```
-    fn sample_volume(&self, rng: &mut impl Rng) -> Self::Output;
+    fn sample_volume<R: Rng + ?Sized>(&self, rng: &mut R) -> Self::Output;
 
     /// Uniformly sample a point from the surface of this shape, centered on 0.
     ///
@@ -31,13 +31,13 @@ pub trait ShapeSample {
     /// //  and the other is somewhere between -1 and 1.
     /// println!("{:?}", square.sample_surface(&mut rand::thread_rng()));
     /// ```
-    fn sample_surface(&self, rng: &mut impl Rng) -> Self::Output;
+    fn sample_surface<R: Rng + ?Sized>(&self, rng: &mut R) -> Self::Output;
 }
 
 impl ShapeSample for Circle {
     type Output = Vec2;
 
-    fn sample_volume(&self, rng: &mut impl Rng) -> Vec2 {
+    fn sample_volume<R: Rng + ?Sized>(&self, rng: &mut R) -> Vec2 {
         // https://mathworld.wolfram.com/DiskPointPicking.html
         let theta = rng.gen_range(0.0..TAU);
         let r_squared = rng.gen_range(0.0..=(self.radius * self.radius));
@@ -45,7 +45,7 @@ impl ShapeSample for Circle {
         Vec2::new(r * theta.cos(), r * theta.sin())
     }
 
-    fn sample_surface(&self, rng: &mut impl Rng) -> Vec2 {
+    fn sample_surface<R: Rng + ?Sized>(&self, rng: &mut R) -> Vec2 {
         let theta = rng.gen_range(0.0..TAU);
         Vec2::new(self.radius * theta.cos(), self.radius * theta.sin())
     }
@@ -54,7 +54,7 @@ impl ShapeSample for Circle {
 impl ShapeSample for Sphere {
     type Output = Vec3;
 
-    fn sample_volume(&self, rng: &mut impl Rng) -> Vec3 {
+    fn sample_volume<R: Rng + ?Sized>(&self, rng: &mut R) -> Vec3 {
         // https://mathworld.wolfram.com/SpherePointPicking.html
         let theta = rng.gen_range(0.0..TAU);
         let phi = rng.gen_range(-1.0_f32..1.0).acos();
@@ -67,7 +67,7 @@ impl ShapeSample for Sphere {
         }
     }
 
-    fn sample_surface(&self, rng: &mut impl Rng) -> Vec3 {
+    fn sample_surface<R: Rng + ?Sized>(&self, rng: &mut R) -> Vec3 {
         let theta = rng.gen_range(0.0..TAU);
         let phi = rng.gen_range(-1.0_f32..1.0).acos();
         Vec3 {
@@ -81,13 +81,13 @@ impl ShapeSample for Sphere {
 impl ShapeSample for Rectangle {
     type Output = Vec2;
 
-    fn sample_volume(&self, rng: &mut impl Rng) -> Vec2 {
+    fn sample_volume<R: Rng + ?Sized>(&self, rng: &mut R) -> Vec2 {
         let x = rng.gen_range(-self.half_size.x..=self.half_size.x);
         let y = rng.gen_range(-self.half_size.y..=self.half_size.y);
         Vec2::new(x, y)
     }
 
-    fn sample_surface(&self, rng: &mut impl Rng) -> Vec2 {
+    fn sample_surface<R: Rng + ?Sized>(&self, rng: &mut R) -> Vec2 {
         let side_distance = rng.gen_range(-1.0..1.0);
         let other_side = rng.gen_range(0..=1) as f32 * 2.0 - 1.0;
 
@@ -102,14 +102,14 @@ impl ShapeSample for Rectangle {
 impl ShapeSample for Cuboid {
     type Output = Vec3;
 
-    fn sample_volume(&self, rng: &mut impl Rng) -> Vec3 {
+    fn sample_volume<R: Rng + ?Sized>(&self, rng: &mut R) -> Vec3 {
         let x = rng.gen_range(-self.half_size.x..=self.half_size.x);
         let y = rng.gen_range(-self.half_size.y..=self.half_size.y);
         let z = rng.gen_range(-self.half_size.z..=self.half_size.z);
         Vec3::new(x, y, z)
     }
 
-    fn sample_surface(&self, rng: &mut impl Rng) -> Vec3 {
+    fn sample_surface<R: Rng + ?Sized>(&self, rng: &mut R) -> Vec3 {
         let side_distance = rng.gen_range(-1.0..1.0);
         let sides = rng.gen_range(0..4);
         let other_side1 = (sides & 1) as f32 * 2.0 - 1.0;
@@ -127,13 +127,13 @@ impl ShapeSample for Cuboid {
 impl ShapeSample for Cylinder {
     type Output = Vec3;
 
-    fn sample_volume(&self, rng: &mut impl Rng) -> Vec3 {
+    fn sample_volume<R: Rng + ?Sized>(&self, rng: &mut R) -> Vec3 {
         let Vec2 { x, y: z } = self.base().sample_volume(rng);
         let y = rng.gen_range(-self.half_height..=self.half_height);
         Vec3::new(x, y, z)
     }
 
-    fn sample_surface(&self, rng: &mut impl Rng) -> Vec3 {
+    fn sample_surface<R: Rng + ?Sized>(&self, rng: &mut R) -> Vec3 {
         // This uses the area of the ends divided by the overall surface area (optimised)
         // [2 (\pi r^2)]/[2 (\pi r^2) + 2 \pi r h] = r/(r + h)
         if rng.gen_bool((self.radius / (self.radius + 2.0 * self.half_height)) as f64) {
@@ -154,7 +154,7 @@ impl ShapeSample for Cylinder {
 impl ShapeSample for Capsule2d {
     type Output = Vec2;
 
-    fn sample_volume(&self, rng: &mut impl Rng) -> Vec2 {
+    fn sample_volume<R: Rng + ?Sized>(&self, rng: &mut R) -> Vec2 {
         let rectangle_area = self.half_length * self.radius * 4.0;
         let capsule_area = rectangle_area + PI * self.radius * self.radius;
         // Check if the random point should be inside the rectangle
@@ -173,7 +173,7 @@ impl ShapeSample for Capsule2d {
         }
     }
 
-    fn sample_surface(&self, rng: &mut impl Rng) -> Vec2 {
+    fn sample_surface<R: Rng + ?Sized>(&self, rng: &mut R) -> Vec2 {
         let rectangle_surface = 4.0 * self.half_length;
         let capsule_surface = rectangle_surface + TAU * self.radius;
         if rng.gen_bool((rectangle_surface / capsule_surface) as f64) {
@@ -199,7 +199,7 @@ impl ShapeSample for Capsule2d {
 impl ShapeSample for Capsule3d {
     type Output = Vec3;
 
-    fn sample_volume(&self, rng: &mut impl Rng) -> Vec3 {
+    fn sample_volume<R: Rng + ?Sized>(&self, rng: &mut R) -> Vec3 {
         let cylinder_vol = PI * self.radius * self.radius * 2.0 * self.half_length;
         // Add 4/3 pi r^3
         let capsule_vol = cylinder_vol + 4.0 / 3.0 * PI * self.radius * self.radius * self.radius;
@@ -218,7 +218,7 @@ impl ShapeSample for Capsule3d {
         }
     }
 
-    fn sample_surface(&self, rng: &mut impl Rng) -> Vec3 {
+    fn sample_surface<R: Rng + ?Sized>(&self, rng: &mut R) -> Vec3 {
         let cylinder_surface = TAU * self.radius * 2.0 * self.half_length;
         let capsule_surface = cylinder_surface + 4.0 * PI * self.radius * self.radius;
         if rng.gen_bool((cylinder_surface / capsule_surface) as f64) {
