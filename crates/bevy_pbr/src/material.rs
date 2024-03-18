7eb151c017d5ba30fb515a0e491e0e09da758118
JMS55 <47158642+JMS55@users.noreply.github.com>
2024-03-17 23:26:50 -0700

    Update crates/bevy_pbr/src/material.rs
    Co-authored-by: vero <email@atlasdostal.com>

--- crates/bevy_pbr/src/material.rs
+++ crates/bevy_pbr/src/material.rs
@@ -197,7 +197,7 @@ pub trait Material: Asset + AsBindGroup + Clone + Sized {
     /// Returns this material's [`crate::meshlet::MeshletMesh`] deferred fragment shader. If [`ShaderRef::Default`] is returned,
     /// the default meshlet mesh deferred fragment shader will be used.
     ///
-    /// This is part of an experimental feature, and is unnecessary to implement unless you are using MeshletMesh's.
+    /// This is part of an experimental feature, and is unnecessary to implement unless you are using `MeshletMesh`'s.
     #[allow(unused_variables)]
     fn meshlet_mesh_deferred_fragment_shader() -> ShaderRef {
         ShaderRef::Default
