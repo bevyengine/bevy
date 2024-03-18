b13b324af76cc267d48ae5c5478e04f2931d5284
JMS55 <47158642+JMS55@users.noreply.github.com>
2024-03-17 23:27:02 -0700

    Update crates/bevy_pbr/src/material.rs
    Co-authored-by: vero <email@atlasdostal.com>

--- crates/bevy_pbr/src/material.rs
+++ crates/bevy_pbr/src/material.rs
@@ -188,7 +188,7 @@ pub trait Material: Asset + AsBindGroup + Clone + Sized {
     /// Returns this material's [`crate::meshlet::MeshletMesh`] prepass fragment shader. If [`ShaderRef::Default`] is returned,
     /// the default meshlet mesh prepass fragment shader will be used.
     ///
-    /// This is part of an experimental feature, and is unnecessary to implement unless you are using MeshletMesh's.
+    /// This is part of an experimental feature, and is unnecessary to implement unless you are using `MeshletMesh`'s.
     #[allow(unused_variables)]
     fn meshlet_mesh_prepass_fragment_shader() -> ShaderRef {
         ShaderRef::Default
