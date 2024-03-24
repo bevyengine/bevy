c3cc41faca784f07928673df863ea423df83a45e
james7132 <contact@jamessliu.com>
2024-03-24 15:35:47 -0700

    Set the logo and favicon for all of Bevy's published crates

--- crates/bevy_audio/src/lib.rs
+++ crates/bevy_audio/src/lib.rs
@@ -1,5 +1,9 @@
 #![forbid(unsafe_code)]
 #![cfg_attr(docsrs, feature(doc_auto_cfg))]
+#![doc(
+    html_logo_url = "https://bevyengine.org/assets/icon.png",
+    html_favicon_url = "https://bevyengine.org/assets/icon.png"
+)]
 
 //! Audio support for the game engine Bevy
 //!
