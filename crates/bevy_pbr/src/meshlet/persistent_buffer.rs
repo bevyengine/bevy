8b99a2a850690264110a6b8786505b7bfdf683f0
james7132 <contact@jamessliu.com>
2024-03-25 13:11:06 -0700

    Allow unsafe code in bevy_pbr

--- crates/bevy_pbr/src/meshlet/persistent_buffer.rs
+++ crates/bevy_pbr/src/meshlet/persistent_buffer.rs
@@ -1,3 +1,4 @@
+
 use bevy_render::{
     render_resource::{
         BindingResource, Buffer, BufferAddress, BufferDescriptor, BufferUsages,
@@ -112,6 +113,7 @@ impl<T: PersistentGpuBufferable> PersistentGpuBuffer<T> {
 /// # Safety
 /// * All data must be a multiple of `wgpu::COPY_BUFFER_ALIGNMENT` bytes.
 /// * The amount of bytes written to `buffer` in `write_bytes_le()` must match `size_in_bytes()`.
+#![allow(unsafe_code)]
 pub unsafe trait PersistentGpuBufferable {
     /// Additional metadata associated with each item, made available during `write_bytes_le`.
     type Metadata;
