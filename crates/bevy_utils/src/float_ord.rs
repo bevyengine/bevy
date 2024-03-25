19223ee03317c7a9ea7e4e1cc03296ab8e329b6f
Kanabenki <lucien.menassol@gmail.com>
2024-03-25 18:21:57 +0100

    Fix `Ord` and `PartialOrd` differing for `FloatOrd` and optimize implementation

--- crates/bevy_utils/src/float_ord.rs
+++ crates/bevy_utils/src/float_ord.rs
@@ -13,28 +13,47 @@ use std::{
 ///
 /// Wrapping a float with `FloatOrd` breaks conformance with the standard
 /// by sorting `NaN` as less than all other numbers and equal to any other `NaN`.
-#[derive(Debug, Copy, Clone, PartialOrd)]
+#[derive(Debug, Copy, Clone)]
 pub struct FloatOrd(pub f32);
 
-#[allow(clippy::derive_ord_xor_partial_ord)]
+impl PartialOrd for FloatOrd {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+
+    fn lt(&self, other: &Self) -> bool {
+        !other.le(self)
+    }
+    // If `self` is NaN, it is equal to another NaN and less than all other floats, so return true.
+    // If `self` isn't NaN and `other` is, the float comparison returns false, which match the `FloatOrd` ordering.
+    // Otherwise, a standard float comparison happens.
+    fn le(&self, other: &Self) -> bool {
+        self.0.is_nan() || self.0 <= other.0
+    }
+    fn gt(&self, other: &Self) -> bool {
+        !self.le(other)
+    }
+    fn ge(&self, other: &Self) -> bool {
+        other.le(self)
+    }
+}
+
 impl Ord for FloatOrd {
     fn cmp(&self, other: &Self) -> Ordering {
-        self.0.partial_cmp(&other.0).unwrap_or_else(|| {
-            if self.0.is_nan() && !other.0.is_nan() {
-                Ordering::Less
-            } else if !self.0.is_nan() && other.0.is_nan() {
-                Ordering::Greater
-            } else {
-                Ordering::Equal
-            }
-        })
+        if self > other {
+            Ordering::Greater
+        } else if self < other {
+            Ordering::Less
+        } else {
+            Ordering::Equal
+        }
     }
 }
 
 impl PartialEq for FloatOrd {
     fn eq(&self, other: &Self) -> bool {
-        if self.0.is_nan() && other.0.is_nan() {
-            true
+        if self.0.is_nan() {
+            other.0.is_nan()
         } else {
             self.0 == other.0
         }
@@ -62,5 +81,39 @@ impl Neg for FloatOrd {
 
     fn neg(self) -> Self::Output {
         FloatOrd(-self.0)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn float_ord_eq() {
+        let nan = FloatOrd(f32::NAN);
+        let num = FloatOrd(0.0);
+
+        assert_eq!(nan, nan);
+
+        assert_ne!(nan, num);
+        assert_ne!(num, nan);
+
+        assert_eq!(num, num);
+    }
+
+    #[test]
+    fn float_ord_cmp() {
+        let nan = FloatOrd(f32::NAN);
+        let zero = FloatOrd(0.0);
+        let one = FloatOrd(1.0);
+
+        assert_eq!(nan.cmp(&nan), Ordering::Equal);
+
+        assert_eq!(nan.cmp(&zero), Ordering::Less);
+        assert_eq!(zero.cmp(&nan), Ordering::Greater);
+
+        assert_eq!(zero.cmp(&zero), Ordering::Equal);
+        assert_eq!(one.cmp(&zero), Ordering::Greater);
+        assert_eq!(zero.cmp(&one), Ordering::Less);
     }
 }
