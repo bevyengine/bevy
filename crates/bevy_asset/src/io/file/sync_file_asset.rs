a206876acbab8749e768c4fb68d601bf226ed544
BeastLe9enD <beastle9end@outlook.de>
2024-03-29 22:21:21 +0100

    Add AsyncSeek impl for FileReader

--- crates/bevy_asset/src/io/file/sync_file_asset.rs
+++ crates/bevy_asset/src/io/file/sync_file_asset.rs
@@ -1,4 +1,4 @@
-use futures_io::{AsyncRead, AsyncWrite};
+use futures_io::{AsyncRead, AsyncSeek, AsyncWrite};
 use futures_lite::Stream;
 
 use crate::io::{
@@ -27,6 +27,18 @@ impl AsyncRead for FileReader {
         let this = self.get_mut();
         let read = this.0.read(buf);
         Poll::Ready(read)
+    }
+}
+
+impl AsyncSeek for FileReader {
+    fn poll_seek(
+        self: Pin<&mut Self>,
+        _cx: &mut std::task::Context<'_>,
+        pos: std::io::SeekFrom,
+    ) -> Poll<std::io::Result<u64>> {
+        let this = self.get_mut();
+        let seek = this.0.seek(pos);
+        Poll::Ready(seek)
     }
 }
 
