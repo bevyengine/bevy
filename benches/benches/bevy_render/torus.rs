da14a71666704a6b3cf5e3fbbf2d8df89304c0d5
W. Black <wmcblack@gmail.com>
2024-03-29 05:05:36 -0700

    Update torus.rs

--- benches/benches/bevy_render/torus.rs
+++ benches/benches/bevy_render/torus.rs
@@ -14,20 +14,3 @@ criterion_group!(
     torus,
 );
 criterion_main!(benches);
-=======
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
-
-use bevy_render::mesh::TorusMeshBuilder;
-
-fn torus(c: &mut Criterion) {
-    c.bench_function("build_torus", |b| {
-        b.iter(|| black_box(TorusMeshBuilder::new(black_box(0.5),black_box(1.0))));
-    });
-}
-
-criterion_group!(
-    benches,
-    torus,
-);
-criterion_main!(benches);
->>>>>>> c421e29b718822d12efc067456c4c13ea2719da1
