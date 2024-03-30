a497de828eab12ff16d3ae2a8701b01e172c0a5c
Bram Buurlage <brambuurlage@gmail.com>
2024-03-30 12:29:58 +0100

    Use BindGroupLayoutEntries::sequential for AutoExposurePipeline

--- crates/bevy_core_pipeline/src/auto_exposure/pipeline.rs
+++ crates/bevy_core_pipeline/src/auto_exposure/pipeline.rs
@@ -3,7 +3,12 @@ use super::compensation_curve::{
 };
 use bevy_asset::prelude::*;
 use bevy_ecs::prelude::*;
-use bevy_render::{render_resource::*, renderer::RenderDevice, texture::Image, view::ViewUniform};
+use bevy_render::{
+    render_resource::{binding_types::*, *},
+    renderer::RenderDevice,
+    texture::Image,
+    view::ViewUniform,
+};
 use std::num::NonZeroU64;
 
 #[derive(Resource)]
@@ -50,94 +55,25 @@ impl FromWorld for AutoExposurePipeline {
         let render_device = world.resource::<RenderDevice>();
 
         Self {
-            histogram_layout:
-                render_device.create_bind_group_layout(
-                    "compute histogram bind group",
-                    &[
-                        BindGroupLayoutEntry {
-                            binding: 0,
-                            visibility: ShaderStages::COMPUTE,
-                            ty: BindingType::Buffer {
-                                ty: BufferBindingType::Uniform,
-                                has_dynamic_offset: false,
-                                min_binding_size: Some(AutoExposureUniform::min_size()),
-                            },
-                            count: None,
-                        },
-                        BindGroupLayoutEntry {
-                            binding: 1,
-                            visibility: ShaderStages::COMPUTE,
-                            ty: BindingType::Texture {
-                                sample_type: TextureSampleType::Float { filterable: false },
-                                view_dimension: TextureViewDimension::D2,
-                                multisampled: false,
-                            },
-                            count: None,
-                        },
-                        BindGroupLayoutEntry {
-                            binding: 2,
-                            visibility: ShaderStages::COMPUTE,
-                            ty: BindingType::Texture {
-                                sample_type: TextureSampleType::Float { filterable: false },
-                                view_dimension: TextureViewDimension::D2,
-                                multisampled: false,
-                            },
-                            count: None,
-                        },
-                        BindGroupLayoutEntry {
-                            binding: 3,
-                            visibility: ShaderStages::COMPUTE,
-                            ty: BindingType::Texture {
-                                sample_type: TextureSampleType::Float { filterable: false },
-                                view_dimension: TextureViewDimension::D1,
-                                multisampled: false,
-                            },
-                            count: None,
-                        },
-                        BindGroupLayoutEntry {
-                            binding: 4,
-                            visibility: ShaderStages::COMPUTE,
-                            ty: BindingType::Buffer {
-                                ty: BufferBindingType::Uniform,
-                                has_dynamic_offset: false,
-                                min_binding_size: Some(
-                                    AutoExposureCompensationCurveUniform::min_size(),
-                                ),
-                            },
-                            count: None,
-                        },
-                        BindGroupLayoutEntry {
-                            binding: 5,
-                            visibility: ShaderStages::COMPUTE,
-                            ty: BindingType::Buffer {
-                                ty: BufferBindingType::Storage { read_only: false },
-                                has_dynamic_offset: false,
-                                min_binding_size: NonZeroU64::new(HISTOGRAM_BIN_COUNT * 4),
-                            },
-                            count: None,
-                        },
-                        BindGroupLayoutEntry {
-                            binding: 6,
-                            visibility: ShaderStages::COMPUTE,
-                            ty: BindingType::Buffer {
-                                ty: BufferBindingType::Storage { read_only: false },
-                                has_dynamic_offset: false,
-                                min_binding_size: NonZeroU64::new(4),
-                            },
-                            count: None,
-                        },
-                        BindGroupLayoutEntry {
-                            binding: 7,
-                            visibility: ShaderStages::COMPUTE,
-                            ty: BindingType::Buffer {
-                                ty: BufferBindingType::Storage { read_only: false },
-                                has_dynamic_offset: true,
-                                min_binding_size: Some(ViewUniform::min_size()),
-                            },
-                            count: None,
-                        },
-                    ],
+            histogram_layout: render_device.create_bind_group_layout(
+                "compute histogram bind group",
+                &BindGroupLayoutEntries::sequential(
+                    ShaderStages::COMPUTE,
+                    (
+                        uniform_buffer_sized(false, Some(AutoExposureUniform::min_size())),
+                        texture_2d(TextureSampleType::Float { filterable: false }),
+                        texture_2d(TextureSampleType::Float { filterable: false }),
+                        texture_1d(TextureSampleType::Float { filterable: false }),
+                        uniform_buffer_sized(
+                            false,
+                            Some(AutoExposureCompensationCurveUniform::min_size()),
+                        ),
+                        storage_buffer_sized(false, NonZeroU64::new(HISTOGRAM_BIN_COUNT * 4)),
+                        storage_buffer_sized(false, NonZeroU64::new(4)),
+                        storage_buffer_sized(true, Some(ViewUniform::min_size())),
+                    ),
                 ),
+            ),
             histogram_shader: METERING_SHADER_HANDLE.clone(),
         }
     }
