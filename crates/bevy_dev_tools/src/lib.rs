6ccb2a306e1b518638aa382b2673d1067998a32f
Mateusz Wachowiak <mateusz_wachowiak@outlook.com>
2024-04-03 20:02:50 +0200

    remove close_on_esc (#12859)
    # Objective
    
    - Remove `close_on_esc`
    - For context about why we are removing it see:
    [discord](https://discordapp.com/channels/691052431525675048/692572690833473578/1225075194524073985)
    
    ## Migration Guide
    
    - Users who added `close_on_esc` in their application will have to
    replace it with their own solution.
    
    ```rust
    pub fn close_on_esc(
        mut commands: Commands,
        focused_windows: Query<(Entity, &Window)>,
        input: Res<ButtonInput<KeyCode>>,
    ) {
        for (window, focus) in focused_windows.iter() {
            if !focus.focused {
                continue;
            }
    
            if input.just_pressed(KeyCode::Escape) {
                commands.entity(window).despawn();
            }
        }
    }
    ```

--- crates/bevy_dev_tools/src/lib.rs
+++ crates/bevy_dev_tools/src/lib.rs
@@ -18,10 +18,6 @@ pub mod fps_overlay;
 #[cfg(feature = "bevy_ui_debug")]
 pub mod ui_debug_overlay;
 
-mod close_on_esc;
-
-pub use crate::close_on_esc::close_on_esc;
-
 /// Enables developer tools in an [`App`]. This plugin is added automatically with `bevy_dev_tools`
 /// feature.
 ///
