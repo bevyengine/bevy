f88f453c3d54e7553780574279776277cdc3b782
Noah Emke <noahremke@gmail.com>
2024-04-05 03:19:15 -0400

    Add assets and .meta file to meta_files example Also added explanation comments

--- examples/asset/meta_files.rs
+++ examples/asset/meta_files.rs
@@ -4,14 +4,21 @@ use bevy::prelude::*;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins(
+            // This just tells the asset server to look in the right examples folder
+            DefaultPlugins.set(AssetPlugin {
+            file_path: "examples/asset/files".to_string(),
+            ..Default::default()
+        }))
         .add_systems(Startup, setup)
         .run();
 }
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    // Without any .meta file specifying settings, the default sampler is used for loading images. 
+    // If you are using a very small image and rendering it larger like seen here, the default linear filtering will result in a blurry image.
     commands.spawn(SpriteBundle {
-        texture: asset_server.load("pixel/bevy_pixel_dark.png"),
+        texture: asset_server.load("bevy_pixel_dark.png"),
         sprite: Sprite { 
             custom_size: Some(Vec2 { x: 160.0, y: 120.0 }), 
             ..Default::default() 
@@ -19,8 +26,15 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
         transform: Transform::from_xyz(-100.0, 0.0, 0.0),
         ..Default::default()
     });
+
+    // When a .meta file is added with the same name as the asset and a '.meta' extension
+    // you can (and must) specify all fields of the asset loader's settings for that 
+    // particular asset, in this case ImageLoaderSettings. Take a look at 
+    // examples/asset/files/bevy_pixel_dark_with_meta.png.meta
+    // for the format and you'll notice, the only non-default option is setting Nearest
+    // filtering. This tends to work much better for pixel art assets. 
     commands.spawn(SpriteBundle {
-        texture: asset_server.load("pixel/bevy_pixel_dark.png"),
+        texture: asset_server.load("bevy_pixel_dark_with_meta.png"),
         sprite: Sprite { 
             custom_size: Some(Vec2 { x: 160.0, y: 120.0 }), 
             ..Default::default() 
@@ -28,5 +42,6 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
         transform: Transform::from_xyz(100.0, 0.0, 0.0),
         ..Default::default()
     });
+    
     commands.spawn(Camera2dBundle::default());
 }
