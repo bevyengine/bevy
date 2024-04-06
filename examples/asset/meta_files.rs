8273e87d3f93e94e4187f9741b9578015bc5d8d8
Noah Emke <noahremke@gmail.com>
2024-04-06 12:14:01 -0400

    Add third image to meta_files example using load_with_settings

--- examples/asset/meta_files.rs
+++ examples/asset/meta_files.rs
@@ -1,6 +1,6 @@
 //! This example demonstrates the usage of '.meta' files to override the default settings for loading an asset
 
-use bevy::prelude::*;
+use bevy::{prelude::*, render::{render_resource::Sampler, texture::{ImageLoaderSettings, ImageSampler}}};
 
 fn main() {
     App::new()
@@ -18,6 +18,7 @@ fn main() {
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     // Without any .meta file specifying settings, the default sampler [ImagePlugin::default()] is used for loading images.
     // If you are using a very small image and rendering it larger like seen here, the default linear filtering will result in a blurry image.
+    // Useful note: The default sampler specified by the ImagePlugin is *not* the 
     commands.spawn(SpriteBundle {
         texture: asset_server.load("bevy_pixel_dark.png"),
         sprite: Sprite {
@@ -44,6 +45,31 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
             ..Default::default()
         },
         transform: Transform::from_xyz(100.0, 0.0, 0.0),
+        ..Default::default()
+    });
+
+    // Another option is to use the AssetServers load_with_settings function.
+    // With this you can specify the same settings upon loading your asset with a 
+    // couple of differences. A big one is that you aren't required to set *every*
+    // setting, just modify the ones that you need. It works by passing in a function
+    // (in this case an anonymous closure) that takes a reference to the settings type 
+    // that is then modified in the function.
+    // Do note that if you want to load the same asset with different settings, the 
+    // settings changes from any loads after the first of the same asset will be ignored.
+    // This is why this one loads a differently named copy of the asset instead of using 
+    // same one as without a .meta file.
+    commands.spawn(SpriteBundle {
+        texture: asset_server.load_with_settings(
+            "bevy_pixel_dark_with_settings.png", 
+            |settings: &mut ImageLoaderSettings| {
+                settings.sampler = ImageSampler::nearest();
+            }
+        ),
+        sprite: Sprite {
+            custom_size: Some(Vec2 { x: 160.0, y: 120.0 }),
+            ..Default::default()
+        },
+        transform: Transform::from_xyz(0.0, 150.0, 0.0),
         ..Default::default()
     });
 
