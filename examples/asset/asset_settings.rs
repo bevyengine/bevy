36057706cf022ca64873308b50af04da5e65680a
Noah Emke <noahremke@gmail.com>
2024-04-06 13:10:05 -0400

    Ran cargo fmt and ci checks Fixed some failing ci checks in asset_settings example

--- examples/asset/asset_settings.rs
+++ examples/asset/asset_settings.rs
@@ -1,6 +1,9 @@
-//! This example demonstrates the usage of '.meta' files and AssetServer::load_with_settings to override the default settings for loading an asset
+//! This example demonstrates the usage of '.meta' files and [`AssetServer::load_with_settings`] to override the default settings for loading an asset
 
-use bevy::{prelude::*, render::{render_resource::Sampler, texture::{ImageLoaderSettings, ImageSampler}}};
+use bevy::{
+    prelude::*,
+    render::texture::{ImageLoaderSettings, ImageSampler},
+};
 
 fn main() {
     App::new()
@@ -18,7 +21,7 @@ fn main() {
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     // Without any .meta file specifying settings, the default sampler [ImagePlugin::default()] is used for loading images.
     // If you are using a very small image and rendering it larger like seen here, the default linear filtering will result in a blurry image.
-    // Useful note: The default sampler specified by the ImagePlugin is *not* the 
+    // Useful note: The default sampler specified by the ImagePlugin is *not* the
     commands.spawn(SpriteBundle {
         texture: asset_server.load("bevy_pixel_dark.png"),
         sprite: Sprite {
@@ -36,7 +39,7 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     // for the format and you'll notice, the only non-default option is setting Nearest
     // filtering. This tends to work much better for pixel art assets.
     // A good reference when filling this out is to check out [ImageLoaderSettings::default()]
-    // and follow to the default implementation of each fields type. 
+    // and follow to the default implementation of each fields type.
     // https://docs.rs/bevy/latest/bevy/render/texture/struct.ImageLoaderSettings.html#
     commands.spawn(SpriteBundle {
         texture: asset_server.load("bevy_pixel_dark_with_meta.png"),
@@ -49,21 +52,21 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     });
 
     // Another option is to use the AssetServers load_with_settings function.
-    // With this you can specify the same settings upon loading your asset with a 
+    // With this you can specify the same settings upon loading your asset with a
     // couple of differences. A big one is that you aren't required to set *every*
     // setting, just modify the ones that you need. It works by passing in a function
-    // (in this case an anonymous closure) that takes a reference to the settings type 
+    // (in this case an anonymous closure) that takes a reference to the settings type
     // that is then modified in the function.
-    // Do note that if you want to load the same asset with different settings, the 
+    // Do note that if you want to load the same asset with different settings, the
     // settings changes from any loads after the first of the same asset will be ignored.
-    // This is why this one loads a differently named copy of the asset instead of using 
+    // This is why this one loads a differently named copy of the asset instead of using
     // same one as without a .meta file.
     commands.spawn(SpriteBundle {
         texture: asset_server.load_with_settings(
-            "bevy_pixel_dark_with_settings.png", 
+            "bevy_pixel_dark_with_settings.png",
             |settings: &mut ImageLoaderSettings| {
                 settings.sampler = ImageSampler::nearest();
-            }
+            },
         ),
         sprite: Sprite {
             custom_size: Some(Vec2 { x: 160.0, y: 120.0 }),
