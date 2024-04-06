f2573ee05a593f826e2c4eb6bc2db598eee2088e
Noah Emke <noahremke@gmail.com>
2024-04-05 23:59:22 -0400

    Add comments with references to meta_files example

--- examples/asset/meta_files.rs
+++ examples/asset/meta_files.rs
@@ -16,7 +16,7 @@ fn main() {
 }
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
-    // Without any .meta file specifying settings, the default sampler is used for loading images.
+    // Without any .meta file specifying settings, the default sampler [ImagePlugin::default()] is used for loading images.
     // If you are using a very small image and rendering it larger like seen here, the default linear filtering will result in a blurry image.
     commands.spawn(SpriteBundle {
         texture: asset_server.load("bevy_pixel_dark.png"),
@@ -30,10 +30,12 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
 
     // When a .meta file is added with the same name as the asset and a '.meta' extension
     // you can (and must) specify all fields of the asset loader's settings for that
-    // particular asset, in this case ImageLoaderSettings. Take a look at
+    // particular asset, in this case [ImageLoaderSettings]. Take a look at
     // examples/asset/files/bevy_pixel_dark_with_meta.png.meta
     // for the format and you'll notice, the only non-default option is setting Nearest
     // filtering. This tends to work much better for pixel art assets.
+    // A good reference when filling this out is to check out [ImageLoaderSettings::default()]
+    // and follow to the default implementation of each fields type.
     commands.spawn(SpriteBundle {
         texture: asset_server.load("bevy_pixel_dark_with_meta.png"),
         sprite: Sprite {
