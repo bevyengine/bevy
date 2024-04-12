b1dca28255911e91872cebd02677a30f4efd2f44
Cyannide <cyannide@nullhost.net>
2024-04-12 17:09:47 -0500

    Fixed issues found by 'cargo run -p ci -- lints'.

--- examples/mobile/build.rs
+++ examples/mobile/build.rs
@@ -1,6 +1,9 @@
 #![allow(missing_docs)]
 
-use std::{env, path::{Path, PathBuf}};
+use std::{
+    env,
+    path::{Path, PathBuf},
+};
 
 fn main() {
     if env::var("CARGO_CFG_TARGET_OS").unwrap() == "android" {
@@ -15,19 +18,14 @@ fn android() {
         let sysroot_libs_path = PathBuf::from(env::var_os("CARGO_NDK_SYSROOT_LIBS_PATH").unwrap());
         let lib_path = sysroot_libs_path.join("libc++_shared.so");
         let dest_path = Path::new(&output_path)
-            .join(&env::var("CARGO_NDK_ANDROID_TARGET").unwrap())
+            .join(env::var("CARGO_NDK_ANDROID_TARGET").unwrap())
             .join("libc++_shared.so");
-        println!("{}", format!("cargo:rerun-if-changed={:?}", &*dest_path)); // use this to force libc++_shared.so to get copied to jniLibs even if project hasn't changed
-        match std::fs::create_dir_all(dest_path.clone().parent().unwrap()) {
-            Err(e) => panic!("Unable to create output dir: {:?}", e),
-            _ => (),
+        println!("cargo:rerun-if-changed={:?}", &*dest_path); // use this to force libc++_shared.so to get copied to jniLibs even if project hasn't changed
+        if let Err(e) = std::fs::create_dir_all(dest_path.clone().parent().unwrap()) {
+            panic!("Unable to create output dir: {:?}", e);
         }
-        match std::fs::copy(
-                lib_path,
-                dest_path,
-            ) {
-            Err(e) => panic!("Unable to copy libc++_shared.so: {}", e),
-            _ => (),
+        if let Err(e) = std::fs::copy(lib_path, dest_path) {
+            panic!("Unable to copy libc++_shared.so: {}", e);
         }
     } else {
         panic!("CARGO_NDK_OUTPUT_PATH not set.");
