d10bd5d8f9d0890228d58a95535d9d354edf21f9
Cyannide <cyannide@nullhost.net>
2024-04-12 12:39:02 -0500

    Updated Bevy Android example to use AGDK GameActivity and Gradle.

---
+++ examples/mobile/app/src/main/java/org/bevyengine/examples/MainActivity.java
@@ -0,0 +1,68 @@
+package org.bevyengine.example;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.WindowCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.core.view.WindowInsetsControllerCompat;
+
+import com.google.androidgamesdk.GameActivity;
+
+import android.os.Bundle;
+import android.content.pm.PackageManager;
+import android.os.Build.VERSION;
+import android.os.Build.VERSION_CODES;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+
+public class MainActivity extends GameActivity {
+
+    static {
+        // Load the STL first to workaround issues on old Android versions:
+        // "if your app targets a version of Android earlier than Android 4.3
+        // (Android API level 18),
+        // and you use libc++_shared.so, you must load the shared library before any other
+        // library that depends on it."
+        // See https://developer.android.com/ndk/guides/cpp-support#shared_runtimes
+        //System.loadLibrary("c++_shared");
+
+        // Load the native library.
+        // The name "android-game" depends on your CMake configuration, must be
+        // consistent here and inside AndroidManifect.xml
+        System.loadLibrary("bevy_mobile_example");
+    }
+
+    private void hideSystemUI() {
+        // This will put the game behind any cutouts and waterfalls on devices which have
+        // them, so the corresponding insets will be non-zero.
+        if (VERSION.SDK_INT >= VERSION_CODES.P) {
+            getWindow().getAttributes().layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_ALWAYS;
+        }
+        // From API 30 onwards, this is the recommended way to hide the system UI, rather than
+        // using View.setSystemUiVisibility.
+        View decorView = getWindow().getDecorView();
+        WindowInsetsControllerCompat controller = new WindowInsetsControllerCompat(getWindow(), decorView);
+        controller.hide(WindowInsetsCompat.Type.systemBars());
+        controller.hide(WindowInsetsCompat.Type.displayCutout());
+        controller.setSystemBarsBehavior(WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // When true, the app will fit inside any system UI windows.
+        // When false, we render behind any system UI windows.
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
+        hideSystemUI();
+        // You can set IME fields here or in native code using GameActivity_setImeEditorInfoFields.
+        // We set the fields in native_engine.cpp.
+        // super.setImeEditorInfoFields(InputType.TYPE_CLASS_TEXT,
+        //     IME_ACTION_NONE, IME_FLAG_NO_FULLSCREEN );
+        super.onCreate(savedInstanceState);
+    }
+
+    public boolean isGooglePlayGames() {
+        PackageManager pm = getPackageManager();
+        return pm.hasSystemFeature("com.google.android.play.feature.HPE_EXPERIENCE");
+    }
+}
+
