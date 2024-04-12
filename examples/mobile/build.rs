d12af883be77555284a67a73377b152d73f375d0
Cyannide <cyannide@nullhost.net>
2024-04-12 16:32:10 -0500

    Improved build.rs to handle more errors and force the shared libc++ to copy to output path.

--- examples/mobile/build.rs
+++ examples/mobile/build.rs
@@ -14,12 +14,22 @@ fn android() {
     if let Ok(output_path) = env::var("CARGO_NDK_OUTPUT_PATH") {
         let sysroot_libs_path = PathBuf::from(env::var_os("CARGO_NDK_SYSROOT_LIBS_PATH").unwrap());
         let lib_path = sysroot_libs_path.join("libc++_shared.so");
-        std::fs::copy(
-            lib_path,
-            Path::new(&output_path)
-                .join(&env::var("CARGO_NDK_ANDROID_TARGET").unwrap())
-                .join("libc++_shared.so"),
-        )
-        .unwrap();
+        let dest_path = Path::new(&output_path)
+            .join(&env::var("CARGO_NDK_ANDROID_TARGET").unwrap())
+            .join("libc++_shared.so");
+        println!("{}", format!("cargo:rerun-if-changed={:?}", &*dest_path)); // use this to force libc++_shared.so to get copied to jniLibs even if project hasn't changed
+        match std::fs::create_dir_all(dest_path.clone().parent().unwrap()) {
+            Err(e) => panic!("Unable to create output dir: {:?}", e),
+            _ => (),
+        }
+        match std::fs::copy(
+                lib_path,
+                dest_path,
+            ) {
+            Err(e) => panic!("Unable to copy libc++_shared.so: {}", e),
+            _ => (),
+        }
+    } else {
+        panic!("CARGO_NDK_OUTPUT_PATH not set.");
     }
 }
