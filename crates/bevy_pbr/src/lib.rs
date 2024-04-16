ac9ff8632f4ee2ce2d73d6e05d26458792ed77f7
Patrick Walton <pcwalton@mimiga.net>
2024-04-15 21:13:11 -0700

    Add `Cascades` to the type registry, fixing lights in glTF.

--- crates/bevy_pbr/src/lib.rs
+++ crates/bevy_pbr/src/lib.rs
@@ -269,6 +269,7 @@ impl Plugin for PbrPlugin {
         app.register_asset_reflect::<StandardMaterial>()
             .register_type::<AmbientLight>()
             .register_type::<CascadeShadowConfig>()
+            .register_type::<Cascades>()
             .register_type::<CascadesVisibleEntities>()
             .register_type::<ClusterConfig>()
             .register_type::<CubemapVisibleEntities>()
