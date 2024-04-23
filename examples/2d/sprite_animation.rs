60eff9d2822d69443374984e216634912087d309
Andrew William Watson <aww@awwsmm.com>
2024-04-23 12:51:51 -0400

    leave comments to explain the example, add minimal UI to explain how to interact with it

--- examples/2d/sprite_animation.rs
+++ examples/2d/sprite_animation.rs
@@ -12,22 +12,23 @@ fn main() {
         .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest())) // prevents blurry sprites
         .add_systems(Startup, setup)
         .add_systems(Update, execute_animations)
-        // press the left arrow key to animate the left sprite
         .add_systems(
             Update,
-            trigger_animation::<LeftSprite>.run_if(input_just_pressed(KeyCode::ArrowLeft)),
-        )
-        // press the right arrow key to animate the right sprite
-        .add_systems(
-            Update,
-            trigger_animation::<RightSprite>.run_if(input_just_pressed(KeyCode::ArrowRight)),
+            (
+                // press the right arrow key to animate the right sprite
+                trigger_animation::<RightSprite>.run_if(input_just_pressed(KeyCode::ArrowRight)),
+                // press the left arrow key to animate the left sprite
+                trigger_animation::<LeftSprite>.run_if(input_just_pressed(KeyCode::ArrowLeft)),
+            ),
         )
         .run();
 }
 
+// This system runs when the user clicks the left arrow key or right arrow key
 fn trigger_animation<S: Component>(mut query: Query<&mut AnimationConfig, With<S>>) {
     // we expect the Component of type S to be used as a marker Component by only a single entity
     let mut animation = query.single_mut();
+    // we create a new timer when the animation is triggered
     animation.frame_timer = AnimationConfig::timer_from_fps(animation.fps);
 }
 
@@ -54,17 +55,25 @@ impl AnimationConfig {
     }
 }
 
+// This system loops through all the sprites in the `TextureAtlas`, from  `first_sprite_index` to
+// `last_sprite_index` (both defined in `AnimationConfig`).
 fn execute_animations(
     time: Res<Time>,
     mut query: Query<(&mut AnimationConfig, &mut TextureAtlas)>,
 ) {
     for (mut config, mut atlas) in &mut query {
+        // we track how long the current sprite has been displayed for
         config.frame_timer.tick(time.delta());
+
+        // If it has been displayed for the user-defined amount of time (fps)...
         if config.frame_timer.just_finished() {
             if atlas.index == config.last_sprite_index {
+                // ...and it IS the last frame, then we move back to the first frame and stop.
                 atlas.index = config.first_sprite_index;
             } else {
+                // ...and it is NOT the last frame, then we move to the next frame...
                 atlas.index += 1;
+                // ...and reset the frame timer to start counting all over again
                 config.frame_timer = AnimationConfig::timer_from_fps(config.fps);
             }
         }
@@ -82,14 +91,19 @@ fn setup(
     asset_server: Res<AssetServer>,
     mut texture_atlas_layouts: ResMut<Assets<TextureAtlasLayout>>,
 ) {
+    commands.spawn(Camera2dBundle::default());
+
+    // load the sprite sheet using the `AssetServer`
     let texture = asset_server.load("textures/rpg/chars/gabe/gabe-idle-run.png");
+
+    // the sprite sheet has 7 sprites arranged in a row, and they are all 24px x 24px
     let layout = TextureAtlasLayout::from_grid(UVec2::splat(24), 7, 1, None, None);
     let texture_atlas_layout = texture_atlas_layouts.add(layout);
 
+    // the first (left-hand) sprite runs at 10 FPS
     let animation_config_1 = AnimationConfig::new(1, 6, 10);
 
-    commands.spawn(Camera2dBundle::default());
-
+    // create the first (left-hand) sprite
     commands.spawn((
         SpriteBundle {
             transform: Transform::from_scale(Vec3::splat(6.0))
@@ -105,8 +119,10 @@ fn setup(
         animation_config_1,
     ));
 
+    // the second (right-hand) sprite runs at 20 FPS
     let animation_config_2 = AnimationConfig::new(1, 6, 20);
 
+    // create the second (right-hand) sprite
     commands.spawn((
         SpriteBundle {
             transform: Transform::from_scale(Vec3::splat(6.0))
@@ -121,4 +137,22 @@ fn setup(
         RightSprite,
         animation_config_2,
     ));
+
+    // create a minimal UI explaining how to interact with the example
+    commands.spawn(TextBundle {
+        text: Text::from_section(
+            "Left Arrow Key: Animate Left Sprite\nRight Arrow Key: Animate Right Sprite",
+            TextStyle {
+                font_size: 40.0,
+                ..default()
+            },
+        ),
+        style: Style {
+            position_type: PositionType::Absolute,
+            top: Val::Px(100.0),
+            left: Val::Px(100.0),
+            ..default()
+        },
+        ..default()
+    });
 }
