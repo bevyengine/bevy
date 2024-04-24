1b97d99575e322998ec4582cfc16573f4abb4b2e
BD103 <59022059+BD103@users.noreply.github.com>
2024-04-24 08:04:46 -0400

    chore: add deprecation notice

--- crates/bevy_dynamic_plugin/src/lib.rs
+++ crates/bevy_dynamic_plugin/src/lib.rs
@@ -9,11 +9,22 @@
 //! This crate allows loading dynamic libraries (`.dylib`, `.so`) that export a single
 //! [`Plugin`](bevy_app::Plugin). For usage, see [`dynamically_load_plugin`].
 //!
+//! # Deprecation
+//!
+//! The current dynamic plugin system is unsound and will be removed in 0.15. You may be interested
+//! in the [Alternatives](#alternatives) listed below. If your use-case is not supported, please
+//! consider commenting on [#13080](https://github.com/bevyengine/bevy/pull/13080) describing how
+//! you use dynamic plugins in your project.
+//!
+//! # Warning
+//!
 //! Note that dynamic linking and loading is inherently unsafe because it allows executing foreign
 //! code. Additionally, Rust does not have a stable ABI and may produce
 //! incompatible libraries across Rust versions, or even subsequent compilations. This will not work
 //! well in scenarios such as modding, but can work if the dynamic plugins and the main app are
 //! built at the same time, such as with Downloadable Content (DLC) packs.
+//!
+//! # Alternatives
 //!
 //! You may be interested in these safer alternatives:
 //!
