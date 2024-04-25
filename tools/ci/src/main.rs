64e39916dbee45273c65ffe751c3b13e1abe6418
BD103 <59022059+BD103@users.noreply.github.com>
2024-04-25 13:11:09 -0400

    refactor(ci): restructure modules
    - Subcommands are now commands
    - Contents of `commands/mod.rs` are now in `main.rs`

--- tools/ci/src/main.rs
+++ tools/ci/src/main.rs
@@ -1,7 +1,12 @@
 //! CI script used for Bevy.
 
+mod ci;
+mod prepare;
 mod commands;
 
+pub(crate) use self::ci::*;
+pub(crate) use self::prepare::*;
+
 fn main() {
-    argh::from_env::<commands::CI>().run();
+    argh::from_env::<CI>().run();
 }
