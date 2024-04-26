de9dc9c20451f13a0ad0d3f122b46c84a45c2c18
Doonv <58695417+doonv@users.noreply.github.com>
2024-04-27 02:52:09 +0300

    Fix `CameraProjection` panic and improve `CameraProjectionPlugin` (#11808)
    # Objective
    
    Fix https://github.com/bevyengine/bevy/issues/11799 and improve
    `CameraProjectionPlugin`
    
    ## Solution
    
    `CameraProjectionPlugin` is now an all-in-one plugin for adding a custom
    `CameraProjection`. I also added `PbrProjectionPlugin` which is like
    `CameraProjectionPlugin` but for PBR.
    
    P.S. I'd like to get this merged after
    https://github.com/bevyengine/bevy/pull/11766.
    
    ---
    
    ## Changelog
    
    - Changed `CameraProjectionPlugin` to be an all-in-one plugin for adding
    a `CameraProjection`
    - Removed `VisibilitySystems::{UpdateOrthographicFrusta,
    UpdatePerspectiveFrusta, UpdateProjectionFrusta}`, now replaced with
    `VisibilitySystems::UpdateFrusta`
    - Added `PbrProjectionPlugin` for projection-specific PBR functionality.
    
    ## Migration Guide
    
    `VisibilitySystems`'s `UpdateOrthographicFrusta`,
    `UpdatePerspectiveFrusta`, and `UpdateProjectionFrusta` variants were
    removed, they were replaced with `VisibilitySystems::UpdateFrusta`

--- crates/bevy_render/src/view/visibility/mod.rs
+++ crates/bevy_render/src/view/visibility/mod.rs
@@ -15,10 +15,7 @@ use bevy_transform::{components::GlobalTransform, TransformSystem};
 use bevy_utils::{Parallel, TypeIdMap};
 
 use crate::{
-    camera::{
-        camera_system, Camera, CameraProjection, OrthographicProjection, PerspectiveProjection,
-        Projection,
-    },
+    camera::{Camera, CameraProjection},
     mesh::Mesh,
     primitives::{Aabb, Frustum, Sphere},
 };
@@ -239,12 +236,8 @@ pub enum VisibilitySystems {
     /// Label for the [`calculate_bounds`], `calculate_bounds_2d` and `calculate_bounds_text2d` systems,
     /// calculating and inserting an [`Aabb`] to relevant entities.
     CalculateBounds,
-    /// Label for the [`update_frusta<OrthographicProjection>`] system.
-    UpdateOrthographicFrusta,
-    /// Label for the [`update_frusta<PerspectiveProjection>`] system.
-    UpdatePerspectiveFrusta,
-    /// Label for the [`update_frusta<Projection>`] system.
-    UpdateProjectionFrusta,
+    /// Label for [`update_frusta`] in [`CameraProjectionPlugin`](crate::camera::CameraProjectionPlugin).
+    UpdateFrusta,
     /// Label for the system propagating the [`InheritedVisibility`] in a
     /// [`hierarchy`](bevy_hierarchy).
     VisibilityPropagate,
@@ -261,13 +254,7 @@ impl Plugin for VisibilityPlugin {
 
         app.configure_sets(
             PostUpdate,
-            (
-                CalculateBounds,
-                UpdateOrthographicFrusta,
-                UpdatePerspectiveFrusta,
-                UpdateProjectionFrusta,
-                VisibilityPropagate,
-            )
+            (CalculateBounds, UpdateFrusta, VisibilityPropagate)
                 .before(CheckVisibility)
                 .after(TransformSystem::TransformPropagate),
         )
@@ -275,24 +262,6 @@ impl Plugin for VisibilityPlugin {
             PostUpdate,
             (
                 calculate_bounds.in_set(CalculateBounds),
-                update_frusta::<OrthographicProjection>
-                    .in_set(UpdateOrthographicFrusta)
-                    .after(camera_system::<OrthographicProjection>)
-                    // We assume that no camera will have more than one projection component,
-                    // so these systems will run independently of one another.
-                    // FIXME: Add an archetype invariant for this https://github.com/bevyengine/bevy/issues/1481.
-                    .ambiguous_with(update_frusta::<PerspectiveProjection>)
-                    .ambiguous_with(update_frusta::<Projection>),
-                update_frusta::<PerspectiveProjection>
-                    .in_set(UpdatePerspectiveFrusta)
-                    .after(camera_system::<PerspectiveProjection>)
-                    // We assume that no camera will have more than one projection component,
-                    // so these systems will run independently of one another.
-                    // FIXME: Add an archetype invariant for this https://github.com/bevyengine/bevy/issues/1481.
-                    .ambiguous_with(update_frusta::<Projection>),
-                update_frusta::<Projection>
-                    .in_set(UpdateProjectionFrusta)
-                    .after(camera_system::<Projection>),
                 (visibility_propagate_system, reset_view_visibility).in_set(VisibilityPropagate),
                 check_visibility::<WithMesh>.in_set(CheckVisibility),
             ),
@@ -320,9 +289,7 @@ pub fn calculate_bounds(
 
 /// Updates [`Frustum`].
 ///
-/// This system is used in system sets [`VisibilitySystems::UpdateProjectionFrusta`],
-/// [`VisibilitySystems::UpdatePerspectiveFrusta`], and
-/// [`VisibilitySystems::UpdateOrthographicFrusta`].
+/// This system is used in [`CameraProjectionPlugin`](crate::camera::CameraProjectionPlugin).
 pub fn update_frusta<T: Component + CameraProjection + Send + Sync + 'static>(
     mut views: Query<
         (&GlobalTransform, &T, &mut Frustum),
