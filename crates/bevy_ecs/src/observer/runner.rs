a6732fed9f8de12044f0265547f4d220f141e993
James O'Brien <james.obrien@drafly.net>
2024-04-26 19:33:57 -0700

    Satisfy MIRI at the cost of performance

--- crates/bevy_ecs/src/observer/runner.rs
+++ crates/bevy_ecs/src/observer/runner.rs
@@ -1,3 +1,4 @@
+use crate as bevy_ecs;
 use crate::{
     component::{ComponentHooks, StorageType},
     system::ObserverSystem,
@@ -16,9 +17,13 @@ pub type BoxedObserverSystem<E = (), B = ()> = Box<dyn ObserverSystem<E, B>>;
 pub(crate) struct ObserverComponent {
     pub(crate) descriptor: ObserverDescriptor,
     pub(crate) runner: ObserverRunner,
-    pub(crate) system: Option<BoxedObserverSystem>,
     pub(crate) last_event_id: u32,
 }
+
+#[derive(Component)]
+#[component(storage = "SparseSet")]
+// This used to be in `ObserverComponent` but MIRI recently got a new lint that complained about the type erasure
+pub(crate) struct ObserverSystemComponent<E: 'static, B: Bundle>(BoxedObserverSystem<E, B>);
 
 impl Component for ObserverComponent {
     const STORAGE_TYPE: StorageType = StorageType::SparseSet;
@@ -45,7 +50,7 @@ impl ObserverComponent {
         world: &mut World,
         descriptor: ObserverDescriptor,
         system: impl IntoObserverSystem<E, B, M>,
-    ) -> Self {
+    ) -> (Self, ObserverSystemComponent<E, B>) {
         let mut system = IntoObserverSystem::into_system(system);
         assert!(
             !system.is_exclusive(),
@@ -53,53 +58,56 @@ impl ObserverComponent {
         );
         system.initialize(world);
         let system: BoxedObserverSystem<E, B> = Box::new(system);
-        Self {
-            descriptor,
-            runner: |mut world, trigger, ptr| {
-                let world = world.as_unsafe_world_cell();
-                let observer_cell =
+        (
+            Self {
+                descriptor,
+                runner: |mut world, trigger, ptr| {
+                    let world = world.as_unsafe_world_cell();
+                    let observer_cell =
                 // SAFETY: Observer was triggered so must still exist in world
                     unsafe { world.get_entity(trigger.observer).debug_checked_unwrap() };
-                // SAFETY: Observer was triggered so must have an `ObserverComponent`
-                let mut state = unsafe {
-                    observer_cell
-                        .get_mut::<ObserverComponent>()
-                        .debug_checked_unwrap()
-                };
+                    // SAFETY: Observer was triggered so must have an `ObserverComponent`
+                    let mut state = unsafe {
+                        observer_cell
+                            .get_mut::<ObserverComponent>()
+                            .debug_checked_unwrap()
+                    };
 
-                // TODO: Move this check into the observer cache to avoid dynamic dispatch
-                // SAFETY: We only access world metadata
-                let last_event = unsafe { world.world_metadata() }.last_event_id;
-                if state.last_event_id == last_event {
-                    return;
-                }
-                state.last_event_id = last_event;
+                    // TODO: Move this check into the observer cache to avoid dynamic dispatch
+                    // SAFETY: We only access world metadata
+                    let last_event = unsafe { world.world_metadata() }.last_event_id;
+                    if state.last_event_id == last_event {
+                        return;
+                    }
+                    state.last_event_id = last_event;
 
-                // SAFETY: Caller ensures `ptr` is castable to `&mut E`
-                let observer: Observer<E, B> = Observer::new(unsafe { ptr.deref_mut() }, trigger);
-                // SAFETY: System is from component
-                let mut system: Box<dyn ObserverSystem<E, B>> = unsafe {
-                    let system = state.system.take().debug_checked_unwrap();
-                    std::mem::transmute(system)
-                };
+                    // SAFETY: Caller ensures `ptr` is castable to `&mut E`
+                    let observer: Observer<E, B> =
+                        Observer::new(unsafe { ptr.deref_mut() }, trigger);
+                    // SAFETY: Observer was triggered so must have an `ObserverSystemComponent`
+                    let system = unsafe {
+                        &mut observer_cell
+                            .get_mut::<ObserverSystemComponent<E, B>>()
+                            .debug_checked_unwrap()
+                            .0
+                    };
 
-                system.update_archetype_component_access(world);
+                    system.update_archetype_component_access(world);
 
-                // SAFETY:
-                // - `update_archetype_component_access` was just called
-                // - there are no outsanding references to world except a private component
-                // - system is an `ObserverSystem` so won't mutate world beyond the access of a `DeferredWorld`
-                // - system is the same type erased system from above
-                unsafe {
-                    system.run_unsafe(std::mem::transmute(observer), world);
-                    system.queue_deferred(world.into_deferred());
-                    state.system = Some(std::mem::transmute(system));
-                }
+                    // SAFETY:
+                    // - `update_archetype_component_access` was just called
+                    // - there are no outsanding references to world except a private component
+                    // - system is an `ObserverSystem` so won't mutate world beyond the access of a `DeferredWorld`
+                    // - system is the same type erased system from above
+                    unsafe {
+                        system.run_unsafe(std::mem::transmute(observer), world);
+                        system.queue_deferred(world.into_deferred());
+                    }
+                },
+                last_event_id: 0,
             },
-            last_event_id: 0,
-            // SAFETY: Same layout
-            system: Some(unsafe { std::mem::transmute(system) }),
-        }
+            ObserverSystemComponent(system),
+        )
     }
 
     pub(crate) fn from_runner(descriptor: ObserverDescriptor, runner: ObserverRunner) -> Self {
@@ -107,7 +115,6 @@ impl ObserverComponent {
             descriptor,
             runner,
             last_event_id: 0,
-            system: None,
         }
     }
 }
