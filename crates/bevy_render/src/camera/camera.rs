907106fa7b31c60e9c82c5a3ff4c8fb79328357f
Patrick Walton <pcwalton@mimiga.net>
2024-04-27 22:09:11 -0700

    Warn if GPU culling isn't supported due to platform limitations

--- crates/bevy_render/src/camera/camera.rs
+++ crates/bevy_render/src/camera/camera.rs
@@ -28,7 +28,7 @@ use bevy_math::{vec2, Dir3, Mat4, Ray3d, Rect, URect, UVec2, UVec4, Vec2, Vec3};
 use bevy_reflect::prelude::*;
 use bevy_render_macros::ExtractComponent;
 use bevy_transform::components::GlobalTransform;
-use bevy_utils::tracing::warn;
+use bevy_utils::{tracing::warn, warn_once};
 use bevy_utils::{HashMap, HashSet};
 use bevy_window::{
     NormalizedWindowRef, PrimaryWindow, Window, WindowCreated, WindowRef, WindowResized,
@@ -923,8 +923,14 @@ pub fn extract_cameras(
                 commands.insert(perspective.clone());
             }
 
-            if gpu_culling && *gpu_preprocessing_support == GpuPreprocessingSupport::Culling {
-                commands.insert(GpuCulling);
+            if gpu_culling {
+                if *gpu_preprocessing_support == GpuPreprocessingSupport::Culling {
+                    commands.insert(GpuCulling);
+                } else {
+                    warn_once!(
+                        "GPU culling isn't supported on this platform; ignoring `GpuCulling`."
+                    );
+                }
             }
         }
     }
