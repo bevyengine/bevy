23f68bf2d29073821ea633ac8fcad397ec01efca
BD103 <59022059+BD103@users.noreply.github.com>
2024-04-29 08:18:16 -0400

    Revert "feat: use macro for declaring commands"
    This reverts commit dca3cca2bbf60b9e8b7f04fa07600508e387f893.

--- tools/ci/src/commands.rs
+++ tools/ci/src/commands.rs
@@ -1,63 +1,29 @@
-/// This macro simplifies declaring new commands and aliases.
-/// 
-/// # Example
-/// 
-/// ```rust,no_compile
-/// // This macro...
-/// define_commands! {
-///     commands: [
-///         test::TestCommand,
-///     ],
-///     alias: [
-///         build: BuildCommand,
-///     ],
-/// }
-/// 
-/// // ...generates this.
-/// mod test;
-/// pub(crate) use test::TestCommand;
-/// 
-/// mod build;
-/// pub(crate) use build::BuildCommand;
-/// ```
-macro_rules! define_commands {
-    {
-        commands: [
-            $($command_module:ident::$command:ident),* $(,)?
-        ],
-        aliases: [
-            $($alias_module:ident::$alias:ident),* $(,)?
-        ] $(,)?
-    } => {
-        $(
-            mod $command_module;
-            pub(crate) use $command_module::$command;
-        )*
+pub(crate) use bench_check::*;
+pub(crate) use cfg_check::*;
+pub(crate) use clippy::*;
+pub(crate) use compile::*;
+pub(crate) use compile_check::*;
+pub(crate) use compile_fail::*;
+pub(crate) use doc::*;
+pub(crate) use doc_check::*;
+pub(crate) use doc_test::*;
+pub(crate) use example_check::*;
+pub(crate) use format::*;
+pub(crate) use lints::*;
+pub(crate) use test::*;
+pub(crate) use test_check::*;
 
-        $(
-            mod $alias_module;
-            pub(crate) use $alias_module::$alias;
-        )*
-    };
-}
-
-define_commands! {
-    commands: [
-        bench_check::BenchCheckCommand,
-        cfg_check::CfgCheckCommand,
-        clippy::ClippyCommand,
-        compile_check::CompileCheckCommand,
-        compile_fail::CompileFailCommand,
-        doc_check::DocCheckCommand,
-        doc_test::DocTestCommand,
-        example_check::ExampleCheckCommand,
-        format::FormatCommand,
-        test::TestCommand,
-        test_check::TestCheckCommand,
-    ],
-    aliases: [
-        compile::CompileCommand,
-        doc::DocCommand,
-        lints::LintsCommand,
-    ],
-}
+mod bench_check;
+mod cfg_check;
+mod clippy;
+mod compile;
+mod compile_check;
+mod compile_fail;
+mod doc;
+mod doc_check;
+mod doc_test;
+mod example_check;
+mod format;
+mod lints;
+mod test;
+mod test_check;
