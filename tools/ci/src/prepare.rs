57dc6361678e25af98b783dce8d6d084b78cf9ab
BD103 <59022059+BD103@users.noreply.github.com>
2024-04-29 08:51:59 -0400

    chore: document `Prepare` and associated items

--- tools/ci/src/prepare.rs
+++ tools/ci/src/prepare.rs
@@ -2,10 +2,33 @@ use bitflags::bitflags;
 
 /// Trait for preparing a subcommand to be run.
 pub trait Prepare {
+    /// A method that returns a list of [`PreparedCommand`]s to be run for a given shell and flags.
+    /// 
+    /// # Example
+    /// 
+    /// ```
+    /// # use crate::{Flag, Prepare, PreparedCommand};
+    /// # use argh::FromArgs;
+    /// # use xshell::Shell;
+    /// #
+    /// #[derive(FromArgs)]
+    /// #[argh(subcommand, name = "check")]
+    /// struct CheckCommand {}
+    /// 
+    /// impl Prepare for CheckCommand {
+    ///     fn prepare<'a>(&self, sh: &'a Shell, flags: Flag) -> Vec<PreparedCommand<'a>> {
+    ///         vec![PreparedCommand::new::<Self>(
+    ///             cmd!(sh, "cargo check --workspace"),
+    ///             "Please fix linter errors",
+    ///         )]
+    ///     }
+    /// }
+    /// ```
     fn prepare<'a>(&self, sh: &'a xshell::Shell, flags: Flag) -> Vec<PreparedCommand<'a>>;
 }
 
 bitflags! {
+    /// Flags that modify how commands are run.
     #[derive(Clone, Copy, Debug, PartialEq, Eq)]
     pub struct Flag: u32 {
         /// Forces certain checks to continue running even if they hit an error.
@@ -13,6 +36,7 @@ bitflags! {
     }
 }
 
+/// A command with associated metadata, created from a command that implements [`Prepare`].
 #[derive(Debug)]
 pub struct PreparedCommand<'a> {
     /// The name of the command.
@@ -32,6 +56,13 @@ pub struct PreparedCommand<'a> {
 }
 
 impl<'a> PreparedCommand<'a> {
+    /// Creates a new [`PreparedCommand`] from a [`Cmd`] and a failure message.
+    /// 
+    /// The other fields of [`PreparedCommand`] are filled in with their default values.
+    /// 
+    /// For more information about creating a [`Cmd`], please see the [`cmd!`](xshell::cmd) macro.
+    /// 
+    /// [`Cmd`]: xshell::Cmd
     pub fn new<T: argh::SubCommand>(
         command: xshell::Cmd<'a>,
         failure_message: &'static str,
@@ -45,11 +76,13 @@ impl<'a> PreparedCommand<'a> {
         }
     }
 
+    /// A builder that overwrites the current sub-directory with a new value.
     pub fn with_subdir(mut self, subdir: &'static str) -> Self {
         self.subdir = Some(subdir);
         self
     }
 
+    /// A builder that adds a new environmental variable to the list.
     pub fn with_env_var(mut self, key: &'static str, value: &'static str) -> Self {
         self.env_vars.push((key, value));
         self
