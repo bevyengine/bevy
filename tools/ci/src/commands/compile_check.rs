0193ed00cdaada2d78fec733663d3f36f5041511
Brezak <bezak.adamko@gmail.com>
2024-05-02 15:57:11 +0200

    Implement json ouptput in our CI tool

--- tools/ci/src/commands/compile_check.rs
+++ tools/ci/src/commands/compile_check.rs
@@ -1,17 +1,24 @@
-use crate::{Flag, Prepare, PreparedCommand};
+use crate::{json::message_format_option, Flag, Prepare, PreparedCommand};
 use argh::FromArgs;
 use xshell::cmd;
 
 /// Checks that the project compiles.
 #[derive(FromArgs, Default)]
 #[argh(subcommand, name = "compile-check")]
-pub struct CompileCheckCommand {}
+pub struct CompileCheckCommand {
+    #[argh(switch)]
+    /// emit errors as json
+    emit_json: bool,
+}
 
 impl Prepare for CompileCheckCommand {
     fn prepare<'a>(&self, sh: &'a xshell::Shell, _flags: Flag) -> Vec<PreparedCommand<'a>> {
+        let format_flag = message_format_option(self.emit_json);
+
         vec![PreparedCommand::new::<Self>(
-            cmd!(sh, "cargo check --workspace"),
+            cmd!(sh, "cargo check {format_flag} --workspace"),
             "Please fix compiler errors in output above.",
-        )]
+        )
+        .with_json(self.emit_json)]
     }
 }
