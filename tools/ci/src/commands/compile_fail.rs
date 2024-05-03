e79390ea658d95caec2f38a125d807e279e8424f
BD103 <59022059+BD103@users.noreply.github.com>
2024-05-02 22:38:42 -0400

    refactor: move `bevy_reflect_compile_fail_tests` to `bevy_reflect/compile_fail`

--- tools/ci/src/commands/compile_fail.rs
+++ tools/ci/src/commands/compile_fail.rs
@@ -32,10 +32,10 @@ impl Prepare for CompileFailCommand {
         // - See crates/bevy_reflect_compile_fail_tests/README.md
         commands.push(
             PreparedCommand::new::<Self>(
-                cmd!(sh, "cargo test --target-dir ../../target {no_fail_fast}"),
+                cmd!(sh, "cargo test --target-dir ../../../target {no_fail_fast}"),
                 "Compiler errors of the Reflect compile fail tests seem to be different than expected! Check locally and compare rust versions.",
             )
-            .with_subdir("crates/bevy_reflect_compile_fail_tests"),
+            .with_subdir("crates/bevy_reflect/compile_fail"),
         );
 
         // Macro Compile Fail Tests
