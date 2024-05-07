f8dfed6c99fafe3661656a37e66490b212f57fdb
BD103 <59022059+BD103@users.noreply.github.com>
2024-05-06 22:55:52 -0400

    feat: create `check_docsrs_metadata.sh`

---
+++ tools/check_docsrs_metadata.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+# Switch to the root project directory, letting this script be called from any folder.
+cd $(dirname $0)/../
+
+echo Gathering crate metadata for current workspace...
+
+# Retrieve the metadata of all crates within this workspace.
+CRATE_METADATA=$(cargo metadata --format-version 1 --no-deps)
+
+# This filters the list of crates to only include crates that will be published to crates.io. It
+# does this by removing all crates that set `publish = false`, or as `cargo-metadata` likes to say,
+# `publish = []`. It then only selects the fields that we need: name, path, and docs.rs metadata.
+PUBLIC_CRATES=$(
+    echo $CRATE_METADATA | \
+        jq '[.packages[] | select(.publish != []) | { name, docsrs: .metadata.docs.rs, manifest_path }]'
+)
+
+# Here we construct what a valid docs.rs metadata would look like. If you want to modify the
+# required configuration, change these lines.
+echo Constructing metadata requirements...
+
+# Build crate with all features enabled and visible.
+ALL_FEATURES='true'
+
+# Scrape examples and embed them in documentation.
+#   - https://doc.rust-lang.org/cargo/reference/unstable.html#scrape-examples
+CARGO_ARGS='[ "-Zunstable-options", "-Zrustdoc-scrape-examples" ]'
+
+# Enable `#[cfg(docsrs)]` blocks for all crates and dependencies, so they can use nightly features.
+RUSTDOC_ARGS='[ "-Zunstable-options", "--cfg", "docsrs" ]'
+
+# Combine all the options together into a single JSON string.
+DOCSRS_METADATA="{ \"all-features\": $ALL_FEATURES, \"cargo-args\": $CARGO_ARGS, \"rustdoc-args\": $RUSTDOC_ARGS }"
+
+echo Crates will be validated to ensure they have the following fields for \`[package.metadata.docs.rs]\`:
+echo $DOCSRS_METADATA | jq '.' # Echo metadata using `jq`'s pretty-printing.
+
+echo Validating all crates...
+
+INVALID_CRATES=$(echo $PUBLIC_CRATES | jq "[.[] | select(.docsrs != $DOCSRS_METADATA)]")
+# INVALID_CRATES=$(echo $PUBLIC_CRATES | jq "[.[] | select(.name == \"yef\")]")
+
+# If there are no invalid crates.
+if [[ $INVALID_CRATES == "[]" ]]; then
+    echo All crates are valid! \(Checked $(echo $PUBLIC_CRATES | jq 'length').\)
+    exit 0
+else
+    # Log how many crates are invalid.
+    echo $(echo $INVALID_CRATES | jq 'length') crates are invalid:
+
+    # Interate over each crate, splitting them up.
+    for CRATE in $(echo $INVALID_CRATES | jq -c '.[]'); do
+        echo - $(echo $CRATE | jq '[.name, .manifest_path] | join(": ")'): 
+    done
+
+    # Return with a non-zero exit code, denoting an error.
+    exit 1
+fi
