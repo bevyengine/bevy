3d9a9b7a9bf4d22a396476d29de14f6d2d3e44f7
BD103 <59022059+BD103@users.noreply.github.com>
2024-05-07 09:29:09 -0400

    feat: construct requirements before parsing metadata

--- tools/check_docsrs_metadata.sh
+++ tools/check_docsrs_metadata.sh
@@ -3,19 +3,6 @@
 # Switch to the root project directory, letting this script be called from any folder.
 cd $(dirname $0)/../
 
-echo Gathering crate metadata for current workspace...
-
-# Retrieve the metadata of all crates within this workspace.
-CRATE_METADATA=$(cargo metadata --format-version 1 --no-deps)
-
-# This filters the list of crates to only include crates that will be published to crates.io. It
-# does this by removing all crates that set `publish = false`, or as `cargo-metadata` likes to say,
-# `publish = []`. It then only selects the fields that we need: name, path, and docs.rs metadata.
-PUBLIC_CRATES=$(
-    echo $CRATE_METADATA | \
-        jq '[.packages[] | select(.publish != []) | { name, docsrs: .metadata.docs.rs, manifest_path }]'
-)
-
 # Here we construct what a valid docs.rs metadata would look like. If you want to modify the
 # required configuration, change these lines.
 echo Constructing metadata requirements...
@@ -36,18 +23,35 @@ DOCSRS_METADATA="{ \"all-features\": $ALL_FEATURES, \"cargo-args\": $CARGO_ARGS,
 echo Crates will be validated to ensure they have the following fields for \`[package.metadata.docs.rs]\`:
 echo $DOCSRS_METADATA | jq '.' # Echo metadata using `jq`'s pretty-printing.
 
+echo Gathering crate metadata for current workspace...
+
+# Retrieve the metadata of all crates within this workspace.
+CRATE_METADATA=$(cargo metadata --format-version 1 --no-deps)
+
+# This filters the list of crates to only include crates that will be published to crates.io. It
+# does this by removing all crates that set `publish = false`, or as `cargo-metadata` likes to say,
+# `publish = []`. It then only selects the fields that we need: name, path, and docs.rs metadata.
+PUBLIC_CRATES=$(
+    echo $CRATE_METADATA | \
+        jq '[.packages[] | select(.publish != []) | { name, docsrs: .metadata.docs.rs, manifest_path }]'
+)
+
 echo Validating all crates...
 
 INVALID_CRATES=$(echo $PUBLIC_CRATES | jq "[.[] | select(.docsrs != $DOCSRS_METADATA)]")
 # INVALID_CRATES=$(echo $PUBLIC_CRATES | jq "[.[] | select(.name == \"yef\")]")
 
+PUBLIC_CRATES_LEN=$(echo $PUBLIC_CRATES | jq 'length')
+
 # If there are no invalid crates.
 if [[ $INVALID_CRATES == "[]" ]]; then
-    echo All crates are valid! \(Checked $(echo $PUBLIC_CRATES | jq 'length').\)
+    echo All crates are valid! \(Checked $PUBLIC_CRATES_LEN.\)
     exit 0
 else
+    INVALID_CRATES_LEN=$(echo $INVALID_CRATES | jq 'length')
+
     # Log how many crates are invalid.
-    echo $(echo $INVALID_CRATES | jq 'length') crates are invalid:
+    echo $INVALID_CRATES_LEN crates are invalid \(out of $PUBLIC_CRATES_LEN\):
 
     # Interate over each crate, splitting them up.
     for CRATE in $(echo $INVALID_CRATES | jq -c '.[]'); do
