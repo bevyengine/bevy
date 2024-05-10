087cd7f5846e2ea5d185d6ade20d0bbac19f858d
Gino Valente <gino.valente.code@gmail.com>
2024-05-10 13:46:24 -0700

    Fix compile-fail tests

--- crates/bevy_reflect/compile_fail/tests/reflect_derive/generics_fail.rs
+++ crates/bevy_reflect/compile_fail/tests/reflect_derive/generics_fail.rs
@@ -13,6 +13,7 @@ struct NoReflect(f32);
 fn main() {
     let mut foo: Box<dyn Struct> = Box::new(Foo::<NoReflect> { a: NoReflect(42.0) });
     //~^ ERROR: `NoReflect` does not provide type registration information
+    //~| ERROR: `NoReflect` can not provide type information through reflection
 
     // foo doesn't implement Reflect because NoReflect doesn't implement Reflect
     foo.get_field::<NoReflect>("a").unwrap();
