de3f0530afc9f83cac5dea0e3d80f6281c0a4ad2
Litttle_fish <38809254+Litttlefish@users.noreply.github.com>
2024-05-16 23:06:14 +0800

    Update build.rs

--- examples/mobile/build.rs
+++ examples/mobile/build.rs
@@ -16,6 +16,7 @@ fn android() {
     if let Ok(output_path) = env::var("CARGO_NDK_OUTPUT_PATH") {
         let sysroot_libs_path = PathBuf::from(env::var_os("CARGO_NDK_SYSROOT_LIBS_PATH").unwrap());
         let lib_path = sysroot_libs_path.join("libc++_shared.so");
+        dbg!(&lib_path);
         std::fs::copy(
             lib_path,
             Path::new(&output_path)
