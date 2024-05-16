8cee4344fe93e410a7ff00c4aff8fef64d7f1601
Rob Parrett <robparrett@gmail.com>
2024-05-16 14:27:50 -0700

    Make instruction text consistent with other examples

--- examples/3d/deferred_rendering.rs
+++ examples/3d/deferred_rendering.rs
@@ -207,14 +207,14 @@ fn setup(
         TextBundle::from_section(
             "",
             TextStyle {
-                font_size: 18.0,
+                font_size: 20.0,
                 ..default()
             },
         )
         .with_style(Style {
             position_type: PositionType::Absolute,
-            top: Val::Px(10.0),
-            left: Val::Px(10.0),
+            top: Val::Px(12.0),
+            left: Val::Px(12.0),
             ..default()
         }),
     );
