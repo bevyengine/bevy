612e77ef7810759b855cd2853e620a10a5081ebf
Spooky Th Ghost <30451500+spooky-th-ghost@users.noreply.github.com>
2024-05-20 15:46:25 -0400

    Adds doc note that Timer and Stopwatch must be progressed manually (#13441)
    # Objective
    - Fix #13421 
    
    ## Solution
    
    - Add an explicit note at the root of each struct that they must be
    ticked manually
    
    ## Testing
    
    - Generated the docs and the changes look good
    ---
    
    ---------
    
    Co-authored-by: François Mockers <francois.mockers@vleue.com>
    Co-authored-by: Matty <weatherleymatthew@gmail.com>

--- crates/bevy_time/src/stopwatch.rs
+++ crates/bevy_time/src/stopwatch.rs
@@ -4,6 +4,7 @@ use bevy_utils::Duration;
 
 /// A Stopwatch is a struct that track elapsed time when started.
 ///
+/// Note that in order to advance the stopwatch [`tick`](Stopwatch::tick) **MUST** be called.
 /// # Examples
 ///
 /// ```
