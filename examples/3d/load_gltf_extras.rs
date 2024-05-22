f052706c6a3b991be515859fbd47e500fab07039
kaosat.dev <kaosat.dev@gmail.com>
2024-05-22 11:19:16 +0200

    chore(): streamlined gltf extras example

--- examples/3d/load_gltf_extras.rs
+++ examples/3d/load_gltf_extras.rs
@@ -1,14 +1,12 @@
-//! Loads and renders a glTF file as a scene, and shows all the different `gltf_extras`.
+//! Loads and renders a glTF file as a scene, and list all the different `gltf_extras`.
 
 use bevy::{
     gltf::{GltfExtras, GltfMaterialExtras, GltfMeshExtras, GltfSceneExtras},
-    pbr::{CascadeShadowConfigBuilder, DirectionalLightShadowMap},
     prelude::*,
 };
 
 fn main() {
     App::new()
-        .insert_resource(DirectionalLightShadowMap { size: 4096 })
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, setup)
         .add_systems(Update, check_for_gltf_extras)
@@ -16,36 +14,19 @@ fn main() {
 }
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
-    commands.spawn((
-        Camera3dBundle {
-            transform: Transform::from_xyz(0.7, 0.7, 1.0)
-                .looking_at(Vec3::new(0.0, 0.3, 0.0), Vec3::Y),
-            ..default()
-        },
-        EnvironmentMapLight {
-            diffuse_map: asset_server.load("environment_maps/pisa_diffuse_rgb9e5_zstd.ktx2"),
-            specular_map: asset_server.load("environment_maps/pisa_specular_rgb9e5_zstd.ktx2"),
-            intensity: 250.0,
-        },
-    ));
+    commands.spawn(Camera3dBundle {
+        transform: Transform::from_xyz(0.7, 0.7, 1.0).looking_at(Vec3::new(0.0, 0.3, 0.0), Vec3::Y),
+        ..default()
+    });
 
     commands.spawn(DirectionalLightBundle {
         directional_light: DirectionalLight {
             shadows_enabled: true,
             ..default()
         },
-        // This is a relatively small scene, so use tighter shadow
-        // cascade bounds than the default for better quality.
-        // We also adjusted the shadow map to be larger since we're
-        // only using a single cascade.
-        cascade_shadow_config: CascadeShadowConfigBuilder {
-            num_cascades: 1,
-            maximum_distance: 1.6,
-            ..default()
-        }
-        .into(),
         ..default()
     });
+    // a barebones scene containing one of each gltf_extra type
     commands.spawn(SceneBundle {
         scene: asset_server.load("models/extras/gltf_extras.glb#Scene0"),
         ..default()
@@ -53,34 +34,40 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
 }
 
 fn check_for_gltf_extras(
-    gltf_extras_query: Query<(Entity, Option<&Name>, &GltfExtras)>,
-    gltf_scene_extras_query: Query<(Entity, Option<&Name>, &GltfSceneExtras)>,
-    gltf_mesh_extras_query: Query<(Entity, Option<&Name>, &GltfMeshExtras)>,
-    gltf_material_extras_query: Query<(Entity, Option<&Name>, &GltfMaterialExtras)>,
+    gltf_extras_query: Query<(Entity, Option<&Name>, &GltfExtras), Added<GltfExtras>>,
+    gltf_scene_extras_query: Query<
+        (Entity, Option<&Name>, &GltfSceneExtras),
+        Added<GltfSceneExtras>,
+    >,
+    gltf_mesh_extras_query: Query<(Entity, Option<&Name>, &GltfMeshExtras), Added<GltfMeshExtras>>,
+    gltf_material_extras_query: Query<
+        (Entity, Option<&Name>, &GltfMaterialExtras),
+        Added<GltfMaterialExtras>,
+    >,
 ) {
     for extra in gltf_extras_query.iter() {
-        println!(
+        info!(
             "primitive extra for {:?} : {:?} (id: {})",
             extra.1, extra.2, extra.0
         );
     }
 
     for extra in gltf_scene_extras_query.iter() {
-        println!(
+        info!(
             "scene extra for {:?} : {:?} (id: {})",
             extra.1, extra.2, extra.0
         );
     }
 
     for extra in gltf_mesh_extras_query.iter() {
-        println!(
+        info!(
             "mesh extra for {:?} : {:?} (id: {})",
             extra.1, extra.2, extra.0
         );
     }
 
     for extra in gltf_material_extras_query.iter() {
-        println!(
+        info!(
             "material extra for {:?} : {:?} (id: {})",
             extra.1, extra.2, extra.0
         );
