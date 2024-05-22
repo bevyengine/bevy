4330a7b7197f86f3bea70e80dfed775fbffa75f7
Olle Lukowski <ollelukowski@hotmail.com>
2024-05-22 14:33:49 +0200

    Change `Rect` to ensure it's `EMPTY` constant has an infinitly negative size.

--- crates/bevy_math/src/rects/rect.rs
+++ crates/bevy_math/src/rects/rect.rs
@@ -20,10 +20,11 @@ pub struct Rect {
 
 impl Rect {
     /// An empty `Rect`, represented by maximum and minimum corner points
-    /// with all `f32::MAX` values.
+    /// at `Vec2::NEG_INFINITY` and `Vec2::INFINITY`, respectively.
+    /// This is so the `Rect` has a infinitly negative size.
     pub const EMPTY: Self = Self {
-        max: Vec2::MAX,
-        min: Vec2::MAX,
+        max: Vec2::NEG_INFINITY,
+        min: Vec2::INFINITY,
     };
     /// Create a new rectangle from two corner points.
     ///
