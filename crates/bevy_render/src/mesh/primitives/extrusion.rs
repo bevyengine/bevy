74b3d53369afa051ea8e10c309d51c97de5d0856
Lynn Büttgenbach <62256001+solis-lumine-vorago@users.noreply.github.com>
2024-05-24 03:47:44 +0200

    Improve docs and add more code comments

--- crates/bevy_render/src/mesh/primitives/extrusion.rs
+++ crates/bevy_render/src/mesh/primitives/extrusion.rs
@@ -12,6 +12,12 @@ pub enum PerimeterSegment {
     /// This segment of the perimeter will be shaded smooth.
     ///
     /// This has the effect of rendering the segment's faces with softened edges, so it is appropriate for curved shapes.
+    ///
+    /// The normals for the vertices that are part of this segment will be calculated based on the positions of their neighbours.
+    /// Each normal is interpolated between the normals of the two line segments connecting it with its neighbours.
+    /// Closer vertices have a stronger effect on the normal than more distant ones.
+    ///
+    /// Since the vertices corresponding to the first and last indices do not have two neighbouring vertices, their normals must be provided manually.
     Smooth {
         /// The normal of the first vertex.
         first_normal: Vec2,
@@ -219,26 +225,34 @@ where
             let mut indices = Vec::with_capacity(index_count);
             let mut uvs = Vec::with_capacity(vert_count);
 
+            // Compute the amount of horizontal space allocated to each segment of the perimeter.
             let uv_segment_delta = 1. / perimeter.len() as f32;
-            for (i, skin) in perimeter.into_iter().enumerate() {
+            for (i, segment) in perimeter.into_iter().enumerate() {
+                // The start of the x range of the area of the current perimeter-segment.
                 let uv_start = i as f32 * uv_segment_delta;
 
-                match skin {
+                match segment {
                     PerimeterSegment::Flat {
-                        indices: skin_indices,
+                        indices: segment_indices,
                     } => {
-                        let uv_delta = uv_segment_delta / (skin_indices.len() - 1) as f32;
-                        for i in 0..(skin_indices.len() - 1) {
+                        let uv_delta = uv_segment_delta / (segment_indices.len() - 1) as f32;
+                        for i in 0..(segment_indices.len() - 1) {
                             let uv_x = uv_start + uv_delta * i as f32;
-                            let a = cap_verts[skin_indices[i] as usize];
-                            let b = cap_verts[skin_indices[i + 1] as usize];
+                            // Get the positions for the current and the next index.
+                            let a = cap_verts[segment_indices[i] as usize];
+                            let b = cap_verts[segment_indices[i + 1] as usize];
+
+                            // Get the index of the next vertex added to the mantel.
                             let index = positions.len() as u32;
 
+                            // Push the positions of the two indices and their equivalent points on the back face.
+                            // This works, since the front face has already been moved to the correct depth.
                             positions.push(a);
                             positions.push(b);
                             positions.push([a[0], a[1], -a[2]]);
                             positions.push([b[0], b[1], -b[2]]);
 
+                            // UVs for the mantel are between (0, 0.5) and (1, 1).
                             uvs.extend_from_slice(&[
                                 [uv_x, 0.5],
                                 [uv_x + uv_delta, 0.5],
@@ -246,11 +260,13 @@ where
                                 [uv_x + uv_delta, 1.],
                             ]);
 
+                            // The normal is calculated to be the normal of the line segment connecting a and b.
                             let n = Vec3::from_array([b[1] - a[1], a[0] - b[0], 0.])
                                 .normalize_or_zero()
                                 .to_array();
                             normals.extend_from_slice(&[n; 4]);
 
+                            // Add the indices for the vertices created above to the mesh.
                             indices.extend_from_slice(&[
                                 index,
                                 index + 2,
@@ -264,38 +280,54 @@ where
                     PerimeterSegment::Smooth {
                         first_normal,
                         last_normal,
-                        indices: skin_indices,
+                        indices: segment_indices,
                     } => {
-                        let uv_delta = uv_segment_delta / (skin_indices.len() - 1) as f32;
+                        let uv_delta = uv_segment_delta / (segment_indices.len() - 1) as f32;
+
+                        // Since the indices for this segment will be added after its vertices have been added,
+                        // we need to store the index of the first vertex that is part of this segment.
                         let base_index = positions.len() as u32;
 
-                        if let Some(i) = skin_indices.first() {
+                        // If there is a first vertex, we need to add it and its counterpart on the back face.
+                        // The normal is provided by `segment.first_normal`.
+                        if let Some(i) = segment_indices.first() {
                             let p = cap_verts[*i as usize];
                             positions.push(p);
                             positions.push([p[0], p[1], -p[2]]);
                             uvs.extend_from_slice(&[[uv_start, 0.5], [uv_start, 1.]]);
                             normals.extend_from_slice(&[first_normal.extend(0.).to_array(); 2]);
                         }
-                        for i in 1..(skin_indices.len() - 1) {
-                            let uv_x = uv_start + uv_delta * i as f32;
-                            let a = cap_verts[skin_indices[i - 1] as usize];
-                            let b = cap_verts[skin_indices[i] as usize];
-                            let c = cap_verts[skin_indices[i + 1] as usize];
 
+                        // For all points inbetween the first and last vertices, we can automatically compute the normals.
+                        for i in 1..(segment_indices.len() - 1) {
+                            let uv_x = uv_start + uv_delta * i as f32;
+
+                            // Get the positions for the last, current and the next index.
+                            let a = cap_verts[segment_indices[i - 1] as usize];
+                            let b = cap_verts[segment_indices[i] as usize];
+                            let c = cap_verts[segment_indices[i + 1] as usize];
+
+                            // Add the current vertex and its counterpart on the backface
                             positions.push(b);
                             positions.push([b[0], b[1], -b[2]]);
 
                             uvs.extend_from_slice(&[[uv_x, 0.5], [uv_x, 1.]]);
 
+                            // The normal for the current vertices can be calculated based on the two neighbouring vertices.
+                            // The normal is interpolated between the normals of the two line segments connecting the current vertex with its neighbours.
+                            // Closer vertices have a stronger effect on the normal than more distant ones.
                             let n = {
                                 let ab = Vec2::from_slice(&b) - Vec2::from_slice(&a);
                                 let bc = Vec2::from_slice(&c) - Vec2::from_slice(&b);
-                                let n = ab + bc;
+                                let n = ab.normalize_or_zero() + bc.normalize_or_zero();
                                 Vec2::new(n.y, -n.x).normalize().extend(0.).to_array()
                             };
                             normals.extend_from_slice(&[n; 2]);
                         }
-                        if let Some(i) = skin_indices.last() {
+
+                        // If there is a last vertex, we need to add it and its counterpart on the back face.
+                        // The normal is provided by `segment.last_normal`.
+                        if let Some(i) = segment_indices.last() {
                             let p = cap_verts[*i as usize];
                             positions.push(p);
                             positions.push([p[0], p[1], -p[2]]);
@@ -306,7 +338,7 @@ where
                             normals.extend_from_slice(&[last_normal.extend(0.).to_array(); 2]);
                         }
 
-                        for i in 0..(skin_indices.len() as u32 - 1) {
+                        for i in 0..(segment_indices.len() as u32 - 1) {
                             let index = base_index + 2 * i;
                             indices.extend_from_slice(&[
                                 index,
