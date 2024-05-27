f8b745fb7f6c8e0a10fb8fb918390551cf2400d5
rydb <ryan.dobe@gmail.com>
2024-05-27 15:58:43 -0500

    added helper function for making loading assets from a folder easier

--- crates/bevy_asset/src/folder.rs
+++ crates/bevy_asset/src/folder.rs
@@ -1,5 +1,8 @@
-use crate as bevy_asset;
+use std::any::TypeId;
+
+use crate::{self as bevy_asset, Assets, Handle};
 use crate::{Asset, UntypedHandle};
+use bevy_ecs::system::Res;
 use bevy_reflect::TypePath;
 
 /// A "loaded folder" containing handles for all assets stored in a given [`AssetPath`].
@@ -9,4 +12,27 @@ use bevy_reflect::TypePath;
 pub struct LoadedFolder {
     #[dependency]
     pub handles: Vec<UntypedHandle>,
+}
+
+/// loads assets of type T from a given [`LoadedFolder`] handle. Returns None if the folder inaccessible. 
+/// 
+/// [`LoadedFolder`]: crate::LoadedFolder
+pub fn load_assets_in<T: Asset>(
+    folders: &Res<Assets<LoadedFolder>>,
+    folder_handle: &Handle<LoadedFolder>
+) -> Option<Vec<Handle<T>>>{
+    let typeid = TypeId::of::<T>();
+
+     if let Some(folder) = folders.get(folder_handle) {
+        let handles: Vec<Handle<T>> = folder
+        .handles
+        .clone()
+        .into_iter()
+        .filter(|handle| handle.type_id() == typeid)
+        .map(|handle| handle.typed::<T>())
+        .collect::<Vec<_>>();
+        Some(handles)
+     } else {
+         None
+     }
 }
