a43cd714d2721d65fd3233a9f25e0907969401ba
rewin <rewin1996@gmail.com>
2024-05-30 22:53:58 +0300

    docs for dev_cli example

--- examples/dev_tools/dev_cli.rs
+++ examples/dev_tools/dev_cli.rs
@@ -1,56 +1,20 @@
 //! Show how to use DevCommands, DevTools and cli dev console
+//! To try this demo you must print into your console while app is running
+//! Try this:
+//! 1. `disable fpsoverlay` -- will hide fps overlay
+//! 2. `enable fpsoverlay` -- will show fps overlay 
+//! 3. `setfield fpsoverlay text_config.font_size 16` -- will change font size in fps overlay
+//! 4. `printcommands` -- will list all dev commands
+//! 5. `setgold 100` -- will set gold
+//! 6. `printgold` -- will print gold
+//! 7. `disable showgold` -- will hide gold
+//! 8. Fell free to add and register own dev commands!
 
 use bevy::dev_tools::fps_overlay::FpsOverlayPlugin;
 use bevy::dev_tools::prelude::*;
 use bevy::ecs::world::Command;
 use bevy::prelude::*;
 
-#[derive(Resource, Default)]
-pub struct Gold(pub usize);
-
-#[derive(Reflect, Default, DevCommand)]
-#[reflect(DevCommand, Default)]
-pub struct SetGold {
-    pub gold: usize,
-}
-impl Command for SetGold {
-    fn apply(self, world: &mut World) {
-        world.insert_resource(Gold(self.gold));
-    }
-}
-
-#[derive(Reflect, Default, DevCommand)]
-#[reflect(DevCommand, Default)]
-pub struct PrintGold {}
-
-impl Command for PrintGold {
-    fn apply(self, world: &mut World) {
-        let gold = world.get_resource::<Gold>().unwrap();
-        info!("Gold: {}", gold.0);
-    }
-}
-
-//We can create toggable dev tool
-#[derive(States, Debug, Clone, Eq, PartialEq, Hash, Default, Reflect)]
-enum ShowGold {
-    #[default]
-    Show,
-    Hide,
-}
-
-impl Toggable for ShowGold {
-    fn enable(world: &mut World) {
-        world.resource_mut::<NextState<ShowGold>>().set(ShowGold::Show);
-    }
-
-    fn disable(world: &mut World) {
-        world.resource_mut::<NextState<ShowGold>>().set(ShowGold::Hide);
-    }
-
-    fn is_enabled(world: &World) -> bool {
-        *world.resource::<State<ShowGold>>() == ShowGold::Show
-    }
-}
 
 fn main() {
     App::new()
@@ -82,6 +46,64 @@ fn setup(mut commands: Commands) {
     commands.spawn(Camera2dBundle::default());
 }
 
+
+/// Code for showing gold and dev commands for gold
+
+
+/// Contains showing gold
+#[derive(Resource, Default)]
+pub struct Gold(pub usize);
+
+/// DevCommand to change gold value
+/// Example:
+/// `setgold 100` -- you need to print this into your console
+#[derive(Reflect, Default, DevCommand)]
+#[reflect(DevCommand, Default)]
+pub struct SetGold {
+    pub gold: usize,
+}
+impl Command for SetGold {
+    fn apply(self, world: &mut World) {
+        world.insert_resource(Gold(self.gold));
+    }
+}
+
+/// DevCommand to print gold amount
+#[derive(Reflect, Default, DevCommand)]
+#[reflect(DevCommand, Default)]
+pub struct PrintGold {}
+
+impl Command for PrintGold {
+    fn apply(self, world: &mut World) {
+        let gold = world.get_resource::<Gold>().unwrap();
+        info!("Gold: {}", gold.0);
+    }
+}
+
+//We can create toggable dev state
+//It will toggle between show and hide by `enable showgold` and `disable showgold` commands
+#[derive(States, Debug, Clone, Eq, PartialEq, Hash, Default, Reflect)]
+enum ShowGold {
+    #[default]
+    Show,
+    Hide,
+}
+
+impl Toggable for ShowGold {
+    fn enable(world: &mut World) {
+        world.resource_mut::<NextState<ShowGold>>().set(ShowGold::Show);
+    }
+
+    fn disable(world: &mut World) {
+        world.resource_mut::<NextState<ShowGold>>().set(ShowGold::Hide);
+    }
+
+    fn is_enabled(world: &World) -> bool {
+        *world.resource::<State<ShowGold>>() == ShowGold::Show
+    }
+}
+
+/// UI stuff
 #[derive(Component)]
 struct ShowGoldNode;
 
@@ -97,7 +119,7 @@ fn destroy_gold_node(mut commands: Commands, q_node: Query<Entity, With<ShowGold
 
 fn show_gold_system(
     mut commands: Commands,
-    mut q_node: Query<Entity, With<ShowGoldNode>>,
+    q_node: Query<Entity, With<ShowGoldNode>>,
     gold : Res<Gold>,
 ) {
     if let Ok(node) = q_node.get_single() {
