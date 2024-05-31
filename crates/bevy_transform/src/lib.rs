51ca90e80159faaa2d5c19adae32f0f693c60740
Torstein Grindvik <torstein.grindvik@muybridge.com>
2024-05-31 19:07:26 +0200

    Add bevy-support feature post-shuffle
    Signed-off-by: Torstein Grindvik <torstein.grindvik@muybridge.com>

--- crates/bevy_transform/src/lib.rs
+++ crates/bevy_transform/src/lib.rs
@@ -5,22 +5,27 @@
     html_favicon_url = "https://bevyengine.org/assets/icon.png"
 )]
 
+#[cfg(feature = "bevy-support")]
 pub mod commands;
 /// The basic components of the transform crate
 pub mod components;
 
 /// Transform related bundles
+#[cfg(feature = "bevy-support")]
 pub mod bundles;
 
 /// Transform related traits
 pub mod traits;
 
 /// Transform related plugins
+#[cfg(feature = "bevy-support")]
 pub mod plugins;
 
 /// Helpers related to computing global transforms
+#[cfg(feature = "bevy-support")]
 pub mod helper;
 /// Systems responsible for transform propagation
+#[cfg(feature = "bevy-support")]
 pub mod systems;
 
 #[doc(hidden)]
@@ -28,6 +33,7 @@ pub mod prelude {
     #[doc(hidden)]
     pub use crate::components::*;
 
+    #[cfg(feature = "bevy-support")]
     #[doc(hidden)]
     pub use crate::{
         bundles::TransformBundle, commands::BuildChildrenTransformExt, helper::TransformHelper,
@@ -35,4 +41,5 @@ pub mod prelude {
     };
 }
 
+#[cfg(feature = "bevy-support")]
 pub use prelude::{TransformPlugin, TransformPoint, TransformSystem};
