49661b99fe2f02e2d66e8af6d3b3584eb5ca7a26
Chris Juchem <chrisjuchem15@gmail.com>
2024-06-10 14:06:05 -0400

    Remove extra call to clear_trackers (#13762)
    Fixes #13758.
    
    # Objective
    
    Calling `update` on the main app already calls `clear_trackers`. Calling
    it again in `SubApps::update` caused RemovedCompenet Events to be
    cleared earlier than they should be.
    
    ## Solution
    
    - Don't call clear_trackers an extra time.
    
    ## Testing
    
    I manually tested the fix with this unit test: 
    ```
    #[cfg(test)]
    mod test {
        use crate::core::{FrameCount, FrameCountPlugin};
        use crate::prelude::*;
    
        #[test]
        fn test_next_frame_removal() {
            #[derive(Component)]
            struct Foo;
    
            #[derive(Resource)]
            struct RemovedCount(usize);
    
            let mut app = App::new();
            app.add_plugins(FrameCountPlugin);
            app.add_systems(Startup, |mut commands: Commands| {
                for _ in 0..100 {
                    commands.spawn(Foo);
                }
                commands.insert_resource(RemovedCount(0));
            });
    
            app.add_systems(First, |counter: Res<FrameCount>| {
                println!("Frame {}:", counter.0)
            });
    
            fn detector_system(
                mut removals: RemovedComponents<Foo>,
                foos: Query<Entity, With<Foo>>,
                mut removed_c: ResMut<RemovedCount>,
            ) {
                for e in removals.read() {
                    println!("  Detected removed Foo component for {e:?}");
                    removed_c.0 += 1;
                }
                let c = foos.iter().count();
                println!("  Total Foos: {}", c);
                assert_eq!(c + removed_c.0, 100);
            }
            fn deleter_system(foos: Query<Entity, With<Foo>>, mut commands: Commands) {
                foos.iter().next().map(|e| {
                    commands.entity(e).remove::<Foo>();
                });
            }
            app.add_systems(Update, (detector_system, deleter_system).chain());
    
            app.update();
            app.update();
            app.update();
            app.update();
        }
    }
    ```

--- crates/bevy_app/src/app.rs
+++ crates/bevy_app/src/app.rs
@@ -919,14 +919,21 @@ impl Termination for AppExit {
 
 #[cfg(test)]
 mod tests {
-    use std::sync::Mutex;
-    use std::{marker::PhantomData, mem};
+    use std::{iter, marker::PhantomData, mem, sync::Mutex};
 
-    use bevy_ecs::prelude::{Resource, World};
-    use bevy_ecs::world::FromWorld;
-    use bevy_ecs::{event::EventWriter, schedule::ScheduleLabel, system::Commands};
+    use bevy_ecs::{
+        change_detection::{DetectChanges, ResMut},
+        component::Component,
+        entity::Entity,
+        event::EventWriter,
+        query::With,
+        removal_detection::RemovedComponents,
+        schedule::{IntoSystemConfigs, ScheduleLabel},
+        system::{Commands, Query, Resource},
+        world::{FromWorld, World},
+    };
 
-    use crate::{App, AppExit, Plugin, Update};
+    use crate::{App, AppExit, Plugin, SubApp, Update};
 
     struct PluginA;
     impl Plugin for PluginA {
@@ -1127,6 +1134,62 @@ mod tests {
             GenericLabel::<u32>(PhantomData).intern(),
             GenericLabel::<u64>(PhantomData).intern()
         );
+    }
+
+    #[test]
+    fn test_update_clears_trackers_once() {
+        #[derive(Component, Copy, Clone)]
+        struct Foo;
+
+        let mut app = App::new();
+        app.world_mut().spawn_batch(iter::repeat(Foo).take(5));
+
+        fn despawn_one_foo(mut commands: Commands, foos: Query<Entity, With<Foo>>) {
+            if let Some(e) = foos.iter().next() {
+                commands.entity(e).despawn();
+            };
+        }
+        fn check_despawns(mut removed_foos: RemovedComponents<Foo>) {
+            let mut despawn_count = 0;
+            for _ in removed_foos.read() {
+                despawn_count += 1;
+            }
+
+            assert_eq!(despawn_count, 2);
+        }
+
+        app.add_systems(Update, despawn_one_foo);
+        app.update(); // Frame 0
+        app.update(); // Frame 1
+        app.add_systems(Update, check_despawns.after(despawn_one_foo));
+        app.update(); // Should see despawns from frames 1 & 2, but not frame 0
+    }
+
+    #[test]
+    fn test_extract_sees_changes() {
+        use super::AppLabel;
+        use crate::{self as bevy_app};
+
+        #[derive(AppLabel, Clone, Copy, Hash, PartialEq, Eq, Debug)]
+        struct MySubApp;
+
+        #[derive(Resource)]
+        struct Foo(usize);
+
+        let mut app = App::new();
+        app.world_mut().insert_resource(Foo(0));
+        app.add_systems(Update, |mut foo: ResMut<Foo>| {
+            foo.0 += 1;
+        });
+
+        let mut sub_app = SubApp::new();
+        sub_app.set_extract(|main_world, _sub_world| {
+            assert!(main_world.get_resource_ref::<Foo>().unwrap().is_changed());
+        });
+
+        app.insert_sub_app(MySubApp, sub_app);
+
+        app.update();
     }
 
     #[test]
