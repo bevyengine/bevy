986015270db5827985329acccb90fb448303ccce
Emerson Coskey <56370779+ecoskey@users.noreply.github.com>
2024-06-11 16:01:26 -0700

    add docs

--- crates/bevy_asset/src/io/pack.rs
+++ crates/bevy_asset/src/io/pack.rs
@@ -7,11 +7,35 @@ pub use bevy_asset_macros::AssetPack;
 
 use crate::AssetServer;
 
+/// This trait (and associated derive macro) provides syntax sugar for loading related assets
+/// whose sources are known at compile time. When using the derive macro, all fields must have
+/// either an `#[embedded("...")]` attribute or a `#[load(...)]` attribute.
+///
+/// `embedded` takes as argument a relative path to the embedded asset
+/// `load` takes as argument an expression that implements `Into<AssetPath<'_>>`. This could be a string
+/// literal, or something else depending on the use-case.
+///
+/// ```no_run
+/// use bevy::asset::io::AssetPack;
+///
+/// #[derive(AssetPack)]
+/// struct MyAssets {
+///     // loads "sprite_1.png" from the current directory into the application binary, and loads
+///     // sprite_1 from there
+///     #[embedded("sprite_1.png")]
+///     pub sprite_1: Handle<Image>;
+///
+///     // loads "sprite_2.png" from the default asset source (most likely the `assets` directory)
+///     #[load("sprite_2.png")] //
+///     pub sprite_2: Handle<Image>;
+/// }
+/// ```
 pub trait AssetPack: Send + Sync + 'static {
     fn init(app: &mut App);
     fn load(asset_server: &AssetServer) -> Self;
 }
 
+/// Provides setup for loading an asset pack of type `T`
 pub struct AssetPackPlugin<T: AssetPack>(PhantomData<T>);
 
 impl<T: AssetPack> Default for AssetPackPlugin<T> {
@@ -27,6 +51,7 @@ impl<T: AssetPack> Plugin for AssetPackPlugin<T> {
     }
 }
 
+/// A Resource that wraps access to an `AssetPack`
 #[derive(Resource)]
 struct Pack<T: AssetPack>(OnceLock<T>);
 
@@ -42,6 +67,7 @@ impl<T: AssetPack> Default for Pack<T> {
     }
 }
 
+/// A SystemParam that wraps `Pack<T>` and `AssetServer` for simple access
 #[derive(SystemParam)]
 pub struct GetPack<'w, T: AssetPack> {
     handles: Res<'w, Pack<T>>,
