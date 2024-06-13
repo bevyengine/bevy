9b1aa5261b6b86868cd071b762a8e4d6d0fc201a
Jan Hohenheim <jan@hohenheim.ch>
2024-06-13 15:33:25 +0200

    Improve comments

--- examples/camera/first_person_view_model.rs
+++ examples/camera/first_person_view_model.rs
@@ -72,9 +72,11 @@ struct WorldModelCamera;
 
 /// Used implicitly by all entities without a `RenderLayers` component.
 /// Our world model camera and all objects other than the player are on this layer.
+/// The light source belongs to both layers.
 const DEFAULT_RENDER_LAYER: usize = 0;
 
 /// Used by the view model camera and the player's arm.
+/// The light source belongs to both layers.
 const VIEW_MODEL_RENDER_LAYER: usize = 1;
 
 fn spawn_view_model(
