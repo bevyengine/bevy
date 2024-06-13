9291ef2f7636a8a4a7d0fac948573d0e11277b55
Jan Hohenheim <jan@hohenheim.ch>
2024-06-13 15:09:16 +0200

    Use idiomatic message

--- examples/camera/first_person_view_model.rs
+++ examples/camera/first_person_view_model.rs
@@ -229,8 +229,7 @@ fn change_fov(
 ) {
     let mut projection = world_model_projection.single_mut();
     let Projection::Perspective(ref mut perspective) = projection.as_mut() else {
-        // We've explicitly built this component with `Projection::Perspective`.
-        unreachable!();
+        unreachable!("The `Projection` component was explicitly built with `Projection::Perspective`");
     };
 
     if input.pressed(KeyCode::ArrowUp) {
