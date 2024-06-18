d3cbf42d0414cf8f8b50021e5f26d5ac547b8c98
Soulghost <xiuyutong1994@163.com>
2024-06-18 22:41:00 +0800

    feat(gltf): add name component to gltf mesh primitive

--- crates/bevy_gltf/src/loader.rs
+++ crates/bevy_gltf/src/loader.rs
@@ -1,6 +1,6 @@
 use crate::{
     vertex_attributes::convert_attribute, Gltf, GltfAssetLabel, GltfExtras, GltfMaterialExtras,
-    GltfMeshExtras, GltfNode, GltfSceneExtras,
+    GltfMaterialName, GltfMeshExtras, GltfNode, GltfSceneExtras,
 };
 
 #[cfg(feature = "bevy_animation")]
@@ -1333,6 +1333,10 @@ fn load_node(
                         mesh_entity.insert(GltfMaterialExtras {
                             value: extras.get().to_string(),
                         });
+                    }
+
+                    if let Some(name) = material.name() {
+                        mesh_entity.insert(GltfMaterialName(String::from(name)));
                     }
 
                     mesh_entity.insert(Name::new(primitive_name(&mesh, &primitive)));
