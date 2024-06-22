dcedd7410c1cafd473c077838e6effec9eb641fd
Thierry Berger <contact@thierryberger.com>
2024-06-22 22:37:03 +0200

    apply mockersf feebacks (or what I understood from them :sweat_smile:)

--- examples/ecs/ambiguity_detection.rs
+++ examples/ecs/ambiguity_detection.rs
@@ -1,26 +1,28 @@
-//! A test to confirm that `bevy` doesn't have system order ambiguity with DefaultPlugins
+//! A test to confirm that `bevy` doesn't have system order ambiguity with [`DefaultPlugins`]
 //! This is run in CI to ensure that this doesn't regress again.
 
 use bevy::{
     ecs::schedule::{LogLevel, ScheduleBuildSettings, ScheduleLabel},
     prelude::*,
 };
-//use bevy_render::{pipelined_rendering::RenderExtractApp, Render, RenderApp};
+use bevy_render::{pipelined_rendering::RenderExtractApp, Render, RenderApp};
 
 /// FIXME: bevy should not have any ambiguities, but it takes time to clean these up,
 /// so we're juste ignoring those for now.
-fn get_ignored_ambiguous_systems() -> Vec<Box<dyn ScheduleLabel>> {
+///
+/// See [#7386](https://github.com/bevyengine/bevy/issues/7386) for relevant issue.
+pub fn get_ignored_ambiguous_systems() -> Vec<Box<dyn ScheduleLabel>> {
     vec![
         Box::new(First),
         Box::new(PreUpdate),
         Box::new(PostUpdate),
         Box::new(Last),
         Box::new(ExtractSchedule),
-        //Box::new(Render),
+        Box::new(Render),
     ]
 }
 
-/// A test to confirm that `bevy` doesn't have system order ambiguity with DefaultPlugins
+/// A test to confirm that `bevy` doesn't have system order ambiguity with [`DefaultPlugins`]
 /// This is run in CI to ensure that this doesn't regress again.
 pub fn main() {
     let mut app = App::new();
@@ -32,8 +34,7 @@ pub fn main() {
         for (_, schedule) in schedules.iter_mut() {
             if ignored_ambiguous_systems
                 .iter()
-                .find(|label| ***label == *schedule.label())
-                .is_some()
+                .any(|label| **label == *schedule.label())
             {
                 continue;
             }
@@ -46,10 +47,10 @@ pub fn main() {
     }
     let sub_app = app.main_mut();
     configure_ambiguity_detection(sub_app);
-    //let sub_app = app.sub_app_mut(RenderApp);
-    //configure_ambiguity_detection(sub_app);
-    //let sub_app = app.sub_app_mut(RenderExtractApp);
-    //configure_ambiguity_detection(sub_app);
+    let sub_app = app.sub_app_mut(RenderApp);
+    configure_ambiguity_detection(sub_app);
+    let sub_app = app.sub_app_mut(RenderExtractApp);
+    configure_ambiguity_detection(sub_app);
 
     app.finish();
     app.cleanup();
@@ -62,17 +63,16 @@ pub fn main() {
         for (_, schedule) in schedules.iter() {
             if ignored_ambiguous_systems
                 .iter()
-                .find(|label| ***label == *schedule.label())
-                .is_some()
+                .any(|label| **label == *schedule.label())
             {
                 continue;
             }
-            assert!(schedule.graph().conflicting_systems().len() == 0);
+            assert!(schedule.graph().conflicting_systems().is_empty());
         }
     }
     let sub_app = app.main();
     assert_no_conflicting_systems(sub_app);
     // RenderApp is not checked here, because it is not within the App at this point.
-    //let sub_app = app.sub_app(RenderExtractApp);
-    //assert_no_conflicting_systems(sub_app);
+    let sub_app = app.sub_app(RenderExtractApp);
+    assert_no_conflicting_systems(sub_app);
 }
