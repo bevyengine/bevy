40019cc86fa0c29d90f3c1296037d19fe228a719
TirushOne <jedmckenna63@gmail.com>
2024-06-26 07:20:44 +1000

    work on loggin and adding metadata

--- crates/bevy_net/src/easy_sockets/socket_manager.rs
+++ crates/bevy_net/src/easy_sockets/socket_manager.rs
@@ -1,11 +1,13 @@
+use std::net::SocketAddr;
 use std::ops::DerefMut;
 use std::sync::{Arc, Weak};
+use bevy_internal::log::warn;
 use bevy_internal::tasks::TaskPool;
 use crate::easy_sockets::{AsyncHandler, Buffer, ErrorAction};
 use crate::easy_sockets::spin_lock::SpinLock;
 
 struct SocketManger<B, S> {
-    handler: AsyncHandler<(Weak<SpinLock<B>>, Option<S>)>
+    handler: AsyncHandler<SocketData<B, S>>
 }
 
 struct BufferUpdateResult {
@@ -15,15 +17,132 @@ struct BufferUpdateResult {
     index: usize
 }
 
+#[cfg(feature = "MetaData")]
+struct ByteMetaData {
+    /// Consecutive recoverable errors
+    /// returned by the buffer during any operations.
+    recoverable_errors: u32,
+
+    #[cfg(feature = "Extra-MetaData")]
+    /// Consecutive recoverable errors
+    /// returned the buffer during read updates.
+    recoverable_errors_read: u32,
+    #[cfg(feature = "Extra-MetaData")]
+    /// Consecutive recoverable errors
+    /// returned the buffer during write updates.
+    recoverable_errors_write: u32,
+    #[cfg(feature = "Extra-MetaData")]
+    /// Consecutive recoverable errors
+    /// returned the buffer during updates to
+    /// miscellaneous properties of the socket.
+    recoverable_errors_properties: u32,
+
+    /// Number of bytes written this tick.
+    written_this_tick: u32,
+    /// Number of bytes read this tick.
+    read_this_tick: u32,
+
+    #[cfg(feature = "Extra-MetaData")]
+    /// Total number of bytes written to
+    /// the port in its lifetime.
+    total_written: u64,
+    #[cfg(feature = "Extra-MetaData")]
+    /// Total number of bytes read from
+    /// the port in its lifetime.
+    total_read: u64,
+}
+
+#[cfg(feature = "MetaData")]
+impl ByteMetaData {
+    fn update(&mut self, result: BufferUpdateResult) {
+
+        let mut recoverible_error_occured = false;
+
+        if let Ok(n) = result.write_result {
+            self.written_this_tick = n as u32;
+        } else {
+            self.written_this_tick = 0;
+
+            let err = result.write_result.unwrap_err();
+
+            if err.is_none() {
+                recoverible_error_occured = true;
+                #[cfg(feature = "Extra-MetaData")]
+                { self.recoverable_errors_write += 1; }
+            } else {
+                #[cfg(feature = "Extra-MetaData")]
+                { self.recoverable_errors_write = 0; }
+            }
+        }
+
+        if let Ok(n) = result.read_result {
+            self.read_this_tick = n as u32;
+        } else {
+            self.read_this_tick = 0;
+
+            let err = result.read_result.unwrap_err();
+
+            if err.is_none() {
+                recoverible_error_occured = true;
+                #[cfg(feature = "Extra-MetaData")]
+                { self.recoverable_errors_read += 1; }
+            } else {
+                #[cfg(feature = "Extra-MetaData")]
+                { self.recoverable_errors_read = 0; }
+            }
+        }
+
+        #[cfg(feature = "Extra-MetaData")]
+        if let Some(err) = result.properties_result {
+            if err.is_none() {
+                recoverible_error_occured = true;
+                #[cfg(feature = "Extra-MetaData")]
+                { self.recoverable_errors_properties += 1; }
+            } else {
+                #[cfg(feature = "Extra-MetaData")]
+                { self.recoverable_errors_properties = 0; }
+            }
+        }
+
+        #[cfg(feature = "Extra-MetaData")]
+        {
+            self.total_written += self.written_this_tick as u64;
+            self.total_read += self.read_this_tick as u64;
+        }
+
+
+        if recoverible_error_occured {
+            self.recoverable_errors += 1;
+        } else {
+            self.recoverable_errors += 0;
+        }
+    }
+
+    fn warn_logging(&self, addr: SocketAddr) {
+        todo!()
+    }
+    
+    fn debug_logging(&self, addr: SocketAddr) {
+        todo!()
+    }
+}
+
+struct SocketData<B, S> {
+    buffer: Weak<SpinLock<B>>,
+    socket: Option<S>,
+    #[cfg(feature = "MetaData")]
+    meta_data: ByteMetaData
+}
+
 impl<B, S> SocketManger<B, S>
 where B: Buffer<InnerSocket = S> {
     fn update_buffers(&mut self, pool: &TaskPool) -> Vec<Result<BufferUpdateResult, usize>> {
-        self.handler.for_each_async_mut(|index, (weak, optional_socket)| async move {
-            if let Some(lock) = weak.upgrade() {
+        self.handler.for_each_async_mut(|index, data | async move {
+            if let Some(lock) = data.buffer.upgrade() {
                 let mut guard = lock.lock_async().await.unwrap();
                 let mut buffer = guard.deref_mut();
                 
-                if let Some(socket) = optional_socket {
+                if let Some(socket) = &mut data.socket {
                     
                     return Ok(
                         BufferUpdateResult {
@@ -46,44 +165,40 @@ where B: Buffer<InnerSocket = S> {
 
         let mut to_be_removed = Vec::with_capacity(self.handler.0.len());
         
-        for res in results {
+        for res in &results {
             
             match res {
                 Ok(UpdateResults) => {
                     if let Some(prop_error) = UpdateResults.properties_result {
                         if prop_error.is_drop() {
-                            self.handler.0[UpdateResults.index].1 = None;
-                        }
-                        //todo log occurrences of recoverable errors
-                    }
-                    
-                    match UpdateResults.read_result {
-                        Ok(n) => {
-                            //todo log bytes read for diagnostics
-                        }
-                        Err(a) => {
-                            if a.is_drop() {
-                                self.handler.0[UpdateResults.index].1 = None;
-                            }
-                            //todo log occurrences of recoverable errors
+                            self.handler.0[UpdateResults.index].socket = None;
                         }
                     }
-                    
-                    match UpdateResults.write_result {
-                        Ok(n) => {
-                            //todo log bytes written for diagnostics
+
+                    if let Err(a) = UpdateResults.read_result {
+                        if a.is_drop() {
+                            self.handler.0[UpdateResults.index].socket = None;
                         }
-                        Err(error) => {
-                            if error.is_drop() {
-                                self.handler.0[UpdateResults.index].1 = None;
-                            }
-                            //todo log occurrences of recoverable errors
+                    }
+
+                    if let Err(a) = UpdateResults.write_result {
+                        if a.is_drop() {
+                            self.handler.0[UpdateResults.index].socket = None;
                         }
                     }
                 }
                 Err(index) => {
-                    to_be_removed.push(index);
+                    to_be_removed.push(*index);
                 }
+            }
+        }
+
+        #[cfg(feature = "MetaData")]
+        for res in results {
+            if let Ok(res) = res {
+                let metadata = &mut self.handler.0[res.index].meta_data;
+                metadata.update(res);
+
             }
         }
         
