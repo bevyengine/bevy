4071e56361364c65f3d2773a3d6acb2ccbf167bd
Martin Svanberg <martin@martinsvanberg.com>
2024-06-26 20:28:04 +0200

    Fix typo in CPU adapter warning

--- crates/bevy_render/src/renderer/mod.rs
+++ crates/bevy_render/src/renderer/mod.rs
@@ -190,7 +190,7 @@ pub async fn initialize_renderer(
 
     if adapter_info.device_type == DeviceType::Cpu {
         warn!(
-            "The selected adapter is using the a driver that only supports software rendering. \
+            "The selected adapter is using a driver that only supports software rendering. \
              This is likely to be very slow. See https://bevyengine.org/learn/errors/b0006/"
         );
     }
