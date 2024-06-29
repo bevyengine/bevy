6e04eef8867208c276fae92db5294828abc6b7f2
TirushOne <jedmckenna63@gmail.com>
2024-06-29 19:16:33 +1000

    refactored, much more readable code

--- crates/bevy_net/src/easy_sockets/socket_manager.rs
+++ crates/bevy_net/src/easy_sockets/socket_manager.rs
@@ -1,10 +1,16 @@
+use std::collections::VecDeque;
 use std::future::Future;
 use std::iter::{Enumerate, Map};
 use std::net::SocketAddr;
 use std::ops::{Deref, DerefMut};
+use std::pin::Pin;
 use std::slice::IterMut;
 use std::sync::{Arc, Mutex, RwLock, Weak};
-use bevy_internal::tasks::{IoTaskPool, TaskPool};
+use std::task::{Context, Poll, Waker};
+use bevy_internal::reflect::List;
+use bevy_internal::render::render_resource::encase::private::RuntimeSizedArray;
+use bevy_internal::tasks::{IoTaskPool, Task, TaskPool};
+use bevy_internal::tasks::futures_lite::FutureExt;
 use crate::easy_sockets::{Buffer, ErrorAction};
 use crate::easy_sockets::spin_lock::{SpinLock, SpinLockGuard};
 
@@ -42,10 +48,88 @@ struct UpdateResults {
     index: usize
 }
 
-struct SocketEnrey<B, S> {
+struct SocketEntry<B, S> {
     buffer: Weak<SpinLock<B>>,
     socket: Option<S>,
-    data: DiagnosticData
+    data: DiagnosticData,
+
+    drop_flag: bool
+}
+
+impl<B, S> SocketEntry<B, S>
+where B: Buffer<InnerSocket = S> {
+
+    /// Returns Ok if the Buffer is still in scope and
+    /// if the socket is also still present and
+    /// was updated.
+    /// Returns Err() if either the buffer or socket are not present.
+    async fn try_update_buffer(&mut self) -> Result<BufferUpdateResult, ()> {
+        if let Some(buffer) = self.buffer.upgrade() {
+            if let Some(socket) = &mut self.socket {
+                let mut guard = buffer.lock_async().await.unwrap();
+
+                return Ok(BufferUpdateResult {
+                    write_result: guard.flush_write_bufs(socket).await,
+                    read_result: guard.fill_read_bufs(socket).await,
+                    properties_result: guard.update_properties(socket).await,
+                })
+
+            }
+            return Err(())
+        }
+
+        return Err(())
+    }
+
+    async fn update(&mut self) {
+        match self.try_update_buffer().await {
+            Ok(update_results) => {
+                let mut should_drop_socket = false;
+                let mut error_occured = false;
+
+                match update_results.properties_result {
+                    Ok(_) => {}
+                    Err(action) => {
+                        error_occured = true;
+                        if action.is_drop() {
+                            should_drop_socket = true;
+                        }
+                    }
+                }
+
+                match update_results.write_result {
+                    Ok(n) => {
+                        self.data.written = n;
+                    }
+                    Err(action) => {
+                        error_occured = true;
+                        if action.is_drop() {
+                            should_drop_socket = true;
+                        }
+                    }
+                }
+
+                match update_results.read_result {
+                    Ok(n) => {
+                        self.data.read = n;
+                    }
+                    Err(action) => {
+                        error_occured = true;
+                        if action.is_drop() {
+                            should_drop_socket = true;
+                        }
+                    }
+                }
+
+                if should_drop_socket {
+                    self.socket = None;
+                }
+            }
+            Err(_) => {
+                self.drop_flag = true;
+            }
+        }
+    }
 }
 
 #[derive(Default)]
@@ -54,137 +138,59 @@ struct DiagnosticData {
     /// buffer has had some kind of 
     /// none fatal error occur
     error_count: u32,
+    /// Number of bytes written this tick.
+    written: usize,
+    /// Number of bytes read this tick.
+    read: usize
 }
 
 struct SocketManger<B, S> {
-    sockets: Vec<SocketEnrey<B, S>>,
-}
-
-struct FutureSpawner<'a, B, S, F, Fut>
-where F: Fn(&'a mut SocketEnrey<B, S>) -> Fut, Fut: Future {
-    inner: IterMut<'a, SocketEnrey<B, S>>,
-    function: F
-}
-
-impl<'a, B, S, F, Fut> Iterator for FutureSpawner<'a, B, S, F, Fut>
-where F: Fn(&'a mut SocketEnrey<B, S>) -> Fut, Fut: Future {
-    type Item = Fut;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        if let Some(entrey) = self.inner.next() {
-            let func = &self.function;
-            return Some(func(entrey))
-        }
-        None
-    }
+    sockets: Vec<SocketEntry<B, S>>,
 }
 
 impl<B, S> SocketManger<B, S>
 where B: Buffer<InnerSocket = S> {
-    fn future_iter<'a, F, Fut>(&'a mut self, func: F) -> FutureSpawner<'a, B, S, F, Fut>
-    where F: Fn(&'a mut SocketEnrey<B, S>) -> Fut, Fut: Future {
-        FutureSpawner { inner: self.sockets.iter_mut(), function: func }
-    }
-
-    async fn update(&mut self) -> Vec<UpdateResults> {
+    fn update(&mut self) {
         let pool = IoTaskPool::get();
 
-        pool.scope(|scope| {
-            for fut in self.future_iter(|entrey| async {
-                if let Some(lock) = entrey.buffer.upgrade() {
-                    if let Some(socket) = &mut entrey.socket {
-                        let mut guard = lock.lock_async().await.unwrap();
-
-                        return Ok(Some(BufferUpdateResult {
-                            write_result: guard.flush_write_bufs(socket).await,
-                            read_result: guard.fill_read_bufs(socket).await,
-                            properties_result: guard.update_properties(socket).await
-                        }))
-                    }
-
-                    return Ok(None)
-                }
-
-                Err(())
-            }).enumerate().map(|(index, future)| {
-                async move {
-                    UpdateResults { results: future.await, index }
-                }
-            }) {
-                scope.spawn(fut)
+        pool.scope(|s| {
+            for socket in self.sockets.iter_mut() {
+                s.spawn(socket.update())
             }
-        })
-    }
+        });
 
-    async fn update_and_handle(&mut self) {
-        let results = self.update().await;
-
-        let mut to_be_removed = Vec::with_capacity(self.sockets.len());
+        let mut i = self.sockets.len() - 1;
+        let mut last_iteration = false;
         
-        for res in results {
-            let index = res.index;
+        loop {
+            if self.sockets[i].drop_flag {
+                self.sockets.remove(i);
+            } else {
+                i -= 1;
+            }
             
-            match res.results {
-                Ok(optional) => {
-                    if let Some(results) = optional {
-                        let mut error_occured = false;
-                        let mut should_drop = false;
-                        
-                        let current_entrey = &mut self.sockets[index];
-                        
-                        match results.write_result {
-                            Ok(n) => {
-                                //todo add write and read rates as diagnostic data
-                            }
-                            Err(action) => {
-                                error_occured = true;
-                                if action.is_drop() {
-                                    should_drop = true;
-                                }
-                            }
-                        }
-
-                        match results.read_result {
-                            Ok(n) => {
-
-                            }
-                            Err(action) => {
-                                error_occured = true;
-                                if action.is_drop() {
-                                    should_drop = true;
-                                }
-                            }
-                        }
-                        
-                        if let Err(action) = results.properties_result {
-                            error_occured = true;
-                            if action.is_drop() {
-                                should_drop = true;
-                            }
-                        }
-                        
-                        if should_drop {
-                            current_entrey.socket = None;
-                        }
-                        
-                        if error_occured {
-                            current_entrey.data.error_count += 1;
-                        } else {
-                            current_entrey.data.error_count = 0;
-                        }
-                    }
-                }
-                Err(_) => {
-                    to_be_removed.push(index);
-                }
+            if last_iteration {
+                return;
+            }
+            
+            if i == 1 {
+                last_iteration = true;
             }
         }
+    }
+    
+    fn register(&mut self, socket: S) -> Result<OwnedBuffer<B>, B::ConstructionError> {
+        let (weak, arc) = OwnedBuffer::new_with_weak(B::build(&socket)?);
+        let entry = SocketEntry {
+            buffer: weak,
+            socket: Some(socket),
+            data: Default::default(),
+            drop_flag: false,
+        };
         
-        to_be_removed.sort_unstable();
+        self.sockets.push(entry);
         
-        for index in to_be_removed.into_iter().rev() {
-            self.sockets.remove(index);
-        }
+        Ok(arc)
     }
 }
 
