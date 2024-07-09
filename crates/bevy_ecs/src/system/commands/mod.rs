bee8fcd9139276f829b87a1d532dba7d8dd349ed
Ben Frankel <ben.frankel7@gmail.com>
2024-07-08 22:53:04 -0700

    Adjust wording

--- crates/bevy_ecs/src/system/commands/mod.rs
+++ crates/bevy_ecs/src/system/commands/mod.rs
@@ -235,8 +235,7 @@ impl<'w, 's> Commands<'w, 's> {
         }
     }
 
-    /// Immediately spawns a new empty [`Entity`] and returns its corresponding
-    /// [`EntityCommands`].
+    /// Reserves a new empty [`Entity`] to be spawned, and returns its corresponding [`EntityCommands`].
     ///
     /// See [`World::spawn_empty`] for more details.
     ///
