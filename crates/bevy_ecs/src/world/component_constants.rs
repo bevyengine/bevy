4ece2aec3a850308c5b3226853cdfef7a4db5d14
Peter Hayman <peteyhayman@gmail.com>
2024-07-10 14:18:34 +1000

    add missing use statement

--- crates/bevy_ecs/src/world/component_constants.rs
+++ crates/bevy_ecs/src/world/component_constants.rs
@@ -1,5 +1,7 @@
 use super::*;
 use crate::{self as bevy_ecs};
+#[cfg(feature = "bevy_reflect")]
+use bevy_reflect::Reflect;
 /// Internal components used by bevy with a fixed component id.
 /// Constants are used to skip [`TypeId`] lookups in hot paths.
 
