eb919418473d70cb209617a765ef939e30a5a7a6
Emerson Coskey <56370779+ecoskey@users.noreply.github.com>
2024-07-13 08:34:59 -0700

    Update examples/asset/asset_pack.rs
    Co-authored-by: Pascal Hertleif <killercup@gmail.com>

--- examples/asset/asset_pack.rs
+++ examples/asset/asset_pack.rs
@@ -5,23 +5,16 @@ use bevy::prelude::*;
 
 fn main() {
     App::new()
-        .add_plugins((DefaultPlugins, AssetPackExamplePlugin))
+        .add_plugins(DefaultPlugins)
+        .add_plugins(AssetPackPlugin::<ExampleAssetPack>::default())
         .add_systems(Startup, setup)
         .run();
 }
-
-struct AssetPackExamplePlugin;
 
 #[derive(AssetPack)]
 struct ExampleAssetPack {
     #[embedded("files/bevy_pixel_dark.png")]
     sprite: Handle<Image>,
-}
-
-impl Plugin for AssetPackExamplePlugin {
-    fn build(&self, app: &mut App) {
-        app.add_plugins(AssetPackPlugin::<ExampleAssetPack>::default());
-    }
 }
 
 fn setup(mut commands: Commands, assets: GetPack<ExampleAssetPack>) {
