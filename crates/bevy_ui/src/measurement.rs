cc9fa7f5d8969109e6e817771fe195928f30b948
BD103 <59022059+BD103@users.noreply.github.com>
2024-07-22 09:23:14 -0400

    fix: make phantom reference `mut`

--- crates/bevy_ui/src/measurement.rs
+++ crates/bevy_ui/src/measurement.rs
@@ -25,7 +25,7 @@ pub struct MeasureArgs<'a> {
     pub font_system: &'a mut bevy_text::cosmic_text::FontSystem,
     // When `bevy_text` is disabled, use `PhantomData` in order to keep lifetime in type signature.
     #[cfg(not(feature = "bevy_text"))]
-    pub font_system: std::marker::PhantomData<&'a ()>,
+    pub font_system: std::marker::PhantomData<&'a mut ()>,
 }
 
 /// A `Measure` is used to compute the size of a ui node
