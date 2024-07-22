fbc69557152bde01d6af694a84b80c77e706a1db
Charlotte McElwain <charlotte.c.mcelwain@gmail.com>
2024-07-22 12:36:53 -0700

    Docs.

--- crates/bevy_core_pipeline/src/smaa/mod.rs
+++ crates/bevy_core_pipeline/src/smaa/mod.rs
@@ -14,8 +14,7 @@
 //! To use SMAA, add [`SmaaSettings`] to a [`bevy_render::camera::Camera`]. In a
 //! pinch, you can simply use the default settings (via the [`Default`] trait)
 //! for a high-quality, high-performance appearance. When using SMAA, you will
-//! likely want to turn the default MSAA off by inserting the
-//! [`bevy_render::view::Msaa::Off`] resource into the [`App`].
+//! likely want set [`Msaa`] to [`Msaa::Off`] for every camera using SMAA.
 //!
 //! Those who have used SMAA in other engines should be aware that Bevy doesn't
 //! yet support the following more advanced features of SMAA:
