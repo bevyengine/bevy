5aa998dc07d5769c82a224a2d21ae0c78df3743a
Matty <weatherleymatthew@gmail.com>
2024-07-25 16:23:32 -0400

    Conversions for Isometry3d ⟷ Transform/GlobalTransform (#14478)
    # Objective
    
    Allow interoperation between `Isometry3d` and the transform types from
    bevy_transform. At least in the short term, the primary goal is to allow
    the extraction of isometries from transform components by users.
    
    ## Solution
    
    - Add explicit `from_isometry`/`to_isometry` methods to `Transform`.
    - Add explicit `from_isometry`/`to_isometry` methods to
    `GlobalTransform`. The former is hidden (primarily for internal use),
    and the latter has the caveats originating in
    [`Affine3A::to_scale_rotation_translation`](https://docs.rs/glam/latest/glam/f32/struct.Affine3A.html#method.to_scale_rotation_translation).
    - Implement the `TransformPoint` trait for `Isometry3d`.

--- crates/bevy_transform/src/components/transform.rs
+++ crates/bevy_transform/src/components/transform.rs
@@ -1,7 +1,7 @@
 use super::GlobalTransform;
 #[cfg(feature = "bevy-support")]
 use bevy_ecs::{component::Component, reflect::ReflectComponent};
-use bevy_math::{Affine3A, Dir3, Mat3, Mat4, Quat, Vec3};
+use bevy_math::{Affine3A, Dir3, Isometry3d, Mat3, Mat4, Quat, Vec3};
 #[cfg(feature = "bevy-support")]
 use bevy_reflect::{prelude::*, Reflect};
 use std::ops::Mul;
@@ -116,6 +116,18 @@ impl Transform {
     pub const fn from_scale(scale: Vec3) -> Self {
         Transform {
             scale,
+            ..Self::IDENTITY
+        }
+    }
+
+    /// Creates a new [`Transform`] that is equivalent to the given [isometry].
+    ///
+    /// [isometry]: Isometry3d
+    #[inline]
+    pub fn from_isometry(iso: Isometry3d) -> Self {
+        Transform {
+            translation: iso.translation.into(),
+            rotation: iso.rotation,
             ..Self::IDENTITY
         }
     }
@@ -524,6 +536,14 @@ impl Transform {
     #[must_use]
     pub fn is_finite(&self) -> bool {
         self.translation.is_finite() && self.rotation.is_finite() && self.scale.is_finite()
+    }
+
+    /// Get the [isometry] defined by this transform's rotation and translation, ignoring scale.
+    ///
+    /// [isometry]: Isometry3d
+    #[inline]
+    pub fn to_isometry(&self) -> Isometry3d {
+        Isometry3d::new(self.translation, self.rotation)
     }
 }
 
