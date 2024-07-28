39edd53fa1f13e6129d75aa4e8918f039bd0eaa7
TrialDragon <cj.2003@live.com>
2024-07-27 18:26:26 -0700

    De-duplicate use of beautified category name

--- tools/example-showcase/src/main.rs
+++ tools/example-showcase/src/main.rs
@@ -502,18 +502,18 @@ header_message = \"Examples (WebGL2)\"
                     continue;
                 }
 
-                // This beautifys the path
+                // This beautifys the category name
                 // to make it a good looking URL
                 // rather than having weird whitespace
                 // and other characters that don't
                 // work well in a URL path.
-                let category_path = root_path.join(
-                    to_show
-                        .category
-                        .replace(['(', ')'], "")
-                        .replace(' ', "-")
-                        .to_lowercase(),
-                );
+                let beautified_category = to_show
+                    .category
+                    .replace(['(', ')'], "")
+                    .replace(' ', "-")
+                    .to_lowercase();
+
+                let category_path = root_path.join(&beautified_category);
 
                 if !categories.contains_key(&to_show.category) {
                     let _ = fs::create_dir_all(&category_path);
@@ -581,11 +581,7 @@ header_message = \"Examples ({})\"
                                 WebApi::Webgpu => "-webgpu",
                                 WebApi::Webgl2 => "",
                             },
-                            to_show
-                                .category
-                                .replace(['(', ')'], "")
-                                .replace(' ', "-")
-                                .to_lowercase(),
+                            &beautified_category,
                             &to_show.technical_name.replace('_', "-"),
                             &to_show.category,
                             &to_show.technical_name,
