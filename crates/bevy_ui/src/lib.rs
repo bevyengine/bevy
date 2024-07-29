34aacd33eb4143b1bd1060cb3f2dacd588c2e1c8
robtfm <50659922+robtfm@users.noreply.github.com>
2024-07-29 19:14:00 +0100

    increase ambiguity

--- crates/bevy_ui/src/lib.rs
+++ crates/bevy_ui/src/lib.rs
@@ -54,6 +54,7 @@ use bevy_app::prelude::*;
 use bevy_ecs::prelude::*;
 use bevy_input::InputSystem;
 use bevy_render::{
+    camera::CameraUpdateSystem,
     view::{check_visibility, VisibilitySystems},
     RenderApp,
 };
@@ -178,6 +179,7 @@ impl Plugin for UiPlugin {
                     .ambiguous_with(update_clipping_system)
                     .ambiguous_with(resolve_outlines_system)
                     .ambiguous_with(ui_layout_system)
+                    .ambiguous_with(CameraUpdateSystem) // we only read the `Camera::order` field
                     .in_set(AmbiguousWithTextSystem),
                 update_clipping_system.after(TransformSystem::TransformPropagate),
                 // Potential conflicts: `Assets<Image>`
