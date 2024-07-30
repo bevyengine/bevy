9661fc0b7e2cdfa6fc4f4eeb5af10367c3b47936
BD103 <59022059+BD103@users.noreply.github.com>
2024-07-30 10:41:38 -0400

    chore!: remove deprecated dynamic plugins

--- crates/bevy_dynamic_plugin/src/lib.rs
+++ crates/bevy_dynamic_plugin/src/lib.rs
@@ -4,25 +4,11 @@
     html_favicon_url = "https://bevyengine.org/assets/icon.png"
 )]
 
-//! Bevy's dynamic plugin loading functionality.
-//!
-//! This crate allows loading dynamic libraries (`.dylib`, `.so`) that export a single
-//! [`Plugin`](bevy_app::Plugin). For usage, see [`dynamically_load_plugin`].
-//!
-//! # Deprecation
-//!
-//! The current dynamic plugin system is unsound and will be removed in 0.15. You may be interested
-//! in the [Alternatives](#alternatives) listed below. If your use-case is not supported, please
-//! consider commenting on [#13080](https://github.com/bevyengine/bevy/pull/13080) describing how
-//! you use dynamic plugins in your project.
-//!
-//! # Warning
-//!
-//! Note that dynamic linking and loading is inherently unsafe because it allows executing foreign
-//! code. Additionally, Rust does not have a stable ABI and may produce
-//! incompatible libraries across Rust versions, or even subsequent compilations. This will not work
-//! well in scenarios such as modding, but can work if the dynamic plugins and the main app are
-//! built at the same time, such as with Downloadable Content (DLC) packs.
+//! Bevy's (deprecated) dynamic plugin loading functionality.
+//! 
+//! This crate used to allow loading plugins from dynamic libraries, but the implementation was
+//! unsound, so it was deprecated in 0.14 and removed in 0.15. You may be interested in the
+//! [Alternatives](#alternatives) listed below.
 //!
 //! # Alternatives
 //!
@@ -35,7 +21,3 @@
 //! [Bevy Assets - Scripting]: https://bevyengine.org/assets/#scripting
 //! [Bevy Assets - Development tools]: https://bevyengine.org/assets/#development-tools
 //! [`stabby`]: https://github.com/ZettaScaleLabs/stabby
-
-mod loader;
-
-pub use loader::*;
