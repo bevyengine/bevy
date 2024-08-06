439679e52ac7978b3ba33a50b9fbfbcfdfc9b673
IceSentry <c.giguere42@gmail.com>
2024-08-06 18:12:30 -0400

    add reference to Mesh2d in docs

--- crates/bevy_sprite/src/mesh2d/material.rs
+++ crates/bevy_sprite/src/mesh2d/material.rs
@@ -138,6 +138,8 @@ pub trait Material2d: AsBindGroup + Asset + Clone + Sized {
 }
 
 /// Sets how a 2d material's base color alpha channel is used for transparency.
+/// Currently, this only works with [`Mesh2d`](crate::mesh2d::Mesh2d). Sprites are always transparent.
+///
 /// This is very similar to [`AlphaMode`](bevy_render::alpha::AlphaMode) but this only applies to 2d meshes.
 /// We use a separate type because 2d doesn't support all the transparency modes that 3d does.
 #[derive(Debug, Default, Reflect, Copy, Clone, PartialEq)]
