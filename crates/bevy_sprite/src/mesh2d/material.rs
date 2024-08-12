6ec6695e40d67663b6b00b3ca0efcf67bc421585
IceSentry <c.giguere42@gmail.com>
2024-08-12 13:17:10 -0400

    add docs to alpha mode

--- crates/bevy_sprite/src/mesh2d/material.rs
+++ crates/bevy_sprite/src/mesh2d/material.rs
@@ -149,6 +149,14 @@ pub enum AlphaMode2d {
     /// Base color alpha values are overridden to be fully opaque (1.0).
     #[default]
     Opaque,
+    /// Reduce transparency to fully opaque or fully transparent
+    /// based on a threshold.
+    ///
+    /// Compares the base color alpha value to the specified threshold.
+    /// If the value is below the threshold,
+    /// considers the color to be fully transparent (alpha is set to 0.0).
+    /// If it is equal to or above the threshold,
+    /// considers the color to be fully opaque (alpha is set to 1.0).
     Mask(f32),
     /// The base color alpha value defines the opacity of the color.
     /// Standard alpha-blending is used to blend the fragment's color
