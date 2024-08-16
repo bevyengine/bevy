98640b52cbfb6f656aa12e63a033aa96e0998ed0
aviac <aviac@mailbox.org>
2024-08-16 15:12:00 +0200

    fix docs

--- crates/bevy_math/src/primitives/dim3.rs
+++ crates/bevy_math/src/primitives/dim3.rs
@@ -253,17 +253,17 @@ impl InfinitePlane3d {
     ///
     /// ## Guarantees
     ///
-    /// - the transformation is a [congruence] meaning it will preserve all distances and angles of
+    /// * the transformation is a [congruence] meaning it will preserve all distances and angles of
     /// the transformed geometry 
-    /// - uses the least rotation possible to transform the geometry
-    /// - if two geometries are transformed with the same isometry, then the relations between
+    /// * uses the least rotation possible to transform the geometry
+    /// * if two geometries are transformed with the same isometry, then the relations between
     /// them, like distances, are also preserved
-    /// - compared to projections, the transformation is lossless (up to floating point errors) reversible 
+    /// * compared to projections, the transformation is lossless (up to floating point errors) reversible 
     ///
     /// ## Non-Guarantees
     ///
-    /// - the rotation used is generally not unique
-    /// - the orientation of the transformed geometry in the XY plane might be arbitrary, to
+    /// * the rotation used is generally not unique
+    /// * the orientation of the transformed geometry in the XY plane might be arbitrary, to
     /// enforce some kind of alignment the user has to use an extra transformation ontop of this
     /// one
     ///
@@ -283,17 +283,17 @@ impl InfinitePlane3d {
     ///
     /// ## Guarantees
     ///
-    /// - the transformation is a [congruence] meaning it will preserve all distances and angles of
+    /// * the transformation is a [congruence] meaning it will preserve all distances and angles of
     /// the transformed geometry 
-    /// - uses the least rotation possible to transform the geometry
-    /// - if two geometries are transformed with the same isometry, then the relations between
+    /// * uses the least rotation possible to transform the geometry
+    /// * if two geometries are transformed with the same isometry, then the relations between
     /// them, like distances, are also preserved
-    /// - compared to projections, the transformation is lossless (up to floating point errors) reversible 
+    /// * compared to projections, the transformation is lossless (up to floating point errors) reversible 
     ///
     /// ## Non-Guarantees
     ///
-    /// - the rotation used is generally not unique
-    /// - the orientation of the transformed geometry in the XY plane might be arbitrary, to
+    /// * the rotation used is generally not unique
+    /// * the orientation of the transformed geometry in the XY plane might be arbitrary, to
     /// enforce some kind of alignment the user has to use an extra transformation ontop of this
     /// one
     ///
