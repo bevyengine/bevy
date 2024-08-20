883f1b79b40b69401f951e139ec42518b41583e7
Gino Valente <gino.valente.code@gmail.com>
2024-08-20 12:18:19 -0700

    Introduce `Type` type

--- crates/bevy_reflect/src/type_info.rs
+++ crates/bevy_reflect/src/type_info.rs
@@ -3,8 +3,10 @@ use crate::{
     DynamicTupleStruct, EnumInfo, ListInfo, MapInfo, PartialReflect, Reflect, ReflectKind, SetInfo,
     StructInfo, TupleInfo, TupleStructInfo, TypePath, TypePathTable,
 };
+use core::fmt::Formatter;
 use std::any::{Any, TypeId};
 use std::fmt::Debug;
+use std::hash::Hash;
 use thiserror::Error;
 
 /// A static accessor to compile-time type information.
@@ -285,6 +287,124 @@ impl TypeInfo {
     impl_cast_method!(as_map: Map => MapInfo);
     impl_cast_method!(as_enum: Enum => EnumInfo);
     impl_cast_method!(as_value: Value => ValueInfo);
+}
+
+/// The base representation of a Rust type.
+///
+/// When possible, it is recommended to use [`&'static TypeInfo`] instead of this
+/// as it provides more information as well as being smaller
+/// (since a reference only takes the same number of bytes as a `usize`).
+///
+/// However, where a static reference to [`TypeInfo`] is not possible,
+/// such as with trait objects and other types that can't implement [`Typed`],
+/// this type can be used instead.
+///
+/// It only requires that the type implements [`TypePath`].
+///
+/// And unlike [`TypeInfo`], this type implements [`Copy`], [`Eq`], and [`Hash`],
+/// making it useful as a key type.
+///
+/// It's especially helpful when compared to [`TypeId`] as it can provide the
+/// actual [type path] when debugging, while still having the same performance
+/// as hashing/comparing [`TypeId`] directly—at the cost of a little more memory.
+///
+/// [`&'static TypeInfo`]: TypeInfo
+#[derive(Copy, Clone)]
+pub struct Type {
+    type_path_table: TypePathTable,
+    type_id: TypeId,
+}
+
+impl Type {
+    /// Create a new [`Type`] from a type that implements [`TypePath`].
+    pub fn of<T: TypePath + ?Sized>() -> Self {
+        Self {
+            type_path_table: TypePathTable::of::<T>(),
+            type_id: TypeId::of::<T>(),
+        }
+    }
+
+    /// Returns the [`TypeId`] of the type.
+    pub fn type_id(&self) -> TypeId {
+        self.type_id
+    }
+
+    /// See [`TypePath::type_path`].
+    pub fn path(&self) -> &'static str {
+        self.type_path_table.path()
+    }
+
+    /// See [`TypePath::short_type_path`].
+    pub fn short_path(&self) -> &'static str {
+        self.type_path_table.short_path()
+    }
+
+    /// See [`TypePath::type_ident`].
+    pub fn ident(&self) -> Option<&'static str> {
+        self.type_path_table.ident()
+    }
+
+    /// See [`TypePath::crate_name`].
+    pub fn crate_name(&self) -> Option<&'static str> {
+        self.type_path_table.crate_name()
+    }
+
+    /// See [`TypePath::module_path`].
+    pub fn module_path(&self) -> Option<&'static str> {
+        self.type_path_table.module_path()
+    }
+
+    /// A representation of the type path of this.
+    ///
+    /// Provides dynamic access to all methods on [`TypePath`].
+    pub fn type_path_table(&self) -> TypePathTable {
+        self.type_path_table
+    }
+
+    /// Check if the given type matches this one.
+    ///
+    /// This only compares the [`TypeId`] of the types
+    /// and does not verify they share the same [`TypePath`]
+    /// (though it implies they do).
+    pub fn is<T: Any>(&self) -> bool {
+        TypeId::of::<T>() == self.type_id
+    }
+}
+
+/// This implementation will only output the [type path] of the type.
+///
+/// If you need to include the [`TypeId`] in the output,
+/// you can access it through [`Type::type_id`].
+///
+/// [type path]: TypePath
+impl Debug for Type {
+    fn fmt(&self, f: &mut Formatter<'_>) -> core::fmt::Result {
+        write!(f, "{}", self.type_path_table.path())
+    }
+}
+
+impl Eq for Type {}
+
+/// This implementation purely relies on the [`TypeId`] of the type,
+/// and not on the [type path].
+///
+/// [type path]: TypePath
+impl PartialEq for Type {
+    #[inline]
+    fn eq(&self, other: &Self) -> bool {
+        self.type_id == other.type_id
+    }
+}
+
+/// This implementation purely relies on the [`TypeId`] of the type,
+/// and not on the [type path].
+///
+/// [type path]: TypePath
+impl Hash for Type {
+    #[inline]
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        self.type_id.hash(state);
+    }
 }
 
 /// A container for compile-time info related to general value types, including primitives.
