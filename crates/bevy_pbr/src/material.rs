766082bfaec02a0406199538ba3a65ef49aebab5
robtfm <50659922+robtfm@users.noreply.github.com>
2024-08-20 13:25:47 +0100

    -uhm

--- crates/bevy_pbr/src/material.rs
+++ crates/bevy_pbr/src/material.rs
@@ -953,8 +953,8 @@ impl<M: Material> RenderAsset for PreparedMaterial<M> {
             Err(AsBindGroupError::RetryNextUpdate) => {
                 Err(PrepareAssetError::RetryNextUpdate(material))
             }
-            Err(e) => {
-                Err(PrepareAssetError::AsBindGroupError(e))
+            Err(other) => {
+                Err(PrepareAssetError::AsBindGroupError(other))
             }
         }
     }
