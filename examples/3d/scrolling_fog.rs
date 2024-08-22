42dcd5e2ce4c56d369617ccadb08cb50087b7a83
Jiří Švejda <jirisvd@gmail.com>
2024-08-22 21:34:33 +0200

    Fix typo

--- examples/3d/scrolling_fog.rs
+++ examples/3d/scrolling_fog.rs
@@ -112,6 +112,6 @@ fn setup(
 /// Moves fog density texture offset every frame.
 fn scroll_fog(time: Res<Time>, mut query: Query<&mut FogVolume>) {
     for mut fog_volume in query.iter_mut() {
-        fog_volume.density_texture_offset += Vec3::new(0.0, 0.00, 0.04) * time.delta_seconds();
+        fog_volume.density_texture_offset += Vec3::new(0.0, 0.0, 0.04) * time.delta_seconds();
     }
 }
