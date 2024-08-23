c92ee3177999e246c84eb146fafd83182cd15ac7
Jan Hohenheim <jan@hohenheim.ch>
2024-08-23 18:19:42 +0200

    Allow ordering variable timesteps around fixed timesteps (#14881)
    # Objective
    
    - Fixes #14873, see that issue for a whole lot of context
    
    ## Solution
    
    - Add a blessed system set for this stuff. See [this Discord
    discussion](https://discord.com/channels/691052431525675048/749335865876021248/1276262931327094908).
    
    Note that the gizmo systems,
    [LWIM](https://github.com/Leafwing-Studios/leafwing-input-manager/pull/522/files#diff-9b59ee4899ad0a5d008889ea89a124a7291316532e42f9f3d6ae842b906fb095R154)
    and now a new plugin I'm working on are all already ordering against
    `run_fixed_main_schedule`, so having a dedicated system set should be
    more robust and hopefully also more discoverable.
    
    ---
    
    ## ~~Showcase~~
    
    ~~I can add a little video of a smooth camera later if this gets merged
    :)~~
    Apparently a release note is not needed, so I'll leave it out. See the
    changes in the fixed timestep example for usage showcase and the video
    in #14873 for a more or less accurate video of the effect (it does not
    use the same solution though, so it is not quite the same)
    
    ## Migration Guide
    
    
    [run_fixed_main_schedule](https://docs.rs/bevy/latest/bevy/time/fn.run_fixed_main_schedule.html)
    is no longer public. If you used to order against it, use the new
    dedicated `RunFixedMainLoopSystem` system set instead. You can replace
    your usage of `run_fixed_main_schedule` one for one by
    `RunFixedMainLoopSystem::FixedMainLoop`, but it is now more idiomatic to
    place your systems in either
    `RunFixedMainLoopSystem::BeforeFixedMainLoop` or
    `RunFixedMainLoopSystem::AfterFixedMainLoop`
    
    Old:
    ```rust
    app.add_systems(
        RunFixedMainLoop,
        some_system.before(run_fixed_main_schedule)
    );
    ```
    
    New:
    ```rust
    app.add_systems(
        RunFixedMainLoop,
        some_system.in_set(RunFixedMainLoopSystem::BeforeFixedMainLoop)
    );
    ```
    
    ---------
    
    Co-authored-by: Tau Gärtli <git@tau.garden>

--- crates/bevy_gizmos/src/lib.rs
+++ crates/bevy_gizmos/src/lib.rs
@@ -246,13 +246,15 @@ impl AppGizmoBuilder for App {
             .init_resource::<GizmoStorage<Config, Swap<Fixed>>>()
             .add_systems(
                 RunFixedMainLoop,
-                start_gizmo_context::<Config, Fixed>.before(bevy_time::run_fixed_main_schedule),
+                start_gizmo_context::<Config, Fixed>
+                    .in_set(bevy_app::RunFixedMainLoopSystem::BeforeFixedMainLoop),
             )
             .add_systems(FixedFirst, clear_gizmo_context::<Config, Fixed>)
             .add_systems(FixedLast, collect_requested_gizmos::<Config, Fixed>)
             .add_systems(
                 RunFixedMainLoop,
-                end_gizmo_context::<Config, Fixed>.after(bevy_time::run_fixed_main_schedule),
+                end_gizmo_context::<Config, Fixed>
+                    .in_set(bevy_app::RunFixedMainLoopSystem::AfterFixedMainLoop),
             )
             .add_systems(
                 Last,
