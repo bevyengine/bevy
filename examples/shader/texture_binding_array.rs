df6bc5368aaae0743adeca19b166e306afde07bb
Charlotte McElwain <charlotte.c.mcelwain@gmail.com>
2024-08-25 15:24:40 -0700

    Ci.

--- examples/shader/texture_binding_array.rs
+++ examples/shader/texture_binding_array.rs
@@ -17,7 +17,6 @@ use bevy::{
         RenderApp,
     },
 };
-use bevy_render::storage::GpuShaderStorageBuffer;
 use std::{num::NonZeroU32, process::exit};
 
 /// This example uses a shader source file from the assets subdirectory
