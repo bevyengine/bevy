249f3f76f8eb7de367763a8997d58e8e2ed10a86
Zac Harrold <zac@harrold.com.au>
2024-08-31 08:03:32 +1000

    Updated `LogPlugin` Documentation
    Previous misunderstanding lead to an incorrect *Performance* section implying the log filter string was the cause of performance loss. Rather, there is performance to be _gained_ by using a compile time feature of the `log` crate to eliminate logging calls entirely. Updated information is based on the [`log` documentation](https://docs.rs/log/#compile-time-filters) and benchmark 4 from [this GitHub comment](https://github.com/gfx-rs/wgpu/issues/6046#issuecomment-2309022590)

--- crates/bevy_log/src/lib.rs
+++ crates/bevy_log/src/lib.rs
@@ -133,14 +133,22 @@ pub(crate) struct FlushGuard(SyncCell<tracing_chrome::FlushGuard>);
 /// This plugin should not be added multiple times in the same process. This plugin
 /// sets up global logging configuration for **all** Apps in a given process, and
 /// rerunning the same initialization multiple times will lead to a panic.
+///
+/// # Performance
+///
+/// Filters applied through this plugin are computed at _runtime_, which will
+/// have a non-zero impact on performance.
+/// To achieve maximum performance, consider using
+/// [_compile time_ filters](https://docs.rs/log/#compile-time-filters)
+/// provided by the [`log`](https://crates.io/crates/log) crate.
+///
+/// ```toml
+/// # cargo.toml
+/// [dependencies]
+/// log = { version = "0.4", features = ["max_level_debug", "release_max_level_warn"] }
+/// ```
 pub struct LogPlugin {
-    /// Filters logs using the [`EnvFilter`] format.
-    ///
-    /// # Performance
-    ///
-    /// Due to how this filter is implemented, it can noticeably degrade performance to use lengthy
-    /// or otherwise complex filter strings. Where possible, use [`level`](`LogPlugin::level`), as
-    /// its global filtering is much more performant.
+    /// Filters logs using the [`EnvFilter`] format
     pub filter: String,
 
     /// Filters out logs that are "less than" the given level.
