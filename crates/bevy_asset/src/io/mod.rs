5589f0da40376fb37b8b9a55214de0d02c1e09b0
Alice Cecile <alice.i.cecile@gmail.com>
2024-09-05 10:24:24 -0400

    Revert accidentally added asset docs (#15054)
    Our branch protection rules were misconfigured, allowing me to push
    directly to `origin/main` 😱 This is now resolved:
    
    
    ![image](https://github.com/user-attachments/assets/bb0ac59a-6998-42f7-80d6-8b3a865c6f53)
    
    This PR reverts those accidental changes, which will get their own PR
    momentarily...

--- crates/bevy_asset/src/io/mod.rs
+++ crates/bevy_asset/src/io/mod.rs
@@ -130,10 +130,7 @@ where
 /// API, where asset bytes and asset metadata bytes are both stored and accessible for a given
 /// `path`. This trait is not object safe, if needed use a dyn [`ErasedAssetReader`] instead.
 ///
-/// This trait defines asset-agnostic mechanisms to interact with a storage system.
-/// For the per-asset-type saving/loading logic, see [`AssetSaver`](crate::saver::AssetSaver) and [`AssetLoader`](crate::loader::AssetLoader).
-///
-/// For a mirrored version of this trait that can write assets to storage, see [`AssetWriter`].
+/// Also see [`AssetWriter`].
 pub trait AssetReader: Send + Sync + 'static {
     /// Returns a future to load the full file data at the provided path.
     ///
@@ -264,10 +261,7 @@ pub enum AssetWriterError {
 /// API, where asset bytes and asset metadata bytes are both stored and accessible for a given
 /// `path`. This trait is not object safe, if needed use a dyn [`ErasedAssetWriter`] instead.
 ///
-/// This trait defines asset-agnostic mechanisms to interact with a storage system.
-/// For the per-asset-type saving/loading logic, see [`AssetSaver`](crate::saver::AssetSaver) and [`AssetLoader`](crate::loader::AssetLoader).
-///
-/// For a mirrored version of this trait that can read assets from storage, see [`AssetReader`].
+/// Also see [`AssetReader`].
 pub trait AssetWriter: Send + Sync + 'static {
     /// Writes the full asset bytes at the provided path.
     fn write<'a>(
