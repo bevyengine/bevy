ce53370ccd407815bf33570bd8496ce7c15ff30d
Emerson Coskey <56370779+ecoskey@users.noreply.github.com>
2024-09-05 14:35:52 -0700

    I am dumb

--- crates/bevy_transform/src/components/global_transform.rs
+++ crates/bevy_transform/src/components/global_transform.rs
@@ -41,7 +41,6 @@ use bevy_reflect::{std_traits::ReflectDefault, Reflect};
 #[cfg_attr(
     feature = "bevy-support",
     derive(Component, Reflect),
-    require(GlobalTransform),
     reflect(Component, Default, PartialEq)
 )]
 pub struct GlobalTransform(Affine3A);
