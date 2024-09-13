a658d53762c821947f4ff334d278bd7cd63ca3e9
Vikas <vikas@posteo.de>
2024-09-14 01:59:21 +0200

    link configure_sets in doc

--- crates/bevy_ecs/src/schedule/config.rs
+++ crates/bevy_ecs/src/schedule/config.rs
@@ -329,7 +329,7 @@ where
     ///
     /// If you configure two [`System`]s like `(GameSystem::A).after(GameSystem::B)` or `(GameSystem::A).before(GameSystem::B)`, the `GameSystem::B` will not be automatically scheduled.
     ///
-    /// This means that the system `GameSystem::A` and the system or systems in `GameSystem::B` will run independently of each other if `GameSystem::B` was never explicitly scheduled with [`App::configure_sets`].
+    /// This means that the system `GameSystem::A` and the system or systems in `GameSystem::B` will run independently of each other if `GameSystem::B` was never explicitly scheduled with [`configure_sets`](bevy_app::App::configure_sets).
     /// If that is the case, `.after`/`.before` will not provide the desired behaviour
     /// and the systems can run in parallel or in any order determined by the scheduler.
     /// Only use `after(GameSystem::B)` and `before(GameSystem::B)` when you know that `B` has already been scheduled for you,
