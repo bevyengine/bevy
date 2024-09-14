762cab785e6434c29f551df5392382572a70e4c1
s-puig <puig.sergi@outlook.com>
2024-09-14 20:44:38 +0200

    Fix bevy_picking sprite backend panic in out of bounds atlas index

--- crates/bevy_sprite/src/picking_backend.rs
+++ crates/bevy_sprite/src/picking_backend.rs
@@ -29,18 +29,15 @@ pub fn sprite_picking(
     primary_window: Query<Entity, With<PrimaryWindow>>,
     images: Res<Assets<Image>>,
     texture_atlas_layout: Res<Assets<TextureAtlasLayout>>,
-    sprite_query: Query<
-        (
-            Entity,
-            Option<&Sprite>,
-            Option<&TextureAtlas>,
-            Option<&Handle<Image>>,
-            &GlobalTransform,
-            Option<&Pickable>,
-            &ViewVisibility,
-        ),
-        Or<(With<Sprite>, With<TextureAtlas>)>,
-    >,
+    sprite_query: Query<(
+        Entity,
+        &Sprite,
+        Option<&TextureAtlas>,
+        &Handle<Image>,
+        &GlobalTransform,
+        Option<&Pickable>,
+        &ViewVisibility,
+    )>,
     mut output: EventWriter<PointerHits>,
 ) {
     let mut sorted_sprites: Vec<_> = sprite_query.iter().collect();
@@ -88,24 +85,18 @@ pub fn sprite_picking(
                     }
 
                     // Hit box in sprite coordinate system
-                    let (extents, anchor) = if let Some((sprite, atlas)) = sprite.zip(atlas) {
-                        let extents = sprite.custom_size.or_else(|| {
-                            texture_atlas_layout
-                                .get(&atlas.layout)
-                                .map(|f| f.textures[atlas.index].size().as_vec2())
-                        })?;
-                        let anchor = sprite.anchor.as_vec();
-                        (extents, anchor)
-                    } else if let Some((sprite, image)) = sprite.zip(image) {
-                        let extents = sprite
-                            .custom_size
-                            .or_else(|| images.get(image).map(|f| f.size().as_vec2()))?;
-                        let anchor = sprite.anchor.as_vec();
-                        (extents, anchor)
-                    } else {
-                        return None;
+                    let extents = match (sprite.custom_size, atlas) {
+                        (Some(size), _) => size,
+                        (None, None) => images.get(image)?.size().as_vec2(),
+                        (None, Some(atlas)) => texture_atlas_layout
+                            .get(&atlas.layout)
+                            .and_then(|layout| layout.textures.get(atlas.index))
+                            // Dropped atlas layouts and indexes out of bounds are rendered as a sprite
+                            .map_or(images.get(image)?.size().as_vec2(), |rect| {
+                                rect.size().as_vec2()
+                            }),
                     };
-
+                    let anchor = sprite.anchor.as_vec();
                     let center = -anchor * extents;
                     let rect = Rect::from_center_half_size(center, extents / 2.0);
 
