a421bb5cc655c35c5815862583106b7761c08a2b
Viktor Gustavsson <villor94@gmail.com>
2024-09-20 22:59:50 +0200

    Add UiChildren system param

--- crates/bevy_ui/src/ghost_hierarchy.rs
+++ crates/bevy_ui/src/ghost_hierarchy.rs
@@ -1,10 +1,11 @@
 //! This module contains [`GhostNode`] and utilities to flatten the UI hierarchy, traversing past ghost nodes.
 
 use bevy_ecs::{prelude::*, system::SystemParam};
-use bevy_hierarchy::{HierarchyQueryExt, Parent};
+use bevy_hierarchy::{Children, HierarchyQueryExt, Parent};
 use bevy_reflect::prelude::*;
 use bevy_render::view::{InheritedVisibility, ViewVisibility, Visibility};
 use bevy_transform::prelude::{GlobalTransform, Transform};
+use smallvec::SmallVec;
 
 use crate::{Node, TargetCamera};
 
@@ -63,5 +64,48 @@ impl<'w, 's> UiRootNodes<'w, 's> {
                         .all(|ancestor| self.ghost_node_query.contains(ancestor))
             })
             .map(|(entity, target_camera, _)| (entity, target_camera))
+    }
+}
+
+/// System param that allows iteration of UI children, skipping over [`GhostNode`].
+///
+/// Traverses the hierarchy depth-first to ensure child order.
+#[derive(SystemParam)]
+pub struct UiChildren<'w, 's> {
+    query: Query<'w, 's, (Option<&'static Children>, Option<&'static GhostNode>)>,
+}
+
+impl<'w, 's> UiChildren<'w, 's> {
+    pub fn iter(&'s self, entity: Entity) -> UiChildrenIter<'w, 's> {
+        UiChildrenIter {
+            stack: self
+                .query
+                .get(entity)
+                .map_or(SmallVec::new(), |(children, _)| {
+                    children.into_iter().flatten().rev().copied().collect()
+                }),
+            query: &self.query,
+        }
+    }
+}
+
+pub struct UiChildrenIter<'w, 's> {
+    stack: SmallVec<[Entity; 8]>,
+    query: &'s Query<'w, 's, (Option<&'static Children>, Option<&'static GhostNode>)>,
+}
+
+impl<'w, 's> Iterator for UiChildrenIter<'w, 's> {
+    type Item = Entity;
+    fn next(&mut self) -> Option<Self::Item> {
+        loop {
+            let entity = self.stack.pop()?;
+            let (children, ghost_node) = self.query.get(entity).ok()?;
+            if ghost_node.is_none() {
+                return Some(entity);
+            }
+            if let Some(children) = children {
+                self.stack.extend(children.iter().copied());
+            }
+        }
     }
 }
