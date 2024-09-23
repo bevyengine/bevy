2d0f09fcbaac19ebbf8206767aa2942af24c3b7e
UnderScore <drojasg25@gmail.com>
2024-09-23 14:16:35 -0400

    epic :3

--- crates/bevy_render/src/lib.rs
+++ crates/bevy_render/src/lib.rs
@@ -23,8 +23,7 @@ pub mod extract_instances;
 mod extract_param;
 pub mod extract_resource;
 pub mod globals;
-pub mod gpu_component_array_buffer;
-pub mod mesh;
+pub mod gpu_component_array_buffer;pub mod mesh;
 #[cfg(not(target_arch = "wasm32"))]
 pub mod pipelined_rendering;
 pub mod primitives;
