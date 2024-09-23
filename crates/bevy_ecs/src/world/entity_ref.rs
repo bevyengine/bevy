0669dd16a42141b597828ac4617bae6750f0292f
Josh Robson Chase <josh@robsonchase.com>
2024-09-23 16:09:17 -0400

    bevy_ecs: flush entities after running observers and hooks in despawn

--- crates/bevy_ecs/src/world/entity_ref.rs
+++ crates/bevy_ecs/src/world/entity_ref.rs
@@ -1323,6 +1323,10 @@ impl<'w> EntityWorldMut<'w> {
             world.removed_components.send(component_id, self.entity);
         }
 
+        // Observers and on_remove hooks may reserve new entities, which
+        // requires a flush before Entities::free may be called.
+        world.flush_entities();
+
         let location = world
             .entities
             .free(self.entity)
