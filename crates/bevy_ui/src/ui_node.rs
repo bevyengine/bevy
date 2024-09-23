55dddaf72e9c14bf044b3ff2114aac8a92e9fb30
Piefayth <Piefayth@users.noreply.github.com>
2024-09-23 12:17:58 -0500

    UI Scrolling (#15291)
    # Objective
    
    - Fixes #8074 
    - Adopts / Supersedes #8104
    
    ## Solution
    
    Adapted from #8104 and affords the same benefits.
    
    **Additions**
    - [x] Update scrolling on relayout (height of node or contents may have
    changed)
    - [x] Make ScrollPosition component optional for ui nodes to avoid
    checking every node on scroll
    - [x] Nested scrollviews
    
    **Omissions**
    - Removed input handling for scrolling from `bevy_ui`. Users should
    update `ScrollPosition` directly.
    
    ### Implementation
    
    Adds a new `ScrollPosition` component. Updating this component on a
    `Node` with an overflow axis set to `OverflowAxis::Scroll` will
    reposition its children by that amount when calculating node transforms.
    As before, no impact on the underlying Taffy layout.
    
    Calculating this correctly is trickier than it was in #8104 due to
    `"Update scrolling on relayout"`.
    
    **Background**
    
    When `ScrollPosition` is updated directly by the user, it can be
    trivially handled in-engine by adding the parent's scroll position to
    the final location of each child node. However, _other layout actions_
    may result in a situation where `ScrollPosition` needs to be updated.
    Consider a 1000 pixel tall vertically scrolling list of 100 elements,
    each 100 pixels tall. Scrolled to the bottom, the
    `ScrollPosition.offset_y` is 9000, just enough to display the last
    element in the list. When removing an element from that list, the new
    desired `ScrollPosition.offset_y` is 8900, but, critically, that is not
    known until after the sizes and positions of the children of the
    scrollable node are resolved.
    
    All user scrolling code today handles this by delaying the resolution by
    one frame. One notable disadvantage of this is the inability to support
    `WinitSettings::desktop_app()`, since there would need to be an input
    AFTER the layout change that caused the scroll position to update for
    the results of the scroll position update to render visually.
    
    I propose the alternative in this PR, which allows for same-frame
    resolution of scrolling layout.
    
    **Resolution**
    
    _Edit: Below resolution is outdated, and replaced with the simpler usage
    of taffy's `Layout::content_size`._
    
    When recursively iterating the children of a node, each child now
    returns a `Vec2` representing the location of their own bottom right
    corner. Then, `[[0,0, [x,y]]` represents a bounding box containing the
    scrollable area filled by that child. Scrollable parents aggregate those
    areas into the bounding box of _all_ children, then consider that result
    against `ScrollPosition` to ensure its validity.
    
    In the event that resolution of the layout of the children invalidates
    the `ScrollPosition` (e.g. scrolled further than there were children to
    scroll to), _all_ children of that node must be recursively
    repositioned. The position of each child must change as a result of the
    change in scroll position.
    
    Therefore, this implementation takes care to only spend the cost of the
    "second layout pass" when a specific node actually had a
    `ScrollPosition` forcibly updated by the layout of its children.
    
    
    ## Testing
    
    Examples in `ui/scroll.rs`. There may be more complex node/style
    interactions that were unconsidered.
    
    ---
    
    ## Showcase
    
    
    
    ![scroll](https://github.com/user-attachments/assets/1331138f-93aa-4a8f-959c-6be18a04ff03)
    
    ## Alternatives
    
    - `bevy_ui` doesn't support scrolling.
    - `bevy_ui` implements scrolling with a one-frame delay on reactions to
    layout changes.

--- crates/bevy_ui/src/ui_node.rs
+++ crates/bevy_ui/src/ui_node.rs
@@ -150,6 +150,47 @@ impl Default for Node {
     }
 }
 
+/// The scroll position of the node.
+/// Updating the values of `ScrollPosition` will reposition the children of the node by the offset amount.
+/// `ScrollPosition` may be updated by the layout system when a layout change makes a previously valid `ScrollPosition` invalid.
+/// Changing this does nothing on a `Node` without a `Style` setting at least one `OverflowAxis` to `OverflowAxis::Scroll`.
+#[derive(Component, Debug, Clone, Reflect)]
+#[reflect(Component, Default)]
+pub struct ScrollPosition {
+    /// How far across the node is scrolled, in pixels. (0 = not scrolled / scrolled to right)
+    pub offset_x: f32,
+    /// How far down the node is scrolled, in pixels. (0 = not scrolled / scrolled to top)
+    pub offset_y: f32,
+}
+
+impl ScrollPosition {
+    pub const DEFAULT: Self = Self {
+        offset_x: 0.0,
+        offset_y: 0.0,
+    };
+}
+
+impl Default for ScrollPosition {
+    fn default() -> Self {
+        Self::DEFAULT
+    }
+}
+
+impl From<&ScrollPosition> for Vec2 {
+    fn from(scroll_pos: &ScrollPosition) -> Self {
+        Vec2::new(scroll_pos.offset_x, scroll_pos.offset_y)
+    }
+}
+
+impl From<&Vec2> for ScrollPosition {
+    fn from(vec: &Vec2) -> Self {
+        ScrollPosition {
+            offset_x: vec.x,
+            offset_y: vec.y,
+        }
+    }
+}
+
 /// Describes the style of a UI container node
 ///
 /// Nodes can be laid out using either Flexbox or CSS Grid Layout.
@@ -865,6 +906,29 @@ impl Overflow {
     pub const fn is_visible(&self) -> bool {
         self.x.is_visible() && self.y.is_visible()
     }
+
+    pub const fn scroll() -> Self {
+        Self {
+            x: OverflowAxis::Scroll,
+            y: OverflowAxis::Scroll,
+        }
+    }
+
+    /// Scroll overflowing items on the x axis
+    pub const fn scroll_x() -> Self {
+        Self {
+            x: OverflowAxis::Scroll,
+            y: OverflowAxis::Visible,
+        }
+    }
+
+    /// Scroll overflowing items on the y axis
+    pub const fn scroll_y() -> Self {
+        Self {
+            x: OverflowAxis::Visible,
+            y: OverflowAxis::Scroll,
+        }
+    }
 }
 
 impl Default for Overflow {
@@ -888,6 +952,8 @@ pub enum OverflowAxis {
     Clip,
     /// Hide overflowing items by influencing layout and then clipping.
     Hidden,
+    /// Scroll overflowing items.
+    Scroll,
 }
 
 impl OverflowAxis {
