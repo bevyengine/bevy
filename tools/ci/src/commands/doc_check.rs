f502587fdb0426eaec13aaa35ccfc8b7fb0d8709
Benjamin Brienen <Benjamin.Brienen@outlook.com>
2024-09-25 18:40:22 +0200

    improve doc check

--- tools/ci/src/commands/doc_check.rs
+++ tools/ci/src/commands/doc_check.rs
@@ -16,6 +16,7 @@ impl Prepare for DocCheckCommand {
             ),
             "Please fix doc warnings in output above.",
         )
-        .with_env_var("RUSTDOCFLAGS", "-D warnings")]
+        .with_env_var("RUSTDOCFLAGS", "-D warnings --cfg=docsrs")
+        .with_env_var("RUSTFLAGS", "--cfg docsrs_dep")]
     }
 }
