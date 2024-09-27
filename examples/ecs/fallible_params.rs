8d0af3270dba9282231429e842f855d7cb627b05
MiniaczQ <xnetroidpl@gmail.com>
2024-09-27 23:43:52 +0200

    document example

--- examples/ecs/fallible_params.rs
+++ examples/ecs/fallible_params.rs
@@ -1,5 +1,5 @@
-//! This example demonstrates how fallible parameters can prevent systems from running
-//! if their acquiry conditions aren't met.
+//! This example demonstrates how fallible parameters can prevent their systems
+//! from running if their acquiry conditions aren't met.
 //!
 //! Fallible parameters include:
 //! - [`Res<R>`], [`ResMut<R>`] - If resource doesn't exist.
@@ -22,10 +22,13 @@ fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, setup)
+        // We add all the systems one after another.
+        // We don't need to use run conditions here.
         .add_systems(Update, (user_input, move_targets, move_pointer).chain())
         .run();
 }
 
+/// Enemy component stores data for movement in a circle.
 #[derive(Component, Default)]
 struct Enemy {
     origin: Vec2,
@@ -34,24 +37,31 @@ struct Enemy {
     rotation_speed: f32,
 }
 
+/// Player component stores data for going after enemies.
 #[derive(Component, Default)]
 struct Player {
     speed: f32,
     rotation_speed: f32,
-    stay_away: f32,
+    min_follow_radius: f32,
 }
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    // Spawn 2D camera.
     commands.spawn(Camera2dBundle::default());
 
+    // Spawn player.
     let texture = asset_server.load("textures/simplespace/ship_C.png");
     commands.spawn((
         Player {
             speed: 100.0,
             rotation_speed: 2.0,
-            stay_away: 50.0,
+            min_follow_radius: 50.0,
         },
         SpriteBundle {
+            sprite: Sprite {
+                color: bevy::color::palettes::tailwind::BLUE_800.into(),
+                ..default()
+            },
             transform: Transform::from_translation(Vec3::ZERO),
             texture,
             ..default()
@@ -59,6 +69,9 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     ));
 }
 
+// System that reads user input.
+// If user presses 'A' we spawn a new random enemy.
+// If user presses 'R' we remove a random enemy (if any exist).
 fn user_input(
     mut commands: Commands,
     enemies: Query<Entity, With<Enemy>>,
@@ -77,7 +90,7 @@ fn user_input(
             },
             SpriteBundle {
                 sprite: Sprite {
-                    color: bevy::color::palettes::basic::RED.into(),
+                    color: bevy::color::palettes::tailwind::RED_800.into(),
                     ..default()
                 },
                 transform: Transform::from_translation(Vec3::ZERO),
@@ -94,7 +107,8 @@ fn user_input(
     }
 }
 
-// TODO: Use [`NonEmptyQuery`]
+// System that moves the enemies in a circle.
+// TODO: Use [`NonEmptyQuery`] when it exists.
 fn move_targets(mut enemies: Query<(&mut Transform, &mut Enemy)>, time: Res<Time>) {
     for (mut transform, mut target) in &mut enemies {
         target.rotation += target.rotation_speed * time.delta_seconds();
@@ -104,29 +118,35 @@ fn move_targets(mut enemies: Query<(&mut Transform, &mut Enemy)>, time: Res<Time
     }
 }
 
-/// Constantly rotates the pointer if there are no targets.
-/// Points towards target if exactly one exists.
-/// Does nothing (system doesn't run) when there are more than one entity.
+/// System that moves the player.
+/// The player will search for enemies if there are none.
+/// If there is one, player will track it.
+/// If there are too many enemies, the player will cease all action (the system will not run).
 fn move_pointer(
+    // `QuerySingleMut` ensures the system runs ONLY when exactly one entity exists.
+    // It gives us mutable access to the query content.
     mut player: QuerySingleMut<(&mut Transform, &Player)>,
+    // `Option<QuerySingle>` ensures that the system runs ONLY when zero or one entity exists.
+    // It gives us readonly access to the query content.
     enemy: Option<QuerySingle<&Transform, (With<Enemy>, Without<Player>)>>,
     time: Res<Time>,
 ) {
-    let (ref mut player_transform, ref player) = &mut *player;
+    let (player_transform, player) = &mut *player;
     if let Some(enemy_transform) = enemy {
+        // Enemy found, rotate and move towards it.
         let delta = enemy_transform.translation - player_transform.translation;
         let distance = delta.length();
         let front = delta / distance;
         let up = Vec3::Z;
         let side = front.cross(up);
         player_transform.rotation = Quat::from_mat3(&Mat3::from_cols(side, front, up));
-
-        let come_closer = distance - player.stay_away;
-        if 0.0 < come_closer {
-            let velocity = (player.speed * time.delta_seconds()).min(come_closer);
+        let max_step = distance - player.min_follow_radius;
+        if 0.0 < max_step {
+            let velocity = (player.speed * time.delta_seconds()).min(max_step);
             player_transform.translation += front * velocity;
         }
     } else {
+        // No enemy found, keep searching.
         player_transform.rotate_axis(Dir3::Z, player.rotation_speed * time.delta_seconds());
     }
 }
