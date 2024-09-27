e788e3bc83a6c53f5a6c2490e37f79e989b2e808
s-puig <39652109+s-puig@users.noreply.github.com>
2024-09-27 22:07:20 +0200

    Implement gamepads as entities (#12770)
    # Objective
    
    - Significantly improve the ergonomics of gamepads and allow new
    features
    
    Gamepads are a bit unergonomic to work with, they use resources but
    unlike other inputs, they are not limited to a single gamepad, to get
    around this it uses an identifier (Gamepad) to interact with anything
    causing all sorts of issues.
    
    1. There are too many: Gamepads, GamepadSettings, GamepadInfo,
    ButtonInput<T>, 2 Axis<T>.
    2. ButtonInput/Axis generic methods become really inconvenient to use
    e.g. any_pressed()
    3. GamepadButton/Axis structs are unnecessary boilerplate:
    
    ```rust
    for gamepad in gamepads.iter() {
            if button_inputs.just_pressed(GamepadButton::new(gamepad, GamepadButtonType::South)) {
                info!("{:?} just pressed South", gamepad);
            } else if button_inputs.just_released(GamepadButton::new(gamepad, GamepadButtonType::South))
            {
                info!("{:?} just released South", gamepad);
            }
    }
    ```
    4. Projects often need to create resources to store the selected gamepad
    and have to manually check if their gamepad is still valid anyways.
    
    - Previously attempted by #3419 and #12674
    
    
    ## Solution
    
    - Implement gamepads as entities.
    
    Using entities solves all the problems above and opens new
    possibilities.
    
    1. Reduce boilerplate and allows iteration
    
    ```rust
    let is_pressed = gamepads_buttons.iter().any(|buttons| buttons.pressed(GamepadButtonType::South))
    ```
    2. ButtonInput/Axis generic methods become ergonomic again 
    ```rust
    gamepad_buttons.any_just_pressed([GamepadButtonType::Start, GamepadButtonType::Select])
    ```
    3. Reduces the number of public components significantly (Gamepad,
    GamepadSettings, GamepadButtons, GamepadAxes)
    4. Components are highly convenient. Gamepad optional features could now
    be expressed naturally (`Option<Rumble> or Option<Gyro>`), allows devs
    to attach their own components and filter them, so code like this
    becomes possible:
    ```rust
    fn move_player<const T: usize>(
        player: Query<&Transform, With<Player<T>>>,
        gamepads_buttons: Query<&GamepadButtons, With<Player<T>>>,
    ) {
        if let Ok(gamepad_buttons) = gamepads_buttons.get_single() {
            if gamepad_buttons.pressed(GamepadButtonType::South) {
                // move player
            }
        }
    }
    ```
    ---
    
    ## Follow-up
    
    - [ ] Run conditions?
    - [ ] Rumble component
    
    # Changelog
    
    ## Added
    
    TODO
    
    ## Changed
    
    TODO
    
    ## Removed
    
    TODO
    
    
    ## Migration Guide
    
    TODO
    
    ---------
    
    Co-authored-by: Carter Anderson <mcanders1@gmail.com>

--- crates/bevy_input/src/button_input.rs
+++ crates/bevy_input/src/button_input.rs
@@ -73,15 +73,11 @@ use {
 /// ```no_run
 /// # use bevy_app::{App, NoopPluginGroup as DefaultPlugins, Update};
 /// # use bevy_ecs::{prelude::{IntoSystemConfigs, Res, Resource, resource_changed}, schedule::Condition};
-/// # use bevy_input::{ButtonInput, prelude::{GamepadButton, KeyCode, MouseButton}};
+/// # use bevy_input::{ButtonInput, prelude::{KeyCode, MouseButton}};
 ///
 /// fn main() {
 ///     App::new()
 ///         .add_plugins(DefaultPlugins)
-///         .add_systems(
-///             Update,
-///             print_gamepad.run_if(resource_changed::<ButtonInput<GamepadButton>>),
-///         )
 ///         .add_systems(
 ///             Update,
 ///             print_mouse.run_if(resource_changed::<ButtonInput<MouseButton>>),
@@ -91,10 +87,6 @@ use {
 ///             print_keyboard.run_if(resource_changed::<ButtonInput<KeyCode>>),
 ///         )
 ///         .run();
-/// }
-///
-/// fn print_gamepad(gamepad: Res<ButtonInput<GamepadButton>>) {
-///     println!("Gamepad: {:?}", gamepad.get_pressed().collect::<Vec<_>>());
 /// }
 ///
 /// fn print_mouse(mouse: Res<ButtonInput<MouseButton>>) {
@@ -112,33 +104,6 @@ use {
 ///     } else {
 ///         println!("keyboard: {:?}", keyboard.get_pressed().collect::<Vec<_>>());
 ///     }
-/// }
-/// ```
-///
-/// Accepting input from multiple devices:
-/// ```no_run
-/// # use bevy_app::{App, NoopPluginGroup as DefaultPlugins, Update};
-/// # use bevy_ecs::{prelude::IntoSystemConfigs, schedule::Condition};
-/// # use bevy_input::{ButtonInput, common_conditions::{input_just_pressed}, prelude::{GamepadButton, Gamepad, GamepadButtonType, KeyCode}};
-///
-/// fn main() {
-///     App::new()
-///         .add_plugins(DefaultPlugins)
-///         .add_systems(
-///             Update,
-///             something_used.run_if(
-///                 input_just_pressed(KeyCode::KeyE)
-///                     .or(input_just_pressed(GamepadButton::new(
-///                         Gamepad::new(0),
-///                         GamepadButtonType::West,
-///                     ))),
-///             ),
-///         )
-///         .run();
-/// }
-///
-/// fn something_used() {
-///     println!("Generic use-ish button pressed.");
 /// }
 /// ```
 ///
