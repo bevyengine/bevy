4e7801388c4fc0a5aff918013fc00331acba5bbc
Mohamed Osama <67656249+moOsama76@users.noreply.github.com>
2024-09-27 03:50:06 +0300

    Rename UiPickingBackend to UiPickingBackendPlugin (#15462)
    solves #15450

--- crates/bevy_ui/src/lib.rs
+++ crates/bevy_ui/src/lib.rs
@@ -194,7 +194,7 @@ impl Plugin for UiPlugin {
         build_ui_render(app);
 
         #[cfg(feature = "bevy_ui_picking_backend")]
-        app.add_plugins(picking_backend::UiPickingBackend);
+        app.add_plugins(picking_backend::UiPickingBackendPlugin);
     }
 
     fn finish(&self, app: &mut App) {
