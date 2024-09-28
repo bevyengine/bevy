ab9a42490a294b29764d12bcfdac5969b37cf836
Dokkae6949 <finnliry@gmail.com>
2024-09-28 17:09:05 +0200

    Improve docs of Commands::spawn
    Link to the Commands::spawn_batch function

--- crates/bevy_ecs/src/system/commands/mod.rs
+++ crates/bevy_ecs/src/system/commands/mod.rs
@@ -339,6 +339,9 @@ impl<'w, 's> Commands<'w, 's> {
     /// Pushes a [`Command`] to the queue for creating a new entity with the given [`Bundle`]'s components,
     /// and returns its corresponding [`EntityCommands`].
     ///
+    /// In case multiple bundles of the same [`Bundle`] type need to be spawned,
+    /// [`spawn_batch`](Self::spawn_batch) should be user for better performance.
+    ///
     /// # Example
     ///
     /// ```
