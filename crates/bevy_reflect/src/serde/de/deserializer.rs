bb0b7013793ac99d9fe81c85d3a062bcb52966df
aecsocket <aecsocket@tutanota.com>
2024-09-30 14:37:35 +0100

    Add default `P = ()` for ReflectDeserializer

--- crates/bevy_reflect/src/serde/de/deserializer.rs
+++ crates/bevy_reflect/src/serde/de/deserializer.rs
@@ -282,7 +282,7 @@ impl ReflectDeserializerProcessor for () {
 /// [`FromReflect`]: crate::FromReflect
 /// [`ReflectFromReflect`]: crate::ReflectFromReflect
 /// [`with_processor`]: Self::with_processor
-pub struct ReflectDeserializer<'a, P> {
+pub struct ReflectDeserializer<'a, P = ()> {
     registry: &'a TypeRegistry,
     processor: Option<&'a mut P>,
 }
