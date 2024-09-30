40c26f80aa5a5b86975e1cb1733f7a6d8a9f9973
charlotte <charlotte.c.mcelwain@gmail.com>
2024-09-30 10:28:55 -0700

    Gpu readback (#15419)
    # Objective
    
    Adds a new `Readback` component to request for readback of a
    `Handle<Image>` or `Handle<ShaderStorageBuffer>` to the CPU in a future
    frame.
    
    ## Solution
    
    We track the `Readback` component and allocate a target buffer to write
    the gpu resource into and map it back asynchronously, which then fires a
    trigger on the entity in the main world. This proccess is asynchronous,
    and generally takes a few frames.
    
    ## Showcase
    
    ```rust
    let mut buffer = ShaderStorageBuffer::from(vec![0u32; 16]);
    buffer.buffer_description.usage |= BufferUsages::COPY_SRC;
    let buffer = buffers.add(buffer);
    
    commands
        .spawn(Readback::buffer(buffer.clone()))
        .observe(|trigger: Trigger<ReadbackComplete>| {
            info!("Buffer data from previous frame {:?}", trigger.event());
        });
    ```
    
    ---------
    
    Co-authored-by: Kristoffer Søholm <k.soeholm@gmail.com>
    Co-authored-by: IceSentry <IceSentry@users.noreply.github.com>

--- crates/bevy_render/src/view/window/screenshot.rs
+++ crates/bevy_render/src/view/window/screenshot.rs
@@ -1,6 +1,7 @@
 use super::ExtractedWindows;
 use crate::{
     camera::{ManualTextureViewHandle, ManualTextureViews, NormalizedRenderTarget, RenderTarget},
+    gpu_readback,
     prelude::{Image, Shader},
     render_asset::{RenderAssetUsages, RenderAssets},
     render_resource::{
@@ -38,9 +39,7 @@ use std::{
         Mutex,
     },
 };
-use wgpu::{
-    CommandEncoder, Extent3d, ImageDataLayout, TextureFormat, COPY_BYTES_PER_ROW_ALIGNMENT,
-};
+use wgpu::{CommandEncoder, Extent3d, TextureFormat};
 
 #[derive(Event, Deref, DerefMut, Reflect, Debug)]
 #[reflect(Debug)]
@@ -376,7 +375,8 @@ fn prepare_screenshot_state(
     let texture_view = texture.create_view(&Default::default());
     let buffer = render_device.create_buffer(&wgpu::BufferDescriptor {
         label: Some("screenshot-transfer-buffer"),
-        size: get_aligned_size(size.width, size.height, format.pixel_size() as u32) as u64,
+        size: gpu_readback::get_aligned_size(size.width, size.height, format.pixel_size() as u32)
+            as u64,
         usage: BufferUsages::MAP_READ | BufferUsages::COPY_DST,
         mapped_at_creation: false,
     });
@@ -442,27 +442,6 @@ impl Plugin for ScreenshotPlugin {
                         .in_set(RenderSet::ManageViews),
                 );
         }
-    }
-}
-
-pub(crate) fn align_byte_size(value: u32) -> u32 {
-    value + (COPY_BYTES_PER_ROW_ALIGNMENT - (value % COPY_BYTES_PER_ROW_ALIGNMENT))
-}
-
-pub(crate) fn get_aligned_size(width: u32, height: u32, pixel_size: u32) -> u32 {
-    height * align_byte_size(width * pixel_size)
-}
-
-pub(crate) fn layout_data(width: u32, height: u32, format: TextureFormat) -> ImageDataLayout {
-    ImageDataLayout {
-        bytes_per_row: if height > 1 {
-            // 1 = 1 row
-            Some(get_aligned_size(width, 1, format.pixel_size() as u32))
-        } else {
-            None
-        },
-        rows_per_image: None,
-        ..Default::default()
     }
 }
 
@@ -619,7 +598,7 @@ fn render_screenshot(
             prepared_state.texture.as_image_copy(),
             wgpu::ImageCopyBuffer {
                 buffer: &prepared_state.buffer,
-                layout: layout_data(width, height, texture_format),
+                layout: gpu_readback::layout_data(width, height, texture_format),
             },
             Extent3d {
                 width,
@@ -687,7 +666,8 @@ pub(crate) fn collect_screenshots(world: &mut World) {
                 // Our buffer has been padded because we needed to align to a multiple of 256.
                 // We remove this padding here
                 let initial_row_bytes = width as usize * pixel_size;
-                let buffered_row_bytes = align_byte_size(width * pixel_size as u32) as usize;
+                let buffered_row_bytes =
+                    gpu_readback::align_byte_size(width * pixel_size as u32) as usize;
 
                 let mut take_offset = buffered_row_bytes;
                 let mut place_offset = initial_row_bytes;
