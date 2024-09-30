2224cb89a17c380eee38c5d2e134ff6cea3f7737
Martin Edlund <edlund@netzwerkplan.de>
2024-09-30 16:10:07 +0200

    fix: more detail on SubCameraView

--- crates/bevy_render/src/camera/camera.rs
+++ crates/bevy_render/src/camera/camera.rs
@@ -67,6 +67,11 @@ impl Default for Viewport {
     }
 }
 
+/// Settings to define a camera sub view.
+///
+/// When added to a camera, only the sub-section of the image defined by `size`
+/// and `offset` (relative to the `full_size` of the whole image) is projected
+/// to the cameras viewport.
 #[derive(Debug, Clone, Copy, Reflect, PartialEq, Eq)]
 pub struct SubCameraView {
     /// Size of the entire camera view
