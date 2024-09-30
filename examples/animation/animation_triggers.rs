f5bfb0e97e6a4331ff8c572724d635f77938f82e
rosefarts <gonesbird@gmail.com>
2024-09-30 13:07:45 +0200

    example init

---
+++ examples/animation/animation_triggers.rs
@@ -0,0 +1,15 @@
+use bevy::prelude::*;
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .add_systems(Update, update)
+        .run();
+}
+
+fn setup() {
+    println!("HI!");
+}
+
+fn update() {}
