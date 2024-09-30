766db9b5fd589eab4daa21dd77003f5bb2903323
Matthew Weatherley <weatherleymatthew@gmail.com>
2024-09-30 04:40:01 -0400

    Fix cubic interpolation formula used by gltf curves

--- crates/bevy_animation/src/gltf_curves.rs
+++ crates/bevy_animation/src/gltf_curves.rs
@@ -394,10 +394,10 @@ where
     T: VectorSpace,
 {
     let coeffs = (vec4(2.0, 1.0, -2.0, 1.0) * lerp + vec4(-3.0, -2.0, 3.0, -1.0)) * lerp;
-    value_start * (coeffs.x + 1.0)
-        + tangent_out_start * step_duration * lerp * coeffs.y
-        + value_end * coeffs.z
-        + tangent_in_end * step_duration * coeffs.w
+    value_start * (coeffs.x * lerp + 1.0)
+        + tangent_out_start * step_duration * lerp * (coeffs.y + 1.0)
+        + value_end * lerp * coeffs.z
+        + tangent_in_end * step_duration * lerp * coeffs.w
 }
 
 fn cubic_spline_interpolate_slices<'a, T: VectorSpace>(
