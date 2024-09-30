d61520c3d2e28ee3b8e1fbfbd55a92873c8455d9
Andreas Nilsson <nilsiker94@gmail.com>
2024-09-30 22:27:20 +0200

    Update World::flush_commands visibility to crate-level

--- crates/bevy_ecs/src/world/mod.rs
+++ crates/bevy_ecs/src/world/mod.rs
@@ -2486,7 +2486,7 @@ impl World {
     ///
     /// This will panic if any of the queued commands are [`spawn`](Commands::spawn).
     /// If this is possible, you should instead use [`flush`](Self::flush).
-    pub fn flush_commands(&mut self) {
+    pub(crate) fn flush_commands(&mut self) {
         // SAFETY: `self.command_queue` is only de-allocated in `World`'s `Drop`
         if !unsafe { self.command_queue.is_empty() } {
             // SAFETY: `self.command_queue` is only de-allocated in `World`'s `Drop`
