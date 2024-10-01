c74da72bc39418c3870f3916d6ea166e0ce48672
rosefarts <gonesbird@gmail.com>
2024-10-01 18:25:39 +0200

    Update bunny_detective.rs

--- examples/animation/bunny_detective.rs
+++ examples/animation/bunny_detective.rs
@@ -7,7 +7,7 @@ use bevy::{
         animation_event::{AnimationEvent, ReflectAnimationEvent},
         AnimationTargetId,
     },
-    color::palettes::css::WHITE,
+    color::palettes::css::{ALICE_BLUE, WHITE},
     pbr::CascadeShadowConfigBuilder,
     prelude::*,
 };
@@ -17,18 +17,22 @@ const DETECTIVE_PATH: &str = "models/animated/buntective.glb";
 
 fn main() {
     App::new()
+        .register_type::<(OnLanded, OnJumped, OnStep)>()
         .add_plugins(DefaultPlugins)
         .insert_resource(AmbientLight {
             color: Color::WHITE,
             brightness: 2000.,
         })
-        .init_resource::<OnLanded>()
+        .init_resource::<ParticleAssets>()
+        .init_resource::<BoneTargets>()
         .add_systems(Startup, setup)
         .add_systems(
             Update,
             (setup_scene_once_loaded, jump_input, simulate_particles),
         )
         .observe(OnLanded::observer)
+        .observe(OnJumped::observer)
+        .observe(OnStep::observer)
         .run();
 }
 
@@ -39,6 +43,35 @@ struct Animations {
     graph: Handle<AnimationGraph>,
 }
 
+#[derive(Resource)]
+struct BoneTargets {
+    root: AnimationTargetId,
+    right_foot: AnimationTargetId,
+    left_foot: AnimationTargetId,
+}
+
+impl Default for BoneTargets {
+    fn default() -> Self {
+        Self {
+            root: AnimationTargetId::from_iter(["Armature"]),
+            right_foot: AnimationTargetId::from_iter([
+                "Armature",
+                "pelvis",
+                "upperleg.r",
+                "lowerleg.r",
+                "foot.r",
+            ]),
+            left_foot: AnimationTargetId::from_iter([
+                "Armature",
+                "pelvis",
+                "upperleg.l",
+                "lowerleg.l",
+                "foot.l",
+            ]),
+        }
+    }
+}
+
 #[derive(Component)]
 struct Particle {
     lifeteime: Timer,
@@ -46,16 +79,13 @@ struct Particle {
     velocity: Vec3,
 }
 
-// The event that will be fired once the detective hits the ground in the jump animation.
-// It's also a resource to make it easier to re-use the asset handles for the mesh and material.
-#[derive(Event, Resource, Reflect, Clone)]
-#[reflect(AnimationEvent)]
-struct OnLanded {
+#[derive(Resource)]
+struct ParticleAssets {
     mesh: Handle<Mesh>,
     material: Handle<StandardMaterial>,
 }
 
-impl FromWorld for OnLanded {
+impl FromWorld for ParticleAssets {
     fn from_world(world: &mut World) -> Self {
         Self {
             mesh: world.resource_mut::<Assets<Mesh>>().add(Sphere::new(0.25)),
@@ -69,13 +99,119 @@ impl FromWorld for OnLanded {
     }
 }
 
+#[derive(Event, Reflect, Clone)]
+#[reflect(AnimationEvent)]
+struct OnJumped;
+
+impl AnimationEvent for OnJumped {
+    fn trigger(&self, _player: Entity, _time: f32, target: Entity, world: &mut World) {
+        world.entity_mut(target).trigger(Self);
+    }
+
+    fn clone_value(&self) -> Box<dyn AnimationEvent> {
+        Box::new(Self)
+    }
+}
+
+impl OnJumped {
+    // Spawn particles once the detective jumps.
+    fn observer(
+        trigger: Trigger<Self>,
+        mut commands: Commands,
+        particle: Res<ParticleAssets>,
+        transforms: Query<&GlobalTransform>,
+    ) {
+        let translation = transforms.get(trigger.entity()).unwrap().translation();
+        let mut rng = thread_rng();
+        // Vertical
+        for _ in 0..5 {
+            let horizontal = rng.gen::<Dir2>() * rng.gen_range(0.0..0.2);
+            let vertical = rng.gen_range(0.0..10.0);
+            let size = rng.gen_range(0.5..1.0);
+            commands.queue(spawn_particle(
+                particle.mesh.clone(),
+                particle.material.clone(),
+                translation.reject_from_normalized(Vec3::Y),
+                rng.gen_range(0.1..0.5),
+                size,
+                Vec3::new(horizontal.x, vertical, horizontal.y),
+            ));
+        }
+        // Horizontal
+        for _ in 0..20 {
+            let horizontal = rng.gen::<Dir2>() * rng.gen_range(6.0..8.0);
+            let vertical = rng.gen_range(0.0..2.0);
+            let size = rng.gen_range(0.5..1.0);
+            commands.queue(spawn_particle(
+                particle.mesh.clone(),
+                particle.material.clone(),
+                translation.reject_from_normalized(Vec3::Y),
+                rng.gen_range(0.5..1.0),
+                size,
+                Vec3::new(horizontal.x, vertical, horizontal.y),
+            ));
+        }
+    }
+}
+
+#[derive(Event, Reflect, Clone)]
+#[reflect(AnimationEvent)]
+struct OnLanded;
+
 impl AnimationEvent for OnLanded {
-    fn trigger(&self, player: Entity, _time: f32, target: Entity, world: &mut World) {
-        println!("LANDED!");
+    fn trigger(&self, _player: Entity, _time: f32, target: Entity, world: &mut World) {
+        world.entity_mut(target).trigger(Self);
+    }
+
+    fn clone_value(&self) -> Box<dyn AnimationEvent> {
+        Box::new(Self)
+    }
+}
+
+impl OnLanded {
+    // Spawn particles once the detective lands.
+    fn observer(
+        trigger: Trigger<Self>,
+        mut commands: Commands,
+        particle: Res<ParticleAssets>,
+        animations: Res<Animations>,
+        mut players: Query<(&mut AnimationPlayer, &mut AnimationTransitions)>,
+        transforms: Query<&GlobalTransform>,
+    ) {
+        // Transition to the `walk` animation.
+        let (mut player, mut transitions) = players.get_mut(trigger.entity()).unwrap();
+        transitions
+            .play(&mut player, animations.walk, Duration::from_secs_f32(1.0))
+            .repeat();
+
+        let translation = transforms.get(trigger.entity()).unwrap().translation();
+        let mut rng = thread_rng();
+        for _ in 0..25 {
+            let horizontal = rng.gen::<Dir2>() * rng.gen_range(8.0..12.0);
+            let vertical = rng.gen_range(0.0..4.0);
+            let size = rng.gen_range(0.5..1.0);
+            commands.queue(spawn_particle(
+                particle.mesh.clone(),
+                particle.material.clone(),
+                translation.reject_from_normalized(Vec3::Y),
+                rng.gen_range(0.5..1.0),
+                size,
+                Vec3::new(horizontal.x, vertical, horizontal.y),
+            ));
+        }
+    }
+}
+
+#[derive(Event, Resource, Reflect, Clone)]
+#[reflect(AnimationEvent)]
+enum OnStep {
+    Start,
+    End,
+}
+
+impl AnimationEvent for OnStep {
+    fn trigger(&self, _player: Entity, _time: f32, target: Entity, world: &mut World) {
         world.entity_mut(target).trigger(self.clone());
-        // Trigger the event on the animation player as well in order to transition to the walk animation at the end of the jump.
-        // TODO: there might be better ways of doing this, works fine though
-        world.entity_mut(player).trigger(self.clone());
     }
 
     fn clone_value(&self) -> Box<dyn AnimationEvent> {
@@ -83,48 +219,37 @@ impl AnimationEvent for OnLanded {
     }
 }
 
-impl OnLanded {
-    // The observer that will run when the detective hits the ground in the jump animation.
+impl OnStep {
+    // Spawn particles on each step the detective takes.
     fn observer(
         trigger: Trigger<Self>,
         mut commands: Commands,
-        animations: Res<Animations>,
-        mut players: Query<(&mut AnimationPlayer, &mut AnimationTransitions)>,
+        particle: Res<ParticleAssets>,
         transforms: Query<&GlobalTransform>,
     ) {
-        if let Ok((mut player, mut transitions)) = players.get_mut(trigger.entity()) {
-            // If this event was targeted to the animation player, transition to the walk animation.
-            // FIXME: setting the `transition_duration` to `1.0s` results in the `OnLanded` event being triggered many times when the transition ends.
-            // I think it's because the transition duration is then longer than the rest of the jump animation, and that causes the bug somehow.
-            transitions
-                .play(&mut player, animations.walk, Duration::from_secs_f32(0.5))
-                .repeat();
-        } else {
-            // spawn a bunch of particles :3
-            let translation = transforms.get(trigger.entity()).unwrap().translation();
-            let mut rng = thread_rng();
-            for _ in 0..25 {
-                let horizontal = rng.gen::<Dir2>() * rng.gen_range(8.0..12.0);
-                let vertical = rng.gen_range(0.0..4.0);
-                let size = rng.gen_range(0.5..1.0);
-                commands.spawn((
-                    Particle {
-                        lifeteime: Timer::from_seconds(1.0, TimerMode::Once),
-                        size,
-                        velocity: Vec3::new(horizontal.x, vertical, horizontal.y),
-                    },
-                    MaterialMeshBundle {
-                        mesh: trigger.event().mesh.clone(),
-                        material: trigger.event().material.clone(),
-                        transform: Transform {
-                            translation: translation.reject_from(Vec3::Y),
-                            scale: Vec3::splat(size),
-                            ..Default::default()
-                        },
-                        ..Default::default()
-                    },
-                ));
-            }
+        let translation = transforms.get(trigger.entity()).unwrap().translation();
+        let mut rng = thread_rng();
+        for _ in 0..25 {
+            let velocity = match trigger.event() {
+                OnStep::Start => {
+                    let horizontal = rng.gen::<Dir2>() * rng.gen_range(1.0..2.0);
+                    let vertical = rng.gen_range(0.0..2.0);
+                    Vec3::new(horizontal.x, vertical, horizontal.y)
+                }
+                OnStep::End => Vec3::new(
+                    rng.gen_range(-0.2..0.2),
+                    rng.gen_range(0.0..2.0),
+                    -rng.gen_range(0.0..10.0),
+                ),
+            };
+            commands.queue(spawn_particle(
+                particle.mesh.clone(),
+                particle.material.clone(),
+                translation.reject_from_normalized(Vec3::Y),
+                rng.gen_range(0.1..0.5),
+                rng.gen_range(0.1..1.0),
+                velocity,
+            ));
         }
     }
 }
@@ -153,6 +278,10 @@ fn setup(
     // Camera
     commands.spawn(Camera3dBundle {
         transform: Transform::from_xyz(2.0, 3.0, 5.0).looking_at(Vec3::new(0.0, 1.5, 0.0), Vec3::Y),
+        camera: Camera {
+            clear_color: ClearColorConfig::Custom(ALICE_BLUE.into()),
+            ..Default::default()
+        },
         ..default()
     });
 
@@ -160,7 +289,6 @@ fn setup(
     commands.spawn(PbrBundle {
         mesh: meshes.add(Plane3d::default().mesh().size(500000.0, 500000.0)),
         material: materials.add(Color::srgb(0.3, 0.5, 0.3)),
-        transform: Transform::from_xyz(0.0, -0.075, 0.0),
         ..default()
     });
 
@@ -193,24 +321,54 @@ fn setup(
 // When the player is added, start the `walk` animation and add the `OnLanded` event.
 fn setup_scene_once_loaded(
     mut commands: Commands,
-    landed: Res<OnLanded>,
     animations: Res<Animations>,
+    bones: Res<BoneTargets>,
     graphs: Res<Assets<AnimationGraph>>,
     mut clips: ResMut<Assets<AnimationClip>>,
     mut players: Query<(Entity, &mut AnimationPlayer), Added<AnimationPlayer>>,
 ) {
     for (entity, mut player) in &mut players {
         let graph = graphs.get(&animations.graph).unwrap();
-        let node = graph.get(animations.jump).unwrap();
-        let clip = node.clip.as_ref().and_then(|id| clips.get_mut(id)).unwrap();
 
-        // Get the id of the "pelvis" bone, this is used to spawn the particles
-        // TODO: this is not necessary in this case but I want to demonstrate the use of `add_event_to_target`
-        let target_id = AnimationTargetId::from_iter(["Armature", "pelvis"]);
-        // Trigger the `OnLanded` event on 0.53s in the jump animation.
-        // The detective hits the ground between frame 8 and 9 and the frame rate is 16 fps.
+        let jump_node = graph.get(animations.jump).unwrap();
+        let jump_clip = jump_node
+            .clip
+            .as_ref()
+            .and_then(|id| clips.get_mut(id))
+            .unwrap();
+
+        // The jump height in the original animation is not very impressive :(
+        // make it a little bit higher.
+        jump_clip.add_curve_to_target(
+            bones.root,
+            TranslationCurve(
+                AnimatableKeyframeCurve::new([
+                    (0.0, Vec3::ZERO),
+                    (0.3, Vec3::Y * 0.5),
+                    (0.5, Vec3::ZERO),
+                ])
+                .unwrap(),
+            ),
+        );
+
+        jump_clip.add_event(0.0, OnJumped);
+        // Trigger the `OnLanded` event after 0.53s in the jump animation.
+        // The feet hits the ground between frame 8 and 9 and the frame rate is 16 fps.
         // 8.5 / 16 = 5.3
-        clip.add_event_to_target(target_id, 0.53, landed.clone());
+        jump_clip.add_event(0.53, OnLanded);
+
+        let walk_node = graph.get(animations.walk).unwrap();
+        let walk_clip = walk_node
+            .clip
+            .as_ref()
+            .and_then(|id| clips.get_mut(id))
+            .unwrap();
+
+        // Trigger `OnStep` events targeting the feet.
+        walk_clip.add_event_to_target(bones.left_foot, 0.05, OnStep::Start);
+        walk_clip.add_event_to_target(bones.left_foot, 0.5, OnStep::End);
+        walk_clip.add_event_to_target(bones.right_foot, 0.55, OnStep::Start);
+        walk_clip.add_event_to_target(bones.right_foot, 0.0, OnStep::End);
 
         let mut transitions = AnimationTransitions::new();
 
@@ -258,5 +416,34 @@ fn simulate_particles(
                 .smooth_nudge(&Vec3::ZERO, 4.0, time.delta_seconds());
         }
         if transform.scale.length_squared() < 0.01 {}
+    }
+}
+
+fn spawn_particle<M: Material>(
+    mesh: Handle<Mesh>,
+    material: Handle<M>,
+    translation: Vec3,
+    lifetime: f32,
+    size: f32,
+    velocity: Vec3,
+) -> impl Command {
+    move |world: &mut World| {
+        world.spawn((
+            Particle {
+                lifeteime: Timer::from_seconds(lifetime, TimerMode::Once),
+                size,
+                velocity,
+            },
+            MaterialMeshBundle {
+                mesh,
+                material,
+                transform: Transform {
+                    translation,
+                    scale: Vec3::splat(size),
+                    ..Default::default()
+                },
+                ..Default::default()
+            },
+        ));
     }
 }
