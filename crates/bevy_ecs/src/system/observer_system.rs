45f1fa6d039610ec5efc31f00b3d7e6540188a9f
Christian Hughes <xdotdash@gmail.com>
2024-09-30 20:33:59 -0500

    poc for normal systems as observers

--- crates/bevy_ecs/src/system/observer_system.rs
+++ crates/bevy_ecs/src/system/observer_system.rs
@@ -1,18 +1,28 @@
-use crate::{
-    prelude::{Bundle, Trigger},
-    system::System,
+use std::{
+    marker::PhantomData,
+    sync::{Arc, Mutex},
 };
 
-use super::IntoSystem;
+use crate::{
+    archetype::ArchetypeComponentId,
+    component::{ComponentId, Tick},
+    event::Event,
+    prelude::{Bundle, Trigger, World},
+    query::Access,
+    system::System,
+    world::{unsafe_world_cell::UnsafeWorldCell, Command, DeferredWorld},
+};
+
+use super::{IntoSystem, SystemIn};
 
 /// Implemented for [`System`]s that have a [`Trigger`] as the first argument.
-pub trait ObserverSystem<E: 'static, B: Bundle, Out = ()>:
+pub trait ObserverSystem<E: Event, B: Bundle, Out = ()>:
     System<In = Trigger<'static, E, B>, Out = Out> + Send + 'static
 {
 }
 
 impl<
-        E: 'static,
+        E: Event,
         B: Bundle,
         Out,
         T: System<In = Trigger<'static, E, B>, Out = Out> + Send + 'static,
@@ -26,7 +36,7 @@ impl<
     label = "the trait `IntoObserverSystem` is not implemented",
     note = "for function `ObserverSystem`s, ensure the first argument is a `Trigger<T>` and any subsequent ones are `SystemParam`"
 )]
-pub trait IntoObserverSystem<E: 'static, B: Bundle, M, Out = ()>: Send + 'static {
+pub trait IntoObserverSystem<E: Event, B: Bundle, M, Out = ()>: Send + 'static {
     /// The type of [`System`] that this instance converts into.
     type System: ObserverSystem<E, B, Out>;
 
@@ -34,20 +44,137 @@ pub trait IntoObserverSystem<E: 'static, B: Bundle, M, Out = ()>: Send + 'static
     fn into_system(this: Self) -> Self::System;
 }
 
-impl<
-        S: IntoSystem<Trigger<'static, E, B>, Out, M> + Send + 'static,
-        M,
-        Out,
-        E: 'static,
-        B: Bundle,
-    > IntoObserverSystem<E, B, M, Out> for S
+#[doc(hidden)]
+pub struct ObserverMarker;
+
+impl<E, B, Out, Marker, S> IntoObserverSystem<E, B, (ObserverMarker, Marker), Out> for S
 where
-    S::System: ObserverSystem<E, B, Out>,
+    E: Event,
+    B: Bundle,
+    S: IntoSystem<Trigger<'static, E, B>, Out, Marker, System: ObserverSystem<E, B, Out>>
+        + Send
+        + 'static,
 {
-    type System = <S as IntoSystem<Trigger<'static, E, B>, Out, M>>::System;
+    type System = S::System;
 
     fn into_system(this: Self) -> Self::System {
         IntoSystem::into_system(this)
+    }
+}
+
+#[doc(hidden)]
+pub struct ExclusiveObserverMarker;
+
+impl<E, B, Marker, S> IntoObserverSystem<E, B, (ExclusiveObserverMarker, Marker), ()> for S
+where
+    E: Event,
+    B: Bundle,
+    S: IntoSystem<(), (), Marker, System: System<In = (), Out = ()>> + Send + 'static,
+{
+    type System = ExclusiveObserverWrapper<E, B, S::System>;
+
+    fn into_system(this: Self) -> Self::System {
+        ExclusiveObserverWrapper {
+            _marker: PhantomData,
+            system: Arc::new(Mutex::new(IntoSystem::into_system(this))),
+        }
+    }
+}
+
+/// A [`System`] wrapper type that queues the held system for execution.
+pub struct ExclusiveObserverWrapper<E: Event, B: Bundle, S: System<In = (), Out = ()>> {
+    _marker: PhantomData<(E, B)>,
+    system: Arc<Mutex<S>>,
+}
+
+impl<E: Event, B: Bundle, S: System<In = (), Out = ()>> System
+    for ExclusiveObserverWrapper<E, B, S>
+{
+    type In = Trigger<'static, E, B>;
+    type Out = ();
+
+    fn name(&self) -> std::borrow::Cow<'static, str> {
+        self.system.lock().unwrap().name()
+    }
+
+    fn component_access(&self) -> &Access<ComponentId> {
+        // The wrapper doesn't access any components.
+        const { &Access::new() }
+    }
+
+    fn archetype_component_access(&self) -> &Access<ArchetypeComponentId> {
+        // The wrapper doesn't access any components.
+        const { &Access::new() }
+    }
+
+    fn is_send(&self) -> bool {
+        self.system.lock().unwrap().is_send()
+    }
+
+    fn is_exclusive(&self) -> bool {
+        // The wrapper only queues the system, it doesn't run it directly.
+        false
+    }
+
+    fn has_deferred(&self) -> bool {
+        // The wrapper queues the system to run later.
+        true
+    }
+
+    unsafe fn run_unsafe(
+        &mut self,
+        _input: SystemIn<'_, Self>,
+        _world: UnsafeWorldCell,
+    ) -> Self::Out {
+        // The wrapped system is queued, not run directly.
+    }
+
+    fn apply_deferred(&mut self, world: &mut World) {
+        self.system.lock().unwrap().run((), world);
+    }
+
+    fn queue_deferred(&mut self, mut world: DeferredWorld) {
+        let system = Arc::clone(&self.system);
+        world.commands().queue(RunSystemArc(system));
+    }
+
+    unsafe fn validate_param_unsafe(&self, world: UnsafeWorldCell) -> bool {
+        self.system.lock().unwrap().validate_param_unsafe(world)
+    }
+
+    fn validate_param(&mut self, world: &World) -> bool {
+        self.system.lock().unwrap().validate_param(world)
+    }
+
+    fn initialize(&mut self, world: &mut World) {
+        self.system.lock().unwrap().initialize(world);
+    }
+
+    fn update_archetype_component_access(&mut self, _world: UnsafeWorldCell) {
+        // The wrapper doesn't access any components.
+    }
+
+    fn check_change_tick(&mut self, change_tick: Tick) {
+        // TODO: is this correct?
+        self.system.lock().unwrap().check_change_tick(change_tick);
+    }
+
+    fn get_last_run(&self) -> Tick {
+        // TODO: is this correct?
+        self.system.lock().unwrap().get_last_run()
+    }
+
+    fn set_last_run(&mut self, last_run: Tick) {
+        // TODO: is this correct?
+        self.system.lock().unwrap().set_last_run(last_run);
+    }
+}
+
+struct RunSystemArc<S: System<In = (), Out = ()>>(Arc<Mutex<S>>);
+
+impl<S: System<In = (), Out = ()>> Command for RunSystemArc<S> {
+    fn apply(self, world: &mut World) {
+        self.0.lock().unwrap().run((), world);
     }
 }
 
@@ -57,9 +184,12 @@ mod tests {
         self as bevy_ecs,
         event::Event,
         observer::Trigger,
-        system::{In, IntoSystem},
+        system::{In, IntoSystem, ResMut, Resource},
         world::World,
     };
+
+    #[derive(Resource)]
+    struct Foo(pub u32);
 
     #[derive(Event)]
     struct TriggerEvent;
@@ -82,5 +212,40 @@ mod tests {
 
         let mut world = World::new();
         world.observe(a.pipe(b));
+    }
+
+    #[test]
+    fn test_exclusive() {
+        fn foo(world: &mut World) {
+            world.get_resource_or_insert_with(|| Foo(0)).0 += 1;
+        }
+
+        let mut world = World::new();
+        world.observe::<TriggerEvent, (), _>(foo);
+        world.flush();
+
+        assert!(world.get_resource::<Foo>().is_none());
+        world.trigger(TriggerEvent);
+        world.flush();
+
+        assert_eq!(world.get_resource::<Foo>().unwrap().0, 1);
+    }
+
+    #[test]
+    fn test_nonexclusive() {
+        fn foo(mut foo: ResMut<Foo>) {
+            foo.0 += 1;
+        }
+
+        let mut world = World::new();
+        world.insert_resource(Foo(0));
+        world.observe::<TriggerEvent, (), _>(foo);
+        world.flush();
+
+        assert_eq!(world.get_resource::<Foo>().unwrap().0, 0);
+        world.trigger(TriggerEvent);
+        world.flush();
+
+        assert_eq!(world.get_resource::<Foo>().unwrap().0, 1);
     }
 }
