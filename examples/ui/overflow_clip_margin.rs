5d55bd262bf4a75cc41bccbe3b17db8ff5b06b31
ickshonpe <david.curthoys@googlemail.com>
2024-10-01 12:23:53 +0100

    Added `overflow_clip_margin` example

---
+++ examples/ui/overflow_clip_margin.rs
@@ -0,0 +1,88 @@
+//! Simple example demonstrating overflow behavior.
+
+use bevy::{color::palettes::css::*, prelude::*, winit::WinitSettings};
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        // Only run the app when there is user input. This will significantly reduce CPU/GPU use.
+        .insert_resource(WinitSettings::desktop_app())
+        .add_systems(Startup, setup)
+        .run();
+}
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn(Camera2dBundle::default());
+
+    let image = asset_server.load("branding/icon.png");
+
+    commands
+        .spawn(NodeBundle {
+            style: Style {
+                width: Val::Percent(100.),
+                height: Val::Percent(100.),
+                align_items: AlignItems::Center,
+                justify_content: JustifyContent::Center,
+                column_gap: Val::Px(100.),
+                ..Default::default()
+            },
+            background_color: ANTIQUE_WHITE.into(),
+            ..Default::default()
+        })
+        .with_children(|parent| {
+            for overflow_clip_margin in [
+                OverflowClipMargin::border_box(30.),
+                OverflowClipMargin::border_box(0.),
+                OverflowClipMargin::padding_box(0.),
+                OverflowClipMargin::content_box(0.),
+            ] {
+                parent
+                    .spawn(NodeBundle {
+                        style: Style {
+                            flex_direction: FlexDirection::Column,
+                            row_gap: Val::Px(10.),
+                            ..Default::default()
+                        },
+                        ..Default::default()
+                    })
+                    .with_children(|parent| {
+                        parent.spawn(TextBundle::from_section(
+                            format!("{overflow_clip_margin:#?}"),
+                            TextStyle::default(),
+                        ));
+                        parent
+                            .spawn(NodeBundle {
+                                style: Style {
+                                    width: Val::Px(100.),
+                                    height: Val::Px(100.),
+                                    padding: UiRect::all(Val::Px(25.)),
+                                    border: UiRect::all(Val::Px(5.)),
+                                    overflow: Overflow::clip(),
+                                    overflow_clip_margin,
+                                    ..Default::default()
+                                },
+                                border_color: Color::BLACK.into(),
+                                background_color: GRAY.into(),
+                                ..Default::default()
+                            })
+                            .with_child(NodeBundle {
+                                style: Style {
+                                    flex_basis: Val::Percent(100.),
+                                    ..Default::default()
+                                },
+                                background_color: LIGHT_CYAN.into(),
+                                ..Default::default()
+                            })
+                            .with_child(ImageBundle {
+                                image: UiImage::new(image.clone()),
+                                style: Style {
+                                    min_width: Val::Px(100.),
+                                    min_height: Val::Px(100.),
+                                    ..Default::default()
+                                },
+                                ..Default::default()
+                            });
+                    });
+            }
+        });
+}
