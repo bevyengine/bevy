6472a40a20715990b2b3e253f98285cb35ec99e3
SpecificProtagonist <specificprotagonist@posteo.org>
2024-10-03 02:02:08 +0200

    fix typo

--- crates/bevy_ecs/src/world/entity_ref.rs
+++ crates/bevy_ecs/src/world/entity_ref.rs
@@ -1202,7 +1202,7 @@ impl<'w> EntityWorldMut<'w> {
     }
 
     /// # Safety
-    /// See [`inser_by_id`]
+    /// See [`insert_by_id`]
     #[inline]
     pub(crate) unsafe fn insert_by_id_with_caller(
         &mut self,
