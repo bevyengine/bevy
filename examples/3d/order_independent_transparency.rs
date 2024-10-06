8fd3861b8a9a3f36f477f28ab60890297ff85f39
Charlotte McElwain <charlotte.c.mcelwain@gmail.com>
2024-10-06 11:04:23 -0700

    Fixes from upstream.

--- examples/3d/order_independent_transparency.rs
+++ examples/3d/order_independent_transparency.rs
@@ -28,10 +28,8 @@ fn setup(
     // camera
     commands
         .spawn((
-            Camera3dBundle {
-                transform: Transform::from_xyz(0.0, 0.0, 10.0).looking_at(Vec3::ZERO, Vec3::Y),
-                ..default()
-            },
+            Camera3d::default(),
+            Transform::from_xyz(0.0, 0.0, 10.0).looking_at(Vec3::ZERO, Vec3::Y),
             // Add this component to this camera to render transparent meshes using OIT
             OrderIndependentTransparencySettings::default(),
             RenderLayers::layer(1),
