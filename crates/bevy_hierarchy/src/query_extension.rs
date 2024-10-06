d33e3c17a85e6b089c2f81ab02727e8e96036ba6
Viktor Gustavsson <villor94@gmail.com>
2024-10-06 20:21:35 +0200

    Add iter_descendants_depth_first

--- crates/bevy_hierarchy/src/query_extension.rs
+++ crates/bevy_hierarchy/src/query_extension.rs
@@ -5,6 +5,7 @@ use bevy_ecs::{
     query::{QueryData, QueryFilter, WorldQuery},
     system::Query,
 };
+use smallvec::SmallVec;
 
 use crate::{Children, Parent};
 
@@ -35,7 +36,7 @@ pub trait HierarchyQueryExt<'w, 's, D: QueryData, F: QueryFilter> {
     /// This will not include the entity itself, and will not include any entities which are not descendants of the entity,
     /// even if they are leaves in the same hierarchical tree.
     ///
-    /// Traverses the hierarchy breadth-first.
+    /// Traverses the hierarchy depth-first.
     fn iter_leaves(&'w self, entity: Entity) -> impl Iterator<Item = Entity> + 'w
     where
         D::ReadOnly: WorldQuery<Item<'w> = &'w Children>;
@@ -68,6 +69,18 @@ pub trait HierarchyQueryExt<'w, 's, D: QueryData, F: QueryFilter> {
     /// # bevy_ecs::system::assert_is_system(system);
     /// ```
     fn iter_descendants(&'w self, entity: Entity) -> DescendantIter<'w, 's, D, F>
+    where
+        D::ReadOnly: WorldQuery<Item<'w> = &'w Children>;
+
+    /// Returns an [`Iterator`] of [`Entity`]s over all of `entity`s descendants.
+    ///
+    /// Can only be called on a [`Query`] of [`Children`] (i.e. `Query<&Children>`).
+    ///
+    /// This is a depth-first alternative to [`HierarchyQueryExt::iter_descendants`].
+    fn iter_descendants_depth_first(
+        &'w self,
+        entity: Entity,
+    ) -> DescendantDepthFirstIter<'w, 's, D, F>
     where
         D::ReadOnly: WorldQuery<Item<'w> = &'w Children>;
 
@@ -156,6 +169,16 @@ impl<'w, 's, D: QueryData, F: QueryFilter> HierarchyQueryExt<'w, 's, D, F> for Q
         DescendantIter::new(self, entity)
     }
 
+    fn iter_descendants_depth_first(
+        &'w self,
+        entity: Entity,
+    ) -> DescendantDepthFirstIter<'w, 's, D, F>
+    where
+        D::ReadOnly: WorldQuery<Item<'w> = &'w Children>,
+    {
+        DescendantDepthFirstIter::new(self, entity)
+    }
+
     fn iter_ancestors(&'w self, entity: Entity) -> AncestorIter<'w, 's, D, F>
     where
         D::ReadOnly: WorldQuery<Item<'w> = &'w Parent>,
@@ -204,6 +227,51 @@ where
 
         if let Ok(children) = self.children_query.get(entity) {
             self.vecdeque.extend(children);
+        }
+
+        Some(entity)
+    }
+}
+
+/// An [`Iterator`] of [`Entity`]s over the descendants of an [`Entity`].
+///
+/// Traverses the hierarchy depth-first.
+pub struct DescendantDepthFirstIter<'w, 's, D: QueryData, F: QueryFilter>
+where
+    D::ReadOnly: WorldQuery<Item<'w> = &'w Children>,
+{
+    children_query: &'w Query<'w, 's, D, F>,
+    stack: SmallVec<[Entity; 8]>,
+}
+
+impl<'w, 's, D: QueryData, F: QueryFilter> DescendantDepthFirstIter<'w, 's, D, F>
+where
+    D::ReadOnly: WorldQuery<Item<'w> = &'w Children>,
+{
+    /// Returns a new [`DescendantDepthFirstIter`].
+    pub fn new(children_query: &'w Query<'w, 's, D, F>, entity: Entity) -> Self {
+        DescendantDepthFirstIter {
+            children_query,
+            stack: children_query
+                .get(entity)
+                .map_or(SmallVec::new(), |children| {
+                    children.iter().rev().copied().collect()
+                }),
+        }
+    }
+}
+
+impl<'w, 's, D: QueryData, F: QueryFilter> Iterator for DescendantDepthFirstIter<'w, 's, D, F>
+where
+    D::ReadOnly: WorldQuery<Item<'w> = &'w Children>,
+{
+    type Item = Entity;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let entity = self.stack.pop()?;
+
+        if let Ok(children) = self.children_query.get(entity) {
+            self.stack.extend(children.iter().rev().copied());
         }
 
         Some(entity)
@@ -264,7 +332,7 @@ mod tests {
         let [a0, a1, a2, a3] = core::array::from_fn(|i| world.spawn(A(i)).id());
 
         world.entity_mut(a0).add_children(&[a1, a2]);
-        world.entity_mut(a2).add_children(&[a3]);
+        world.entity_mut(a1).add_children(&[a3]);
 
         let mut system_state = SystemState::<(Query<&Children>, Query<&A>)>::new(world);
         let (children_query, a_query) = system_state.get(world);
@@ -274,6 +342,25 @@ mod tests {
             .collect();
 
         assert_eq!([&A(1), &A(2), &A(3)], result.as_slice());
+    }
+
+    #[test]
+    fn descendant_depth_first_iter() {
+        let world = &mut World::new();
+
+        let [a0, a1, a2, a3] = core::array::from_fn(|i| world.spawn(A(i)).id());
+
+        world.entity_mut(a0).add_children(&[a1, a2]);
+        world.entity_mut(a1).add_children(&[a3]);
+
+        let mut system_state = SystemState::<(Query<&Children>, Query<&A>)>::new(world);
+        let (children_query, a_query) = system_state.get(world);
+
+        let result: Vec<_> = a_query
+            .iter_many(children_query.iter_descendants_depth_first(a0))
+            .collect();
+
+        assert_eq!([&A(1), &A(3), &A(2)], result.as_slice());
     }
 
     #[test]
