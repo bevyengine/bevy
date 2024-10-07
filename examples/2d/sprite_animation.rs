dc366a7fbef246ccd3cf6bd713ff2e7bcd0921e5
Emerson Coskey <56370779+ecoskey@users.noreply.github.com>
2024-10-07 15:51:46 -0700

    fix sprite_animation

--- examples/2d/sprite_animation.rs
+++ examples/2d/sprite_animation.rs
@@ -56,24 +56,23 @@ impl AnimationConfig {
 
 // This system loops through all the sprites in the `TextureAtlas`, from  `first_sprite_index` to
 // `last_sprite_index` (both defined in `AnimationConfig`).
-fn execute_animations(
-    time: Res<Time>,
-    mut query: Query<(&mut AnimationConfig, &mut TextureAtlas)>,
-) {
-    for (mut config, mut atlas) in &mut query {
+fn execute_animations(time: Res<Time>, mut query: Query<(&mut AnimationConfig, &mut Sprite)>) {
+    for (mut config, mut sprite) in &mut query {
         // we track how long the current sprite has been displayed for
         config.frame_timer.tick(time.delta());
 
         // If it has been displayed for the user-defined amount of time (fps)...
         if config.frame_timer.just_finished() {
-            if atlas.index == config.last_sprite_index {
-                // ...and it IS the last frame, then we move back to the first frame and stop.
-                atlas.index = config.first_sprite_index;
-            } else {
-                // ...and it is NOT the last frame, then we move to the next frame...
-                atlas.index += 1;
-                // ...and reset the frame timer to start counting all over again
-                config.frame_timer = AnimationConfig::timer_from_fps(config.fps);
+            if let Some(atlas) = &mut sprite.atlas {
+                if atlas.index == config.last_sprite_index {
+                    // ...and it IS the last frame, then we move back to the first frame and stop.
+                    atlas.index = config.first_sprite_index;
+                } else {
+                    // ...and it is NOT the last frame, then we move to the next frame...
+                    atlas.index += 1;
+                    // ...and reset the frame timer to start counting all over again
+                    config.frame_timer = AnimationConfig::timer_from_fps(config.fps);
+                }
             }
         }
     }
