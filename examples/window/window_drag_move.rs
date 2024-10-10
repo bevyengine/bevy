e39fc925941df813a8334c911de63356e7018cb5
Shane Celis <shane.celis@gmail.com>
2024-10-10 03:47:37 -0400

    refactor: Use CompassOctant not ResizeDirection.
    Add docs.

--- examples/window/window_drag_move.rs
+++ examples/window/window_drag_move.rs
@@ -1,17 +1,37 @@
-use bevy::{
-    prelude::*,
-    window::ResizeDirection,
-};
+//! This example illustrates drag and drag resize without window decorations.
+//!
+//! When window decorations are not present, the user cannot drag the window.
+//! The `start_drag_move()` function will permit the application to make the
+//! window draggable. It does require that the left mouse button was pressed
+//! when it is called.
+use bevy::{math::CompassOctant, prelude::*};
 
-#[derive(Clone, Copy, Eq, PartialEq, Debug, Hash, States)]
+/// Determine what do on left click.
+#[derive(Resource, Debug)]
 enum LeftClickAction {
+    /// Do nothing.
     Nothing,
+    /// Drag the window on left click.
     Drag,
+    /// Resize the window on left click.
     Resize,
 }
 
-#[derive(Resource, Default)]
-struct ResizeDir(ResizeDirection);
+/// What direction index should the window resize toward.
+#[derive(Resource)]
+struct ResizeDir(usize);
+
+/// Directions that the drag resizes the window toward.
+const DIRECTIONS: [CompassOctant; 8] = [
+    CompassOctant::North,
+    CompassOctant::NorthEast,
+    CompassOctant::East,
+    CompassOctant::SouthEast,
+    CompassOctant::South,
+    CompassOctant::SouthWest,
+    CompassOctant::West,
+    CompassOctant::NorthWest,
+];
 
 fn main() {
     App::new()
@@ -22,22 +42,18 @@ fn main() {
             }),
             ..default()
         }))
-        .init_resource::<ResizeDir>()
-        .insert_state(LeftClickAction::Drag)
+        .insert_resource(ResizeDir(7))
+        .insert_resource(LeftClickAction::Drag)
         .add_systems(Startup, setup)
-        .add_systems(Update, (change_state, move_windows))
+        .add_systems(Update, (handle_input, move_windows))
         .run();
 }
 
 fn setup(mut commands: Commands) {
+    // Camera
+    commands.spawn(Camera3d::default());
 
-    // camera
-    commands.spawn((
-        Camera3d::default(),
-        // Transform::from_xyz(-1.0, 1.0, 1.0).looking_at(Vec3::new(-1.0, 1.0, 0.0), Vec3::Y),
-        // CameraController::default(),
-    ));
-
+    // UI
     let style = TextStyle::default();
     commands
         .spawn((
@@ -54,46 +70,72 @@ fn setup(mut commands: Commands) {
         ))
         .with_children(|c| {
             c.spawn(TextBundle::from_sections([
-                TextSection::new("Controls:\n", style.clone()),
+                TextSection::new(
+                    "Demonstrate drag and drag resize without window decorations.\n\nControls:\n",
+                    style.clone(),
+                ),
                 TextSection::new("A - change left click action [", style.clone()),
                 TextSection::new("Drag", style.clone()),
                 TextSection::new("]\n", style.clone()),
-
-                TextSection::new("D - change resize direction [", style.clone()),
+                TextSection::new("S / D - change resize direction [", style.clone()),
                 TextSection::new("NorthWest", style.clone()),
                 TextSection::new("]\n", style.clone()),
             ]));
         });
 }
 
-fn change_state(input: Res<ButtonInput<KeyCode>>,
-                state: Res<State<LeftClickAction>>,
-                mut next_state: ResMut<NextState<LeftClickAction>>,
-                mut example_text: Query<&mut Text>) {
+fn handle_input(
+    input: Res<ButtonInput<KeyCode>>,
+    mut action: ResMut<LeftClickAction>,
+    mut dir: ResMut<ResizeDir>,
+    mut example_text: Query<&mut Text>,
+) {
     use LeftClickAction::*;
     if input.just_pressed(KeyCode::KeyA) {
-        let s = match state.get() {
+        *action = match *action {
             Drag => Resize,
             Resize => Nothing,
             Nothing => Drag,
         };
-        next_state.set(s);
         let mut example_text = example_text.single_mut();
-        example_text.sections[2].value = format!("{:?}", s);
+        example_text.sections[2].value = format!("{:?}", *action);
+    }
+
+    if input.just_pressed(KeyCode::KeyS) {
+        dir.0 = dir
+            .0
+            .checked_sub(1)
+            .unwrap_or(DIRECTIONS.len().saturating_sub(1));
+        let mut example_text = example_text.single_mut();
+        example_text.sections[5].value = format!("{:?}", DIRECTIONS[dir.0]);
+    }
+
+    if input.just_pressed(KeyCode::KeyD) {
+        dir.0 = (dir.0 + 1) % DIRECTIONS.len();
+        let mut example_text = example_text.single_mut();
+        example_text.sections[5].value = format!("{:?}", DIRECTIONS[dir.0]);
     }
 }
 
-
-fn move_windows(mut windows: Query<&mut Window>,
-                state: Res<State<LeftClickAction>>,
-                input: Res<ButtonInput<MouseButton>>,
-                dir: Res<ResizeDir>) {
+fn move_windows(
+    mut windows: Query<&mut Window>,
+    action: Res<LeftClickAction>,
+    input: Res<ButtonInput<MouseButton>>,
+    dir: Res<ResizeDir>,
+) {
+    // Both `start_drag_move()` and `start_drag_resize()` must be called after a
+    // left mouse button press.
+    //
+    // winit 0.30.5 may panic when initiated without a left mouse button press.
     if input.just_pressed(MouseButton::Left) {
         for mut window in windows.iter_mut() {
-            match state.get() {
+            match *action {
                 LeftClickAction::Nothing => (),
                 LeftClickAction::Drag => window.start_drag_move(),
-                LeftClickAction::Resize => window.start_drag_resize(dir.0),
+                LeftClickAction::Resize => {
+                    let d = DIRECTIONS[dir.0];
+                    window.start_drag_resize(d);
+                }
             }
         }
     }
