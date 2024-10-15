9f5f5d3d41b85cedb4a8fb499558bd40bbdba3c5
Torstein Grindvik <52322338+torsteingrindvik@users.noreply.github.com>
2024-10-15 04:08:31 +0200

    bevy_reflect: get_represented_kind_info APIs for reflected kinds (#14380)
    # Objective
    
    Fixes #14378
    
    ---------
    
    Signed-off-by: Torstein Grindvik <torstein.grindvik@muybridge.com>
    Co-authored-by: Torstein Grindvik <torstein.grindvik@muybridge.com>
    Co-authored-by: Gino Valente <49806985+MrGVSV@users.noreply.github.com>
    Co-authored-by: Alice Cecile <alice.i.cecile@gmail.com>

--- crates/bevy_reflect/src/struct_trait.rs
+++ crates/bevy_reflect/src/struct_trait.rs
@@ -73,6 +73,11 @@ pub trait Struct: PartialReflect {
 
     /// Clones the struct into a [`DynamicStruct`].
     fn clone_dynamic(&self) -> DynamicStruct;
+
+    /// Will return `None` if [`TypeInfo`] is not available.
+    fn get_represented_struct_info(&self) -> Option<&'static StructInfo> {
+        self.get_represented_type_info()?.as_struct().ok()
+    }
 }
 
 /// A container for compile-time named struct info.
