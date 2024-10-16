59f7045880c63a7b4cbd4b716a0d4a25c8731dca
Alice Cecile <alice.i.cecile@gmail.com>
2024-10-16 15:14:21 -0400

    Add warning to example

--- examples/ui/ghost_nodes.rs
+++ examples/ui/ghost_nodes.rs
@@ -1,6 +1,14 @@
 //! This example demonstrates the use of Ghost Nodes.
 //!
 //! UI layout will ignore ghost nodes, and treat their children as if they were direct descendants of the first non-ghost ancestor.
+//!
+//! # Warning
+//!
+//! This is an experimental feature, and should be used with caution,
+//! especially in concert with 3rd party plugins or systems that may not be aware of ghost nodes.
+//!
+//! To add GhostNode components to entities, you must enable the `ghost_nodes` feature flag,
+//! as they are otherwise unconstructable even though the type is defined.
 
 use bevy::{prelude::*, ui::experimental::GhostNode, winit::WinitSettings};
 
