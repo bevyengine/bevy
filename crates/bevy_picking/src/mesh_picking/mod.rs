c9f45513e2efc1374badbbcc2130c94ebf1f8edd
BD103 <59022059+BD103@users.noreply.github.com>
2024-10-24 09:22:48 -0400

    fix: rename `MeshPickingBackend` in doc links

--- crates/bevy_picking/src/mesh_picking/mod.rs
+++ crates/bevy_picking/src/mesh_picking/mod.rs
@@ -21,7 +21,7 @@ use bevy_reflect::prelude::*;
 use bevy_render::{prelude::*, view::RenderLayers};
 use ray_cast::{MeshRayCast, RayCastSettings, RayCastVisibility, SimplifiedMesh};
 
-/// Runtime settings for the [`MeshPickingBackend`].
+/// Runtime settings for the [`MeshPickingBackendPlugin`].
 #[derive(Resource, Reflect)]
 #[reflect(Resource, Default)]
 pub struct MeshPickingBackendSettings {
@@ -49,7 +49,7 @@ impl Default for MeshPickingBackendSettings {
     }
 }
 
-/// An optional component that marks cameras and target entities that should be used in the [`MeshPickingBackend`].
+/// An optional component that marks cameras and target entities that should be used in the [`MeshPickingBackendPlugin`].
 /// Only needed if [`MeshPickingBackendSettings::require_markers`] is set to `true`, and ignored otherwise.
 #[derive(Debug, Clone, Default, Component, Reflect)]
 #[reflect(Component, Default)]
