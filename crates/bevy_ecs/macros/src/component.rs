0bb7ed087bea70d2fc9cd1c49d90357f2825dc23
Alice Cecile <alice.i.cecile@gmail.com>
2024-11-06 14:48:02 -0500

    Support other forms of related components in the macro

--- crates/bevy_ecs/macros/src/component.rs
+++ crates/bevy_ecs/macros/src/component.rs
@@ -75,79 +75,31 @@ pub fn derive_component(input: TokenStream) -> TokenStream {
         .predicates
         .push(parse_quote! { Self: Send + Sync + 'static });
 
-    let requires = &attrs.requires;
-    let mut register_required = Vec::with_capacity(attrs.requires.iter().len());
-    let mut register_recursive_requires = Vec::with_capacity(attrs.requires.iter().len());
+    let RelatedComponentsReturn {
+        register_related: register_suggested,
+        register_recursive_related: register_recursive_suggested,
+        docs: suggested_component_docs,
+    } = related_components(&attrs.suggests, Relatedness::Suggested);
+    let RelatedComponentsReturn {
+        register_related: register_included,
+        register_recursive_related: register_recursive_included,
+        docs: included_component_docs,
+    } = related_components(&attrs.includes, Relatedness::Included);
+    let RelatedComponentsReturn {
+        register_related: register_required,
+        register_recursive_related: register_recursive_required,
+        docs: required_component_docs,
+    } = related_components(&attrs.requires, Relatedness::Required);
 
-    if let Some(requires) = requires {
-        let relatedness = quote! { #bevy_ecs_path::component::Relatedness::Required };
-        for require in requires {
-            let ident = &require.path;
-            register_recursive_requires.push(quote! {
-                <#ident as Component>::register_related_components(
-                    requiree,
-                    components,
-                    storages,
-                    required_components,
-                    inheritance_depth + 1,
-                );
-            });
-            match &require.func {
-                Some(RequireFunc::Path(func)) => {
-                    register_required.push(quote! {
-                        components.register_related_components_manual::<Self, #ident>(
-                            storages,
-                            required_components,
-                            || { let x: #ident = #func().into(); x },
-                            inheritance_depth,
-                            #relatedness,
-                        );
-                    });
-                }
-                Some(RequireFunc::Closure(func)) => {
-                    register_required.push(quote! {
-                        components.register_related_components_manual::<Self, #ident>(
-                            storages,
-                            required_components,
-                            || { let x: #ident = (#func)().into(); x },
-                            inheritance_depth,
-                            #relatedness,
-                        );
-                    });
-                }
-                None => {
-                    register_required.push(quote! {
-                        components.register_related_components_manual::<Self, #ident>(
-                            storages,
-                            required_components,
-                            <#ident as Default>::default,
-                            inheritance_depth,
-                            #relatedness,
-                        );
-                    });
-                }
-            }
-        }
-    }
     let struct_name = &ast.ident;
     let (impl_generics, type_generics, where_clause) = &ast.generics.split_for_impl();
-
-    let required_component_docs = attrs.requires.map(|r| {
-        let paths = r
-            .iter()
-            .map(|r| format!("[`{}`]", r.path.to_token_stream()))
-            .collect::<Vec<_>>()
-            .join(", ");
-        let doc = format!("Required Components: {paths}. \n\n A component's Required Components are inserted whenever it is inserted. Note that this will also insert the required components _of_ the required components, recursively, in depth-first order.");
-        quote! {
-            #[doc = #doc]
-        }
-    });
 
     // This puts `register_required` before `register_recursive_requires` to ensure that the constructors of _all_ top
     // level components are initialized first, giving them precedence over recursively defined constructors for the same component type
     TokenStream::from(quote! {
         #required_component_docs
+        #included_component_docs
+        #suggested_component_docs
         impl #impl_generics #bevy_ecs_path::component::Component for #struct_name #type_generics #where_clause {
             const STORAGE_TYPE: #bevy_ecs_path::component::StorageType = #storage;
             fn register_related_components(
@@ -157,8 +109,14 @@ pub fn derive_component(input: TokenStream) -> TokenStream {
                 required_components: &mut #bevy_ecs_path::component::RelatedComponents,
                 inheritance_depth: u16,
             ) {
+                #(#register_suggested)*
+                #(#register_recursive_suggested)*
+
+                #(#register_included)*
+                #(#register_recursive_included)*
+
                 #(#register_required)*
-                #(#register_recursive_requires)*
+                #(#register_recursive_required)*
             }
 
             #[allow(unused_variables)]
@@ -172,8 +130,136 @@ pub fn derive_component(input: TokenStream) -> TokenStream {
     })
 }
 
+// Because the macro crate cannot access the `bevy_ecs` crate, we need to create our own equivalent.
+// This should be kept in sync with the actual `bevy_ecs` crate!
+enum Relatedness {
+    Suggested,
+    Included,
+    Required,
+}
+
+impl Relatedness {
+    /// Returns the token that represents the corresponding `Relatedness` enum variant in `bevy_ecs`.
+    fn token(&self) -> TokenStream2 {
+        let bevy_ecs_path: Path = crate::bevy_ecs_path();
+        match self {
+            Relatedness::Suggested => quote! { #bevy_ecs_path::component::Relatedness::Included },
+            Relatedness::Included => quote! { #bevy_ecs_path::component::Relatedness::Included },
+            Relatedness::Required => quote! { #bevy_ecs_path::component::Relatedness::Required },
+        }
+    }
+
+    /// Returns the stringified name of this `Relatedness` enum variant for use in docs.
+    fn doc_name(&self) -> &'static str {
+        match self {
+            Relatedness::Suggested => "Suggested",
+            Relatedness::Included => "Included",
+            Relatedness::Required => "Required",
+        }
+    }
+
+    /// Returns the doc string fragment that explains the corresponding `Relatedness` enum variant.
+    fn doc_explanation(&self) -> &'static str {
+        match self {
+            Relatedness::Suggested => "This component might work well with the components listed above, unlocking new functionality.",
+            Relatedness::Included => "A component's Included Components are inserted whenever it is inserted. Note that this will also insert the included components _of_ the included components, recursively, in depth-first order.",
+            Relatedness::Required => "A component's Required Components are inserted whenever it is inserted. Note that this will also insert the required components _of_ the required components, recursively, in depth-first order. Unlike included components, this relationship cannot be removed.",
+        }
+    }
+}
+
+struct RelatedComponentsReturn {
+    register_related: Vec<TokenStream2>,
+    register_recursive_related: Vec<TokenStream2>,
+    docs: Option<TokenStream2>,
+}
+
+/// Generates the code needed to add related components to the component's related components list.
+fn related_components(
+    attribute: &Option<Punctuated<Related, Comma>>,
+    relatedness: Relatedness,
+) -> RelatedComponentsReturn {
+    let mut register_related = Vec::with_capacity(attribute.iter().len());
+    let mut register_recursive_related = Vec::with_capacity(attribute.iter().len());
+
+    let relatedness_token = relatedness.token();
+
+    if let Some(related) = attribute {
+        for require in related {
+            let ident = &require.path;
+            register_recursive_related.push(quote! {
+                <#ident as Component>::register_related_components(
+                    requiree,
+                    components,
+                    storages,
+                    required_components,
+                    inheritance_depth + 1,
+                );
+            });
+            match &require.func {
+                Some(RequireFunc::Path(func)) => {
+                    register_related.push(quote! {
+                        components.register_related_components_manual::<Self, #ident>(
+                            storages,
+                            required_components,
+                            || { let x: #ident = #func().into(); x },
+                            inheritance_depth,
+                            #relatedness_token,
+                        );
+                    });
+                }
+                Some(RequireFunc::Closure(func)) => {
+                    register_related.push(quote! {
+                        components.register_related_components_manual::<Self, #ident>(
+                            storages,
+                            required_components,
+                            || { let x: #ident = (#func)().into(); x },
+                            inheritance_depth,
+                            #relatedness_token,
+                        );
+                    });
+                }
+                None => {
+                    register_related.push(quote! {
+                        components.register_related_components_manual::<Self, #ident>(
+                            storages,
+                            required_components,
+                            <#ident as Default>::default,
+                            inheritance_depth,
+                            #relatedness_token,
+                        );
+                    });
+                }
+            }
+        }
+    }
+
+    let docs = attribute.as_ref().map(|r| {
+        let paths = r
+            .iter()
+            .map(|r| format!("[`{}`]", r.path.to_token_stream()))
+            .collect::<Vec<_>>()
+            .join(", ");
+        let doc_name = relatedness.doc_name();
+        let doc_explanation = relatedness.doc_explanation();
+        let doc = format!("{doc_name}: {paths}. \n\n {doc_explanation}");
+        quote! {
+            #[doc = #doc]
+        }
+    });
+
+    RelatedComponentsReturn {
+        register_related,
+        register_recursive_related,
+        docs,
+    }
+}
+
 pub const COMPONENT: &str = "component";
 pub const STORAGE: &str = "storage";
+
+pub const SUGGEST: &str = "suggest";
+pub const INCLUDE: &str = "include";
 pub const REQUIRE: &str = "require";
 
 pub const ON_ADD: &str = "on_add";
@@ -183,7 +269,9 @@ pub const ON_REMOVE: &str = "on_remove";
 
 struct Attrs {
     storage: StorageTy,
-    requires: Option<Punctuated<Require, Comma>>,
+    suggests: Option<Punctuated<Related, Comma>>,
+    includes: Option<Punctuated<Related, Comma>>,
+    requires: Option<Punctuated<Related, Comma>>,
     on_add: Option<ExprPath>,
     on_insert: Option<ExprPath>,
     on_replace: Option<ExprPath>,
@@ -196,7 +284,7 @@ enum StorageTy {
     SparseSet,
 }
 
-struct Require {
+struct Related {
     path: Path,
     func: Option<RequireFunc>,
 }
@@ -217,6 +305,8 @@ fn parse_component_attr(ast: &DeriveInput) -> Result<Attrs> {
         on_insert: None,
         on_replace: None,
         on_remove: None,
+        suggests: None,
+        includes: None,
         requires: None,
     };
 
@@ -251,9 +341,41 @@ fn parse_component_attr(ast: &DeriveInput) -> Result<Attrs> {
                     Err(nested.error("Unsupported attribute"))
                 }
             })?;
+        } else if attr.path().is_ident(SUGGEST) {
+            let punctuated =
+                attr.parse_args_with(Punctuated::<Related, Comma>::parse_terminated)?;
+            for suggest in punctuated.iter() {
+                if !require_paths.insert(suggest.path.to_token_stream().to_string()) {
+                    return Err(syn::Error::new(
+                        suggest.path.span(),
+                        "Duplicate suggested components are not allowed.",
+                    ));
+                }
+            }
+            if let Some(current) = &mut attrs.suggests {
+                current.extend(punctuated);
+            } else {
+                attrs.suggests = Some(punctuated);
+            }
+        } else if attr.path().is_ident(INCLUDE) {
+            let punctuated =
+                attr.parse_args_with(Punctuated::<Related, Comma>::parse_terminated)?;
+            for include in punctuated.iter() {
+                if !require_paths.insert(include.path.to_token_stream().to_string()) {
+                    return Err(syn::Error::new(
+                        include.path.span(),
+                        "Duplicate included components are not allowed.",
+                    ));
+                }
+            }
+            if let Some(current) = &mut attrs.includes {
+                current.extend(punctuated);
+            } else {
+                attrs.includes = Some(punctuated);
+            }
         } else if attr.path().is_ident(REQUIRE) {
             let punctuated =
-                attr.parse_args_with(Punctuated::<Require, Comma>::parse_terminated)?;
+                attr.parse_args_with(Punctuated::<Related, Comma>::parse_terminated)?;
             for require in punctuated.iter() {
                 if !require_paths.insert(require.path.to_token_stream().to_string()) {
                     return Err(syn::Error::new(
@@ -273,7 +395,7 @@ fn parse_component_attr(ast: &DeriveInput) -> Result<Attrs> {
     Ok(attrs)
 }
 
-impl Parse for Require {
+impl Parse for Related {
     fn parse(input: syn::parse::ParseStream) -> Result<Self> {
         let path = input.parse::<Path>()?;
         let func = if input.peek(Paren) {
@@ -288,7 +410,7 @@ impl Parse for Require {
         } else {
             None
         };
-        Ok(Require { path, func })
+        Ok(Related { path, func })
     }
 }
 
