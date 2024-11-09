6369690e1459e903a6adb4ac856bfd4816e8e6ce
Carter Anderson <mcanders1@gmail.com>
2024-11-08 17:10:20 -0800

    Remove Volume from prelude

--- crates/bevy_image/src/lib.rs
+++ crates/bevy_image/src/lib.rs
@@ -3,7 +3,7 @@
 #![allow(unsafe_code)]
 
 pub mod prelude {
-    pub use crate::{BevyDefault as _, Image, ImageFormat, TextureError, Volume};
+    pub use crate::{BevyDefault as _, Image, ImageFormat, TextureError};
 }
 
 mod image;
