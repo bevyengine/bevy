54e109569085be10ad633bad2adc4b4a62a01813
Tim <JustTheCoolDude@gmail.com>
2024-11-10 12:33:41 +0000

    Use backticks in deprecation message
    Co-authored-by: Joona Aalto <jondolf.dev@gmail.com>

--- crates/bevy_math/src/rotation2d.rs
+++ crates/bevy_math/src/rotation2d.rs
@@ -333,7 +333,7 @@ impl Rot2 {
     #[inline]
     #[deprecated(
         since = "0.15.0",
-        note = "Use angle_to() instead, the semantics of angle_between will change in the future."
+        note = "Use `angle_to` instead, the semantics of `angle_between` will change in the future."
     )]
     pub fn angle_between(self, other: Self) -> f32 {
         (other * self.inverse()).as_radians()
