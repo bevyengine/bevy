2e097118db9f7a9e74e6acb733bcedddfb237298
Tim <JustTheCoolDude@gmail.com>
2024-11-10 12:43:22 +0000

    Avoid code duplication
    Co-authored-by: Joona Aalto <jondolf.dev@gmail.com>

--- crates/bevy_math/src/rotation2d.rs
+++ crates/bevy_math/src/rotation2d.rs
@@ -336,7 +336,7 @@ impl Rot2 {
         note = "Use `angle_to` instead, the semantics of `angle_between` will change in the future."
     )]
     pub fn angle_between(self, other: Self) -> f32 {
-        (other * self.inverse()).as_radians()
+        self.angle_to(other)
     }
 
     /// Returns the angle in radians needed to make `self` and `other` coincide.
