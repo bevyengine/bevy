650ab0bfb07fe163e3b110e6a6175fa84f32456c
Benjamin Brienen <Benjamin.Brienen@outlook.com>
2024-11-17 01:31:46 +0100

    cleanup

--- crates/bevy_ecs/src/component.rs
+++ crates/bevy_ecs/src/component.rs
@@ -1845,15 +1845,8 @@ mod tests {
         struct C;
 
         let mut world = World::new();
-
-        // `B` requires `C` with runtime registration.
-        // This *fails*, but not when using `#[require(C)]` for `B`.
         world.register_required_components::<B, C>();
-
-        // Adding runtime registration for `A -> B` also doesn't help if done after `B -> C`,
-        // but it *does* work if done before.
         let _ = world.try_register_required_components::<A, B>();
-
         let entity = world.spawn(A).id();
         assert!(world.entity(entity).get::<C>().is_some());
     }
@@ -1871,11 +1864,8 @@ mod tests {
         struct C;
 
         let mut world = World::new();
-
         world.register_required_components::<B, C>();
-
         let _ = world.try_register_required_components::<A, B>();
-
         let id = world.spawn(B).id();
         assert!(world.entity(id).get::<C>().is_some());
     }
