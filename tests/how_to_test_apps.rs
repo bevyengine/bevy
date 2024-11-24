acef08197aea20c4ffbfedc975c04a05153d91be
Rich Churcher <rich.churcher@gmail.com>
2024-11-25 08:46:57 +1300

    Fix docs

--- tests/how_to_test_apps.rs
+++ tests/how_to_test_apps.rs
@@ -1,13 +1,12 @@
-#![allow(missing_docs)]
-/// Demonstrates simple integration testing of Bevy applications.
-///
-/// By substituting [`DefaultPlugins`] with [`MinimalPlugins`], Bevy can run completely headless.
-///
-/// The list of minimal plugins does not include things like window or input handling. The downside
-/// of this is that resources or entities associated with those systems (for example:
-/// `ButtonInput::<KeyCode>`) need to be manually added, either directly or via e.g.
-/// [`InputPlugin`]. The upside, however, is that the test has complete control over these
-/// resources, meaning we can fake user input, fake the window being moved around, and more.
+//! Demonstrates simple integration testing of Bevy applications.
+//!
+//! By substituting [`DefaultPlugins`] with [`MinimalPlugins`], Bevy can run completely headless.
+//!
+//! The list of minimal plugins does not include things like window or input handling. The downside
+//! of this is that resources or entities associated with those systems (for example:
+//! `ButtonInput::<KeyCode>`) need to be manually added, either directly or via e.g.
+//! [`InputPlugin`]. The upside, however, is that the test has complete control over these
+//! resources, meaning we can fake user input, fake the window being moved around, and more.
 use bevy::prelude::*;
 
 #[derive(Component)]
