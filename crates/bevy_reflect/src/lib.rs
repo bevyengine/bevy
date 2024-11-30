229ef7f5317b8a44c8019798b8a5620ff128c27a
Rich Churcher <rich.churcher@gmail.com>
2024-11-30 15:52:14 +1300

    Try cfg_attr

--- crates/bevy_reflect/src/lib.rs
+++ crates/bevy_reflect/src/lib.rs
@@ -1,5 +1,5 @@
 #![expect(missing_docs, reason = "Not all docs are written yet, see #3492.")]
-#![allow(internal_features)]
+#![cfg_attr(any(docsrs, docsrs_dep), expect(internal_features))]
 #![cfg_attr(any(docsrs, docsrs_dep), feature(doc_auto_cfg, rustdoc_internals))]
 #![doc(
     html_logo_url = "https://bevyengine.org/assets/icon.png",
