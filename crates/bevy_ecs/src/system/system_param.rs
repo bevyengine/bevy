410f3c478a0a72f9cdc6e5f3e705242d165c5683
SpecificProtagonist <specificprotagonist@posteo.org>
2024-12-03 20:51:50 +0100

    Use disqualified for B0001 (#16623)
    # Objective
    
    Fix #16553

--- crates/bevy_ecs/src/system/system_param.rs
+++ crates/bevy_ecs/src/system/system_param.rs
@@ -28,6 +28,7 @@ use core::{
     marker::PhantomData,
     ops::{Deref, DerefMut},
 };
+use disqualified::ShortName;
 
 use super::Populated;
 use variadics_please::all_tuples;
@@ -354,7 +355,7 @@ fn assert_component_access_compatibility(
         return;
     }
     let accesses = conflicts.format_conflict_list(world);
-    panic!("error[B0001]: Query<{query_type}, {filter_type}> in system {system_name} accesses component(s){accesses} in a way that conflicts with a previous system parameter. Consider using `Without<T>` to create disjoint Queries or merging conflicting Queries into a `ParamSet`. See: https://bevyengine.org/learn/errors/b0001");
+    panic!("error[B0001]: Query<{}, {}> in system {system_name} accesses component(s){accesses} in a way that conflicts with a previous system parameter. Consider using `Without<T>` to create disjoint Queries or merging conflicting Queries into a `ParamSet`. See: https://bevyengine.org/learn/errors/b0001", ShortName(query_type), ShortName(filter_type));
 }
 
 // SAFETY: Relevant query ComponentId and ArchetypeComponentId access is applied to SystemMeta. If
