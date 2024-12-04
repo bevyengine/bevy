f7daeb1f46ba1169848a49b312ffdb9364863d2d
Patrick Walton <pcwalton@mimiga.net>
2024-12-03 23:05:06 -0800

    Remove unused import

--- examples/tools/scene_viewer/main.rs
+++ examples/tools/scene_viewer/main.rs
@@ -20,7 +20,6 @@ mod animation_plugin;
 mod morph_viewer_plugin;
 mod scene_viewer_plugin;
 
-use bevy_render::view::GpuCulling;
 use camera_controller::{CameraController, CameraControllerPlugin};
 use morph_viewer_plugin::MorphViewerPlugin;
 use scene_viewer_plugin::{SceneHandle, SceneViewerPlugin};
