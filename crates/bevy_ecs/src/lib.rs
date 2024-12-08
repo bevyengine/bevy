5777b10e207a79b3c5f035eb20509bf5678de394
rosefarts <gonesbird@gmail.com>
2024-12-08 02:17:28 +0100

    test

--- crates/bevy_ecs/src/lib.rs
+++ crates/bevy_ecs/src/lib.rs
@@ -1484,8 +1484,9 @@ mod tests {
     #[test]
     fn resource_scope() {
         let mut world = World::default();
+        assert!(world.try_resource_scope::<A, _>(|_, _| {}).is_none());
         world.insert_resource(A(0));
-        world.resource_scope(|world: &mut World, mut value: Mut<A>| {
+        world.try_resource_scope(|world: &mut World, mut value: Mut<A>| {
             value.0 += 1;
             assert!(!world.contains_resource::<A>());
         });
