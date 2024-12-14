552e24230dfd7f9fb6a26c50cdfeeef3c536b8f2
Patrick Walton <pcwalton@mimiga.net>
2024-12-14 11:35:07 -0800

    Add `#[reflect(Debug, PartialEq)]` too

--- crates/bevy_scene/src/components.rs
+++ crates/bevy_scene/src/components.rs
@@ -16,7 +16,7 @@ use crate::{DynamicScene, Scene};
 /// Adding this component will spawn the scene as a child of that entity.
 /// Once it's spawned, the entity will have a [`SceneInstance`](crate::SceneInstance) component.
 #[derive(Component, Clone, Debug, Default, Deref, DerefMut, Reflect, PartialEq, Eq, From)]
-#[reflect(Component, Default)]
+#[reflect(Component, Default, Debug, PartialEq)]
 #[require(Transform)]
 #[cfg_attr(feature = "bevy_render", require(Visibility))]
 pub struct SceneRoot(pub Handle<Scene>);
@@ -24,7 +24,7 @@ pub struct SceneRoot(pub Handle<Scene>);
 /// Adding this component will spawn the scene as a child of that entity.
 /// Once it's spawned, the entity will have a [`SceneInstance`](crate::SceneInstance) component.
 #[derive(Component, Clone, Debug, Default, Deref, DerefMut, Reflect, PartialEq, Eq, From)]
-#[reflect(Component, Default)]
+#[reflect(Component, Default, Debug, PartialEq)]
 #[require(Transform)]
 #[cfg_attr(feature = "bevy_render", require(Visibility))]
 pub struct DynamicSceneRoot(pub Handle<DynamicScene>);
