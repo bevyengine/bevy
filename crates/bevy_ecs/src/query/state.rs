d0130a34108ece47c77232d3b4a6e85874090547
Charlotte McElwain <charlotte.c.mcelwain@gmail.com>
2024-12-13 19:25:47 -0800

    Check resource reads and write in `update_archetype_component_access`.

--- crates/bevy_ecs/src/query/state.rs
+++ crates/bevy_ecs/src/query/state.rs
@@ -583,6 +583,20 @@ impl<D: QueryData, F: QueryFilter> QueryState<D, F> {
             {
                 access.add_component_write(archetype_component_id);
             }
+            if self
+                .component_access
+                .access
+                .has_resource_read(component_id)
+            {
+                access.add_resource_read(archetype_component_id);
+            }
+            if self
+                .component_access
+                .access
+                .has_resource_write(component_id)
+            {
+                access.add_resource_write(archetype_component_id);
+            }
         }
     }
 
