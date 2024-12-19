806d8c55945687fea9758cceb7ebe599890dccb7
BD103 <59022059+BD103@users.noreply.github.com>
2024-12-19 09:26:34 -0500

    Revert "fix: feature-flag function reflection benchmarks"
    This reverts commit e4c29fb0e7cb604c01708ff0df3007e05a67507d.

--- benches/benches/bevy_reflect/main.rs
+++ benches/benches/bevy_reflect/main.rs
@@ -2,7 +2,6 @@
 
 use criterion::criterion_main;
 
-#[cfg(feature = "reflect_functions")]
 mod function;
 mod list;
 mod map;
@@ -10,16 +9,9 @@ mod path;
 mod r#struct;
 
 criterion_main!(
-    function_benches,
+    function::benches,
     list::benches,
     map::benches,
     path::benches,
     r#struct::benches,
 );
-
-// The `criterion_main!` macro doesn't support `#[cfg(...)]` annotations, so we have to create a
-// separate function instead.
-fn function_benches() {
-    #[cfg(feature = "reflect_functions")]
-    function::benches();
-}
