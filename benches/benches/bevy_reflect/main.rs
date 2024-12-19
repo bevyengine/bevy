e4c29fb0e7cb604c01708ff0df3007e05a67507d
BD103 <59022059+BD103@users.noreply.github.com>
2024-12-19 08:32:26 -0500

    fix: feature-flag function reflection benchmarks

--- benches/benches/bevy_reflect/main.rs
+++ benches/benches/bevy_reflect/main.rs
@@ -2,6 +2,7 @@
 
 use criterion::criterion_main;
 
+#[cfg(feature = "reflect_functions")]
 mod function;
 mod list;
 mod map;
@@ -9,9 +10,16 @@ mod path;
 mod r#struct;
 
 criterion_main!(
-    function::benches,
+    function_benches,
     list::benches,
     map::benches,
     path::benches,
     r#struct::benches,
 );
+
+// The `criterion_main!` macro doesn't support `#[cfg(...)]` annotations, so we have to create a
+// separate function instead.
+fn function_benches() {
+    #[cfg(feature = "reflect_functions")]
+    function::benches()
+}
