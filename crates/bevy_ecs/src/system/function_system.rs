3a447fe08652d311d99f887319f660032d321183
MiniaczQ <xnetroidpl@gmail.com>
2024-12-24 03:36:03 +0100

    Set panic as default fallible system param behavior (#16638)
    # Objective
    
    Fixes: #16578
    
    ## Solution
    
    This is a patch fix, proper fix requires a breaking change.
    
    Added `Panic` enum variant and using is as the system meta default.
    Warn once behavior can be enabled same way disabling panic (originally
    disabling wans) is.
    
    To fix an issue with the current architecture, where **all** combinator
    system params get checked together,
    combinator systems only check params of the first system.
    This will result in old, panicking behavior on subsequent systems and
    will be fixed in 0.16.
    
    ## Testing
    
    Ran unit tests and `fallible_params` example.
    
    ---------
    
    Co-authored-by: François Mockers <mockersf@gmail.com>
    Co-authored-by: François Mockers <francois.mockers@vleue.com>

--- crates/bevy_ecs/src/system/function_system.rs
+++ crates/bevy_ecs/src/system/function_system.rs
@@ -60,7 +60,7 @@ impl SystemMeta {
             is_send: true,
             has_deferred: false,
             last_run: Tick::new(0),
-            param_warn_policy: ParamWarnPolicy::Once,
+            param_warn_policy: ParamWarnPolicy::Panic,
             #[cfg(feature = "trace")]
             system_span: info_span!("system", name = name),
             #[cfg(feature = "trace")]
@@ -190,6 +190,8 @@ impl SystemMeta {
 /// State machine for emitting warnings when [system params are invalid](System::validate_param).
 #[derive(Clone, Copy)]
 pub enum ParamWarnPolicy {
+    /// Stop app with a panic.
+    Panic,
     /// No warning should ever be emitted.
     Never,
     /// The warning will be emitted once and status will update to [`Self::Never`].
@@ -200,6 +202,7 @@ impl ParamWarnPolicy {
     /// Advances the warn policy after validation failed.
     #[inline]
     fn advance(&mut self) {
+        // Ignore `Panic` case, because it stops execution before this function gets called.
         *self = Self::Never;
     }
 
@@ -209,15 +212,21 @@ impl ParamWarnPolicy {
     where
         P: SystemParam,
     {
-        if matches!(self, Self::Never) {
-            return;
+        match self {
+            Self::Panic => panic!(
+                "{0} could not access system parameter {1}",
+                name,
+                disqualified::ShortName::of::<P>()
+            ),
+            Self::Once => {
+                log::warn!(
+                    "{0} did not run because it requested inaccessible system parameter {1}",
+                    name,
+                    disqualified::ShortName::of::<P>()
+                );
+            }
+            Self::Never => {}
         }
-
-        log::warn!(
-            "{0} did not run because it requested inaccessible system parameter {1}",
-            name,
-            disqualified::ShortName::of::<P>()
-        );
     }
 }
 
@@ -231,6 +240,11 @@ where
 {
     /// Set warn policy.
     fn with_param_warn_policy(self, warn_policy: ParamWarnPolicy) -> FunctionSystem<M, F>;
+
+    /// Warn only once about invalid system parameters.
+    fn param_warn_once(self) -> FunctionSystem<M, F> {
+        self.with_param_warn_policy(ParamWarnPolicy::Once)
+    }
 
     /// Disable all param warnings.
     fn never_param_warn(self) -> FunctionSystem<M, F> {
