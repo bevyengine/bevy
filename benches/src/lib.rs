b287de77afc57b450be20cb34a9e67da19c725ab
BD103 <59022059+BD103@users.noreply.github.com>
2024-12-26 15:09:29 -0500

    chore: remove `format_bench!`
    It was unused, which is a good thing! Yippee!

--- benches/src/lib.rs
+++ benches/src/lib.rs
@@ -29,21 +29,3 @@ macro_rules! bench {
         concat!(module_path!(), "::", $name)
     };
 }
-
-/// Automatically generates the qualified name of a benchmark given string formatting arguments and
-/// its module path.
-///
-/// This macro takes string formatting arguments (see [`std::fmt`]) as input and returns a
-/// [String]. Its results are determined at runtime, so there is a small cost to using this macro.
-///
-/// If you just want to generate variants of the same benchmark name for different inputs (which is
-/// most common), please use [`BenchmarkId`](criterion::BenchmarkId) instead.
-#[macro_export]
-#[deprecated = "Prefer to use `criterion`'s built-in `BenchmarkId` instead."]
-macro_rules! format_bench {
-    ($($arg:tt)*) => {{
-        let mut qualified_name = concat!(module_path!(), "::").to_string();
-        qualified_name.push_str(format!($($arg)*));
-        qualified_name
-    }};
-}
