e838980a0808894f64641c2817490c3d2633e302
Trashtalk <trashtalk217@gmail.com>
2024-12-27 21:22:13 +0100

    fixed merge conflict (again) and added tests)

--- crates/bevy_ecs/src/storage/sorted_vec_set.rs
+++ crates/bevy_ecs/src/storage/sorted_vec_set.rs
@@ -1,3 +1,4 @@
+use alloc::vec::Vec;
 use core::cmp::Ordering;
 use smallvec::SmallVec;
 
@@ -91,7 +92,7 @@ impl<const N: usize> SortedVecSet<N> {
             }
             // It's only in the difference if it's not in other,
             // and this is the only place in other it could be
-            j < other.len() && other.0[j] != *current
+            j == other.len() || other.0[j] != *current
         });
     }
 
@@ -246,5 +247,56 @@ impl<'a, const N: usize> Iterator for Difference<'a, N> {
 impl<'a, const N: usize> From<Difference<'a, N>> for SortedVecSet<N> {
     fn from(difference: Difference<'a, N>) -> Self {
         SortedVecSet(SmallVec::from_iter(difference))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{
+        storage::SortedVecSet,
+    };
+
+    #[test]
+    fn insert_and_remove() {
+        let mut set = SortedVecSet::<8>::new();
+        set.insert(1);
+        set.insert(3);
+        set.insert(3);
+        assert_eq!(set, SortedVecSet::<8>::from_vec(vec![1, 3]));
+        set.insert(5);
+        set.insert(2);
+        assert_eq!(set, SortedVecSet::<8>::from_vec(vec![1, 2, 3, 5]));
+        set.remove(4);
+        set.remove(3);
+        set.remove(3);
+        assert_eq!(set, SortedVecSet::<8>::from_vec(vec![1, 2, 5]));
+    }
+
+    #[test]
+    fn set_operations() {
+        let set_1 = SortedVecSet::<8>::from_vec(vec![1, 2, 5, 6]);
+        let set_2 = SortedVecSet::<8>::from_vec(vec![2, 3, 6, 7, 8, 9]);
+
+        let difference: Vec<usize> = set_1.difference(&set_2).collect();
+        assert_eq!(difference, vec![1, 5]);
+        let difference: Vec<usize> = set_2.difference(&set_1).collect();
+        assert_eq!(difference, vec![3, 7, 8, 9]);
+
+        let intersection: Vec<usize> = set_1.intersection(&set_2).collect();
+        assert_eq!(intersection, vec![2, 6]);
+        let intersection: Vec<usize> = set_2.intersection(&set_1).collect();
+        assert_eq!(intersection, vec![2, 6]);
+
+        let mut set_3 = SortedVecSet::<8>::from_vec(vec![3, 4, 5, 6]);
+        set_3.difference_with(&SortedVecSet::<8>::from_vec(vec![4, 5, 8, 9]));
+        assert_eq!(set_3, SortedVecSet::<8>::from_vec(vec![3, 6]));
+
+        let mut set_4 = SortedVecSet::<8>::from_vec(vec![2, 5, 8, 9]);
+        set_4.intersect_with(&SortedVecSet::<8>::from_vec(vec![2, 3, 4, 8, 9]));
+        assert_eq!(set_4, SortedVecSet::<8>::from_vec(vec![2, 8, 9]));
+
+        let mut set_5 = SortedVecSet::<8>::from_vec(vec![2, 7, 9, 10, 11]);
+        set_5.union_with(&SortedVecSet::<8>::from_vec(vec![3, 4, 9, 11]));
+        assert_eq!(set_5, SortedVecSet::<8>::from_vec(vec![2, 3, 4, 7, 9, 10, 11]));
     }
 }
