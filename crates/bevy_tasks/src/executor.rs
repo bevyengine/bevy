ee90fd41168372180ef7e291615faa0e9cb093a7
MichiRecRoom <1008889+LikeLakers2@users.noreply.github.com>
2025-01-02 17:29:45 -0500

    Change the message so it matches the original - turns out i like putting periods by accident

--- crates/bevy_tasks/src/executor.rs
+++ crates/bevy_tasks/src/executor.rs
@@ -51,7 +51,7 @@ pub struct LocalExecutor<'a>(LocalExecutorInner<'a>);
 impl Executor<'_> {
     /// Construct a new [`Executor`]
     #[expect(clippy::allow_attributes, reason = "This lint may not always trigger.")]
-    #[allow(dead_code, reason = "not all feature flags require this function.")]
+    #[allow(dead_code, reason = "not all feature flags require this function")]
     pub const fn new() -> Self {
         Self(ExecutorInner::new())
     }
@@ -60,7 +60,7 @@ impl Executor<'_> {
 impl LocalExecutor<'_> {
     /// Construct a new [`LocalExecutor`]
     #[expect(clippy::allow_attributes, reason = "This lint may not always trigger.")]
-    #[allow(dead_code, reason = "not all feature flags require this function.")]
+    #[allow(dead_code, reason = "not all feature flags require this function")]
     pub const fn new() -> Self {
         Self(LocalExecutorInner::new())
     }
