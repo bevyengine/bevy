e07ac2ff338c4ce19212391f9d25d6256582aebf
Benjamin Brienen <Benjamin.Brienen@outlook.com>
2025-01-01 19:36:14 -0500

    fix benches

--- benches/benches/bevy_ecs/observers/simple.rs
+++ benches/benches/bevy_ecs/observers/simple.rs
@@ -1,6 +1,6 @@
 use core::hint::black_box;
 
-use bevy_ecs::{event::Event, observer::{Trigger, TriggerTarget}, world::World};
+use bevy_ecs::{event::Event, observer::{Trigger, TriggerTargets}, world::World};
 
 use criterion::Criterion;
 use rand::{prelude::SliceRandom, SeedableRng};
