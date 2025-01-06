27802e697586fc64ccf4d7eee038540f2788221d
MichiRecRoom <1008889+LikeLakers2@users.noreply.github.com>
2025-01-06 18:10:58 -0500

    bevy_render: Apply `#![deny(clippy::allow_attributes, clippy::allow_attributes_without_reason)]` (#17194)
    # Objective
    - https://github.com/bevyengine/bevy/issues/17111
    
    ## Solution
    Set the `clippy::allow_attributes` and
    `clippy::allow_attributes_without_reason` lints to `deny`, and bring
    `bevy_render` in line with the new restrictions.
    
    ## Testing
    `cargo clippy` and `cargo test --package bevy_render` were run, and no
    errors were encountered.

--- crates/bevy_render/src/mesh/allocator.rs
+++ crates/bevy_render/src/mesh/allocator.rs
@@ -156,7 +156,6 @@ pub struct MeshBufferSlice<'a> {
 pub struct SlabId(pub NonMaxU32);
 
 /// Data for a single slab.
-#[allow(clippy::large_enum_variant)]
 enum Slab {
     /// A slab that can contain multiple objects.
     General(GeneralSlab),
@@ -527,7 +526,10 @@ impl MeshAllocator {
     }
 
     /// A generic function that copies either vertex or index data into a slab.
-    #[allow(clippy::too_many_arguments)]
+    #[expect(
+        clippy::too_many_arguments,
+        reason = "Could be rewritten with less arguments using a QueryData-implementing struct, but doesn't need to be."
+    )]
     fn copy_element_data(
         &mut self,
         mesh_id: &AssetId<Mesh>,
