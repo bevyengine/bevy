37bd92b19dc91a817ca42a3fa9d22510fdca7b69
AlephCubed <jothebro500@gmail.com>
2025-01-08 12:22:31 -0800

    Removed `run` from `MinimalPlugins` example.

--- crates/bevy_internal/src/default_plugins.rs
+++ crates/bevy_internal/src/default_plugins.rs
@@ -131,5 +131,5 @@ plugin_group! {
     /// App::new().add_plugins(MinimalPlugins.set(ScheduleRunnerPlugin::run_loop(
     ///     // Run 60 times per second.
     ///     Duration::from_secs_f64(1.0 / 60.0),
-    /// ))).run();
+    /// )));
 }
