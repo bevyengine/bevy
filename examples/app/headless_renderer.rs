5fc4d50e16361a8d25f53d0c4aba1d4b3c81281f
tigregalis <anak.harimau@gmail.com>
2025-01-08 13:00:41 +0800

    fix headless_renderer example exiting immediately

--- examples/app/headless_renderer.rs
+++ examples/app/headless_renderer.rs
@@ -29,6 +29,7 @@ use bevy::{
         renderer::{RenderContext, RenderDevice, RenderQueue},
         Extract, Render, RenderApp, RenderSystems,
     },
+    window::ExitCondition,
     winit::WinitPlugin,
 };
 use crossbeam_channel::{Receiver, Sender};
@@ -93,7 +94,7 @@ fn main() {
                     primary_window: None,
                     // Don’t automatically exit due to having no windows.
                     // Instead, the code in `update()` will explicitly produce an `AppExit` event.
-                    exit_condition: bevy::window::ExitCondition::DontExit,
+                    exit_condition: ExitCondition::DontExit,
                     ..default()
                 })
                 // WinitPlugin will panic in environments without a display server.
