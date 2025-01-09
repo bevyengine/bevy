2b488c2b5ba1451ea0dc7b71a70567da91eb6aa8
MichiRecRoom <1008889+LikeLakers2@users.noreply.github.com>
2025-01-09 01:29:36 -0500

    bevy_mesh: Apply `#![deny(clippy::allow_attributes, clippy::allow_attributes_without_reason)]` (#17218)
    # Objective
    - https://github.com/bevyengine/bevy/issues/17111
    
    ## Solution
    Set the `clippy::allow_attributes` and
    `clippy::allow_attributes_without_reason` lints to `deny`, and bring
    `bevy_mesh` in line with the new restrictions.
    
    ## Testing
    `cargo clippy --tests` and `cargo test --package bevy_mesh` were run,
    and no errors were encountered.

--- crates/bevy_mesh/src/lib.rs
+++ crates/bevy_mesh/src/lib.rs
@@ -1,4 +1,9 @@
 #![expect(missing_docs, reason = "Not all docs are written yet, see #3492.")]
+#![deny(
+    clippy::allow_attributes,
+    clippy::allow_attributes_without_reason,
+    reason = "See #17111; To be removed once all crates are in-line with these attributes"
+)]
 
 extern crate alloc;
 extern crate core;
