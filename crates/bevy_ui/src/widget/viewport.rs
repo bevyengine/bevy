be3ea9b6007e193662445984ebb2733a49a3bcdc
chompaa <antony.m.3012@gmail.com>
2025-01-08 23:25:35 -0500

    Add viewport widget

---
+++ crates/bevy_ui/src/widget/viewport.rs
@@ -0,0 +1,206 @@
+use bevy_asset::Assets;
+use bevy_ecs::{
+    entity::Entity,
+    event::EventReader,
+    observer::Trigger,
+    prelude::Component,
+    query::Changed,
+    reflect::ReflectComponent,
+    system::{Commands, Local, Query, Res, ResMut},
+    world::OnAdd,
+};
+use bevy_hierarchy::{BuildChildren, Children, HierarchyQueryExt};
+use bevy_image::Image;
+use bevy_math::Rect;
+#[cfg(feature = "bevy_ui_picking_backend")]
+use bevy_picking::{
+    events::PointerState,
+    hover::HoverMap,
+    pointer::{Location, PointerId, PointerInput},
+    PickingBehavior,
+};
+use bevy_reflect::Reflect;
+use bevy_render::{
+    camera::{Camera, NormalizedRenderTarget},
+    render_resource::Extent3d,
+};
+use bevy_transform::components::GlobalTransform;
+use bevy_utils::{default, HashSet};
+#[cfg(feature = "bevy_ui_picking_backend")]
+use uuid::Uuid;
+
+use crate::{ComputedNode, Node, PositionType, Val};
+
+use super::ImageNode;
+
+#[derive(Component, Debug, Clone, Copy, Reflect)]
+#[reflect(Component, Debug)]
+pub struct Viewport {
+    camera: Entity,
+}
+
+impl Viewport {
+    pub fn new(camera: Entity) -> Self {
+        Self { camera }
+    }
+}
+
+#[cfg(feature = "bevy_ui_picking_backend")]
+pub fn viewport_picking(
+    mut commands: Commands,
+    viewport_query: Query<(&Viewport, &PointerId)>,
+    node_query: Query<(&ComputedNode, &ImageNode, &GlobalTransform)>,
+    camera_query: Query<&Camera>,
+    children_query: Query<&Children>,
+    hover_map: Res<HoverMap>,
+    pointer_state: Res<PointerState>,
+    mut pointer_inputs: EventReader<PointerInput>,
+    // TODO: Remove when #17230 is resolved
+    mut dragged_last_frame: Local<HashSet<(Entity, PointerId)>>,
+) {
+    let mut viewport_picks: HashSet<(Entity, PointerId)> = dragged_last_frame.drain().collect();
+
+    for (hover_pointer_id, hits) in hover_map.iter() {
+        for (entity, _hit_data) in hits.iter() {
+            if viewport_query.contains(*entity) {
+                viewport_picks.insert((*entity, *hover_pointer_id));
+            }
+        }
+    }
+
+    // Currently, we have only retrieved viewport entities if they are being hovered. However, this
+    // does not allow dragging in-and-out of viewports.
+    //
+    // We resolve this by considering viewports that are being dragged.
+    for ((pointer_id, _pointer_button), pointer_state) in pointer_state.pointer_buttons.iter() {
+        for &target in pointer_state
+            .dragging
+            .keys()
+            .filter(|&entity| viewport_query.contains(*entity))
+        {
+            dragged_last_frame.insert((target, *pointer_id));
+            viewport_picks.insert((target, *pointer_id));
+        }
+    }
+
+    for (root, pick_pointer_id) in viewport_picks {
+        let Ok((&viewport, &viewport_pointer_id)) = viewport_query.get(root) else {
+            // TODO: Error?
+            continue;
+        };
+
+        let Some((computed_node, image_node, global_transform)) = children_query
+            .iter_descendants(root)
+            .find_map(|child| node_query.get(child).ok())
+        else {
+            continue;
+        };
+
+        let Some(cam_viewport_size) = camera_query
+            .get(viewport.camera)
+            .ok()
+            .map(|camera| camera.logical_viewport_size())
+            .flatten()
+        else {
+            // TODO: Error?
+            continue;
+        };
+
+        let node_rect = Rect::from_center_size(
+            global_transform.translation().truncate(),
+            computed_node.size(),
+        );
+        let top_left = node_rect.min * computed_node.inverse_scale_factor();
+        let logical_size = computed_node.size() * computed_node.inverse_scale_factor();
+
+        for input in pointer_inputs
+            .read()
+            .filter(|input| input.pointer_id == pick_pointer_id)
+        {
+            let local_position = (input.location.position - top_left) / logical_size;
+            let position = local_position * cam_viewport_size;
+
+            let location = Location {
+                position,
+                target: NormalizedRenderTarget::Image(image_node.image.clone().into()),
+            };
+
+            commands.send_event(PointerInput {
+                location,
+                pointer_id: viewport_pointer_id,
+                action: input.action,
+            });
+        }
+    }
+}
+
+pub fn on_viewport_added(
+    trigger: Trigger<OnAdd, Viewport>,
+    mut commands: Commands,
+    viewport_query: Query<&Viewport>,
+    camera_query: Query<&Camera>,
+) {
+    let node = trigger.target();
+
+    let viewport = viewport_query.get(node).unwrap();
+    let Ok(camera) = camera_query.get(viewport.camera) else {
+        // TODO: Error?
+        return;
+    };
+    let Some(image_handle) = camera.target.as_image() else {
+        // TODO: Error?
+        return;
+    };
+    commands
+        .spawn((
+            ImageNode::new(image_handle.clone()),
+            Node {
+                position_type: PositionType::Absolute,
+                top: Val::ZERO,
+                bottom: Val::ZERO,
+                left: Val::ZERO,
+                right: Val::ZERO,
+                ..default()
+            },
+            #[cfg(feature = "bevy_ui_picking_backend")]
+            PickingBehavior::IGNORE,
+        ))
+        .set_parent(node);
+
+    #[cfg(feature = "bevy_ui_picking_backend")]
+    commands
+        .entity(node)
+        .insert(PointerId::Custom(Uuid::new_v4()));
+}
+
+pub fn update_viewport_render_target_size(
+    viewport_query: Query<(Entity, &Viewport)>,
+    node_query: Query<&ComputedNode, Changed<ComputedNode>>,
+    camera_query: Query<&Camera>,
+    children_query: Query<&Children>,
+    mut images: ResMut<Assets<Image>>,
+) {
+    for (root, viewport) in &viewport_query {
+        let Some(computed_node) = children_query
+            .iter_descendants(root)
+            .find_map(|child| node_query.get(child).ok())
+        else {
+            // Node hasn't been changed, or one wasn't found
+            continue;
+        };
+
+        let camera = camera_query.get(viewport.camera).unwrap();
+        let size = computed_node.size();
+
+        let Some(image_handle) = camera.target.as_image() else {
+            // TODO: Error?
+            continue;
+        };
+        let size = Extent3d {
+            width: u32::max(1, size.x as u32),
+            height: u32::max(1, size.y as u32),
+            ..default()
+        };
+        images.get_mut(image_handle).unwrap().resize(size);
+    }
+}
