eff31fc0dcc4b68efdedbba61e77431191c627a9
MichiRecRoom <1008889+LikeLakers2@users.noreply.github.com>
2025-01-10 23:11:07 -0500

    bevy_image: Apply `#![deny(clippy::allow_attributes, clippy::allow_attributes_without_reason)]` (#17289)
    # Objective
    - https://github.com/bevyengine/bevy/issues/17111
    
    ## Solution
    Set the `clippy::allow_attributes` and
    `clippy::allow_attributes_without_reason` lints to `deny`, and bring
    `bevy_image` in line with the new restrictions.
    
    ## Testing
    `cargo clippy --tests --package bevy_image` was run, and no errors were
    encountered.
    
    I could not run the above command with `--all-features` due to some
    compilation errors with `bevy_core_pipeline` and `bevy_math` - but
    hopefully CI catches anything I missed.
    
    ---------
    
    Co-authored-by: Benjamin Brienen <benjamin.brienen@outlook.com>

--- crates/bevy_image/src/lib.rs
+++ crates/bevy_image/src/lib.rs
@@ -1,5 +1,9 @@
 #![expect(missing_docs, reason = "Not all docs are written yet, see #3492.")]
-#![allow(unsafe_code)]
+#![deny(
+    clippy::allow_attributes,
+    clippy::allow_attributes_without_reason,
+    reason = "See #17111; To be removed once all crates are in-line with these attributes"
+)]
 
 extern crate alloc;
 
