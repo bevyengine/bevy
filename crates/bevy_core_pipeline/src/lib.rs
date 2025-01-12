1376c564f4adaa907df974426bf132e2f4d9f25c
MichiRecRoom <1008889+LikeLakers2@users.noreply.github.com>
2025-01-12 00:17:16 -0500

    Downgrade the lints to warn - see https://github.com/bevyengine/bevy/pull/17320

--- crates/bevy_core_pipeline/src/lib.rs
+++ crates/bevy_core_pipeline/src/lib.rs
@@ -1,6 +1,6 @@
 #![expect(missing_docs, reason = "Not all docs are written yet, see #3492.")]
 #![forbid(unsafe_code)]
-#![deny(
+#![warn(
     clippy::allow_attributes,
     clippy::allow_attributes_without_reason,
     reason = "See #17111; To be removed once all crates are in-line with these attributes"
