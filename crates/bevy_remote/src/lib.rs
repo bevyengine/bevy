86219d74843d46f01ef6b4eee82d25583e1a13f6
Ben Whitley <dev@purplg.com>
2025-01-14 02:55:40 -0500

    Add BRP method to mutate a component (#16940)
    # Objective
    
    Add a method to mutate components with BRP.
    
    Currently the only way to modify a component on an entity with BRP is to
    insert a new one with the new values. This isn't ideal for several
    reasons, one reason being that the client has to know what all the
    fields are of the component and stay in sync with the server.
    
    ## Solution
    
    Add a new BRP method called `bevy/mutate_component` to mutate a single
    field in a component on an entity.
    
    ## Testing
    
    Tested on a simple scene on all `Transform`, `Name`, and a custom
    component.
    
    ---
    
    ## Showcase
    
    Example JSON-RPC request to change the `Name` of an entity to "New
    name!"
    
    ```json
    {
        "jsonrpc": "2.0",
        "id": 0,
        "method": "bevy/mutate_component",
        "params": {
            "entity": 4294967308,
            "component": "bevy_ecs::name::Name",
            "path": ".name",
            "value": "New name!"
        }
    }
    ```
    
    Or setting the X translation to 10.0 on a Transform:
    
    ```json
    {
        "jsonrpc": "2.0",
        "id": 0,
        "method": "bevy/mutate_component",
        "params": {
            "entity": 4294967308,
            "component": "bevy_transform::components::transform::Transform",
            "path": ".translation.x",
            "value": 10.0
        }
    }
    ```
    
    Clip of my Emacs BRP package using this method:
    
    
    https://github.com/user-attachments/assets/a786b245-5c20-4189-859f-2261c5086a68
    
    ---------
    
    Co-authored-by: François Mockers <mockersf@gmail.com>

--- crates/bevy_remote/src/lib.rs
+++ crates/bevy_remote/src/lib.rs
@@ -194,6 +194,19 @@
 //!
 //! `result`: null.
 //!
+//! ### `bevy/mutate_component`
+//!
+//! Mutate a field in a component.
+//!
+//! `params`:
+//! - `entity`: The ID of the entity to with the component to mutate.
+//! - `component`: The component's [fully-qualified type name].
+//! - `path`: The path of the field within the component. See
+//!   [`GetPath`](bevy_reflect::GetPath#syntax) for more information on formatting this string.
+//! - `value`: The value to insert at `path`.
+//!
+//! `result`: null.
+//!
 //! ### bevy/reparent
 //!
 //! Assign a new parent to one or more entities.
@@ -418,6 +431,10 @@ impl Default for RemotePlugin {
             .with_method(
                 builtin_methods::BRP_REGISTRY_SCHEMA_METHOD,
                 builtin_methods::export_registry_types,
+            )
+            .with_method(
+                builtin_methods::BRP_MUTATE_COMPONENT_METHOD,
+                builtin_methods::process_remote_mutate_component_request,
             )
             .with_watching_method(
                 builtin_methods::BRP_GET_AND_WATCH_METHOD,
