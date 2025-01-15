e0109ae485376c52f51ea91c21c7701df568b250
JMS55 <47158642+JMS55@users.noreply.github.com>
2025-01-14 17:39:31 -0800

    Remove envmap

--- examples/3d/decal.rs
+++ examples/3d/decal.rs
@@ -46,12 +46,6 @@ fn setup(
         Camera3d::default(),
         CameraController::default(),
         DepthPrepass, // Must enable the depth prepass to render forward decals
-        EnvironmentMapLight {
-            diffuse_map: asset_server.load("environment_maps/pisa_diffuse_rgb9e5_zstd.ktx2"),
-            specular_map: asset_server.load("environment_maps/pisa_specular_rgb9e5_zstd.ktx2"),
-            intensity: 2000.0,
-            ..default()
-        },
         Transform::from_xyz(2.0, 9.5, 2.5).looking_at(Vec3::ZERO, Vec3::Y),
     ));
 
