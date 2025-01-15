f3aedbc6cad8ddeb7b2f8b574b8a8aed6d8082d5
Christian Hughes <xdotdash@gmail.com>
2025-01-14 21:43:48 -0600

    usage notes for IntoX traits

--- crates/bevy_ecs/src/system/observer_system.rs
+++ crates/bevy_ecs/src/system/observer_system.rs
@@ -21,6 +21,12 @@ impl<
 }
 
 /// Implemented for systems that convert into [`ObserverSystem`].
+///
+/// # Usage notes
+///
+/// This trait should only be used as a bound for trait implementations or as an
+/// argument to a function. If an observer system needs to be returned from a
+/// function or stored somewhere, use [`ObserverSystem`] instead of this trait.
 #[diagnostic::on_unimplemented(
     message = "`{Self}` cannot become an `ObserverSystem`",
     label = "the trait `IntoObserverSystem` is not implemented",
