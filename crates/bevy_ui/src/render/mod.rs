11ed90b2c0e0415d037cd011b22b0d39f0b23a6a
ickshonpe <david.curthoys@googlemail.com>
2025-01-16 13:42:13 +0000

    renamed `view_visibility` variables to `inherited_visibility`

--- crates/bevy_ui/src/render/mod.rs
+++ crates/bevy_ui/src/render/mod.rs
@@ -294,7 +294,7 @@ pub fn extract_uinode_background_colors(
     mapping: Extract<Query<RenderEntity>>,
 ) {
     let default_camera_entity = default_ui_camera.get();
-    for (entity, uinode, transform, view_visibility, clip, camera, background_color) in
+    for (entity, uinode, transform, inherited_visibility, clip, camera, background_color) in
         &uinode_query
     {
         let Some(camera_entity) = camera.map(TargetCamera::entity).or(default_camera_entity) else {
@@ -306,7 +306,7 @@ pub fn extract_uinode_background_colors(
         };
 
         // Skip invisible backgrounds
-        if !view_visibility.get() || background_color.0.is_fully_transparent() {
+        if !inherited_visibility.get() || background_color.0.is_fully_transparent() {
             continue;
         }
 
@@ -356,7 +356,7 @@ pub fn extract_uinode_images(
     mapping: Extract<Query<RenderEntity>>,
 ) {
     let default_camera_entity = default_ui_camera.get();
-    for (entity, uinode, transform, view_visibility, clip, camera, image) in &uinode_query {
+    for (entity, uinode, transform, inherited_visibility, clip, camera, image) in &uinode_query {
         let Some(camera_entity) = camera.map(TargetCamera::entity).or(default_camera_entity) else {
             continue;
         };
@@ -366,7 +366,7 @@ pub fn extract_uinode_images(
         };
 
         // Skip invisible images
-        if !view_visibility.get()
+        if !inherited_visibility.get()
             || image.color.is_fully_transparent()
             || image.image.id() == TRANSPARENT_IMAGE_HANDLE.id()
             || image.image_mode.uses_slices()
@@ -452,7 +452,7 @@ pub fn extract_uinode_borders(
         node,
         computed_node,
         global_transform,
-        view_visibility,
+        inherited_visibility,
         maybe_clip,
         maybe_camera,
         (maybe_border_color, maybe_outline),
@@ -470,7 +470,7 @@ pub fn extract_uinode_borders(
         };
 
         // Skip invisible borders and removed nodes
-        if !view_visibility.get() || node.display == Display::None {
+        if !inherited_visibility.get() || node.display == Display::None {
             continue;
         }
 
@@ -694,7 +694,7 @@ pub fn extract_text_sections(
         entity,
         uinode,
         global_transform,
-        view_visibility,
+        inherited_visibility,
         clip,
         camera,
         computed_block,
@@ -706,7 +706,7 @@ pub fn extract_text_sections(
         };
 
         // Skip if not visible or if size is set to zero (e.g. when a parent is set to `Display::None`)
-        if !view_visibility.get() || uinode.is_empty() {
+        if !inherited_visibility.get() || uinode.is_empty() {
             continue;
         }
 
