bd971f6c03d4e3e2ff937e5041daafc7baee7978
Sigma-dev <antonin.duthil@epitech.eu>
2025-01-16 20:00:57 +0100

    fix segment3d render primitive

--- examples/math/render_primitives.rs
+++ examples/math/render_primitives.rs
@@ -192,8 +192,8 @@ const SEGMENT_2D: Segment2d = Segment2d {
 };
 
 const SEGMENT_3D: Segment3d = Segment3d {
-    direction: Dir3::X,
-    half_length: BIG_3D,
+    point1: Vec3::new(-BIG_3D / 2., 0., 0.),
+    point2: Vec3::new(BIG_3D / 2., 0., 0.),
 };
 
 const POLYLINE_2D: Polyline2d<4> = Polyline2d {
