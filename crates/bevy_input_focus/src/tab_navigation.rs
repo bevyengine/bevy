6e00d7b50a193031e53f2dced8bc1d2f7e7e1063
Carter Anderson <mcanders1@gmail.com>
2025-01-20 13:41:11 -0800

    fmt

--- crates/bevy_input_focus/src/tab_navigation.rs
+++ crates/bevy_input_focus/src/tab_navigation.rs
@@ -29,7 +29,7 @@ use bevy_ecs::prelude::ReflectComponent;
 use bevy_ecs::{
     component::Component,
     entity::Entity,
-    hierarchy::{Children, ChildOf},
+    hierarchy::{ChildOf, Children},
     observer::Trigger,
     query::{With, Without},
     system::{Commands, Query, Res, ResMut, SystemParam},
