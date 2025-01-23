41e79ae826e1af2c510da442211386da05dcb920
Zachary Harrold <zac@harrold.com.au>
2025-01-23 13:45:24 +1100

    Refactored `ComponentHook` Parameters into `HookContext` (#17503)
    # Objective
    
    - Make the function signature for `ComponentHook` less verbose
    
    ## Solution
    
    - Refactored `Entity`, `ComponentId`, and `Option<&Location>` into a new
    `HookContext` struct.
    
    ## Testing
    
    - CI
    
    ---
    
    ## Migration Guide
    
    Update the function signatures for your component hooks to only take 2
    arguments, `world` and `context`. Note that because `HookContext` is
    plain data with all members public, you can use de-structuring to
    simplify migration.
    
    ```rust
    // Before
    fn my_hook(
        mut world: DeferredWorld,
        entity: Entity,
        component_id: ComponentId,
    ) { ... }
    
    // After
    fn my_hook(
        mut world: DeferredWorld,
        HookContext { entity, component_id, caller }: HookContext,
    ) { ... }
    ``` 
    
    Likewise, if you were discarding certain parameters, you can use `..` in
    the de-structuring:
    
    ```rust
    // Before
    fn my_hook(
        mut world: DeferredWorld,
        entity: Entity,
        _: ComponentId,
    ) { ... }
    
    // After
    fn my_hook(
        mut world: DeferredWorld,
        HookContext { entity, .. }: HookContext,
    ) { ... }
    ```

--- crates/bevy_ecs/src/bundle.rs
+++ crates/bevy_ecs/src/bundle.rs
@@ -1704,9 +1704,8 @@ fn sorted_remove<T: Eq + Ord + Copy>(source: &mut Vec<T>, remove: &[T]) {
 #[cfg(test)]
 mod tests {
     use crate as bevy_ecs;
-    use crate::{component::ComponentId, prelude::*, world::DeferredWorld};
+    use crate::{component::HookContext, prelude::*, world::DeferredWorld};
     use alloc::vec;
-    use core::panic::Location;
 
     #[derive(Component)]
     struct A;
@@ -1715,39 +1714,19 @@ mod tests {
     #[component(on_add = a_on_add, on_insert = a_on_insert, on_replace = a_on_replace, on_remove = a_on_remove)]
     struct AMacroHooks;
 
-    fn a_on_add(
-        mut world: DeferredWorld,
-        _: Entity,
-        _: ComponentId,
-        _: Option<&'static Location<'static>>,
-    ) {
+    fn a_on_add(mut world: DeferredWorld, _: HookContext) {
         world.resource_mut::<R>().assert_order(0);
     }
 
-    fn a_on_insert<T1, T2>(
-        mut world: DeferredWorld,
-        _: T1,
-        _: T2,
-        _: Option<&'static Location<'static>>,
-    ) {
+    fn a_on_insert(mut world: DeferredWorld, _: HookContext) {
         world.resource_mut::<R>().assert_order(1);
     }
 
-    fn a_on_replace<T1, T2>(
-        mut world: DeferredWorld,
-        _: T1,
-        _: T2,
-        _: Option<&'static Location<'static>>,
-    ) {
+    fn a_on_replace(mut world: DeferredWorld, _: HookContext) {
         world.resource_mut::<R>().assert_order(2);
     }
 
-    fn a_on_remove<T1, T2>(
-        mut world: DeferredWorld,
-        _: T1,
-        _: T2,
-        _: Option<&'static Location<'static>>,
-    ) {
+    fn a_on_remove(mut world: DeferredWorld, _: HookContext) {
         world.resource_mut::<R>().assert_order(3);
     }
 
@@ -1780,10 +1759,10 @@ mod tests {
         world.init_resource::<R>();
         world
             .register_component_hooks::<A>()
-            .on_add(|mut world, _, _, _| world.resource_mut::<R>().assert_order(0))
-            .on_insert(|mut world, _, _, _| world.resource_mut::<R>().assert_order(1))
-            .on_replace(|mut world, _, _, _| world.resource_mut::<R>().assert_order(2))
-            .on_remove(|mut world, _, _, _| world.resource_mut::<R>().assert_order(3));
+            .on_add(|mut world, _| world.resource_mut::<R>().assert_order(0))
+            .on_insert(|mut world, _| world.resource_mut::<R>().assert_order(1))
+            .on_replace(|mut world, _| world.resource_mut::<R>().assert_order(2))
+            .on_remove(|mut world, _| world.resource_mut::<R>().assert_order(3));
 
         let entity = world.spawn(A).id();
         world.despawn(entity);
@@ -1807,10 +1786,10 @@ mod tests {
         world.init_resource::<R>();
         world
             .register_component_hooks::<A>()
-            .on_add(|mut world, _, _, _| world.resource_mut::<R>().assert_order(0))
-            .on_insert(|mut world, _, _, _| world.resource_mut::<R>().assert_order(1))
-            .on_replace(|mut world, _, _, _| world.resource_mut::<R>().assert_order(2))
-            .on_remove(|mut world, _, _, _| world.resource_mut::<R>().assert_order(3));
+            .on_add(|mut world, _| world.resource_mut::<R>().assert_order(0))
+            .on_insert(|mut world, _| world.resource_mut::<R>().assert_order(1))
+            .on_replace(|mut world, _| world.resource_mut::<R>().assert_order(2))
+            .on_remove(|mut world, _| world.resource_mut::<R>().assert_order(3));
 
         let mut entity = world.spawn_empty();
         entity.insert(A);
@@ -1824,8 +1803,8 @@ mod tests {
         let mut world = World::new();
         world
             .register_component_hooks::<A>()
-            .on_replace(|mut world, _, _, _| world.resource_mut::<R>().assert_order(0))
-            .on_insert(|mut world, _, _, _| {
+            .on_replace(|mut world, _| world.resource_mut::<R>().assert_order(0))
+            .on_insert(|mut world, _| {
                 if let Some(mut r) = world.get_resource_mut::<R>() {
                     r.assert_order(1);
                 }
@@ -1846,22 +1825,22 @@ mod tests {
         world.init_resource::<R>();
         world
             .register_component_hooks::<A>()
-            .on_add(|mut world, entity, _, _| {
+            .on_add(|mut world, context| {
                 world.resource_mut::<R>().assert_order(0);
-                world.commands().entity(entity).insert(B);
+                world.commands().entity(context.entity).insert(B);
             })
-            .on_remove(|mut world, entity, _, _| {
+            .on_remove(|mut world, context| {
                 world.resource_mut::<R>().assert_order(2);
-                world.commands().entity(entity).remove::<B>();
+                world.commands().entity(context.entity).remove::<B>();
             });
 
         world
             .register_component_hooks::<B>()
-            .on_add(|mut world, entity, _, _| {
+            .on_add(|mut world, context| {
                 world.resource_mut::<R>().assert_order(1);
-                world.commands().entity(entity).remove::<A>();
+                world.commands().entity(context.entity).remove::<A>();
             })
-            .on_remove(|mut world, _, _, _| {
+            .on_remove(|mut world, _| {
                 world.resource_mut::<R>().assert_order(3);
             });
 
@@ -1878,27 +1857,27 @@ mod tests {
         world.init_resource::<R>();
         world
             .register_component_hooks::<A>()
-            .on_add(|mut world, entity, _, _| {
+            .on_add(|mut world, context| {
                 world.resource_mut::<R>().assert_order(0);
-                world.commands().entity(entity).insert(B).insert(C);
+                world.commands().entity(context.entity).insert(B).insert(C);
             });
 
         world
             .register_component_hooks::<B>()
-            .on_add(|mut world, entity, _, _| {
+            .on_add(|mut world, context| {
                 world.resource_mut::<R>().assert_order(1);
-                world.commands().entity(entity).insert(D);
+                world.commands().entity(context.entity).insert(D);
             });
 
         world
             .register_component_hooks::<C>()
-            .on_add(|mut world, _, _, _| {
+            .on_add(|mut world, _| {
                 world.resource_mut::<R>().assert_order(3);
             });
 
         world
             .register_component_hooks::<D>()
-            .on_add(|mut world, _, _, _| {
+            .on_add(|mut world, _| {
                 world.resource_mut::<R>().assert_order(2);
             });
 
