b317a7252d92c9893f158d910fecba69563a7c4d
mgi388 <135186256+mgi388@users.noreply.github.com>
2025-01-27 07:23:18 +1100

    tweak docs

--- crates/bevy_winit/src/custom_cursor.rs
+++ crates/bevy_winit/src/custom_cursor.rs
@@ -18,15 +18,13 @@ pub struct CustomCursorImage {
     pub texture_atlas: Option<TextureAtlas>,
     /// Whether the image should be flipped along its x-axis.
     ///
-    /// If true, the cursor's hotspot will be automatically flipped along with
-    /// the image. You don't need to adjust the `hotspot` field to account for
-    /// the flip.
+    /// If true, the cursor's `hotspot` automatically flips along with the
+    /// image.
     pub flip_x: bool,
     /// Whether the image should be flipped along its y-axis.
     ///
-    /// If true, the cursor's hotspot will be automatically flipped along with
-    /// the image. You don't need to adjust the `hotspot` field to account for
-    /// the flip.
+    /// If true, the cursor's `hotspot` automatically flips along with the
+    /// image.
     pub flip_y: bool,
     /// An optional rectangle representing the region of the image to render,
     /// instead of rendering the full image. This is an easy one-off alternative
@@ -37,6 +35,10 @@ pub struct CustomCursorImage {
     pub rect: Option<URect>,
     /// X and Y coordinates of the hotspot in pixels. The hotspot must be within
     /// the image bounds.
+    ///
+    /// If you are flipping the image using `flip_x` or `flip_y`, you don't need
+    /// to adjust this field to account for the flip because it is adjusted
+    /// automatically.
     pub hotspot: (u16, u16),
 }
 
