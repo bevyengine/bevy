628076241a0247f0908c3b374ab17748552c6f7e
Rich Churcher <rich.churcher@gmail.com>
2025-01-29 08:53:18 +1300

    Update crates/bevy_transform/src/components/global_transform.rs
    Co-authored-by: Benjamin Brienen <benjamin.brienen@outlook.com>

--- crates/bevy_transform/src/components/global_transform.rs
+++ crates/bevy_transform/src/components/global_transform.rs
@@ -16,7 +16,7 @@ use bevy_reflect::{ReflectDeserialize, ReflectSerialize};
 ///
 /// * To place or move an entity, you should set its [`Transform`].
 /// * To get the absolute transform of an entity, you should get its [`GlobalTransform`]
-///   after [`TransformPropagate`] or [`PostUpdate`] to avoid a 1 frame lag.
+///   after [`TransformPropagate`] or [`PostUpdate`] to avoid being 1 frame out of date.
 ///   * You can use [`compute_transform`] to get the global `translation`, `rotation` and `scale`.
 /// * [`GlobalTransform`] is fully managed by bevy and should not be mutated directly.
 ///   Use [`Transform`] instead, and the [`GlobalTransform`] will be automatically updated with [`TransformPropagate`].
