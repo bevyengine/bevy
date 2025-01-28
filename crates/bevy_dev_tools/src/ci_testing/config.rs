5cde88dc6b1682003c0a34a5e5d5564dc17691f3
François Mockers <francois.mockers@vleue.com>
2025-01-28 13:51:41 +0100

    can run with missing or incomplete config file

--- crates/bevy_dev_tools/src/ci_testing/config.rs
+++ crates/bevy_dev_tools/src/ci_testing/config.rs
@@ -6,7 +6,7 @@ use serde::Deserialize;
 /// It gets used when the `bevy_ci_testing` feature is enabled to automatically
 /// exit a Bevy app when run through the CI. This is needed because otherwise
 /// Bevy apps would be stuck in the game loop and wouldn't allow the CI to progress.
-#[derive(Deserialize, Resource, PartialEq, Debug)]
+#[derive(Deserialize, Resource, PartialEq, Debug, Default)]
 pub struct CiTestingConfig {
     /// The setup for this test.
     #[serde(default)]
