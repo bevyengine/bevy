b18de4879f8abded4de49d9ae7ab98b0045b080d
Zac Harrold <zac@harrold.com.au>
2025-01-30 19:10:37 +1100

    Added benchmarks for iterating the index and updating it.

---
+++ benches/benches/bevy_ecs/index/index_iter_indexed.rs
@@ -0,0 +1,38 @@
+use bevy_ecs::{prelude::*, system::SystemId};
+use criterion::black_box;
+use glam::*;
+
+const PLANETS: u8 = 16;
+const SPAWNS: usize = 10_000;
+
+#[derive(Component, Copy, Clone, PartialEq, Eq, Hash)]
+#[component(immutable)]
+struct Planet(u8);
+
+fn find_planet_zeroes_indexed(mut query: QueryByIndex<Planet, &Planet>) {
+    let query = query.at(&Planet(0));
+    for planet in query.iter() {
+        let _ = black_box(planet);
+    }
+}
+
+pub struct Benchmark(World, SystemId);
+
+impl Benchmark {
+    pub fn new() -> Self {
+        let mut world = World::new();
+
+        world.add_index::<Planet>();
+
+        world.spawn_batch((0..PLANETS).map(Planet).cycle().take(SPAWNS));
+
+        let id = world.register_system(find_planet_zeroes_indexed);
+
+        Self(world, id)
+    }
+
+    #[inline(never)]
+    pub fn run(&mut self) {
+        let _ = self.0.run_system(self.1);
+    }
+}
---
+++ benches/benches/bevy_ecs/index/index_iter_naive.rs
@@ -0,0 +1,35 @@
+use bevy_ecs::{prelude::*, system::SystemId};
+use criterion::black_box;
+use glam::*;
+
+const PLANETS: u8 = 16;
+const SPAWNS: usize = 10_000;
+
+#[derive(Component, Copy, Clone, PartialEq, Eq, Hash)]
+#[component(immutable)]
+struct Planet(u8);
+
+fn find_planet_zeroes_naive(query: Query<&Planet>) {
+    for planet in query.iter().filter(|&&planet| planet == Planet(0)) {
+        let _ = black_box(planet);
+    }
+}
+
+pub struct Benchmark(World, SystemId);
+
+impl Benchmark {
+    pub fn new() -> Self {
+        let mut world = World::new();
+
+        world.spawn_batch((0..PLANETS).map(Planet).cycle().take(SPAWNS));
+
+        let id = world.register_system(find_planet_zeroes_naive);
+
+        Self(world, id)
+    }
+
+    #[inline(never)]
+    pub fn run(&mut self) {
+        let _ = self.0.run_system(self.1);
+    }
+}
