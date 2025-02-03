b978b13a7b36b3544ab93e81270fd15f2cc8f473
Erick Z <567737+eckz@users.noreply.github.com>
2025-02-03 22:53:51 +0100

    Implementing Reflect on *MeshBuilder types (#17600)
    # Objective
    
    - Most of the `*MeshBuilder` classes are not implementing `Reflect`
    
    ## Solution
    
    - Implementing `Reflect` for all `*MeshBuilder` were is possible.
    - Make sure all `*MeshBuilder` implements `Default`.
    - Adding new `MeshBuildersPlugin` that registers all `*MeshBuilder`
    types.
    
    ## Testing
    
    - `cargo run -p ci`
    - Tested some examples like `3d_scene` just in case something was
    broken.

--- crates/bevy_mesh/src/primitives/dim3/sphere.rs
+++ crates/bevy_mesh/src/primitives/dim3/sphere.rs
@@ -1,6 +1,7 @@
 use crate::{Indices, Mesh, MeshBuilder, Meshable, PrimitiveTopology};
 use bevy_asset::RenderAssetUsages;
 use bevy_math::{ops, primitives::Sphere};
+use bevy_reflect::prelude::*;
 use core::f32::consts::PI;
 use hexasphere::shapes::IcoSphere;
 use thiserror::Error;
@@ -19,7 +20,8 @@ pub enum IcosphereError {
 }
 
 /// A type of sphere mesh.
-#[derive(Clone, Copy, Debug)]
+#[derive(Clone, Copy, Debug, Reflect)]
+#[reflect(Default, Debug)]
 pub enum SphereKind {
     /// An icosphere, a spherical mesh that consists of similar sized triangles.
     Ico {
@@ -46,7 +48,8 @@ impl Default for SphereKind {
 }
 
 /// A builder used for creating a [`Mesh`] with an [`Sphere`] shape.
-#[derive(Clone, Copy, Debug, Default)]
+#[derive(Clone, Copy, Debug, Default, Reflect)]
+#[reflect(Default, Debug)]
 pub struct SphereMeshBuilder {
     /// The [`Sphere`] shape.
     pub sphere: Sphere,
