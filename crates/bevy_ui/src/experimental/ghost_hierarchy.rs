5bb06c1d756def55c720864c4dffdb7d8b7b4c26
ickshonpe <david.curthoys@googlemail.com>
2025-02-08 17:56:31 +0000

    fixed GhostNode requirements

--- crates/bevy_ui/src/experimental/ghost_hierarchy.rs
+++ crates/bevy_ui/src/experimental/ghost_hierarchy.rs
@@ -21,13 +21,7 @@ use smallvec::SmallVec;
 #[derive(Component, Debug, Copy, Clone, Reflect)]
 #[cfg_attr(feature = "ghost_nodes", derive(Default))]
 #[reflect(Component, Debug)]
-#[require(
-    Visibility,
-    Transform,
-    ComputedNodeTarge,
-    ComputedNodeTarge,
-    ComputedNodeTargetCamera
-)]
+#[require(Visibility, Transform, ComputedNodeTargetCamera)]
 pub struct GhostNode;
 
 #[cfg(feature = "ghost_nodes")]
