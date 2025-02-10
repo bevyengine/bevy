92099e75c2f2b507f72c61d5f55a56b688e77b35
John Skovbekk <jmcjack@gmail.com>
2025-02-10 13:26:26 -0600

    Add message to thread count assertion

--- crates/bevy_app/src/task_pool_plugin.rs
+++ crates/bevy_app/src/task_pool_plugin.rs
@@ -159,7 +159,7 @@ impl Default for TaskPoolOptions {
 impl TaskPoolOptions {
     /// Create a configuration that forces using the given number of threads.
     pub fn with_num_threads(thread_count: usize) -> Self {
-        assert!(thread_count > 2);
+        assert!(thread_count > 2, "Default task pool policies requires at least 3 threads");
         TaskPoolOptions {
             min_total_threads: thread_count,
             max_total_threads: thread_count,
