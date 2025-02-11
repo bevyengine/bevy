c6509eec3c2920a17c61ac9df80e13500a7cf1d1
ickshonpe <david.curthoys@googlemail.com>
2025-02-11 17:56:22 +0000

    Split the row and column in many buttons into two text sections with different colours.

--- examples/stress_tests/many_buttons.rs
+++ examples/stress_tests/many_buttons.rs
@@ -289,14 +289,23 @@ fn spawn_button(
 
     if spawn_text {
         builder.with_children(|parent| {
-            parent.spawn((
-                Text(format!("{column}, {row}")),
-                TextFont {
-                    font_size: FONT_SIZE,
-                    ..default()
-                },
-                TextColor(Color::srgb(0.2, 0.2, 0.2)),
-            ));
+            parent
+                .spawn((
+                    Text(format!("{column}, ")),
+                    TextFont {
+                        font_size: FONT_SIZE,
+                        ..default()
+                    },
+                    TextColor(Color::srgb(0.5, 0.2, 0.2)),
+                ))
+                .with_child((
+                    TextSpan(format!("{row}")),
+                    TextFont {
+                        font_size: FONT_SIZE,
+                        ..default()
+                    },
+                    TextColor(Color::srgb(0.2, 0.2, 0.5)),
+                ));
         });
     }
 }
