6847fb3ec24f2db99fcd2483b2c0d0704bb3d24a
John Skovbekk <jmcjack@gmail.com>
2025-02-11 22:59:59 -0600

    Change assertion to a warning

--- crates/bevy_app/src/task_pool_plugin.rs
+++ crates/bevy_app/src/task_pool_plugin.rs
@@ -12,7 +12,7 @@ use alloc::string::ToString;
 use bevy_platform_support::sync::Arc;
 use bevy_tasks::{AsyncComputeTaskPool, ComputeTaskPool, IoTaskPool, TaskPoolBuilder};
 use core::{fmt::Debug, marker::PhantomData};
-use log::trace;
+use log::{trace, warn};
 
 #[cfg(not(target_arch = "wasm32"))]
 use {crate::Last, bevy_ecs::prelude::NonSend};
@@ -159,14 +159,13 @@ impl Default for TaskPoolOptions {
 impl TaskPoolOptions {
     /// Create a configuration that forces using the given number of threads.
     ///
-    /// # Panics
-    ///
-    /// Values below 3 are not supported by Bevy (IO, Async and Compute pools each need at least one thread), and will panic.
+    /// Warns for values below 3 as three minimum are needed for the default pool assignments (IO, Async and Compute pools each need at least one thread).
     pub fn with_num_threads(thread_count: usize) -> Self {
-        assert!(
-            thread_count > 2,
-            "The default TaskPoolOptions thread assignment policies require a minimum of one thread each for the IO, Async, and Compute pools. Please specify a thread_count of at least 3."
-        );
+        if thread_count < 3 {
+            warn!(
+                "The default TaskPoolOptions thread assignment policies require a minimum of one thread each for the IO, Async, and Compute pools. Three threads will be assigned."
+            );
+        }
         TaskPoolOptions {
             min_total_threads: thread_count,
             max_total_threads: thread_count,
