36515a471bdd7d13e4c29bd032d3885c4f837c76
SpecificProtagonist <specificprotagonist@posteo.org>
2025-02-15 09:50:38 +0100

    make easier to run

--- tools/rustdoc-wrapper/rustdoc.sh
+++ tools/rustdoc-wrapper/rustdoc.sh
@@ -2,6 +2,7 @@
 
 # Gets clobbered, so make a backup.
 export SET_CARGO_MANIFEST_PATH="$CARGO_MANIFEST_PATH"
+export SET_CARGO_MANIFEST_DIR="$CARGO_MANIFEST_DIR"
 # Pass on all arguments to our rustdoc wrapper.
 # Use a different target directory because the workspace-level one
 # will be locked.
