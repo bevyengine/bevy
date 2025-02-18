1405016972ddf4f4b8036a6f6f8533a5c49ada4d
Patrick Walton <pcwalton@mimiga.net>
2025-02-18 01:01:04 -0800

    Make the `MaterialBindGroupAllocator` public

--- crates/bevy_pbr/src/lib.rs
+++ crates/bevy_pbr/src/lib.rs
@@ -59,6 +59,7 @@ pub use light::*;
 pub use light_probe::*;
 pub use lightmap::*;
 pub use material::*;
+pub use material_bind_groups::*;
 pub use mesh_material::*;
 pub use parallax::*;
 pub use pbr_material::*;
