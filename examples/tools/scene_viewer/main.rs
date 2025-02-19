c0a5cffbecfb9c456b9745473456c831c976c300
Patrick Walton <pcwalton@mimiga.net>
2025-02-19 09:11:29 -0800

    Implement occlusion culling for directional light shadow maps.
    Two-phase occlusion culling can be helpful for shadow maps just as it
    can for a prepass, in order to reduce vertex and alpha mask fragment
    shading overhead. This patch implements occlusion culling for shadow
    maps from directional lights, when the `OcclusionCulling` component is
    present on the entities containing the lights. Shadow maps from point
    lights are deferred to a follow-up patch. Much of this patch involves
    expanding the hierarchical Z-buffer to cover shadow maps in addition to
    standard view depth buffers.
    
    The `scene_viewer` example has been updated to add `OcclusionCulling` to
    the directional light.
    
    This improved the performance of the rend3 sci-fi test scene when
    enabling shadows.

--- examples/tools/scene_viewer/main.rs
+++ examples/tools/scene_viewer/main.rs
@@ -209,10 +209,13 @@ fn setup_scene_after_load(
         // Spawn a default light if the scene does not have one
         if !scene_handle.has_light || args.add_light == Some(true) {
             info!("Spawning a directional light");
-            commands.spawn((
+            let mut light = commands.spawn((
                 DirectionalLight::default(),
                 Transform::from_xyz(1.0, 1.0, 0.0).looking_at(Vec3::ZERO, Vec3::Y),
             ));
+            if args.occlusion_culling == Some(true) {
+                light.insert(OcclusionCulling);
+            }
 
             scene_handle.has_light = true;
         }
