0abd439771d255898844d110ff8f1543c20d21eb
Aaron Loucks <aloucks@cofront.net>
2025-03-02 10:21:49 -0500

    Check for both invisible and headless in about_to_wait on Windows

--- crates/bevy_winit/src/state.rs
+++ crates/bevy_winit/src/state.rs
@@ -491,7 +491,11 @@ impl<T: Event> ApplicationHandler<T> for WinitAppRunnerState<T> {
         {
             let winit_windows = self.world().non_send_resource::<WinitWindows>();
             let headless = winit_windows.windows.is_empty();
-            if self.startup_forced_updates > 0 || headless {
+            let all_invisible = winit_windows
+                .windows
+                .iter()
+                .all(|(_, w)| !w.is_visible().unwrap_or(false));
+            if self.startup_forced_updates > 0 || headless || all_invisible {
                 self.redraw_requested(event_loop);
             }
         }
