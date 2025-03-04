338aa26e1bc2db4fa2fdf5d2ba9e405fc3d206fc
andriyDev <andriydzikh@gmail.com>
2025-03-03 19:19:39 -0800

    Rename `arc_asset` example to `access_asset_from_async`.

--- examples/asset/access_asset_from_async.rs
+++ examples/asset/access_asset_from_async.rs
@@ -61,7 +61,7 @@ fn setup(
 
     // Create the parameters for mesh generation.
     commands.insert_resource(MeshGeneration {
-        height_interpolation: asset_server.load("arc_asset_heights.li.ron"),
+        height_interpolation: asset_server.load("access_asset_from_async_heights.li.ron"),
         mesh,
         size: UVec2::new(30, 30),
     });
