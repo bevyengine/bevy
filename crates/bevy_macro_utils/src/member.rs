b6a8eb68d422e19538c368a278ccc7c1549c9fb2
Tim Overbeek <oorbecktim@gmail.com>
2025-03-08 20:40:06 +0100

    docs

--- crates/bevy_macro_utils/src/member.rs
+++ crates/bevy_macro_utils/src/member.rs
@@ -2,12 +2,13 @@ use syn::{Ident, Member};
 
 /// Converts an optional identifier or index into a [`syn::Member`] variant.
 ///
-/// This is useful for when u want to acces a field inside a `quote!` block regardless of whether it is an identifier or an index.
-/// There is also [`syn::Field::members`], but when u are working with single fields this method won't work.
-///
+/// This is useful for when you want to access a field inside a `quote!` block regardless of whether it is an identifier or an index.
+/// There is also [syn::Field::members], but this method doesn't work when you're dealing with single / filtered fields.
+/// 
 /// Rust struct syntax allows for `Struct { foo: "string" }` with explicitly
 /// named fields. It allows the `Struct { 0: "string" }` syntax when the struct
 /// is declared as a tuple struct.
+/// 
 /// # Example
 /// ```rust
 /// use syn::{Ident, parse_str, DeriveInput, Data, DataStruct};
@@ -31,7 +32,7 @@ use syn::{Ident, Member};
 ///     .iter()
 ///     .enumerate()
 ///     .filter(|(_, field)| field.attrs.iter().any(|attr| attr.path().is_ident("my_derive")))
-///     .map(|(i, field)| { as_member(&field.ident, i) });
+///     .map(|(i, field)| { as_member(field.ident.as_ref(), i) });
 ///
 /// // it won't matter now if it's a named field or a unnamed field. e.g self.field or self.0
 /// quote!(
