7205b3530cadc0d776cd4a6d373bd85c1e431898
Tim Overbeek <oorbecktim@gmail.com>
2025-03-08 20:07:36 +0100

    option::Some

--- crates/bevy_ecs/macros/src/component.rs
+++ crates/bevy_ecs/macros/src/component.rs
@@ -654,7 +654,7 @@ fn hook_register_function_call(
     function.map(|meta| {
         quote! {
             fn #hook() -> #FQOption<#bevy_ecs_path::component::ComponentHook> {
-                #FQOption(#meta)
+                #FQOption::Some(#meta)
             }
         }
     })
