701afdbf19c8412ec557fd71236e7c6bae0c59f7
Lucas Farias <lucasfnf@gmail.com>
2025-03-12 07:32:36 -0300

    Propagate parents' transforms to attachments

--- crates/bevy_bone_attachments/src/lib.rs
+++ crates/bevy_bone_attachments/src/lib.rs
@@ -8,7 +8,12 @@ pub mod scene;
 
 extern crate alloc;
 
-use bevy_app::Plugin;
+use alloc::vec::Vec;
+use bevy_app::{Plugin, PostUpdate};
+use bevy_ecs::{
+    entity::Entity, relationship::RelationshipTarget, schedule::IntoSystemConfigs, system::Query,
+};
+use bevy_transform::{components::Transform, TransformSystem};
 
 /// Most frequently used objects of [`bevy_bone_attachments`](self) for easy access
 pub mod prelude {
@@ -26,5 +31,33 @@ impl Plugin for BoneAttachmentsPlugin {
     fn build(&self, app: &mut bevy_app::App) {
         app.register_type::<relationship::AttachingModels>()
             .register_type::<relationship::AttachedTo>();
+
+        app.add_systems(
+            PostUpdate,
+            propagate_transform_to_attachments.after(TransformSystem::TransformPropagate),
+        );
+    }
+}
+
+fn propagate_transform_to_attachments(
+    parents: Query<(Entity, &relationship::AttachingModels)>,
+    mut transforms: Query<&mut Transform>,
+) {
+    let mut parents_without_transform = Vec::new();
+    let mut children_without_transform = Vec::new();
+    for (entity, children) in parents.iter() {
+        let Ok(parent_transform) = transforms.get(entity).cloned() else {
+            parents_without_transform.push(entity);
+            continue;
+        };
+
+        for child in children.iter() {
+            let Ok(mut transform) = transforms.get_mut(child) else {
+                children_without_transform.push(child);
+                continue;
+            };
+
+            *transform = parent_transform;
+        }
     }
 }
