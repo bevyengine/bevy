f40ad12562ab92a78bcf958d378216c8f1e3a77d
IceSentry <c.giguere42@gmail.com>
2025-03-15 01:16:01 -0400

    add readme and flags

--- crates/bevy_anti_aliasing/src/lib.rs
+++ crates/bevy_anti_aliasing/src/lib.rs
@@ -1,3 +1,11 @@
+#![expect(missing_docs, reason = "Not all docs are written yet, see #3492.")]
+#![forbid(unsafe_code)]
+#![cfg_attr(docsrs, feature(doc_auto_cfg))]
+#![doc(
+    html_logo_url = "https://bevyengine.org/assets/icon.png",
+    html_favicon_url = "https://bevyengine.org/assets/icon.png"
+)]
+
 use bevy_app::Plugin;
 use contrast_adaptive_sharpening::CasPlugin;
 use fxaa::FxaaPlugin;
