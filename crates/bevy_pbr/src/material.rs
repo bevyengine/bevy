8d5474a2f285d2abd3c54c6545a1a7abb12c8d2c
charlotte <charlotte.c.mcelwain@gmail.com>
2025-03-18 23:22:39 -0700

    Fix unecessary specialization checks for apps with many materials (#18410)
    # Objective
    
    For materials that aren't being used or a visible entity doesn't have an
    instance of, we were unnecessarily constantly checking whether they
    needed specialization, saying yes (because the material had never been
    specialized for that entity), and failing to look up the material
    instance.
    
    ## Solution
    
    If an entity doesn't have an instance of the material, it can't possibly
    need specialization, so exit early before spending time doing the check.
    
    Fixes #18388.

--- crates/bevy_pbr/src/material.rs
+++ crates/bevy_pbr/src/material.rs
@@ -863,6 +863,9 @@ pub fn specialize_material_meshes<M: Material>(
             .or_default();
 
         for (_, visible_entity) in visible_entities.iter::<Mesh3d>() {
+            let Some(material_asset_id) = render_material_instances.get(visible_entity) else {
+                continue;
+            };
             let entity_tick = entity_specialization_ticks.get(visible_entity).unwrap();
             let last_specialized_tick = view_specialized_material_pipeline_cache
                 .get(visible_entity)
@@ -874,9 +877,6 @@ pub fn specialize_material_meshes<M: Material>(
             if !needs_specialization {
                 continue;
             }
-            let Some(material_asset_id) = render_material_instances.get(visible_entity) else {
-                continue;
-            };
             let Some(mesh_instance) = render_mesh_instances.render_mesh_queue_data(*visible_entity)
             else {
                 continue;
