b953e9d483d9c574c552757322945d26b1ee3712
Aaron Loucks <aloucks@cofront.net>
2025-03-22 19:17:34 -0400

    Prevent reading RequestRedraw events before the world update
    They are now read after the world update.

--- crates/bevy_winit/src/state.rs
+++ crates/bevy_winit/src/state.rs
@@ -504,12 +504,6 @@ impl<T: BufferedEvent> WinitAppRunnerState<T> {
         let mut focused_windows_state: SystemState<(Res<WinitSettings>, Query<(Entity, &Window)>)> =
             SystemState::new(self.world_mut());
 
-        if let Some(app_redraw_events) = self.world().get_resource::<Events<RequestRedraw>>()
-            && redraw_event_reader.read(app_redraw_events).last().is_some()
-        {
-            self.redraw_requested = true;
-        }
-
         let (config, windows) = focused_windows_state.get(self.world());
         let focused = windows.iter().any(|(_, window)| window.focused);
 
