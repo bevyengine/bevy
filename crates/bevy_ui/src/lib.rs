f6578adf7bd955eab4ef4ae6c2f852188ae7e86f
darth levi <73083466+darthLeviN@users.noreply.github.com>
2025-03-24 03:50:15 +0400

    register ComputedNodeTarget (#18503)
    I had no reference to `ComputedNodeTarget` in my project. After updating
    to bevy 0.16.0-rc1 i got a compile error complaining about this.

--- crates/bevy_ui/src/lib.rs
+++ crates/bevy_ui/src/lib.rs
@@ -168,6 +168,7 @@ impl Plugin for UiPlugin {
             .register_type::<BoxShadowSamples>()
             .register_type::<UiAntiAlias>()
             .register_type::<TextShadow>()
+            .register_type::<ComputedNodeTarget>()
             .configure_sets(
                 PostUpdate,
                 (
