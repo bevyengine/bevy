d719aeb4c989f03b96935d1f5990a3ea97a53a4a
Pnoenix <Ph03n1x.V7@gamil.com>
2025-03-26 18:03:31 +0100

    Run cargo fmt --all

--- crates/bevy_mesh/src/mesh.rs
+++ crates/bevy_mesh/src/mesh.rs
@@ -839,7 +839,7 @@ impl Mesh {
         if self.primitive_topology != other.primitive_topology {
             return Err(MeshMergeError::IncompatiblePrimitiveTopology {
                 self_primitive_topology: self.primitive_topology,
-                other_primitive_topology: other.primitive_topology
+                other_primitive_topology: other.primitive_topology,
             });
         }
 
@@ -1404,16 +1404,20 @@ impl MeshDeserializer {
 /// Error that can occur when calling [`Mesh::merge`].
 #[derive(Error, Debug, Clone)]
 pub enum MeshMergeError {
-  #[error("Incompatible vertex attribute types {} and {}", self_attribute.name, other_attribute.map(|a| a.name).unwrap_or("None"))]
-  IncompatibleVertexAttributes {
-    self_attribute: MeshVertexAttribute,
-    other_attribute: Option<MeshVertexAttribute>,
-  },
-  #[error("Incompatible primitive topologies {:?} and {:?}", self_primitive_topology, other_primitive_topology)]
-  IncompatiblePrimitiveTopology {
-    self_primitive_topology: PrimitiveTopology,
-    other_primitive_topology: PrimitiveTopology,
-  }
+    #[error("Incompatible vertex attribute types {} and {}", self_attribute.name, other_attribute.map(|a| a.name).unwrap_or("None"))]
+    IncompatibleVertexAttributes {
+        self_attribute: MeshVertexAttribute,
+        other_attribute: Option<MeshVertexAttribute>,
+    },
+    #[error(
+        "Incompatible primitive topologies {:?} and {:?}",
+        self_primitive_topology,
+        other_primitive_topology
+    )]
+    IncompatiblePrimitiveTopology {
+        self_primitive_topology: PrimitiveTopology,
+        other_primitive_topology: PrimitiveTopology,
+    },
 }
 
 #[cfg(test)]
