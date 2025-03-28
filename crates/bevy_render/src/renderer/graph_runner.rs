b3c83465b31fd644308277e9bae7dfa5d0a86a81
JMS55 <47158642+JMS55@users.noreply.github.com>
2025-03-27 22:35:47 -0700

    Fix and improve tracy rendering spans (#18588)
    * `submit_graph_commands` was incorrectly timing the command buffer
    generation tasks as well, and not only the queue submission. Moved the
    span to fix that.
    * Added a new `command_buffer_generation_tasks` span as a parent for all
    the individual command buffer generation tasks that don't run as part of
    the Core3d span.
    
    
    ![image](https://github.com/user-attachments/assets/5a20c2f5-f1df-4c03-afbb-4865327aea33)

--- crates/bevy_render/src/renderer/graph_runner.rs
+++ crates/bevy_render/src/renderer/graph_runner.rs
@@ -87,10 +87,10 @@ impl RenderGraphRunner {
         finalizer(render_context.command_encoder());
 
         let (render_device, mut diagnostics_recorder) = {
+            let (commands, render_device, diagnostics_recorder) = render_context.finish();
+
             #[cfg(feature = "trace")]
             let _span = info_span!("submit_graph_commands").entered();
-
-            let (commands, render_device, diagnostics_recorder) = render_context.finish();
             queue.submit(commands);
 
             (render_device, diagnostics_recorder)
