5e1840fa00073c26f19f94a8e0f5d2cdbd41cd7f
Robin KAY <komadori@gekkou.co.uk>
2025-03-29 23:28:14 +0000

    Expose symbols needed to vendor SetMeshBindGroup.

--- crates/bevy_pbr/src/render/mod.rs
+++ crates/bevy_pbr/src/render/mod.rs
@@ -13,4 +13,5 @@ pub use light::*;
 pub use mesh::*;
 pub use mesh_bindings::MeshLayouts;
 pub use mesh_view_bindings::*;
+pub use morph::MorphIndices;
 pub use skin::{extract_skins, prepare_skins, SkinUniforms, MAX_JOINTS};
