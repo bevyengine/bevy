d3bae2cf64c7b1649c8fab48294b7d1d9d728596
mintlu8 <mintlux667@gmail.com>
2025-03-29 11:40:25 +0800

    Use lerp instead.

--- crates/bevy_color/src/interpolate.rs
+++ crates/bevy_color/src/interpolate.rs
@@ -1,24 +1,20 @@
-use bevy_math::StableInterpolate;
+use bevy_math::{StableInterpolate, VectorSpace};
 
 use crate::{Laba, LinearRgba, Oklaba, Srgba, Xyza};
 
 macro_rules! impl_stable_interpolate_linear {
-    ($name: ident $(, $field: ident)*) => {
-        impl StableInterpolate for $name {
+    ($($name: ident),*) => {
+        $(impl StableInterpolate for $name {
             fn interpolate_stable(&self, other: &Self, t: f32) -> Self {
-                $name {
-                    $($field: self.$field.interpolate_stable(&other.$field, t),)*
-                }
+                $name::lerp(*self, *other, t)
             }
-        }
+        })*
     };
 }
 
-impl_stable_interpolate_linear!(LinearRgba, red, green, blue, alpha);
-impl_stable_interpolate_linear!(Srgba, red, green, blue, alpha);
-impl_stable_interpolate_linear!(Xyza, x, y, z, alpha);
-impl_stable_interpolate_linear!(Laba, lightness, a, b, alpha);
-impl_stable_interpolate_linear!(Oklaba, lightness, a, b, alpha);
+impl_stable_interpolate_linear! {
+    LinearRgba, Srgba, Xyza, Laba, Oklaba
+}
 
 #[cfg(test)]
 mod test {
