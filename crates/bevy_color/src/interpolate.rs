580b7c6dfcc252c67f9bf0b5038b1ed3a2da05c2
mintlu8 <mintlux667@gmail.com>
2025-03-29 11:43:49 +0800

    Improved tests.

--- crates/bevy_color/src/interpolate.rs
+++ crates/bevy_color/src/interpolate.rs
@@ -20,7 +20,7 @@ impl_stable_interpolate_linear! {
 mod test {
     use bevy_math::StableInterpolate;
 
-    use crate::Srgba;
+    use crate::{Gray, Laba, LinearRgba, Oklaba, Srgba, Xyza};
 
     #[test]
     pub fn test_color_stable_interpolate() {
@@ -30,5 +30,24 @@ mod test {
             b.interpolate_stable(&w, 0.5),
             Srgba::new(0.5, 0.5, 0.5, 1.0),
         );
+
+        let b = LinearRgba::BLACK;
+        let w = LinearRgba::WHITE;
+        assert_eq!(
+            b.interpolate_stable(&w, 0.5),
+            LinearRgba::new(0.5, 0.5, 0.5, 1.0),
+        );
+
+        let b = Xyza::BLACK;
+        let w = Xyza::WHITE;
+        assert_eq!(b.interpolate_stable(&w, 0.5), Xyza::gray(0.5),);
+
+        let b = Laba::BLACK;
+        let w = Laba::WHITE;
+        assert_eq!(b.interpolate_stable(&w, 0.5), Laba::new(0.5, 0.0, 0.0, 1.0),);
+
+        let b = Oklaba::BLACK;
+        let w = Oklaba::WHITE;
+        assert_eq!(b.interpolate_stable(&w, 0.5), Oklaba::gray(0.5),);
     }
 }
