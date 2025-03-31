cf77085966b3c195f328c6d85ba34fa84e012aca
Kristoffer Søholm <k.soeholm@gmail.com>
2025-04-01 00:26:46 +0200

    Fix issues in log_diagnostics example

--- examples/diagnostics/log_diagnostics.rs
+++ examples/diagnostics/log_diagnostics.rs
@@ -8,22 +8,17 @@ use bevy::{
 fn main() {
     App::new()
         .add_plugins((
+            // The diagnostics plugins need to be added after DefaultPlugins as they use e.g. the time plugin for timestamps.
             DefaultPlugins,
-            // Adds frame time diagnostics
-            FrameTimeDiagnosticsPlugin::default(),
-            // Adds a system that prints diagnostics to the console
+            // Adds a system that prints diagnostics to the console.
+            // The other diagnostics plugins can still be used without this if you want to use them in an ingame overlay for example.
             LogDiagnosticsPlugin::default(),
-            // Any plugin can register diagnostics. Uncomment this to add an entity count diagnostics:
-            // bevy::diagnostic::EntityCountDiagnosticsPlugin::default(),
-
-            // Uncomment this to add an asset count diagnostics:
-            // bevy::asset::diagnostic::AssetCountDiagnosticsPlugin::<Texture>::default(),
-
-            // Uncomment this to add system info diagnostics:
-            // bevy::diagnostic::SystemInformationDiagnosticsPlugin::default()
-
-            // Uncomment this to add rendering diagnostics:
-            // bevy::render::diagnostic::RenderDiagnosticsPlugin::default(),
+            // Adds frame time, FPS and frame count diagnostics.
+            FrameTimeDiagnosticsPlugin::default(),
+            // Adds an entity count diagnostic.
+            bevy::diagnostic::EntityCountDiagnosticsPlugin::default(),
+            // Adds cpu and memory usage diagnostics for systems and the entire game process.
+            bevy::diagnostic::SystemInformationDiagnosticsPlugin::default(),
         ))
         .run();
 }
