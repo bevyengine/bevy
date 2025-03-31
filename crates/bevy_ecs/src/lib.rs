21d24af2a019a4ffbe50b81dc79d770d69d63e4b
Chris Russell <8494645+chescock@users.noreply.github.com>
2025-03-31 09:49:16 -0400

    Generic `When` wrapper to make any system param fallible.

--- crates/bevy_ecs/src/lib.rs
+++ crates/bevy_ecs/src/lib.rs
@@ -93,7 +93,7 @@ pub mod prelude {
             Command, Commands, Deferred, EntityCommand, EntityCommands, In, InMut, InRef,
             IntoSystem, Local, NonSend, NonSendMut, ParamSet, Populated, Query, ReadOnlySystem,
             Res, ResMut, Single, System, SystemIn, SystemInput, SystemParamBuilder,
-            SystemParamFunction,
+            SystemParamFunction, When,
         },
         world::{
             EntityMut, EntityRef, EntityWorldMut, FilteredResources, FilteredResourcesMut,
