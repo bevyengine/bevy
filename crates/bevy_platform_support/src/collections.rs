c2d943a700051945f1f504ca455b24ef4850414b
Alice I Cecile <alice.i.cecile@gmail.com>
2025-04-02 16:27:46 -0700

    Add UX regression test

--- crates/bevy_platform_support/src/collections.rs
+++ crates/bevy_platform_support/src/collections.rs
@@ -52,3 +52,29 @@ pub mod hash_table {
         IterMut, OccupiedEntry, VacantEntry,
     };
 }
+
+#[cfg(test)]
+mod tests {
+
+    // This is a compilation test for the UX regressions laid out in
+    // https://github.com/bevyengine/bevy/issues/18690.
+    // It ensures that the `HashSet` and `HashMap` types are usable without any confusing end-user problems.
+    // This test should never be removed: any future solution must make this test compiles as written.
+    #[test]
+    fn hash_set_and_map_new_work() {
+        use super::{HashMap, HashSet};
+
+        let new_set = HashSet::<u32>::new();
+        let new_map = HashMap::<u32, u32>::new();
+        let default_set = HashSet::<u32>::default();
+        let default_map = HashMap::<u32, u32>::default();
+
+        // This requires the types of new and default to match,
+        // which only works when HashMap's S generic is RandomState.
+        assert_eq!(new_set, default_set);
+        assert_eq!(new_map, default_map);
+
+        let _new_set_with_capacity = HashSet::<u32>::with_capacity(10);
+        let _new_map_with_capacity = HashMap::<u32, u32>::with_capacity(10);
+    }
+}
