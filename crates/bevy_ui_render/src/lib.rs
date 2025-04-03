9be9903b5322548d0df1f649b54e9c5b1a5017a4
ickshonpe <david.curthoys@googlemail.com>
2025-04-03 14:58:32 +0100

    added expect missing docs for ui render crate

--- crates/bevy_ui_render/src/lib.rs
+++ crates/bevy_ui_render/src/lib.rs
@@ -1,3 +1,10 @@
+#![expect(missing_docs, reason = "Not all docs are written yet, see #3492.")]
+#![cfg_attr(docsrs, feature(doc_auto_cfg))]
+#![doc(
+    html_logo_url = "https://bevyengine.org/assets/icon.png",
+    html_favicon_url = "https://bevyengine.org/assets/icon.png"
+)]
+
 pub mod box_shadow;
 mod pipeline;
 mod render_pass;
