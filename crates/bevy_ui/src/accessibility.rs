86cec2a3ca24dd46ce98a7166e70bc4f649b5ac4
ickshonpe <david.curthoys@googlemail.com>
2025-04-03 21:38:29 +0100

    Floor the coordinate values.

--- crates/bevy_ui/src/accessibility.rs
+++ crates/bevy_ui/src/accessibility.rs
@@ -49,7 +49,12 @@ fn calc_bounds(
             let half_size = 0.5 * node.size;
             let min = (center - half_size) * node.inverse_scale_factor;
             let max = (center + half_size) * node.inverse_scale_factor;
-            let bounds = Rect::new(min.x as f64, min.y as f64, max.x as f64, max.y as f64);
+            let bounds = Rect::new(
+                min.x.floor() as f64,
+                min.y.floor() as f64,
+                max.x.floor() as f64,
+                max.y.floor() as f64,
+            );
             accessible.set_bounds(bounds);
         }
     }
