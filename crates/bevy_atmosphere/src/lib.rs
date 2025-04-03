66fa6891b6409332cc3e2996f78a86036abfe5a9
Lucas Farias <lucasfnf@gmail.com>
2025-04-03 09:21:51 -0300

    Update docs of `bevy_atmosphere`

--- crates/bevy_atmosphere/src/lib.rs
+++ crates/bevy_atmosphere/src/lib.rs
@@ -28,7 +28,7 @@
 //! performance more finely, the [`AtmosphereSettings`] camera component
 //! manages the size of each LUT and the sample count for each ray.
 //!
-//! Given how similar it is to [`crate::volumetric_fog`], it might be expected
+//! Given how similar it is to [`VolumetricFog`](bevy_render_3d::VolumetricFog), it might be expected
 //! that these two modules would work together well. However for now using both
 //! at once is untested, and might not be physically accurate. These may be
 //! integrated into a single module in the future.
