0d2c45f157e0a19bac3e0e7fbd702d72cf17afc6
Alice Cecile <alice.i.cecile@gmail.com>
2025-04-10 22:08:07 -0700

    Link to never_say_never crate

--- crates/bevy_ecs/src/never.rs
+++ crates/bevy_ecs/src/never.rs
@@ -1,6 +1,6 @@
 //! A workaround for the `!` type in stable Rust.
 //!
-//! This approach is taken from the `never_say_never` crate,
+//! This approach is taken from the [`never_say_never`] crate,
 //! reimplemented here to avoid adding a new dependency.
 //!
 //! This module exists due to a change in [never type fallback inference] in the Rust 2024 edition.
@@ -14,6 +14,7 @@
 //! Please read [issue #18778](https://github.com/bevyengine/bevy/issues/18778) for an explanation of why
 //! Bevy has chosen to use this workaround.
 //!
+//! [`never_say_never`]: https://crates.io/crates/never_say_never
 //! [never type fallback inference]: https://doc.rust-lang.org/edition-guide/rust-2024/never-type-fallback.html
 
 mod fn_ret {
