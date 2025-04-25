7f5f9985f1e1be2a552be698f4f9b68ecf117a0b
ickshonpe <david.curthoys@googlemail.com>
2025-04-25 10:54:58 +0100

    Added `UiVec`, `UiTransform`, `UiGlobalTransform` types.

--- crates/bevy_ui/src/lib.rs
+++ crates/bevy_ui/src/lib.rs
@@ -17,6 +17,7 @@ pub mod widget;
 
 #[cfg(feature = "bevy_ui_picking_backend")]
 pub mod picking_backend;
+pub mod ui_transform;
 
 use bevy_derive::{Deref, DerefMut};
 use bevy_reflect::{std_traits::ReflectDefault, Reflect};
---
+++ crates/bevy_ui/src/ui_transform.rs
@@ -0,0 +1,91 @@
+use crate::Val;
+use bevy_derive::Deref;
+use bevy_ecs::component::Component;
+use bevy_ecs::prelude::ReflectComponent;
+use bevy_math::Affine2;
+use bevy_math::Vec2;
+use bevy_reflect::prelude::*;
+
+#[derive(Debug, PartialEq, Clone, Copy, Reflect)]
+pub struct UiVec {
+    /// Translate the node along the x-axis.
+    /// `Val::Percent` values are resolved based on the computed width of the Ui Node.
+    /// `Val::Auto` is resolved to `0.`.
+    x: Val,
+    /// Translate the node along the y-axis.
+    /// `Val::Percent` values are resolved based on the computed width of the Ui Node.
+    /// `Val::Auto` is resolved to `0.`.
+    y: Val,
+}
+
+impl UiVec {
+    pub const ZERO: Self = Self {
+        x: Val::ZERO,
+        y: Val::ZERO,
+    };
+
+    pub const fn px(x: f32, y: f32) -> Self {
+        Self {
+            x: Val::Px(x),
+            y: Val::Px(y),
+        }
+    }
+
+    pub const fn percent(x: f32, y: f32) -> Self {
+        Self {
+            x: Val::Percent(x),
+            y: Val::Percent(y),
+        }
+    }
+}
+
+impl Default for UiVec {
+    fn default() -> Self {
+        Self::ZERO
+    }
+}
+
+#[derive(Component, Debug, PartialEq, Clone, Copy, Reflect)]
+#[reflect(Component, Default, PartialEq, Debug, Clone)]
+#[cfg_attr(
+    feature = "serialize",
+    derive(serde::Serialize, serde::Deserialize),
+    reflect(Serialize, Deserialize)
+)]
+pub struct UiTransform {
+    /// Translate the node.
+    pub translation: UiVec,
+    /// Scale the node. A negative value reflects the node in that axis.
+    pub scale: Vec2,
+    /// Rotate the node clockwise by the given value in radians.
+    pub rotation: f32,
+}
+
+impl UiTransform {
+    pub const IDENTITY: Self = Self {
+        translation: UiVec::ZERO,
+        scale: Vec2::ONE,
+        rotation: 0.,
+    };
+}
+
+impl Default for UiTransform {
+    fn default() -> Self {
+        Self::IDENTITY
+    }
+}
+
+#[derive(Component, Debug, PartialEq, Clone, Copy, Reflect, Deref)]
+#[reflect(Component, Default, PartialEq, Debug, Clone)]
+#[cfg_attr(
+    feature = "serialize",
+    derive(serde::Serialize, serde::Deserialize),
+    reflect(Serialize, Deserialize)
+)]
+pub struct UiGlobalTransform(Affine2);
+
+impl Default for UiGlobalTransform {
+    fn default() -> Self {
+        Self(Affine2::IDENTITY)
+    }
+}
