9723d734dc877de6ea7c4dba6fcdac787641dc76
zuiyu1998 <1542844298@qq.com>
2025-04-26 15:15:30 +0800

    Add set_render_pipeline for TrackedRenderPass

--- crates/bevy_render/src/frame_graph/render_context.rs
+++ crates/bevy_render/src/frame_graph/render_context.rs
@@ -2,7 +2,10 @@ use super::{
     FrameGraphBuffer, FrameGraphError, GraphResource, RenderPassInfo, ResourceRead, ResourceRef,
     ResourceTable,
 };
-use crate::renderer::RenderDevice;
+use crate::{
+    render_resource::{CachedRenderPipelineId, PipelineCache, RenderPipeline},
+    renderer::RenderDevice,
+};
 use core::ops::Range;
 
 pub trait ExtraResource {
@@ -13,17 +16,18 @@ pub trait ExtraResource {
     ) -> Result<Self::Resource, FrameGraphError>;
 }
 
-pub struct RenderContext {
+pub struct RenderContext<'a> {
     render_device: RenderDevice,
     pub(crate) resource_table: ResourceTable,
     command_buffer_queue: Vec<wgpu::CommandBuffer>,
+    pipeline_cache: &'a PipelineCache,
 }
 
-impl RenderContext {
-    pub fn begin_render_pass<'a>(
-        &'a mut self,
+impl<'a> RenderContext<'a> {
+    pub fn begin_render_pass<'b>(
+        &'b mut self,
         render_pass_info: &RenderPassInfo,
-    ) -> Result<TrackedRenderPass<'a>, FrameGraphError> {
+    ) -> Result<TrackedRenderPass<'a, 'b>, FrameGraphError> {
         let mut command_encoder = self
             .render_device
             .create_command_encoder(&wgpu::CommandEncoderDescriptor::default());
@@ -35,6 +39,15 @@ impl RenderContext {
             render_pass,
             render_context: self,
         })
+    }
+
+    pub fn get_render_pipeline(
+        &self,
+        id: CachedRenderPipelineId,
+    ) -> Result<&RenderPipeline, FrameGraphError> {
+        self.pipeline_cache
+            .get_render_pipeline(id)
+            .ok_or(FrameGraphError::ResourceNotFound)
     }
 
     pub fn get_resource<ResourceType: GraphResource>(
@@ -51,13 +64,23 @@ impl RenderContext {
     }
 }
 
-pub struct TrackedRenderPass<'a> {
+pub struct TrackedRenderPass<'a, 'b> {
     command_encoder: wgpu::CommandEncoder,
     render_pass: wgpu::RenderPass<'static>,
-    render_context: &'a mut RenderContext,
+    render_context: &'b mut RenderContext<'a>,
 }
 
-impl<'a> TrackedRenderPass<'a> {
+impl<'a, 'b> TrackedRenderPass<'a, 'b> {
+    pub fn set_render_pipeline(
+        &mut self,
+        id: CachedRenderPipelineId,
+    ) -> Result<(), FrameGraphError> {
+        let pipeline = self.render_context.get_render_pipeline(id)?;
+        self.render_pass.set_pipeline(pipeline);
+
+        Ok(())
+    }
+
     pub fn draw_indexed(&mut self, indices: Range<u32>, base_vertex: i32, instances: Range<u32>) {
         self.render_pass
             .draw_indexed(indices, base_vertex, instances);
