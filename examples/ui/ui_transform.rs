fb9fc0ff0575ef4c15b0c6ec477af78844a4ab2f
ickshonpe <david.curthoys@googlemail.com>
2025-04-27 11:46:15 +0100

    Added `ui_transform` example

---
+++ examples/ui/ui_transform.rs
@@ -0,0 +1,392 @@
+//! An example demonstrating how to translate, rotate and scale UI elements.
+use bevy::color::palettes::css::DARK_GRAY;
+use bevy::color::palettes::css::RED;
+use bevy::color::palettes::css::YELLOW;
+use bevy::prelude::*;
+
+// #[derive(Resource)]
+// struct Center(Entity);
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .add_systems(Update, button_system)
+        .add_systems(Update, translation_system)
+        .run();
+}
+
+const NORMAL_BUTTON: Color = Color::WHITE;
+const HOVERED_BUTTON: Color = Color::Srgba(YELLOW);
+const PRESSED_BUTTON: Color = Color::Srgba(RED);
+
+/// A button that rotates the target node
+#[derive(Component)]
+pub struct RotateButton(pub f32);
+
+/// A button that scales the target node
+#[derive(Component)]
+pub struct ScaleButton(pub f32);
+
+/// Marker component so the systems know which entities to translate, rotate and scale
+#[derive(Component)]
+pub struct TargetNode;
+
+/// Handles button interactions
+fn button_system(
+    mut interaction_query: Query<
+        (
+            &Interaction,
+            &mut BackgroundColor,
+            Option<&RotateButton>,
+            Option<&ScaleButton>,
+        ),
+        (Changed<Interaction>, With<Button>),
+    >,
+    mut rotator_query: Query<&mut UiTransform, With<TargetNode>>,
+) {
+    for (interaction, mut color, maybe_rotate, maybe_scale) in &mut interaction_query {
+        match *interaction {
+            Interaction::Pressed => {
+                *color = PRESSED_BUTTON.into();
+                if let Some(step) = maybe_rotate {
+                    for mut transform in rotator_query.iter_mut() {
+                        transform.rotation += step.0;
+                    }
+                }
+                if let Some(step) = maybe_scale {
+                    for mut transform in rotator_query.iter_mut() {
+                        transform.scale += step.0;
+                        transform.scale =
+                            transform.scale.clamp(Vec2::splat(0.25), Vec2::splat(3.0));
+                    }
+                }
+            }
+            Interaction::Hovered => {
+                *color = HOVERED_BUTTON.into();
+            }
+            Interaction::None => {
+                *color = NORMAL_BUTTON.into();
+            }
+        }
+    }
+}
+
+// move the rotating panel when the arrow keys are pressed
+fn translation_system(
+    time: Res<Time>,
+    input: Res<ButtonInput<KeyCode>>,
+    mut translation_query: Query<&mut UiTransform, With<TargetNode>>,
+) {
+    let controls = [
+        (KeyCode::ArrowLeft, -Vec2::X),
+        (KeyCode::ArrowRight, Vec2::X),
+        (KeyCode::ArrowUp, -Vec2::Y),
+        (KeyCode::ArrowDown, Vec2::Y),
+    ];
+    for &(key_code, direction) in &controls {
+        if input.pressed(key_code) {
+            for mut transform in translation_query.iter_mut() {
+                let d = direction * 50.0 * time.delta_secs();
+                let (Val::Px(x), Val::Px(y)) = (transform.translation.x, transform.translation.y)
+                else {
+                    continue;
+                };
+                let x = (x + d.x).clamp(-150., 150.);
+                let y = (y + d.y).clamp(-150., 150.);
+
+                transform.translation = UiVec::px(x, y);
+            }
+        }
+    }
+}
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    // UI camera
+
+    commands.spawn(Camera2d::default());
+    // Root node filling the whole screen
+    commands
+        .spawn((
+            Node {
+                width: Val::Percent(100.),
+                height: Val::Percent(100.),
+                align_items: AlignItems::Center,
+                justify_content: JustifyContent::Center,
+                ..default()
+            },
+            BackgroundColor(Color::BLACK),
+        ))
+        .with_children(|parent| {
+            // Horizontal container for two sections
+            parent
+                .spawn((
+                    Node {
+                        align_items: AlignItems::Center,
+                        justify_content: JustifyContent::SpaceEvenly,
+                        column_gap: Val::Px(25.0),
+                        row_gap: Val::Px(25.0),
+                        ..default()
+                    },
+                    BackgroundColor(Color::BLACK),
+                ))
+                .with_children(|parent| {
+                    // Left column of controls
+                    parent
+                        .spawn((
+                            Node {
+                                flex_direction: FlexDirection::Column,
+                                justify_content: JustifyContent::Center,
+                                padding: UiRect::all(Val::Px(10.0)),
+                                row_gap: Val::Px(10.0),
+                                column_gap: Val::Px(10.0),
+                                ..default()
+                            },
+                            BackgroundColor(Color::BLACK),
+                            GlobalZIndex(1),
+                        ))
+                        .with_children(|parent| {
+                            // Rotate left button
+                            parent
+                                .spawn((
+                                    Button, // inserts ButtonBundle’s internals automatically
+                                    Node {
+                                        width: Val::Px(50.),
+                                        height: Val::Px(50.),
+                                        align_items: AlignItems::Center,
+                                        justify_content: JustifyContent::Center,
+                                        ..default()
+                                    },
+                                    BackgroundColor(Color::WHITE),
+                                    RotateButton(-std::f32::consts::FRAC_PI_8),
+                                ))
+                                .with_children(|parent| {
+                                    parent.spawn((
+                                        Text::new("<--"),
+                                        TextFont {
+                                            font: asset_server.load("fonts/FiraSans-Bold.ttf"),
+                                            font_size: 16.0,
+                                            ..Default::default()
+                                        },
+                                        TextColor(Color::BLACK),
+                                    ));
+                                });
+
+                            // Scale down button
+                            parent
+                                .spawn((
+                                    Button,
+                                    Node {
+                                        width: Val::Px(50.0),
+                                        height: Val::Px(50.0),
+                                        align_items: AlignItems::Center,
+                                        justify_content: JustifyContent::Center,
+                                        ..default()
+                                    },
+                                    BackgroundColor(Color::WHITE),
+                                    ScaleButton(-0.25),
+                                ))
+                                .with_children(|parent| {
+                                    parent.spawn((
+                                        Text::new("-"),
+                                        TextFont {
+                                            font: asset_server.load("fonts/FiraSans-Bold.ttf"),
+                                            font_size: 16.0,
+                                            ..Default::default()
+                                        },
+                                        TextColor(Color::BLACK),
+                                    ));
+                                });
+                        });
+
+                    // Target node with its own set of buttons
+                    parent
+                        .spawn((
+                            Node {
+                                flex_direction: FlexDirection::Column,
+                                justify_content: JustifyContent::SpaceBetween,
+                                align_items: AlignItems::Center,
+                                width: Val::Px(300.0),
+                                height: Val::Px(300.0),
+                                ..default()
+                            },
+                            BackgroundColor(DARK_GRAY.into()),
+                            TargetNode,
+                        ))
+                        .with_children(|parent| {
+                            // Top
+                            parent
+                                .spawn((
+                                    Button,
+                                    Node {
+                                        width: Val::Px(50.0),
+                                        height: Val::Px(50.0),
+                                        align_items: AlignItems::Center,
+                                        justify_content: JustifyContent::Center,
+                                        ..default()
+                                    },
+                                    BackgroundColor(Color::WHITE),
+                                ))
+                                .with_children(|parent| {
+                                    parent.spawn((
+                                        Text::new("Top"),
+                                        TextFont {
+                                            font: asset_server.load("fonts/FiraSans-Bold.ttf"),
+                                            font_size: 16.0,
+                                            ..Default::default()
+                                        },
+                                        TextColor(Color::BLACK),
+                                    ));
+                                });
+
+                            // Middle row (Left and Right)
+                            parent.spawn(Node::default()).with_children(|parent| {
+                                // Left
+                                parent
+                                    .spawn((
+                                        Button,
+                                        Node {
+                                            width: Val::Px(50.0),
+                                            height: Val::Px(50.0),
+                                            align_items: AlignItems::Center,
+                                            justify_content: JustifyContent::Center,
+                                            ..default()
+                                        },
+                                        BackgroundColor(Color::WHITE),
+                                        UiTransform::from_angle(std::f32::consts::FRAC_PI_2),
+                                    ))
+                                    .with_children(|parent| {
+                                        parent.spawn((
+                                            Text::new("Left"),
+                                            TextFont {
+                                                font: asset_server.load("fonts/FiraSans-Bold.ttf"),
+                                                font_size: 16.0,
+                                                ..Default::default()
+                                            },
+                                            TextColor(Color::BLACK),
+                                        ));
+                                    });
+                                // Right
+                                parent
+                                    .spawn((
+                                        Button,
+                                        Node {
+                                            width: Val::Px(50.0),
+                                            height: Val::Px(50.0),
+                                            align_items: AlignItems::Center,
+                                            justify_content: JustifyContent::Center,
+                                            ..default()
+                                        },
+                                        BackgroundColor(Color::WHITE),
+                                        UiTransform::from_angle(-std::f32::consts::FRAC_PI_2),
+                                    ))
+                                    .with_children(|parent| {
+                                        parent.spawn((
+                                            Text::new("Right"),
+                                            TextFont {
+                                                font: asset_server.load("fonts/FiraSans-Bold.ttf"),
+                                                font_size: 16.0,
+                                                ..Default::default()
+                                            },
+                                            TextColor(Color::BLACK),
+                                        ));
+                                    });
+                            });
+
+                            // Bottom
+                            parent
+                                .spawn((
+                                    Button,
+                                    Node {
+                                        width: Val::Px(50.0),
+                                        height: Val::Px(50.0),
+                                        align_items: AlignItems::Center,
+                                        justify_content: JustifyContent::Center,
+                                        ..default()
+                                    },
+                                    BackgroundColor(Color::WHITE),
+                                    UiTransform::from_angle(std::f32::consts::PI),
+                                ))
+                                .with_children(|parent| {
+                                    parent.spawn((
+                                        Text::new("Bottom"),
+                                        TextFont {
+                                            font: asset_server.load("fonts/FiraSans-Bold.ttf"),
+                                            font_size: 16.0,
+                                            ..Default::default()
+                                        },
+                                        TextColor(Color::BLACK),
+                                    ));
+                                });
+                        });
+
+                    // Right column of controls
+                    parent
+                        .spawn((
+                            Node {
+                                flex_direction: FlexDirection::Column,
+                                justify_content: JustifyContent::Center,
+                                row_gap: Val::Px(10.0),
+                                column_gap: Val::Px(10.0),
+                                padding: UiRect::all(Val::Px(10.0)),
+                                ..default()
+                            },
+                            BackgroundColor(Color::BLACK),
+                            GlobalZIndex(1),
+                        ))
+                        .with_children(|parent| {
+                            // Rotate right button
+                            parent
+                                .spawn((
+                                    Button,
+                                    Node {
+                                        height: Val::Px(50.0),
+                                        width: Val::Px(50.0),
+                                        align_items: AlignItems::Center,
+                                        justify_content: JustifyContent::Center,
+                                        ..default()
+                                    },
+                                    BackgroundColor(Color::WHITE),
+                                    RotateButton(std::f32::consts::FRAC_PI_8),
+                                ))
+                                .with_children(|parent| {
+                                    parent.spawn((
+                                        Text::new("-->"),
+                                        TextFont {
+                                            font: asset_server.load("fonts/FiraSans-Bold.ttf"),
+                                            font_size: 16.0,
+                                            ..Default::default()
+                                        },
+                                        TextColor(Color::BLACK),
+                                    ));
+                                });
+
+                            // Scale up button
+                            parent
+                                .spawn((
+                                    Button,
+                                    Node {
+                                        height: Val::Px(50.0),
+                                        width: Val::Px(50.0),
+                                        align_items: AlignItems::Center,
+                                        justify_content: JustifyContent::Center,
+                                        ..default()
+                                    },
+                                    BackgroundColor(Color::WHITE),
+                                    ScaleButton(0.25),
+                                ))
+                                .with_children(|parent| {
+                                    parent.spawn((
+                                        Text::new("+"),
+                                        TextFont {
+                                            font: asset_server.load("fonts/FiraSans-Bold.ttf"),
+                                            font_size: 16.0,
+                                            ..Default::default()
+                                        },
+                                        TextColor(Color::BLACK),
+                                    ));
+                                });
+                        });
+                });
+        });
+}
