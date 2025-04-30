d91ccb9f829f69f145b0080f7334cc35a2a1219f
Conner Petzold <cpetzold@gmail.com>
2025-04-30 00:34:32 -0400

    Update to use @grind086's vertex shader method

--- crates/bevy_sprite/src/tilemap_chunk/tilemap_chunk_material.rs
+++ crates/bevy_sprite/src/tilemap_chunk/tilemap_chunk_material.rs
@@ -1,12 +1,18 @@
-use crate::{Material2d, Material2dPlugin};
+use crate::{AlphaMode2d, Material2d, Material2dKey, Material2dPlugin};
 use bevy_app::{App, Plugin};
 use bevy_asset::{load_internal_asset, weak_handle, Asset, Handle};
 use bevy_image::Image;
 use bevy_reflect::prelude::*;
-use bevy_render::render_resource::*;
+use bevy_render::{
+    mesh::{Mesh, MeshVertexAttribute, MeshVertexBufferLayoutRef},
+    render_resource::*,
+};
 
 pub const TILEMAP_CHUNK_MATERIAL_SHADER_HANDLE: Handle<Shader> =
     weak_handle!("40f33e62-82f8-4578-b3fa-f22989e7c4bb");
+
+pub const ATTRIBUTE_TILE_INDEX: MeshVertexAttribute =
+    MeshVertexAttribute::new("Vertex_TileIndex", 264043692, VertexFormat::Uint32);
 
 /// Plugin that adds support for tilemap chunk materials.
 #[derive(Default)]
@@ -31,6 +37,8 @@ impl Plugin for TilemapChunkMaterialPlugin {
 /// efficiently using a single draw call per chunk.
 #[derive(Asset, TypePath, AsBindGroup, Debug, Clone)]
 pub struct TilemapChunkMaterial {
+    pub alpha_mode: AlphaMode2d,
+
     #[texture(0, dimension = "2d_array")]
     #[sampler(1)]
     pub tileset: Handle<Image>,
@@ -42,5 +50,27 @@ pub struct TilemapChunkMaterial {
 impl Material2d for TilemapChunkMaterial {
     fn fragment_shader() -> ShaderRef {
         TILEMAP_CHUNK_MATERIAL_SHADER_HANDLE.into()
+    }
+
+    fn vertex_shader() -> ShaderRef {
+        TILEMAP_CHUNK_MATERIAL_SHADER_HANDLE.into()
+    }
+
+    fn alpha_mode(&self) -> AlphaMode2d {
+        self.alpha_mode
+    }
+
+    fn specialize(
+        descriptor: &mut RenderPipelineDescriptor,
+        layout: &MeshVertexBufferLayoutRef,
+        _key: Material2dKey<Self>,
+    ) -> Result<(), SpecializedMeshPipelineError> {
+        let vertex_layout = layout.0.get_layout(&[
+            Mesh::ATTRIBUTE_POSITION.at_shader_location(0),
+            Mesh::ATTRIBUTE_UV_0.at_shader_location(1),
+            ATTRIBUTE_TILE_INDEX.at_shader_location(5),
+        ])?;
+        descriptor.vertex.buffers = vec![vertex_layout];
+        Ok(())
     }
 }
