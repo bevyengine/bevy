8aa8b88e2e47d7cd0278113a22a15c86cef8c908
Conner Petzold <cpetzold@gmail.com>
2025-05-05 18:40:53 -0400

    Tilemap rendering

--- crates/bevy_sprite/src/lib.rs
+++ crates/bevy_sprite/src/lib.rs
@@ -16,7 +16,7 @@ mod picking_backend;
 mod render;
 mod sprite;
 mod texture_slice;
-mod tilemap_chunk;
+mod tilemap;
 
 /// The sprite prelude.
 ///
@@ -41,7 +41,7 @@ pub use picking_backend::*;
 pub use render::*;
 pub use sprite::*;
 pub use texture_slice::*;
-pub use tilemap_chunk::*;
+pub use tilemap::*;
 
 use bevy_app::prelude::*;
 use bevy_asset::{load_internal_asset, weak_handle, AssetEvents, Assets, Handle};
@@ -98,12 +98,7 @@ impl Plugin for SpritePlugin {
             .register_type::<TextureSlicer>()
             .register_type::<Anchor>()
             .register_type::<Mesh2d>()
-            .add_plugins((
-                Mesh2dRenderPlugin,
-                ColorMaterialPlugin,
-                TilemapChunkPlugin,
-                TilemapChunkMaterialPlugin,
-            ))
+            .add_plugins((Mesh2dRenderPlugin, ColorMaterialPlugin, TilemapPlugin))
             .add_systems(
                 PostUpdate,
                 (
