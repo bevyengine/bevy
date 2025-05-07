9e2bd8ac1850ed922073d47384785769ff60a058
Chris Russell <8494645+chescock@users.noreply.github.com>
2025-05-07 14:20:08 -0400

    Generic `SystemParam` impls for `Option` and `Result` (#18766)
    # Objective
    
    Provide a generic `impl SystemParam for Option<P>` that uses system
    parameter validation. This immediately gives useful impls for params
    like `EventReader` and `GizmosState` that are defined in terms of `Res`.
    It also allows third-party system parameters to be usable with `Option`,
    which was previously impossible due to orphan rules.
    
    Note that this is a behavior change for `Option<Single>`. It currently
    fails validation if there are multiple matching entities, but with this
    change it will pass validation and produce `None`.
    
    Also provide an impl for `Result<P, SystemParamValidationError>`. This
    allows systems to inspect the error if necessary, either for bubbling it
    up or for checking the `skipped` flag.
    
    Fixes #12634
    Fixes #14949
    Related to #18516
    
    ## Solution
    
    Add generic `SystemParam` impls for `Option` and `Result`, and remove
    the impls for specific types.
    
    Update documentation and `fallible_params` example with the new
    semantics for `Option<Single>`.

--- crates/bevy_ecs/src/system/builder.rs
+++ crates/bevy_ecs/src/system/builder.rs
@@ -7,7 +7,8 @@ use crate::{
     query::{QueryData, QueryFilter, QueryState},
     resource::Resource,
     system::{
-        DynSystemParam, DynSystemParamState, Local, ParamSet, Query, SystemMeta, SystemParam, When,
+        DynSystemParam, DynSystemParamState, Local, ParamSet, Query, SystemMeta, SystemParam,
+        SystemParamValidationError, When,
     },
     world::{
         FilteredResources, FilteredResourcesBuilder, FilteredResourcesMut,
@@ -707,6 +708,36 @@ unsafe impl<'w, 's, T: FnOnce(&mut FilteredResourcesMutBuilder)>
         }
 
         access
+    }
+}
+
+/// A [`SystemParamBuilder`] for an [`Option`].
+#[derive(Clone)]
+pub struct OptionBuilder<T>(T);
+
+// SAFETY: `OptionBuilder<B>` builds a state that is valid for `P`, and any state valid for `P` is valid for `Option<P>`
+unsafe impl<P: SystemParam, B: SystemParamBuilder<P>> SystemParamBuilder<Option<P>>
+    for OptionBuilder<B>
+{
+    fn build(self, world: &mut World, meta: &mut SystemMeta) -> <Option<P> as SystemParam>::State {
+        self.0.build(world, meta)
+    }
+}
+
+/// A [`SystemParamBuilder`] for a [`Result`] of [`SystemParamValidationError`].
+#[derive(Clone)]
+pub struct ResultBuilder<T>(T);
+
+// SAFETY: `ResultBuilder<B>` builds a state that is valid for `P`, and any state valid for `P` is valid for `Result<P, SystemParamValidationError>`
+unsafe impl<P: SystemParam, B: SystemParamBuilder<P>>
+    SystemParamBuilder<Result<P, SystemParamValidationError>> for ResultBuilder<B>
+{
+    fn build(
+        self,
+        world: &mut World,
+        meta: &mut SystemMeta,
+    ) -> <Result<P, SystemParamValidationError> as SystemParam>::State {
+        self.0.build(world, meta)
     }
 }
 
