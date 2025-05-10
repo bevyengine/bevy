15432f2033dbba4953b19cf55218c775dd760055
Greeble <166992735+greeble-dev@users.noreply.github.com>
2025-05-10 13:17:10 +0100

    Updated `asset_processing` example.

--- examples/asset/processing/asset_processing.rs
+++ examples/asset/processing/asset_processing.rs
@@ -12,6 +12,7 @@ use bevy::{
     prelude::*,
     reflect::TypePath,
 };
+use bevy_asset::AssetPath;
 use serde::{Deserialize, Serialize};
 use std::convert::Infallible;
 use thiserror::Error;
@@ -159,11 +160,10 @@ impl AssetLoader for CoolTextLoader {
         for (path, settings_override) in ron.dependencies_with_settings {
             let loaded = load_context
                 .loader()
-                .with_settings(move |settings| {
-                    *settings = settings_override.clone();
-                })
                 .immediate()
-                .load::<Text>(&path)
+                .load::<Text>(AssetPath::from(path).with_settings_fn(move |settings| {
+                    *settings = settings_override.clone();
+                }))
                 .await?;
             base_text.push_str(&loaded.get().0);
         }
