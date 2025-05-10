da7d24fe50b5e493afc2bf2d2568d0006e42b7b0
Conner Petzold <cpetzold@gmail.com>
2025-05-09 21:27:23 -0400

    wip tilemap rendering

--- crates/bevy_sprite/src/tilemap/mod.rs
+++ crates/bevy_sprite/src/tilemap/mod.rs
@@ -2,15 +2,28 @@ mod tilemap_chunk;
 mod tilemap_chunk_material;
 
 use bevy_app::{App, Plugin};
+use bevy_asset::Handle;
 use bevy_color::Color;
-use bevy_ecs::component::Component;
-use bevy_math::{IVec2, UVec2};
-use bevy_platform::collections::{HashMap, HashSet};
+use bevy_derive::{Deref, DerefMut};
+use bevy_ecs::{
+    component::Component,
+    entity::Entity,
+    name::Name,
+    relationship::{RelationshipSourceCollection, RelationshipTarget},
+};
+use bevy_image::Image;
+use bevy_math::{IRect, IVec2, UVec2};
+use bevy_platform::collections::{hash_map::Values, HashMap, HashSet};
+use bevy_render::view::Visibility;
+use bevy_transform::components::Transform;
+use bevy_utils::default;
 use derive_more::derive::AsRef;
 use lettuces::storage::grid::Grid;
 pub use tilemap_chunk::*;
 pub use tilemap_chunk_material::*;
 use tracing::warn;
+
+use crate::AlphaMode2d;
 
 /// Plugin that adds support for tilemap chunk materials.
 #[derive(Default)]
@@ -23,51 +36,134 @@ impl Plugin for TilemapPlugin {
     }
 }
 
-#[derive(Clone, Copy)]
+/// A component representing a tileset image containing all tile textures.
+#[derive(Component, Clone, Debug, Default)]
+pub struct Tileset {
+    pub image: Handle<Image>,
+    pub tile_size: UVec2,
+}
+
+#[derive(Component, Clone)]
+#[require(TileStorage, Tileset, Name::new("Tilemap"), Transform, Visibility)]
+pub struct Tilemap {
+    chunks: EntitiesMap,
+
+    /// The alpha mode to use for the tilemap
+    pub alpha_mode: AlphaMode2d,
+}
+
+impl Default for Tilemap {
+    fn default() -> Self {
+        Self {
+            chunks: EntitiesMap::default(),
+            alpha_mode: AlphaMode2d::default(),
+        }
+    }
+}
+
+impl RelationshipTarget for Tilemap {
+    type Collection = EntitiesMap;
+    type Relationship = TilemapChunk;
+
+    const LINKED_SPAWN: bool = false;
+
+    fn collection(&self) -> &Self::Collection {
+        &self.chunks
+    }
+
+    fn collection_mut_risky(&mut self) -> &mut Self::Collection {
+        &mut self.chunks
+    }
+
+    fn from_collection_risky(collection: Self::Collection) -> Self {
+        Self {
+            chunks: collection,
+            alpha_mode: AlphaMode2d::default(),
+        }
+    }
+}
+
+#[derive(Clone, Copy, Default, Debug)]
 pub struct TileFlip {
     pub x: bool,
     pub y: bool,
     pub d: bool,
 }
 
-#[derive(Clone, Copy, AsRef)]
+#[derive(Clone, Copy, Debug, AsRef)]
 pub struct TileData {
-    tileset_index: u16,
-    color: Color,
-    visible: bool,
-    flip: TileFlip,
+    pub tileset_index: u16,
+    pub color: Color,
+    pub visible: bool,
+    pub flip: TileFlip,
 }
 
-#[derive(Default, Component)]
-pub struct TileStorage {
-    data: TileStorageData,
-    chunk_size: IVec2,
-    dirty_chunk_positions: HashSet<IVec2>,
+impl Default for TileData {
+    fn default() -> Self {
+        Self {
+            tileset_index: u16::MAX,
+            color: Color::WHITE,
+            visible: true,
+            flip: TileFlip::default(),
+        }
+    }
+}
+
+impl TileData {
+    pub fn from_index(tileset_index: u16) -> Self {
+        Self {
+            tileset_index,
+            ..default()
+        }
+    }
 }
 
 pub enum TileStorageAccessError {
     OutOfBounds { x: i32, y: i32 },
 }
 
-impl TileStorage {
-    pub fn new(chunk_size: UVec2) -> Self {
-        Self::sparse(chunk_size)
-    }
+#[derive(Component)]
+pub struct TileStorage {
+    data: TileStorageData,
+    chunk_size: IVec2,
+    dirty_chunk_positions: HashSet<IVec2>,
+}
 
-    pub fn sparse(chunk_size: UVec2) -> Self {
+impl Default for TileStorage {
+    fn default() -> Self {
         Self {
             data: TileStorageData::Sparse(HashMap::new()),
-            chunk_size: chunk_size.as_ivec2(),
+            chunk_size: IVec2::splat(32),
             dirty_chunk_positions: HashSet::new(),
         }
     }
+}
 
-    pub fn dense(size: UVec2, chunk_size: UVec2) -> Self {
+impl TileStorage {
+    pub fn new() -> Self {
+        Self::sparse()
+    }
+
+    pub fn sparse() -> Self {
+        Self {
+            data: TileStorageData::Sparse(HashMap::new()),
+            ..default()
+        }
+    }
+
+    pub fn dense(size: UVec2) -> Self {
         Self {
             data: TileStorageData::Dense(Grid::new(size.x as usize, size.y as usize)),
-            chunk_size: chunk_size.as_ivec2(),
-            dirty_chunk_positions: HashSet::new(),
+            ..default()
         }
+    }
+
+    pub fn set_chunk_size(&mut self, chunk_size: UVec2) {
+        self.chunk_size = chunk_size.as_ivec2();
+    }
+
+    pub fn chunk_size(&self) -> UVec2 {
+        self.chunk_size.as_uvec2()
     }
 
     pub fn get(&self, tile_position: IVec2) -> Result<Option<&TileData>, TileStorageAccessError> {
@@ -86,17 +182,21 @@ impl TileStorage {
         }
     }
 
-    pub fn set(&mut self, tile_position: IVec2, tile_data: TileData) {
+    pub fn set(&mut self, tile_position: IVec2, tile_data: Option<TileData>) {
         match &mut self.data {
             TileStorageData::Sparse(tiles) => {
-                tiles.insert(tile_position, tile_data);
+                if let Some(tile_data) = tile_data {
+                    tiles.insert(tile_position, tile_data);
+                } else {
+                    tiles.remove(&tile_position);
+                }
             }
             TileStorageData::Dense(tiles) => {
                 let Some(tile) = tiles.get_mut(tile_position.x as usize, tile_position.y as usize)
                 else {
                     return;
                 };
-                *tile = Some(tile_data);
+                *tile = tile_data;
             }
         }
 
@@ -104,13 +204,28 @@ impl TileStorage {
             .insert(tile_position.div_euclid(self.chunk_size));
     }
 
-    pub fn fill(&mut self, tile_data: TileData) {
+    pub fn fill_rect_with<F>(&mut self, rect: IRect, mut f: F)
+    where
+        F: FnMut(IVec2) -> Option<TileData>,
+    {
+        for y in rect.min.y..rect.max.y {
+            for x in rect.min.x..rect.max.x {
+                self.set(IVec2::new(x, y), f(IVec2::new(x, y)));
+            }
+        }
+    }
+
+    pub fn fill_rect(&mut self, rect: IRect, tile_data: Option<TileData>) {
+        self.fill_rect_with(rect, |_| tile_data);
+    }
+
+    pub fn fill(&mut self, tile_data: Option<TileData>) {
         match &mut self.data {
             TileStorageData::Sparse(_) => {
                 warn!("TileStorage::fill is not supported for sparse tile storage");
             }
             TileStorageData::Dense(tiles) => {
-                tiles.fill(Some(tile_data));
+                tiles.fill(tile_data);
                 self.set_all_dirty();
             }
         }
@@ -149,12 +264,60 @@ impl TileStorage {
         }
     }
 
-    pub fn clear_dirty_chunk_positions(&mut self) {
-        self.dirty_chunk_positions.clear();
+    pub fn clear_dirty_chunk_positions(&mut self, chunk_positions: HashSet<IVec2>) {
+        self.dirty_chunk_positions
+            .retain(|chunk_position| !chunk_positions.contains(chunk_position));
     }
 
     pub fn iter_dirty_chunk_positions(&self) -> impl Iterator<Item = &IVec2> {
         self.dirty_chunk_positions.iter()
+    }
+
+    pub fn rect_tiles(
+        &self,
+        rect: IRect,
+    ) -> Result<Vec<Option<&TileData>>, TileStorageAccessError> {
+        let IRect { min, max } = rect;
+        match &self.data {
+            TileStorageData::Sparse(tiles) => {
+                let mut result = Vec::with_capacity(rect.size().element_product() as usize);
+                for y in min.y..max.y {
+                    for x in min.x..max.x {
+                        result.push(tiles.get(&IVec2::new(x, y)));
+                    }
+                }
+                Ok(result)
+            }
+            TileStorageData::Dense(tiles) => {
+                if min.x < 0
+                    || min.y < 0
+                    || max.x >= tiles.cols() as i32
+                    || max.y >= tiles.rows() as i32
+                {
+                    return Err(TileStorageAccessError::OutOfBounds { x: min.x, y: min.y });
+                }
+                let mut result = Vec::with_capacity(rect.size().element_product() as usize);
+                for y in min.y..max.y {
+                    for x in min.x..max.x {
+                        result.push(tiles[(x as usize, y as usize)].as_ref());
+                    }
+                }
+                Ok(result)
+            }
+        }
+    }
+
+    pub fn chunk_tiles(
+        &self,
+        chunk_position: IVec2,
+    ) -> Result<Vec<Option<&TileData>>, TileStorageAccessError> {
+        let chunk_size = self.chunk_size;
+        let chunk_rect = IRect::from_corners(
+            chunk_position * chunk_size,
+            (chunk_position + IVec2::splat(1)) * chunk_size,
+        );
+
+        self.rect_tiles(chunk_rect)
     }
 }
 
@@ -166,5 +329,48 @@ pub enum TileStorageData {
 impl Default for TileStorageData {
     fn default() -> Self {
         Self::Sparse(HashMap::new())
+    }
+}
+
+#[derive(Default, Clone, Deref, DerefMut)]
+pub struct EntitiesMap(HashMap<IVec2, Entity>);
+
+impl RelationshipSourceCollection for EntitiesMap {
+    type SourceIter<'a> = std::iter::Copied<Values<'a, IVec2, Entity>>;
+
+    fn new() -> Self {
+        Self::default()
+    }
+
+    fn with_capacity(capacity: usize) -> Self {
+        Self(HashMap::with_capacity(capacity))
+    }
+
+    fn reserve(&mut self, additional: usize) {
+        self.0.reserve(additional);
+    }
+
+    fn add(&mut self, _: Entity) -> bool {
+        false
+    }
+
+    fn remove(&mut self, _: Entity) -> bool {
+        false
+    }
+
+    fn iter(&self) -> Self::SourceIter<'_> {
+        self.0.values().copied()
+    }
+
+    fn len(&self) -> usize {
+        self.0.len()
+    }
+
+    fn shrink_to_fit(&mut self) {
+        self.0.shrink_to_fit();
+    }
+
+    fn clear(&mut self) {
+        self.0.clear();
     }
 }
--- crates/bevy_sprite/src/tilemap/tilemap_chunk.rs
+++ crates/bevy_sprite/src/tilemap/tilemap_chunk.rs
@@ -1,28 +1,34 @@
-use crate::{AlphaMode2d, Anchor, MeshMaterial2d};
-use bevy_app::{App, Plugin, Update};
+use crate::MeshMaterial2d;
+use bevy_app::{App, Plugin, PreUpdate};
 use bevy_asset::{Assets, Handle, RenderAssetUsages};
 use bevy_derive::{Deref, DerefMut};
 use bevy_ecs::{
-    component::Component,
+    component::{Component, HookContext},
     entity::Entity,
-    observer::Trigger,
+    hierarchy::ChildOf,
+    name::Name,
     query::Changed,
+    relationship::{Relationship, RelationshipHookMode, RelationshipTarget},
     resource::Resource,
+    schedule::IntoScheduleConfigs,
     system::{Commands, Query, ResMut},
-    world::OnAdd,
+    world::DeferredWorld,
 };
 use bevy_image::{Image, ImageSampler};
 use bevy_math::{FloatOrd, UVec2, Vec2, Vec3};
-use bevy_platform::collections::HashMap;
+use bevy_platform::collections::{hash_map::Entry, HashMap, HashSet};
 use bevy_render::{
     mesh::{Indices, Mesh, Mesh2d, PrimitiveTopology},
     render_resource::{
         Extent3d, TextureDescriptor, TextureDimension, TextureFormat, TextureUsages,
     },
+    view::ViewVisibility,
 };
+use bevy_transform::components::Transform;
+use lettuces::IVec2;
 use tracing::warn;
 
-use super::{TilemapChunkMaterial, ATTRIBUTE_TILE_INDEX};
+use super::{TileStorage, Tilemap, TilemapChunkMaterial, Tileset, ATTRIBUTE_TILE_INDEX};
 
 /// Plugin that handles the initialization and updating of tilemap chunks.
 /// Adds systems for processing newly added tilemap chunks and updating their indices.
@@ -30,13 +36,14 @@ pub struct TilemapChunkPlugin;
 
 impl Plugin for TilemapChunkPlugin {
     fn build(&self, app: &mut App) {
-        app.init_resource::<TilemapChunkMeshCache>()
-            .add_observer(on_add_tilemap_chunk)
-            .add_systems(Update, update_tilemap_chunk_indices);
+        app.init_resource::<TilemapChunkMeshCache>().add_systems(
+            PreUpdate,
+            (spawn_missing_tilemap_chunks, update_visible_tilemap_chunks).chain(),
+        );
     }
 }
 
-type TilemapChunkMeshCacheKey = (UVec2, FloatOrd, FloatOrd, FloatOrd, FloatOrd);
+type TilemapChunkMeshCacheKey = (UVec2, FloatOrd, FloatOrd);
 
 /// A resource storing the meshes for each tilemap chunk size.
 #[derive(Resource, Default, Deref, DerefMut)]
@@ -44,154 +51,252 @@ pub struct TilemapChunkMeshCache(HashMap<TilemapChunkMeshCacheKey, Handle<Mesh>>
 
 /// A component representing a chunk of a tilemap.
 /// Each chunk is a rectangular section of tiles that is rendered as a single mesh.
-#[derive(Component, Clone, Debug, Default)]
-#[require(Mesh2d, MeshMaterial2d<TilemapChunkMaterial>, TilemapChunkIndices, Anchor)]
+#[derive(Component, Clone, Debug)]
+#[component(
+    immutable,
+    on_insert = <Self as Relationship>::on_insert,
+    on_replace = <Self as Relationship>::on_replace,
+)]
+#[require(Mesh2d, MeshMaterial2d<TilemapChunkMaterial>)]
 pub struct TilemapChunk {
-    /// The size of the chunk in tiles
-    pub chunk_size: UVec2,
-    /// The size to use for each tile, not to be confused with the size of a tile in the tileset image.
-    /// The size of the tile in the tileset image is determined by the tileset image's dimensions.
-    pub tile_display_size: UVec2,
-    /// Handle to the tileset image containing all tile textures
-    pub tileset: Handle<Image>,
-    /// The alpha mode to use for the tilemap chunk
-    pub alpha_mode: AlphaMode2d,
+    tilemap: Entity,
+    location: IVec2,
 }
 
-/// Component storing the indices of tiles within a chunk.
-/// Each index corresponds to a specific tile in the tileset.
-#[derive(Component, Clone, Debug, Default, Deref, DerefMut)]
-pub struct TilemapChunkIndices(pub Vec<Option<u16>>);
+impl Relationship for TilemapChunk {
+    type RelationshipTarget = Tilemap;
 
-fn on_add_tilemap_chunk(
-    trigger: Trigger<OnAdd, TilemapChunk>,
-    tilemap_chunk_query: Query<(&TilemapChunk, &TilemapChunkIndices, &Anchor)>,
-    mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<TilemapChunkMaterial>>,
-    mut images: ResMut<Assets<Image>>,
-    mut tilemap_chunk_mesh_cache: ResMut<TilemapChunkMeshCache>,
-) {
-    let chunk_entity = trigger.target();
-    let Ok((
-        TilemapChunk {
-            chunk_size,
-            tile_display_size,
-            tileset,
-            alpha_mode,
-        },
-        indices,
-        anchor,
-    )) = tilemap_chunk_query.get(chunk_entity)
-    else {
-        warn!("Tilemap chunk {} not found", chunk_entity);
-        return;
-    };
-
-    let expected_indices_length = chunk_size.element_product() as usize;
-    if indices.len() != expected_indices_length {
-        warn!(
-            "Invalid indices length for tilemap chunk {} of size {}. Expected {}, got {}",
-            chunk_entity,
-            chunk_size,
-            indices.len(),
-            expected_indices_length
-        );
-        return;
+    fn from(tilemap: Entity) -> Self {
+        Self {
+            tilemap,
+            location: IVec2::ZERO,
+        }
     }
 
-    let indices_image = make_chunk_image(chunk_size, &indices.0);
+    fn get(&self) -> Entity {
+        self.tilemap
+    }
 
-    let display_size = (chunk_size * tile_display_size).as_vec2();
-
-    let mesh_key: TilemapChunkMeshCacheKey = (
-        *chunk_size,
-        FloatOrd(display_size.x),
-        FloatOrd(display_size.y),
-        FloatOrd(anchor.as_vec().x),
-        FloatOrd(anchor.as_vec().y),
-    );
-
-    let mesh = tilemap_chunk_mesh_cache
-        .entry(mesh_key)
-        .or_insert_with(|| meshes.add(make_chunk_mesh(chunk_size, &display_size, anchor)));
-
-    commands.entity(chunk_entity).insert((
-        Mesh2d(mesh.clone()),
-        MeshMaterial2d(materials.add(TilemapChunkMaterial {
-            tileset: tileset.clone(),
-            indices: images.add(indices_image),
-            alpha_mode: *alpha_mode,
-        })),
-    ));
-}
-
-fn update_tilemap_chunk_indices(
-    query: Query<
-        (
-            Entity,
-            &TilemapChunk,
-            &TilemapChunkIndices,
-            &MeshMaterial2d<TilemapChunkMaterial>,
-        ),
-        Changed<TilemapChunkIndices>,
-    >,
-    mut materials: ResMut<Assets<TilemapChunkMaterial>>,
-    mut images: ResMut<Assets<Image>>,
-) {
-    for (chunk_entity, TilemapChunk { chunk_size, .. }, indices, material) in query {
-        let expected_indices_length = chunk_size.element_product() as usize;
-        if indices.len() != expected_indices_length {
-            warn!(
-                "Invalid TilemapChunkIndices length for tilemap chunk {} of size {}. Expected {}, got {}",
-                chunk_entity,
-                chunk_size,
-                indices.len(),
-                expected_indices_length
-            );
-            continue;
+    fn on_insert(
+        mut world: DeferredWorld,
+        HookContext {
+            entity,
+            caller,
+            relationship_hook_mode,
+            ..
+        }: HookContext,
+    ) {
+        match relationship_hook_mode {
+            RelationshipHookMode::Run => {}
+            RelationshipHookMode::Skip => return,
+            RelationshipHookMode::RunIfNotLinked => {
+                if <Self::RelationshipTarget as RelationshipTarget>::LINKED_SPAWN {
+                    return;
+                }
+            }
         }
 
-        let Some(material) = materials.get_mut(material.id()) else {
+        let &TilemapChunk {
+            tilemap: tilemap_entity,
+            location,
+        } = world.get(entity).unwrap();
+
+        if tilemap_entity == entity {
             warn!(
-                "TilemapChunkMaterial not found for tilemap chunk {}",
-                chunk_entity
+                "{}The {}{{ tilemap: {tilemap_entity}, location: {location} }} relationship on entity {entity} points to itself. The invalid {} relationship has been removed.",
+                caller
+                    .map(|location| format!("{location}: "))
+                    .unwrap_or_default(),
+                core::any::type_name::<Self>(),
+                core::any::type_name::<Self>()
             );
-            continue;
-        };
-        let Some(indices_image) = images.get_mut(&material.indices) else {
+            world.commands().entity(entity).remove::<Self>();
+            return;
+        }
+
+        let Ok(mut tilemap_entity_mut) = world.get_entity_mut(tilemap_entity) else {
             warn!(
-                "TilemapChunkMaterial indices image not found for tilemap chunk {}",
-                chunk_entity
+                "{}The {}{{ tilemap: {tilemap_entity}, location: {location} }} relationship on entity {entity} relates to an entity that does not exist. The invalid {} relationship has been removed.",
+                caller
+                    .map(|location| format!("{location}: "))
+                    .unwrap_or_default(),
+                core::any::type_name::<Self>(),
+                core::any::type_name::<Self>()
             );
-            continue;
+            world.commands().entity(entity).remove::<Self>();
+            return;
         };
-        let Some(data) = indices_image.data.as_mut() else {
-            warn!(
-                "TilemapChunkMaterial indices image data not found for tilemap chunk {}",
-                chunk_entity
-            );
-            continue;
-        };
-        data.clear();
-        data.extend(
-            indices
-                .iter()
-                .copied()
-                .flat_map(|i| u16::to_ne_bytes(i.unwrap_or(u16::MAX))),
-        );
+
+        if let Some(mut tilemap) = tilemap_entity_mut.get_mut::<Tilemap>() {
+            match tilemap.chunks.entry(location) {
+                Entry::Occupied(e) => {
+                    warn!(
+                        "{}The {}{{ tilemap: {tilemap_entity}, location: {location} }} relationship on entity {entity} relates to a location that is already occupied by {}. The invalid {} relationship has been removed.",
+                        caller
+                            .map(|location| format!("{location}: "))
+                            .unwrap_or_default(),
+                        core::any::type_name::<Self>(),
+                        e.get(),
+                        core::any::type_name::<Self>(),
+                    );
+                    world.commands().entity(entity).remove::<Self>();
+                }
+                Entry::Vacant(e) => {
+                    e.insert(entity);
+                }
+            }
+        } else {
+            let mut tilemap = Tilemap::default();
+            tilemap.chunks.insert(location, entity);
+            world.commands().entity(tilemap_entity).insert(tilemap);
+        }
+    }
+
+    fn on_replace(
+        mut world: DeferredWorld,
+        HookContext {
+            entity,
+            relationship_hook_mode,
+            ..
+        }: HookContext,
+    ) {
+        match relationship_hook_mode {
+            RelationshipHookMode::Run => {}
+            RelationshipHookMode::Skip => return,
+            RelationshipHookMode::RunIfNotLinked => {
+                if <Self::RelationshipTarget as RelationshipTarget>::LINKED_SPAWN {
+                    return;
+                }
+            }
+        }
+
+        let &TilemapChunk { tilemap, location } = world.get(entity).unwrap();
+
+        if let Some(mut tilemap) = world.get_mut::<Tilemap>(tilemap) {
+            if let Entry::Occupied(e) = tilemap.chunks.entry(location) {
+                if *e.get() == entity {
+                    e.remove();
+                }
+            }
+        }
     }
 }
 
-fn make_chunk_image(size: &UVec2, indices: &[Option<u16>]) -> Image {
+fn spawn_missing_tilemap_chunks(
+    tilemap_query: Query<(Entity, &Tilemap, &TileStorage, &Tileset), Changed<TileStorage>>,
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut tilemap_chunk_mesh_cache: ResMut<TilemapChunkMeshCache>,
+) {
+    for (tilemap_entity, tilemap, tile_storage, tileset) in tilemap_query {
+        let chunk_size = tile_storage.chunk_size();
+        let display_size = (chunk_size * tileset.tile_size).as_vec2();
+
+        for chunk_position in tile_storage
+            .iter_dirty_chunk_positions()
+            .filter(|pos| !tilemap.chunks.contains_key(*pos))
+        {
+            let chunk_world_position = chunk_position.as_vec2() * display_size;
+
+            let mesh_key: TilemapChunkMeshCacheKey = (
+                chunk_size,
+                FloatOrd(display_size.x),
+                FloatOrd(display_size.y),
+            );
+
+            let mesh = tilemap_chunk_mesh_cache
+                .entry(mesh_key)
+                .or_insert_with(|| meshes.add(make_chunk_mesh(&chunk_size, &display_size)));
+
+            commands.spawn((
+                Name::new(format!("TilemapChunk: {chunk_position}")),
+                TilemapChunk {
+                    tilemap: tilemap_entity,
+                    location: *chunk_position,
+                },
+                Transform::from_translation(chunk_world_position.extend(0.0)),
+                Mesh2d(mesh.clone()),
+                ChildOf(tilemap_entity),
+            ));
+        }
+    }
+}
+
+fn update_visible_tilemap_chunks(
+    tilemap_query: Query<(Entity, &Tilemap, &mut TileStorage, &Tileset)>,
+    mut chunk_query: Query<(
+        Entity,
+        &TilemapChunk,
+        &mut MeshMaterial2d<TilemapChunkMaterial>,
+        &ViewVisibility,
+    )>,
+    mut chunk_materials: ResMut<Assets<TilemapChunkMaterial>>,
+    mut images: ResMut<Assets<Image>>,
+) {
+    for (tilemap_entity, tilemap, mut tile_storage, tileset) in tilemap_query {
+        let mut chunk_positions_to_clear = HashSet::new();
+        for chunk_pos in tile_storage.iter_dirty_chunk_positions() {
+            let Some(chunk_entity) = tilemap.chunks.get(chunk_pos) else {
+                continue;
+            };
+            let Ok((chunk_entity, chunk, mut chunk_material, visibility)) =
+                chunk_query.get_mut(*chunk_entity)
+            else {
+                continue;
+            };
+            if !visibility.get() {
+                continue;
+            }
+
+            chunk_positions_to_clear.insert(chunk.location);
+
+            let chunk_size = tile_storage.chunk_size();
+            let Ok(chunk_tiles) = tile_storage.chunk_tiles(chunk.location) else {
+                warn!(
+                    "Unable to access TileStorage data for tilemap chunk {} in tilemap {}",
+                    chunk_entity, tilemap_entity
+                );
+                continue;
+            };
+
+            let indices = chunk_tiles.iter().copied().flat_map(|tile| {
+                u16::to_ne_bytes(tile.map(|tile| tile.tileset_index).unwrap_or(u16::MAX))
+            });
+
+            if let Some(material) = chunk_materials.get_mut(chunk_material.id()) {
+                let Some(chunk_image) = images.get_mut(&material.indices) else {
+                    return;
+                };
+                let Some(data) = chunk_image.data.as_mut() else {
+                    warn!(
+                    "TilemapChunkMaterial indices image data not found for tilemap chunk {} in tilemap {}",
+                    chunk_entity,
+                    tilemap_entity
+                );
+                    return;
+                };
+                data.clear();
+                data.extend(indices);
+            } else {
+                let chunk_image = make_chunk_image(&chunk_size, indices.collect::<Vec<_>>());
+
+                let material = chunk_materials.add(TilemapChunkMaterial {
+                    alpha_mode: tilemap.alpha_mode,
+                    tileset: tileset.image.clone(),
+                    indices: images.add(chunk_image),
+                });
+
+                *chunk_material = MeshMaterial2d(material);
+            }
+        }
+
+        tile_storage.clear_dirty_chunk_positions(chunk_positions_to_clear);
+    }
+}
+
+fn make_chunk_image(size: &UVec2, data: Vec<u8>) -> Image {
     Image {
-        data: Some(
-            indices
-                .iter()
-                .copied()
-                .flat_map(|i| u16::to_ne_bytes(i.unwrap_or(u16::MAX)))
-                .collect(),
-        ),
+        data: Some(data),
         texture_descriptor: TextureDescriptor {
             size: Extent3d {
                 width: size.x,
@@ -212,13 +317,11 @@ fn make_chunk_image(size: &UVec2, indices: &[Option<u16>]) -> Image {
     }
 }
 
-fn make_chunk_mesh(size: &UVec2, display_size: &Vec2, anchor: &Anchor) -> Mesh {
+fn make_chunk_mesh(size: &UVec2, display_size: &Vec2) -> Mesh {
     let mut mesh = Mesh::new(
         PrimitiveTopology::TriangleList,
         RenderAssetUsages::RENDER_WORLD | RenderAssetUsages::MAIN_WORLD,
     );
-
-    let offset = display_size * (Vec2::splat(-0.5) - anchor.as_vec());
 
     let num_quads = size.element_product() as usize;
     let quad_size = display_size / size.as_vec2();
@@ -231,7 +334,7 @@ fn make_chunk_mesh(size: &UVec2, display_size: &Vec2, anchor: &Anchor) -> Mesh {
         for x in 0..size.x {
             let i = positions.len() as u32;
 
-            let p0 = offset + quad_size * UVec2::new(x, y).as_vec2();
+            let p0 = quad_size * UVec2::new(x, y).as_vec2();
             let p1 = p0 + quad_size;
 
             positions.extend([
