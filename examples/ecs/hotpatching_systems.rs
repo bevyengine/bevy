bc4aaa169c66dbd97eba243c5b9d6dde29613367
François Mockers <francois.mockers@vleue.com>
2025-05-20 22:38:03 +0200

    example on how to patch something out of the ecs

--- examples/ecs/hotpatching_systems.rs
+++ examples/ecs/hotpatching_systems.rs
@@ -9,11 +9,27 @@
 //! `on_click` system, and those changes will be hotpatched into the running
 //! application.
 
+use std::time::Duration;
+
 use bevy::{color::palettes, prelude::*};
 
 fn main() {
+    let (sender, receiver) = crossbeam_channel::unbounded::<()>();
+
+    std::thread::spawn(move || {
+        while receiver.recv().is_ok() {
+            let start = bevy::platform::time::Instant::now();
+            // You can also make any part outside of a system hot patchable by wrapping it
+            // In this part, only the duration is hot patchable:
+            let duration = bevy::dev_tools::hotpatch::call(|| Duration::from_secs(2));
+            std::thread::sleep(duration);
+            info!("done after {:?}", start.elapsed());
+        }
+    });
+
     App::new()
         .add_plugins(DefaultPlugins)
+        .insert_resource(TaskSender(sender))
         .add_systems(Startup, setup)
         .add_systems(Update, update_text)
         .run();
@@ -23,9 +39,18 @@ fn update_text(mut text: Single<&mut Text>) {
     text.0 = "before".to_string();
 }
 
-fn on_click(_click: Trigger<Pointer<Click>>, mut color: Single<&mut TextColor>) {
+fn on_click(
+    _click: Trigger<Pointer<Click>>,
+    mut color: Single<&mut TextColor>,
+    task_sender: Res<TaskSender>,
+) {
     color.0 = palettes::tailwind::RED_600.into();
+
+    let _ = task_sender.0.send(());
 }
+
+#[derive(Resource)]
+struct TaskSender(crossbeam_channel::Sender<()>);
 
 fn setup(mut commands: Commands) {
     commands.spawn(Camera2d);
