7c6166356f1ac690843b276e88a673b1043405e9
Lucas Farias <lucasfnf@gmail.com>
2025-05-24 18:25:38 -0300

    Improve `compile_fail`

--- tools/ci/src/commands/compile_fail.rs
+++ tools/ci/src/commands/compile_fail.rs
@@ -10,17 +10,19 @@ pub struct CompileFailCommand {}
 impl Prepare for CompileFailCommand {
     fn prepare<'a>(&self, sh: &'a xshell::Shell, args: Args) -> Vec<PreparedCommand<'a>> {
         let no_fail_fast = args.keep_going();
+        let jobs = args.build_jobs();
+        let test_threads = args.test_threads();
+        let jobs_ref = jobs.as_ref();
+        let test_threads_ref = test_threads.as_ref();
 
         let mut commands = vec![];
 
         // Macro Compile Fail Tests
         // Run tests (they do not get executed with the workspace tests)
         // - See crates/bevy_macros_compile_fail_tests/README.md
-        let jobs = args.build_jobs();
-        let test_threads = args.test_threads();
         commands.push(
             PreparedCommand::new::<Self>(
-                cmd!(sh, "cargo test --target-dir ../../../target {no_fail_fast...} {jobs...} -- {test_threads...}"),
+                cmd!(sh, "cargo test --target-dir ../../../target {no_fail_fast...} {jobs_ref...} -- {test_threads_ref...}"),
                 "Compiler errors of the macros compile fail tests seem to be different than expected! Check locally and compare rust versions.",
             )
             .with_subdir("crates/bevy_derive/compile_fail"),
@@ -29,11 +31,9 @@ impl Prepare for CompileFailCommand {
         // ECS Compile Fail Tests
         // Run UI tests (they do not get executed with the workspace tests)
         // - See crates/bevy_ecs_compile_fail_tests/README.md
-        let jobs = args.build_jobs();
-        let test_threads = args.test_threads();
         commands.push(
             PreparedCommand::new::<Self>(
-                cmd!(sh, "cargo test --target-dir ../../../target {no_fail_fast...} {jobs...} -- {test_threads...}"),
+                cmd!(sh, "cargo test --target-dir ../../../target {no_fail_fast...} {jobs_ref...} -- {test_threads_ref...}"),
                 "Compiler errors of the ECS compile fail tests seem to be different than expected! Check locally and compare rust versions.",
             )
             .with_subdir("crates/bevy_ecs/compile_fail"),
@@ -42,8 +42,6 @@ impl Prepare for CompileFailCommand {
         // Reflect Compile Fail Tests
         // Run tests (they do not get executed with the workspace tests)
         // - See crates/bevy_reflect_compile_fail_tests/README.md
-        let jobs = args.build_jobs();
-        let test_threads = args.test_threads();
         commands.push(
             PreparedCommand::new::<Self>(
                 cmd!(sh, "cargo test --target-dir ../../../target {no_fail_fast...} {jobs...} -- {test_threads...}"),
