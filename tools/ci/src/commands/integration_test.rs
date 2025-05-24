9dfe148c7bc0b432c7b54b514df60860d6a4d1f8
Lucas Farias <lucasfnf@gmail.com>
2025-05-24 18:29:34 -0300

    Fix `integration_test`

--- tools/ci/src/commands/integration_test.rs
+++ tools/ci/src/commands/integration_test.rs
@@ -10,6 +10,10 @@ pub struct IntegrationTestCommand {}
 impl Prepare for IntegrationTestCommand {
     fn prepare<'a>(&self, sh: &'a xshell::Shell, args: Args) -> Vec<PreparedCommand<'a>> {
         let no_fail_fast = args.keep_going();
+        let jobs = args.build_jobs();
+        let test_threads = args.test_threads();
+        let jobs_ref = jobs.as_ref();
+        let test_threads_ref = test_threads.as_ref();
 
         get_integration_tests(sh)
             .into_iter()
@@ -17,7 +21,7 @@ impl Prepare for IntegrationTestCommand {
                 PreparedCommand::new::<Self>(
                     cmd!(
                         sh,
-                        "cargo test --manifest-path {path}/Cargo.toml --tests {no_fail_fast...}"
+                        "cargo test --manifest-path {path}/Cargo.toml --tests {no_fail_fast...} {jobs_ref...} -- {test_threads_ref...}"
                     ),
                     "Please fix failing integration tests in output above.",
                 )
