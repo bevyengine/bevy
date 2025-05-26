d79efada3b68d07963bd6978abbd27a4d5a397f7
Griffin <33357138+DGriffin91@users.noreply.github.com>
2025-05-26 11:00:33 -0700

    Optional explicit compressed image format support (#19190)
    # Objective
    
    - Allow compressed image formats to be used with `ImagePlugin` and
    `GltfPlugin` in cases where there is no `RenderDevice` resource. (For
    example, when using a custom render backend)
    
    ## Solution
    
    - Define a `CompressedImageFormatSupport` component that allows the user
    to explicitly determine which formats are supported.
    
    ~~Not sure if this is the best solution. Alternatively, I considered
    initializing CompressedImageFormatSupport from render device features
    separately, it would need to run after the render device is initialized
    but before `ImagePlugin` and `GltfPlugin` finish. Not sure where the
    best place for that to happen would be.~~
    
    Update: decided on going with @greeble-dev solution: defining the
    `CompressedImageFormatSupport` resource in `bevy_image`, but letting
    `bevy_render` register the resource value.

--- crates/bevy_image/src/image.rs
+++ crates/bevy_image/src/image.rs
@@ -11,6 +11,7 @@ use bevy_reflect::{std_traits::ReflectDefault, Reflect};
 
 use bevy_asset::{Asset, RenderAssetUsages};
 use bevy_color::{Color, ColorToComponents, Gray, LinearRgba, Srgba, Xyza};
+use bevy_ecs::resource::Resource;
 use bevy_math::{AspectRatio, UVec2, UVec3, Vec2};
 use core::hash::Hash;
 use serde::{Deserialize, Serialize};
@@ -1647,6 +1648,12 @@ impl CompressedImageFormats {
         }
     }
 }
+
+/// For defining which compressed image formats are supported. This will be initialized from available device features
+/// in `finish()` of the bevy `RenderPlugin`, but is left for the user to specify if not using the `RenderPlugin`, or
+/// the WGPU backend.
+#[derive(Resource)]
+pub struct CompressedImageFormatSupport(pub CompressedImageFormats);
 
 #[cfg(test)]
 mod test {
