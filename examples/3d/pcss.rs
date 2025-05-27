8368effba10bd4b52af340ed4fd435beda4f8075
JMS55 <47158642+JMS55@users.noreply.github.com>
2025-05-27 12:42:44 -0400

    Remove import

--- examples/3d/pcss.rs
+++ examples/3d/pcss.rs
@@ -3,7 +3,7 @@
 use std::f32::consts::PI;
 
 use bevy::{
-    anti_aliasing::taa::{TemporalAntiAliasPlugin, TemporalAntiAliasing},
+    anti_aliasing::taa::TemporalAntiAliasing,
     core_pipeline::{
         prepass::{DepthPrepass, MotionVectorPrepass},
         Skybox,
