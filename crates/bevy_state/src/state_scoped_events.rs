b7118e5aad3d2304c65b37bc3c5885175063a5f0
AlephCubed <jothebro500@gmail.com>
2025-05-27 12:55:04 -0700

    Changed to `clear_event_on_exit_state`, which no longer adds the event.
    Also added `clear_event_on_enter_state`.

--- crates/bevy_state/src/state_scoped_events.rs
+++ crates/bevy_state/src/state_scoped_events.rs
@@ -18,21 +18,33 @@ fn clear_event_queue<E: Event>(w: &mut World) {
     }
 }
 
-#[derive(Resource)]
-struct ClearEventsOnExitState<S: States> {
-    cleanup_fns: HashMap<S, Vec<fn(&mut World)>>,
+#[derive(Copy, Clone)]
+enum TransitionType {
+    OnExit,
+    OnEnter,
 }
 
-impl<S: States> ClearEventsOnExitState<S> {
-    fn add_event<E: Event>(&mut self, state: S) {
-        self.cleanup_fns
-            .entry(state)
-            .or_default()
-            .push(clear_event_queue::<E>);
+#[derive(Resource)]
+struct ClearEventsOnStateTransition<S: States> {
+    on_exit: HashMap<S, Vec<fn(&mut World)>>,
+    on_enter: HashMap<S, Vec<fn(&mut World)>>,
+}
+
+impl<S: States> ClearEventsOnStateTransition<S> {
+    fn add_event<E: Event>(&mut self, state: S, transition_type: TransitionType) {
+        let map = match transition_type {
+            TransitionType::OnExit => &mut self.on_exit,
+            TransitionType::OnEnter => &mut self.on_enter,
+        };
+        map.entry(state).or_default().push(clear_event_queue::<E>);
     }
 
-    fn cleanup(&self, w: &mut World, state: S) {
-        let Some(fns) = self.cleanup_fns.get(&state) else {
+    fn cleanup(&self, w: &mut World, state: S, transition_type: TransitionType) {
+        let map = match transition_type {
+            TransitionType::OnExit => &self.on_exit,
+            TransitionType::OnEnter => &self.on_enter,
+        };
+        let Some(fns) = map.get(&state) else {
             return;
         };
         for callback in fns {
@@ -41,16 +53,15 @@ impl<S: States> ClearEventsOnExitState<S> {
     }
 }
 
-impl<S: States> Default for ClearEventsOnExitState<S> {
+impl<S: States> Default for ClearEventsOnStateTransition<S> {
     fn default() -> Self {
         Self {
-            cleanup_fns: HashMap::default(),
+            on_exit: HashMap::default(),
+            on_enter: HashMap::default(),
         }
     }
 }
 
-/// Clears events marked with [`ClearEventsOnExitState<S>`] when their state no
-/// longer matches the world state.
 fn clear_events_on_exit_state<S: States>(
     mut c: Commands,
     mut transitions: EventReader<StateTransitionEvent<S>>,
@@ -66,48 +77,85 @@ fn clear_events_on_exit_state<S: States>(
     };
 
     c.queue(move |w: &mut World| {
-        w.resource_scope::<ClearEventsOnExitState<S>, ()>(|w, events| {
-            events.cleanup(w, exited);
+        w.resource_scope::<ClearEventsOnStateTransition<S>, ()>(|w, events| {
+            events.cleanup(w, exited, TransitionType::OnEnter);
         });
     });
 }
 
-fn add_event_cleared_on_exit_state<E: Event, S: States>(
+fn clear_events_on_enter_state<S: States>(
+    mut c: Commands,
+    mut transitions: EventReader<StateTransitionEvent<S>>,
+) {
+    let Some(transition) = transitions.read().last() else {
+        return;
+    };
+    if transition.entered == transition.exited {
+        return;
+    }
+    let Some(entered) = transition.entered.clone() else {
+        return;
+    };
+
+    c.queue(move |w: &mut World| {
+        w.resource_scope::<ClearEventsOnStateTransition<S>, ()>(|w, events| {
+            events.cleanup(w, entered, TransitionType::OnEnter);
+        });
+    });
+}
+
+fn clear_event_on_state_transition<E: Event, S: States>(
     app: &mut SubApp,
     _p: PhantomData<E>,
     state: S,
+    transition_type: TransitionType,
 ) {
-    if !app.world().contains_resource::<ClearEventsOnExitState<S>>() {
-        app.init_resource::<ClearEventsOnExitState<S>>();
+    if !app.world().contains_resource::<ClearEventsOnStateTransition<S>>() {
+        app.init_resource::<ClearEventsOnStateTransition<S>>();
     }
-    app.add_event::<E>();
     app.world_mut()
-        .resource_mut::<ClearEventsOnExitState<S>>()
-        .add_event::<E>(state.clone());
-    app.add_systems(OnExit(state), clear_events_on_exit_state::<S>);
+        .resource_mut::<ClearEventsOnStateTransition<S>>()
+        .add_event::<E>(state.clone(), transition_type);
+    app.add_systems(OnExit(state), match transition_type {
+        TransitionType::OnExit => clear_events_on_exit_state::<S>,
+        TransitionType::OnEnter => clear_events_on_enter_state::<S>,
+    });
 }
 
 /// Extension trait for [`App`] adding methods for registering state scoped events.
 pub trait StateScopedEventsAppExt {
-    /// Adds an [`Event`] that is automatically cleaned up when leaving the specified `state`.
+    /// Clears an [`Event`] when leaving the specified `state`.
     ///
     /// Note that event cleanup is ordered ambiguously relative to [`DespawnOnEnterState`](crate::prelude::DespawnOnEnterState)
     /// and [`DespawnOnExitState`](crate::prelude::DespawnOnExitState) entity
     /// cleanup and the [`OnExit`] schedule for the target state. All of these (state scoped
     /// entities and events cleanup, and `OnExit`) occur within schedule [`StateTransition`](crate::prelude::StateTransition).
-    fn add_event_cleared_on_exit_state<E: Event>(&mut self, state: impl States) -> &mut Self;
+    fn clear_event_on_exit_state<E: Event>(&mut self, state: impl States) -> &mut Self;
+
+    /// Clears an [`Event`] when leaving the specified `state`.
+    fn clear_event_on_enter_state<E: Event>(&mut self, state: impl States) -> &mut Self;
 }
 
 impl StateScopedEventsAppExt for App {
-    fn add_event_cleared_on_exit_state<E: Event>(&mut self, state: impl States) -> &mut Self {
-        add_event_cleared_on_exit_state(self.main_mut(), PhantomData::<E>, state);
+    fn clear_event_on_exit_state<E: Event>(&mut self, state: impl States) -> &mut Self {
+        clear_event_on_state_transition(self.main_mut(), PhantomData::<E>, state, TransitionType::OnExit);
+        self
+    }
+
+    fn clear_event_on_enter_state<E: Event>(&mut self, state: impl States) -> &mut Self {
+        clear_event_on_state_transition(self.main_mut(), PhantomData::<E>, state, TransitionType::OnEnter);
         self
     }
 }
 
 impl StateScopedEventsAppExt for SubApp {
-    fn add_event_cleared_on_exit_state<E: Event>(&mut self, state: impl States) -> &mut Self {
-        add_event_cleared_on_exit_state(self, PhantomData::<E>, state);
+    fn clear_event_on_exit_state<E: Event>(&mut self, state: impl States) -> &mut Self {
+        clear_event_on_state_transition(self, PhantomData::<E>, state, TransitionType::OnExit);
+        self
+    }
+
+    fn clear_event_on_enter_state<E: Event>(&mut self, state: impl States) -> &mut Self {
+        clear_event_on_state_transition(self, PhantomData::<E>, state, TransitionType::OnEnter);
         self
     }
 }
