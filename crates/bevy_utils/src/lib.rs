1732c2253bbefd55cc414dcc1de9ffafd1bf91f5
atlv <email@atlasdostal.com>
2025-05-26 23:43:49 -0400

    refactor(render): move WgpuWrapper into bevy_utils (#19303)
    # Objective
    
    - A step towards splitting out bevy_camera from bevy_render
    
    ## Solution
    
    - Move a shim type into bevy_utils to avoid a dependency cycle
    - Manually expand Deref/DerefMut to avoid having a bevy_derive
    dependency so early in the dep tree
    
    ## Testing
    
    - It compiles

--- crates/bevy_utils/src/lib.rs
+++ crates/bevy_utils/src/lib.rs
@@ -48,6 +48,8 @@ pub mod prelude {
 
 pub mod synccell;
 pub mod syncunsafecell;
+#[cfg(feature = "wgpu_wrapper")]
+mod wgpu_wrapper;
 
 mod default;
 mod once;
@@ -56,6 +58,9 @@ mod once;
 pub use once::OnceFlag;
 
 pub use default::default;
+
+#[cfg(feature = "wgpu_wrapper")]
+pub use wgpu_wrapper::WgpuWrapper;
 
 use core::mem::ManuallyDrop;
 
