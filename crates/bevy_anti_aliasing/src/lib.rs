8255e6cda98ecba2972745bd5ecf0ac7db02d315
JMS55 <47158642+JMS55@users.noreply.github.com>
2025-06-02 09:04:08 -0700

    Make TAA non-experimental, fixes (#18349)
    The first 4 commits are designed to be reviewed independently.
    
    - Mark TAA non-experimental now that motion vectors are written for
    skinned and morphed meshes, along with skyboxes, and add it to
    DefaultPlugins
    - Adjust halton sequence to match what DLSS is going to use, doesn't
    really affect anything, but may as well
    - Make MipBias a required component on TAA instead of inserting it in
    the render world
    - Remove MipBias, TemporalJitter, RenderLayers, etc from the render
    world if they're removed from the main world (fixes a retained render
    world bug)
    - Remove TAA components from the render world properly if
    TemporalAntiAliasing is removed from the main world (fixes a retained
    render world bug)
    - extract_taa_settings() now has to query over `Option<&mut
    TemporalAntiAliasing>`, which will match every single camera, in order
    to cover cameras that had TemporalAntiAliasing removed this frame. This
    kind of sucks, but I can't think of anything better.
    - We probably have the same bug with every other rendering feature
    component we have.
    
    ---------
    
    Co-authored-by: Alice Cecile <alice.i.cecile@gmail.com>

--- crates/bevy_anti_aliasing/src/lib.rs
+++ crates/bevy_anti_aliasing/src/lib.rs
@@ -10,18 +10,17 @@ use bevy_app::Plugin;
 use contrast_adaptive_sharpening::CasPlugin;
 use fxaa::FxaaPlugin;
 use smaa::SmaaPlugin;
+use taa::TemporalAntiAliasPlugin;
 
 pub mod contrast_adaptive_sharpening;
-pub mod experimental;
 pub mod fxaa;
 pub mod smaa;
-
-mod taa;
+pub mod taa;
 
 #[derive(Default)]
 pub struct AntiAliasingPlugin;
 impl Plugin for AntiAliasingPlugin {
     fn build(&self, app: &mut bevy_app::App) {
-        app.add_plugins((FxaaPlugin, CasPlugin, SmaaPlugin));
+        app.add_plugins((FxaaPlugin, SmaaPlugin, TemporalAntiAliasPlugin, CasPlugin));
     }
 }
