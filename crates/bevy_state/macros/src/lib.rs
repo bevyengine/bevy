7d90fe13320a04f20358a739feaef26e4a45444e
Phil Ellison <phil.j.ellison@gmail.com>
2025-06-04 21:16:33 +0100

    deny(missing_docs) for bevy_state

--- crates/bevy_state/macros/src/lib.rs
+++ crates/bevy_state/macros/src/lib.rs
@@ -1,5 +1,6 @@
-#![expect(missing_docs, reason = "Not all docs are written yet, see #3492.")]
 #![cfg_attr(docsrs, feature(doc_auto_cfg))]
+
+//! Macros for deriving [`States`] and [`SubStates`] traits.
 
 extern crate proc_macro;
 
@@ -8,11 +9,15 @@ mod states;
 use bevy_macro_utils::BevyManifest;
 use proc_macro::TokenStream;
 
+/// Implements the [`States`] trait for a type - see the trait
+/// docs for an example usage.
 #[proc_macro_derive(States, attributes(states))]
 pub fn derive_states(input: TokenStream) -> TokenStream {
     states::derive_states(input)
 }
 
+/// Implements the [`SubStates`] trait for a type - see the trait
+/// docs for an example usage.
 #[proc_macro_derive(SubStates, attributes(states, source))]
 pub fn derive_substates(input: TokenStream) -> TokenStream {
     states::derive_substates(input)
