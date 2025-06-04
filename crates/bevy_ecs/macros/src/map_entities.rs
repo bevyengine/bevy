4a65741e607dc90a00ca315cca71a8fc8c5b1bb8
Matthias Wuketich <matthias.wuketich@bnet.at>
2025-06-04 12:14:13 +0200

    simplified code + improved documentation

--- crates/bevy_ecs/macros/src/map_entities.rs
+++ crates/bevy_ecs/macros/src/map_entities.rs
@@ -17,28 +17,28 @@ fn map_struct(name: syn::Ident, fields: &Fields) -> proc_macro2::TokenStream {
     let mut map_entities = vec![];
 
     'fields: for (i, field) in fields.iter().enumerate() {
-        // skip that field
+        // check for skipping fields
         for attr in &field.attrs {
             if attr.path().is_ident("skip_mapping") {
                 match &attr.meta {
+                    // skip that fields
                     syn::Meta::Path(_) => continue 'fields,
+                    // the attribute has been used incorrectly
                     _ => panic!("just use the bare `#[skip_mapping]`"),
                 }
             }
         }
 
         let ty = &field.ty;
-        let map_field = if let Some(field_name) = &field.ident {
-            // Named field (struct)
-            quote! {
-                <#ty as bevy_ecs::entity::MapEntities>::map_entities(&mut self.#field_name, entity_mapper);
-            }
-        } else {
+        let member = match &field.ident {
+            // a named field (struct)
+            Some(field_name) => syn::Member::Named(field_name.clone()),
             // Unnamed field (tuple-like struct)
-            let idx = syn::Index::from(i);
-            quote! {
-                <#ty as bevy_ecs::entity::MapEntities>::map_entities(&mut self.#idx, entity_mapper);
-            }
+            None => syn::Member::Unnamed(syn::Index::from(i)),
+        };
+
+        let map_field = quote! {
+            <#ty as bevy_ecs::entity::MapEntities>::map_entities(&mut self.#member, entity_mapper);
         };
 
         map_entities.push(map_field);
