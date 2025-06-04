7cab789f64d36a793f0e733c5c130ca6fd82b772
Phil Ellison <phil.j.ellison@gmail.com>
2025-06-04 21:23:43 +0100

    Remove accidental internal linking

--- crates/bevy_state/macros/src/lib.rs
+++ crates/bevy_state/macros/src/lib.rs
@@ -1,6 +1,6 @@
 #![cfg_attr(docsrs, feature(doc_auto_cfg))]
 
-//! Macros for deriving [`States`] and [`SubStates`] traits.
+//! Macros for deriving `States` and `SubStates` traits.
 
 extern crate proc_macro;
 
@@ -9,14 +9,14 @@ mod states;
 use bevy_macro_utils::BevyManifest;
 use proc_macro::TokenStream;
 
-/// Implements the [`States`] trait for a type - see the trait
+/// Implements the `States` trait for a type - see the trait
 /// docs for an example usage.
 #[proc_macro_derive(States, attributes(states))]
 pub fn derive_states(input: TokenStream) -> TokenStream {
     states::derive_states(input)
 }
 
-/// Implements the [`SubStates`] trait for a type - see the trait
+/// Implements the `SubStates` trait for a type - see the trait
 /// docs for an example usage.
 #[proc_macro_derive(SubStates, attributes(states, source))]
 pub fn derive_substates(input: TokenStream) -> TokenStream {
