13bd4e3cd455ffa1ea6531481828f9719e810bcd
ickshonpe <david.curthoys@googlemail.com>
2025-06-14 16:10:48 +0100

    Added `TextInputCommand` enum Added `TextInputCommands` component that wraps a `TextInputCommand` queue.

--- crates/bevy_text/src/input/mod.rs
+++ crates/bevy_text/src/input/mod.rs
@@ -1,8 +1,13 @@
+use std::collections::VecDeque;
+
 use bevy_ecs::component::Component;
+use bevy_math::IVec2;
 use cosmic_text::Buffer;
 use cosmic_text::Editor;
 use cosmic_text::Metrics;
+use cosmic_text::Motion;
 
+/// Text input buffer
 #[derive(Component, Debug)]
 pub struct TextInputBuffer {
     pub editor: Editor<'static>,
@@ -15,3 +20,41 @@ impl Default for TextInputBuffer {
         }
     }
 }
+
+/// Text input commands queue
+#[derive(Component)]
+pub struct TextInputCommands {
+    pub commands_queue: VecDeque<TextInputCommand>,
+}
+
+/// Text input commands
+pub enum TextInputCommand {
+    Submit,
+    Copy,
+    Cut,
+    Paste,
+    /// Move the cursor with some motion
+    Motion {
+        motion: Motion,
+        select: bool,
+    },
+    Insert(char),
+    Overwrite(char),
+    Enter,
+    Backspace,
+    Delete,
+    Indent,
+    Unindent,
+    Click(IVec2),
+    DoubleClick(IVec2),
+    TripleClick(IVec2),
+    Drag(IVec2),
+    Scroll {
+        lines: i32,
+    },
+    Undo,
+    Redo,
+    SelectAll,
+}
+
+pub fn apply_text_input_commands() {}
