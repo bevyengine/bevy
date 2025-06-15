b497fd24e9d3341e474969fd0bfb112abfd43619
JMS55 <47158642+JMS55@users.noreply.github.com>
2025-06-15 12:54:18 -0400

    Use SkipDeferredLighting

--- crates/bevy_solari/src/realtime/extract.rs
+++ crates/bevy_solari/src/realtime/extract.rs
@@ -1,5 +1,6 @@
 use super::{prepare::SolariLightingResources, SolariLighting};
 use bevy_ecs::system::{Commands, ResMut};
+use bevy_pbr::deferred::SkipDeferredLighting;
 use bevy_render::{camera::Camera, sync_world::RenderEntity, MainWorld};
 
 pub fn extract_solari_lighting(mut main_world: ResMut<MainWorld>, mut commands: Commands) {
@@ -10,10 +11,17 @@ pub fn extract_solari_lighting(mut main_world: ResMut<MainWorld>, mut commands: 
             .get_entity(entity)
             .expect("Camera entity wasn't synced.");
         if solari_lighting.is_some() && camera.is_active {
-            entity_commands.insert(solari_lighting.as_deref().unwrap().clone());
+            entity_commands.insert((
+                solari_lighting.as_deref().unwrap().clone(),
+                SkipDeferredLighting,
+            ));
             solari_lighting.as_mut().unwrap().reset = false;
         } else {
-            entity_commands.remove::<(SolariLighting, SolariLightingResources)>();
+            entity_commands.remove::<(
+                SolariLighting,
+                SolariLightingResources,
+                SkipDeferredLighting,
+            )>();
         }
     }
 }
