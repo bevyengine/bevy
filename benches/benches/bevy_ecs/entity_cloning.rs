be07ccdc75222a8d47918a39158a077dc146f279
Urben1680 <urben1680@gmail.com>
2025-06-17 00:21:42 +0200

    fmt

--- benches/benches/bevy_ecs/entity_cloning.rs
+++ benches/benches/bevy_ecs/entity_cloning.rs
@@ -135,10 +135,7 @@ fn bench_clone_single_unfiltered_all<B: Bundle + Default + GetTypeRegistration>(
 
 /// A helper function that benchmarks running [`EntityCloner::clone_entity`] with[`bevy_ecs::entity::DenyAll`]
 /// filter and allowing 5 components which each have one required component.
-fn bench_single_filtered_complex_into_target(
-    b: &mut Bencher,
-    scenario: &str,
-) {
+fn bench_single_filtered_complex_into_target(b: &mut Bencher, scenario: &str) {
     let mut world = World::default();
 
     let (source, target);
