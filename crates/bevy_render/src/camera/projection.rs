2915a3b90389cc46314878facc9cbfad7a14d177
atlv <email@atlasdostal.com>
2025-06-17 14:37:26 -0400

    rename GlobalTransform::compute_matrix to to_matrix (#19643)
    # Objective
    
    - compute_matrix doesn't compute anything, it just puts an Affine3A into
    a Mat4. the name is inaccurate
    
    ## Solution
    
    - rename it to conform with to_isometry (which, ironically, does compute
    a decomposition which is rather expensive)
    
    ## Testing
    
    - Its a rename. If it compiles, its good to go
    
    ---------
    
    Co-authored-by: Alice Cecile <alice.i.cecile@gmail.com>

--- crates/bevy_render/src/camera/projection.rs
+++ crates/bevy_render/src/camera/projection.rs
@@ -93,8 +93,7 @@ pub trait CameraProjection {
     /// This code is called by [`update_frusta`](crate::view::visibility::update_frusta) system
     /// for each camera to update its frustum.
     fn compute_frustum(&self, camera_transform: &GlobalTransform) -> Frustum {
-        let clip_from_world =
-            self.get_clip_from_view() * camera_transform.compute_matrix().inverse();
+        let clip_from_world = self.get_clip_from_view() * camera_transform.to_matrix().inverse();
         Frustum::from_clip_from_world_custom_far(
             &clip_from_world,
             &camera_transform.translation(),
