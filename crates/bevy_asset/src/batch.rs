011f8ce97bc8d3025906ecffd66e5d9c280dfe07
shishanyue <62888460+shishanyue@users.noreply.github.com>
2025-06-21 09:06:31 +0800

    have a problem

--- crates/bevy_asset/src/batch.rs
+++ crates/bevy_asset/src/batch.rs
@@ -1,17 +1,23 @@
 use alloc::vec::Vec;
 
-use crate::{Asset, UntypedHandle};
+use crate::{Asset, AssetPath, UntypedHandle};
 use bevy_reflect::TypePath;
 
 pub struct LoadBatchRequest {
-    pub requests: Vec<&'static str>,
+    pub requests: Vec<AssetPath<'static>>,
 }
 
 impl LoadBatchRequest {
-    pub fn new(requests: Vec<&'static str>) -> Self {
-        Self { requests }
+    pub fn new<T>(requests: Vec<T>) -> Self
+    where
+        T:Into<AssetPath<'static>>,
+    {
+        Self {
+            requests: requests.into_iter().map(Into::into).collect(),
+        }
     }
 }
+
 /// A "loaded batch" containing handles for all assets stored in a given [`AssetPath`].
 ///
 /// This is produced by [`AssetServer::load_batch`](crate::prelude::AssetServer::load_batch).
