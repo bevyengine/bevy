64814619e974a6f92c7509ebfcd86d3830814bb1
Conner Petzold <cpetzold@gmail.com>
2025-07-01 23:32:31 -0400

    TilemapChunk single quad; TileData (color/visibility)

--- crates/bevy_sprite/src/tilemap_chunk/tilemap_chunk_material.rs
+++ crates/bevy_sprite/src/tilemap_chunk/tilemap_chunk_material.rs
@@ -1,12 +1,12 @@
-use crate::{AlphaMode2d, Material2d, Material2dKey, Material2dPlugin};
+use crate::{AlphaMode2d, Material2d, Material2dPlugin, TileData};
 use bevy_app::{App, Plugin};
-use bevy_asset::{embedded_asset, embedded_path, Asset, AssetPath, Handle};
-use bevy_image::Image;
+use bevy_asset::{embedded_asset, embedded_path, Asset, AssetPath, Handle, RenderAssetUsages};
+use bevy_color::ColorToPacked;
+use bevy_image::{Image, ImageSampler, ToExtents};
+use bevy_math::UVec2;
 use bevy_reflect::prelude::*;
-use bevy_render::{
-    mesh::{Mesh, MeshVertexBufferLayoutRef},
-    render_resource::*,
-};
+use bevy_render::render_resource::*;
+use bytemuck::{Pod, Zeroable};
 
 /// Plugin that adds support for tilemap chunk materials.
 pub struct TilemapChunkMaterialPlugin;
@@ -32,7 +32,7 @@ pub struct TilemapChunkMaterial {
     pub tileset: Handle<Image>,
 
     #[texture(2, sample_type = "u_int")]
-    pub indices: Handle<Image>,
+    pub tile_data: Handle<Image>,
 }
 
 impl Material2d for TilemapChunkMaterial {
@@ -43,27 +43,75 @@ impl Material2d for TilemapChunkMaterial {
         )
     }
 
-    fn vertex_shader() -> ShaderRef {
-        ShaderRef::Path(
-            AssetPath::from_path_buf(embedded_path!("tilemap_chunk_material.wgsl"))
-                .with_source("embedded"),
-        )
-    }
-
     fn alpha_mode(&self) -> AlphaMode2d {
         self.alpha_mode
     }
+}
 
-    fn specialize(
-        descriptor: &mut RenderPipelineDescriptor,
-        layout: &MeshVertexBufferLayoutRef,
-        _key: Material2dKey<Self>,
-    ) -> Result<(), SpecializedMeshPipelineError> {
-        let vertex_layout = layout.0.get_layout(&[
-            Mesh::ATTRIBUTE_POSITION.at_shader_location(0),
-            Mesh::ATTRIBUTE_UV_0.at_shader_location(1),
-        ])?;
-        descriptor.vertex.buffers = vec![vertex_layout];
-        Ok(())
+#[repr(C)]
+#[derive(Clone, Copy, Debug, Pod, Zeroable)]
+pub struct PackedTileData {
+    tileset_index: u16,
+    flags: u16,            // flags (visibility, etc.)
+    color_red_green: u16,  // r in low 8 bits, g in high 8 bits
+    color_blue_alpha: u16, // b in low 8 bits, a in high 8 bits
+}
+
+impl PackedTileData {
+    fn empty() -> Self {
+        Self {
+            tileset_index: u16::MAX,
+            flags: 0,
+            color_red_green: 0,
+            color_blue_alpha: 0,
+        }
+    }
+}
+
+impl From<TileData> for PackedTileData {
+    fn from(
+        TileData {
+            tileset_index,
+            visible,
+            color,
+        }: TileData,
+    ) -> Self {
+        let [r, g, b, a] = color.to_srgba().to_u8_array();
+
+        Self {
+            tileset_index,
+            flags: visible as u16,
+            color_red_green: (r as u16) | ((g as u16) << 8),
+            color_blue_alpha: (b as u16) | ((a as u16) << 8),
+        }
+    }
+}
+
+impl From<Option<TileData>> for PackedTileData {
+    fn from(maybe_tile_data: Option<TileData>) -> Self {
+        maybe_tile_data
+            .map(Into::into)
+            .unwrap_or(PackedTileData::empty())
+    }
+}
+
+pub fn make_chunk_tile_data_image(size: &UVec2, data: &[PackedTileData]) -> Image {
+    Image {
+        data: Some(bytemuck::cast_slice(data).to_vec()),
+        data_order: TextureDataOrder::default(),
+        texture_descriptor: TextureDescriptor {
+            size: size.to_extents(),
+            dimension: TextureDimension::D2,
+            format: TextureFormat::Rgba16Uint,
+            label: None,
+            mip_level_count: 1,
+            sample_count: 1,
+            usage: TextureUsages::TEXTURE_BINDING | TextureUsages::COPY_DST,
+            view_formats: &[],
+        },
+        sampler: ImageSampler::nearest(),
+        texture_view_descriptor: None,
+        asset_usage: RenderAssetUsages::RENDER_WORLD | RenderAssetUsages::MAIN_WORLD,
+        copy_on_resize: false,
     }
 }
