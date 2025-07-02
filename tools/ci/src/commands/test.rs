26a5bcfa97bfa7f01e65d9928757aef034f91a7f
Lucas Farias <lucasfnf@gmail.com>
2025-07-02 17:14:02 -0300

    Too much white space

--- tools/ci/src/commands/test.rs
+++ tools/ci/src/commands/test.rs
@@ -14,13 +14,13 @@ impl Prepare for TestCommand {
         let test_threads = args.test_threads();
 
         vec![PreparedCommand::new::<Self>(
-                cmd!(
-                    sh,
-                    // `--benches` runs each benchmark once in order to verify that they behave
-                    // correctly and do not panic.
-                    "cargo test --workspace --lib --bins --tests --benches {no_fail_fast...} {jobs...} -- {test_threads...}"
-                ),
-                "Please fix failing tests in output above.",
+            cmd!(
+                sh,
+                // `--benches` runs each benchmark once in order to verify that they behave
+                // correctly and do not panic.
+                "cargo test --workspace --lib --bins --tests --benches {no_fail_fast...} {jobs...} -- {test_threads...}"
+            ),
+            "Please fix failing tests in output above.",
         )]
     }
 }
