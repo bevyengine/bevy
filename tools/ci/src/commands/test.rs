00f06f9c51ddb15e1c6df13de4e3ff81fb601146
Lucas Farias <lucasfnf@gmail.com>
2025-07-02 19:23:00 -0300

    `cargo test --benches` does not accept `--test-threads`

--- tools/ci/src/commands/test.rs
+++ tools/ci/src/commands/test.rs
@@ -13,12 +13,19 @@ impl Prepare for TestCommand {
         let jobs = args.build_jobs();
         let test_threads = args.test_threads();
 
+        let jobs_ref = &jobs;
         vec![PreparedCommand::new::<Self>(
+            cmd!(
+                sh,
+                "cargo test --workspace --lib --bins --tests {no_fail_fast...} {jobs_ref...} -- {test_threads...}"
+            ),
+            "Please fix failing tests in output above.",
+        ),PreparedCommand::new::<Self>(
             cmd!(
                 sh,
                 // `--benches` runs each benchmark once in order to verify that they behave
                 // correctly and do not panic.
-                "cargo test --workspace --lib --bins --tests --benches {no_fail_fast...} {jobs...} -- {test_threads...}"
+                "cargo test --benches {no_fail_fast...} {jobs_ref...}"
             ),
             "Please fix failing tests in output above.",
         )]
