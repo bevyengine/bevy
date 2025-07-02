4ece338eeb1d3d718ff2667610262970ef9bd797
andriyDev <andriydzikh@gmail.com>
2025-07-02 11:32:17 -0700

    Update `take_extract` docs to use `as_mut` to call the extract fn.

--- crates/bevy_app/src/sub_app.rs
+++ crates/bevy_app/src/sub_app.rs
@@ -177,13 +177,13 @@ impl SubApp {
     /// ```
     /// # use bevy_app::SubApp;
     /// # let mut app = SubApp::new();
-    /// let default_fn = app.take_extract();
+    /// let mut default_fn = app.take_extract();
     /// app.set_extract(move |main, render| {
     ///     // Do pre-extract custom logic
     ///     // [...]
     ///
     ///     // Call Bevy's default, which executes the Extract phase
-    ///     if let Some(f) = default_fn.as_ref() {
+    ///     if let Some(f) = default_fn.as_mut() {
     ///         f(main, render);
     ///     }
     ///
