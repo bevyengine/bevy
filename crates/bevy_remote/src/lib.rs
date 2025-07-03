b0be8d365e93e8af92b7f7912ce683f48ef55810
Joe Buehler <jbuehler23@gmail.com>
2025-07-03 15:37:56 +0100

    Apply suggestion from @janhohenheim
    Co-authored-by: Jan Hohenheim <jan@hohenheim.ch>

--- crates/bevy_remote/src/lib.rs
+++ crates/bevy_remote/src/lib.rs
@@ -136,7 +136,7 @@
 //!   - `components` (optional): An array of [fully-qualified type names] of components to fetch,
 //!     see _below_ example for a query to list all the type names in **your** project.
 //!   - `option` (optional): An array of fully-qualified type names of components to fetch optionally.
-//!     to fetch all reflectable components, you can pass in the string `"All"`.
+//!     to fetch all reflectable components, you can pass in the string `"all"`.
 //!   - `has` (optional): An array of fully-qualified type names of components whose presence will be
 //!     reported as boolean values.
 //! - `filter` (optional):
