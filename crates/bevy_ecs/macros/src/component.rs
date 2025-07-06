c9916abeb34670ffef3a7ac83afbe4040cbd06bd
Giacomo Stevanato <giaco.stevanato@gmail.com>
2025-07-06 13:56:46 +0200

    Avoid useless register_component

--- crates/bevy_ecs/macros/src/component.rs
+++ crates/bevy_ecs/macros/src/component.rs
@@ -307,8 +307,7 @@ pub fn derive_component(input: TokenStream) -> TokenStream {
                 recursion_check_stack: &mut #bevy_ecs_path::__macro_exports::Vec<#bevy_ecs_path::component::ComponentId>
             ) {
                 #bevy_ecs_path::component::enforce_no_required_components_recursion(components, recursion_check_stack);
-                let self_id = components.register_component::<Self>();
-                recursion_check_stack.push(self_id);
+                recursion_check_stack.push(requiree);
                 #(#register_required)*
                 recursion_check_stack.pop();
             }
