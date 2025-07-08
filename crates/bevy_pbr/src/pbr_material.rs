6a46e238802b2b4495dfac88727ba10820f63eca
T772 <arminnessie@gmail.com>
2025-07-08 19:02:31 +0200

    Make clear that lighting won't look correct if `is_srgb` is `false`

--- crates/bevy_pbr/src/pbr_material.rs
+++ crates/bevy_pbr/src/pbr_material.rs
@@ -386,6 +386,17 @@ pub struct StandardMaterial {
     ///
     /// [`Mesh::generate_tangents`]: bevy_render::mesh::Mesh::generate_tangents
     /// [`Mesh::with_generated_tangents`]: bevy_render::mesh::Mesh::with_generated_tangents
+    ///
+    /// # Usage
+    ///
+    /// ```rust
+    /// let normal_handle = asset_server.load_with_settings(
+    ///     "textures/parallax_example/cube_normal.png",
+    ///     // The normal map texture is in linear color space. Lighting won't look correct
+    ///     // if `is_srgb` is `true`, which is the default.
+    ///     |settings: &mut ImageLoaderSettings| settings.is_srgb = false,
+    /// );
+    /// ```
     #[texture(9)]
     #[sampler(10)]
     #[dependency]
