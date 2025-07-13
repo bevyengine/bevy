e5aa94132c48788ea791321d2101e783d32dabc4
JMS55 <47158642+JMS55@users.noreply.github.com>
2025-07-13 10:23:38 -0700

    Solari initial GI (#20020)
    # Objective
    - Add 1-bounce RT GI
    
    ## Solution
    
    - Implement a very very basic version of ReSTIR GI
    https://d1qx31qr3h6wln.cloudfront.net/publications/ReSTIR%20GI.pdf
    - Pretty much a copy of the ReSTIR DI code, but adjusted for GI. 
    - Didn't implement add more spatial samples, or do anything needed for
    better quality.
    - Didn't try to improve perf at all yet (it's actually faster than DI
    though, unfortunately 😅)
    - Didn't spend any time cleaning up the shared abstractions between
    DI/GI
    
    ---
    
    ## Showcase
    <img width="2564" height="1500" alt="image"
    src="https://github.com/user-attachments/assets/1ff7be1c-7d7d-4e53-8aa6-bcec1553db3f"
    />

--- crates/bevy_solari/src/realtime/prepare.rs
+++ crates/bevy_solari/src/realtime/prepare.rs
@@ -17,14 +17,19 @@ use bevy_render::{
     renderer::RenderDevice,
 };
 
-/// Size of a Reservoir shader struct in bytes.
-const RESERVOIR_STRUCT_SIZE: u64 = 32;
+/// Size of a DI Reservoir shader struct in bytes.
+const DI_RESERVOIR_STRUCT_SIZE: u64 = 32;
+
+/// Size of a GI Reservoir shader struct in bytes.
+const GI_RESERVOIR_STRUCT_SIZE: u64 = 48;
 
 /// Internal rendering resources used for Solari lighting.
 #[derive(Component)]
 pub struct SolariLightingResources {
-    pub reservoirs_a: Buffer,
-    pub reservoirs_b: Buffer,
+    pub di_reservoirs_a: Buffer,
+    pub di_reservoirs_b: Buffer,
+    pub gi_reservoirs_a: Buffer,
+    pub gi_reservoirs_b: Buffer,
     pub previous_gbuffer: (Texture, TextureView),
     pub previous_depth: (Texture, TextureView),
     pub view_size: UVec2,
@@ -47,18 +52,30 @@ pub fn prepare_solari_lighting_resources(
             continue;
         }
 
-        let size = (view_size.x * view_size.y) as u64 * RESERVOIR_STRUCT_SIZE;
-
-        let reservoirs_a = render_device.create_buffer(&BufferDescriptor {
-            label: Some("solari_lighting_reservoirs_a"),
-            size,
+        let di_reservoirs_a = render_device.create_buffer(&BufferDescriptor {
+            label: Some("solari_lighting_di_reservoirs_a"),
+            size: (view_size.x * view_size.y) as u64 * DI_RESERVOIR_STRUCT_SIZE,
             usage: BufferUsages::STORAGE,
             mapped_at_creation: false,
         });
 
-        let reservoirs_b = render_device.create_buffer(&BufferDescriptor {
-            label: Some("solari_lighting_reservoirs_b"),
-            size,
+        let di_reservoirs_b = render_device.create_buffer(&BufferDescriptor {
+            label: Some("solari_lighting_di_reservoirs_b"),
+            size: (view_size.x * view_size.y) as u64 * DI_RESERVOIR_STRUCT_SIZE,
+            usage: BufferUsages::STORAGE,
+            mapped_at_creation: false,
+        });
+
+        let gi_reservoirs_a = render_device.create_buffer(&BufferDescriptor {
+            label: Some("solari_lighting_gi_reservoirs_a"),
+            size: (view_size.x * view_size.y) as u64 * GI_RESERVOIR_STRUCT_SIZE,
+            usage: BufferUsages::STORAGE,
+            mapped_at_creation: false,
+        });
+
+        let gi_reservoirs_b = render_device.create_buffer(&BufferDescriptor {
+            label: Some("solari_lighting_gi_reservoirs_b"),
+            size: (view_size.x * view_size.y) as u64 * GI_RESERVOIR_STRUCT_SIZE,
             usage: BufferUsages::STORAGE,
             mapped_at_creation: false,
         });
@@ -88,8 +105,10 @@ pub fn prepare_solari_lighting_resources(
         let previous_depth_view = previous_depth.create_view(&TextureViewDescriptor::default());
 
         commands.entity(entity).insert(SolariLightingResources {
-            reservoirs_a,
-            reservoirs_b,
+            di_reservoirs_a,
+            di_reservoirs_b,
+            gi_reservoirs_a,
+            gi_reservoirs_b,
             previous_gbuffer: (previous_gbuffer, previous_gbuffer_view),
             previous_depth: (previous_depth, previous_depth_view),
             view_size,
