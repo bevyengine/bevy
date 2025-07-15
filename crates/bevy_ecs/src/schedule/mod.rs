596d8a70d143e3e9c8b5529179f37c6022a0003d
Peter Hayman <peteyhayman@gmail.com>
2025-07-15 12:08:31 +1000

    fmt

--- crates/bevy_ecs/src/schedule/mod.rs
+++ crates/bevy_ecs/src/schedule/mod.rs
@@ -13,8 +13,8 @@ use self::graph::*;
 pub use self::{condition::*, config::*, executor::*, schedule::*, set::*};
 pub use pass::ScheduleBuildPass;
 
-pub use self::graph::NodeId;
 pub use self::graph::GraphInfo;
+pub use self::graph::NodeId;
 
 /// An implementation of a graph data structure.
 pub mod graph;
