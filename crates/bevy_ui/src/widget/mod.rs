d66edffe19771d3aca0704782fe527cc86e102ad
ickshonpe <david.curthoys@googlemail.com>
2025-07-29 12:10:13 +0100

    Renamed text input modules to `input_box` and `input_field`

--- crates/bevy_ui/src/widget/mod.rs
+++ crates/bevy_ui/src/widget/mod.rs
@@ -2,16 +2,16 @@
 
 mod button;
 mod image;
+mod input_box;
+mod input_field;
 mod label;
 mod text;
-mod text_box;
-mod text_input;
 mod viewport;
 
 pub use button::*;
 pub use image::*;
+pub use input_box::*;
+pub use input_field::*;
 pub use label::*;
 pub use text::*;
-pub use text_box::*;
-pub use text_input::*;
 pub use viewport::*;
