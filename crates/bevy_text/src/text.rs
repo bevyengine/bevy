b8322b8feb2f7f5a0f52959891e857cb83dd059a
ickshonpe <david.curthoys@googlemail.com>
2025-07-30 18:29:37 +0100

    Removed selection block color component.

--- crates/bevy_text/src/text.rs
+++ crates/bevy_text/src/text.rs
@@ -401,30 +401,6 @@ impl TextColor {
     pub const WHITE: Self = TextColor(Color::WHITE);
 }
 
-/// The color of the text selection block.
-#[derive(Component, Copy, Clone, Debug, Deref, DerefMut, Reflect, PartialEq)]
-#[reflect(Component, Default, Debug, PartialEq, Clone)]
-pub struct TextSelectionBlockColor(pub Color);
-
-impl Default for TextSelectionBlockColor {
-    fn default() -> Self {
-        Self::new(DARK_GOLDENROD)
-    }
-}
-
-impl<T: Into<Color>> From<T> for TextSelectionBlockColor {
-    fn from(color: T) -> Self {
-        Self(color.into())
-    }
-}
-
-impl TextSelectionBlockColor {
-    /// Set the text selection color
-    pub fn new(color: impl Into<Color>) -> Self {
-        Self(color.into())
-    }
-}
-
 /// The background color of the text for this section.
 #[derive(Component, Copy, Clone, Debug, Deref, DerefMut, Reflect, PartialEq)]
 #[reflect(Component, Default, Debug, PartialEq, Clone)]
