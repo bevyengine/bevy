1cf0d3f8c2b896ead1edf5abe6c960e004cbdf7c
Greeble <166992735+greeble-dev@users.noreply.github.com>
2025-08-01 16:17:25 +0100

    Removed camera and rotation conversions that were only used by node conversion.

--- crates/bevy_gltf/src/convert_coordinates.rs
+++ crates/bevy_gltf/src/convert_coordinates.rs
@@ -1,8 +1,6 @@
-use core::f32::consts::PI;
 use serde::{Deserialize, Serialize};
 
-use bevy_math::{Mat4, Quat, Vec3};
-use bevy_transform::components::Transform;
+use bevy_math::{Mat4, Vec3};
 
 pub(crate) trait ConvertCoordinates {
     /// Converts the glTF coordinates to Bevy's coordinate system.
@@ -17,17 +15,6 @@ pub(crate) trait ConvertCoordinates {
     ///
     /// See <https://registry.khronos.org/glTF/specs/2.0/glTF-2.0.html#coordinate-system-and-units>
     fn convert_coordinates(self) -> Self;
-}
-
-pub(crate) trait ConvertCameraCoordinates {
-    /// Like `convert_coordinates`, but uses the following for the lens rotation:
-    /// - forward: -Z
-    /// - up: Y
-    /// - right: X
-    ///
-    /// The same convention is used for lights.
-    /// See <https://registry.khronos.org/glTF/specs/2.0/glTF-2.0.html#view-matrix>
-    fn convert_camera_coordinates(self) -> Self;
 }
 
 impl ConvertCoordinates for Vec3 {
@@ -49,35 +36,12 @@ impl ConvertCoordinates for [f32; 4] {
     }
 }
 
-impl ConvertCoordinates for Quat {
-    fn convert_coordinates(self) -> Self {
-        // Solution of q' = r q r*
-        Quat::from_array([-self.x, self.y, -self.z, self.w])
-    }
-}
-
 impl ConvertCoordinates for Mat4 {
     fn convert_coordinates(self) -> Self {
         let m: Mat4 = Mat4::from_scale(Vec3::new(-1.0, 1.0, -1.0));
         // Same as the original matrix
         let m_inv = m;
         m_inv * self * m
-    }
-}
-
-impl ConvertCoordinates for Transform {
-    fn convert_coordinates(mut self) -> Self {
-        self.translation = self.translation.convert_coordinates();
-        self.rotation = self.rotation.convert_coordinates();
-        self
-    }
-}
-
-impl ConvertCameraCoordinates for Transform {
-    fn convert_camera_coordinates(mut self) -> Self {
-        self.translation = self.translation.convert_coordinates();
-        self.rotate_y(PI);
-        self
     }
 }
 
