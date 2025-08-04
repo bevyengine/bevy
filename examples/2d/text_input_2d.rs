203572692f64a2f63ae71a9211c78ea87c38eb88
ickshonpe <david.curthoys@googlemail.com>
2025-08-04 13:48:16 +0100

    Added text input 2d example

---
+++ examples/2d/text_input_2d.rs
@@ -0,0 +1,20 @@
+//! basic bevy_2d text input
+use bevy::{
+    color::palettes::css::*,
+    math::ops,
+    prelude::*,
+    sprite::Anchor,
+    text::{FontSmoothing, LineBreak, TextBounds},
+};
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .add_systems(Update, update)
+        .run();
+}
+
+fn setup() {}
+
+fn update() {}
