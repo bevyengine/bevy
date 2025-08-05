622bbe68bc06a2c93c5e2752aaa7a44052c1bf8e
ickshonpe <david.curthoys@googlemail.com>
2025-08-05 21:15:57 +0100

    Added example skeleton.

--- examples/2d/text_input_2d.rs
+++ examples/2d/text_input_2d.rs
@@ -4,17 +4,37 @@ use bevy::{
     math::ops,
     prelude::*,
     sprite::Anchor,
-    text::{FontSmoothing, LineBreak, TextBounds},
+    text::{
+        FontSmoothing, LineBreak, Placeholder, TextBounds, TextInputBuffer, TextInputTarget,
+        TextLayoutInfo, UndoHistory,
+    },
 };
+use bevy_render::Extract;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, setup)
         .add_systems(Update, update)
+        .add_systems(PostUpdate, update_targets)
+        .add_systems(ExtractSchedule, extract_text_input)
         .run();
 }
 
-fn setup() {}
+fn setup(mut commands: Commands) {
+    commands.spawn((
+        Transform::default(),
+        TextInputBuffer::default(),
+        UndoHistory::default(),
+        Placeholder::new("type here.."),
+    ));
+}
+
+fn update_targets() {}
 
 fn update() {}
+
+fn extract_text_input(
+    query: Extract<Query<(Entity, &GlobalTransform, &TextLayoutInfo, &TextInputTarget)>>,
+) {
+}
