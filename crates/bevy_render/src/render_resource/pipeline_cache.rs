a4796561e1ffa69d717bdf840c651795c63e7727
james7132 <contact@jamessliu.com>
2025-08-06 03:07:46 -0700

    Depend on bevy_tasks::block_on instead of futures_lite

--- crates/bevy_render/src/render_resource/pipeline_cache.rs
+++ crates/bevy_render/src/render_resource/pipeline_cache.rs
@@ -759,7 +759,7 @@ fn create_pipeline_task(
         return CachedPipelineState::Creating(bevy_tasks::AsyncComputeTaskPool::get().spawn(task));
     }
 
-    match futures_lite::future::block_on(task) {
+    match bevy_tasks::block_on(task) {
         Ok(pipeline) => CachedPipelineState::Ok(pipeline),
         Err(err) => CachedPipelineState::Err(err),
     }
@@ -774,7 +774,7 @@ fn create_pipeline_task(
     task: impl Future<Output = Result<Pipeline, PipelineCacheError>> + Send + 'static,
     _sync: bool,
 ) -> CachedPipelineState {
-    match futures_lite::future::block_on(task) {
+    match bevy_tasks::block_on(task) {
         Ok(pipeline) => CachedPipelineState::Ok(pipeline),
         Err(err) => CachedPipelineState::Err(err),
     }
