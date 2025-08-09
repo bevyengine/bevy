8925799b1d67d20100d52adf4c13111b5e168ecf
atlv <email@atlasdostal.com>
2025-08-09 18:25:10 -0400

    Add a prelude to bevy_camera (#20483)
    # Objective
    
    - title. this is the exact set of prelude re-exports bevy_render
    provides of bevy_mesh. We will soon be ready to yeet the re-exports.
    - also fix an oversight with bevy mesh prelude
    
    ## Solution
    
    - do it
    
    ## Testing
    
    - cargo check --examples

--- crates/bevy_internal/src/prelude.rs
+++ crates/bevy_internal/src/prelude.rs
@@ -18,8 +18,12 @@ pub use crate::window::prelude::*;
 pub use crate::image::prelude::*;
 
 #[doc(hidden)]
-#[cfg(feature = "bevy_image")]
+#[cfg(feature = "bevy_mesh")]
 pub use crate::mesh::prelude::*;
+
+#[doc(hidden)]
+#[cfg(feature = "bevy_camera")]
+pub use crate::camera::prelude::*;
 
 pub use bevy_derive::{bevy_main, Deref, DerefMut};
 
@@ -38,10 +42,6 @@ pub use crate::animation::prelude::*;
 #[doc(hidden)]
 #[cfg(feature = "bevy_color")]
 pub use crate::color::prelude::*;
-
-#[doc(hidden)]
-#[cfg(feature = "bevy_core_pipeline")]
-pub use crate::core_pipeline::prelude::*;
 
 #[doc(hidden)]
 #[cfg(feature = "bevy_pbr")]
