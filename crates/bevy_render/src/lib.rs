6d959c12d1ff6858420c0e7e38d1a26468689fe7
atlv <email@atlasdostal.com>
2025-08-08 20:43:35 -0700

    Update crates/bevy_render/src/lib.rs

--- crates/bevy_render/src/lib.rs
+++ crates/bevy_render/src/lib.rs
@@ -3,13 +3,13 @@
 //! Both `bevy_render` and `wgpu` have a number of environment variable options for changing the runtime behavior
 //! of both crates. Many of these may be useful in development or release environments. 
 //!
-//! - `WGPU_DEBUG=1` enables debug labels in release builds
-//! - `WGPU_VALIDATION=0` disables validation layers
-//! - `WGPU_FORCE_FALLBACK_ADAPTER=1` attempts to force software rendering
-//! - `WGPU_ADAPTER_NAME`: select a specific adapter by name
-//! - `WGPU_SETTINGS_PRIO=webgl2` uses webgl2 limits
-//! - `WGPU_SETTINGS_PRIO=compatibility` uses webgpu limits
-//! - `VERBOSE_SHADER_ERROR=1` prints more detailed information about WGSL compilation errors
+//! - `WGPU_DEBUG=1` enables debug labels, which can be useful in release builds.
+//! - `WGPU_VALIDATION=0` disables validation layers. This can help with particularly spammy errors.
+//! - `WGPU_FORCE_FALLBACK_ADAPTER=1` attempts to force software rendering. This typically matches what is used in CI.
+//! - `WGPU_ADAPTER_NAME` allows selecting a specific adapter by name.
+//! - `WGPU_SETTINGS_PRIO=webgl2` uses webgl2 limits.
+//! - `WGPU_SETTINGS_PRIO=compatibility` uses webgpu limits.
+//! - `VERBOSE_SHADER_ERROR=1` prints more detailed information about WGSL compilation errors, such as shader defs and shader entrypoint.
 
 #![expect(missing_docs, reason = "Not all docs are written yet, see #3492.")]
 #![expect(unsafe_code, reason = "Unsafe code is used to improve performance.")]
