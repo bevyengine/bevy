b5fa5913a116ee75b48a1432d5f757e1a44189e5
atlas dostal <rodol@rivalrebels.com>
2025-08-10 01:05:04 -0400

    Use bevy_light in pbr instead of bevy_pbr::light re-export

--- crates/bevy_pbr/src/meshlet/material_pipeline_prepare.rs
+++ crates/bevy_pbr/src/meshlet/material_pipeline_prepare.rs
@@ -2,14 +2,14 @@ use super::{
     instance_manager::InstanceManager, pipelines::MeshletPipelines,
     resource_manager::ResourceManager,
 };
-use crate::{irradiance_volume::IrradianceVolume, *};
+use crate::*;
 use bevy_camera::{Camera3d, Projection};
 use bevy_core_pipeline::{
     prepass::{DeferredPrepass, DepthPrepass, MotionVectorPrepass, NormalPrepass},
     tonemapping::{DebandDither, Tonemapping},
 };
 use bevy_derive::{Deref, DerefMut};
-use bevy_light::EnvironmentMapLight;
+use bevy_light::{EnvironmentMapLight, IrradianceVolume, ShadowFilteringMethod};
 use bevy_mesh::VertexBufferLayout;
 use bevy_platform::collections::{HashMap, HashSet};
 use bevy_render::erased_render_asset::ErasedRenderAssets;
