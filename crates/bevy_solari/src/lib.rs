9d42adea390e8fcfd9d8f3b4747a0a4b181f432a
JMS55 <47158642+JMS55@users.noreply.github.com>
2025-08-09 21:28:38 -0400

    Fix compile

--- crates/bevy_solari/src/lib.rs
+++ crates/bevy_solari/src/lib.rs
@@ -5,6 +5,12 @@
 //! See [`SolariPlugins`] for more info.
 //!
 //! ![`bevy_solari` logo](https://raw.githubusercontent.com/bevyengine/bevy/refs/heads/main/assets/branding/bevy_solari.svg)
+
+#[cfg(feature = "std")]
+extern crate std;
+
+extern crate alloc;
+
 pub mod pathtracer;
 pub mod realtime;
 pub mod scene;
