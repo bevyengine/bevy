a51ad851462564048a987f26bd01558332d2a215
2ne1ugly <chattermin@gmail.com>
2025-08-24 15:27:43 +0900

    implement Reflect for indexmap and adjust docs accordingly

--- crates/bevy_ecs/src/entity/index_set.rs
+++ crates/bevy_ecs/src/entity/index_set.rs
@@ -21,8 +21,10 @@ use indexmap::set::{self, IndexSet};
 use super::{Entity, EntityHash, EntitySetIterator};
 
 use bevy_platform::prelude::Box;
+use bevy_reflect::Reflect;
 
 /// An [`IndexSet`] pre-configured to use [`EntityHash`] hashing.
+#[cfg_attr(feature = "bevy_reflect", derive(Reflect))]
 #[cfg_attr(feature = "serialize", derive(serde::Deserialize, serde::Serialize))]
 #[derive(Debug, Clone, Default)]
 pub struct EntityIndexSet(pub(crate) IndexSet<Entity, EntityHash>);
