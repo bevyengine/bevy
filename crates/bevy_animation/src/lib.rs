0909086e3abeab5e1c988be624ad711d5b3874db
Piotr Siuszko <siuszko@zoho.com>
2025-08-25 10:50:50 +0200

    Update lib.rs

--- crates/bevy_animation/src/lib.rs
+++ crates/bevy_animation/src/lib.rs
@@ -1230,6 +1230,7 @@ pub fn animate_targets(
 
 /// Specifies query for animation systems.
 /// It allows to specify which Time resource to use based on the [`bevy_ecs::query::QueryFilter`].
+/// That can be especially useful when you want to use a custom time resource for subset of animations.
 /// By default, there is added a one version with Time<()> and Without [`DontUseDefaultAnimationTime`] component.
 pub fn specify_animation_system<T: Default, F: bevy_ecs::query::QueryFilter + 'static>(
     app: &mut App,
