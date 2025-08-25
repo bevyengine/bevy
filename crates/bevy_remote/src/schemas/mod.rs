9523c69538f7b925db7cbb1ed525400222c17d46
Piotr Siuszko <siuszko@zoho.com>
2025-08-25 22:23:12 +0200

    Sort `get_registered_reflect_types` response. It makes the response more consistent and reduce chance of diff with the same content but in different order

--- crates/bevy_remote/src/schemas/mod.rs
+++ crates/bevy_remote/src/schemas/mod.rs
@@ -48,10 +48,13 @@ impl SchemaTypesMetadata {
 
     /// Build reflect types list for a given type registration
     pub fn get_registered_reflect_types(&self, reg: &TypeRegistration) -> Vec<String> {
-        self.type_data_map
+        let mut result: Vec<String> = self
+            .type_data_map
             .iter()
             .filter_map(|(id, name)| reg.data_by_id(*id).and(Some(name.clone())))
-            .collect()
+            .collect();
+        result.sort();
+        result
     }
 
     /// Checks if slice contains string value that matches checked `TypeData`
