8f9eec8b1c0a28bd75718aaa3f6b778ae19db6a8
Daniel Skates <zeophlite@gmail.com>
2025-09-05 00:16:15 +0800

    Cleanup

--- crates/bevy_ui/src/widget/mod.rs
+++ crates/bevy_ui/src/widget/mod.rs
@@ -1,15 +1,19 @@
 //! This module contains the basic building blocks of Bevy's UI
 
 mod button;
+mod event;
 mod image;
 mod label;
 mod text;
 mod text_box;
+mod text_field;
 mod viewport;
 
 pub use button::*;
+pub use event::*;
 pub use image::*;
 pub use label::*;
 pub use text::*;
 pub use text_box::*;
+pub use text_field::*;
 pub use viewport::*;
