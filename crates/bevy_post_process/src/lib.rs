237377662a387d39cfc1ed666b71065c3f3df4cc
atlas <email@atlasdostal.com>
2025-09-05 00:04:58 -0400

    Revert "add workaround for plugin groups"
    This reverts commit cc4c61bee7dd0ad524b82f484ecc95d1d3c95606.

--- crates/bevy_post_process/src/lib.rs
+++ crates/bevy_post_process/src/lib.rs
@@ -17,27 +17,20 @@ use crate::{
     bloom::BloomPlugin, dof::DepthOfFieldPlugin, effect_stack::EffectStackPlugin,
     motion_blur::MotionBlurPlugin, msaa_writeback::MsaaWritebackPlugin,
 };
-use bevy_app::{plugin_group, App, Plugin};
+use bevy_app::{App, Plugin};
 
-// TODO: remove this plugin once we can order plugin groups correctly in DefaultPlugins
 /// Adds bloom, motion blur, depth of field, and chromatic aberration support.
 #[derive(Default)]
 pub struct PostProcessPlugin;
 
 impl Plugin for PostProcessPlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugins(PostProcessPlugins);
-    }
-}
-
-plugin_group! {
-    /// Adds bloom, motion blur, depth of field, and chromatic aberration support.
-    #[derive(Default)]
-    pub struct PostProcessPlugins {
-        :MsaaWritebackPlugin,
-        :BloomPlugin,
-        :MotionBlurPlugin,
-        :DepthOfFieldPlugin,
-        :EffectStackPlugin,
+        app.add_plugins((
+            MsaaWritebackPlugin,
+            BloomPlugin,
+            MotionBlurPlugin,
+            DepthOfFieldPlugin,
+            EffectStackPlugin,
+        ));
     }
 }
