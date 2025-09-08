f05852f6e8944f661da39c25a133a424c2576b24
MevLyshkin <siuszko@zoho.com>
2025-09-08 19:00:33 +0200

    Update crates/bevy_animation/src/lib.rs
    Co-authored-by: Alice Cecile <alice.i.cecile@gmail.com>

--- crates/bevy_animation/src/lib.rs
+++ crates/bevy_animation/src/lib.rs
@@ -1228,7 +1228,7 @@ pub fn animate_targets(
         });
 }
 
-/// Specifies query for animation systems.
+/// Specifies the query used for animation systems.
 /// It allows to specify which Time resource to use based on the [`bevy_ecs::query::QueryFilter`].
 /// That can be especially useful when you want to use a custom time resource for subset of animations.
 /// By default, there is added a one version with Time<()> and Without [`DontUseDefaultAnimationTime`] component.
