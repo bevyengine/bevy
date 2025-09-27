b3f759abe580ebbb6f2f92d08f7774b73796a488
Christopher Biscardi <chris@christopherbiscardi.com>
2025-09-27 08:40:47 -0700

    grid

--- examples/ui/anchor_layout.rs
+++ examples/ui/anchor_layout.rs
@@ -19,127 +19,109 @@ fn main() {
 fn spawn_layout(mut commands: Commands, asset_server: Res<AssetServer>) {
     let font = asset_server.load("fonts/FiraSans-Bold.ttf");
     commands.spawn(Camera2d);
-    commands
-        .spawn((
+
+    let rows = [
+        (
+            "left: 10px\ntop: 10px",
             Node {
-                // fill the entire window
-                width: percent(100),
-                height: percent(100),
-                flex_direction: FlexDirection::Column,
-                align_items: AlignItems::Center,
-                padding: MARGIN.all(),
-                row_gap: MARGIN,
+                left: px(10),
+                top: px(10),
                 ..default()
             },
-            BackgroundColor(Color::BLACK),
-        ))
-        .with_children(|builder| {
-            let rows = [
-                [
-                    (
-                        "left: 10px\ntop: 10px",
-                        Node {
-                            left: px(10),
-                            top: px(10),
-                            ..default()
-                        },
-                    ),
-                    (
-                        "center: 10px\ntop: 10px",
-                        Node {
-                            margin: auto().horizontal(),
-                            top: px(10),
-                            ..default()
-                        },
-                    ),
-                    (
-                        "right: 10px\ntop: 10px",
-                        Node {
-                            right: px(10),
-                            top: px(10),
-                            ..default()
-                        },
-                    ),
-                ],
-                [
-                    (
-                        "left: 10px\ncenter: 10px",
-                        Node {
-                            left: px(10),
-                            margin: UiRect::vertical(Val::Auto),
-                            ..default()
-                        },
-                    ),
-                    (
-                        "center: 10px\ncenter: 10px",
-                        Node {
-                            margin: UiRect::all(Val::Auto),
-                            ..default()
-                        },
-                    ),
-                    (
-                        "right: 10px\ncenter: 10px",
-                        Node {
-                            right: px(10),
-                            margin: UiRect::vertical(Val::Auto),
-                            ..default()
-                        },
-                    ),
-                ],
-                [
-                    (
-                        "left: 10px\nbottom: 10px",
-                        Node {
-                            left: px(10),
-                            bottom: px(10),
-                            ..default()
-                        },
-                    ),
-                    (
-                        "center: 10px\nbottom: 10px",
-                        Node {
-                            margin: UiRect::horizontal(Val::Auto),
-                            bottom: px(10),
-                            ..default()
-                        },
-                    ),
-                    (
-                        "right: 10px\nbottom: 10px",
-                        Node {
-                            right: px(10),
-                            bottom: px(10),
-                            ..default()
-                        },
-                    ),
-                ],
-            ];
-            for row in rows {
-                let font = font.clone();
+        ),
+        (
+            "center: 10px\ntop: 10px",
+            Node {
+                margin: auto().horizontal(),
+                top: px(10),
+                ..default()
+            },
+        ),
+        (
+            "right: 10px\ntop: 10px",
+            Node {
+                right: px(10),
+                top: px(10),
+                ..default()
+            },
+        ),
+        (
+            "left: 10px\ncenter: 10px",
+            Node {
+                left: px(10),
+                margin: UiRect::vertical(Val::Auto),
+                ..default()
+            },
+        ),
+        (
+            "center: 10px\ncenter: 10px",
+            Node {
+                margin: UiRect::all(Val::Auto),
+                ..default()
+            },
+        ),
+        (
+            "right: 10px\ncenter: 10px",
+            Node {
+                right: px(10),
+                margin: UiRect::vertical(Val::Auto),
+                ..default()
+            },
+        ),
+        (
+            "left: 10px\nbottom: 10px",
+            Node {
+                left: px(10),
+                bottom: px(10),
+                ..default()
+            },
+        ),
+        (
+            "center: 10px\nbottom: 10px",
+            Node {
+                margin: UiRect::horizontal(Val::Auto),
+                bottom: px(10),
+                ..default()
+            },
+        ),
+        (
+            "right: 10px\nbottom: 10px",
+            Node {
+                right: px(10),
+                bottom: px(10),
+                ..default()
+            },
+        ),
+    ];
 
-                builder.spawn((
-                    Node {
-                        width: percent(100),
-                        height: percent(100),
-                        flex_direction: FlexDirection::Row,
-                        column_gap: MARGIN,
-                        ..default()
-                    },
-                    Children::spawn(SpawnIter(
-                        row.into_iter()
-                            .map(move |v| anchored_node(font.clone(), v.1, v.0)),
-                    )),
-                ));
-            }
-        });
+    // let font = font.clone();
+    commands.spawn((
+        Node {
+            // fill the entire window
+            width: percent(100),
+            height: percent(100),
+            padding: MARGIN.all(),
+            row_gap: MARGIN,
+            column_gap: MARGIN,
+            display: Display::Grid,
+            grid_template_columns: RepeatedGridTrack::fr(3, 1.),
+            grid_template_rows: RepeatedGridTrack::fr(3, 1.),
+            ..default()
+        },
+        BackgroundColor(Color::BLACK),
+        Children::spawn(SpawnIter(
+            rows.into_iter()
+                .map(move |v| anchored_node(font.clone(), v.1, v.0)),
+        )),
+    ));
 }
 
 fn anchored_node(font: Handle<Font>, node: Node, label: &str) -> impl Bundle {
     (
         // outer gray box
         Node {
-            display: Display::Flex,
-            width: percent(100),
-            height: percent(100),
+            grid_column: GridPlacement::span(1),
+            grid_row: GridPlacement::span(1),
             ..default()
         },
         BackgroundColor(Color::srgb(0.25, 0.25, 0.25)),
