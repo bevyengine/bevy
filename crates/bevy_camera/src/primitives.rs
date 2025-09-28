0ab4e00ae4617c385cd538e843a5ff81c01c47cd
Saratii <friedchicken14821@gmail.com>
2025-09-27 20:15:41 -0500

    mmm

--- crates/bevy_camera/src/primitives.rs
+++ crates/bevy_camera/src/primitives.rs
@@ -130,6 +130,15 @@ impl Aabb {
         let signed_distance = p_normal.dot(aabb_center_world) + half_space.d();
         signed_distance > r
     }
+
+    /// The math simplifies when the AABB is already in world space, improving performance.
+    #[inline]
+    pub fn is_in_half_space_identity(&self, half_space: &HalfSpace) -> bool {
+        let p_normal = half_space.normal();
+        let r = self.half_extents.abs().dot(p_normal.abs());
+        let signed_distance = p_normal.dot(self.center) + half_space.d();
+        signed_distance > r
+    }
 }
 
 impl From<Sphere> for Aabb {
@@ -343,6 +352,17 @@ impl Frustum {
     pub fn contains_aabb(&self, aabb: &Aabb, world_from_local: &Affine3A) -> bool {
         for half_space in &self.half_spaces {
             if !aabb.is_in_half_space(half_space, world_from_local) {
+                return false;
+            }
+        }
+        true
+    }
+
+    /// contains_aabb is often called with Affine3A::IDENTITY, so we provide an optimized version.
+    #[inline]
+    pub fn contains_aabb_identity(&self, aabb: &Aabb) -> bool {
+        for half_space in &self.half_spaces {
+            if !aabb.is_in_half_space_identity(half_space) {
                 return false;
             }
         }
