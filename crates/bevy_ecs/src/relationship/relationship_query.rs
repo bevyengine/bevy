294bb5149941ccb152e5d3824e72dec3595aa42f
Codaishin <codaishin@gmail.com>
2025-09-30 16:10:38 +0200

    allow hierarchy filtering for breadth- and depth-first iteration
    refactor `DescendantIter` to be a wrapper around a generic traversal strategy. `filter_hierarchies` returns an iterator using that strategy.

--- crates/bevy_ecs/src/relationship/relationship_query.rs
+++ crates/bevy_ecs/src/relationship/relationship_query.rs
@@ -1,3 +1,4 @@
+use super::SourceIter;
 use crate::{
     entity::Entity,
     query::{QueryData, QueryFilter},
@@ -6,8 +7,6 @@ use crate::{
 };
 use alloc::collections::VecDeque;
 use smallvec::SmallVec;
-
-use super::SourceIter;
 
 impl<'w, 's, D: QueryData, F: QueryFilter> Query<'w, 's, D, F> {
     /// If the given `entity` contains the `R` [`Relationship`] component, returns the
@@ -101,11 +100,11 @@ impl<'w, 's, D: QueryData, F: QueryFilter> Query<'w, 's, D, F> {
     pub fn iter_descendants<S: RelationshipTarget>(
         &'w self,
         entity: Entity,
-    ) -> DescendantIter<'w, 's, D, F, S>
+    ) -> DescendantIter<BreadthFirst<'w, 's, D, F, S>>
     where
         D::ReadOnly: QueryData<Item<'w, 's> = &'w S>,
     {
-        DescendantIter::new(self, entity)
+        DescendantIter(BreadthFirst::new(self, entity))
     }
 
     /// Iterates all descendant entities as defined by the given `entity`'s [`RelationshipTarget`] and their recursive
@@ -118,12 +117,12 @@ impl<'w, 's, D: QueryData, F: QueryFilter> Query<'w, 's, D, F> {
     pub fn iter_descendants_depth_first<S: RelationshipTarget>(
         &'w self,
         entity: Entity,
-    ) -> DescendantDepthFirstIter<'w, 's, D, F, S>
+    ) -> DescendantIter<DepthFirst<'w, 's, D, F, S>>
     where
         D::ReadOnly: QueryData<Item<'w, 's> = &'w S>,
         SourceIter<'w, S>: DoubleEndedIterator,
     {
-        DescendantDepthFirstIter::new(self, entity)
+        DescendantIter(DepthFirst::new(self, entity))
     }
 
     /// Iterates all ancestors of the given `entity` as defined by the `R` [`Relationship`].
@@ -143,10 +142,10 @@ impl<'w, 's, D: QueryData, F: QueryFilter> Query<'w, 's, D, F> {
     }
 }
 
-/// An [`Iterator`] of [`Entity`]s over the descendants of an [`Entity`].
+/// A [`DescendantsTraversal`] of [`Entity`]s over the descendants of an [`Entity`].
 ///
 /// Traverses the hierarchy breadth-first.
-pub struct DescendantIter<'w, 's, D: QueryData, F: QueryFilter, S: RelationshipTarget>
+pub struct BreadthFirst<'w, 's, D: QueryData, F: QueryFilter, S: RelationshipTarget>
 where
     D::ReadOnly: QueryData<Item<'w, 's> = &'w S>,
 {
@@ -154,13 +153,13 @@ where
     vecdeque: VecDeque<Entity>,
 }
 
-impl<'w, 's, D: QueryData, F: QueryFilter, S: RelationshipTarget> DescendantIter<'w, 's, D, F, S>
+impl<'w, 's, D: QueryData, F: QueryFilter, S: RelationshipTarget> BreadthFirst<'w, 's, D, F, S>
 where
     D::ReadOnly: QueryData<Item<'w, 's> = &'w S>,
 {
     /// Returns a new [`DescendantIter`].
-    pub fn new(children_query: &'w Query<'w, 's, D, F>, entity: Entity) -> Self {
-        DescendantIter {
+    fn new(children_query: &'w Query<'w, 's, D, F>, entity: Entity) -> Self {
+        BreadthFirst {
             children_query,
             vecdeque: children_query
                 .get(entity)
@@ -169,45 +168,30 @@ where
                 .collect(),
         }
     }
-
-    /// Creates an iterator which uses a closure to determine if recursive [`RelationshipTarget`]s
-    /// should be yielded.
-    ///
-    /// Once the the provided closure returns `false` for an [`Entity`] it and its recursive
-    /// [`RelationshipTarget`]s will not be yielded, effectively skipping that sub hierarchy.
-    pub fn filter_hierarchies<HF>(self, filter: HF) -> FilterDescendantIter<'w, 's, D, F, S, HF>
-    where
-        HF: FnMut(&Entity) -> bool,
-    {
-        FilterDescendantIter {
-            iter: self,
-            hierarchy_filter: filter,
-        }
-    }
 }
 
-impl<'w, 's, D: QueryData, F: QueryFilter, S: RelationshipTarget> Iterator
-    for DescendantIter<'w, 's, D, F, S>
+impl<'w, 's, D: QueryData, F: QueryFilter, S: RelationshipTarget> DescendantsTraversal
+    for BreadthFirst<'w, 's, D, F, S>
 where
     D::ReadOnly: QueryData<Item<'w, 's> = &'w S>,
 {
-    type Item = Entity;
+    fn next_root(&mut self) -> Option<Entity> {
+        self.vecdeque.pop_front()
+    }
 
-    fn next(&mut self) -> Option<Self::Item> {
-        let entity = self.vecdeque.pop_front()?;
+    fn set_children(&mut self, root: Entity) {
+        let Ok(children) = self.children_query.get(root) else {
+            return;
+        };
 
-        if let Ok(children) = self.children_query.get(entity) {
-            self.vecdeque.extend(children.iter());
-        }
-
-        Some(entity)
+        self.vecdeque.extend(children.iter());
     }
 }
 
-/// An [`Iterator`] of [`Entity`]s over the descendants of an [`Entity`].
+/// A [`DescendantsTraversal`] of [`Entity`]s over the descendants of an [`Entity`].
 ///
 /// Traverses the hierarchy depth-first.
-pub struct DescendantDepthFirstIter<'w, 's, D: QueryData, F: QueryFilter, S: RelationshipTarget>
+pub struct DepthFirst<'w, 's, D: QueryData, F: QueryFilter, S: RelationshipTarget>
 where
     D::ReadOnly: QueryData<Item<'w, 's> = &'w S>,
 {
@@ -215,15 +199,14 @@ where
     stack: SmallVec<[Entity; 8]>,
 }
 
-impl<'w, 's, D: QueryData, F: QueryFilter, S: RelationshipTarget>
-    DescendantDepthFirstIter<'w, 's, D, F, S>
+impl<'w, 's, D: QueryData, F: QueryFilter, S: RelationshipTarget> DepthFirst<'w, 's, D, F, S>
 where
     D::ReadOnly: QueryData<Item<'w, 's> = &'w S>,
     SourceIter<'w, S>: DoubleEndedIterator,
 {
     /// Returns a new [`DescendantDepthFirstIter`].
-    pub fn new(children_query: &'w Query<'w, 's, D, F>, entity: Entity) -> Self {
-        DescendantDepthFirstIter {
+    fn new(children_query: &'w Query<'w, 's, D, F>, entity: Entity) -> Self {
+        DepthFirst {
             children_query,
             stack: children_query
                 .get(entity)
@@ -232,22 +215,22 @@ where
     }
 }
 
-impl<'w, 's, D: QueryData, F: QueryFilter, S: RelationshipTarget> Iterator
-    for DescendantDepthFirstIter<'w, 's, D, F, S>
+impl<'w, 's, D: QueryData, F: QueryFilter, S: RelationshipTarget> DescendantsTraversal
+    for DepthFirst<'w, 's, D, F, S>
 where
     D::ReadOnly: QueryData<Item<'w, 's> = &'w S>,
     SourceIter<'w, S>: DoubleEndedIterator,
 {
-    type Item = Entity;
+    fn next_root(&mut self) -> Option<Entity> {
+        self.stack.pop()
+    }
 
-    fn next(&mut self) -> Option<Self::Item> {
-        let entity = self.stack.pop()?;
+    fn set_children(&mut self, root: Entity) {
+        let Ok(children) = self.children_query.get(root) else {
+            return;
+        };
 
-        if let Ok(children) = self.children_query.get(entity) {
-            self.stack.extend(children.iter().rev());
-        }
-
-        Some(entity)
+        self.stack.extend(children.iter().rev());
     }
 }
 
@@ -289,41 +272,86 @@ where
 /// An [`Iterator`] of [`Entity`]s over the descendants of an [`Entity`].
 ///
 /// Allows conditional skipping of sub hierarchies.
-pub struct FilterDescendantIter<'w, 's, D, QF, S, HF>
-where
-    D: QueryData,
-    D::ReadOnly: QueryData<Item<'w, 's> = &'w S>,
-    QF: QueryFilter,
-    S: RelationshipTarget,
-    HF: FnMut(&Entity) -> bool,
-{
-    iter: DescendantIter<'w, 's, D, QF, S>,
-    hierarchy_filter: HF,
+/// If all sub hierarchies are linear (only one child) then this yields the same as [`MapWhile`](core::iter::MapWhile).
+pub struct FilterHierarchies<T, F> {
+    iter: T,
+    hierarchy_filter: F,
 }
 
-impl<'w, 's, D, QF, S, HF> Iterator for FilterDescendantIter<'w, 's, D, QF, S, HF>
+impl<T, F> Iterator for FilterHierarchies<T, F>
 where
-    D: QueryData,
-    D::ReadOnly: QueryData<Item<'w, 's> = &'w S>,
-    QF: QueryFilter,
-    S: RelationshipTarget,
-    HF: FnMut(&Entity) -> bool,
+    T: DescendantsTraversal,
+    F: FnMut(&Entity) -> bool,
 {
     type Item = Entity;
 
     fn next(&mut self) -> Option<Self::Item> {
-        let mut entity = self.iter.vecdeque.pop_front()?;
+        let mut root = self.iter.next_root()?;
 
-        while !(self.hierarchy_filter)(&entity) {
-            entity = self.iter.vecdeque.pop_front()?;
+        while !(self.hierarchy_filter)(&root) {
+            root = self.iter.next_root()?;
         }
+        self.iter.set_children(root);
 
-        if let Ok(children) = self.iter.children_query.get(entity) {
-            self.iter.vecdeque.extend(children.iter());
-        }
-
-        Some(entity)
+        Some(root)
     }
+}
+
+/// A [`Iterator`] of [`Entity`]s over the descendants of an [`Entity`].
+///
+/// Concrete traversal strategy depends on the `Traversal` type.
+pub struct DescendantIter<Traversal>(Traversal);
+
+impl<Traversal> DescendantIter<Traversal> {
+    /// Creates an iterator which uses a closure to determine if recursive [`RelationshipTarget`]s
+    /// should be yielded.
+    ///
+    /// Once the the provided closure returns `false` for an [`Entity`] it and its recursive
+    /// [`RelationshipTarget`]s will not be yielded, effectively skipping that sub hierarchy.
+    pub fn filter_hierarchies<HF>(self, filter: HF) -> FilterHierarchies<Self, HF>
+    where
+        HF: FnMut(&Entity) -> bool,
+    {
+        FilterHierarchies {
+            iter: self,
+            hierarchy_filter: filter,
+        }
+    }
+}
+
+impl<Traversal> Iterator for DescendantIter<Traversal>
+where
+    Traversal: DescendantsTraversal,
+{
+    type Item = Entity;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let next_root = self.0.next_root()?;
+        self.0.set_children(next_root);
+
+        Some(next_root)
+    }
+}
+
+impl<Traversal> DescendantsTraversal for DescendantIter<Traversal>
+where
+    Traversal: DescendantsTraversal,
+{
+    fn next_root(&mut self) -> Option<Entity> {
+        self.0.next_root()
+    }
+
+    fn set_children(&mut self, root: Entity) {
+        self.0.set_children(root);
+    }
+}
+
+/// A trait to implement a concrete descendant traversal strategy
+///
+/// Used to streamline breadth-first and depth-first iteration
+trait DescendantsTraversal {
+    fn next_root(&mut self) -> Option<Entity>;
+    fn set_children(&mut self, root: Entity);
 }
 
 #[cfg(test)]
@@ -334,59 +362,109 @@ mod test_iter_descendants {
     };
     use alloc::{vec, vec::Vec};
 
-    #[test]
-    fn filter_hierarchies_all() -> Result<(), RunSystemError> {
-        let mut world = World::new();
-        let root = world.spawn_empty().id();
-        let children = vec![
-            world.spawn(ChildOf(root)).id(),
-            world.spawn(ChildOf(root)).id(),
-            world.spawn(ChildOf(root)).id(),
-        ];
+    mod iter_descendants_breadth_first {
+        use super::*;
 
-        let descendants = world.run_system_once(move |q: Query<&Children>| {
-            q.iter_descendants(root)
-                .filter_hierarchies(|_| true)
-                .collect::<Vec<_>>()
-        })?;
+        #[test]
+        fn iter_all() -> Result<(), RunSystemError> {
+            let mut world = World::new();
+            let root = world.spawn_empty().id();
+            let a = world.spawn(ChildOf(root)).id();
+            let aa = world.spawn(ChildOf(a)).id();
+            let ab = world.spawn(ChildOf(a)).id();
+            let b = world.spawn(ChildOf(root)).id();
+            let ba = world.spawn(ChildOf(b)).id();
+            let bb = world.spawn(ChildOf(b)).id();
 
-        assert_eq!(descendants, children);
-        Ok(())
+            let descendants = world.run_system_once(move |q: Query<&Children>| {
+                q.iter_descendants(root).collect::<Vec<_>>()
+            })?;
+
+            assert_eq!(descendants, vec![a, b, aa, ab, ba, bb]);
+            Ok(())
+        }
     }
 
-    #[test]
-    fn filter_hierarchies_skip_flat() -> Result<(), RunSystemError> {
-        let mut world = World::new();
-        let root = world.spawn_empty().id();
-        let c0 = world.spawn(ChildOf(root)).id();
-        let c_skip = world.spawn(ChildOf(root)).id();
-        let c2 = world.spawn(ChildOf(root)).id();
+    mod iter_descendants_depth_first {
+        use super::*;
 
-        let descendants = world.run_system_once(move |q: Query<&Children>| {
-            q.iter_descendants(root)
-                .filter_hierarchies(|e| e != &c_skip)
-                .collect::<Vec<_>>()
-        })?;
+        #[test]
+        fn iter_all() -> Result<(), RunSystemError> {
+            let mut world = World::new();
+            let root = world.spawn_empty().id();
+            let a = world.spawn(ChildOf(root)).id();
+            let aa = world.spawn(ChildOf(a)).id();
+            let ab = world.spawn(ChildOf(a)).id();
+            let b = world.spawn(ChildOf(root)).id();
+            let ba = world.spawn(ChildOf(b)).id();
+            let bb = world.spawn(ChildOf(b)).id();
 
-        assert_eq!(descendants, vec![c0, c2]);
-        Ok(())
+            let descendants = world.run_system_once(move |q: Query<&Children>| {
+                q.iter_descendants_depth_first(root).collect::<Vec<_>>()
+            })?;
+
+            assert_eq!(descendants, vec![a, aa, ab, b, ba, bb]);
+            Ok(())
+        }
     }
 
-    #[test]
-    fn filter_hierarchies_skip_sub_hierarchy() -> Result<(), RunSystemError> {
-        let mut world = World::new();
-        let root = world.spawn_empty().id();
-        let c0 = world.spawn(ChildOf(root)).id();
-        let c_skip = world.spawn((ChildOf(root), children![(), ()])).id();
-        let c2 = world.spawn(ChildOf(root)).id();
+    mod filter_hierarchies {
+        use super::*;
 
-        let descendants = world.run_system_once(move |q: Query<&Children>| {
-            q.iter_descendants(root)
-                .filter_hierarchies(|e| e != &c_skip)
-                .collect::<Vec<_>>()
-        })?;
+        #[test]
+        fn iter_all() -> Result<(), RunSystemError> {
+            let mut world = World::new();
+            let root = world.spawn_empty().id();
+            let children = vec![
+                world.spawn(ChildOf(root)).id(),
+                world.spawn(ChildOf(root)).id(),
+                world.spawn(ChildOf(root)).id(),
+            ];
 
-        assert_eq!(descendants, vec![c0, c2]);
-        Ok(())
+            let descendants = world.run_system_once(move |q: Query<&Children>| {
+                q.iter_descendants(root)
+                    .filter_hierarchies(|_| true)
+                    .collect::<Vec<_>>()
+            })?;
+
+            assert_eq!(descendants, children);
+            Ok(())
+        }
+
+        #[test]
+        fn skip_entity_when_flat() -> Result<(), RunSystemError> {
+            let mut world = World::new();
+            let root = world.spawn_empty().id();
+            let a = world.spawn(ChildOf(root)).id();
+            let skip = world.spawn(ChildOf(root)).id();
+            let b = world.spawn(ChildOf(root)).id();
+
+            let descendants = world.run_system_once(move |q: Query<&Children>| {
+                q.iter_descendants(root)
+                    .filter_hierarchies(|e| e != &skip)
+                    .collect::<Vec<_>>()
+            })?;
+
+            assert_eq!(descendants, vec![a, b]);
+            Ok(())
+        }
+
+        #[test]
+        fn skip_sub_hierarchy() -> Result<(), RunSystemError> {
+            let mut world = World::new();
+            let root = world.spawn_empty().id();
+            let a = world.spawn(ChildOf(root)).id();
+            let skip = world.spawn((ChildOf(root), children![(), ()])).id();
+            let b = world.spawn(ChildOf(root)).id();
+
+            let descendants = world.run_system_once(move |q: Query<&Children>| {
+                q.iter_descendants(root)
+                    .filter_hierarchies(|e| e != &skip)
+                    .collect::<Vec<_>>()
+            })?;
+
+            assert_eq!(descendants, vec![a, b]);
+            Ok(())
+        }
     }
 }
