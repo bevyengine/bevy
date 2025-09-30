c179b27a836ae1b5b0ef232a0e38c2b50666a93a
Kevin Reid <kpreid@switchb.org>
2025-09-30 14:20:23 -0700

    Replace `feature(doc_auto_cfg)` with `feature(doc_cfg)`. (#21304)
    ## Objective
    
    Fixes #21303 (but does not add any testing that similar issues will not
    recur).
    
    ## Solution
    
    Globally replace `feature(doc_auto_cfg)` with `feature(doc_cfg)`.
    
    ## Testing
    
    Tested that `RUSTDOCFLAGS=--cfg=docsrs cargo +nightly doc -p
    bevy_platform` succeeds. This is not a test of all documentation builds,
    but I do not know of a way to do that until bevy’s dependencies catch
    up.

--- crates/bevy_derive/src/lib.rs
+++ crates/bevy_derive/src/lib.rs
@@ -1,7 +1,7 @@
 //! Assorted proc macro derive functions.
 
 #![forbid(unsafe_code)]
-#![cfg_attr(docsrs, feature(doc_auto_cfg))]
+#![cfg_attr(docsrs, feature(doc_cfg))]
 #![doc(
     html_logo_url = "https://bevy.org/assets/icon.png",
     html_favicon_url = "https://bevy.org/assets/icon.png"
