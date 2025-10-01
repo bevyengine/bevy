6abaf4554d46f311d9763d402c1e6969dcece3cd
ickshonpe <david.curthoys@googlemail.com>
2025-10-01 10:17:27 +0100

    Improved ont_count method

--- crates/bevy_text/src/font_atlas_set.rs
+++ crates/bevy_text/src/font_atlas_set.rs
@@ -21,17 +21,16 @@ impl FontAtlasSets {
         let id: AssetId<Font> = id.into();
         self.sets.get(&id)
     }
+
     /// Get a mutable reference to the [`FontAtlasSet`] with the given font asset id.
     pub fn get_mut(&mut self, id: impl Into<AssetId<Font>>) -> Option<&mut FontAtlasSet> {
         let id: AssetId<Font> = id.into();
         self.sets.get_mut(&id)
     }
 
-    /// Returns the total number of fonts in all sets
+    /// Returns the total number of rasterized fonts across all sets.
     pub fn font_count(&self) -> usize {
-        self.sets
-            .iter()
-            .fold(0, |count, (_, set)| count + set.len())
+        self.sets.values().map(FontAtlasSet::len).sum()
     }
 }
 
