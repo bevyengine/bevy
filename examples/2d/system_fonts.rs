616d0e0891db127114cb8a1498344bbeacaeacde
dsgallups <dsgallups@protonmail.com>
2025-10-05 07:11:23 -0400

    feat: example

---
+++ examples/2d/system_fonts.rs
@@ -0,0 +1,44 @@
+//! Uses a system font to display text
+use bevy::prelude::*;
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .run();
+}
+
+fn setup(mut commands: Commands, mut fonts: ResMut<Assets<Font>>) {
+    commands.spawn(Camera2d);
+
+    let font = fonts.add(Font::System {
+        families: vec![
+            Family::Name("Liberation Sans".to_string()),
+            Family::Name("Ubuntu".to_string()),
+            Family::Name("Noto Sans".to_string()),
+        ],
+        weight: Weight::NORMAL,
+        stretch: Stretch::Normal,
+        style: Style::Normal,
+    });
+
+    commands.spawn((
+        Node {
+            width: percent(100.),
+            height: percent(100.),
+            display: Display::Flex,
+            padding: UiRect::all(px(20.)),
+            justify_content: JustifyContent::Center,
+            ..default()
+        },
+        children![(
+            Text::new("System Font UI Text"),
+            TextFont::default().with_font(font.clone()),
+        )],
+    ));
+
+    commands.spawn((
+        Text2d::new("System Font 2D Text"),
+        TextFont::default().with_font(font),
+    ));
+}
