bevyengine/bevy

commit cc4398e17d48d59ddf823b0fbc0436d430bff787

Avoid a "RefCell already borrowed" error with WINIT_WINDOWS (#21338)
# Objective

Fixes #21319.

## Solution

In the "about_to_wait" state of Bevy's winit event loop, we borrow
WINIT_WINDOWS only to check the conditions for a redraw, and perform the
call after that check is concluded.

## Testing

Tested by confirming that the RefCell error no longer occurs in my
application. In my application, the error occurred when the
`bevy-egui-inspector` crate was used.

I do not know whether the original reporter of #21319 was also using
third-party Bevy plugins that might have caused the issue. It would be
good if they could check whether this change fixes it for them.

Tested on Windows 11, where the bug is known to occur. The code that was
changed is only included in Windows builds so there is little need to
test on other platforms.

---------

Co-authored-by: Alice Cecile <alice.i.cecile@gmail.com>
Co-authored-by: Dimitrios Loukadakis <dloukadakis@users.noreply.github.com>

crates/bevy_winit/src/state.rs
@@ -469,24 +469,23 @@ impl<M: Message> ApplicationHandler<M> for WinitAppRunnerState<M> {
         // invisible window creation. https://github.com/bevyengine/bevy/issues/18027
         #[cfg(target_os = "windows")]
         {
-            WINIT_WINDOWS.with_borrow(|winit_windows| {
-                let headless = winit_windows.windows.is_empty();
-                let exiting = self.app_exit.is_some();
-                let reactive = matches!(self.update_mode, UpdateMode::Reactive { .. });
-                let all_invisible = winit_windows
-                    .windows
-                    .iter()
-                    .all(|(_, w)| !w.is_visible().unwrap_or(false));
-                if !exiting
-                    && (self.startup_forced_updates > 0
-                        || headless
-                        || all_invisible
-                        || reactive
-                        || self.window_event_received)
-                {
-                    self.redraw_requested(event_loop);
-                }
-            });
+            fn headless_or_all_invisible() -> bool {
+                WINIT_WINDOWS.with_borrow(|winit_windows| {
+                    winit_windows
+                        .windows
+                        .iter()
+                        .all(|(_, w)| !w.is_visible().unwrap_or(false))
+                })
+            }
+
+            if !self.app_exit.is_some()
+                && (self.startup_forced_updates > 0
+                    || matches!(self.update_mode, UpdateMode::Reactive { .. })
+                    || self.window_event_received
+                    || headless_or_all_invisible())
+            {
+                self.redraw_requested(event_loop);
+            }
         }
     }
 
@@ -1039,10 +1038,10 @@ mod tests {
         app.add_systems(
             Update,
             move |mut window: Single<(Entity, &mut Window)>,
-             mut window_backend_scale_factor_changed: MessageWriter<
-                WindowBackendScaleFactorChanged,
-            >,
-             mut window_scale_factor_changed: MessageWriter<WindowScaleFactorChanged>| {
+                  mut window_backend_scale_factor_changed: MessageWriter<
+                      WindowBackendScaleFactorChanged,
+                  >,
+                  mut window_scale_factor_changed: MessageWriter<WindowScaleFactorChanged>| {
                 react_to_scale_factor_change(
                     window.0,
                     &mut window.1,

commit 11059de44e4d58a5354c44a8eb87e6d8387e24fe

Fix clippy when `target_os = "windows"` (#21419)
Fixes a clippy error introduced by #21338. Issue was inside a
`#[cfg(target_os = "windows")]` block so it wasn't caught by CI.

```rust
error: this boolean expression can be simplified
   --> crates\bevy_winit\src\state.rs:481:16
    |
481 |             if !self.app_exit.is_some()
    |                ^^^^^^^^^^^^^^^^^^^^^^^^ help: try: `self.app_exit.is_none()`
```

Tested with Rust 1.90.0, Win10, `cargo run -p ci -- lints`, `cargo run
--example window_settings`.

crates/bevy_winit/src/state.rs
@@ -478,7 +478,7 @@ impl<M: Message> ApplicationHandler<M> for WinitAppRunnerState<M> {
                 })
             }
 
-            if !self.app_exit.is_some()
+            if self.app_exit.is_none()
                 && (self.startup_forced_updates > 0
                     || matches!(self.update_mode, UpdateMode::Reactive { .. })
                     || self.window_event_received