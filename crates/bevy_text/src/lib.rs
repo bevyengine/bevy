970c1f7e412cded468587640682ca9dc87f65e93
ickshonpe <david.curthoys@googlemail.com>
2025-10-16 13:57:18 +0100

    fixed default font asset loader

--- crates/bevy_text/src/lib.rs
+++ crates/bevy_text/src/lib.rs
@@ -105,7 +105,7 @@ impl Plugin for TextPlugin {
         {
             use bevy_asset::{AssetId, Assets};
             let mut assets = app.world_mut().resource_mut::<Assets<_>>();
-            let asset = Font::try_from_bytes(DEFAULT_FONT_DATA.to_vec()).unwrap();
+            let asset = Font::try_from_bytes(DEFAULT_FONT_DATA.to_vec());
             assets.insert(AssetId::default(), asset).unwrap();
         };
     }
