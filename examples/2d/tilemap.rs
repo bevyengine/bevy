575b1fb94730fab3b5978e0ce3b92a9d473302fd
James Bell <jamescarterbell@gmail.com>
2025-11-05 15:22:13 -0500

    Working example

---
+++ examples/2d/tilemap.rs
@@ -0,0 +1,68 @@
+//! Shows a tilemap chunk rendered with a single draw call.
+
+use bevy::{
+    color::palettes::tailwind::RED_400, image::{ImageArrayLayout, ImageLoaderSettings}, prelude::*, sprite::{TileStorage, Tilemap, CommandsTilemapExt}, sprite_render::{TileRenderData, TilemapChunkRenderData, TilemapRenderData}
+};
+use rand::{Rng, SeedableRng};
+use rand_chacha::ChaCha8Rng;
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
+        .add_systems(Startup, setup)
+        .add_systems(Update, update_tilemap)
+        .run();
+}
+
+#[derive(Component, Deref, DerefMut)]
+struct UpdateTimer(Timer);
+
+#[derive(Resource, Deref, DerefMut)]
+struct SeededRng(ChaCha8Rng);
+
+fn setup(mut commands: Commands, assets: Res<AssetServer>) {
+    // We're seeding the PRNG here to make this example deterministic for testing purposes.
+    // This isn't strictly required in practical use unless you need your app to be deterministic.
+    let mut rng = ChaCha8Rng::seed_from_u64(42);
+
+    let chunk_size = UVec2::splat(16);
+    let tile_display_size = UVec2::splat(8);
+
+    commands.spawn((
+        Transform::default(),
+        Visibility::default(),
+        Tilemap::new(chunk_size, tile_display_size),
+        TilemapRenderData {
+            tileset: assets.load_with_settings(
+                "textures/array_texture.png",
+                |settings: &mut ImageLoaderSettings| {
+                    // The tileset texture is expected to be an array of tile textures, so we tell the
+                    // `ImageLoader` that our texture is composed of 4 stacked tile images.
+                    settings.array_layout = Some(ImageArrayLayout::RowCount { rows: 4 });
+                },
+            ),
+            ..default()
+        }
+    ));
+
+    commands.spawn(Camera2d);
+
+    commands.insert_resource(SeededRng(rng));
+}
+
+#[derive(Component)]
+struct MovePlayer;
+
+fn update_tilemap(
+    map: Single<Entity, With<Tilemap>>,
+    mut commands: Commands,
+    mut rng: ResMut<SeededRng>,
+) {
+    let map = *map;
+    for i in 0..rng.random_range(1..=20) {
+        let x = rng.random_range(-64..=64);
+        let y = rng.random_range(-64..=64);
+
+        commands.set_tile(map, IVec2::new(x, y), Some(TileRenderData { tileset_index: rng.random_range(0..4), ..Default::default()}));
+    }
+}
