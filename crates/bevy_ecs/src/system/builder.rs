e6e32caa3bd78cf5bb43c07db484aa5f1ba39b40
Emerson Coskey <emerson@coskey.dev>
2025-11-05 21:13:52 -0800

    Add SystemRunner

--- crates/bevy_ecs/src/system/builder.rs
+++ crates/bevy_ecs/src/system/builder.rs
@@ -7,8 +7,8 @@ use crate::{
     query::{QueryData, QueryFilter, QueryState},
     resource::Resource,
     system::{
-        DynSystemParam, DynSystemParamState, If, Local, ParamSet, Query, SystemParam,
-        SystemParamValidationError,
+        BoxedSystem, DynSystemParam, DynSystemParamState, If, IntoSystem, Local, ParamSet, Query,
+        SystemInput, SystemParam, SystemParamValidationError, SystemRunner, SystemRunnerState,
     },
     world::{
         FilteredResources, FilteredResourcesBuilder, FilteredResourcesMut,
@@ -201,6 +201,13 @@ impl ParamBuilder {
     pub fn query_filtered<'w, 's, D: QueryData + 'static, F: QueryFilter + 'static>(
     ) -> impl SystemParamBuilder<Query<'w, 's, D, F>> {
         Self
+    }
+
+    /// Helper method for adding a [`SystemRunner`] as a param, equivalent to [`SystemRunnerBuilder::from_system`]
+    pub fn system<'w, 's, In: SystemInput + 'static, Out: 'static, Marker>(
+        system: impl IntoSystem<In, Out, Marker>,
+    ) -> impl SystemParamBuilder<SystemRunner<'w, 's, In, Out>> {
+        SystemRunnerBuilder::from_system(system)
     }
 }
 
@@ -613,6 +620,32 @@ pub struct IfBuilder<T>(T);
 unsafe impl<P: SystemParam, B: SystemParamBuilder<P>> SystemParamBuilder<If<P>> for IfBuilder<B> {
     fn build(self, world: &mut World) -> <If<P> as SystemParam>::State {
         self.0.build(world)
+    }
+}
+
+/// A [`SystemParamBuilder`] for a [`SystemRunner`]
+pub struct SystemRunnerBuilder<In: SystemInput + 'static = (), Out: 'static = ()>(
+    BoxedSystem<In, Out>,
+);
+
+impl<In: SystemInput + 'static, Out: 'static> SystemRunnerBuilder<In, Out> {
+    /// Returns a `SystemRunnerBuilder` created from a given system.
+    pub fn from_system<S: IntoSystem<In, Out, M>, M>(system: S) -> Self {
+        Self(Box::new(S::into_system(system)))
+    }
+}
+
+// SAFETY: the state returned is always valid. In particular the access always
+// matches the contained system.
+unsafe impl<'w, 's, In: SystemInput + 'static, Out: 'static>
+    SystemParamBuilder<SystemRunner<'w, 's, In, Out>> for SystemRunnerBuilder<In, Out>
+{
+    fn build(mut self, world: &mut World) -> SystemRunnerState<In, Out> {
+        let access = self.0.initialize(world);
+        SystemRunnerState {
+            system: Some(self.0),
+            access,
+        }
     }
 }
 
