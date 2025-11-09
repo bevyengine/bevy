a0da3d9395cb9a76b279944f58174e4f6fec30cd
Jan Hohenheim <jan@hohenheim.ch>
2025-11-09 02:33:18 +0100

    Fix import

--- crates/bevy_asset/src/io/web.rs
+++ crates/bevy_asset/src/io/web.rs
@@ -221,7 +221,7 @@ mod web_asset_cache {
     use alloc::string::String;
     use alloc::vec::Vec;
     use bevy_platform::hash::FixedHasher;
-    use core::hash::{Hash, Hasher};
+    use core::hash::{BuildHasher as _, Hash, Hasher};
     use futures_lite::AsyncWriteExt;
     use std::io;
     use std::path::PathBuf;
