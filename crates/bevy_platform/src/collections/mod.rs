832d7ad22c7ce8ebd98487ee86b10bc8363c4e1f
Jan Hohenheim <jan@hohenheim.ch>
2025-11-09 01:34:16 +0100

    Re-export all collections

--- crates/bevy_platform/src/collections/mod.rs
+++ crates/bevy_platform/src/collections/mod.rs
@@ -1,4 +1,5 @@
-//! Provides [`HashMap`] and [`HashSet`] from [`hashbrown`] with some customized defaults.
+//! Provides [`HashMap`] and [`HashSet`] from [`hashbrown`] with some customized defaults,
+//! as well as re-exports of [`alloc::collections`].
 //!
 //! Also provides the [`HashTable`] type, which is specific to [`hashbrown`].
 
@@ -10,3 +11,7 @@ pub use hashbrown::Equivalent;
 pub mod hash_map;
 pub mod hash_set;
 pub mod hash_table;
+
+crate::cfg::alloc! {
+    pub use alloc::collections::{VecDeque, BTreeMap, BTreeSet, BinaryHeap, LinkedList, TryReserveError, vec_deque, btree_map, btree_set, binary_heap, linked_list};
+}
