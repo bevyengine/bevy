fe8ab7bb582e41ec76c55b92051b2f7de86bc64f
charlotte 🌸 <charlotte.c.mcelwain@gmail.com>
2025-11-17 14:53:28 -0800

    Update crates/bevy_render/src/renderer/mod.rs
    Co-authored-by: atlv <email@atlasdostal.com>

--- crates/bevy_render/src/renderer/mod.rs
+++ crates/bevy_render/src/renderer/mod.rs
@@ -111,7 +111,7 @@ pub fn render_system(
     if let Err(error) = time_sender.0.try_send(Instant::now()) {
         match error {
             bevy_time::TrySendError::Full(_) => {
-                panic!("The TimeSender channel should always be empty during render. You might need to add the bevy::core::time_system to your app.", );
+                panic!("The TimeSender channel should always be empty during render. You might need to add the bevy::core::time_system to your app.");
             }
             bevy_time::TrySendError::Disconnected(_) => {
                 // ignore disconnected errors, the main world probably just got dropped during shutdown
