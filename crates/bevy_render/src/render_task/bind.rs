5c0bd04a7e232a9acb21e9aba2f2e8780e58f896
Jasmine Schweitzer <jasmine.schweitzer@nominal.io>
2025-11-18 11:38:51 -0500

    API improvements

--- crates/bevy_render/src/render_task/bind.rs
+++ crates/bevy_render/src/render_task/bind.rs
@@ -3,7 +3,8 @@ use crate::render_resource::{
 };
 use bevy_derive::Deref;
 use wgpu::{
-    BindingResource, BindingType, BufferBindingType, StorageTextureAccess, TextureViewDimension,
+    BindingResource, BindingType, BufferBindingType, SamplerBindingType, StorageTextureAccess,
+    TextureViewDimension,
 };
 
 /// Corresponds to `var<storage, read> my_buffer: T` in a WGSL shader.
@@ -13,6 +14,14 @@ pub struct StorageBufferReadOnly<'a>(pub &'a Buffer);
 /// Corresponds to `var<storage, read_write> my_buffer: T` in a WGSL shader.
 #[derive(Clone, Deref)]
 pub struct StorageBufferReadWrite<'a>(pub &'a Buffer);
+
+/// Corresponds to `var<uniform> my_buffer: T` in a WGSL shader.
+#[derive(Clone, Deref)]
+pub struct UniformBuffer<'a>(pub &'a Buffer);
+
+/// Corresponds to `var<uniform> my_buffer: T` in a WGSL shader.
+#[derive(Clone, Deref)]
+pub struct DynamicUniformBuffer<'a>(pub &'a Buffer);
 
 /// Corresponds to `var my_texture: texture_2d<T>` or `var my_texture: texture_depth_2d` in a WGSL shader.
 #[derive(Clone, Deref)]
@@ -33,6 +42,14 @@ pub struct StorageTextureReadOnly<'a>(pub &'a TextureView);
 /// Corresponds to `var my_texture: texture_storage_2d<F, atomic>` in a WGSL shader.
 #[derive(Clone, Deref)]
 pub struct StorageTextureAtomic<'a>(pub &'a TextureView);
+
+/// Corresponds to `var my_sampler: sampler` in a WGSL shader.
+#[derive(Clone, Deref)]
+pub struct SamplerNonFiltering<'a>(pub &'a crate::render_resource::Sampler);
+
+/// Corresponds to `var my_sampler: sampler` in a WGSL shader.
+#[derive(Clone, Deref)]
+pub struct SamplerFiltering<'a>(pub &'a crate::render_resource::Sampler);
 
 impl<'a> IntoBinding<'a> for StorageBufferReadOnly<'a> {
     fn into_binding(self) -> BindingResource<'a> {
@@ -62,6 +79,40 @@ impl<'a> IntoBindGroupLayoutEntryBuilder for StorageBufferReadWrite<'a> {
         BindingType::Buffer {
             ty: BufferBindingType::Storage { read_only: false },
             has_dynamic_offset: false,
+            min_binding_size: None,
+        }
+        .into_bind_group_layout_entry_builder()
+    }
+}
+
+impl<'a> IntoBinding<'a> for UniformBuffer<'a> {
+    fn into_binding(self) -> BindingResource<'a> {
+        self.0.as_entire_binding()
+    }
+}
+
+impl<'a> IntoBindGroupLayoutEntryBuilder for UniformBuffer<'a> {
+    fn into_bind_group_layout_entry_builder(self) -> BindGroupLayoutEntryBuilder {
+        BindingType::Buffer {
+            ty: BufferBindingType::Uniform,
+            has_dynamic_offset: false,
+            min_binding_size: None,
+        }
+        .into_bind_group_layout_entry_builder()
+    }
+}
+
+impl<'a> IntoBinding<'a> for DynamicUniformBuffer<'a> {
+    fn into_binding(self) -> BindingResource<'a> {
+        self.0.as_entire_binding()
+    }
+}
+
+impl<'a> IntoBindGroupLayoutEntryBuilder for DynamicUniformBuffer<'a> {
+    fn into_bind_group_layout_entry_builder(self) -> BindGroupLayoutEntryBuilder {
+        BindingType::Buffer {
+            ty: BufferBindingType::Uniform,
+            has_dynamic_offset: true,
             min_binding_size: None,
         }
         .into_bind_group_layout_entry_builder()
@@ -150,5 +201,30 @@ impl<'a> IntoBindGroupLayoutEntryBuilder for StorageTextureAtomic<'a> {
             view_dimension: TextureViewDimension::D2,
         }
         .into_bind_group_layout_entry_builder()
+    }
+}
+
+impl<'a> IntoBinding<'a> for SamplerNonFiltering<'a> {
+    fn into_binding(self) -> BindingResource<'a> {
+        BindingResource::Sampler(self.0)
+    }
+}
+
+impl<'a> IntoBindGroupLayoutEntryBuilder for SamplerNonFiltering<'a> {
+    fn into_bind_group_layout_entry_builder(self) -> BindGroupLayoutEntryBuilder {
+        BindingType::Sampler(SamplerBindingType::NonFiltering)
+            .into_bind_group_layout_entry_builder()
+    }
+}
+
+impl<'a> IntoBinding<'a> for SamplerFiltering<'a> {
+    fn into_binding(self) -> BindingResource<'a> {
+        BindingResource::Sampler(self.0)
+    }
+}
+
+impl<'a> IntoBindGroupLayoutEntryBuilder for SamplerFiltering<'a> {
+    fn into_bind_group_layout_entry_builder(self) -> BindGroupLayoutEntryBuilder {
+        BindingType::Sampler(SamplerBindingType::Filtering).into_bind_group_layout_entry_builder()
     }
 }
