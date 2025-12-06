b81bcae515bdc820a42970539221dc8792646216
TeamDman <TeamDman9201@gmail.com>
2025-12-06 18:03:49 -0500

    Fix window icon integration test build failing on non-windows platforms

--- tests-integration/window-icon/src/main.rs
+++ tests-integration/window-icon/src/main.rs
@@ -10,9 +10,18 @@ fn main() {
                 title: "Bevy Window Icon Example".into(),
                 ..default()
             }),
-            primary_window_icon: Some(WindowIcon::PlatformSpecific(
-                WindowIconSource::ResourceName("aaa_my_icon".to_string()),
-            )),
+            primary_window_icon: Some({
+                #[cfg(target_os = "windows")]
+                {
+                    WindowIcon::PlatformSpecific(WindowIconSource::ResourceName(
+                        "aaa_my_icon".to_string(),
+                    ))
+                }
+                #[cfg(not(target_os = "windows"))]
+                {
+                    WindowIcon::PlatformDefault
+                }
+            }),
             ..default()
         }))
         .run();
