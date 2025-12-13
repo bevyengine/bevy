b8db6f7a54c33a5b75a8f52cb16d4d42dfd6ebf1
andriyDev <andriydzikh@gmail.com>
2025-12-13 00:50:59 -0800

    Fix a clippy lint.

--- crates/bevy_asset/src/processor/process.rs
+++ crates/bevy_asset/src/processor/process.rs
@@ -223,6 +223,10 @@ pub trait ErasedProcessor: Send + Sync {
     ) -> BoxedFuture<'a, Result<Box<dyn AssetMetaDyn>, ProcessError>>;
     /// Type-erased variant of [`Process::reader_required_features`].
     // Note: This takes &self just to be dyn compatible.
+    #[expect(
+        clippy::result_large_err,
+        reason = "this is only an error here because this isn't a future"
+    )]
     fn reader_required_features(
         &self,
         settings: &dyn Settings,
