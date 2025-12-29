e809c1320a74d4aa012c914623b2d71b4e363c4a
Aevyrie Roessler <aevyrie@gmail.com>
2025-12-28 23:34:56 -0800

    Revert toml formatter changes, fix line that depends on another PR.

--- examples/stress_tests/bevymark_3d.rs
+++ examples/stress_tests/bevymark_3d.rs
@@ -121,7 +121,6 @@ fn main() {
             FrameTimeDiagnosticsPlugin::default(),
             LogDiagnosticsPlugin::default(),
         ))
-        .insert_resource(StaticTransformOptimizations::disabled())
         .insert_resource(WinitSettings::continuous())
         .insert_resource(args)
         .insert_resource(BevyCounter {
