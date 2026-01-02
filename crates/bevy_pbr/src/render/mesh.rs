d1cd1e5173dc39b08904ef3ae60cab413ce21256
Aevyrie Roessler <aevyrie@gmail.com>
2026-01-01 18:16:46 -0800

    Refactor buffered channel logic into a struct.

--- crates/bevy_pbr/src/render/mesh.rs
+++ crates/bevy_pbr/src/render/mesh.rs
@@ -1750,8 +1750,139 @@ pub fn set_mesh_motion_vector_flags(
     }
 }
 
+/// A channel that uses thread locals to buffer messages for efficient parallel processing.
+///
+/// Cache this channel in a [`Local`] between system runs to reuse allocated memory.
+///
+/// This is faster than sending each message individually into a channel when communicating between
+/// tasks. Unlike `Parallel`, this allows you to execute a consuming task while producing tasks are
+/// concurrently sending data into the channel, enabling you to run serial processing concurrently
+/// with the parallel processing step of an algorithm.
+///
+/// ### Usage
+///
+/// ```
+/// use bevy_app::{App, ScheduleRunnerPlugin, TaskPoolPlugin, Update};
+/// use bevy_diagnostic::FrameCountPlugin;
+/// use bevy_ecs::system::Local;
+/// use bevy_pbr::BufferedChannel;
+/// use bevy_tasks::{ComputeTaskPool, TaskPool, TaskPoolBuilder};
+///
+/// App::new().add_plugins(TaskPoolPlugin::default()).add_systems(Update, parallel_system).update();
+///
+/// fn parallel_system(channel: Local<BufferedChannel<u32>>) {
+///     // Every update, set up the channel to get a sender and receiver.
+///     let (rx, tx) = channel.setup();
+///
+///     ComputeTaskPool::get().scope(|scope| {
+///         // Spawn a few producing tasks in parallel that send data using a buffered channel
+///         for _ in 0..5 {
+///             let tx = tx.clone();
+///             scope.spawn(async move {
+///                 for i in 0..10_000 {
+///                     tx.send(i);
+///                 }
+///             });
+///         }
+///         drop(tx);
+///
+///         // Spawn a single consumer task that reads from the producers as
+///         scope.spawn(async move {
+///             // Note we have to `flatten()`, as we are receiving batches of values in `Vec`s.
+///             let total: u32 = rx.iter().flatten().sum();
+///             assert_eq!(total, 249_975_000);
+///         });
+///     });
+/// }
+/// ```
+pub struct BufferedChannel<T: Send> {
+    /// The size of batches of buffered data sent through the channel.
+    pub chunk_size: usize,
+    /// Thread-local buffer of `T`.
+    buffer: Parallel<Vec<T>>,
+}
+
+impl<T: Send> Default for BufferedChannel<T> {
+    fn default() -> Self {
+        Self {
+            // This was tuned based on benchmarks across a wide range of sizes.
+            chunk_size: 1024,
+            buffer: Parallel::default(),
+        }
+    }
+}
+
+/// A [`BufferedChannel`] sender that buffers messages in a thread local `Vec`, flushing messages
+/// from the thread local when the buffered sender is dropped.
+pub struct BufferedSender<'a, T: Send> {
+    chunk_size: usize,
+    parallel: &'a Parallel<Vec<T>>,
+    // Use a `std` channel, the speed doesn't matter much because we are buffering.
+    tx: std::sync::mpsc::Sender<Vec<T>>,
+}
+
+impl<T: Send> BufferedChannel<T> {
+    pub fn setup(&self) -> (std::sync::mpsc::Receiver<Vec<T>>, BufferedSender<'_, T>) {
+        let (tx, rx) = std::sync::mpsc::channel();
+        (
+            rx,
+            BufferedSender {
+                chunk_size: self.chunk_size,
+                parallel: &self.buffer,
+                tx,
+            },
+        )
+    }
+}
+
+impl<'a, T: Send> BufferedSender<'a, T> {
+    /// Send an item. This is buffered and will only be sent into the channel once the number of
+    /// items in the thread local has exceeded [`BufferedChannel::chunk_size`], or the sender is
+    /// dropped and the thread local buffer is automatically flushed.
+    pub fn send(&self, item: T) {
+        let mut local = self.parallel.borrow_local_mut();
+        local.push(item);
+        if local.len() >= self.chunk_size {
+            let _ = self.tx.send(core::mem::take(&mut *local));
+        }
+    }
+
+    fn flush(&self) {
+        let mut local = self.parallel.borrow_local_mut();
+        if !local.is_empty() {
+            let _ = self.tx.send(core::mem::take(&mut *local));
+        }
+    }
+}
+
+impl<'a, T: Send> Clone for BufferedSender<'a, T> {
+    fn clone(&self) -> Self {
+        Self {
+            chunk_size: self.chunk_size,
+            parallel: self.parallel,
+            tx: self.tx.clone(),
+        }
+    }
+}
+
+impl<'a, T: Send> Drop for BufferedSender<'a, T> {
+    fn drop(&mut self) {
+        self.flush();
+    }
+}
+
+#[derive(Default)]
+pub struct GpuMeshBuildingChunks {
+    prepared: BufferedChannel<(
+        MainEntity,
+        RenderMeshInstanceGpuPrepared,
+        Option<MeshCullingData>,
+    )>,
+    reextract: BufferedChannel<MainEntity>,
+    removed: BufferedChannel<MainEntity>,
+}
+
 /// Creates the [`RenderMeshInstanceGpu`]s and [`MeshInputUniform`]s when GPU
-/// mesh uniforms are built.
 pub fn collect_meshes_for_gpu_building(
     render_mesh_instances: ResMut<RenderMeshInstances>,
     batched_instance_buffers: ResMut<
@@ -1766,16 +1897,7 @@ pub fn collect_meshes_for_gpu_building(
     skin_uniforms: Res<SkinUniforms>,
     frame_count: Res<FrameCount>,
     mut meshes_to_reextract_next_frame: ResMut<MeshesToReextractNextFrame>,
-    prepared_chunk: Local<
-        Parallel<
-            Vec<(
-                MainEntity,
-                RenderMeshInstanceGpuPrepared,
-                Option<MeshCullingData>,
-            )>,
-        >,
-    >,
-    reextract_chunk: Local<Parallel<Vec<MainEntity>>>,
+    chunks: Local<GpuMeshBuildingChunks>,
 ) {
     let RenderMeshInstances::GpuBuilding(render_mesh_instances) =
         render_mesh_instances.into_inner()
@@ -1794,25 +1916,18 @@ pub fn collect_meshes_for_gpu_building(
     } = batched_instance_buffers.into_inner();
     previous_input_buffer.clear();
 
-    /// The size of batches of data sent through the channel from parallel workers to the consumer.
-    /// This was tuned based on benchmarks across a wide range of entity counts.
-    const CHUNK_SIZE: usize = 1024;
-
     // Channels used by parallel workers to send data to the single consumer.
-    let (prepared_tx, prepared_rx) = std::sync::mpsc::channel::<
-        Vec<(
-            MainEntity,
-            RenderMeshInstanceGpuPrepared,
-            Option<MeshCullingData>,
-        )>,
-    >();
-    let (removed_tx, removed_rx) = std::sync::mpsc::channel::<Vec<MainEntity>>();
-    let (reextract_tx, reextract_rx) = std::sync::mpsc::channel::<Vec<MainEntity>>();
+    let (prepared_rx, prepared_tx) = chunks.prepared.setup();
+    let (reextract_rx, reextract_tx) = chunks.reextract.setup();
+    let (removed_rx, removed_tx) = chunks.removed.setup();
 
     // A single worker that consumes the meshes prepared in parallel by multiple producers.
     // This part of the workload cannot be parallelized due to the shared mutable state.
     let mesh_consumer_worker = &mut Some(move || {
         let _span = info_span!("prepared_mesh_consumer").entered();
+        // In an ideal world, these three for loops would be a single loop, so we can process
+        // incoming batches no matter which channel they come into, instead of one at a time.
+        // Probably doable using async_channel and awaiting all three receivers at once.
         for (entity, prepared, mesh_culling_builder) in prepared_rx.iter().flatten() {
             let Some(instance_data_index) = prepared.update(
                 entity,
@@ -1838,8 +1953,6 @@ pub fn collect_meshes_for_gpu_building(
     });
 
     // Reference data shared between tasks
-    let prepared_chunk = &prepared_chunk;
-    let reextract_chunk = &reextract_chunk;
     let mesh_allocator = &mesh_allocator;
     let mesh_material_ids = &mesh_material_ids;
     let render_material_bindings = &render_material_bindings;
@@ -1878,44 +1991,28 @@ pub fn collect_meshes_for_gpu_building(
                     let removed_tx = removed_tx.clone();
                     scope.spawn(async move {
                         let _span = info_span!("prepared_mesh_producer").entered();
-                        let prepared_chunk = &mut prepared_chunk.borrow_local_mut();
-                        let reextract_chunk = &mut reextract_chunk.borrow_local_mut();
                         changed
                             .drain(..)
-                            .for_each(|(entity, mesh_instance_builder)| {
-                                match mesh_instance_builder.prepare(
-                                    entity,
-                                    mesh_allocator,
-                                    mesh_material_ids,
-                                    render_material_bindings,
-                                    render_lightmaps,
-                                    skin_uniforms,
-                                    frame_count,
-                                ) {
-                                    Some(prepared) => prepared_chunk.push((entity, prepared, None)),
-                                    None => reextract_chunk.push(entity),
-                                }
+                            .for_each(
+                                |(entity, mesh_instance_builder)| match mesh_instance_builder
+                                    .prepare(
+                                        entity,
+                                        mesh_allocator,
+                                        mesh_material_ids,
+                                        render_material_bindings,
+                                        render_lightmaps,
+                                        skin_uniforms,
+                                        frame_count,
+                                    ) {
+                                    Some(prepared) => {
+                                        prepared_tx.send((entity, prepared, None));
+                                    }
+                                    None => reextract_tx.send(entity),
+                                },
+                            );
 
-                                // Once a local batch of work has grown large enough, we send it to
-                                // the consumer to start processing immediately, so it does not need
-                                // to wait for us to process the entire queue.
-                                if prepared_chunk.len() >= CHUNK_SIZE {
-                                    prepared_tx.send(core::mem::take(prepared_chunk)).ok();
-                                }
-                                if reextract_chunk.len() >= CHUNK_SIZE {
-                                    reextract_tx.send(core::mem::take(reextract_chunk)).ok();
-                                }
-                            });
-
-                        // Send any remaining data to be processed
-                        if !prepared_chunk.is_empty() {
-                            prepared_tx.send(core::mem::take(prepared_chunk)).ok();
-                        }
-                        if !reextract_chunk.is_empty() {
-                            reextract_tx.send(core::mem::take(reextract_chunk)).ok();
-                        }
-                        if !removed.is_empty() {
-                            removed_tx.send(core::mem::take(removed)).ok();
+                        for entity in removed.drain(..) {
+                            removed_tx.send(entity);
                         }
                     });
                 }
@@ -1929,8 +2026,6 @@ pub fn collect_meshes_for_gpu_building(
                     let removed_tx = removed_tx.clone();
                     scope.spawn(async move {
                         let _span = info_span!("prepared_mesh_producer").entered();
-                        let prepared_chunk = &mut prepared_chunk.borrow_local_mut();
-                        let reextract_chunk = &mut reextract_chunk.borrow_local_mut();
                         changed.drain(..).for_each(
                             |(entity, mesh_instance_builder, mesh_culling_builder)| {
                                 match mesh_instance_builder.prepare(
@@ -1942,35 +2037,20 @@ pub fn collect_meshes_for_gpu_building(
                                     skin_uniforms,
                                     frame_count,
                                 ) {
-                                    Some(prepared) => prepared_chunk.push((
-                                        entity,
-                                        prepared,
-                                        Some(mesh_culling_builder),
-                                    )),
-                                    None => reextract_chunk.push(entity),
-                                }
-
-                                // Once a local batch of work has grown large enough, we send it to
-                                // the consumer to start processing immediately, so it does not need
-                                // to wait for us to process the entire queue.
-                                if prepared_chunk.len() >= CHUNK_SIZE {
-                                    prepared_tx.send(core::mem::take(prepared_chunk)).ok();
-                                }
-                                if reextract_chunk.len() >= CHUNK_SIZE {
-                                    reextract_tx.send(core::mem::take(reextract_chunk)).ok();
+                                    Some(prepared) => {
+                                        prepared_tx.send((
+                                            entity,
+                                            prepared,
+                                            Some(mesh_culling_builder),
+                                        ));
+                                    }
+                                    None => reextract_tx.send(entity),
                                 }
                             },
                         );
 
-                        // Send any remaining data to be processed
-                        if !prepared_chunk.is_empty() {
-                            prepared_tx.send(core::mem::take(prepared_chunk)).ok();
-                        }
-                        if !reextract_chunk.is_empty() {
-                            reextract_tx.send(core::mem::take(reextract_chunk)).ok();
-                        }
-                        if !removed.is_empty() {
-                            removed_tx.send(core::mem::take(removed)).ok();
+                        for entity in removed.drain(..) {
+                            removed_tx.send(entity);
                         }
                     });
                 }
