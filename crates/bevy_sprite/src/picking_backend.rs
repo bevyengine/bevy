3d1b471bfdb858c6b44c290ff20750562e508c5a
mirsella <mirsella@protonmail.com>
2026-01-07 10:46:20 +0100

    Add warning log for sprite picking failure on compressed textures

--- crates/bevy_sprite/src/picking_backend.rs
+++ crates/bevy_sprite/src/picking_backend.rs
@@ -20,6 +20,7 @@ use bevy_camera::{
 use bevy_color::Alpha;
 use bevy_ecs::prelude::*;
 use bevy_image::prelude::*;
+use bevy_log::warn;
 use bevy_math::{prelude::*, FloatExt};
 use bevy_picking::backend::prelude::*;
 use bevy_reflect::prelude::*;
@@ -243,12 +244,21 @@ fn sprite_picking(
                                     break 'valid_pixel true;
                                 };
                                 // grab pixel and check alpha
-                                let Ok(color) = image.get_color_at(
+                                let color = match image.get_color_at(
                                     cursor_pixel_space.x as u32,
                                     cursor_pixel_space.y as u32,
-                                ) else {
-                                    // We don't know how to interpret the pixel.
-                                    break 'valid_pixel false;
+                                ) {
+                                    Ok(color) => color,
+                                    Err(err) => {
+                                        warn!(
+                                            "Failed to get pixel color for sprite picking on entity {:?}: {:?}. \
+                                            This is probably caused by the use of a compressed texture format. \
+                                            Consider using `SpritePickingMode::BoundingBox`.",
+                                            entity,
+                                            err
+                                        );
+                                        break 'valid_pixel false;
+                                    }
                                 };
                                 // Check the alpha is above the cutoff.
                                 color.alpha() > cutoff
