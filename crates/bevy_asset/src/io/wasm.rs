cd977df8eb98457d190c89469bb81bd480a3051c
Carter Anderson <mcanders1@gmail.com>
2026-01-07 13:43:49 -0800

    bevy_asset: support upgrading Reader to SeekableReader (#22182)
    # Objective
    
    #22104 added `AsyncSeek` support, but it has some downsides:
    
    - It is very "loose": it relies on `AssetLoader` to "attest" that it
    needs the seek feature. However, AssetLoaders both have access to the
    `AsyncSeek` API no matter what (it is a supertrait of Reader), and
    AssetReaders provide `AsyncSeek` behavior even if it isn't requested. In
    practice this is likely to create situations where AssetLoaders don't
    request `AsyncSeek` and just rely on it being provided by default,
    causing unexpected incompatibilities when AssetLoader consumers try to
    use other AssetReaders that don't support AsyncSeek.
    - It encourages building "fallback" behavior into the AssetReader in
    cases where AsyncSeek cannot be supported directly (ex: read the while
    contents into a Vec). From the perspective of loaders, this _silently_
    changes the performance characteristics, and _forces_ a specific kind of
    fallback behavior. It would be better if the fallback behavior was in
    the hands of the AssetLoader.
    
    ## Solution
    
    - Remove `ReaderRequiredFeatures` and associated functionality 
    - Add a new `SeekableReader` trait, where `SeekableReader: Reader +
    AsyncSeek`.
    - Add a new `Reader::seekable(&mut self) -> Result<&mut dyn
    SeekableReader, ReaderNotSeekableError>`, which can either fail or cast
    to `SeekableReader`, if that is supported.
    
    ```rust
    let seekable_reader = reader.seekable()?;
    seekable_reader.seek(SeekFrom::Start(10)).await?;
    ```
    
    This gives `AssetLoader` implementers more clarity when it comes to
    `Reader` feature support, gives them more autonomy over fallback
    behavior, makes our APIs more static, and cuts down on the complexity of
    the system as a whole.
    
    ---------
    
    Co-authored-by: andriyDev <andriydzikh@gmail.com>
    Co-authored-by: Jan Hohenheim <jan@hohenheim.ch>

--- crates/bevy_asset/src/io/wasm.rs
+++ crates/bevy_asset/src/io/wasm.rs
@@ -1,6 +1,5 @@
 use crate::io::{
-    get_meta_path, AssetReader, AssetReaderError, EmptyPathStream, PathStream, Reader,
-    ReaderRequiredFeatures, VecReader,
+    get_meta_path, AssetReader, AssetReaderError, EmptyPathStream, PathStream, Reader, VecReader,
 };
 use alloc::{borrow::ToOwned, boxed::Box, format};
 use js_sys::{Uint8Array, JSON};
@@ -93,11 +92,7 @@ impl HttpWasmAssetReader {
 }
 
 impl AssetReader for HttpWasmAssetReader {
-    async fn read<'a>(
-        &'a self,
-        path: &'a Path,
-        _required_features: ReaderRequiredFeatures,
-    ) -> Result<impl Reader + 'a, AssetReaderError> {
+    async fn read<'a>(&'a self, path: &'a Path) -> Result<impl Reader + 'a, AssetReaderError> {
         let path = self.root_path.join(path);
         self.fetch_bytes(path).await
     }
