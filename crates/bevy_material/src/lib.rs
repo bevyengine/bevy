d74d23b6a70a76b99f32b9efd5d20de729a0695b
atlas dostal <rodol@rivalrebels.com>
2026-01-08 07:50:41 -0500

    doc fixes

--- crates/bevy_material/src/lib.rs
+++ crates/bevy_material/src/lib.rs
@@ -38,7 +38,7 @@ pub mod prelude {
     pub use crate::alpha::AlphaMode;
 }
 
-/// Common [`Material`] properties, calculated for a specific material instance.
+/// Common material properties, calculated for a specific material instance.
 #[derive(Default)]
 pub struct MaterialProperties {
     /// Is this material should be rendered by the deferred renderer when.
@@ -46,25 +46,29 @@ pub struct MaterialProperties {
     pub render_method: OpaqueRendererMethod,
     /// The [`AlphaMode`] of this material.
     pub alpha_mode: AlphaMode,
-    /// The bits in the [`MeshPipelineKey`] for this material.
+    /// The bits in the [`ErasedMeshPipelineKey`] for this material.
     ///
     /// These are precalculated so that we can just "or" them together in
-    /// [`queue_material_meshes`].
+    /// [`queue_material_meshes`](https://docs.rs/bevy/latest/bevy/pbr/fn.queue_material_meshes.html).
     pub mesh_pipeline_key_bits: ErasedMeshPipelineKey,
     /// Add a bias to the view depth of the mesh which can be used to force a specific render order
     /// for meshes with equal depth, to avoid z-fighting.
     /// The bias is in depth-texture units so large values may be needed to overcome small depth differences.
     pub depth_bias: f32,
-    /// Whether the material would like to read from [`ViewTransmissionTexture`](bevy_core_pipeline::core_3d::ViewTransmissionTexture).
+    /// Whether the material would like to read from
+    /// [`ViewTransmissionTexture`](https://docs.rs/bevy/latest/bevy/core_pipeline/core_3d/struct.ViewTransmissionTexture.html).
     ///
-    /// This allows taking color output from the [`Opaque3d`] pass as an input, (for screen-space transmission) but requires
-    /// rendering to take place in a separate [`Transmissive3d`] pass.
+    /// This allows taking color output from the [`Opaque3d`](https://docs.rs/bevy/latest/bevy/core_pipeline/core_3d/struct.Opaque3d.html)
+    /// pass as an input, (for screen-space transmission) but requires rendering to take place in a separate
+    /// [`Transmissive3d`](https://docs.rs/bevy/latest/bevy/core_pipeline/core_3d/struct.Transmissive3d.html) pass.
     pub reads_view_transmission_texture: bool,
     pub render_phase_type: RenderPhaseType,
     pub material_layout: Option<BindGroupLayoutDescriptor>,
-    /// Backing array is a size of 4 because the `StandardMaterial` needs 4 draw functions by default
+    /// Backing array is a size of 4 because the [`StandardMaterial`](https://docs.rs/bevy/latest/bevy/pbr/struct.StandardMaterial.html)
+    /// needs 4 draw functions by default
     pub draw_functions: SmallVec<[(InternedDrawFunctionLabel, DrawFunctionId); 4]>,
-    /// Backing array is a size of 3 because the `StandardMaterial` has 3 custom shaders (`frag`, `prepass_frag`, `deferred_frag`) which is the
+    /// Backing array is a size of 3 because the [`StandardMaterial`](https://docs.rs/bevy/latest/bevy/pbr/struct.StandardMaterial.html)
+    /// has 3 custom shaders (`frag`, `prepass_frag`, `deferred_frag`) which is the
     /// most common use case
     pub shaders: SmallVec<[(InternedShaderLabel, Handle<Shader>); 3]>,
     /// Whether this material *actually* uses bindless resources, taking the
