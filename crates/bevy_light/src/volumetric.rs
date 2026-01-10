5c08e0a84f86abda1cfb99b3c5e05263301317ab
Lucas Farias <lucasfnf@gmail.com>
2026-01-09 22:59:06 -0300

    Wording on docs for `VolumetricLight`

--- crates/bevy_light/src/volumetric.rs
+++ crates/bevy_light/src/volumetric.rs
@@ -10,7 +10,7 @@ use bevy_transform::components::Transform;
 /// Add this component to a [`DirectionalLight`](crate::DirectionalLight) with a shadow map
 /// (`shadows_enabled: true`) to make volumetric fog interact with it.
 ///
-/// This allows the light to cause atmospheric scattering
+/// This allows the light to affect the atmosphere
 /// and generate light shafts/god rays.
 #[derive(Clone, Copy, Component, Default, Debug, Reflect)]
 #[reflect(Component, Default, Debug, Clone)]
