1f9e2d2a2c6e485eadaa73068031da3c8a64bd85
ickshonpe <david.curthoys@googlemail.com>
2026-01-14 13:51:08 +0000

    Updated example

--- examples/ui/generic_font_families.rs
+++ examples/ui/generic_font_families.rs
@@ -1,6 +1,13 @@
 //! This example demonstrates generic font families
 
-use bevy::{color::palettes::css::YELLOW, prelude::*, text::CosmicFontSystem};
+use bevy::{
+    color::palettes::{
+        css::{WHEAT, YELLOW},
+        tailwind::{ZINC_600, ZINC_800},
+    },
+    prelude::*,
+    text::CosmicFontSystem,
+};
 
 fn main() {
     let mut app = App::new();
@@ -16,31 +23,39 @@ fn main() {
     app.run();
 }
 
-fn setup(mut commands: Commands) {
+const FONT_SIZE: f32 = 25.;
+
+fn setup(mut commands: Commands, font_system: Res<CosmicFontSystem>) {
     // UI camera
     commands.spawn(Camera2d);
 
     commands
         .spawn((Node {
             display: Display::Grid,
-            grid_template_columns: vec![GridTrack::flex(1.0), GridTrack::flex(1.0)],
+            grid_template_columns: vec![RepeatedGridTrack::fr(3, 1.)],
             margin: UiRect::AUTO,
-            row_gap: px(15),
-            column_gap: px(30),
+            row_gap: px(25),
+            column_gap: px(15),
             ..Default::default()
         },))
         .with_children(|builder| {
             builder.spawn((
                 Node {
                     justify_self: JustifySelf::Center,
-                    grid_column: GridPlacement::span(2),
+                    grid_column: GridPlacement::span(3),
                     margin: UiRect::bottom(px(15)),
                     ..default()
                 },
                 Text::new("Generic Font Families"),
-                TextFont::from_font_size(35.),
+                TextFont::from_font_size(FONT_SIZE),
                 Underline,
             ));
+
+            let outline = Outline {
+                color: ZINC_600.into(),
+                width: px(2.),
+                offset: px(4.),
+            };
 
             for (source, description) in [
                 (FontSource::SansSerif, "generic sans serif font"),
@@ -50,19 +65,29 @@ fn setup(mut commands: Commands) {
                 (FontSource::Monospace, "generic monospace font"),
             ] {
                 builder.spawn((
-                    Node {
-                        justify_self: JustifySelf::End,
-                        ..default()
-                    },
-                    Text::new(format!("FontSource::{source:?}")),
-                    TextFont::from_font_size(40.),
-                    TextColor(YELLOW.into()),
+                    Text::new(description),
+                    TextFont::from(source.clone()).with_font_size(FONT_SIZE),
+                    TextColor(WHEAT.into()),
+                    TextLayout::new_with_justify(Justify::Center),
+                    outline,
                 ));
 
                 builder.spawn((
-                    Text::new(description),
-                    TextFont::from(source).with_font_size(40.0),
-                    TextColor::WHITE,
+                    Text::new(format!("FontSource::{source:?}")),
+                    TextFont::from_font_size(FONT_SIZE),
+                    TextColor(YELLOW.into()),
+                    TextLayout::new_with_justify(Justify::Center),
+                    outline,
+                ));
+
+                // Get the family name for the `FontSource` from `CosmicFontSystem`.
+                // The unwrap here is safe, `get_family_name` only returns `None` if the source is a handle.
+                let family_name = font_system.get_family(&source).unwrap();
+                builder.spawn((
+                    Text::new(family_name),
+                    TextFont::from_font_size(FONT_SIZE),
+                    TextLayout::new_with_justify(Justify::Center),
+                    outline,
                 ));
             }
         });
