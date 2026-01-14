33c84c1ebcbc1ba089d163899a882d0af8807869
molikto <molikto@gmail.com>
2026-01-14 17:59:40 +0800

    Update crates/bevy_reflect/src/impls/core/primitives.rs
    Co-authored-by: Kevin Chen <chen.kevin.f@gmail.com>

--- crates/bevy_reflect/src/impls/core/primitives.rs
+++ crates/bevy_reflect/src/impls/core/primitives.rs
@@ -240,7 +240,7 @@ impl PartialReflect for &'static str {
 
     fn reflect_partial_cmp(&self, value: &dyn PartialReflect) -> Option<core::cmp::Ordering> {
         if let Some(value) = value.try_downcast_ref::<Self>() {
-            Some(PartialOrd::partial_cmp(self, value)?)
+            PartialOrd::partial_cmp(self, value)
         } else {
             None
         }
