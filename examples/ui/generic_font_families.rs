633917dc4f54176794211513f04cb053eefadd3a
ickshonpe <david.curthoys@googlemail.com>
2026-01-14 11:55:13 +0000

    Improved the example

--- examples/ui/generic_font_families.rs
+++ examples/ui/generic_font_families.rs
@@ -1,11 +1,13 @@
 //! This example demonstrates generic font families
 
-use bevy::{prelude::*, text::CosmicFontSystem};
+use bevy::{color::palettes::css::YELLOW, prelude::*, text::CosmicFontSystem};
 
 fn main() {
     let mut app = App::new();
     app.add_plugins(DefaultPlugins).add_systems(Startup, setup);
 
+    // The default font will be used where there is no system font matching the
+    // generic font variant's font name stored in Cosmic Text's `Database`.
     app.world_mut()
         .resource_mut::<CosmicFontSystem>()
         .db_mut()
@@ -18,35 +20,50 @@ fn setup(mut commands: Commands) {
     // UI camera
     commands.spawn(Camera2d);
 
-    commands.spawn((
-        Node {
-            flex_direction: FlexDirection::Column,
-            left: px(250),
-            top: px(250),
-            row_gap: px(10.),
+    commands
+        .spawn((Node {
+            display: Display::Grid,
+            grid_template_columns: vec![GridTrack::flex(1.0), GridTrack::flex(1.0)],
+            margin: UiRect::AUTO,
+            row_gap: px(15),
+            column_gap: px(30),
             ..Default::default()
-        },
-        children![
-            (
-                Text::new("generic sans serif font"),
-                TextFont::from(FontSource::SansSerif).with_font_size(30.)
-            ),
-            (
-                Text::new("generic serif font"),
-                TextFont::from(FontSource::Serif).with_font_size(30.)
-            ),
-            (
-                Text::new("generic fantasy font"),
-                TextFont::from(FontSource::Fantasy).with_font_size(30.)
-            ),
-            (
-                Text::new("generic cursive font"),
-                TextFont::from(FontSource::Cursive).with_font_size(30.)
-            ),
-            (
-                Text::new("generic monospace font"),
-                TextFont::from(FontSource::Monospace).with_font_size(30.)
-            )
-        ],
-    ));
+        },))
+        .with_children(|builder| {
+            builder.spawn((
+                Node {
+                    justify_self: JustifySelf::Center,
+                    grid_column: GridPlacement::span(2),
+                    margin: UiRect::bottom(px(15)),
+                    ..default()
+                },
+                Text::new("Generic Font Families"),
+                TextFont::from_font_size(35.),
+                Underline,
+            ));
+
+            for (source, description) in [
+                (FontSource::SansSerif, "generic sans serif font"),
+                (FontSource::Serif, "generic serif font"),
+                (FontSource::Fantasy, "generic fantasy font"),
+                (FontSource::Cursive, "generic cursive font"),
+                (FontSource::Monospace, "generic monospace font"),
+            ] {
+                builder.spawn((
+                    Node {
+                        justify_self: JustifySelf::End,
+                        ..default()
+                    },
+                    Text::new(format!("FontSource::{source:?}")),
+                    TextFont::from_font_size(40.),
+                    TextColor(YELLOW.into()),
+                ));
+
+                builder.spawn((
+                    Text::new(description),
+                    TextFont::from(source).with_font_size(40.0),
+                    TextColor::WHITE,
+                ));
+            }
+        });
 }
