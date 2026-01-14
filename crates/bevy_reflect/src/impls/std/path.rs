208ac6d7f579cb1a7599c6ca667e857546e1e6d6
molikto <molikto@gmail.com>
2026-01-14 17:59:33 +0800

    Update crates/bevy_reflect/src/impls/std/path.rs
    Co-authored-by: Kevin Chen <chen.kevin.f@gmail.com>

--- crates/bevy_reflect/src/impls/std/path.rs
+++ crates/bevy_reflect/src/impls/std/path.rs
@@ -97,7 +97,7 @@ impl PartialReflect for &'static Path {
 
     fn reflect_partial_cmp(&self, value: &dyn PartialReflect) -> Option<core::cmp::Ordering> {
         if let Some(value) = value.try_downcast_ref::<Self>() {
-            Some(PartialOrd::partial_cmp(self, value)?)
+            PartialOrd::partial_cmp(self, value)
         } else {
             None
         }
