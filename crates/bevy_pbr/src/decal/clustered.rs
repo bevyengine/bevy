cdca25c01520de9311e6846532e63e43452ebdd4
Patrick Walton <pcwalton@mimiga.net>
2026-01-15 01:25:14 -0800

    Mark fields as public that are necessary to use indirect light in custom PBR shaders.
    My [fork] of [Hanabi], the Bevy VFX system, now supports full PBR
    rendering, including indirect light. To do that, it has to create its
    own bind groups containing the light probe and irradiance volume
    textures. Right now, it can't do that, because critical fields in
    `RenderClusteredDecals`, `EnvironmentMapIds`, and
    `RenderViewLightProbes` are private.
    
    This commit changes the minimal set of fields from crate-private to
    public in order to allow Hanabi to render particles using a fully-custom
    shader and pipeline while taking maximal advantage of Bevy's PBR code.
    For reference, with this change, as well as the CPU-side setup, the GPU
    code to use Bevy's PBR implementation is reduced to:
    
    ```wgsl
    var pbr_input = pbr_types::pbr_input_new();
    ... populate `pbr_input` ...
    color = pbr_functions::apply_pbr_lighting(pbr_input);
    ```
    
    My [Hanabi branch for PBR particles] is available for reference if
    desired.
    
    [fork]: https://github.com/pcwalton/bevy_hanabi
    
    [Hanabi]: https://github.com/djeedai/bevy_hanabi
    
    [Hanabi branch for PBR particles]: https://github.com/pcwalton/bevy_hanabi/tree/lit-particles

--- crates/bevy_pbr/src/decal/clustered.rs
+++ crates/bevy_pbr/src/decal/clustered.rs
@@ -69,7 +69,7 @@ pub struct RenderClusteredDecals {
     /// Maps an index in the shader binding array to the associated decal image.
     ///
     /// [`Self::texture_to_binding_index`] holds the inverse mapping.
-    binding_index_to_textures: Vec<AssetId<Image>>,
+    pub binding_index_to_textures: Vec<AssetId<Image>>,
     /// Maps a decal image to the shader binding array.
     ///
     /// [`Self::binding_index_to_textures`] holds the inverse mapping.
