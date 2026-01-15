ad12b5f62ebcd5c927f8d196ad4e25eeb3552216
Kevin Chen <chen.kevin.f@gmail.com>
2026-01-15 00:01:23 -0500

    docs: add logo stuff too since that seems standard

--- crates/bevy_material/src/lib.rs
+++ crates/bevy_material/src/lib.rs
@@ -9,6 +9,10 @@
 )]
 #![cfg_attr(any(docsrs, docsrs_dep), feature(rustdoc_internals))]
 #![cfg_attr(docsrs, feature(doc_cfg))]
+#![doc(
+    html_logo_url = "https://bevy.org/assets/icon.png",
+    html_favicon_url = "https://bevy.org/assets/icon.png"
+)]
 
 use bevy_asset::Handle;
 use bevy_shader::Shader;
