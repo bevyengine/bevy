9d96a10952569d962f13afc2335092a9711e13e5
molikto <molikto@gmail.com>
2026-01-16 03:36:22 +0800

    reflect PartialOrd (#22452)
    # Objective
    
    be able to compare ordering of two `dyn PartialReflect` values
    
    ## Solution.
    
    add #[reflect(PartialOrd)] and method
    `PartialReflect::reflect_partial_cmp`. Default impl are added when makes
    sense.
    
    Also fixed a bug that `reflect_partial_eq` on `Enum` doesn't compare
    field length, causing `A::V(1) == A2::V(1, 2)`
    
    
    ## Testing
    
    Unit tests are added.
    
    ---------
    
    Co-authored-by: Kevin Chen <chen.kevin.f@gmail.com>

--- crates/bevy_reflect/derive/src/impls/tuple_structs.rs
+++ crates/bevy_reflect/derive/src/impls/tuple_structs.rs
@@ -32,6 +32,7 @@ pub(crate) fn impl_tuple_struct(reflect_struct: &ReflectStruct) -> proc_macro2::
         reflect_struct.meta(),
         || Some(quote!(#bevy_reflect_path::tuple_struct::tuple_struct_partial_eq)),
         || None,
+        || Some(quote!(#bevy_reflect_path::tuple_struct::tuple_struct_partial_cmp)),
     );
     let clone_fn = reflect_struct.get_clone_impl();
 
