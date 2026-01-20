a53c87b4dea3a5f025275b5254f78be91f9a78ef
Patrick Walton <pcwalton@mimiga.net>
2026-01-20 12:52:43 -0800

    Implement gradual falloff and blending for light probes.
    Currently, if a fragment overlaps multiple reflection probes and/or
    irradiance volumes, Bevy arbitrarily chooses one to provide diffuse
    and/or specular light. This is unsightly. The standard approach is to
    accumulate radiance and irradiance as a weighted sum. In most engines,
    light probes have an artist-controllable *falloff* range, which causes
    the weight of each probe to diminish gradually from the center of the
    probe.
    
    This PR implements both falloff and blending for light probes.
    Reflection probes and irradiance volumes are blended using a weighted
    sum. In the case of reflection probes, if the weights sum to less than
    1.0, and an environment map is present on the camera, than the
    environment map receives the remaining weight necessary to bring the
    total weight up to 1.0. This is useful for reflection probes that
    correspond to building interiors, to allow smooth transitions between
    the indoor building and an exterior environment map when exiting the
    building.
    
    Falloff is specified as a fraction of the *interior* of each light probe
    that applies gradual falloff, instead of specifying a distance *outside*
    the light probe over which the influence diminishes. (See the
    documentation comments in `LightProbe` for more detail.) The reason why
    I chose to do it this way is that the voxel contents of an irradiance
    volume would be ill-defined within the falloff range otherwise. Clamping
    to the edge of the 3D voxel cube inside the falloff region (i.e.
    extending the edge voxels out) is likely to be incorrect, and extending
    the voxel region to encompass the falloff range plus the interior range
    would complicate the calculations in the performance-critical PBR shader.
    
    TODO: talk about the new example.

---
+++ examples/3d/light_probe_blending.rs
@@ -0,0 +1,123 @@
+//! Light probe blending example.
+
+use bevy::{
+    color::palettes::css::WHITE, light::NoParallaxCorrection, prelude::*, render::view::Hdr,
+};
+
+#[derive(Clone, Copy, Component, Debug)]
+struct ReflectiveSphere;
+
+const SPHERE_MOVEMENT_SPEED: f32 = 0.3;
+const CAMERA_OFFSET: Vec3 = vec3(2.0, 2.0, 2.0);
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .add_systems(Update, (move_sphere, make_camera_follow_sphere).chain())
+        .run();
+}
+
+fn setup(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    commands.spawn((
+        Camera3d::default(),
+        Transform::from_xyz(2.0, 2.0, 2.0).looking_at(Vec3::ZERO, Vec3::Y),
+        Hdr,
+        EnvironmentMapLight {
+            diffuse_map: asset_server.load("environment_maps/pisa_diffuse_rgb9e5_zstd.ktx2"),
+            specular_map: asset_server.load("environment_maps/pisa_specular_rgb9e5_zstd.ktx2"),
+            intensity: 5000.0,
+            ..default()
+        },
+    ));
+
+    commands.spawn((
+        DirectionalLight {
+            color: WHITE.into(),
+            illuminance: 10000.0,
+            ..default()
+        },
+        Transform::from_translation(CAMERA_OFFSET).looking_at(Vec3::ZERO, Vec3::Y),
+    ));
+
+    commands.spawn(SceneRoot(
+        asset_server.load(GltfAssetLabel::Scene(0).from_asset("models/two_rooms.glb")),
+    ));
+
+    let sphere = meshes.add(Sphere::default().mesh().uv(32, 18));
+    let material = materials.add(StandardMaterial {
+        base_color: WHITE.into(),
+        metallic: 1.0,
+        perceptual_roughness: 0.0,
+        ..default()
+    });
+
+    commands.spawn((
+        Mesh3d(sphere),
+        MeshMaterial3d(material),
+        Transform::IDENTITY,
+        ReflectiveSphere,
+    ));
+
+    commands.spawn((
+        LightProbe {
+            falloff: Vec3::splat(0.5),
+        },
+        EnvironmentMapLight {
+            diffuse_map: asset_server
+                .load("textures/light_probe_blending_example/diffuse_room1.ktx2"),
+            specular_map: asset_server
+                .load("textures/light_probe_blending_example/specular_room1.ktx2"),
+            intensity: 5000.0,
+            ..default()
+        },
+        Transform::from_scale(Vec3::splat(15.0)),
+    ));
+
+    commands.spawn((
+        LightProbe {
+            falloff: Vec3::splat(0.5),
+        },
+        EnvironmentMapLight {
+            diffuse_map: asset_server
+                .load("textures/light_probe_blending_example/diffuse_room2.ktx2"),
+            specular_map: asset_server
+                .load("textures/light_probe_blending_example/specular_room2.ktx2"),
+            intensity: 5000.0,
+            ..default()
+        },
+        Transform::from_scale(Vec3::splat(15.0)).with_translation(vec3(0.0, 0.0, -11.0)),
+    ));
+}
+
+fn move_sphere(mut spheres: Query<&mut Transform, With<ReflectiveSphere>>, time: Res<Time>) {
+    let Some(t) = SmoothStepCurve
+        .ping_pong()
+        .unwrap()
+        .forever()
+        .unwrap()
+        .sample(time.elapsed_secs() * SPHERE_MOVEMENT_SPEED)
+    else {
+        return;
+    };
+    for mut sphere_transform in &mut spheres {
+        sphere_transform.translation.z = -11.0 * t;
+    }
+}
+
+fn make_camera_follow_sphere(
+    mut cameras: Query<&mut Transform, (With<Camera>, Without<ReflectiveSphere>)>,
+    spheres: Query<&Transform, With<ReflectiveSphere>>,
+) {
+    let Some(sphere_transform) = spheres.iter().next() else {
+        return;
+    };
+    for mut camera_transform in &mut cameras {
+        camera_transform.translation = sphere_transform.translation + CAMERA_OFFSET;
+    }
+}
