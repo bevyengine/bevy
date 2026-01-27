7f435d36036ee45972e7d1e88d852a664ddce9f0
Alice Cecile <alice.i.cecile@gmail.com>
2026-01-26 22:00:58 -0800

    cargo fmt

--- crates/bevy_render/src/camera.rs
+++ crates/bevy_render/src/camera.rs
@@ -41,12 +41,12 @@ use bevy_ecs::{
 };
 use bevy_image::Image;
 use bevy_log::warn;
+use bevy_log::warn_once;
 use bevy_math::{uvec2, vec2, Mat4, URect, UVec2, UVec4, Vec2};
 use bevy_platform::collections::{HashMap, HashSet};
 use bevy_reflect::prelude::*;
 use bevy_transform::components::GlobalTransform;
 use bevy_window::{PrimaryWindow, Window, WindowCreated, WindowResized, WindowScaleFactorChanged};
-use bevy_log::warn_once;
 use wgpu::TextureFormat;
 
 #[derive(Default)]
