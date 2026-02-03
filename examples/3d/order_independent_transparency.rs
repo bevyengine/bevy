26c38f31b449881996d7be3ec7adba1db1cd8a32
Christophe Dehais <christophe.dehais@gmail.com>
2026-02-03 15:51:39 +0100

    Add camera rotation on drag

--- examples/3d/order_independent_transparency.rs
+++ examples/3d/order_independent_transparency.rs
@@ -9,8 +9,9 @@ use bevy::{
     camera::visibility::RenderLayers,
     color::palettes::css::{BLUE, GREEN, RED, YELLOW},
     core_pipeline::{oit::OrderIndependentTransparencySettings, prepass::DepthPrepass},
+    picking::window::update_window_hits,
     prelude::*,
-    window::{PresentMode, PrimaryWindow},
+    window::{CursorIcon, PresentMode, PrimaryWindow, SystemCursorIcon},
 };
 use bevy_ecs::system::SystemParam;
 
@@ -99,6 +100,7 @@ fn main() {
         .add_systems(Startup, setup)
         .add_systems(Update, handle_keyboard_shortcuts)
         .add_systems(Update, scene_change_watcher)
+        .add_systems(Update, update_window_hits)
         .add_systems(
             Update,
             (
@@ -110,11 +112,65 @@ fn main() {
         .run();
 }
 
+/// Component used to memorize the camera transform when initiating a Drag event
+#[derive(Component)]
+struct BaseTransform(Transform);
+
 /// Sets up the base scene
-fn setup(mut commands: Commands, mut resources: SceneResources, app_state: Res<AppState>) {
+fn setup(
+    mut commands: Commands,
+    mut resources: SceneResources,
+    app_state: Res<AppState>,
+    window: Single<Entity, With<PrimaryWindow>>,
+) {
+    // Spawn the main UI
     spawn_ui(&mut commands);
 
-    // camera
+    // Drag events handling on the window, tied to the camera rotation
+    commands
+        .entity(*window)
+        .observe(
+            |event: On<Pointer<Drag>>,
+             mut commands: Commands,
+             mut camera_transforms: Single<
+                (&mut Transform, &BaseTransform),
+                With<Camera3d>,
+            >| {
+                commands
+                    .entity(event.entity)
+                    .insert(CursorIcon::System(SystemCursorIcon::Grabbing));
+
+                // During drag the additional rotation is relative to the transform stored when Drag was initiated
+                let (ref mut transform, base_transform) = *camera_transforms;
+                **transform = base_transform.0;
+
+                const RADIANS_PER_PIXEL: f32 = -std::f32::consts::PI / 600.0;
+                let angle = event.distance.x * RADIANS_PER_PIXEL;
+                transform.rotate_around(Vec3::ZERO, Quat::from_rotation_y(angle));
+            },
+        )
+        .observe(
+            |_: On<Pointer<DragStart>>,
+             mut commands: Commands,
+             camera: Single<(Entity, &Transform), With<Camera3d>>| {
+                let (camera, transform) = *camera;
+                // Memorize the current transform 
+                commands
+                    .entity(camera)
+                    .insert(BaseTransform(*transform));
+            },
+        )
+        .observe(
+            |_: On<Pointer<DragEnd>>,
+             mut commands: Commands,
+             window: Single<Entity, With<PrimaryWindow>>| {
+                commands
+                    .entity(*window)
+                    .insert(CursorIcon::System(SystemCursorIcon::Default));
+            },
+        );
+
+    // Camera configuration
     let mut camera = commands.spawn((
         Camera3d::default(),
         Transform::from_xyz(0.0, 0.0, 10.0).looking_at(Vec3::ZERO, Vec3::Y),
@@ -135,7 +191,7 @@ fn setup(mut commands: Commands, mut resources: SceneResources, app_state: Res<A
         camera.insert(DepthPrepass);
     }
 
-    // light
+    // Light
     commands.spawn((
         PointLight {
             shadow_maps_enabled: false,
@@ -145,7 +201,7 @@ fn setup(mut commands: Commands, mut resources: SceneResources, app_state: Res<A
         RenderLayers::layer(1),
     ));
 
-    // spawn the default scene
+    // Spawn the default scene
     SCENES[0].2(&mut commands, &mut resources); //&mut meshes, &mut materials);
 }
 
@@ -181,40 +237,65 @@ fn handle_keyboard_shortcuts(
 }
 
 fn spawn_ui(commands: &mut Commands) {
+    // Invite to interact
     commands.spawn((
-        widgets::main_ui_node(),
-        children![
-            widgets::option_buttons::<AppSetting>(
-                "Scene",
-                &(SCENES
-                    .iter()
-                    .enumerate()
-                    .map(|(i, scene)| (AppSetting::ChangeScene(i), scene.0))
-                    .collect::<Vec<_>>())
-            ),
-            widgets::option_buttons(
-                "Order Independent [T]ransparency",
-                &[
-                    (AppSetting::EnableOIT(true), "On"),
-                    (AppSetting::EnableOIT(false), "Off")
-                ]
-            ),
-            widgets::option_buttons(
-                "[D]epth Prepass",
-                &[
-                    (AppSetting::UseDepthPrepass(true), "On"),
-                    (AppSetting::UseDepthPrepass(false), "Off")
-                ]
-            ),
-            widgets::option_buttons(
-                "Enable [V]Sync",
-                &[
-                    (AppSetting::EnableVsync(true), "On"),
-                    (AppSetting::EnableVsync(false), "Off")
-                ]
-            ),
-        ],
+        Node {
+            position_type: PositionType::Absolute,
+            top: px(12.0),
+            left: px(0.0),
+            right: px(0.0),
+            justify_content: JustifyContent::Center,
+            ..default()
+        },
+        children![(
+            Text::new("Drag view to spin around"),
+            TextFont {
+                font_size: 18.0,
+                ..default()
+            },
+        )],
     ));
+
+    // Buttons
+    commands
+        .spawn((
+            widgets::main_ui_node(),
+            children![
+                widgets::option_buttons::<AppSetting>(
+                    "Scene",
+                    &(SCENES
+                        .iter()
+                        .enumerate()
+                        .map(|(i, scene)| (AppSetting::ChangeScene(i), scene.0))
+                        .collect::<Vec<_>>())
+                ),
+                widgets::option_buttons(
+                    "Order Independent [T]ransparency",
+                    &[
+                        (AppSetting::EnableOIT(true), "On"),
+                        (AppSetting::EnableOIT(false), "Off")
+                    ]
+                ),
+                widgets::option_buttons(
+                    "[D]epth Prepass",
+                    &[
+                        (AppSetting::UseDepthPrepass(true), "On"),
+                        (AppSetting::UseDepthPrepass(false), "Off")
+                    ]
+                ),
+                widgets::option_buttons(
+                    "Enable [V]Sync",
+                    &[
+                        (AppSetting::EnableVsync(true), "On"),
+                        (AppSetting::EnableVsync(false), "Off")
+                    ]
+                ),
+            ],
+        ))
+        // Prevent the event from bubble up so that view drag does not initiate when interacting with the UI
+        .observe(|mut event: On<Pointer<Drag>>| {
+            event.propagate(false);
+        });
 }
 
 fn update_radio_buttons(
@@ -386,12 +467,19 @@ fn spawn_quads(commands: &mut Commands, resources: &mut SceneResources) {
         Transform::from_rotation(Quat::from_rotation_y(0.5))
             .mul_transform(Transform::from_xyz(x, y, z))
     };
+
+    // Make the quads double-sided
+    let common_params = StandardMaterial {
+        alpha_mode: AlphaMode::Blend,
+        cull_mode: None,
+        ..default()
+    };
+
     commands.spawn((
         Mesh3d(quad_handle.clone()),
         MeshMaterial3d(materials.add(StandardMaterial {
             base_color: RED.with_alpha(0.5).into(),
-            alpha_mode: AlphaMode::Blend,
-            ..default()
+            ..common_params.clone()
         })),
         xform(1.0, -0.1, 0.),
         render_layers.clone(),
@@ -400,8 +488,7 @@ fn spawn_quads(commands: &mut Commands, resources: &mut SceneResources) {
         Mesh3d(quad_handle.clone()),
         MeshMaterial3d(materials.add(StandardMaterial {
             base_color: BLUE.with_alpha(0.8).into(),
-            alpha_mode: AlphaMode::Blend,
-            ..default()
+            ..common_params.clone()
         })),
         xform(0.5, 0.2, -0.5),
         render_layers.clone(),
@@ -410,8 +497,7 @@ fn spawn_quads(commands: &mut Commands, resources: &mut SceneResources) {
         Mesh3d(quad_handle.clone()),
         MeshMaterial3d(materials.add(StandardMaterial {
             base_color: GREEN.with_green(1.0).with_alpha(0.5).into(),
-            alpha_mode: AlphaMode::Blend,
-            ..default()
+            ..common_params.clone()
         })),
         xform(0.0, 0.4, -1.),
         render_layers.clone(),
@@ -420,8 +506,7 @@ fn spawn_quads(commands: &mut Commands, resources: &mut SceneResources) {
         Mesh3d(quad_handle.clone()),
         MeshMaterial3d(materials.add(StandardMaterial {
             base_color: YELLOW.with_alpha(0.3).into(),
-            alpha_mode: AlphaMode::Blend,
-            ..default()
+            ..common_params.clone()
         })),
         xform(-0.5, 0.6, -1.1),
         render_layers.clone(),
@@ -430,8 +515,7 @@ fn spawn_quads(commands: &mut Commands, resources: &mut SceneResources) {
         Mesh3d(quad_handle.clone()),
         MeshMaterial3d(materials.add(StandardMaterial {
             base_color: BLUE.with_alpha(0.2).into(),
-            alpha_mode: AlphaMode::Blend,
-            ..default()
+            ..common_params
         })),
         xform(-0.8, 0.8, -1.2),
         render_layers.clone(),
