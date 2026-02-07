827f4c6f37b00411901d5dbc3813ce39944d0039
Kevin Chen <chen.kevin.f@gmail.com>
2026-02-07 00:53:59 -0500

    docs: slight adjustment to console info

--- crates/bevy_dev_tools/src/easy_screenshot.rs
+++ crates/bevy_dev_tools/src/easy_screenshot.rs
@@ -157,11 +157,11 @@ pub enum RecordScreen {
 /// The [`Update`] systems that the [`EasyScreenRecordPlugin`] runs
 /// to start and stop recording on user command and
 /// to send frames to the thread that manages video file creation.
-/// These systems manipulate [`virtual`](bevy_time::Virtual) 
+/// These systems manipulate [`virtual`](bevy_time::Virtual)
 /// [`time`](bevy_time::Time)` in order to capture frames for video.
-/// 
-/// If any application [`Update`] systems have behavior that depend 
-/// on virtual time and must be recorded, ensure that these systems run 
+///
+/// If any application [`Update`] systems have behavior that depend
+/// on virtual time and must be recorded, ensure that these systems run
 /// [`after(EasyScreenRecordSystems)`](bevy_ecs::schedule::IntoScheduleConfigs::after).
 /// The application may run slower on screen, but the video playback will be at
 /// normal speed.
@@ -303,10 +303,10 @@ impl Plugin for EasyScreenRecordPlugin {
                                 virtual_time.pause();
                             }
                             Some(RecordScreen::Stop) => {
-                                info!("stopped recording. still processing video");
                                 tx.send(RecordCommand::Stop).unwrap();
                                 *recording = false;
                                 virtual_time.unpause();
+                                info!("stopped recording. still processing video");
                             }
                             _ => {}
                         }
