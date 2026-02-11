e68e63eafad9517c824540554c9c2b7349f336c1
Adam Sasine <adam.sasine@gmail.com>
2026-02-10 20:51:53 -0800

    Check XDG_CONFIG_HOME on Linux

--- crates/bevy_platform/src/dirs/linux.rs
+++ crates/bevy_platform/src/dirs/linux.rs
@@ -1,7 +1,25 @@
-use std::{env::home_dir, path::PathBuf};
+use std::{
+    env::{self, home_dir},
+    path::PathBuf,
+};
+
+/// The path if it's absolute or [`None`].
+///
+/// [XDG Base Directory Specification] requires that the path specified in environment variables must be absolute. If it's not, we should ignore it and fallback to the default path.
+///
+/// [XDG Base Directory Specification]: https://specifications.freedesktop.org/basedir/latest/
+fn is_absolute_path(path: impl Into<PathBuf>) -> Option<PathBuf> {
+    let path = path.into();
+    if path.is_absolute() {
+        Some(path)
+    } else {
+        None
+    }
+}
 
 /// Returns the path to the directory used for application settings.
 pub fn preferences_dir() -> Option<PathBuf> {
-    // TODO: Support XDG_CONFIG
-    home_dir().map(|home| home.join(".config"))
+    env::var_os("XDG_CONFIG_HOME")
+        .and_then(is_absolute_path)
+        .or_else(|| home_dir().map(|home| home.join(".config")))
 }
