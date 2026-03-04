5a80d22d20fb2336fccf7eeb55cd0e52f7acbc85
Damon <pvdoriginal2019@gmail.com>
2026-03-04 22:26:07 +0200

    Update extraction.rs

--- examples/ecs/extraction.rs
+++ examples/ecs/extraction.rs
@@ -2,12 +2,6 @@
 //!
 //! Multiple entities are spawned, each with a different marker component: A, B, C.
 //! Each component contains the current elapsed time, updated each frame on the Main World.
-//!
-//! A is extracted via the automatic `ComponentExtractionPlguin`.
-//! B is extracted manually each frame.
-//! C is extracted manually when the 'Space' key is pressed.
-//!
-//! Press 'Enter' to display the state of each component from both worlds.
 
 use bevy::prelude::*;
 use bevy_render::{
@@ -16,43 +10,47 @@ use bevy_render::{
     Extract, Render, RenderApp,
 };
 
+// The A component is extracted automatically through `ExtractComponentPlugin`.
+//
+// It is required to implement `ExtractComponent`. A custom implementation can be provided
+// if you wish to alter the A component when extracting, rather than simply cloning it.
+//
+// To be noted that the `SyncToRenderWorld` component, which spawns the corresponding entity on the Render World,
+// is automatically added as a requirement through the `ExtractComponentPlugin`.
 #[derive(Component, Clone, ExtractComponent)]
 struct A(pub f32);
 
-// Note that entities need the `SyncToRenderWorld`` component in order to exist in both worlds.
-// This requirement is normally added by `ExtractComponentPlugin`, but needs to be added manually if not using that.
-
-#[derive(Component, Clone, ExtractComponent)]
+// The B component is extracted manually inside the `extract_components` system.
+// `SyncToRenderWorld` ensures that an equivalent entity will be spawned in the Render World.
+#[derive(Component, Clone)]
 #[require(SyncToRenderWorld)]
 struct B(pub f32);
 
-#[derive(Component, Clone, ExtractComponent)]
+// The C component is the same B, but it only syncs when the `Space` key is pressed.
+#[derive(Component, Clone)]
 #[require(SyncToRenderWorld)]
 struct C(pub f32);
 
-// Component synced between Worlds, used to control when the states are displayed.
-#[derive(Component, Clone, ExtractComponent)]
-struct ShouldDisplay(pub bool);
-
-// Message sent to trigger the extraction of C.
+// Message sent when the `Space` key is pressed, causing the extraction of C.
 #[derive(Message)]
 struct ExtractMessage;
+
+// Resource inserted in each world, read when displaying its name.
+#[derive(Resource)]
+struct WorldName(pub String);
 
 fn main() {
     let mut app = App::new();
 
-    app.add_systems(Startup, setup).add_systems(
-        Update,
-        (
-            set_time,
-            read_inputs,
-            display_state::<0>.run_if(should_display),
-        ),
-    );
+    // Main World
+    app.insert_resource(WorldName("Main World".into()));
+
+    app.add_systems(Startup, setup)
+        .add_systems(Update, (set_time, trigger_extraction, display_state));
 
     app.add_plugins((
         DefaultPlugins,
-        ExtractComponentPlugin::<ShouldDisplay>::default(),
+        // Plugin for automatically extracting A.
         ExtractComponentPlugin::<A>::default(),
     ));
 
@@ -62,38 +60,40 @@ fn main() {
         return;
     };
 
-    render_app.add_systems(ExtractSchedule, extract_components);
+    // Render World
+    render_app.insert_resource(WorldName("Render World".into()));
 
-    render_app.add_systems(Render, display_state::<1>.run_if(should_display));
+    render_app
+        .add_systems(ExtractSchedule, extract_components)
+        .add_systems(Render, display_state);
 
     app.run();
 }
 
+// Spawns the components on the Main World. Runs on Startup.
 fn setup(mut commands: Commands) {
     commands.spawn(A(0.0));
     commands.spawn(B(0.0));
     commands.spawn(C(0.0));
-
-    commands.spawn(ShouldDisplay(false));
 }
 
+// Sets the elapsed time on each of the components on the Main World. Runs each frame.
 fn set_time(mut a: Single<&mut A>, mut b: Single<&mut B>, mut c: Single<&mut C>, time: Res<Time>) {
     a.0 = time.elapsed_secs();
     b.0 = time.elapsed_secs();
     c.0 = time.elapsed_secs();
 }
 
-fn should_display(should_display: Single<&ShouldDisplay>) -> bool {
-    return should_display.0;
-}
-
-// The WORLD generic is used to avoid duplicating the system for each world.
-fn display_state<const WORLD: usize>(
+// Displays the values from each of the components. The same system is used for both worlds.
+fn display_state(
     a: Option<Single<&A>>,
     b: Option<Single<&B>>,
     c: Option<Single<&C>>,
+
+    // Resource we inserted to debug the name of the World.
+    world_name: Res<WorldName>,
 ) {
-    info!("State of Components in World {WORLD}:");
+    info!("State of Components in World {}:", world_name.0);
 
     info!("A: {:?}", a.map(|a| a.0));
     info!("B: {:?}", b.map(|b| b.0));
@@ -101,18 +101,14 @@ fn display_state<const WORLD: usize>(
     info!("");
 }
 
-fn read_inputs(
-    mut should_display: Single<&mut ShouldDisplay>,
-    mut writer: MessageWriter<ExtractMessage>,
-    keys: Res<ButtonInput<KeyCode>>,
-) {
-    should_display.0 = keys.just_pressed(KeyCode::Enter);
-
+// Writes a message when the Space key is pressed, which is later read by the `extract_components` system.
+fn trigger_extraction(mut writer: MessageWriter<ExtractMessage>, keys: Res<ButtonInput<KeyCode>>) {
     if keys.pressed(KeyCode::Space) {
         writer.write(ExtractMessage);
     }
 }
 
+// System that Extracts B unconditionally, and C when the `Space` key was pressed (see the `trigger_extraction` system).
 fn extract_components(
     b: Extract<Query<(RenderEntity, &B)>>,
     c: Extract<Query<(RenderEntity, &C)>>,
