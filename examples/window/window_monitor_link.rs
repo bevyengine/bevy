7639a7e59e111f718bae9aee8a68dbf5cad4527c
Sapein <5852983+Sapein@users.noreply.github.com>
2026-03-07 11:19:23 -0600

    Update Example so it doesn't print to console, but instead uses Bevy UI to display the monitor.

--- examples/window/window_monitor_link.rs
+++ examples/window/window_monitor_link.rs
@@ -7,10 +7,41 @@ use bevy::window::{Monitor, PrimaryWindow};
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_systems(Update, print_monitor)
+        .add_systems(Startup, setup)
+        .add_systems(Update, update_monitor)
         .run();
 }
 
-fn print_monitor(primary_window: Single<&OnMonitor, With<PrimaryWindow>>, monitors: Query<(Entity, &Monitor)>) {
-    println!("{:?}", monitors.iter().find(|(e, ..)| *e == primary_window.0).unwrap().1.name)
+fn setup(mut commands: Commands) {
+    // Camera
+    commands.spawn(Camera2d);
+
+    // UI
+    commands.spawn((
+        Node {
+            position_type: PositionType::Absolute,
+            padding: UiRect::all(px(5)),
+            ..default()
+        },
+        BackgroundColor(Color::BLACK.with_alpha(0.75)),
+        GlobalZIndex(i32::MAX),
+        children![(
+            Text::default(),
+            children![
+                TextSpan::new(
+                    "Current Monitor: Unknown",
+                ),
+            ]
+        )],
+    ));
+}
+
+fn update_monitor(primary_window: Single<&OnMonitor, With<PrimaryWindow>>, monitors: Query<(Entity, &Monitor)>, example_text: Query<Entity, With<Text>>, mut writer: TextUiWriter,) -> Result{
+    if let Some(current_monitor) = monitors.iter().find(|(e, ..)| *e == primary_window.0).unwrap().1.name.clone() {
+        *writer.text(example_text.single()?, 1) = format!("Current Monitor: {:?}", current_monitor);
+    } else {
+
+        *writer.text(example_text.single()?, 1) = "Current Monitor: Unknown".to_string();
+    }
+    Ok(())
 }
