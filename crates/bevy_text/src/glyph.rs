6644598ce411bbefeec5c6ab2be34b103af669ec
ickshonpe <david.curthoys@googlemail.com>
2026-03-16 19:51:28 +0000

    updated doc comment to reflect changed field name

--- crates/bevy_text/src/glyph.rs
+++ crates/bevy_text/src/glyph.rs
@@ -17,7 +17,7 @@ pub struct PositionedGlyph {
     pub position: Vec2,
     /// Information about the glyph's atlas.
     pub atlas_info: GlyphAtlasInfo,
-    /// The index of the glyph in the [`ComputedTextBlock`](crate::ComputedTextBlock)'s tracked spans.
+    /// The index of the glyph in the [`ComputedTextBlock`](crate::ComputedTextBlock)'s tracked sections.
     pub section_index: usize,
     /// The index of the glyph's line.
     pub line_index: usize,
