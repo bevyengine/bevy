bevyengine/bevy

commit 0dcba0d0c66974462282323b2d5ee34ffbb36119

Multi-draw sorted render phases if the platform supports it. (#23005)
Even though sorted render phases use multi-draw indirect and GPU
preprocessing, they only draw one mesh at a time. This patch fixes that
by introducing the notion of *batch set* comparisons, which allow
batches to be grouped into sets that can be multi-drawn together. This
mirrors the way that binned render phases use two-level bins, with outer
batch sets containing inner batches. Sorting sorts first on batch set
key, then on batch key. Then, as we generate batches, we track batch
sets in addition to batches and generate multi-draw commands as we go.

This PR accordingly modifies the definition of `GetBatchData` to support
separate comparison keys for batch sets and batch data and is therefore
technically a breaking change.

crates/bevy_render/src/batching/gpu_preprocessing.rs
@@ -4,6 +4,7 @@ use core::{
     any::TypeId,
     marker::PhantomData,
     mem,
+    ops::Range,
     sync::atomic::{AtomicU32, Ordering},
 };
 
@@ -47,7 +48,7 @@ use crate::{
     Render, RenderApp, RenderDebugFlags, RenderSystems,
 };
 
-use super::{BatchMeta, GetBatchData, GetFullBatchData};
+use super::{BatchSetMeta, GetBatchData, GetFullBatchData};
 
 #[derive(Default)]
 pub struct BatchingPlugin {
@@ -874,6 +875,17 @@ where
             phantom: PhantomData,
         }
     }
+
+    /// Allocates a single set of indirect parameters in the appropriate buffer.
+    fn allocate(&mut self, no_indirect_drawing: bool, item_is_indexed: bool) -> Option<u32> {
+        if no_indirect_drawing {
+            None
+        } else if item_is_indexed {
+            Some(self.buffers.indexed.allocate(1))
+        } else {
+            Some(self.buffers.non_indexed.allocate(1))
+        }
+    }
 }
 
 /// The buffers containing all the information that indirect draw commands use
@@ -1248,9 +1260,9 @@ where
     }
 }
 
-/// Information about a render batch that we're building up during a sorted
-/// render phase.
-struct SortedRenderBatch<F>
+/// Information about a single render batch set that we're building up during a
+/// sorted render phase.
+struct SortedRenderBatchSet<F>
 where
     F: GetBatchData,
 {
@@ -1268,16 +1280,16 @@ where
     /// [`IndirectParametersBuffers`].
     ///
     /// If CPU culling is being used, then this will be `None`.
-    indirect_parameters_index: Option<NonMaxU32>,
+    indirect_parameters_index_range: Option<Range<u32>>,
 
     /// Metadata that can be used to determine whether an instance can be placed
     /// into this batch.
     ///
     /// If `None`, the item inside is unbatchable.
-    meta: Option<BatchMeta<F::CompareData>>,
+    meta: Option<(BatchSetMeta<F::BatchSetCompareData>, F::BatchCompareData)>,
 }
 
-impl<F> SortedRenderBatch<F>
+impl<F> SortedRenderBatchSet<F>
 where
     F: GetBatchData,
 {
@@ -1297,17 +1309,18 @@ where
         let (batch_range, batch_extra_index) =
             phase.items[self.phase_item_start_index as usize].batch_range_and_extra_index_mut();
         *batch_range = self.instance_start_index..instance_end_index;
-        *batch_extra_index = match self.indirect_parameters_index {
-            Some(indirect_parameters_index) => PhaseItemExtraIndex::IndirectParametersIndex {
-                range: u32::from(indirect_parameters_index)
-                    ..(u32::from(indirect_parameters_index) + 1),
-                batch_set_index: None,
-            },
+        *batch_extra_index = match self.indirect_parameters_index_range {
+            Some(ref indirect_parameters_index_range) => {
+                PhaseItemExtraIndex::IndirectParametersIndex {
+                    range: (*indirect_parameters_index_range).clone(),
+                    batch_set_index: None,
+                }
+            }
             None => PhaseItemExtraIndex::None,
         };
-        if let Some(indirect_parameters_index) = self.indirect_parameters_index {
+        if let Some(ref indirect_parameters_index_range) = self.indirect_parameters_index_range {
             phase_indirect_parameters_buffers
-                .add_batch_set(self.indexed, indirect_parameters_index.into());
+                .add_batch_set(self.indexed, indirect_parameters_index_range.start);
         }
     }
 }
@@ -1408,7 +1421,7 @@ pub fn batch_and_prepare_sorted_render_phase<I, GFBD>(
         );
 
         // Walk through the list of phase items, building up batches as we go.
-        let mut batch: Option<SortedRenderBatch<GFBD>> = None;
+        let mut batch_set: Option<SortedRenderBatchSet<GFBD>> = None;
 
         for current_index in 0..phase.items.len() {
             // Get the index of the input data, and comparison metadata, for
@@ -1425,8 +1438,8 @@ pub fn batch_and_prepare_sorted_render_phase<I, GFBD>(
             // If the index isn't present the item is not part of this pipeline and so will be skipped.
             let Some((current_input_index, current_meta)) = current_batch_input_index else {
                 // Break a batch if we need to.
-                if let Some(batch) = batch.take() {
-                    batch.flush(
+                if let Some(batch_set) = batch_set.take() {
+                    batch_set.flush(
                         data_buffer.len() as u32,
                         phase,
                         &mut phase_indirect_parameters_buffers.buffers,
@@ -1435,85 +1448,124 @@ pub fn batch_and_prepare_sorted_render_phase<I, GFBD>(
 
                 continue;
             };
-            let current_meta =
-                current_meta.map(|meta| BatchMeta::new(&phase.items[current_index], meta));
+            let current_meta = current_meta.map(|(batch_set_meta, batch_meta)| {
+                (
+                    BatchSetMeta::new(&phase.items[current_index], batch_set_meta),
+                    batch_meta,
+                )
+            });
 
             // Determine if this entity can be included in the batch we're
             // building up.
-            let can_batch = batch.as_ref().is_some_and(|batch| {
-                // `None` for metadata indicates that the items are unbatchable.
-                match (&current_meta, &batch.meta) {
-                    (Some(current_meta), Some(batch_meta)) => current_meta == batch_meta,
-                    (_, _) => false,
-                }
-            });
+            let can_batch = match batch_set.as_ref() {
+                None => SortedPhaseItemBatchability::BreakBatchSet,
+                Some(batch_set) => match (&current_meta, &batch_set.meta) {
+                    (
+                        &Some((ref current_batch_set_key, ref current_bin_key)),
+                        &Some((ref batch_set_key, ref bin_key)),
+                    ) => {
+                        if *current_batch_set_key == *batch_set_key {
+                            if *current_bin_key == *bin_key {
+                                SortedPhaseItemBatchability::BatchOk
+                            } else {
+                                SortedPhaseItemBatchability::BreakBatch
+                            }
+                        } else {
+                            SortedPhaseItemBatchability::BreakBatchSet
+                        }
+                    }
+                    _ => SortedPhaseItemBatchability::BreakBatchSet,
+                },
+            };
 
             // Make space in the data buffer for this instance.
             let output_index = data_buffer.add() as u32;
 
-            // If we can't batch, break the existing batch and make a new one.
-            if !can_batch {
-                // Break a batch if we need to.
-                if let Some(batch) = batch.take() {
-                    batch.flush(
-                        output_index,
-                        phase,
-                        &mut phase_indirect_parameters_buffers.buffers,
-                    );
+            // If we can't batch, break the existing batch or batch set and make
+            // a new one.
+            match can_batch {
+                SortedPhaseItemBatchability::BreakBatchSet => {
+                    // Flush the existing batch set.
+                    if let Some(batch_set) = batch_set.take() {
+                        batch_set.flush(
+                            output_index,
+                            phase,
+                            &mut phase_indirect_parameters_buffers.buffers,
+                        );
+                    }
+
+                    let indirect_parameters_index = phase_indirect_parameters_buffers
+                        .allocate(no_indirect_drawing, item_is_indexed);
+
+                    // Start a new batch.
+                    if let Some(indirect_parameters_index) = indirect_parameters_index {
+                        GFBD::write_batch_indirect_parameters_metadata(
+                            item_is_indexed,
+                            output_index,
+                            None,
+                            &mut phase_indirect_parameters_buffers.buffers,
+                            indirect_parameters_index,
+                        );
+
+                        batch_set = Some(SortedRenderBatchSet {
+                            phase_item_start_index: current_index as u32,
+                            instance_start_index: output_index,
+                            indexed: item_is_indexed,
+                            indirect_parameters_index_range: Some(
+                                indirect_parameters_index..(indirect_parameters_index + 1),
+                            ),
+                            meta: current_meta,
+                        });
+                    };
                 }
 
-                let indirect_parameters_index = if no_indirect_drawing {
-                    None
-                } else if item_is_indexed {
-                    Some(
-                        phase_indirect_parameters_buffers
-                            .buffers
-                            .indexed
-                            .allocate(1),
-                    )
-                } else {
-                    Some(
-                        phase_indirect_parameters_buffers
-                            .buffers
-                            .non_indexed
-                            .allocate(1),
-                    )
-                };
+                SortedPhaseItemBatchability::BreakBatch => {
+                    // Allocate the indirect parameters.
+                    let maybe_indirect_parameters_index = phase_indirect_parameters_buffers
+                        .allocate(no_indirect_drawing, item_is_indexed);
 
-                // Start a new batch.
-                if let Some(indirect_parameters_index) = indirect_parameters_index {
-                    GFBD::write_batch_indirect_parameters_metadata(
-                        item_is_indexed,
-                        output_index,
-                        None,
-                        &mut phase_indirect_parameters_buffers.buffers,
-                        indirect_parameters_index,
-                    );
-                };
+                    if let (&mut Some(ref mut batch_set), Some(indirect_parameters_index)) =
+                        (&mut batch_set, maybe_indirect_parameters_index)
+                    {
+                        GFBD::write_batch_indirect_parameters_metadata(
+                            item_is_indexed,
+                            output_index,
+                            None,
+                            &mut phase_indirect_parameters_buffers.buffers,
+                            indirect_parameters_index,
+                        );
 
-                batch = Some(SortedRenderBatch {
-                    phase_item_start_index: current_index as u32,
-                    instance_start_index: output_index,
-                    indexed: item_is_indexed,
-                    indirect_parameters_index: indirect_parameters_index.and_then(NonMaxU32::new),
-                    meta: current_meta,
-                });
-            }
+                        batch_set.meta = current_meta;
+
+                        let indirect_parameters_index_range = batch_set
+                            .indirect_parameters_index_range
+                            .as_mut()
+                            .expect("Can't allocate in a multidraw set if we aren't multidrawing");
+                        debug_assert_eq!(
+                            indirect_parameters_index,
+                            indirect_parameters_index_range.end
+                        );
+                        indirect_parameters_index_range.end += 1;
+                    }
+                }
+
+                SortedPhaseItemBatchability::BatchOk => {}
+            };
 
             // Add a new preprocessing work item so that the preprocessing
             // shader will copy the per-instance data over.
-            if let Some(batch) = batch.as_ref() {
+            if let Some(batch_set) = batch_set.as_ref() {
                 work_item_buffer.push(
                     item_is_indexed,
                     PreprocessWorkItem {
                         input_index: current_input_index.into(),
                         output_or_indirect_parameters_index: match (
                             no_indirect_drawing,
-                            batch.indirect_parameters_index,
+                            &batch_set.indirect_parameters_index_range,
                         ) {
                             (true, _) => output_index,
-                            (false, Some(indirect_parameters_index)) => {
-                                indirect_parameters_index.into()
+                            (false, Some(indirect_parameters_index_range)) => {
+                                indirect_parameters_index_range.end - 1
                             }
                             (false, None) => 0,
                         },
@@ -1522,15 +1574,29 @@ pub fn batch_and_prepare_sorted_render_phase<I, GFBD>(
             }
         }
 
-        // Flush the final batch if necessary.
-        if let Some(batch) = batch.take() {
-            batch.flush(
+        // Flush the final batch set if necessary.
+        if let Some(batch_set) = batch_set.take() {
+            batch_set.flush(
                 data_buffer.len() as u32,
                 phase,
                 &mut phase_indirect_parameters_buffers.buffers,
             );
         }
     }
+}
+
+/// How a single sorted phase item can be batched with the previous phase item.
+#[derive(Clone, Copy, PartialEq)]
+enum SortedPhaseItemBatchability {
+    /// The item can be batched with the previous item.
+    BatchOk,
+    /// The item can't be batched with the previous item, but can still go in
+    /// the same batch set.
+    ///
+    /// That is, the item can be multi-drawn with the previous item.
+    BreakBatch,
+    /// The item needs to create a new batch set.
+    BreakBatchSet,
 }
 
 /// Creates batches for a render phase that uses bins.

crates/bevy_pbr/src/render/mesh.rs
@@ -1,7 +1,6 @@
 use crate::contact_shadows::ViewContactShadowsUniformOffset;
 use crate::{
-    material_bind_groups::{MaterialBindGroupIndex, MaterialBindGroupSlot},
-    resources::write_atmosphere_buffer,
+    material_bind_groups::MaterialBindGroupSlot, resources::write_atmosphere_buffer,
     skin::skin_uniforms_from_world,
 };
 use bevy_asset::uuid::Uuid;
@@ -39,7 +38,7 @@ use bevy_mesh::{
 };
 use bevy_platform::collections::{hash_map::Entry, HashMap};
 use bevy_render::impl_atomic_pod;
-use bevy_render::mesh::allocator::SlabId;
+use bevy_render::mesh::allocator::{MeshSlabs, SlabId};
 use bevy_render::mesh::morph::{
     MorphTargetImage, MorphTargetsResource, RenderMorphTargetAllocator,
 };
@@ -2441,6 +2440,17 @@ pub fn get_image_texture<'a>(
     }
 }
 
+/// Data that must be identical for meshes to be multi-drawn together.
+#[derive(Clone, Copy, PartialEq)]
+pub struct MeshBatchSetCompareData {
+    /// The bind group for the material.
+    material_bind_group_index: MaterialBindGroupIndex,
+    /// The slabs that the mesh data is stored in.
+    mesh_slabs: MeshSlabs,
+    /// The bindless slab that stores the lightmap for this mesh, if applicable.
+    lightmap_slab: Option<NonMaxU32>,
+}
+
 impl GetBatchData for MeshPipeline {
     type Param = (
         SRes<RenderMeshInstances>,
@@ -2450,13 +2460,8 @@ impl GetBatchData for MeshPipeline {
         SRes<SkinUniforms>,
         SRes<MorphIndices>,
     );
-    // The material bind group ID, the mesh ID, and the lightmap ID,
-    // respectively.
-    type CompareData = (
-        MaterialBindGroupIndex,
-        AssetId<Mesh>,
-        Option<LightmapSlabIndex>,
-    );
+    type BatchSetCompareData = MeshBatchSetCompareData;
+    type BatchCompareData = AssetId<Mesh>;
 
     type BufferData = MeshUniform;
 
@@ -2465,7 +2470,10 @@ impl GetBatchData for MeshPipeline {
             Self::Param,
         >,
         (_entity, main_entity): (Entity, MainEntity),
-    ) -> Option<(Self::BufferData, Option<Self::CompareData>)> {
+    ) -> Option<(
+        Self::BufferData,
+        Option<(Self::BatchSetCompareData, Self::BatchCompareData)>,
+    )> {
         let RenderMeshInstances::CpuBuilding(ref mesh_instances) = **mesh_instances else {
             error!(
                 "`get_batch_data` should never be called in GPU mesh uniform \
@@ -2479,6 +2487,7 @@ impl GetBatchData for MeshPipeline {
                 Some(mesh_vertex_slice) => mesh_vertex_slice.range.start,
                 None => 0,
             };
+        let mesh_slabs = mesh_allocator.mesh_slabs(&mesh_instance.mesh_asset_id())?;
         let maybe_lightmap = lightmaps.render_lightmaps.get(&main_entity);
 
         let current_skin_index = skin_uniforms.skin_index(main_entity);
@@ -2496,9 +2505,12 @@ impl GetBatchData for MeshPipeline {
                 Some(mesh_instance.tag()),
             ),
             mesh_instance.should_batch().then_some((
-                material_bind_group_index.group,
+                MeshBatchSetCompareData {
+                    material_bind_group_index: material_bind_group_index.group,
+                    mesh_slabs,
+                    lightmap_slab: maybe_lightmap.map(|lightmap| lightmap.slab_index.0),
+                },
                 mesh_instance.mesh_asset_id(),
-                maybe_lightmap.map(|lightmap| lightmap.slab_index),
             )),
         ))
     }
@@ -2508,9 +2520,12 @@ impl GetFullBatchData for MeshPipeline {
     type BufferInputData = MeshInputUniform;
 
     fn get_index_and_compare_data(
-        (mesh_instances, lightmaps, _, _, _, _): &SystemParamItem<Self::Param>,
+        (mesh_instances, lightmaps, _, mesh_allocator, _, _): &SystemParamItem<Self::Param>,
         main_entity: MainEntity,
-    ) -> Option<(NonMaxU32, Option<Self::CompareData>)> {
+    ) -> Option<(
+        NonMaxU32,
+        Option<(Self::BatchSetCompareData, Self::BatchCompareData)>,
+    )> {
         // This should only be called during GPU building.
         let RenderMeshInstances::GpuBuilding(ref mesh_instances) = **mesh_instances else {
             error!(
@@ -2521,14 +2536,19 @@ impl GetFullBatchData for MeshPipeline {
         };
 
         let mesh_instance = mesh_instances.get(&main_entity)?;
+        let mesh_slabs = mesh_allocator.mesh_slabs(&mesh_instance.mesh_asset_id())?;
         let maybe_lightmap = lightmaps.render_lightmaps.get(&main_entity);
+        let material_bind_group_index = mesh_instance.material_bindings_index();
 
         Some((
             NonMaxU32::new(mesh_instance.gpu_specific.current_uniform_index())?,
             mesh_instance.should_batch().then_some((
-                mesh_instance.material_bindings_index().group,
+                MeshBatchSetCompareData {
+                    material_bind_group_index: material_bind_group_index.group,
+                    mesh_slabs,
+                    lightmap_slab: maybe_lightmap.map(|lightmap| lightmap.slab_index.0),
+                },
                 mesh_instance.mesh_asset_id(),
-                maybe_lightmap.map(|lightmap| lightmap.slab_index),
             )),
         ))
     }

crates/bevy_render/src/batching/mod.rs
@@ -38,7 +38,7 @@ pub struct NoAutomaticBatching;
 ///   due to having to split data across separate uniform bindings within the
 ///   same buffer due to the maximum uniform buffer binding size.
 #[derive(PartialEq)]
-struct BatchMeta<T: PartialEq> {
+struct BatchSetMeta<T: PartialEq> {
     /// The pipeline id encompasses all pipeline configuration including vertex
     /// buffers and layouts, shaders and their specializations, bind group
     /// layouts, etc.
@@ -50,9 +50,9 @@ struct BatchMeta<T: PartialEq> {
     user_data: T,
 }
 
-impl<T: PartialEq> BatchMeta<T> {
+impl<T: PartialEq> BatchSetMeta<T> {
     fn new(item: &impl CachedRenderPipelinePhaseItem, user_data: T) -> Self {
-        BatchMeta {
+        BatchSetMeta {
             pipeline_id: item.cached_pipeline(),
             draw_function_id: item.draw_function(),
             dynamic_offset: match item.extra_index() {
@@ -78,10 +78,19 @@ pub trait GetBatchData {
     /// The system parameters [`GetBatchData::get_batch_data`] needs in
     /// order to compute the batch data.
     type Param: SystemParam + 'static;
-    /// Data used for comparison between phase items. If the pipeline id, draw
-    /// function id, per-instance data buffer dynamic offset and this data
-    /// matches, the draws can be batched.
-    type CompareData: PartialEq;
+    /// Data used for comparison between phase items to decide whether items can
+    /// be batched.
+    ///
+    /// If this data, and the [`Self::BatchSetCompareData`], are identical to
+    /// those of the previous phase item, the items can be batched together.
+    type BatchCompareData: PartialEq;
+    /// Data used for comparison between phase items to decide whether items can
+    /// be grouped in the same batch set (i.e. multi-drawn).
+    ///
+    /// If this data is identical to that of the previous phase items, and the
+    /// current platform supports multi-draw, the items can be multi-drawn
+    /// together.
+    type BatchSetCompareData: PartialEq;
     /// The per-instance data to be inserted into the
     /// [`crate::render_resource::GpuArrayBuffer`] containing these data for all
     /// instances.
@@ -97,7 +106,10 @@ pub trait GetBatchData {
     fn get_batch_data(
         param: &SystemParamItem<Self::Param>,
         query_item: (Entity, MainEntity),
-    ) -> Option<(Self::BufferData, Option<Self::CompareData>)>;
+    ) -> Option<(
+        Self::BufferData,
+        Option<(Self::BatchSetCompareData, Self::BatchCompareData)>,
+    )>;
 }
 
 /// A trait to support getting data used for batching draw commands via phase
@@ -130,7 +142,10 @@ pub trait GetFullBatchData: GetBatchData {
     fn get_index_and_compare_data(
         param: &SystemParamItem<Self::Param>,
         query_item: MainEntity,
-    ) -> Option<(NonMaxU32, Option<Self::CompareData>)>;
+    ) -> Option<(
+        NonMaxU32,
+        Option<(Self::BatchSetCompareData, Self::BatchCompareData)>,
+    )>;
 
     /// Returns the index of the [`GetFullBatchData::BufferInputData`] that the
     /// GPU preprocessing phase will use.
@@ -202,14 +217,16 @@ where
 /// [`no_gpu_preprocessing::batch_and_prepare_sorted_render_phase`].
 fn batch_and_prepare_sorted_render_phase<I, GBD>(
     phase: &mut SortedRenderPhase<I>,
-    mut process_item: impl FnMut(&mut I) -> Option<GBD::CompareData>,
+    mut process_item: impl FnMut(&mut I) -> Option<(GBD::BatchSetCompareData, GBD::BatchCompareData)>,
 ) where
     I: CachedRenderPipelinePhaseItem + SortedPhaseItem,
     GBD: GetBatchData,
 {
     let items = phase.items.values_mut().map(|item| {
         let batch_data = match process_item(item) {
-            Some(compare_data) if I::AUTOMATIC_BATCHING => Some(BatchMeta::new(item, compare_data)),
+            Some(compare_data) if I::AUTOMATIC_BATCHING => {
+                Some(BatchSetMeta::new(item, compare_data))
+            }
             _ => None,
         };
         (item.batch_range_mut(), batch_data)

crates/bevy_render/src/mesh/allocator.rs
@@ -192,7 +192,7 @@ enum Slab {
 }
 
 /// IDs of the slabs associated with a single mesh.
-#[derive(Clone, Default, PartialEq, Eq, PartialOrd, Ord, Hash, Debug)]
+#[derive(Clone, Copy, Default, PartialEq, Eq, PartialOrd, Ord, Hash, Debug)]
 pub struct MeshSlabs {
     /// The slab storing the mesh's vertex data.
     pub vertex_slab_id: SlabId,

crates/bevy_sprite_render/src/mesh2d/mesh.rs
@@ -340,19 +340,26 @@ impl GetBatchData for Mesh2dPipeline {
         SRes<RenderAssets<RenderMesh>>,
         SRes<MeshAllocator>,
     );
-    type CompareData = (Material2dBindGroupId, AssetId<Mesh>);
+    type BatchSetCompareData = (Material2dBindGroupId, AssetId<Mesh>);
+    type BatchCompareData = ();
     type BufferData = Mesh2dUniform;
 
     fn get_batch_data(
         (mesh_instances, _, _): &SystemParamItem<Self::Param>,
         (_entity, main_entity): (Entity, MainEntity),
-    ) -> Option<(Self::BufferData, Option<Self::CompareData>)> {
+    ) -> Option<(
+        Self::BufferData,
+        Option<(Self::BatchSetCompareData, Self::BatchCompareData)>,
+    )> {
         let mesh_instance = mesh_instances.get(&main_entity)?;
         Some((
             Mesh2dUniform::from_components(&mesh_instance.transforms, mesh_instance.tag),
             mesh_instance.automatic_batching.then_some((
-                mesh_instance.material_bind_group_id,
-                mesh_instance.mesh_asset_id,
+                (
+                    mesh_instance.material_bind_group_id,
+                    mesh_instance.mesh_asset_id,
+                ),
+                (),
             )),
         ))
     }
@@ -375,7 +382,10 @@ impl GetFullBatchData for Mesh2dPipeline {
     fn get_index_and_compare_data(
         _: &SystemParamItem<Self::Param>,
         _query_item: MainEntity,
-    ) -> Option<(NonMaxU32, Option<Self::CompareData>)> {
+    ) -> Option<(
+        NonMaxU32,
+        Option<(Self::BatchSetCompareData, Self::BatchCompareData)>,
+    )> {
         error!(
             "`get_index_and_compare_data` is only intended for GPU mesh uniform building, \
             but this is not yet implemented for 2d meshes"

commit 7be7610203de2c00c0330227856440ee91e7fb72

make batch set creation unconditional on drawing mode (#23515)
# Objective

- fix transparency on mac and webgpu (broken since #23005)

## Solution

- we were only creating sorted phase batch sets in indirect drawing
mode. this meant direct drawing did not draw them at all

## Testing

- transparency_3d example

crates/bevy_render/src/batching/gpu_preprocessing.rs
@@ -1579,17 +1579,16 @@ pub fn batch_and_prepare_sorted_render_phase<I, GFBD>(
                             &mut phase_indirect_parameters_buffers.buffers,
                             indirect_parameters_index,
                         );
+                    }
 
-                        batch_set = Some(SortedRenderBatchSet {
-                            phase_item_start_index: current_index as u32,
-                            instance_start_index: output_index,
-                            indexed: item_is_indexed,
-                            indirect_parameters_index_range: Some(
-                                indirect_parameters_index..(indirect_parameters_index + 1),
-                            ),
-                            meta: current_meta,
-                        });
-                    };
+                    batch_set = Some(SortedRenderBatchSet {
+                        phase_item_start_index: current_index as u32,
+                        instance_start_index: output_index,
+                        indexed: item_is_indexed,
+                        indirect_parameters_index_range: indirect_parameters_index
+                            .map(|i| i..(i + 1)),
+                        meta: current_meta,
+                    });
                 }
 
                 SortedPhaseItemBatchability::BreakBatch => {