66214c5a87edf056b91670c7861bb16275c01c13
momoluna444 <32905054+momoluna444@users.noreply.github.com>
2026-03-26 10:12:53 +0800

    Do not return early when tonemapping is set to None

--- crates/bevy_core_pipeline/src/tonemapping/node.rs
+++ crates/bevy_core_pipeline/src/tonemapping/node.rs
@@ -40,9 +40,8 @@ pub fn tonemapping(
 ) {
     let (view_uniform_offset, target, view_tonemapping_pipeline, tonemapping) = view.into_inner();
 
-    if *tonemapping == Tonemapping::None {
-        return;
-    }
+    // NOTE: Do not return early when tonemapping is set to None, as color grading
+    // and auto exposure still depend on this pass.
 
     if !target.is_hdr() {
         return;
