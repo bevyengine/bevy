bc89d238ce1852712708e466ba47a71a8d197567
Mat <mattpowell90@gmail.com>
2026-03-27 12:58:41 +0000

    refactor: use vertices for an irregular convex polygon to visually distinguish the different shape

--- examples/3d/3d_shapes.rs
+++ examples/3d/3d_shapes.rs
@@ -93,10 +93,11 @@ fn setup(
         meshes.add(Extrusion::new(Triangle2d::default(), 1.)),
         meshes.add(Extrusion::new(
             ConvexPolygon::new(vec![
-                Vec2::new(-0.75, -0.75),
-                Vec2::new(0.5, -0.75),
-                Vec2::new(0.5, 0.5),
-                Vec2::new(-0.75, 0.5),
+                Vec2::new(0.0, 0.8),
+                Vec2::new(-0.47, 0.25),
+                Vec2::new(-0.47, -0.65),
+                Vec2::new(0.47, -0.65),
+                Vec2::new(0.47, 0.25),
             ]).unwrap(),
             1.0,
         )),
