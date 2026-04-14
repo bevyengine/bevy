40c2a0212c7744caf905b9876ad63fa289232e1c
Nico Zweifel <34443492+NicoZweifel@users.noreply.github.com>
2026-04-14 11:19:08 +0200

    Delete traits.rs

--- crates/bevy_scene/macros/src/bsn/traits.rs
+++
@@ -1,8 +0,0 @@
-use proc_macro2::TokenStream;
-use syn::parse::Parse;
-
-use crate::bsn::codegen::BsnCodegenCtx;
-
-pub trait BsnTokenStream: Parse {
-    fn to_tokens(&self, ctx: &mut BsnCodegenCtx) -> TokenStream;
-}
