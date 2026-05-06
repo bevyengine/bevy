ce060603df5a4dc16141d17f7097a5555401bc8f
Kevin Chen <chen.kevin.f@gmail.com>
2026-05-05 21:41:18 -0400

    use is_some_and, wording of DX12

--- crates/bevy_render/src/settings.rs
+++ crates/bevy_render/src/settings.rs
@@ -129,14 +129,16 @@ impl Default for WgpuSettings {
 
         let mut instance_flags = InstanceFlags::default();
         #[cfg(not(debug_assertions))]
-        // wgpu executes additional necessary logic during validation passes for dx-12 backends,
-        // so the `VALIDATION_INDIRECT_CALL` flag should stay for dx-12.
-        if !backends.unwrap().contains(Backends::DX12) {
-            // Removing this flag improves performance.
-            instance_flags.remove(InstanceFlags::VALIDATION_INDIRECT_CALL);
+        {
+            // wgpu executes additional necessary logic during validation passes for the DX12 backend,
+            // so the `VALIDATION_INDIRECT_CALL` flag should stay for DX12.
+            if !backends.is_some_and(|backends| backends.contains(Backends::DX12)) {
+                // Removing this flag improves performance.
+                instance_flags.remove(InstanceFlags::VALIDATION_INDIRECT_CALL);
+            }
         }
         #[cfg(all(not(debug_assertions), feature = "raw_vulkan_init"))]
-        // intending to use vulkan even if backends contains dx-12
+        // intending to use vulkan even if backends contains DX12
         instance_flags.remove(InstanceFlags::VALIDATION_INDIRECT_CALL);
 
         instance_flags = instance_flags.with_env();
