435cb0c757438b1bc32800d5e4932d4f27248fed
Greeble <166992735+greeble-dev@users.noreply.github.com>
2026-05-06 12:31:02 +0100

    Added `#[inline(never)]` to `type_info`.

--- crates/bevy_reflect/derive/src/impls/typed.rs
+++ crates/bevy_reflect/derive/src/impls/typed.rs
@@ -148,7 +148,7 @@ pub(crate) fn impl_typed(
 
     quote! {
         impl #impl_generics #bevy_reflect_path::Typed for #type_path #ty_generics #where_reflect_clause {
-            #[inline]
+            #[inline(never)]
             fn type_info() -> &'static #bevy_reflect_path::TypeInfo {
                 #type_info_cell
             }
