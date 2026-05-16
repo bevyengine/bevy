4a974ed58a7ee9c6969250742fb63274cbc7701c
musjj <72612857+musjj@users.noreply.github.com>
2026-05-17 03:18:20 +0700

    Re-export everything from the gltf library

--- crates/bevy_gltf/src/lib.rs
+++ crates/bevy_gltf/src/lib.rs
@@ -165,7 +165,7 @@ pub use {assets::*, label::GltfAssetLabel, loader::*, material::GltfMaterial};
 /// Re-exports for GLTF
 pub mod gltf {
     #[doc(hidden)]
-    pub use gltf::{Animation, Document, Gltf, Material, Mesh, Primitive, Scene, Texture};
+    pub use gltf::*;
 }
 
 // Has to store an Arc<Mutex<...>> as there is no other way to mutate fields of asset loaders.
