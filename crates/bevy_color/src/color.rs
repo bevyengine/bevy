f462738ab3fc9d0d82120bd55badb9e1d32ba130
Marco Buono <thecoreh@gmail.com>
2026-05-23 20:15:48 -0300

    Remove needless `.into()` calls

--- crates/bevy_color/src/color.rs
+++ crates/bevy_color/src/color.rs
@@ -861,8 +861,8 @@ impl Saturation for Color {
         match &mut new {
             Color::Srgba(x) => *x = Hsla::from(*x).with_saturation(saturation).into(),
             Color::LinearRgba(x) => *x = Hsla::from(*x).with_saturation(saturation).into(),
-            Color::Hsla(x) => *x = x.with_saturation(saturation).into(),
-            Color::Hsva(x) => *x = x.with_saturation(saturation).into(),
+            Color::Hsla(x) => *x = x.with_saturation(saturation),
+            Color::Hsva(x) => *x = x.with_saturation(saturation),
             Color::Hwba(x) => *x = Hsla::from(*x).with_saturation(saturation).into(),
             Color::Laba(x) => *x = Hsla::from(*x).with_saturation(saturation).into(),
             Color::Lcha(x) => *x = Hsla::from(*x).with_saturation(saturation).into(),
