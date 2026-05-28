3555e2284083a3cfa684c378a807e820f4a3b8a8
Daniel Skates <zeophlite@gmail.com>
2026-05-28 17:27:46 +0800

    Rework docs to remove most of render from bevy_extract

--- crates/bevy_extract/src/sync_component.rs
+++ crates/bevy_extract/src/sync_component.rs
@@ -13,7 +13,7 @@ use crate::sync_world::{EntityRecord, PendingSyncEntity, SyncToSubWorld};
 
 use bevy_log::warn_once;
 
-/// Plugin that registers a component for automatic sync to the render world. See [`SyncWorldPlugin`] for more information.
+/// Plugin that registers a component for automatic sync to the sub world. See [`SyncWorldPlugin`] for more information.
 ///
 /// This plugin is automatically added by [`ExtractComponentPlugin`], and only needs to be added for manual extraction implementations.
 ///
@@ -24,7 +24,7 @@ use bevy_log::warn_once;
 /// # Implementation details
 ///
 /// It adds [`SyncToSubWorld`] as a required component to make the [`SyncWorldPlugin`] aware of the component, and
-/// handles cleanup of the component in the render world when it is removed from an entity.
+/// handles cleanup of the component in the sub world when it is removed from an entity.
 ///
 /// [`ExtractComponentPlugin`]: crate::extract_component::ExtractComponentPlugin
 /// [`SyncWorldPlugin`]: crate::sync_world::SyncWorldPlugin
@@ -39,7 +39,7 @@ impl<C: SyncComponent<L, F>, L: AppLabel, F> Default for SyncComponentPlugin<C, 
 }
 
 /// Trait that links components from the main world with output components in
-/// the render world. It is used by [`SyncComponentPlugin`].
+/// the sub world. It is used by [`SyncComponentPlugin`].
 ///
 /// The marker type `F` is only used as a way to bypass the orphan rules. To
 /// implement the trait for a foreign type you can use a local type as the
@@ -47,7 +47,7 @@ impl<C: SyncComponent<L, F>, L: AppLabel, F> Default for SyncComponentPlugin<C, 
 ///
 /// [`ExtractComponent`]: crate::extract_component::ExtractComponent
 pub trait SyncComponent<L: AppLabel, F = ()>: Component {
-    /// Describes what components should be removed from the render world if the
+    /// Describes what components should be removed from the sub world if the
     /// implementing component is removed.
     type Target: Bundle<Effect: NoBundleEffect>;
     // TODO: https://github.com/rust-lang/rust/issues/29661
