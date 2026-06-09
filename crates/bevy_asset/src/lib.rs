e21d2534fa45035c467e4d1b1d9748c9ccabfbfb
GroveDG <87248833+GroveDG@users.noreply.github.com>
2026-06-09 00:30:20 -0400

    Add additional generic impls of `VisitAssetDependencies`. (#24556)
    # Objective
    
    Some implementations of `VisitAssetDependencies` were tied specifically
    to types directly containing asset handles and IDs such as
    `Option<Handle<A>>`. This prevents marking many types as `#[dependency]`
    even though they contain types which implement `VisitAssetDependencies`.
    
    ## Solution
    
    Replaced implemenations of `VisitAssetDependencies` that were
    specifically to asset handles and IDs with ones generic across `V:
    VisitAssetDependencies`. Added additional implementations for various
    `std` types and collections.
    
    ## Testing
    
    No tests were added or modified.

--- crates/bevy_asset/src/lib.rs
+++ crates/bevy_asset/src/lib.rs
@@ -207,6 +207,8 @@ use crate::{
     processor::{AssetProcessor, Process},
 };
 use alloc::{
+    boxed::Box,
+    collections::{BTreeMap, BTreeSet, VecDeque},
     string::{String, ToString},
     sync::Arc,
     vec::Vec,
@@ -475,25 +477,9 @@ impl<A: Asset> VisitAssetDependencies for Handle<A> {
     }
 }
 
-impl<A: Asset> VisitAssetDependencies for Option<Handle<A>> {
-    fn visit_dependencies(&self, visit: &mut impl FnMut(UntypedAssetId)) {
-        if let Some(handle) = self {
-            visit(handle.id().untyped());
-        }
-    }
-}
-
 impl VisitAssetDependencies for UntypedHandle {
     fn visit_dependencies(&self, visit: &mut impl FnMut(UntypedAssetId)) {
         visit(self.id());
-    }
-}
-
-impl VisitAssetDependencies for Option<UntypedHandle> {
-    fn visit_dependencies(&self, visit: &mut impl FnMut(UntypedAssetId)) {
-        if let Some(handle) = self {
-            visit(handle.id());
-        }
     }
 }
 
@@ -503,23 +489,45 @@ impl VisitAssetDependencies for UntypedAssetId {
     }
 }
 
-impl<A: Asset, const N: usize> VisitAssetDependencies for [Handle<A>; N] {
+impl<V: VisitAssetDependencies> VisitAssetDependencies for Option<V> {
     fn visit_dependencies(&self, visit: &mut impl FnMut(UntypedAssetId)) {
-        for dependency in self {
-            visit(dependency.id().untyped());
+        if let Some(dependency) = self {
+            dependency.visit_dependencies(visit);
         }
     }
 }
 
-impl<const N: usize> VisitAssetDependencies for [UntypedHandle; N] {
+impl<V: VisitAssetDependencies, const N: usize> VisitAssetDependencies for [V; N] {
     fn visit_dependencies(&self, visit: &mut impl FnMut(UntypedAssetId)) {
         for dependency in self {
-            visit(dependency.id());
+            dependency.visit_dependencies(visit);
         }
+    }
+}
+
+impl<V: VisitAssetDependencies> VisitAssetDependencies for [V] {
+    fn visit_dependencies(&self, visit: &mut impl FnMut(UntypedAssetId)) {
+        for dependency in self {
+            dependency.visit_dependencies(visit);
+        }
+    }
+}
+
+impl<V: VisitAssetDependencies> VisitAssetDependencies for Box<V> {
+    fn visit_dependencies(&self, visit: &mut impl FnMut(UntypedAssetId)) {
+        self.as_ref().visit_dependencies(visit);
     }
 }
 
 impl<V: VisitAssetDependencies> VisitAssetDependencies for Vec<V> {
+    fn visit_dependencies(&self, visit: &mut impl FnMut(UntypedAssetId)) {
+        for dependency in self {
+            dependency.visit_dependencies(visit);
+        }
+    }
+}
+
+impl<V: VisitAssetDependencies> VisitAssetDependencies for VecDeque<V> {
     fn visit_dependencies(&self, visit: &mut impl FnMut(UntypedAssetId)) {
         for dependency in self {
             dependency.visit_dependencies(visit);
@@ -535,18 +543,26 @@ impl<V: VisitAssetDependencies> VisitAssetDependencies for HashSet<V> {
     }
 }
 
-impl<A: Asset, K> VisitAssetDependencies for HashMap<K, Handle<A>> {
+impl<V: VisitAssetDependencies, K> VisitAssetDependencies for HashMap<K, V> {
     fn visit_dependencies(&self, visit: &mut impl FnMut(UntypedAssetId)) {
         for dependency in self.values() {
-            visit(dependency.id().untyped());
+            dependency.visit_dependencies(visit);
         }
     }
 }
 
-impl<K> VisitAssetDependencies for HashMap<K, UntypedHandle> {
+impl<V: VisitAssetDependencies> VisitAssetDependencies for BTreeSet<V> {
+    fn visit_dependencies(&self, visit: &mut impl FnMut(UntypedAssetId)) {
+        for dependency in self {
+            dependency.visit_dependencies(visit);
+        }
+    }
+}
+
+impl<V: VisitAssetDependencies, K> VisitAssetDependencies for BTreeMap<K, V> {
     fn visit_dependencies(&self, visit: &mut impl FnMut(UntypedAssetId)) {
         for dependency in self.values() {
-            visit(dependency.id());
+            dependency.visit_dependencies(visit);
         }
     }
 }
