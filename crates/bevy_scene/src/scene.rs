bevyengine/bevy

commit 5d5e3f9551ca57a228cf49912df1663f05a037c2

Implement `Scene` for `Option<P>` where `P: Scene` (#24536)
This allows easier conditional behavior without having to resort to
`Box<dyn Scene>` with an empty `bsn!{}` in the negative case.

# Objective

While designing custom widgets and extra scene tooling I have found
several cases where I would like to optionally include a set of
components.
Right now (as best I can tell) this requires constructing a `Box<dyn
Scene>` which either has the components, or is an empty scene
(`bsn!{}`).

Every time I try to do this, I always first end up with a type error
that the `if` and `else` branches don't have matching types.
This is fixable, but prone to mistakes. 
Allowing `Option<P: Scene>` is much simpler, and doesn't require
allocations.

## Solution

Allow `Option<P: Scene>` to be a `Scene` which optionally resolves the
contained scene.

## Testing

I have been using a new-typed version of this implementation
(`ConditionalScene<P>(Option<P>)`) in my own personal widget library to
get around Orphan rules.

I added a simple `#[test]` to the `bevy_scene` test suite.

---

## Showcase

I put together an example widget for a "PullQuote" which can optionally
use the inherited font from the parent, or use a larger font by default.

<details>
  <summary>Click to view showcase</summary>

```rust
use bevy::{
    feathers::{
        FeathersPlugins,
        constants::{fonts, size},
        dark_theme::create_dark_theme,
        font_styles::InheritableFont,
        theme::{ThemeBorderColor, ThemedText, UiTheme},
        tokens,
    },
    prelude::*,
};

fn main() {
    App::new()
        .add_plugins((DefaultPlugins, FeathersPlugins))
        .insert_resource(UiTheme(create_dark_theme()))
        .add_systems(Startup, scene.spawn())
        .run();
}

fn scene() -> impl SceneList {
    bsn_list!(Camera2d, ui())
}

#[derive(SceneComponent, Default, Clone)]
#[scene(PullQuoteProps)]
pub struct PullQuote;

pub struct PullQuoteProps {

crates/bevy_scene/src/scene.rs
@@ -211,6 +211,21 @@ macro_rules! scene_impl {
 
 all_tuples!(scene_impl, 0, 12, P);
 
+impl<P> Scene for Option<P>
+where
+    P: Scene,
+{
+    #[inline]
+    fn resolve(
+        self,
+        context: &mut ResolveContext,
+        scene: &mut ResolvedScene,
+    ) -> Result<(), ResolveSceneError> {
+        self.map(|optional_scene| optional_scene.resolve(context, scene))
+            .unwrap_or(Ok(()))
+    }
+}
+
 /// A [`Scene`] that patches a [`Template`] of type `T` with a given function `F`.
 ///
 /// Functionally, a [`TemplatePatch`] scene will initialize a [`Default`] value of the patched

crates/bevy_scene/src/lib.rs
@@ -2202,4 +2202,25 @@ mod tests {
             .unwrap();
         assert_eq!(entity.get::<Foo>().unwrap().value, vec![10usize]);
     }
+
+    #[test]
+    fn scene_with_optional_components() {
+        let mut app = test_app();
+        let world = app.world_mut();
+
+        #[derive(Component, Default, Clone)]
+        struct Foo;
+
+        let optional_component = Some(bsn! {
+            Foo
+        });
+
+        let entity = world
+            .spawn_scene(bsn! {
+                #MaybeFoo
+                {optional_component}
+            })
+            .unwrap();
+        assert!(entity.get::<Foo>().is_some());
+    }
 }

commit d0c5d20e0952aff29160730173da7bda79852c6b

bsn: Expand support for optional scenes and scene lists (#24565)
# Objective

It should be possible to support `Option` wrapped scenes and scene lists
in Bevy. https://github.com/bevyengine/bevy/pull/24536 got us part of
the way there, but it didn't support SceneList. It also didn't add nice
support for `Option<Box<dyn SceneList>>` in cases like "scene props".

#24536 also introduces a bug, as it doesn't register the dependencies of
the wrapped scene.

## Solution

Add more thorough support for optional scenes. This is now possible:

```rust
#[derive(SceneComponent, Default, Clone)]
#[scene(FeathersButtonProps)]
pub struct FeathersButton;

pub struct FeathersButtonProps {
    pub caption: Option<Box<dyn SceneList>>,
}

impl FeathersButton {
    fn scene(props: FeathersButtonProps) -> impl Scene {
        bsn! {
            Children [
                {props.caption}
            ]
        }
    }
}

// Later
bsn! {
    @FeathersButton {
        @caption: bsn! { Node }
    }
}
```

This also fixes the bug introduced in #24536

crates/bevy_scene/src/scene.rs
@@ -211,9 +211,9 @@ macro_rules! scene_impl {
 
 all_tuples!(scene_impl, 0, 12, P);
 
-impl<P> Scene for Option<P>
+impl<S> Scene for Option<S>
 where
-    P: Scene,
+    S: Scene,
 {
     #[inline]
     fn resolve(
@@ -221,8 +221,37 @@ where
         context: &mut ResolveContext,
         scene: &mut ResolvedScene,
     ) -> Result<(), ResolveSceneError> {
-        self.map(|optional_scene| optional_scene.resolve(context, scene))
+        self.map(|value| value.resolve(context, scene))
             .unwrap_or(Ok(()))
+    }
+
+    #[inline]
+    fn register_dependencies(&self, dependencies: &mut SceneDependencies) {
+        if let Some(value) = &self {
+            value.register_dependencies(dependencies);
+        }
+    }
+}
+
+impl<L: SceneList> SceneList for Option<L>
+where
+    L: SceneList,
+{
+    #[inline]
+    fn resolve_list(
+        self,
+        context: &mut ResolveContext,
+        scenes: &mut Vec<ResolvedScene>,
+    ) -> std::prelude::v1::Result<(), ResolveSceneError> {
+        self.map(|scene_list| scene_list.resolve_list(context, scenes))
+            .unwrap_or(Ok(()))
+    }
+
+    #[inline]
+    fn register_dependencies(&self, dependencies: &mut SceneDependencies) {
+        if let Some(scene_list) = &self {
+            scene_list.register_dependencies(dependencies);
+        }
     }
 }
 
@@ -534,6 +563,12 @@ impl<S: Scene> From<SceneScope<S>> for Box<dyn Scene> {
     }
 }
 
+impl<S: Scene> From<SceneScope<S>> for Option<Box<dyn Scene>> {
+    fn from(value: SceneScope<S>) -> Self {
+        Some(Box::new(value))
+    }
+}
+
 impl<S: SceneList> From<SceneListScope<S>> for Box<dyn SceneList> {
     fn from(value: SceneListScope<S>) -> Self {
         Box::new(value)
@@ -543,6 +578,18 @@ impl<S: SceneList> From<SceneListScope<S>> for Box<dyn SceneList> {
 impl<S: Scene> From<SceneScope<S>> for Box<dyn SceneList> {
     fn from(value: SceneScope<S>) -> Self {
         Box::new(value)
+    }
+}
+
+impl<S: Scene> From<SceneScope<S>> for Option<Box<dyn SceneList>> {
+    fn from(value: SceneScope<S>) -> Self {
+        Some(Box::new(value))
+    }
+}
+
+impl<S: SceneList> From<SceneListScope<S>> for Option<Box<dyn SceneList>> {
+    fn from(value: SceneListScope<S>) -> Self {
+        Some(Box::new(value))
     }
 }
 