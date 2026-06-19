ce8af254fb5d68bb97761636d5bad78745eae8a8
ickshonpe <david.curthoys@googlemail.com>
2026-06-19 11:37:07 +0100

    Shorter text

--- examples/stress_tests/many_text.rs
+++ examples/stress_tests/many_text.rs
@@ -10,9 +10,7 @@ use bevy::{
 
 const LOREM_TEXT: &str = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do \
 eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis \
-nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure \
-dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur \
-sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
 
 #[derive(FromArgs, Resource)]
 /// `many_text` UI text stress test
