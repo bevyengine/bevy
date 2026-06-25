2b1935fb61659ccc7e332657dfb3a03aca9319c1
HeartofPhos <43216176+HeartofPhos@users.noreply.github.com>
2026-06-25 18:56:54 +0200

    Fix bsn nested entity references (#24742)
    fix nested named entities
    
    # Objective
    
    Fix nested entity references
    
    ## Solution
    
    Iterate over entity references instead of only insert first
    
    ## Testing
    
    Added relevent test case
    
    ---------
    
    Co-authored-by: Heart <>

--- crates/bevy_scene/src/lib.rs
+++ crates/bevy_scene/src/lib.rs
@@ -2959,4 +2959,65 @@ mod tests {
             .unwrap();
         assert!(entity.get::<Foo>().is_some());
     }
+
+    #[test]
+    fn scene_nested_entity_references() {
+        let mut app = test_app();
+        let world = app.world_mut();
+
+        #[derive(Component, FromTemplate)]
+        struct Ref(Entity);
+
+        let patch = bsn! {
+            #patch
+            Children [
+                Ref(#patch)
+            ]
+        };
+
+        let root = bsn! {
+            #root
+            patch
+        };
+
+        let expected_id = Some(world.spawn_scene(root).unwrap().id());
+        let actual_id = world
+            .query::<&Ref>()
+            .query(world)
+            .single()
+            .ok()
+            .map(|r| r.0);
+
+        assert_eq!(expected_id, actual_id);
+    }
+
+    #[test]
+    fn scene_list_nested_entity_references() {
+        let mut app = test_app();
+        let world = app.world_mut();
+
+        #[derive(Component, FromTemplate)]
+        struct Ref(Entity);
+
+        let patch = bsn! {
+            #patch
+            Children [
+                Ref(#patch)
+            ]
+        };
+
+        let root = bsn_list! {
+            #root patch
+        };
+
+        let expected_id = Some(world.spawn_scene_list(root).unwrap()[0]);
+        let actual_id = world
+            .query::<&Ref>()
+            .query(world)
+            .single()
+            .ok()
+            .map(|r| r.0);
+
+        assert_eq!(expected_id, actual_id);
+    }
 }
