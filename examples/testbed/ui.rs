9757037c3dda5da9c6b90e0a89880b7446e7b57e
ickshonpe <david.curthoys@googlemail.com>
2026-06-27 21:44:23 +0100

    added elliptical border radius test scene to UI testbed

--- examples/testbed/ui.rs
+++ examples/testbed/ui.rs
@@ -37,6 +37,10 @@ fn main() {
     .add_systems(OnEnter(Scene::Text), text::setup)
     .add_systems(OnEnter(Scene::Grid), grid::setup)
     .add_systems(OnEnter(Scene::Borders), borders::setup)
+    .add_systems(
+        OnEnter(Scene::EllipticalBorderRadius),
+        elliptical_border_radius::setup,
+    )
     .add_systems(OnEnter(Scene::BoxShadow), box_shadow::setup)
     .add_systems(OnEnter(Scene::TextWrap), text_wrap::setup)
     .add_systems(OnEnter(Scene::Overflow), overflow::setup)
@@ -76,6 +80,7 @@ enum Scene {
     Text,
     Grid,
     Borders,
+    EllipticalBorderRadius,
     BoxShadow,
     TextWrap,
     Overflow,
@@ -113,7 +118,8 @@ impl Next for Scene {
             Scene::Image => Scene::Text,
             Scene::Text => Scene::Grid,
             Scene::Grid => Scene::Borders,
-            Scene::Borders => Scene::BoxShadow,
+            Scene::Borders => Scene::EllipticalBorderRadius,
+            Scene::EllipticalBorderRadius => Scene::BoxShadow,
             Scene::BoxShadow => Scene::TextWrap,
             Scene::TextWrap => Scene::Overflow,
             Scene::Overflow => Scene::Slice,
@@ -831,6 +837,296 @@ mod borders {
     }
 }
 
+mod elliptical_border_radius {
+    use bevy::{color::palettes::css::*, prelude::*};
+
+    pub fn setup(mut commands: Commands) {
+        commands.spawn((
+            Camera2d,
+            DespawnOnExit(super::Scene::EllipticalBorderRadius),
+        ));
+        commands
+            .spawn((
+                Node {
+                    width: percent(100),
+                    height: percent(100),
+                    flex_wrap: FlexWrap::Wrap,
+                    column_gap: px(40),
+                    row_gap: px(40),
+                    margin: auto().all(),
+                    padding: px(30).all(),
+                    ..default()
+                },
+                BackgroundColor(Color::srgb(0.12, 0.12, 0.14)),
+                DespawnOnExit(super::Scene::EllipticalBorderRadius),
+            ))
+            .with_children(|builder| {
+                builder.spawn((
+                    Node {
+                        width: px(200),
+                        height: px(100),
+                        border: UiRect::all(px(8)),
+                        border_radius: BorderRadius::elliptical(
+                            Val2::new(px(90), px(24)),
+                            Val2::new(px(18), px(70)),
+                            Val2::new(px(110), px(32)),
+                            Val2::new(px(28), px(58)),
+                        ),
+                        ..default()
+                    },
+                    BackgroundColor(ORANGE.into()),
+                    BorderColor::all(RED),
+                    Outline {
+                        width: px(4),
+                        offset: px(8),
+                        color: WHITE.into(),
+                    },
+                ));
+
+                builder.spawn((
+                    Node {
+                        width: px(150),
+                        height: px(150),
+                        border: UiRect {
+                            left: px(16),
+                            right: px(4),
+                            top: px(24),
+                            bottom: px(8),
+                        },
+                        border_radius: BorderRadius::elliptical(
+                            Val2::new(percent(65), percent(20)),
+                            Val2::new(percent(20), percent(65)),
+                            Val2::new(percent(65), percent(20)),
+                            Val2::new(percent(20), percent(65)),
+                        ),
+                        ..default()
+                    },
+                    BackgroundColor(MEDIUM_SEA_GREEN.into()),
+                    BorderColor::all(DARK_GREEN),
+                    Outline {
+                        width: px(3),
+                        offset: px(10),
+                        color: LIME.into(),
+                    },
+                ));
+
+                builder.spawn((
+                    Node {
+                        width: px(210),
+                        height: px(75),
+                        border: UiRect::axes(px(12), px(4)),
+                        border_radius: BorderRadius::elliptical(
+                            Val2::new(px(140), px(18)),
+                            Val2::new(px(140), px(18)),
+                            Val2::new(px(42), px(54)),
+                            Val2::new(px(42), px(54)),
+                        ),
+                        ..default()
+                    },
+                    BackgroundColor(DODGER_BLUE.into()),
+                    BorderColor::all(NAVY),
+                    Outline {
+                        width: px(5),
+                        offset: px(6),
+                        color: SKY_BLUE.into(),
+                    },
+                ));
+                builder.spawn((
+                    Node {
+                        width: px(160),
+                        height: px(120),
+                        border: UiRect::axes(px(20), px(20)),
+                        border_radius: BorderRadius::elliptical(
+                            Val2::new(px(50), px(10)),
+                            Val2::new(px(50), px(10)),
+                            Val2::new(px(50), px(10)),
+                            Val2::new(px(50), px(10)),
+                        ),
+                        ..default()
+                    },
+                    BackgroundColor(RED.into()),
+                    BorderColor::all(WHITE),
+                    Outline {
+                        width: px(3),
+                        offset: px(5),
+                        color: SKY_BLUE.into(),
+                    },
+                ));
+
+                builder.spawn((
+                    Node {
+                        width: px(160),
+                        height: px(120),
+                        border: UiRect::axes(px(20), px(20)),
+                        border_radius: BorderRadius::all(px(30)),
+                        ..default()
+                    },
+                    BackgroundColor(RED.into()),
+                    BorderColor::all(WHITE),
+                    Outline {
+                        width: px(3),
+                        offset: px(5),
+                        color: SKY_BLUE.into(),
+                    },
+                ));
+
+                builder.spawn((
+                    Node {
+                        width: px(160),
+                        height: px(120),
+                        border: UiRect::axes(px(20), px(20)),
+                        border_radius: BorderRadius::elliptical(
+                            Val2::new(px(25), px(20)),
+                            Val2::new(px(20), px(25)),
+                            Val2::new(px(20), px(25)),
+                            Val2::new(px(20), px(25)),
+                        ),
+                        ..default()
+                    },
+                    BackgroundColor(RED.into()),
+                    BorderColor::all(WHITE),
+                    Outline {
+                        width: px(3),
+                        offset: px(5),
+                        color: SKY_BLUE.into(),
+                    },
+                ));
+
+                builder.spawn((
+                    Node {
+                        width: px(160),
+                        height: px(120),
+                        border: UiRect::axes(px(10), px(10)),
+                        border_radius: BorderRadius::elliptical(
+                            Val2::new(px(40), px(30)),
+                            Val2::new(px(40), px(30)),
+                            Val2::new(px(40), px(30)),
+                            Val2::new(px(40), px(30)),
+                        ),
+                        ..default()
+                    },
+                    BackgroundColor(RED.into()),
+                    BorderColor::all(WHITE),
+                    Outline {
+                        width: px(3),
+                        offset: px(5),
+                        color: SKY_BLUE.into(),
+                    },
+                ));
+
+                builder.spawn((
+                    Node {
+                        width: px(160),
+                        height: px(80),
+                        border: UiRect::axes(px(10), px(10)),
+                        border_radius: BorderRadius::elliptical(
+                            Val2::new(percent(50), percent(50)),
+                            Val2::new(percent(50), percent(50)),
+                            Val2::new(percent(50), percent(50)),
+                            Val2::new(percent(50), percent(50)),
+                        ),
+                        ..default()
+                    },
+                    BackgroundColor(RED.into()),
+                    BorderColor::all(WHITE),
+                    Outline {
+                        width: px(3),
+                        offset: px(5),
+                        color: SKY_BLUE.into(),
+                    },
+                ));
+
+                builder.spawn((
+                    Node {
+                        width: px(80),
+                        height: px(160),
+                        border: UiRect::axes(px(10), px(10)),
+                        border_radius: BorderRadius::elliptical(
+                            Val2::new(percent(50), percent(50)),
+                            Val2::new(percent(50), percent(50)),
+                            Val2::new(percent(50), percent(50)),
+                            Val2::new(percent(50), percent(50)),
+                        ),
+                        ..default()
+                    },
+                    BackgroundColor(RED.into()),
+                    BorderColor::all(WHITE),
+                    Outline {
+                        width: px(3),
+                        offset: px(5),
+                        color: SKY_BLUE.into(),
+                    },
+                ));
+
+                builder.spawn((
+                    Node {
+                        width: px(160),
+                        height: px(80),
+                        border: UiRect::axes(px(20), px(10)),
+                        border_radius: BorderRadius::elliptical(
+                            Val2::new(percent(50), percent(50)),
+                            Val2::new(percent(50), percent(50)),
+                            Val2::new(percent(50), percent(50)),
+                            Val2::new(percent(50), percent(50)),
+                        ),
+                        ..default()
+                    },
+                    BackgroundColor(RED.into()),
+                    BorderColor::all(WHITE),
+                    Outline {
+                        width: px(3),
+                        offset: px(5),
+                        color: SKY_BLUE.into(),
+                    },
+                ));
+
+                builder.spawn((
+                    Node {
+                        width: px(80),
+                        height: px(160),
+                        border: UiRect::all(px(10)).with_right(px(25)),
+                        border_radius: BorderRadius::elliptical(
+                            Val2::new(percent(50), percent(50)),
+                            Val2::new(percent(50), percent(50)),
+                            Val2::new(percent(50), percent(50)),
+                            Val2::new(percent(50), percent(50)),
+                        ),
+                        ..default()
+                    },
+                    BackgroundColor(RED.into()),
+                    BorderColor::all(WHITE),
+                    Outline {
+                        width: px(3),
+                        offset: px(5),
+                        color: SKY_BLUE.into(),
+                    },
+                ));
+
+                builder.spawn((
+                    Node {
+                        width: px(160),
+                        height: px(80),
+                        border: UiRect::all(px(5)),
+                        border_radius: BorderRadius::elliptical(
+                            Val2::new(percent(50), percent(50)),
+                            Val2::new(percent(50), percent(50)),
+                            Val2::new(percent(50), percent(50)),
+                            Val2::new(px(20), px(20)),
+                        ),
+                        ..default()
+                    },
+                    BackgroundColor(RED.into()),
+                    BorderColor::all(WHITE),
+                    Outline {
+                        width: px(3),
+                        offset: px(5),
+                        color: SKY_BLUE.into(),
+                    },
+                ));
+            });
+    }
+}
+
 mod box_shadow {
     use bevy::{color::palettes::css::*, prelude::*};
 
@@ -857,7 +1153,7 @@ mod box_shadow {
                         Vec2::ZERO,
                         10.,
                         0.,
-                        BorderRadius::bottom_right(px(10)),
+                        BorderRadius::bottom_right(Val2::all(px(10))),
                     ),
                     (Vec2::new(200., 50.), Vec2::ZERO, 10., 0., BorderRadius::MAX),
                     (
@@ -872,7 +1168,7 @@ mod box_shadow {
                         Vec2::splat(20.),
                         10.,
                         10.,
-                        BorderRadius::bottom_right(px(10)),
+                        BorderRadius::bottom_right(Val2::all(px(10))),
                     ),
                     (
                         Vec2::splat(100.),
@@ -1457,7 +1753,7 @@ mod transformations {
                                 Node {
                                     width: px(100),
                                     height: px(100),
-                                    border_radius: BorderRadius::bottom_right(px(25.)),
+                                    border_radius: BorderRadius::bottom_right(Val2::all(px(25.))),
                                     ..default()
                                 },
                                 BackgroundColor(background.into()),
@@ -1468,7 +1764,7 @@ mod transformations {
                                 Node {
                                     width: px(100),
                                     height: px(100),
-                                    border_radius: BorderRadius::bottom_right(px(25.)),
+                                    border_radius: BorderRadius::bottom_right(Val2::all(px(25.))),
                                     ..default()
                                 },
                                 BackgroundColor(background.into()),
@@ -1821,7 +2117,7 @@ mod outer_color {
     use bevy::prelude::*;
 
     pub fn setup(mut commands: Commands) {
-        let radius = percent(33.);
+        let radius = Val2::all(percent(33.));
         let width = px(10.);
 
         commands.spawn((Camera2d, DespawnOnExit(super::Scene::OuterColor)));
@@ -1842,7 +2138,7 @@ mod outer_color {
                     (UiRect::top(width), BorderRadius::top(radius), false),
                     (UiRect::ZERO, BorderRadius::bottom_left(radius), true),
                     (UiRect::left(width), BorderRadius::left(radius), false),
-                    (UiRect::all(width), BorderRadius::all(radius), true),
+                    (UiRect::all(width), BorderRadius::all(radius.x), true),
                     (UiRect::right(width), BorderRadius::right(radius), false),
                     (UiRect::ZERO, BorderRadius::top_right(radius), true),
                     (UiRect::bottom(width), BorderRadius::bottom(radius), false),
