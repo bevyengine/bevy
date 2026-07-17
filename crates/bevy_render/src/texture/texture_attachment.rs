bevyengine/bevy

commit 493a2f477b3bcc1b52d45e2359fc7e26d7037b27

Handle optional depth and stencil views of view depth and prepass depth texture (#24725)
# Objective
Related: https://github.com/bevyengine/bevy/issues/21358
Supersedes https://github.com/bevyengine/bevy/pull/22556

## Solution

To support custom depth formats, we need to handle depth and stencil
views depending on usage: only single aspect view can be used as
resource binding in shaders, only full aspect view can be used as a
render attachment.

Refactor `DepthAttachment`, `ViewDepthTexture` and
`ViewPrepassTextures::depth` to handle optional depth and stencil views.

This does not yet support custom depth formats. The depth formats needs
to be added to pipeline key similar to how color texture formats are
handled, as a followup (adopt
https://github.com/bevyengine/bevy/pull/21493)

## Testing

CI

crates/bevy_render/src/texture/texture_attachment.rs
@@ -5,7 +5,7 @@ use bevy_color::LinearRgba;
 use core::sync::atomic::{AtomicBool, Ordering};
 use wgpu::{
     Color as WgpuColor, LoadOp, Operations, RenderPassColorAttachment,
-    RenderPassDepthStencilAttachment, StoreOp,
+    RenderPassDepthStencilAttachment, StoreOp, TextureViewDescriptor,
 };
 
 /// A wrapper for a [`CachedTexture`] that is used as a [`RenderPassColorAttachment`].
@@ -85,20 +85,172 @@ impl ColorAttachment {
     }
 }
 
-/// A wrapper for a [`TextureView`] that is used as a depth-only [`RenderPassDepthStencilAttachment`].
 #[derive(Clone)]
-pub struct DepthAttachment {
-    pub view: TextureView,
-    clear_value: Option<f32>,
-    is_first_call: Arc<AtomicBool>,
+enum DepthStencilData {
+    DepthOnly {
+        depth_clear_value: Option<f32>,
+        depth_is_first_call: Arc<AtomicBool>,
+    },
+    StencilOnly {
+        stencil_clear_value: Option<u32>,
+        stencil_is_first_call: Arc<AtomicBool>,
+    },
+    DepthStencil {
+        depth_clear_value: Option<f32>,
+        stencil_clear_value: Option<u32>,
+        depth_is_first_call: Arc<AtomicBool>,
+        stencil_is_first_call: Arc<AtomicBool>,
+    },
 }
 
-impl DepthAttachment {
-    pub fn new(view: TextureView, clear_value: Option<f32>) -> Self {
+/// Depth and stencil views of a depth texture.
+///
+/// Texture views with single depth or stencil aspect can be read in shaders.
+/// Texture views with all aspect is renderable.
+/// See <https://gpuweb.github.io/gpuweb/#depth-formats>
+/// and <https://gpuweb.github.io/gpuweb/#abstract-opdef-renderable-texture-view>.
+#[derive(Clone)]
+pub enum DepthStencilViews {
+    /// The texture only has depth aspect.
+    DepthOnly { depth_view: TextureView },
+    /// The texture only has stencil aspect.
+    StencilOnly { stencil_view: TextureView },
+    /// The texture has combined depth and stencil.
+    DepthStencil {
+        /// A texture view with both depth and stencil aspects, renderable, but can't be used as a binding resource.
+        combined_view: TextureView,
+        /// A texture view with depth only aspect, sample type `unfilterable-float` or `depth` in shaders, not renderable.
+        depth_view: TextureView,
+        /// A texture view with stencil only aspect, sample type `uint` in shaders, not renderable.
+        stencil_view: TextureView,
+    },
+}
+
+impl DepthStencilViews {
+    /// Return [`DepthStencilViews::DepthStencil::depth_view`] or [`DepthStencilViews::DepthOnly::depth_view`] or `None`.
+    pub fn depth_only_view(&self) -> Option<&TextureView> {
+        match self {
+            DepthStencilViews::DepthStencil { depth_view, .. }
+            | DepthStencilViews::DepthOnly { depth_view } => Some(depth_view),
+            DepthStencilViews::StencilOnly { .. } => None,
+        }
+    }
+
+    /// Return [`DepthStencilViews::DepthStencil::stencil_view`] or [`DepthStencilViews::StencilOnly::stencil_view`] or `None`.
+    pub fn stencil_only_view(&self) -> Option<&TextureView> {
+        match self {
+            DepthStencilViews::DepthOnly { .. } => None,
+            DepthStencilViews::DepthStencil { stencil_view, .. }
+            | DepthStencilViews::StencilOnly { stencil_view } => Some(stencil_view),
+        }
+    }
+
+    /// Return [`DepthStencilViews::DepthStencil::combined_view`] or [`DepthStencilViews::DepthOnly::depth_view`] or [`DepthStencilViews::StencilOnly::stencil_view`].
+    pub fn attachment_view(&self) -> &TextureView {
+        match self {
+            DepthStencilViews::DepthOnly { depth_view } => depth_view,
+            DepthStencilViews::StencilOnly { stencil_view } => stencil_view,
+            DepthStencilViews::DepthStencil { combined_view, .. } => combined_view,
+        }
+    }
+}
+
+/// A wrapper for a [`CachedTexture`] that is used as a depth [`RenderPassDepthStencilAttachment`].
+#[derive(Clone)]
+pub struct DepthStencilAttachment {
+    pub texture: CachedTexture,
+    pub previous_frame_texture: Option<CachedTexture>,
+    depth_stencil_view_attachment: DepthStencilViewAttachment,
+    pub previous_frame_depth_stencil_views: Option<DepthStencilViews>,
+}
+
+impl DepthStencilAttachment {
+    pub fn depth_stencil_views(&self) -> &DepthStencilViews {
+        &self.depth_stencil_view_attachment.depth_stencil_views
+    }
+
+    pub fn new(
+        texture: CachedTexture,
+        previous_frame_texture: Option<CachedTexture>,
+        depth_clear_value: Option<f32>,
+        stencil_clear_value: Option<u32>,
+    ) -> Self {
+        let depth_stencil_views = match texture.texture.format().channels() {
+            wgpu_types::TextureChannel::DEPTH_STENCIL => DepthStencilViews::DepthStencil {
+                combined_view: texture
+                    .texture
+                    .create_view(&TextureViewDescriptor::default()),
+                depth_view: texture.texture.create_view(&TextureViewDescriptor {
+                    aspect: wgpu::TextureAspect::DepthOnly,
+                    ..Default::default()
+                }),
+                stencil_view: texture.texture.create_view(&TextureViewDescriptor {
+                    aspect: wgpu::TextureAspect::StencilOnly,
+                    ..Default::default()
+                }),
+            },
+            wgpu_types::TextureChannel::DEPTH => DepthStencilViews::DepthOnly {
+                depth_view: texture.texture.create_view(&TextureViewDescriptor {
+                    aspect: wgpu::TextureAspect::DepthOnly,
+                    ..Default::default()
+                }),
+            },
+            wgpu_types::TextureChannel::STENCIL => DepthStencilViews::StencilOnly {
+                stencil_view: texture.texture.create_view(&TextureViewDescriptor {
+                    aspect: wgpu::TextureAspect::StencilOnly,
+                    ..Default::default()
+                }),
+            },
+            _ => {
+                panic!("Can't create depth attachment. Texture format is not depth-stencil format")
+            }
+        };
+
+        let previous_frame_depth_stencil_views = match previous_frame_texture
+            .as_ref()
+            .map(|t| t.texture.format().channels())
+        {
+            Some(wgpu_types::TextureChannel::DEPTH_STENCIL) => {
+                Some(DepthStencilViews::DepthStencil {
+                    combined_view: texture
+                        .texture
+                        .create_view(&TextureViewDescriptor::default()),
+                    depth_view: texture.texture.create_view(&TextureViewDescriptor {
+                        aspect: wgpu::TextureAspect::DepthOnly,
+                        ..Default::default()
+                    }),
+                    stencil_view: texture.texture.create_view(&TextureViewDescriptor {
+                        aspect: wgpu::TextureAspect::StencilOnly,
+                        ..Default::default()
+                    }),
+                })
+            }
+            Some(wgpu_types::TextureChannel::DEPTH) => Some(DepthStencilViews::DepthOnly {
+                depth_view: texture.texture.create_view(&TextureViewDescriptor {
+                    aspect: wgpu::TextureAspect::DepthOnly,
+                    ..Default::default()
+                }),
+            }),
+            Some(wgpu_types::TextureChannel::STENCIL) => Some(DepthStencilViews::StencilOnly {
+                stencil_view: texture.texture.create_view(&TextureViewDescriptor {
+                    aspect: wgpu::TextureAspect::StencilOnly,
+                    ..Default::default()
+                }),
+            }),
+            None => None,
+            Some(_) => {
+                panic!("Can't create depth attachment. Texture format is not depth-stencil format")
+            }
+        };
         Self {
-            view,
-            clear_value,
-            is_first_call: Arc::new(AtomicBool::new(clear_value.is_some())),
+            texture,
+            previous_frame_texture,
+            depth_stencil_view_attachment: DepthStencilViewAttachment::new(
+                depth_stencil_views,
+                depth_clear_value,
+                stencil_clear_value,
+            ),
+            previous_frame_depth_stencil_views,
         }
     }
 
@@ -106,29 +258,167 @@ impl DepthAttachment {
     /// `clear_value` if this is the first time calling this function with `store` == [`StoreOp::Store`],
     /// and a clear value was provided, otherwise it will be loaded.
     pub fn get_attachment(&self, store: StoreOp) -> RenderPassDepthStencilAttachment<'_> {
-        let first_call = self
-            .is_first_call
-            .fetch_and(store != StoreOp::Store, Ordering::Relaxed);
+        self.depth_stencil_view_attachment.get_attachment(store)
+    }
 
-        RenderPassDepthStencilAttachment {
-            view: &self.view,
-            depth_ops: Some(Operations {
-                load: if first_call {
-                    // If first_call is true, then a clear value will always have been provided in the constructor
-                    LoadOp::Clear(self.clear_value.unwrap())
-                } else {
-                    LoadOp::Load
+    /// Marks this depth attachment as unused this frame so that it'll be
+    /// cleared at first use.
+    pub fn prepare_for_new_frame(&self) {
+        self.depth_stencil_view_attachment.prepare_for_new_frame();
+    }
+}
+
+/// A wrapper for a [`TextureView`] that is used as a [`RenderPassDepthStencilAttachment`].
+#[derive(Clone)]
+pub struct DepthStencilViewAttachment {
+    pub depth_stencil_views: DepthStencilViews,
+    depth_stencil_data: DepthStencilData,
+}
+
+impl DepthStencilViewAttachment {
+    pub fn new(
+        depth_stencil_views: DepthStencilViews,
+        depth_clear_value: Option<f32>,
+        stencil_clear_value: Option<u32>,
+    ) -> Self {
+        let depth_stencil_data = match &depth_stencil_views {
+            DepthStencilViews::DepthStencil { .. } => DepthStencilData::DepthStencil {
+                depth_clear_value,
+                stencil_clear_value,
+                depth_is_first_call: Arc::new(AtomicBool::new(depth_clear_value.is_some())),
+                stencil_is_first_call: Arc::new(AtomicBool::new(stencil_clear_value.is_some())),
+            },
+            DepthStencilViews::DepthOnly { .. } => DepthStencilData::DepthOnly {
+                depth_clear_value,
+                depth_is_first_call: Arc::new(AtomicBool::new(depth_clear_value.is_some())),
+            },
+            DepthStencilViews::StencilOnly { .. } => DepthStencilData::StencilOnly {
+                stencil_clear_value,
+                stencil_is_first_call: Arc::new(AtomicBool::new(stencil_clear_value.is_some())),
+            },
+        };
+        Self {
+            depth_stencil_views,
+            depth_stencil_data,
+        }
+    }
+
+    /// Get this texture view as an attachment. The attachment will be cleared with a value of
+    /// `clear_value` if this is the first time calling this function with `store` == [`StoreOp::Store`],
+    /// and a clear value was provided, otherwise it will be loaded.
+    pub fn get_attachment(&self, store: StoreOp) -> RenderPassDepthStencilAttachment<'_> {
+        match (&self.depth_stencil_data, &self.depth_stencil_views) {
+            (
+                DepthStencilData::DepthStencil {
+                    depth_clear_value,
+                    stencil_clear_value,
+                    depth_is_first_call,
+                    stencil_is_first_call,
                 },
-                store,
-            }),
-            stencil_ops: None,
+                DepthStencilViews::DepthStencil { combined_view, .. },
+            ) => {
+                let depth_is_first_call =
+                    depth_is_first_call.fetch_and(store != StoreOp::Store, Ordering::Relaxed);
+                let stencil_is_first_call =
+                    stencil_is_first_call.fetch_and(store != StoreOp::Store, Ordering::Relaxed);
+                RenderPassDepthStencilAttachment {
+                    view: combined_view,
+                    depth_ops: Some(Operations {
+                        load: if depth_is_first_call {
+                            // If first_call is true, then a clear value will always have been provided in the constructor
+                            LoadOp::Clear(depth_clear_value.unwrap())
+                        } else {
+                            LoadOp::Load
+                        },
+                        store,
+                    }),
+                    stencil_ops: Some(Operations {
+                        load: if stencil_is_first_call {
+                            // If first_call is true, then a clear value will always have been provided in the constructor
+                            LoadOp::Clear(stencil_clear_value.unwrap())
+                        } else {
+                            LoadOp::Load
+                        },
+                        store,
+                    }),
+                }
+            }
+            (
+                DepthStencilData::DepthOnly {
+                    depth_clear_value,
+                    depth_is_first_call,
+                },
+                DepthStencilViews::DepthOnly { depth_view },
+            ) => {
+                let depth_is_first_call =
+                    depth_is_first_call.fetch_and(store != StoreOp::Store, Ordering::Relaxed);
+                RenderPassDepthStencilAttachment {
+                    view: depth_view,
+                    depth_ops: Some(Operations {
+                        load: if depth_is_first_call {
+                            // If first_call is true, then a clear value will always have been provided in the constructor
+                            LoadOp::Clear(depth_clear_value.unwrap())
+                        } else {
+                            LoadOp::Load
+                        },
+                        store,
+                    }),
+                    stencil_ops: None,
+                }
+            }
+            (
+                DepthStencilData::StencilOnly {
+                    stencil_clear_value,
+                    stencil_is_first_call,
+                },
+                DepthStencilViews::StencilOnly { stencil_view },
+            ) => {
+                let stencil_is_first_call =
+                    stencil_is_first_call.fetch_and(store != StoreOp::Store, Ordering::Relaxed);
+                RenderPassDepthStencilAttachment {
+                    view: stencil_view,
+                    depth_ops: None,
+                    stencil_ops: Some(Operations {
+                        load: if stencil_is_first_call {
+                            // If first_call is true, then a clear value will always have been provided in the constructor
+                            LoadOp::Clear(stencil_clear_value.unwrap())
+                        } else {
+                            LoadOp::Load
+                        },
+                        store,
+                    }),
+                }
+            }
+
+            _ => unreachable!("Mismatched depth stencil data and views"),
         }
     }
 
     /// Marks this depth attachment as unused this frame so that it'll be
     /// cleared at first use.
     pub fn prepare_for_new_frame(&self) {
-        self.is_first_call.store(true, Ordering::Relaxed);
+        match &self.depth_stencil_data {
+            DepthStencilData::DepthStencil {
+                depth_is_first_call,
+                stencil_is_first_call,
+                ..
+            } => {
+                depth_is_first_call.store(true, Ordering::Relaxed);
+                stencil_is_first_call.store(true, Ordering::Relaxed);
+            }
+            DepthStencilData::DepthOnly {
+                depth_is_first_call,
+                ..
+            } => {
+                depth_is_first_call.store(true, Ordering::Relaxed);
+            }
+            DepthStencilData::StencilOnly {
+                stencil_is_first_call,
+                ..
+            } => {
+                stencil_is_first_call.store(true, Ordering::Relaxed);
+            }
+        }
     }
 }
 

crates/bevy_core_pipeline/src/core_2d/main_opaque_pass_2d_node.rs
@@ -9,7 +9,7 @@ use bevy_render::{
     render_phase::ViewBinnedRenderPhases,
     render_resource::{RenderPassDescriptor, StoreOp},
     renderer::{RenderContext, ViewQuery},
-    view::{ExtractedView, ViewDepthTexture, ViewTarget},
+    view::{ExtractedView, ViewDepthStencilTexture, ViewTarget},
 };
 
 use super::AlphaMask2d;
@@ -20,7 +20,7 @@ pub fn main_opaque_pass_2d(
         &ExtractedCamera,
         &ExtractedView,
         &ViewTarget,
-        &ViewDepthTexture,
+        &ViewDepthStencilTexture,
     )>,
     opaque_phases: Res<ViewBinnedRenderPhases<Opaque2d>>,
     alpha_mask_phases: Res<ViewBinnedRenderPhases<AlphaMask2d>>,

crates/bevy_core_pipeline/src/core_2d/main_transparent_pass_2d_node.rs
@@ -9,7 +9,7 @@ use bevy_render::{
     render_phase::ViewSortedRenderPhases,
     render_resource::{RenderPassDescriptor, StoreOp},
     renderer::{RenderContext, ViewQuery},
-    view::{ExtractedView, ViewDepthTexture, ViewTarget},
+    view::{ExtractedView, ViewDepthStencilTexture, ViewTarget},
 };
 
 pub fn main_transparent_pass_2d(
@@ -18,7 +18,7 @@ pub fn main_transparent_pass_2d(
         &ExtractedCamera,
         &ExtractedView,
         &ViewTarget,
-        &ViewDepthTexture,
+        &ViewDepthStencilTexture,
     )>,
     transparent_phases: Res<ViewSortedRenderPhases<Transparent2d>>,
     mut ctx: RenderContext,

crates/bevy_core_pipeline/src/core_2d/mod.rs
@@ -39,7 +39,7 @@ use bevy_render::{
     renderer::RenderDevice,
     sync_world::MainEntity,
     texture::TextureCache,
-    view::{Msaa, ViewDepthTexture},
+    view::{Msaa, ViewDepthStencilTexture},
     Extract, ExtractSchedule, Render, RenderApp, RenderSystems,
 };
 
@@ -459,8 +459,10 @@ pub fn prepare_core_2d_depth_textures(
             })
             .clone();
 
-        commands
-            .entity(view)
-            .insert(ViewDepthTexture::new(cached_texture, Some(0.0)));
+        commands.entity(view).insert(ViewDepthStencilTexture::new(
+            cached_texture,
+            Some(0.0),
+            None,
+        ));
     }
 }

crates/bevy_core_pipeline/src/core_3d/main_opaque_pass_3d_node.rs
@@ -13,7 +13,7 @@ use bevy_render::{
     render_phase::ViewBinnedRenderPhases,
     render_resource::{PipelineCache, RenderPassDescriptor, StoreOp},
     renderer::{RenderContext, ViewQuery},
-    view::{ExtractedView, ViewDepthTexture, ViewTarget, ViewUniformOffset},
+    view::{ExtractedView, ViewDepthStencilTexture, ViewTarget, ViewUniformOffset},
 };
 
 use super::AlphaMask3d;
@@ -24,7 +24,7 @@ pub fn main_opaque_pass_3d(
         &ExtractedCamera,
         &ExtractedView,
         &ViewTarget,
-        &ViewDepthTexture,
+        &ViewDepthStencilTexture,
         Option<&SkyboxPipelineId>,
         Option<&SkyboxBindGroup>,
         &ViewUniformOffset,

crates/bevy_core_pipeline/src/core_3d/main_transparent_pass_3d_node.rs
@@ -13,7 +13,7 @@ use bevy_render::{
     render_phase::ViewSortedRenderPhases,
     render_resource::{PipelineCache, RenderPassDescriptor, StoreOp},
     renderer::{RenderContext, ViewQuery},
-    view::{ExtractedView, ViewDepthTexture, ViewTarget},
+    view::{ExtractedView, ViewDepthStencilTexture, ViewTarget},
 };
 
 pub fn main_transparent_pass_3d(
@@ -22,7 +22,7 @@ pub fn main_transparent_pass_3d(
         &ExtractedCamera,
         &ExtractedView,
         &ViewTarget,
-        &ViewDepthTexture,
+        &ViewDepthStencilTexture,
         Option<&MainPassResolutionOverride>,
         Has<OrderIndependentTransparencySettings>,
         Option<&OitResolvePipelineId>,

commit fb89a8649d9b359e53ffb6e5492ebb7c059ac8af

Fix previous frame depth-stencil view  (#25043)
# Objective

- Fix bugs in Solari introduced by
https://github.com/bevyengine/bevy/pull/24725

## Solution

- Fix the copy-paste bug where previous frame depth view was built using
the current depth texture, and not the previous depth texture.

## Testing
- Tested in Solari

crates/bevy_render/src/texture/texture_attachment.rs
@@ -153,6 +153,44 @@ impl DepthStencilViews {
             DepthStencilViews::DepthStencil { combined_view, .. } => combined_view,
         }
     }
+
+    /// Create the appropriate views for a depth-stencil texture based on its texture format.
+    fn from_texture(texture: &CachedTexture) -> Self {
+        let depth_view = || {
+            texture.texture.create_view(&TextureViewDescriptor {
+                aspect: wgpu::TextureAspect::DepthOnly,
+                ..Default::default()
+            })
+        };
+
+        let stencil_view = || {
+            texture.texture.create_view(&TextureViewDescriptor {
+                aspect: wgpu::TextureAspect::StencilOnly,
+                ..Default::default()
+            })
+        };
+
+        match texture.texture.format().channels() {
+            wgpu_types::TextureChannel::DEPTH_STENCIL => DepthStencilViews::DepthStencil {
+                combined_view: texture
+                    .texture
+                    .create_view(&TextureViewDescriptor::default()),
+                depth_view: depth_view(),
+                stencil_view: stencil_view(),
+            },
+            wgpu_types::TextureChannel::DEPTH => DepthStencilViews::DepthOnly {
+                depth_view: depth_view(),
+            },
+            wgpu_types::TextureChannel::STENCIL => DepthStencilViews::StencilOnly {
+                stencil_view: stencil_view(),
+            },
+            _ => {
+                panic!(
+                    "Can't create depth attachment. Texture format is not a depth-stencil format."
+                )
+            }
+        }
+    }
 }
 
 /// A wrapper for a [`CachedTexture`] that is used as a depth [`RenderPassDepthStencilAttachment`].
@@ -175,73 +213,12 @@ impl DepthStencilAttachment {
         depth_clear_value: Option<f32>,
         stencil_clear_value: Option<u32>,
     ) -> Self {
-        let depth_stencil_views = match texture.texture.format().channels() {
-            wgpu_types::TextureChannel::DEPTH_STENCIL => DepthStencilViews::DepthStencil {
-                combined_view: texture
-                    .texture
-                    .create_view(&TextureViewDescriptor::default()),
-                depth_view: texture.texture.create_view(&TextureViewDescriptor {
-                    aspect: wgpu::TextureAspect::DepthOnly,
-                    ..Default::default()
-                }),
-                stencil_view: texture.texture.create_view(&TextureViewDescriptor {
-                    aspect: wgpu::TextureAspect::StencilOnly,
-                    ..Default::default()
-                }),
-            },
-            wgpu_types::TextureChannel::DEPTH => DepthStencilViews::DepthOnly {
-                depth_view: texture.texture.create_view(&TextureViewDescriptor {
-                    aspect: wgpu::TextureAspect::DepthOnly,
-                    ..Default::default()
-                }),
-            },
-            wgpu_types::TextureChannel::STENCIL => DepthStencilViews::StencilOnly {
-                stencil_view: texture.texture.create_view(&TextureViewDescriptor {
-                    aspect: wgpu::TextureAspect::StencilOnly,
-                    ..Default::default()
-                }),
-            },
-            _ => {
-                panic!("Can't create depth attachment. Texture format is not depth-stencil format")
-            }
-        };
+        let depth_stencil_views = DepthStencilViews::from_texture(&texture);
 
-        let previous_frame_depth_stencil_views = match previous_frame_texture
+        let previous_frame_depth_stencil_views = previous_frame_texture
             .as_ref()
-            .map(|t| t.texture.format().channels())
-        {
-            Some(wgpu_types::TextureChannel::DEPTH_STENCIL) => {
-                Some(DepthStencilViews::DepthStencil {
-                    combined_view: texture
-                        .texture
-                        .create_view(&TextureViewDescriptor::default()),
-                    depth_view: texture.texture.create_view(&TextureViewDescriptor {
-                        aspect: wgpu::TextureAspect::DepthOnly,
-                        ..Default::default()
-                    }),
-                    stencil_view: texture.texture.create_view(&TextureViewDescriptor {
-                        aspect: wgpu::TextureAspect::StencilOnly,
-                        ..Default::default()
-                    }),
-                })
-            }
-            Some(wgpu_types::TextureChannel::DEPTH) => Some(DepthStencilViews::DepthOnly {
-                depth_view: texture.texture.create_view(&TextureViewDescriptor {
-                    aspect: wgpu::TextureAspect::DepthOnly,
-                    ..Default::default()
-                }),
-            }),
-            Some(wgpu_types::TextureChannel::STENCIL) => Some(DepthStencilViews::StencilOnly {
-                stencil_view: texture.texture.create_view(&TextureViewDescriptor {
-                    aspect: wgpu::TextureAspect::StencilOnly,
-                    ..Default::default()
-                }),
-            }),
-            None => None,
-            Some(_) => {
-                panic!("Can't create depth attachment. Texture format is not depth-stencil format")
-            }
-        };
+            .map(DepthStencilViews::from_texture);
+
         Self {
             texture,
             previous_frame_texture,