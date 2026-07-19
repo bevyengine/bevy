985c90df1c404ec450e7ac035da6acb2539c065a
Kevin Chen <chen.kevin.f@gmail.com>
2026-07-19 00:39:14 -0400

    separate out custom theme into different file

---
+++ examples/helpers/theme.rs
@@ -0,0 +1,46 @@
+use bevy::{color::palettes, feathers::theme::ThemeProps, platform::collections::HashMap};
+
+/// Creates a basic feathers theme props for the radio buttons.
+pub fn basic_radio_button_theme() -> ThemeProps {
+    let mut color = HashMap::new();
+    color.insert(bevy::feathers::tokens::RADIO_TEXT, Color::BLACK);
+    color.insert(bevy::feathers::tokens::RADIO_MARK, Color::BLACK);
+    color.insert(bevy::feathers::tokens::RADIO_MARK_HOVER, Color::BLACK);
+    color.insert(bevy::feathers::tokens::RADIO_MARK_PRESSED, Color::BLACK);
+
+    color.insert(bevy::feathers::tokens::RADIO_BG, Color::WHITE);
+    color.insert(
+        bevy::feathers::tokens::RADIO_BG_HOVER,
+        palettes::basic::GRAY.into(),
+    );
+    color.insert(
+        bevy::feathers::tokens::RADIO_BG_PRESSED,
+        palettes::basic::GRAY.into(),
+    );
+    color.insert(bevy::feathers::tokens::RADIO_BG_CHECKED, Color::WHITE);
+    color.insert(bevy::feathers::tokens::RADIO_BG_CHECKED_HOVER, Color::BLACK);
+    color.insert(
+        bevy::feathers::tokens::RADIO_BG_CHECKED_PRESSED,
+        Color::BLACK,
+    );
+
+    color.insert(bevy::feathers::tokens::RADIO_BORDER, Color::BLACK);
+    color.insert(
+        bevy::feathers::tokens::RADIO_BORDER_HOVER,
+        palettes::basic::GRAY.into(),
+    );
+    color.insert(
+        bevy::feathers::tokens::RADIO_BORDER_PRESSED,
+        palettes::basic::BLACK.into(),
+    );
+    color.insert(bevy::feathers::tokens::RADIO_BORDER_CHECKED, Color::BLACK);
+    color.insert(
+        bevy::feathers::tokens::RADIO_BORDER_CHECKED_HOVER,
+        Color::BLACK,
+    );
+    color.insert(
+        bevy::feathers::tokens::RADIO_BORDER_CHECKED_PRESSED,
+        Color::BLACK,
+    );
+    ThemeProps { color }
+}
