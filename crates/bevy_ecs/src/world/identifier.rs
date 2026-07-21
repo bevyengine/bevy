5da77c099f827c6a5decd345e7d6f3517cf58143
Ed Swartz <875407+eswartz@users.noreply.github.com>
2026-07-21 10:17:35 -0500

    Apply suggested fix for `try_update` from nightly 1.99+ compiler.
    ```
    warning: use of deprecated method `core::sync::atomic::Atomic::<usize>::fetch_update`: renamed to `try_update` for consistency
      --> crates/bevy_ecs/src/world/identifier.rs:36:14
    ```
    
    Atomic*::try_update is available since 1.95
    https://doc.rust-lang.org/stable/std/sync/atomic/type.AtomicUsize.html#method.try_update

--- crates/bevy_ecs/src/world/identifier.rs
+++ crates/bevy_ecs/src/world/identifier.rs
@@ -33,7 +33,7 @@ impl WorldId {
     pub fn new() -> Option<Self> {
         MAX_WORLD_ID
             // We use `Relaxed` here since this atomic only needs to be consistent with itself
-            .fetch_update(Ordering::Relaxed, Ordering::Relaxed, |val| {
+            .try_update(Ordering::Relaxed, Ordering::Relaxed, |val| {
                 val.checked_add(1)
             })
             .map(WorldId)
