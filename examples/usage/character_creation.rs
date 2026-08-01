946f02c5b829883acfabd16fe633ead9a540a52b
Kevin Chen <chen.kevin.f@gmail.com>
2026-08-01 00:46:31 -0400

    Name Text Input

--- examples/usage/character_creation.rs
+++ examples/usage/character_creation.rs
@@ -2,9 +2,10 @@
 //! widgets in the `ui_widgets` crate.
 //!
 //! When a UI widget is interacted with by an application user,
-//! various events are triggered that that the user presumably wants
-//! to be recognized by the application. It is up to the application to
-//! handle and interpret these events, which usually means updating its own internal
+//! various events may be triggered that that the user presumably wants
+//! to be recognized by the application. State may be directly changed
+//! on the widgets themselves depending on the widget. It is up to the application to
+//! handle and interpret these events and changes, which usually means updating its own internal
 //! state and refreshing its user interface so that the user sees the most
 //! recent data.
 //!
@@ -17,7 +18,9 @@
 use bevy::{
     color::palettes,
     ecs::schedule::IntoScheduleConfigs,
+    input_focus::tab_navigation::TabIndex,
     prelude::*,
+    text::{EditableText, TextCursorStyle},
     ui::Checked,
     ui_widgets::{Checkbox, RadioButton, RadioGroup, TextInput, ValueChange},
     window::{CursorIcon, PrimaryWindow, SystemCursorIcon},
@@ -30,7 +33,13 @@ fn main() {
         .add_systems(Startup, setup)
         .add_systems(
             Update,
-            refresh_character.run_if(resource_changed::<Character>),
+            (
+                // Updates the Model if the user changed the name via text input
+                on_changed_editable_text,
+                // Updates the View after any Model changes
+                refresh_character.run_if(resource_changed::<Character>),
+            )
+                .chain(),
         )
         .run();
 }
@@ -39,12 +48,15 @@ fn setup(mut commands: Commands, character: Res<Character>) {
     commands.spawn_scene_list(bsn_list! {
         Camera2d,
 
-        // This scene will serve as our "Controller" in our MVC design.
-        // The user interacts with the controller, which will affect the
-        // "View" (the rendered character).
+        // This scene will serve as one half of our "View"
+        // and all of the "Controller" in our MVC design.
+        // The user interacts with UI widgets, which are processed by the "Controller"
+        // via observers and other systems. The observers and systems update the
+        // state and also update the look of the UI widgets
+        // (i.e. changing text color of a selected option, inserting an X).
         ui(&character),
 
-        // This scene will serve as our "View" in our MVC design.
+        // This scene will serve as the other half of our "View" in our MVC design.
         // The user will see the character they are creating.
         character_view(&character),
     })
@@ -103,10 +115,10 @@ struct CharacterView;
 
 // --- END MARKER COMPONENTS --- //
 
-// --- START CONTROLLER --- //
+// --- WIDGET VIEW & CONTROLLER --- //
 
-/// Spawns the ui widgets that will serve as the "Controller"
-/// in our MVC design.
+/// Spawns the ui widgets that will serve as half of our "View",
+/// with "Controller" logic as important observers in our MVC design.
 fn ui(character: &Character) -> impl Scene {
     bsn! {
         // UI will take up the left half of the screen.
@@ -137,6 +149,8 @@ fn ui(character: &Character) -> impl Scene {
                     Text::new("Character Creator")
                 ],
 
+                name_text_input_row(character),
+
                 hat_type_radio_group(character),
 
                 tint_yellow_checkbox_row(character),
@@ -145,7 +159,8 @@ fn ui(character: &Character) -> impl Scene {
     }
 }
 
-/// An observer that styles the cursor, used primarily for buttons / checkboxes
+/// An observer that styles the cursor, used primarily for buttons / checkboxes.
+/// This is not part of the Controller.
 fn on_pointer_over_pointer_cursor(
     _event: On<Pointer<Over>>,
     mut window_q: Query<Entity, With<PrimaryWindow>>,
@@ -158,7 +173,22 @@ fn on_pointer_over_pointer_cursor(
     }
 }
 
-/// An observer that styles the cursor, used for all widgets
+/// An observer that styles the cursor, used primarily for text input.
+/// This is not part of the Controller.
+fn on_pointer_over_text_cursor(
+    _event: On<Pointer<Over>>,
+    mut window_q: Query<Entity, With<PrimaryWindow>>,
+    mut commands: Commands,
+) {
+    for window in window_q.iter_mut() {
+        commands
+            .entity(window)
+            .insert(CursorIcon::System(SystemCursorIcon::Text));
+    }
+}
+
+/// An observer that styles the cursor, used for all widgets.
+/// This is not part of the Controller.
 fn on_pointer_out_default_cursor(
     _event: On<Pointer<Out>>,
     mut window_q: Query<Entity, With<PrimaryWindow>>,
@@ -170,6 +200,71 @@ fn on_pointer_out_default_cursor(
             .insert(CursorIcon::System(SystemCursorIcon::Default));
     }
 }
+
+// --- START TEXT INPUT -- //
+
+/// Creates the text input that allows the user to input a name for the character.
+fn name_text_input_row(character: &Character) -> impl Scene {
+    bsn! {
+        Node {
+            flex_direction: FlexDirection::Row,
+            align_items: AlignItems::Center,
+            justify_content: JustifyContent::SpaceBetween,
+        }
+        Children [
+            Node
+            Children [
+                Text::new("Name: ")
+            ],
+
+            name_text_input(character)
+        ]
+    }
+}
+
+fn name_text_input(character: &Character) -> impl Scene {
+    let name = character.name.clone();
+    bsn! {
+        Node {
+            width: px(200),
+            border: px(5),
+            border_radius: BorderRadius::all(px(10)),
+            padding: UiRect::axes(px(5), px(2)),
+        }
+        NameInput
+        TextInput
+        EditableText::new(name)
+        // TabIndex Component is necessary for the text input to receive focus for typing.
+        TabIndex(0)
+        // This is necessary so that the text cursor pops up when the input has focus.
+        TextCursorStyle::default()
+        BackgroundColor(Color::BLACK)
+        on(on_pointer_over_text_cursor)
+        on(on_pointer_out_default_cursor)
+    }
+}
+
+/// A system that implements Controller logic to update the Name Input.
+/// This particular system updates the Model upon any change in value to the text input's `EditableText`.
+/// The text input widget does not regularly emit any events on value change. Instead, the
+/// `EditableText`'s value can be polled under app-specific conditions to update the Model when appropriate.
+fn on_changed_editable_text(
+    name_input_q: Query<&EditableText, With<NameInput>>,
+    mut character: ResMut<Character>,
+) {
+    let Ok(editable_text) = name_input_q.single_inner() else {
+        return;
+    };
+    let new_name = editable_text.value().to_string();
+    if character.name != new_name {
+        character.name = new_name;
+    }
+
+    // We do not need to update the ui widget view because it updates itself.
+    // However, that is not the norm with headless widgets!
+}
+
+// --- END TEXT INPUT -- //
 
 // --- START RADIO GROUP -- //
 
@@ -183,7 +278,7 @@ fn hat_type_radio_group(character: &Character) -> impl Scene {
         }
         RadioGroup
         HatTypeRadioGroup
-        // This observer is important -- it reacts to the user's input!
+        // This observer is part of the Controller -- it reacts to the user's input!
         on(on_value_change_hat_type)
         Children [
             Node
@@ -237,6 +332,7 @@ fn hat_type_radio_button(hat_type: HatType, character: &Character) -> Box<dyn Sc
     }
 }
 
+/// This observer is part of the Controller logic.
 /// This observer will update the `Character` Resource based on a change to the Hat Type Radio Group.
 /// Radio groups emit a ValueChange<Entity> event when the user clicks on a radio button.
 /// The value of the event is of the clicked radio button.
@@ -267,7 +363,7 @@ fn on_value_change_hat_type(
     // Update the Model
     character.hat_type = *new_hat_type;
 
-    // Update the Controller
+    // Update the Widget portion of the View
     for (button_entity, hat_type, has_checked, children) in hat_type_value_q.iter() {
         if character.hat_type == *hat_type {
             commands.entity(button_entity).insert(Checked);
@@ -280,7 +376,7 @@ fn on_value_change_hat_type(
             commands.entity(children[0]).insert(TextColor(Color::WHITE));
         }
     }
-    // Because character has been modified, refresh_character will run and update the "View".
+    // Because character has been modified, refresh_character will run and update the other half of the "View".
 }
 
 // --- END RADIO GROUP -- //
@@ -317,7 +413,7 @@ fn tint_yellow_checkbox(character: &Character) -> Box<dyn Scene> {
             BackgroundColor(Color::WHITE)
             on(on_pointer_over_pointer_cursor)
             on(on_pointer_out_default_cursor)
-            // This observer is important -- it reacts to the user's input!
+            // This observer is part of the controller -- it reacts to the user's input!
             on(on_value_change_tint_yellow)
         }
     };
@@ -342,6 +438,7 @@ fn tint_yellow_checkbox(character: &Character) -> Box<dyn Scene> {
     }
 }
 
+/// This observer is part of the Controller logic.
 /// This observer will update the `Character` Resource based on a change to the Tint Yellow Checkbox.
 /// Checkboxes emit a ValueChange<bool> event when the user clicks on a checkbox.
 /// The value of the event is the value of the toggle.
@@ -366,7 +463,7 @@ fn on_value_change_tint_yellow(
     // Update the Model
     character.tint_yellow = event.value;
 
-    // Update the Controller
+    // Update the Widget portion of the View
     if character.tint_yellow {
         commands.entity(event.source).insert(Checked);
         // The checkbox only has one child for the X and its color
@@ -375,14 +472,14 @@ fn on_value_change_tint_yellow(
         commands.entity(event.source).remove::<Checked>();
         commands.entity(children[0]).insert(Text::new(" "));
     }
-    // Because character has been modified, refresh_character will run and update the "View".
+    // Because character has been modified, refresh_character will run and update the other half of the "View".
 }
 
 // --- END CHECK BOX -- //
 
-// --- END CONTROLLER --- //
+// --- END WIDGET VIEW & CONTROLLER --- //
 
-// --- START VIEW --- //
+// --- START CHARACTER VIEW --- //
 
 /// A system that updates the "View" whenever the "Model" has changed.
 fn refresh_character(
@@ -470,4 +567,4 @@ fn character_name_and_age(character: &Character) -> impl Scene {
     }
 }
 
-// --- END VIEW --- //
+// --- END CHARACTER VIEW --- //
