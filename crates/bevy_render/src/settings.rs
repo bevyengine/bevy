0d778c80cc0f1a7e4216719c30de5876fff4a213
Luo Zhiaho <luo_zhihao@outlook.com>
2026-08-05 10:06:53 +0800

    Revert "remove TEXTURE_ADAPTER_SPECIFIC_FORMAT_FEATURES from default"
    This reverts commit e1ef061b30df856e27652d43e1d9b9937711c396.

--- crates/bevy_render/src/settings.rs
+++ crates/bevy_render/src/settings.rs
@@ -148,7 +148,7 @@ impl Default for WgpuSettings {
             backends,
             power_preference,
             priority,
-            features: wgpu::Features::empty(),
+            features: wgpu::Features::TEXTURE_ADAPTER_SPECIFIC_FORMAT_FEATURES,
             disabled_features: None,
             limits,
             constrained_limits: None,
