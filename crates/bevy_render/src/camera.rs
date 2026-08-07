ff3eb5749ca442436b5b9e6e91e40b595d5a9cdd
Sapein <5852983+Sapein@users.noreply.github.com>
2026-08-07 10:31:04 -0500

    Chain camera_system and box_cameras.

--- crates/bevy_render/src/camera.rs
+++ crates/bevy_render/src/camera.rs
@@ -73,11 +73,12 @@ impl Plugin for CameraPlugin {
             ))
             .add_systems(
                 PostStartup,
-                (camera_system, box_cameras).in_set(CameraUpdateSystems),
+                (camera_system, box_cameras).chain().in_set(CameraUpdateSystems),
             )
             .add_systems(
                 PostUpdate,
                 (camera_system, box_cameras)
+                    .chain()
                     .in_set(CameraUpdateSystems)
                     .before(AssetEventSystems)
                     .before(visibility::update_frusta),
