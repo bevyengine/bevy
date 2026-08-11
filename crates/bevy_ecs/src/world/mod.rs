9c2591103ffeb14c8c77d035e49fac8a1f7375b3
JaySpruce <jsprucebruce@gmail.com>
2026-08-10 21:09:05 -0500

    Add earlier early-outs to bundle and component registration (#25295)
    ## Objective
    
    `World::register_bundle` is called near the beginning of every
    bundle-related operation (i.e. `insert` or `remove`) to get the
    `BundleId` of the bundle, or register the bundle if it's new. However,
    the check for whether it already exists only happens after a
    `ComponentsRegistrator` is created, which happens to contain a `Vec`.
    The registrator isn't necessary to check if the bundle exists, so
    theoretically we're creating an empty `Vec` for no reason at every
    insertion or similar operation.
    
    ## Solution
    
    Add earlier outs, including other similar situations.
    
    A little bit of performance:
    <img width="633" height="201" alt="Screenshot_20260804_131431"
    src="https://github.com/user-attachments/assets/544785ed-0a9e-46b5-9eac-cd3b7e1c7934"
    />

--- crates/bevy_ecs/src/world/mod.rs
+++ crates/bevy_ecs/src/world/mod.rs
@@ -324,6 +324,11 @@ impl World {
     /// happens automatically during system initialization.
     #[doc(alias = "register_resource")]
     pub fn register_component<T: Component>(&mut self) -> ComponentId {
+        // This is a hot path, so return early to avoid the `Vec::new` in `ComponentsRegistrator`
+        if let Some(id) = self.component_id::<T>() {
+            return id;
+        }
+
         self.components_registrator().register_component::<T>()
     }
 
@@ -3389,6 +3394,11 @@ impl World {
     }
 
     pub(crate) fn register_bundle_info<B: Bundle>(&mut self) -> BundleId {
+        // This is a hot path, so return early to avoid the `Vec::new` in `ComponentsRegistrator`
+        if let Some(bundle_id) = self.bundles.get_id(TypeId::of::<B>()) {
+            return bundle_id;
+        }
+
         // SAFETY: These come from the same world. `Self.components_registrator` can't be used since we borrow other fields too.
         let mut registrator =
             unsafe { ComponentsRegistrator::new(&mut self.components, &mut self.component_ids) };
@@ -3401,6 +3411,11 @@ impl World {
     }
 
     pub(crate) fn register_contributed_bundle_info<B: Bundle>(&mut self) -> BundleId {
+        // This is a hot path, so return early to avoid the `Vec::new` in `ComponentsRegistrator`
+        if let Some(bundle_id) = self.bundles.get_contributed_bundle_id(TypeId::of::<B>()) {
+            return bundle_id;
+        }
+
         // SAFETY: These come from the same world. `Self.components_registrator` can't be used since we borrow other fields too.
         let mut registrator =
             unsafe { ComponentsRegistrator::new(&mut self.components, &mut self.component_ids) };
