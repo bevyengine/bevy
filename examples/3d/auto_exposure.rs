c4d92594daec21f8b4599269f8920d49e5c71cf6
aviac <aviac@mailbox.org>
2026-08-12 11:37:10 +0200

    fix compiler errors in all other crates that depend on curves

--- examples/3d/auto_exposure.rs
+++ examples/3d/auto_exposure.rs
@@ -12,8 +12,9 @@
 //! | `V`                | Visualize Metering Mask                |
 
 use bevy::{
+    curve::cubic_splines::LinearSpline,
     light::Skybox,
-    math::{cubic_splines::LinearSpline, vec2},
+    math::vec2,
     post_process::auto_exposure::{
         AutoExposure, AutoExposureCompensationCurve, AutoExposurePlugin,
     },
