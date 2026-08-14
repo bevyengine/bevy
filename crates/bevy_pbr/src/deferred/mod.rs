91a09dccacce3a184e0ed814ea079ce8e762e310
Lampan-git <erikwallhede@gmail.com>
2026-08-14 16:06:46 +0200

    Change time_span to pass_span in deferred_lighting

--- crates/bevy_pbr/src/deferred/mod.rs
+++ crates/bevy_pbr/src/deferred/mod.rs
@@ -151,7 +151,6 @@ pub fn deferred_lighting(
 
     let diagnostics = ctx.diagnostic_recorder();
     let diagnostics = diagnostics.as_deref();
-    let time_span = diagnostics.time_span(ctx.command_encoder(), "deferred_lighting");
 
     let bind_group_2 = ctx.render_device().create_bind_group(
         "deferred_lighting_layout_group_2",
@@ -174,6 +173,7 @@ pub fn deferred_lighting(
         occlusion_query_set: None,
         multiview_mask: None,
     });
+    let pass_span = diagnostics.pass_span(&mut render_pass, "deferred_lighting");
 
     render_pass.set_render_pipeline(pipeline);
 
@@ -185,8 +185,7 @@ pub fn deferred_lighting(
     render_pass.set_bind_group(1, &mesh_view_bind_group.binding_array, &[]);
     render_pass.set_bind_group(2, &bind_group_2, &[]);
     render_pass.draw(0..3, 0..1);
-    drop(render_pass);
-    time_span.end(ctx.command_encoder());
+    pass_span.end(&mut render_pass);
 }
 
 #[derive(Resource)]
