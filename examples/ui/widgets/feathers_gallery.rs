236649e8f26190ce52e71edd3078c996bee921ad
ickshonpe <david.curthoys@googlemail.com>
2026-08-21 09:14:03 +0100

    Reverted example changes included by mistake.

--- examples/ui/widgets/feathers_gallery.rs
+++ examples/ui/widgets/feathers_gallery.rs
@@ -88,8 +88,8 @@ fn scene() -> impl SceneList {
     bsn_list![Camera2d, demo_root()]
 }
 
-fn demo_root() -> Box<dyn Scene> {
-    Box::new(bsn! {
+fn demo_root() -> impl Scene {
+    bsn! {
         Node {
             width: percent(100),
             height: percent(100),
@@ -105,7 +105,7 @@ fn demo_root() -> Box<dyn Scene> {
             demo_column_1(),
             demo_column_2(),
         ]
-    })
+    }
 }
 
 #[derive(Component, Debug, Clone, Default, PartialEq)]
