eba7b0e75e0ff097c63a89878ea8847ea1df8b1f
Mona Mayrhofer <mona.mayrhofer@proton.me>
2026-08-22 20:41:41 +0200

    Fix bevy not exporting bevy::extract

--- crates/bevy_internal/src/lib.rs
+++ crates/bevy_internal/src/lib.rs
@@ -41,6 +41,8 @@ pub use bevy_core_pipeline as core_pipeline;
 pub use bevy_dev_tools as dev_tools;
 pub use bevy_diagnostic as diagnostic;
 pub use bevy_ecs as ecs;
+#[cfg(feature = "bevy_extract")]
+pub use bevy_extract as extract;
 #[cfg(feature = "bevy_feathers")]
 pub use bevy_feathers as feathers;
 #[cfg(feature = "bevy_gilrs")]
