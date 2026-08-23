9062c8abe412053ef352809ba2950c8d2e654aaf
JaySpruce <jsprucebruce@gmail.com>
2026-08-23 10:49:58 -0500

    silence warning about `ImmutableSparseSet::iter_mut` being unused

--- crates/bevy_ecs/src/storage/sparse_set.rs
+++ crates/bevy_ecs/src/storage/sparse_set.rs
@@ -556,6 +556,14 @@ pub(crate) struct ImmutableSparseSet<I, V: 'static> {
 
 macro_rules! impl_sparse_set {
     ($ty:ident) => {
+        #[expect(
+            clippy::allow_attributes,
+            reason = "Some instances are `pub` and can't be considered unused."
+        )]
+        #[allow(
+            unused,
+            reason = "This is an implementation macro that exists to avoid code duplication, and some implementors are private and don't use every method."
+        )]
         impl<I: SparseSetIndex, V> $ty<I, V> {
             /// Returns the number of elements in the sparse set.
             #[inline]
