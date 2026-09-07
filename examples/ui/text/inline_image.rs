0c49caaf830ae13a8211d8b2c7c8fbaef1ecdd5c
ickshonpe <david.curthoys@googlemail.com>
2026-09-07 11:47:58 +0100

    updated example

--- examples/ui/text/inline_image.rs
+++ examples/ui/text/inline_image.rs
@@ -13,7 +13,14 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn((
         Text::new("[Text]"),
         Node {
-            margin: auto().all(),
+            margin: px(25).all(),
+            justify_self: JustifySelf::Center,
+            align_self: AlignSelf::Center,
+            ..default()
+        },
+        BackgroundColor(bevy::color::palettes::css::MAROON.into()),
+        Outline {
+            width: px(2),
             ..default()
         },
         children![
