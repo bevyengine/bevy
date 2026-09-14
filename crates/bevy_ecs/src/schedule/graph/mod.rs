869007cfd173e2d54d287ca550c6c5fedd94e744
Trashtalk217 <trashtalk217@gmail.com>
2026-09-14 02:51:29 +0200

    Apply batched suggestions from code review
    Co-authored-by: Christian Hughes <9044780+ItsDoot@users.noreply.github.com>

--- crates/bevy_ecs/src/schedule/graph/mod.rs
+++ crates/bevy_ecs/src/schedule/graph/mod.rs
@@ -37,15 +37,17 @@ impl Dependency {
         }
     }
 
-    // Marks de dependency as weak.
-    // A weak dependency allows systems to run in parallel if they do not conflict.
+    /// Marks the dependency as weak.
+    /// A weak dependency allows systems to run in parallel if they do not conflict.
     pub fn set_weak(mut self) -> Self {
         self.is_weak = true;
         self
     }
 
-    // Marks the dependency to ignore deferred commands between systems.
-    // This ensures that there will not be a sync point between systems, where commands get flushed.
+    /// Marks the dependency to ignore deferred commands between systems.
+    /// This tells the [`AutoInsertApplyDeferredPass`] to ignore this dependency when considering sync points.
+    ///
+    /// [`AutoInsertApplyDeferredPass`]: crate::schedule::AutoInsertApplyDeferredPass
     pub fn ignore_deferred(mut self) -> Self {
         self.ignore_deferred = true;
         self
