84473f6b47f5485d8e2e2a9113816b19b63baaed
ickshonpe <david.curthoys@googlemail.com>
2026-09-19 11:34:55 +0100

    Doc comment edit

--- crates/bevy_text/src/text.rs
+++ crates/bevy_text/src/text.rs
@@ -617,7 +617,7 @@ impl<const N: usize> From<[FontSource; N]> for FontSource {
     }
 }
 
-/// Holds the default font source. Used when `Fontsource` is set to `Default`.
+/// The default font source used to resolve `FontSource::Default`.
 /// `DefaultFontSource(FontSource::Default)` is mapped to the default Font asset.
 #[derive(Clone, Debug, Reflect, PartialEq, Default, Resource, Deref, DerefMut)]
 pub struct DefaultFontSource(pub FontSource);
