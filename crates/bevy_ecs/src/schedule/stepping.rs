90f5b4703cbf56bd6f55885362a532127ea61c3d
Trashtalk <trashtalk217@gmail.com>
2026-09-21 19:55:21 +0200

    forgot to uncomment a line of code

--- crates/bevy_ecs/src/schedule/stepping.rs
+++ crates/bevy_ecs/src/schedule/stepping.rs
@@ -1029,7 +1029,7 @@ impl ScheduleState {
     }
 }
 
-// #[cfg(all(test, feature = "bevy_debug_stepping"))]
+#[cfg(all(test, feature = "bevy_debug_stepping"))]
 #[expect(clippy::print_stdout, reason = "Allowed in tests.")]
 mod tests {
     use super::*;
