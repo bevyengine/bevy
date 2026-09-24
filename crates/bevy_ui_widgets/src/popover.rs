6061f110bd05e9398eaebacf86657abed670f276
ickshonpe <david.curthoys@googlemail.com>
2026-09-24 15:05:36 +0100

    Removed unused

--- crates/bevy_ui_widgets/src/popover.rs
+++ crates/bevy_ui_widgets/src/popover.rs
@@ -13,8 +13,8 @@ use bevy_ecs::{
 use bevy_math::{Affine2, Rect, Vec2};
 use bevy_reflect::Reflect;
 use bevy_ui::{
-    update_border_radius, update_computed_nodes, ComputedNode, ComputedUiRenderTargetInfo, Node,
-    PositionType, UiGlobalTransform, UiSystems, UiTransform, Val2,
+    ComputedNode, ComputedUiRenderTargetInfo, Node, PositionType, UiGlobalTransform, UiSystems,
+    UiTransform, Val2,
 };
 
 use crate::update_scrollbar_thumb;
