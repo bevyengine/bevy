3d75a57a371ebffa9f6274745c89bc6b0eb9e38f
Matt Good <29016+mgood@users.noreply.github.com>
2026-09-25 15:18:52 -0700

    add cfg guards to unused imports if all atomics are missing

--- crates/bevy_reflect/src/impls/core/sync.rs
+++ crates/bevy_reflect/src/impls/core/sync.rs
@@ -1,3 +1,4 @@
+#[cfg(target_has_atomic)]
 use crate::{
     error::ReflectCloneError,
     info::{OpaqueInfo, TypeInfo, Typed},
@@ -8,10 +9,14 @@ use crate::{
     type_registry::{GetTypeRegistration, ReflectFromPtr, TypeRegistration},
     utility::NonGenericTypeInfoCell,
 };
+#[cfg(target_has_atomic)]
 use bevy_platform::prelude::*;
+#[cfg(target_has_atomic)]
 use bevy_reflect_derive::impl_type_path;
+#[cfg(target_has_atomic)]
 use core::fmt;
 
+#[cfg(target_has_atomic)]
 macro_rules! impl_reflect_for_atomic {
     ($ty:ty, $ordering:expr) => {
         impl_type_path!($ty);
